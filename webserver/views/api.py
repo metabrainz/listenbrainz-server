df21bcdef17bd0d31b0f9a16446262835028b64a
Robert Kaye <rob@musicbrainz.org>
2015-09-05 17:29:38 +0100

    Interim check-in

--- webserver/views/api.py
+++ webserver/views/api.py
@@ -8,17 +8,35 @@ import json
 
 api_bp = Blueprint('api', __name__)
 
-@api_bp.route("/<uuid:mbid>/low-level", methods=["POST"])
-def submit_low_level(mbid):
-    """Endpoint for submitting low-level information to AcousticBrainz."""
+def validate_listen(listen):
+
+
+@api_bp.route("/post/listen/<userid>", methods=["POST"])
+def submit_listen(mbid):
+    """Endpoint for submitting a listen to ListenBrainz."""
+
     raw_data = request.get_data()
     try:
         data = json.loads(raw_data.decode("utf-8"))
     except ValueError as e:
         raise BadRequest("Cannot parse JSON document: %s" % e)
 
+    # Sanity check the submission
+    payload = data['payload']
+    if len(payload) == 0:
+        raise BadRequest("JSON document does not contain any listens.")
+
+    if (data['listen_type'] == "single" or data['listen_type'] == 'playing_now') and len(payload) > 1:
+        raise BadRequest("JSON document contains more than listen for a single/playing_now. "
+                         "It should contain only one. ")
+
+    for i, listen in enumerate(payload):
+        err = validate_listen(listen):
+        if not err:
+            raise BadRequest("payload index %d error: " + err)
+
     try:
-        submit_low_level_data(mbid, data)
+        print data
     except BadDataException as e:
         raise BadRequest(e)
     return ""
