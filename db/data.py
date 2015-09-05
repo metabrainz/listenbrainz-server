53e858023d3e4736d49d493231306f6ee715e9ee
Alastair Porter <alastair@porter.net.nz>
2015-09-05 21:42:08 +0100

    Implement submit and get endpoints

--- db/data.py
+++ db/data.py
@@ -1,10 +1,59 @@
 from hashlib import sha256
 import logging
-import copy
-import time
 import json
-import os
 import db
 import db.exceptions
+import uuid
 
-# TODO: Add database access functionality.
+def get_id_from_scribble(data):
+    data_json = json.dumps(data, sort_keys=True, separators=(',', ':'))
+    data_sha256 = sha256(data_json.encode("utf-8")).hexdigest()
+
+    with db.create_cursor() as cursor:
+        cursor.execute("""SELECT s.gid
+                          FROM scribble s
+                     LEFT JOIN scribble_json sj
+                            ON sj.id = s.data
+                        WHERE sj.data_sha256 = %s""", (data_sha256, ))
+
+        if cursor.rowcount:
+            return cursor.fetchone()["gid"]
+        else:
+            return None
+
+def submit_scribble(data):
+    data_json = json.dumps(data, sort_keys=True, separators=(',', ':'))
+    data_sha256 = sha256(data_json.encode("utf-8")).hexdigest()
+
+    with db.create_cursor() as cursor:
+        cursor.execute("""INSERT INTO scribble_json (data, data_sha256)
+                            VALUES (%s, %s)
+                         RETURNING id""", (data_json, data_sha256))
+        id = cursor.fetchone()["id"]
+        gid = str(uuid.uuid4())
+        cursor.execute("""INSERT INTO scribble (gid, data, submitted)
+                            VALUES (%s, %s, now())""", (gid, id))
+
+        db.commit()
+        return gid
+
+def load_scribble(messybrainz_id):
+    with db.create_cursor() as cursor:
+        query = """SELECT sj.data
+                        , r.musicbrainz_recording_id
+                     FROM scribble_json sj
+                LEFT JOIN scribble s
+                       ON sj.id = s.data
+                LEFT JOIN scribble_cluster sc
+                       ON sc.gid = s.gid
+                LEFT JOIN redirect r
+                       ON r.cluster_id = sc.cluster_id
+                    WHERE s.gid = %s"""
+        cursor.execute(query, (str(messybrainz_id), ))
+        if not cursor.rowcount:
+            raise db.exceptions.NoDataFoundException
+        row = cursor.fetchone()
+        data = row["data"]
+        recording_id = row["musicbrainz_recording_id"]
+        data["musicbrainz_recording_id"] = recording_id
+        return data
--- webserver/views/api.py
+++ webserver/views/api.py
@@ -1,6 +1,10 @@
 from __future__ import absolute_import
-from flask import Blueprint
+from flask import Blueprint, request, Response, jsonify
 from webserver.decorators import crossdomain
+from werkzeug.exceptions import BadRequest, NotFound
+import db.data
+import db.exceptions
+import json
 
 api_bp = Blueprint('api', __name__)
 
@@ -8,14 +12,45 @@ api_bp = Blueprint('api', __name__)
 @api_bp.route("/submit", methods=["POST"])
 @crossdomain()
 def submit():
-    raise NotImplementedError
+    raw_data = request.get_data()
+    try:
+        data = json.loads(raw_data.decode("utf-8"))
+    except ValueError as e:
+        raise BadRequest("Cannot parse JSON document: %s" % e)
+
+    try:
+        gid = db.data.get_id_from_scribble(data)
+        if gid:
+            status = "known"
+            data = db.data.load_scribble(gid)
+            musicbrainz_recording_id = data["musicbrainz_recording_id"]
+            if musicbrainz_recording_id:
+                status = "matched"
+        else:
+            status = "new"
+            musicbrainz_recording_id = None
+            gid = db.data.submit_scribble(data)
+        response = {"status": status,
+                    "musicbrainz_recording_id": musicbrainz_recording_id,
+                    "messybrainz_id": gid
+                   }
+        return jsonify(response)
+    except db.exceptions.BadDataException as e:
+        raise BadRequest(e)
 
 
 @api_bp.route("/<uuid:messybrainz_id>")
 @crossdomain()
 def get(messybrainz_id):
-    raise NotImplementedError
-
+    try:
+        data = db.data.load_scribble(messybrainz_id)
+        status = "unmatched"
+        if data["musicbrainz_recording_id"]:
+            status = "matched"
+        response = {"status": status, "payload": data}
+        return jsonify(response)
+    except db.exceptions.NoDataFoundException:
+        raise NotFound
 
 @api_bp.route("/<uuid:messybrainz_id>/aka")
 @crossdomain()
