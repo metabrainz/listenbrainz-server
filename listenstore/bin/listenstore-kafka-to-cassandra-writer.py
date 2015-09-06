394e5b7c3d18da9bc4bd619e3df7b9b6c2bc6061
RJ <rj@metabrew.com>
2015-09-06 18:51:26 +0000

    print insert progress periodically

--- listenstore/bin/listenstore-kafka-to-cassandra-writer.py
+++ listenstore/bin/listenstore-kafka-to-cassandra-writer.py
@@ -5,9 +5,16 @@ from listenstore.cli import Command
 class KafkaToCassandra(Command):
     desc = "Print listens fetched from kafka"
 
+    def __init__(self):
+        super(KafkaToCassandra, self).__init__()
+        self.inserts = 0
+
     def received_listen(self, listen):
-        self.log("INSERTING: %s" % repr(listen))
+        self.log.debug("INSERTING: %s" % repr(listen))
         self.listenStore.insert(listen)
+        self.inserts += 1
+        if self.inserts % 100 == 0:
+            self.log.info("inserted from kafka -> cassandra: %d" % self.inserts)
 
     def run(self):
         self.kafkaConsumer.start_listens(self.received_listen)
