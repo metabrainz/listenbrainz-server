f3bda2ce9dd7800c09e007d4a67c121147ad2e11
Robert Kaye <rob@musicbrainz.org>
2015-09-06 10:50:25 +0100

    That is probably enough sanity checking for this minute.

--- webserver/views/api.py
+++ webserver/views/api.py
@@ -7,12 +7,43 @@ from kconn import _kafka
 
 api_bp = Blueprint('listen', __name__)
 
-#def validate_listen(listen):
+MAX_LISTEN_SIZE = 10240    # overall listen size, to prevent egrigrious spamming
+MAX_TAGS_PER_LISTEN = 50
+MAX_TAG_SIZE = 64
+
+def validate_listen(listen):
+    """ Make sure that required keys are present, filled out and not too large."""
+
+    print json.dumps(listen, indent=4)
+
+    if 'listened_at' in listen and 'track_metadata' in listen and len(listen) > 2:
+        raise BadRequest("JSON document may only contain listened_at and track_metadata top level keys.")
+
+    # Validate basic metadata
+    try:
+        if not listen['track_metadata']['track_name']:
+            raise BadRequest("JSON document does not contain required track_metadata.track_name")
+
+        if not listen['track_metadata']['artist_name']:
+            raise BadRequest("JSON document does not contain required track_metadata.artist_name")
+
+    except KeyError:
+        raise BadRequest("JSON document does not contain a valid metadata.track_name and/or track_metadata.artist_name")
+
+    # Validate tags
+    if 'additional_info' in listen['track_metadata']:
+        tags = listen['track_metadata']['additional_info']['tags']
+        if len(tags) > MAX_TAGS_PER_LISTEN:
+            raise BadRequest("JSON document may not contain more than %d items in track_metadata.additional_info.tags" % MAX_TAGS_PER_LISTEN)
+
+        for tag in tags:
+            if len(tag) > MAX_TAG_SIZE:
+                raise BadRequest("JSON document may not contain track_metadata.additional_info.tags longer than %d characters." % MAX_TAG_SIZE)
 
 # TODO: ensure that we're logged in when we get to the oauth bit
 @api_bp.route("/listen/user/<user_id>", methods=["POST"])
 def submit_listen(user_id):
-    """Endpoint for submitting a listen to ListenBrainz."""
+    """Endpoint for submitting a listen to ListenBrainz. Sanity check listen and then pass on to Kafka."""
 
     raw_data = request.get_data()
     try:
@@ -21,21 +52,29 @@ def submit_listen(user_id):
         raise BadRequest("Cannot parse JSON document: %s" % e)
 
     # Sanity check the submission
-    payload = data['payload']
-    if len(payload) == 0:
-        raise BadRequest("JSON document does not contain any listens.")
+    try:
+        payload = data['payload']
+        if len(payload) == 0:
+            raise BadRequest("JSON document does not contain any listens.")
 
-    if (data['listen_type'] == "single" or data['listen_type'] == 'playing_now') and len(payload) > 1:
-        raise BadRequest("JSON document contains more than listen for a single/playing_now. "
-                         "It should contain only one. ")
+        if len(raw_data) > len(payload) * MAX_LISTEN_SIZE:
+            raise BadRequest("JSON document is too large. In aggregate, listens may not be larger than %d characters." % MAX_LISTEN_SIZE)
+
+        if data['listen_type'] not in ('playing_now', 'single', 'import'):
+            raise BadRequest("JSON document required a valid listen_type key")
+
+        if (data['listen_type'] == "single" or data['listen_type'] == 'playing_now') and len(payload) > 1:
+            raise BadRequest("JSON document contains more than listen for a single/playing_now. "
+                             "It should contain only one. ")
+    except KeyError:
+        raise BadRequest("Invalid JSON document submitted.")
+
 
     for i, listen in enumerate(payload):
-#        err = validate_listen(listen):
-#        if not err:
-#            raise BadRequest("payload index %d error: " + err)
+        validate_listen(listen)
 
         listen['user_id'] = user_id
-        # Catch exception here
+        # TODO: Catch exception here
         producer = SimpleProducer(_kafka)
         producer.send_messages(b'listens', json.dumps(listen).encode('utf-8'))
 
