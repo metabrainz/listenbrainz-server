f9527c920cc646b3bce5b74f67463278d46abcc9
Russ Garrett <russ@garrett.co.uk>
2015-09-06 16:37:15 +0100

    Install postgres 9.4

---
+++ chef/cookbooks/ourpostgres/attributes/default.rb
@@ -0,0 +1,4 @@
+default[:cassandra][:data_file_directories]  = [ '/var/lib/cassandra/data' ]
+default[:cassandra][:commitlog_directory]    = '/var/lib/cassandra/commitlog'
+default[:cassandra][:saved_caches_directory] = '/var/lib/cassandra/saved_caches'
+
