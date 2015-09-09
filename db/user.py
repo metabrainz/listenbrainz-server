b6bdcacd2401db692cc0ad5f6e81bc6d08b911b5
Roman Tsukanov <roman@tsukanov.me>
2015-09-09 12:39:16 +0200

    Go back to perviously used version of SQL query formatting.

--- db/user.py
+++ db/user.py
@@ -4,35 +4,30 @@ import uuid
 
 def create(musicbrainz_id):
     # TODO(roman): Do we need to make sure that musicbrainz_id is case insensitive?
-    result = db.session.execute(
-        'INSERT INTO "user" (musicbrainz_id, auth_token) VALUES (:mb_id, :token) '
-        'RETURNING id',
-        {"mb_id": musicbrainz_id, "token": str(uuid.uuid4())}
-    )
+    result = db.session.execute("""INSERT INTO "user" (musicbrainz_id, auth_token)
+                                        VALUES (:mb_id, :token)
+                                     RETURNING id""",
+                                {"mb_id": musicbrainz_id, "token": str(uuid.uuid4())})
     db.session.commit()
     return result.fetchone()["id"]
 
 
 def get(id):
     """Get user with a specified ID (integer)."""
-    result = db.session.execute(
-        'SELECT id, created, musicbrainz_id, auth_token '
-        'FROM "user" '
-        'WHERE id = :id',
-        {"id": id}
-    )
+    result = db.session.execute("""SELECT id, created, musicbrainz_id, auth_token
+                                     FROM "user"
+                                    WHERE id = :id""",
+                                {"id": id})
     row = result.fetchone()
     return dict(row) if row else None
 
 
 def get_by_mb_id(musicbrainz_id):
     """Get user with a specified MusicBrainz ID."""
-    result = db.session.execute(
-        'SELECT id, created, musicbrainz_id, auth_token '
-        'FROM "user" '
-        'WHERE LOWER(musicbrainz_id) = LOWER(:mb_id)',
-        {"mb_id": musicbrainz_id}
-    )
+    result = db.session.execute("""SELECT id, created, musicbrainz_id, auth_token
+                                     FROM "user"
+                                    WHERE LOWER(musicbrainz_id) = LOWER(:mb_id)""",
+                                {"mb_id": musicbrainz_id})
     row = result.fetchone()
     return dict(row) if row else None
 
