6a69b3f537be0cc7c180b5edec6ceea21aa490fb
Roman Tsukanov <roman@tsukanov.me>
2015-09-15 13:16:21 +0200

    Count can be None.

--- webserver/views/api.py
+++ webserver/views/api.py
@@ -112,10 +112,10 @@ def submit_listen(user_id):
 @api_bp.route("/listen/user/<user_id>")
 def get_listens(user_id):
     try:
-        count = int(request.args.get('count'))
+        count = int(request.args.get('count') or DEFAULT_ITEMS_PER_GET)
     except ValueError:
         raise BadRequest("Invalid count argument.")
-    count = min(count or DEFAULT_ITEMS_PER_GET, MAX_ITEMS_PER_GET)
+    count = min(count, MAX_ITEMS_PER_GET)
     max_ts = request.args.get('max_ts') or None
 
     cassandra = webserver.create_cassandra()
