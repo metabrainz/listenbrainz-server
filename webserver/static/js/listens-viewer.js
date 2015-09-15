a548170f9d78dcde4ef487ab298c6dc80609b63b
Roman Tsukanov <roman@tsukanov.me>
2015-09-15 18:24:06 +0200

    Add pagination to user page.

--- webserver/static/js/listens-viewer.js
+++ webserver/static/js/listens-viewer.js
@@ -6,7 +6,9 @@ var container = document.getElementById(CONTAINER_ELEMENT_ID);
 var ListensViewer = React.createClass({
     getInitialState: function () {
         return {
-            listens: null
+            userId: null,
+            listens: null,
+            page: 0
         };
     },
     componentDidMount: function() {
@@ -17,32 +19,76 @@ var ListensViewer = React.createClass({
         }
         $.get("/listen/user/" + container.dataset.userId, function(data) {
             if (this.isMounted()) {
-                this.setState({listens: data.payload.listens});
-                console.log(this.state.listens);
+                this.setState({
+                    userId: container.dataset.userId,
+                    listens: data.payload.listens
+                });
             }
         }.bind(this));
     },
     componentDidUpdate: function() {
         jQuery("abbr.timeago").timeago();
     },
+    handlePreviousPage: function (maxTimestamp) {
+       this.setState({listens: null});
+        $.get("/listen/user/" + container.dataset.userId + "?max_ts=" + maxTimestamp,
+            function(data) {
+                this.setState({
+                    listens: data.payload.listens,
+                    page: this.state.page - 1
+                });
+            }.bind(this)
+        );
+    },
+    handleNextPage: function (minTimestamp) {
+       this.setState({listens: null});
+        $.get("/listen/user/" + container.dataset.userId + "?min_ts=" + minTimestamp,
+            function(data) {
+                this.setState({
+                    listens: data.payload.listens,
+                    page: this.state.page + 1
+                });
+            }.bind(this)
+        );
+    },
     render: function () {
         if (this.state.listens) {
             var items = [];
             this.state.listens.forEach(function (listen) {
-                items.push(<Listen metadata={listen.track_metadata} timestamp={listen.listened_at} />);
+                items.push(<Listen
+                    key={listen.listened_at}
+                    metadata={listen.track_metadata}
+                    timestamp={listen.listened_at}
+                    />);
             }.bind(this));
             if (items.length > 0) {
                 return (
-                    <table className="recordings table table-condensed table-hover">
-                        <thead>
-                        <tr>
-                            <th>track</th>
-                            <th>artist</th>
-                            <th>time</th>
-                        </tr>
-                        </thead>
-                        <tbody>{items}</tbody>
-                    </table>
+                    <div>
+                        <table className="recordings table table-condensed table-hover">
+                            <thead>
+                            <tr>
+                                <th>track</th>
+                                <th>artist</th>
+                                <th>time</th>
+                            </tr>
+                            </thead>
+                            <tbody>{items}</tbody>
+                        </table>
+                        <ul className="pager">
+                            <PreviousPageButton
+                                key={"previous" + this.state.page}
+                                userId={this.state.userId}
+                                maxTimestamp={this.state.listens[0].listened_at + 1}
+                                onSwitch={this.handlePreviousPage}
+                                />
+                            <NextPageButton
+                                key={"next" + this.state.page}
+                                userId={this.state.userId}
+                                minTimestamp={this.state.listens[this.state.listens.length-1].listened_at - 1}
+                                onSwitch={this.handleNextPage}
+                                />
+                        </ul>
+                    </div>
                 );
             } else {
                 return (<p className="text-muted">No listens.</p>);
@@ -68,6 +114,73 @@ var Listen = React.createClass({
                 <td><abbr className="timeago" title={timestamp}></abbr></td>
             </tr>
         );
+    }
+});
+
+var PreviousPageButton = React.createClass({
+    propTypes: {
+        userId: React.PropTypes.string.isRequired,
+        maxTimestamp: React.PropTypes.number.isRequired,
+        onSwitch: React.PropTypes.func.isRequired
+    },
+    getInitialState: function () {
+        return {
+            enabled: false
+        };
+    },
+    componentDidMount: function() {
+        console.log("CHECKING IF PREV");
+        $.get("/listen/user/" + container.dataset.userId +
+              "?max_ts=" + this.props.maxTimestamp,
+            function(data) {
+                this.setState({enabled: data.payload.count > 0});
+            }.bind(this));
+    },
+    switchPage: function() {
+        this.props.onSwitch(this.props.maxTimestamp);
+    },
+    render: function () {
+        if (this.state.enabled) {
+            return <li className="previous">
+                <a href="#" onClick={this.switchPage}>&larr; Previous</a>
+            </li>;
+        } else {
+            return <li style={{display: 'none'}}></li>;
+        }
+    }
+});
+
+var NextPageButton = React.createClass({
+    propTypes: {
+        userId: React.PropTypes.string.isRequired,
+        minTimestamp: React.PropTypes.number.isRequired,
+        onSwitch: React.PropTypes.func.isRequired
+    },
+    getInitialState: function () {
+        return {
+            enabled: false
+        };
+    },
+    componentDidMount: function() {
+        console.log("CHECKING IF NEXT");
+        $.get("/listen/user/" + container.dataset.userId +
+              "?min_ts=" + this.props.minTimestamp,
+            function(data) {
+                this.setState({enabled: data.payload.count > 0});
+            }.bind(this));
+    },
+    switchPage: function() {
+        this.props.onSwitch(this.props.minTimestamp);
+    },
+    render: function () {
+        if (this.state.enabled) {
+            return <li className="next">
+                <a href="#" onClick={this.switchPage}>Next &rarr;</a>
+            </li>
+           ;
+        } else {
+            return <li style={{display: 'none'}}></li>;
+        }
     }
 });
 
