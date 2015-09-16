962a6a69951997a28f5d5c1addc01c8b2c03136c
Richard Jones <rj@metabrew.com>
2015-09-16 14:52:22 +0100

    make sure we install pg 9.4 correctly

--- chef/cookbooks/ourpostgres/recipes/default.rb
+++ chef/cookbooks/ourpostgres/recipes/default.rb
@@ -1,13 +1,29 @@
-node.set['postgresql']['enable_pgdg_apt'] = true
-node.set['postgresql']['version'] = '9.4'
+node.set[:postgresql][:enable_pgdg_apt] = true
+node.set[:postgresql][:version]         = "9.4"
+
 node.set['postgresql']['pg_hba'] = [
-   { :type => 'local', 
-     :db => 'all', 
-     :user => 'all', 
-     :addr => nil, 
+   { :type => 'local',
+     :db => 'all',
+     :user => 'all',
+     :addr => nil,
      :method => 'trust'
    }
 ]
+
+# copied from attributes of postgresql cookbook, because otherwise they
+# get set when the postgresql.version is still the default (not 9.4)
+# which causes all sorts of problems.
+# I think this is a bug/poor design in the postgresql cookbook.
+node.set[:postgresql][:config][:data_directory]="/var/lib/postgresql/#{node['postgresql']['version']}/main"
+node.set[:postgresql][:config][:ident_file]    ="/etc/postgresql/#{node['postgresql']['version']}/main/pg_ident.conf"
+node.set[:postgresql][:config][:hba_file]      ="/etc/postgresql/#{node['postgresql']['version']}/main/pg_hba.conf"
+node.set[:postgresql][:config][:external_pid_file]      ="/var/run/postgresql/#{node['postgresql']['version']}-main.pid"
+node.set[:postgresql][:config][:max_connections] = 100
+node.set[:postgresql][:dir] = "/etc/postgresql/#{node['postgresql']['version']}/main"
+node.set[:postgresql][:client][:packages] = ["postgresql-client-#{node['postgresql']['version']}","libpq-dev"]
+node.set[:postgresql][:server][:packages] = ["postgresql-#{node['postgresql']['version']}"]
+node.set[:postgresql][:contrib][:packages] = ["postgresql-contrib-#{node['postgresql']['version']}"]
+
 
 include_recipe "postgresql"
 include_recipe "postgresql::server"
