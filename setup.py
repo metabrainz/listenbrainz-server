9e379fd0e6bc4dba28a252479bae71b1ac2d9529
Robert Kaye <rob@musicbrainz.org>
2015-09-22 17:42:36 +0200

    Adding a first cut of setup.py. Most likely will need more install_requires

---
+++ setup.py
@@ -0,0 +1,20 @@
+import glob
+import os
+from setuptools import setup
+
+version = os.environ.get('RELEASE_VERSION', '99.0.0.dev0')
+
+setup(
+    name='messybrainz',
+    version=version,
+    author='MessyBrainz',
+    author_email='support@metabrainz.org',
+    packages=['messybrainz'],
+    scripts=[x for x in glob.glob('bin/*.py') if x != 'bin/__init__.py'],
+    license='LICENSE.txt',
+    description='python interface to the messybrainz database.',
+    install_requires=[
+        "SQLAlchemy == 1.0.8"
+    ],
+    zip_safe=False
+)
