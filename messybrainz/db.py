0fcd1dd4caf0ef7e9fa6c6412ab60ad3c4742095
Alastair Porter <alastair@porter.net.nz>
2015-09-22 20:31:50 +0200

    Use a Null sqlalchemy pool because we use pgbouncer

--- messybrainz/db.py
+++ messybrainz/db.py
@@ -1,4 +1,5 @@
 from sqlalchemy import create_engine
+from sqlalchemy.pool import NullPool
 
 # This value must be incremented after schema changes on replicated tables!
 SCHEMA_VERSION = 1
@@ -7,7 +8,7 @@ engine = None
 
 def init_db_engine(connect_str):
     global engine
-    engine = create_engine(connect_str)
+    engine = create_engine(connect_str, poolclass=NullPool)
 
 def run_sql_script(sql_file_path):
     with open(sql_file_path) as sql:
