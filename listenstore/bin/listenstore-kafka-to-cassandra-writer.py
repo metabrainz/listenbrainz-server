64f7033e8295d1b1c0a6d2c13ce2a53d2ab98883
Robert Kaye <rob@musicbrainz.org>
2015-09-23 15:59:28 +0200

    Implement cassandra batch writes

--- listenstore/bin/listenstore-kafka-to-cassandra-writer.py
+++ listenstore/bin/listenstore-kafka-to-cassandra-writer.py
@@ -9,17 +9,9 @@ class KafkaToCassandra(Command):
     def __init__(self):
         super(KafkaToCassandra, self).__init__()
         self.log = logging.getLogger(__name__)
-        self.inserts = 0
-
-    def received_listen(self, listen):
-        self.log.debug("INSERTING: %s" % repr(listen))
-        self.listenStore.insert(listen)
-        self.inserts += 1
-        if self.inserts % 100 == 0:
-            self.log.info("inserted from kafka -> cassandra: %d" % self.inserts)
 
     def run(self):
-        self.kafkaConsumer.start_listens(self.received_listen)
+        self.kafkaConsumer.start_listens(self.listenStore)
 
 
 if __name__ == '__main__':
