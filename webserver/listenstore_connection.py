c2caadbca4bce046f249ce17d0d1350bda22a8aa
Alastair Porter <alastair@porter.net.nz>
2015-10-13 20:45:21 +0200

    Remove unneeded variable definitions

--- webserver/listenstore_connection.py
+++ webserver/listenstore_connection.py
@@ -2,9 +2,7 @@ from listenstore import listenstore
 from flask import current_app
 
 def get_listenstore():
-    server = current_app.config['CASSANDRA_SERVER']
-    keyspace = current_app.config['CASSANDRA_KEYSPACE']
     return listenstore.ListenStore({
-        'cassandra_server': server,
-        'cassandra_keyspace': keyspace,
+        'cassandra_server': current_app.config['CASSANDRA_SERVER'],
+        'cassandra_keyspace': current_app.config['CASSANDRA_KEYSPACE'],
     })
