6098e353e67324baed8cfb743f1dc39f3afaa07e
Robert Kaye <rob@musicbrainz.org>
2016-06-21 13:04:07 +0200

    Nuke berkshit and chef!

--- chef/provision.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash -e
-if [ $(whoami) != "root" ]
-then
-    echo "Please be root."
-    exit 1
-fi
-set -x
-cd .. && berks vendor ./chef/vendored-cookbooks && cd -
-chef-solo --no-fork -c ./solo.rb --environment production -j ./node.json
