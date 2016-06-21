4a9302a79d0e8d6bbc69b8212edc0880bd8f9d0b
Robert Kaye <rob@musicbrainz.org>
2016-06-21 19:15:04 +0200

    Start removing kafka and also deal with further docker shenanigans

--- webserver/views/index.py
+++ webserver/views/index.py
@@ -55,26 +55,4 @@ def roadmap():
 def current_status():
 
     load = "%.2f %.2f %.2f" % os.getloadavg()
-
-    process = subprocess.Popen([current_app.config['KAFKA_RUN_CLASS_BINARY'], "kafka.tools.ConsumerOffsetChecker",
-        '--topic', 'listens', '--group', 'listen-group'], stdout=subprocess.PIPE)
-    out, err = process.communicate()
-
-    print out
-
-    lines = out.split("\n")
-    data = []
-    for line in lines:
-        if line.startswith("listen-group"):
-            data = line.split()
-
-    if len(data) >= 6:
-        kafka_stats = { 
-                        'offset' : locale.format("%d", int(data[3]), grouping=True),
-                        'size' : locale.format("%d", int(data[4]), grouping=True),
-                        'lag' : locale.format("%d", int(data[5]), grouping=True) 
-                      } 
-    else:
-        kafka_stats = { 'offset' : "(unknown/empty)", 'size' : "-", 'lag' : "-" } 
-
-    return render_template("index/current-status.html", load=load, kstats=kafka_stats)
+    return render_template("index/current-status.html", load=load)
