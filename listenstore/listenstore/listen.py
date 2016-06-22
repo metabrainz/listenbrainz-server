a38a5999340c2b8606e3ae6adee09c52ac95bfe4
Robert Kaye <rob@musicbrainz.org>
2016-06-22 19:38:04 +0200

    Moving the redis consumer further along, but still nothing gets written to the DB.

--- listenstore/listenstore/listen.py
+++ listenstore/listenstore/listen.py
@@ -5,9 +5,9 @@ import datetime
 
 class Listen(object):
     """ Represents a listen object """
-    def __init__(self, uid=None, timestamp=None, artist_msid=None, album_msid=None,
+    def __init__(self, user_id=None, timestamp=None, artist_msid=None, album_msid=None,
                  recording_msid=None, data=None):
-        self.uid = uid
+        self.user_id = user_id
         self.timestamp = timestamp
         self.artist_msid = artist_msid
         self.album_msid = album_msid
@@ -20,7 +20,7 @@ class Listen(object):
     @classmethod
     def from_json(cls, j):
         """Factory to make Listen() objects from a dict"""
-        return cls(  uid=j['user_id']
+        return cls(  user_id=j['user_id']
                   , timestamp=j['listened_at']
                   , artist_msid=j['track_metadata']['additional_info'].get('artist_msid')
                   , album_msid=j['track_metadata']['additional_info'].get('album_msid')
@@ -29,7 +29,7 @@ class Listen(object):
                   )
 
     def validate(self):
-        return (self.uid is not None and self.timestamp is not None and self.artist_msid is not None
+        return (self.user_id is not None and self.timestamp is not None and self.artist_msid is not None
                 and self.recording_msid is not None and self.data is not None)
 
     @property
@@ -40,5 +40,5 @@ class Listen(object):
         return unicode(self).encode("utf-8")
 
     def __unicode__(self):
-        return u"<Listen: uid: %s, time: %s, artist_msid: %s, album_msid: %s, recording_msid: %s>" % \
-               (self.uid, self.timestamp, self.artist_msid, self.album_msid, self.recording_msid)
+        return u"<Listen: user_id: %s, time: %s, artist_msid: %s, album_msid: %s, recording_msid: %s>" % \
+               (self.user_id, self.timestamp, self.artist_msid, self.album_msid, self.recording_msid)
--- redis-consumer/redis-consumer.py
+++ redis-consumer/redis-consumer.py
@@ -3,58 +3,66 @@
 import sys
 import os
 sys.path.append(os.path.join(os.path.dirname(os.path.realpath(__file__)), "../listenstore"))
+sys.path.append(os.path.join(os.path.dirname(os.path.realpath(__file__)), ".."))
+import config
 
 import ujson
 import logging
-from listenstore.listenstore import Listen
+from listenstore.listenstore import Listen, PostgresListenStore
 from time import time, sleep
-from webserver.redis_connection import _redis
+from redis import Redis
 
 BATCH_SIZE = 1000
-REPORT_FREQUENCY = 10000
+REPORT_FREQUENCY = 2000
 NO_ITEM_DELAY = .1   # in seconds
-BATCH_TIMEOUT = 1    # in seconds
+BATCH_TIMEOUT = 3    # in seconds
 
 # TODO: Add support for now playing
 
-
-class KafkaConsumer(object):
-    def __init__(self, conf):
+class RedisConsumer(object):
+    def __init__(self):
         self.log = logging.getLogger(__name__)
         self.total_inserts = 0
         self.inserts = 0
-        self.listenstore = None
 
-
-    def start_listens(self, listenstore):
-        self.listenstore = listenstore
-        return self.start()
-
-
-    def start(self):
+    def start(self, database_uri, redis_host):
         self.log.info("RedisListenConsumer started")
 
-        r = _redis
-
+        r = Redis(redis_host)
+        ls = PostgresListenStore({
+          'SQLALCHEMY_DATABASE_URI': database_uri,
+        })
         while True:
             listens = []
             t0 = time()
 
+            # TODO: Check to make sure the list is empty when you arrive here
+
             while True:
                 listen = r.rpoplpush("listens", "listens-pending")
-                if not listen:
+                if listen:
+                    data = ujson.loads(listen)
+                    l = Listen().from_json(data)
+                    if l.validate():
+                        listens.append(Listen(listen))
+                    else:
+                        # TODO: Find why UID is missing periodically
+                        self.log.error("inbound listen: " + str(listen))
+                        self.log.error("invalid listen: " + str(l))
+                else:
                     sleep(NO_ITEM_DELAY)
 
-                if time() - t0 > BATCH_TIMEOUT:
+                if time() - t0 > BATCH_TIMEOUT or len(listens) > BATCH_SIZE:
                     break
 
             if not listens:
                 continue
+            self.log.info("got %d listens" % len(listens))
 
             broken = True
             while broken:
                 try:
-                    self.listenstore.insert_batch(listens)
+                    ls.insert_postgresql(listens)
                     broken = False
 
                 except ValueError as e:
@@ -63,12 +71,17 @@ class KafkaConsumer(object):
 
             # clear the listens-pending list
             r.ltrim("listens-pending", 1, 0)
+            self.log.info("num items: %d pending %d" % (r.llen("listens"), r.llen('listens-pending')))
 
             self.inserts += len(listens)
             if self.inserts >= REPORT_FREQUENCY:
                 t1 = time()
                 self.total_inserts += self.inserts
-                self.log.info("Inserted %d rows in %.1fs (%.2f listens/sec). Total %d rows. last offset: %d" % \
-                    (self.inserts, t1 - t0, self.inserts / (t1 - t0), self.total_inserts, last_offset))
+                self.log.info("Inserted %d rows in %.1fs (%.2f listens/sec). Total %d rows." % \
+                    (self.inserts, t1 - t0, self.inserts / (t1 - t0), self.total_inserts))
                 self.inserts = 0
                 t0 = 0
+
+if __name__ == "__main__":
+    rc = RedisConsumer()
+    rc.start(config.SQLALCHEMY_DATABASE_URI, config.REDIS_HOST)
