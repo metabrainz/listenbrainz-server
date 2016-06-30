9073f131a807eea963df97a41612f56637a34d8a
Pinkesh Badjatiya <pinkeshbadjatiya@gmail.com>
2016-06-30 18:53:41 +0530

    Update tests for Postgres

--- listenstore/tests/test_listenstore.py
+++ listenstore/tests/test_listenstore.py
@@ -4,27 +4,26 @@ import unittest2
 import logging
 from datetime import date, datetime
 from .util import generate_data
-from listenstore.listenstore import ListenStore
+from listenstore.listenstore import PostgresListenStore
 
-# TODO: update for postgres
 
 class TestListenStore(unittest2.TestCase):
 
     @classmethod
-    @unittest2.skip("We don't have Cassandra on Jenkins server")
+    # @unittest2.skip("We don't have Postgres on Jenkins server")
     def setUpClass(self):
         self.log = logging.getLogger(__name__)
-        conf = {"replication_factor": 1,
-                "cassandra_keyspace": "listenbrainz_integration_test",
-                "cassandra_server": "localhost:9092"}
-        self.logstore = ListenStore(conf)
+        conf = {
+            "SQLALCHEMY_DATABASE_URI": "postgresql://listenbrainz@/listenbrainz"
+        }
+        self.logstore = PostgresListenStore(conf)
         self._create_test_data()
 
     @classmethod
     def _create_test_data(self):
         self.log.info("Inserting test data...")
         test_data = generate_data(datetime(2015, 9, 3, 0, 0, 0), 1000)
-        self.logstore.insert_batch(test_data)
+        self.logstore.insert_postgresql(test_data)
         self.log.info("Test data inserted")
 
     @classmethod
@@ -32,7 +31,7 @@ class TestListenStore(unittest2.TestCase):
         #self.logstore.drop_schema()
         self.logstore = None
 
-    @unittest2.skip("We don't have Cassandra on Jenkins server")
+    # @unittest2.skip("We don't have Postgres on Jenkins server")
     def test_fetch_listens(self):
-        listens = self.logstore.fetch_listens(uid="test", limit=10)
+        listens = self.logstore.fetch_listens(user_id="test", limit=10)
         self.assertEqual(len(list(listens)), 10)
