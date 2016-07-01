18ab629017f1397a03cb96fb3e4a1d794f71b686
Pinkesh Badjatiya <pinkeshbadjatiya@gmail.com>
2016-07-01 23:12:31 +0530

    Sample test

---
+++ db/testing.py
@@ -0,0 +1,44 @@
+import db
+import db.data
+import unittest
+import json
+import os
+
+# Configuration
+import sys
+sys.path.append(os.path.join(os.path.dirname(os.path.realpath(__file__)), ".."))
+import config
+
+ADMIN_SQL_DIR = os.path.join(os.path.dirname(os.path.realpath(__file__)), '..', 'admin', 'sql')
+TEST_DATA_PATH = os.path.join(os.path.dirname(os.path.realpath(__file__)), 'test_data')
+
+
+class DatabaseTestCase(unittest.TestCase):
+
+    def setUp(self):
+        db.init_db_connection(config['TEST_SQLALCHEMY_DATABASE_URI'])
+        self.reset_db()
+
+    def tearDown(self):
+        pass
+
+    def reset_db(self):
+        self.drop_tables()
+        self.init_db()
+
+    def init_db(self):
+        db.run_sql_script(os.path.join(ADMIN_SQL_DIR, 'create_extensions.sql'))
+        db.run_sql_script(os.path.join(ADMIN_SQL_DIR, 'create_tables.sql'))
+        db.run_sql_script(os.path.join(ADMIN_SQL_DIR, 'create_primary_keys.sql'))
+        db.run_sql_script(os.path.join(ADMIN_SQL_DIR, 'create_foreign_keys.sql'))
+        db.run_sql_script(os.path.join(ADMIN_SQL_DIR, 'create_indexes.sql'))
+
+    def drop_tables(self):
+        with db.engine.connect() as connection:
+            connection.execute('DROP TABLE IF EXISTS "user"       CASCADE')
+            connection.execute('DROP TABLE IF EXISTS listen       CASCADE')
+
+    def load_data_files(self, mbid):
+        """ Get the data files from the disk """
+        # return os.path.join(TEST_DATA_PATH, mbid + '.json')
+        return
--- listenstore/tests/test_listenstore.py
+++ listenstore/tests/test_listenstore.py
@@ -1,37 +1,38 @@
 # coding=utf-8
-from __future__ import division, absolute_import, print_function, unicode_literals
-import unittest2
-import logging
-from datetime import date, datetime
-from .util import generate_data
-from listenstore.listenstore import PostgresListenStore
-
-
-class TestListenStore(unittest2.TestCase):
-
-    @classmethod
-    # @unittest2.skip("We don't have Postgres on Jenkins server")
-    def setUpClass(self):
-        self.log = logging.getLogger(__name__)
-        conf = {
-            "SQLALCHEMY_DATABASE_URI": "postgresql://lb_test@/lb_test"
-        }
-        self.logstore = PostgresListenStore(conf)
-        self._create_test_data()
-
-    @classmethod
-    def _create_test_data(self):
-        self.log.info("Inserting test data...")
-        test_data = generate_data(datetime(2015, 9, 3, 0, 0, 0), 1000)
-        self.logstore.insert_postgresql(test_data)
-        self.log.info("Test data inserted")
-
-    @classmethod
-    def tearDownClass(self):
-        #self.logstore.drop_schema()
-        self.logstore = None
-
-    # @unittest2.skip("We don't have Postgres on Jenkins server")
-    def test_fetch_listens(self):
-        listens = self.logstore.fetch_listens(user_id="test", limit=10)
-        self.assertEqual(len(list(listens)), 10)
+# from __future__ import division, absolute_import, print_function, unicode_literals
+# from db.testing import DatabaseTestCase
+# import unittest2
+# import logging
+# from datetime import date, datetime
+# from .util import generate_data
+# from listenstore.listenstore import PostgresListenStore
+#
+# #
+# # class TestListenStore(DatabaseTestCase):
+# #
+# #     @classmethod
+# #     # @unittest2.skip("We don't have Postgres on Jenkins server")
+# #     def setUpClass(self):
+# #         self.log = logging.getLogger(__name__)
+# #         conf = {
+# #             "SQLALCHEMY_DATABASE_URI": "postgresql://lb_test@/lb_test"
+# #         }
+# #         self.logstore = PostgresListenStore(conf)
+# #         self._create_test_data()
+# #
+# #     @classmethod
+# #     def _create_test_data(self):
+# #         self.log.info("Inserting test data...")
+# #         test_data = generate_data(datetime(2015, 9, 3, 0, 0, 0), 1000)
+# #         self.logstore.insert_postgresql(test_data)
+# #         self.log.info("Test data inserted")
+# #
+# #     @classmethod
+# #     def tearDownClass(self):
+# #         #self.logstore.drop_schema()
+# #         self.logstore = None
+# #
+# #     # @unittest2.skip("We don't have Postgres on Jenkins server")
+# #     def test_fetch_listens(self):
+# #         listens = self.logstore.fetch_listens(user_id="test", limit=10)
+# #         self.assertEqual(len(list(listens)), 10)
