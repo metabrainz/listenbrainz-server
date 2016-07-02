575fef531830d8b8661601be7c0ad79a82fbb1f0
Pinkesh Badjatiya <pinkeshbadjatiya@gmail.com>
2016-07-03 01:20:26 +0530

    Remove redundant `token` column from the session table

--- admin/sql/create_primary_keys.sql
+++ admin/sql/create_primary_keys.sql
@@ -2,6 +2,6 @@ BEGIN;
 
 ALTER TABLE "user" ADD CONSTRAINT user_pkey PRIMARY KEY (id);
 ALTER TABLE listen ADD CONSTRAINT listen_pkey PRIMARY KEY (user_id, ts);
-ALTER TABLE session ADD CONSTRAINT sessions_pkey PRIMARY KEY (sid);
+ALTER TABLE session ADD CONSTRAINT session_pkey PRIMARY KEY (sid);
 
 COMMIT;
