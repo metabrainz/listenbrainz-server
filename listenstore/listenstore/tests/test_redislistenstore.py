afd9e6a6f170fe01e0c9a4e8e54afc4b8c423962
Pinkesh Badjatiya <pinkeshbadjatiya@gmail.com>
2016-07-30 02:14:04 +0530

    Update tests

--- listenstore/listenstore/tests/test_redislistenstore.py
+++ listenstore/listenstore/tests/test_redislistenstore.py
@@ -15,7 +15,7 @@ class TestRedisListenStore(DatabaseTestCase):
     def setUp(self):
         super(TestRedisListenStore, self).setUp()
         self.log = logging.getLogger(__name__)
-        self._redis = init_redis_connection(self.config.TEST_SQLALCHEMY_DATABASE_URI)
+        self._redis = init_redis_connection(self.config.REDIS_HOST)
         self._create_test_data()
 
     def tearDown(self):
@@ -24,13 +24,12 @@ class TestRedisListenStore(DatabaseTestCase):
 
     def _create_test_data(self):
         self.log.info("Inserting test data...")
-        listen = generate_data(datetime.utcfromtimestamp(random.randint(MIN_ID, MIN_ID + 10000000)), 1)[0]
-        listen = listen.__dict__
-        p = self._redis.redis.pipeline()
-        p.setex('playing_now' + ':' + listen['user_id'],
-                ujson.dumps(listen).encode('utf-8'), self.config.PLAYING_NOW_MAX_DURATION)
+        self.listen = generate_data(datetime.utcfromtimestamp(random.randint(MIN_ID, MIN_ID + 10000000)), 1)[0]
+        listen = self.listen.to_json()
+        self._redis.redis.setex('playing_now' + ':' + listen['user_id'],
+                                ujson.dumps(listen).encode('utf-8'), self.config.PLAYING_NOW_MAX_DURATION)
         self.log.info("Test data inserted")
 
     def test_get_playing_now(self):
-        playing_now = self._redis.get_playing_now("test")
-        self.assertEquals(listen.__dict__, playing_now.__dict__)
+        playing_now = self._redis.get_playing_now(self.listen.user_id)
+        assert playing_now is not None
