88a70516b72d74d2bf7bed8e86a0ae9dc0b0734d
Robert Kaye <rob@musicbrainz.org>
2016-08-10 16:24:02 +0200

    Finished fixing the tests

--- tests/utils.py
+++ tests/utils.py
@@ -1,6 +1,7 @@
 # coding=utf-8
 from __future__ import division, absolute_import, print_function, unicode_literals
 from datetime import datetime
+import time
 import sys
 import os
 
@@ -8,19 +9,25 @@ sys.path.append(os.path.join(os.path.dirname(os.path.realpath(__file__)), "../li
 from listenstore.listen import Listen
 import uuid
 import pytz
+import db.user
 
 def generate_data(from_date, num_records):
     test_data = []
     current_date = to_epoch(from_date)
     artist_msid = str(uuid.uuid4())
 
+    user = db.user.get_by_mb_id("test")
+    if not user:
+        db.user.create("test")
+        user = db.user.get_by_mb_id("test")
+
     for i in range(num_records):
         current_date += 1   # Add one second
-        item = Listen(user_id="test", timestamp=current_date, artist_msid=artist_msid,
+        item = Listen(user_id=user['id'], timestamp=current_date, artist_msid=artist_msid,
                       recording_msid=str(uuid.uuid4()))
         test_data.append(item)
     return test_data
 
 
 def to_epoch(date):
-    return (date - pytz.utc.localize(datetime.utcfromtimestamp(0))).total_seconds()
+    return int(time.mktime(date.timetuple()))
