c321040d8fb35f8d31fa0e5bedc7027b52162615
Robert Kaye <rob@musicbrainz.org>
2016-08-29 02:00:21 +0200

    Interim check-in on the way to creating a generic redis pub sub class

--- redis-consumer/redis-consumer.py
+++ redis-consumer/redis-consumer.py
@@ -5,8 +5,7 @@ import os
 sys.path.append(os.path.join(os.path.dirname(os.path.realpath(__file__)), "../listenstore"))
 sys.path.append(os.path.join(os.path.dirname(os.path.realpath(__file__)), ".."))
 from redis import Redis
-from redis_keys import REDIS_LISTEN_JSON, REDIS_LISTEN_JSON_REFCOUNT, REDIS_LISTEN_CONSUMER_IDS, \
-    REDIS_LISTEN_CONSUMERS, REDIS_LISTEN_CONSUMER_IDS_PENDING
+from redis_pubsub import RedisPubSubSubscriber, NoSubscriberNameSetException, WriteFailException
 import config
 
 import ujson
@@ -15,112 +14,57 @@ from listen import Listen
 from listenstore.listenstore import PostgresListenStore
 from time import time, sleep
 
-BATCH_SIZE = 1000
 REPORT_FREQUENCY = 5000
-BATCH_TIMEOUT = 3      # in seconds. Don't let a listen get older than this before writing
-REQUEST_TIMEOUT = 1    # in seconds. Wait this long to get a listen from redis
-
 CONSUMER_NAME = "pg"
+KEYSPACE_NAME = "listen"
 
-class RedisConsumer(object):
-    def __init__(self):
+class RedisConsumer(RedisPubSubSubscriber):
+    def __init__(self, redis, database_uri):
+        RedisPubSubSubscriber.__init__(self, redis, KEYSPACE_NAME)
+
         self.log = logging.getLogger(__name__)
         self.total_inserts = 0
         self.inserts = 0
         self.time = 0
-
-    def _register_self(self, r):
-        consumers = r.smembers(REDIS_LISTEN_CONSUMERS)
-        if not CONSUMER_NAME in consumers:
-            r.sadd(REDIS_LISTEN_CONSUMERS, CONSUMER_NAME)
-
-    def start(self, database_uri, redis_host):
-        self.log.info("RedisListenConsumer started")
-
-        r = Redis(redis_host)
-        self._register_self(r)
-        ls = PostgresListenStore({
+        self.ls = PostgresListenStore({
           'SQLALCHEMY_DATABASE_URI': database_uri,
         })
 
-        # If we have listens on the pending list, lets move them back to the submission list
-        ids = r.lrange(REDIS_LISTEN_CONSUMER_IDS_PENDING + CONSUMER_NAME, 0, -1)
-        if ids:
-            r.lpush(REDIS_LISTEN_CONSUMER_IDS + CONSUMER_NAME, *ids)
-            r.ltrim(REDIS_LISTEN_CONSUMER_IDS_PENDING + CONSUMER_NAME, 1, 0)
+    def write(self, listens):
+        submit = []
+        for listen in listens:
+            submit.append(Listen().from_json(listen))
+        t0 = time()
+        self.ls.insert(submit)
+        self.time += time() - t0
 
-        # 0 indicates that we've received no listens yet
-        batch_start_time = 0 
+    def start(self):
+        self.log.info("RedisListenConsumer started")
+
+        self.register_subscriber(CONSUMER_NAME)
         while True:
-            listens = []
-            listen_ids = []
+            try:
+                count = self.subscriber()            
+            except NoSubscriberNameSetException as e:
+                self.log.error("RedisListenConsumer has no subscriber name set.")
+                return
+            except WriteFailException as e:
+                self.log.error("RedisListenConsumer failed to write to Postgres.")
+                return
 
-            while True:
-                id = r.brpoplpush(REDIS_LISTEN_CONSUMER_IDS + CONSUMER_NAME, 
-                    REDIS_LISTEN_CONSUMER_IDS_PENDING + CONSUMER_NAME, REQUEST_TIMEOUT)
-                # If we got an id, fetch the listen and add to our list 
-                if id:
-                    # record the time when we get a first listen in this batch
-                    if not batch_start_time:
-                        batch_start_time = time()
-
-                    listen = r.get(REDIS_LISTEN_JSON + id)
-                    if listen:
-                        listen_ids.append(id)
-                        data = ujson.loads(listen)
-                        l = Listen().from_json(data)
-                        if l.validate():
-                            listens.append(l)
-                        else:
-                            self.log.error("invalid listen: " + str(l))
-
-                    else:
-                        # TODO: What happens if we don't get a listen?
-                        pass
-
-                if time() - batch_start_time >= BATCH_TIMEOUT:
-                    break
-
-                if len(listens) >= BATCH_SIZE:
-                    break
-
-            # We've collected listens to write, now write them
-            broken = True
-            while broken:
-                try:
-                    t0 = time()
-                    ls.insert(listens)
-                    self.time += time() - t0
-                    broken = False
-
-                except ValueError as e:
-                    self.log.error("Cannot insert listens: %s" % unicode(e))
-                    broken = False
-
-            # Clear the start time, since we've cleaned out the batch
-            batch_start_time = 0
-
-
-            # clear the listens-pending list
-            r.ltrim(REDIS_LISTEN_CONSUMER_IDS_PENDING + CONSUMER_NAME, 1, 0)
-
-            # now clean up the listens from redis
-            for id in listen_ids:
-                # Get the refcount for this listen. If 0, delete it and the refcount
-                refcount = r.decr(REDIS_LISTEN_JSON_REFCOUNT + id)
-                if refcount == 0:
-                    r.delete(REDIS_LISTEN_JSON + id)
-                    r.delete(REDIS_LISTEN_JSON_REFCOUNT + id)
+            if not count:
+                continue
 
             # collect and occasionally print some stats
-            self.inserts += len(listens)
+            self.inserts += count
             if self.inserts >= REPORT_FREQUENCY:
                 self.total_inserts += self.inserts
                 self.log.info("Inserted %d rows in %.1fs (%.2f listens/sec). Total %d rows." % \
-                    (self.inserts, self.time, self.inserts / self.time, self.total_inserts))
+                    (count, self.time, count / self.time, self.total_inserts))
                 self.inserts = 0
                 self.time = 0
 
 if __name__ == "__main__":
-    rc = RedisConsumer()
-    rc.start(config.SQLALCHEMY_DATABASE_URI, config.REDIS_HOST)
+    r = Redis(config.REDIS_HOST)
+    rc = RedisConsumer(r,config.SQLALCHEMY_DATABASE_URI)
+    rc.start()
--- webserver/views/api_tools.py
+++ webserver/views/api_tools.py
@@ -7,14 +7,7 @@ import ujson
 
 from webserver.external import messybrainz
 from webserver.redis_connection import _redis
-
-# For a detailed description of the PubSub Redis mechanism that is being implemented 
-# here see this page: https://davidmarquis.wordpress.com/2013/01/03/reliable-delivery-message-queues-with-redis/
-# The one change from the solution outlined above is the addition to ref-counts for the JSON
-# Each consumer must decrement the refcount and when the refcount reaches 0, it is responsible
-# for removing the JSON and REFCOUNT keys from redis
-from redis_keys import REDIS_LISTEN_NEXTID, REDIS_LISTEN_CONSUMERS, REDIS_LISTEN_JSON, \
-    REDIS_LISTEN_JSON_REFCOUNT, REDIS_LISTEN_CONSUMER_IDS
+from redis_pubsub import RedisPubSubPublisher, NoSubscribersException
 
 #: Maximum overall listen size in bytes, to prevent egregious spamming.
 MAX_LISTEN_SIZE = 10240
@@ -40,6 +33,9 @@ LISTEN_TYPE_SINGLE = 1
 LISTEN_TYPE_IMPORT = 2
 LISTEN_TYPE_PLAYING_NOW = 3
 
+# PubSub Keyspace
+LISTEN_KEYSPACE = "listen"
+
 def insert_payload(payload, user, listen_type=LISTEN_TYPE_IMPORT):
     """ Convert the payload into augmented listens then submit them.
         Returns: augmented_listens
@@ -53,54 +49,23 @@ def insert_payload(payload, user, listen_type=LISTEN_TYPE_IMPORT):
 
 
 def _send_listens_to_redis(listen_type, listens):
-
-    p = _redis.redis.pipeline()
-    r = _redis.redis
-
-    consumers = []
     for listen in listens:
         if listen_type == LISTEN_TYPE_PLAYING_NOW:
             try:
                 expire_time = listen["track_metadata"]["additional_info"].get("duration",
                                     current_app.config['PLAYING_NOW_MAX_DURATION'])
-                p.setex('playing_now' + ':' + listen['user_id'],
+                _redis.redis.setex('playing_now' + ':' + listen['user_id'],
                         ujson.dumps(listen).encode('utf-8'), expire_time)
             except Exception, e:
                 current_app.logger.error("Redis rpush playing_now write error: " + str(sys.exc_info()[0]))
                 raise InternalServerError("Cannot record playing_now at this time.")
         else:
+            pubsub = RedisPubSubPublisher(_redis.redis, LISTEN_KEYSPACE)
             try:
-                # in order to allow multiple readers of the listen stream, we have a set
-                # called consumers. If a consumer is registered in this set, they *must*
-                # consume the listens at some point, otherwise redis will fill up.
-                if not consumers:
-                    consumers = r.smembers(REDIS_LISTEN_CONSUMERS)
-
-                if consumers:
-                    # Each listen gets a redis specific ID
-                    nextid = r.incr(REDIS_LISTEN_NEXTID)
-
-                    # Each listen is then inserted into redis using the id from above and a 
-                    # refcount is set so that we can remove data from redis in a timely fashion
-                    p.set(REDIS_LISTEN_JSON + str(nextid), ujson.dumps(listen).encode('utf-8'))
-                    p.set(REDIS_LISTEN_JSON_REFCOUNT + str(nextid), len(consumers))
-
-                    # Finally a message id is added a list kept for each consumer. This way
-                    # the consumer can pop message ids from the list and then consume the
-                    # listen. Each consumer must DECR the corresponding REDIS_LISTEN_JSON_REFCOUNT
-                    # key. If it goes to 0, then the consumer must remove the REDIS_LISTEN_JSON and 
-                    # REDIS_LISTEN_JSON_REFCOUNT keys
-                    for consumer in consumers:
-                        p.lpush(REDIS_LISTEN_CONSUMER_IDS + consumer, nextid)
-                else:
-                    current_app.logger.error("No consumers registered in redis. Not accepting listens.")
-                    raise InternalServerError("Cannot record listen at this time. (No consumers registered.)")
-
-            except:
-                current_app.logger.error("Redis rpush listens write error: " + str(sys.exc_info()[0]))
-                raise InternalServerError("Cannot record listen at this time.")
-
-    p.execute()
+                pubsub.publish(listens)
+            except NoSubscribersException:
+                current_app.logger.error("No consumers registered in redis. Not accepting listens.")
+                raise InternalServerError("Cannot record listen at this time. (No consumers registered.)")
 
 
 def _validate_listen(listen, listen_type):
