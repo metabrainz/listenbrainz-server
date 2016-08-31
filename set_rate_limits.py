32a434a7780329a82a605c824894563054f88a0d
Robert Kaye <rob@musicbrainz.org>
2016-08-31 12:48:12 +0200

    Fix rate limit setting

--- set_rate_limits.py
+++ set_rate_limits.py
@@ -2,10 +2,15 @@
 
 import sys
 from redis import Redis
-from webserver.rate_limiter import RATELIMIT_PER_TOKEN_KEY 
-from webserver.rate_limiter import RATELIMIT_PER_IP_KEY
-from webserver.rate_limiter import RATELIMIT_WINDOW_KEY
+#from webserver.rate_limiter import RATELIMIT_PER_TOKEN_KEY 
+#from webserver.rate_limiter import RATELIMIT_PER_IP_KEY
+#from webserver.rate_limiter import RATELIMIT_WINDOW_KEY
 import config
+
+# TODO: Do not duplicate these. Import from webserver when doing the source tree re-org
+RATELIMIT_PER_TOKEN_KEY = "rate_limit_per_token_limit"
+RATELIMIT_PER_IP_KEY = "rate_limit_per_ip_limit"
+RATELIMIT_WINDOW_KEY = "rate_limit_window"
 
 # Yes, I could use getoptgetargparsewtfbbw, but then I would spend 20 mimnutes re-learning the stupid syntax. 
 # Or, I could just do it myself in the space of seconds.
@@ -13,8 +18,13 @@ import config
 # to figure out how to do this. So, we have this script. If you want to see it part of manage.py, you'll
 # have to do it.
 
+r = Redis(config.REDIS_HOST)
 if len(sys.argv) < 4:
     print "Usage: %s <per ip limit> <per token limit> <window in s>" % (sys.argv[0])
+    print "Current values:"
+    print "   Requests per token: ", r.get(RATELIMIT_PER_TOKEN_KEY)
+    print "      Requests per ip: ", r.get(RATELIMIT_PER_IP_KEY)
+    print "          window size: ", r.get(RATELIMIT_WINDOW_KEY)
     sys.exit(-1)
 
 try:
@@ -46,7 +56,6 @@ except ValueError:
 if window <= 0:
     print "Invalid window size. Must be non zero integer."
 
-r = Redis(config.REDIS_HOST)
 r.set(RATELIMIT_PER_TOKEN_KEY, per_token)
 r.set(RATELIMIT_PER_IP_KEY, per_ip)
 r.set(RATELIMIT_WINDOW_KEY, window)
