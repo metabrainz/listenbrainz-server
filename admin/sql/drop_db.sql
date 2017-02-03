3272801f61b7498c0e0fe61c0c8b5a159cee6e2d
Alastair Porter <alastair@porter.net.nz>
2017-02-03 18:38:38 +0100

    Remove psql commands from sql scripts

--- admin/sql/drop_db.sql
+++ admin/sql/drop_db.sql
@@ -1,4 +1,2 @@
-\set ON_ERROR_STOP 1
-
 DROP DATABASE IF EXISTS messybrainz;
 DROP USER IF EXISTS messybrainz;
--- admin/sql/drop_test_db.sql
+++ admin/sql/drop_test_db.sql
@@ -1,4 +1,2 @@
-\set ON_ERROR_STOP 1
-
 DROP DATABASE IF EXISTS msb_test;
 DROP USER IF EXISTS msb_test;
