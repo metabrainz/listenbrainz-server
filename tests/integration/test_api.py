e5e3a54a24bb0c4b1dd0b3330a2cf2f21c4d7ced
Param Singh <paramsingh258@gmail.com>
2017-03-08 18:04:15 +0530

    Add a method that gets the path to data files for integration tests
    Also, fix some whitespace problems and remove a few redundant tests.

--- tests/integration/test_api.py
+++ tests/integration/test_api.py
@@ -10,6 +10,8 @@ import db.user
 import time
 import json
 
+TEST_DATA_PATH = os.path.join(os.path.dirname(os.path.realpath(__file__)), '..', '..', 'testdata')
+
 # lifted from AcousticBrainz
 def is_valid_uuid(u):
     try:
@@ -26,36 +28,38 @@ class APITestCase(ServerTestCase):
     def test_get_listens(self):
         """ Test to make sure that the api sends valid listens on get requests.
         """
-        with open(os.path.join(os.getcwd(), 'testdata', 'valid_single.json'), 'r') as f:
+        with open(self.path_to_data_file('valid_single.json'), 'r') as f:
             payload = json.load(f)
+
+        # send a listen
         payload['payload'][0]['listened_at'] = int(time.time())
         response = self.send_data(payload)
         self.assert200(response)
+
+        # This sleep allows for the influx subscriber to take its time in getting
+        # the listen submitted from redis and writing it to influx.
+        # Removing it causes an empty list of listens to be returned.
         time.sleep(10)
+
         url = url_for('api_v1.get_listens', user_name = self.user['musicbrainz_id'])
         response = self.client.get(url, query_string = {'count': '1'})
         self.assert200(response)
-        data = json.loads(response.data)
-        self.assertTrue('payload' in data)
-        data = data['payload']
+        data = json.loads(response.data)['payload']
+
         # make sure user id is correct
-        self.assertTrue('user_id' in data)
         self.assertEquals(data['user_id'], self.user['musicbrainz_id'])
+
         # make sure that count is 1 and list also contains 1 listen
-        self.assertTrue('count' in data)
         self.assertEquals(data['count'], 1)
-        self.assertTrue('listens' in data)
         self.assertEquals(len(data['listens']), 1)
+
         # make sure timestamp is the same as sent
-        self.assertTrue('listened_at' in data['listens'][0])
         sent_time = payload['payload'][0]['listened_at']
         self.assertEquals(data['listens'][0]['listened_at'], sent_time)
+
         # make sure that artist msid, release msid and recording msid are present in data
-        self.assertTrue('recording_msid' in data['listens'][0])
         self.assertTrue(is_valid_uuid(data['listens'][0]['recording_msid']))
-        self.assertTrue('artist_msid' in data['listens'][0]['track_metadata'])
         self.assertTrue(is_valid_uuid(data['listens'][0]['track_metadata']['artist_msid']))
-        self.assertTrue('release_msid' in data['listens'][0]['track_metadata'])
         self.assertTrue(is_valid_uuid(data['listens'][0]['track_metadata']['release_msid']))
 
     def send_data(self, payload):
@@ -71,7 +75,7 @@ class APITestCase(ServerTestCase):
     def test_unauthorized_submission(self):
         """ Test for checking that unauthorized submissions return 401
         """
-        with open(os.path.join(os.getcwd(), 'testdata', 'valid_single.json'), 'r') as f:
+        with open(self.path_to_data_file('valid_single.json'), 'r') as f:
             payload = json.load(f)
 
         # request with no authorization header
@@ -94,7 +98,7 @@ class APITestCase(ServerTestCase):
     def test_valid_single(self):
         """ Test for valid submissioon of listen_type listen
         """
-        with open(os.path.join(os.getcwd(), 'testdata', 'valid_single.json'), 'r') as f:
+        with open(self.path_to_data_file('valid_single.json'), 'r') as f:
             payload = json.load(f)
         response = self.send_data(payload)
         self.assert200(response)
@@ -103,7 +107,7 @@ class APITestCase(ServerTestCase):
         """ Test for an invalid submission which has listen_type 'single' but
             more than one listen in payload
         """
-        with open(os.path.join(os.getcwd(), 'testdata', 'single_more_than_one_listen.json'), 'r') as f:
+        with open(self.path_to_data_file('single_more_than_one_listen.json'), 'r') as f:
             payload = json.load(f)
         response = self.send_data(payload)
         self.assert400(response)
@@ -111,7 +115,7 @@ class APITestCase(ServerTestCase):
     def test_valid_playing_now(self):
         """ Test for valid submission of listen_type 'playing_now'
         """
-        with open(os.path.join(os.getcwd(), 'testdata', 'valid_playing_now.json'), 'r') as f:
+        with open(self.path_to_data_file('valid_playing_now.json'), 'r') as f:
             payload = json.load(f)
         response = self.send_data(payload)
         self.assert200(response)
@@ -120,7 +124,7 @@ class APITestCase(ServerTestCase):
         """ Test for invalid submission of listen_type 'playing_now' which contains
             timestamp 'listened_at'
         """
-        with open(os.path.join(os.getcwd(), 'testdata', 'playing_now_with_ts.json'), 'r') as f:
+        with open(self.path_to_data_file('playing_now_with_ts.json'), 'r') as f:
             payload = json.load(f)
         response = self.send_data(payload)
         self.assert400(response)
@@ -129,7 +133,7 @@ class APITestCase(ServerTestCase):
         """ Test for invalid submission of listen_type 'playing_now' which contains
             more than one listen in payload
         """
-        with open(os.path.join(os.getcwd(), 'testdata', 'playing_now_more_than_one_listen.json'), 'r') as f:
+        with open(self.path_to_data_file('playing_now_more_than_one_listen.json'), 'r') as f:
             payload = json.load(f)
         response = self.send_data(payload)
         self.assert400(response)
@@ -137,7 +141,7 @@ class APITestCase(ServerTestCase):
     def test_valid_import(self):
         """ Test for a valid submission of listen_type 'import'
         """
-        with open(os.path.join(os.getcwd(), 'testdata', 'valid_import.json'), 'r') as f:
+        with open(self.path_to_data_file('valid_import.json'), 'r') as f:
             payload = json.load(f)
         response = self.send_data(payload)
         self.assert200(response)
@@ -146,7 +150,7 @@ class APITestCase(ServerTestCase):
         """ Test for invalid submission in which the overall size of the listens sent is more than
             10240 bytes
         """
-        with open(os.path.join(os.getcwd(), 'testdata', 'too_large_listen.json'), 'r') as f:
+        with open(self.path_to_data_file('too_large_listen.json'), 'r') as f:
             payload = json.load(f)
         response = self.send_data(payload)
         self.assert400(response)
@@ -155,7 +159,7 @@ class APITestCase(ServerTestCase):
         """ Test for invalid submission in which a listen contains more than the allowed
             number of tags in additional_info.
         """
-        with open(os.path.join(os.getcwd(), 'testdata', 'too_many_tags.json'), 'r') as f:
+        with open(self.path_to_data_file('too_many_tags.json'), 'r') as f:
             payload = json.load(f)
         response = self.send_data(payload)
         self.assert400(response)
@@ -163,7 +167,7 @@ class APITestCase(ServerTestCase):
     def test_too_long_tag(self):
         """ Test for invalid submission in which a listen contains a tag of length > 64
         """
-        with open(os.path.join(os.getcwd(), 'testdata', 'too_long_tag.json'), 'r') as f:
+        with open(self.path_to_data_file('too_long_tag.json'), 'r') as f:
             payload = json.load(f)
         response = self.send_data(payload)
         self.assert400(response)
@@ -172,7 +176,7 @@ class APITestCase(ServerTestCase):
         """ Test for invalid submission in which a listen contains an invalid release_mbid
             in additional_info
         """
-        with open(os.path.join(os.getcwd(), 'testdata', 'invalid_release_mbid.json'), 'r') as f:
+        with open(self.path_to_data_file('invalid_release_mbid.json'), 'r') as f:
             payload = json.load(f)
         response = self.send_data(payload)
         self.assert400(response)
@@ -181,7 +185,7 @@ class APITestCase(ServerTestCase):
         """ Test for invalid submission in which a listen contains an invalid artist_mbid
             in additional_info
         """
-        with open(os.path.join(os.getcwd(), 'testdata', 'invalid_artist_mbid.json'), 'r') as f:
+        with open(self.path_to_data_file('invalid_artist_mbid.json'), 'r') as f:
             payload = json.load(f)
         response = self.send_data(payload)
         self.assert400(response)
@@ -190,7 +194,15 @@ class APITestCase(ServerTestCase):
         """ Test for invalid submission in which a listen contains an invalid recording_mbid
             in additional_info
         """
-        with open(os.path.join(os.getcwd(), 'testdata', 'invalid_recording_mbid.json'), 'r') as f:
+        with open(self.path_to_data_file('invalid_recording_mbid.json'), 'r') as f:
             payload = json.load(f)
         response = self.send_data(payload)
         self.assert400(response)
+
+    def path_to_data_file(self, fn): 
+        """ Returns the path of the test data file relative to the test file.
+
+            Args:
+                fn: the name of the data file
+        """
+        return os.path.join(TEST_DATA_PATH, fn)
