0090aba6119411ee2429aad78cf66c8b49005157
Param Singh <paramsingh258@gmail.com>
2017-03-24 22:39:04 +0530

    Handle bad data in a better way in alpha_importer
    Data that is invalid used to make alpha_importer go into an
    infinite loop. Change the send_batch function such that it finds
    invalid data and skips it.

--- alpha_importer.py
+++ alpha_importer.py
@@ -1,4 +1,4 @@
-from __future__ import print_function
+from __future__ import print_function, division
 from redis import Redis
 import config
 import requests
@@ -9,6 +9,7 @@ import sys
 import logging
 from logging.handlers import RotatingFileHandler
 import os
+import json
 
 redis_connection = Redis(host=config.REDIS_HOST, port=config.REDIS_PORT)
 
@@ -19,7 +20,7 @@ logger.setLevel(logging.DEBUG)
 if config.IMPORTER_LOG_FILE == "-":
     handler = logging.StreamHandler(sys.stdout)
 else:
-    handler = RotatingFileHandler(config.IMPORTER_LOG_FILE, maxBytes = 512 * 1024, backupCount = 100)
+    handler = RotatingFileHandler(config.IMPORTER_LOG_FILE, maxBytes=512 * 1024, backupCount=100)
 
 handler.setLevel(logging.DEBUG)
 formatter = logging.Formatter('%(asctime)s - %(levelname)s - %(message)s')
@@ -44,7 +45,7 @@ def get_batch(username, max_ts):
     if r.status_code == 200:
         return ujson.loads(r.text)
     else:
-        logger.error("get from alpha http response code: {}".format(r.status_code))
+        logger.error("GET from alpha HTTP response code: {}".format(r.status_code))
         raise HTTPError
 
 
@@ -61,14 +62,53 @@ def extract_data(batch):
     return data
 
 
-def send_batch(data, token):
+def send_batch(data, token, retries=5):
+    """
+    Sends a batch of data. If fails, then breaks the data into two parts and
+    recursively tries to send them, until we find the culprit listen
+
+    Returns: number of listens successfully sent
+    """
+
+    if not data['payload']:
+        return 0
+
     send_url = '{}/1/submit-listens'.format(config.BETA_URL)
-    r = requests.post(send_url, headers = {'Authorization': 'Token {}'.format(token)}, data = ujson.dumps(data))
-    if r.status_code != 200:
-        logger.error("submission to beta returned response code: {}".format(r.status_code))
-        logger.error(data)
-        # For now, skip the error and move on. This should be addressed in LB-142.
-        #raise HTTPError
+    done = False
+    for _ in xrange(retries):
+        r = requests.post(send_url, headers={'Authorization': 'Token {}'.format(token)}, data = ujson.dumps(data))
+        if r.status_code == 200:
+            done = True
+            break
+        else:
+            time.sleep(config.IMPORTER_DELAY)
+
+    if done:
+        return len(data['payload'])
+    elif not done and len(data['payload']) == 1:
+        # try to send the bad listen one more time and if it doesn't work
+        # log the error
+        r = requests.post(send_url, headers={'Authorization': 'Token {}'.format(token)}, data = ujson.dumps(data))
+        if r.status_code == 200:
+            return 1
+        else:
+            logger.error("Unable to submit bad listen to beta:")
+            logger.error(json.dumps(data, indent=4))
+            logger.error("Response code from beta: {}".format(r.status_code))
+            logger.error(r.text)
+            return 0
+    else:
+        slice_index = len(data['payload']) // 2
+        # send first half
+        half_data = {
+            'listen_type': 'import',
+            'payload': data['payload'][slice_index :],
+        }
+        sent = send_batch(half_data, token, retries)
+        # send second half
+        half_data['payload'] = data['payload'][: slice_index]
+        sent += send_batch(half_data, token, retries)
+        return sent
 
 
 def import_from_alpha(username, token):
@@ -79,14 +119,16 @@ def import_from_alpha(username, token):
             batch = get_batch(username, next_max)
             if batch['payload']['count'] == 0:
                 logger.info('All pages done for user {}'.format(username))
-                logger.info('Total number of pages done: {}'.format(page_count))
-                return True
+                logger.info('Total number of pages done: {}'.format(page_count - 1))
+                return
             data = extract_data(batch)
             sent = send_batch(data, token)
             next_max = data['payload'][-1]['listened_at']
             logger.info('Page #{} done.'.format(page_count))
+            if sent < len(data['payload']):
+                logger.error("Only wrote {} out of {} listens for page {}".format(sent, len(data['payload']), page_count))
             page_count += 1
-        except HTTPError:
+        except HTTPError as e:
             time.sleep(config.IMPORTER_DELAY)
 
 
@@ -102,8 +144,8 @@ if __name__ == '__main__':
     while True:
         if not queue_empty():
             username, token = queue_front()
-            if import_from_alpha(username, token):
-                queue_pop()
-                update_status(username, "DONE")
+            import_from_alpha(username, token)
+            queue_pop()
+            update_status(username, "DONE")
         else:
             time.sleep(3)
