d356a9ca918dac42fc2ca404d3d07a8d130cbf57
Robert Kaye <rob@musicbrainz.org>
2017-04-19 19:05:01 +0200

    Make the initial connection to rabbitmq more resilient.

--- webserver/rabbitmq_connection.py
+++ webserver/rabbitmq_connection.py
@@ -1,9 +1,23 @@
+import sys
+from time import sleep
 import pika
 
 _rabbitmq = None
 
-def init_rabbitmq_connection(host, port):
+def init_rabbitmq_connection(app):
     """Create a connection to the RabbitMQ server."""
     global _rabbitmq
-    _rabbitmq = pika.BlockingConnection(pika.ConnectionParameters(host=host, port=port))
-    return _rabbitmq
+
+    if not app.config.has_key("RABBITMQ_HOST"):
+        app.logger.error("RabbitMQ host:port not defined. Sleeping 2 seconds, and exiting.")
+        sys.exit(-1)
+
+    while True:
+        try:
+            _rabbitmq = pika.BlockingConnection(pika.ConnectionParameters(host=app.config['RABBITMQ_HOST'], port=app.config['RABBITMQ_PORT']))
+            app.logger.info("Connected connect to rabbitmq!")
+            return
+        except pika.exceptions.ConnectionClosed:
+            app.logger.error("Cannot connect to rabbitmq, sleeping 2 seconds")
+            sleep(2)
+            continue
