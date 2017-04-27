556de4d1dc4efa0692e37bc8c17b5771c74abc63
Param Singh <paramsingh258@gmail.com>
2017-04-27 16:07:33 +0530

    Add test for influx-writer's dedup
    Also refactor code to make the path_to_data_file function
    part of IntegrationTestCase.

--- tests/integration/__init__.py
+++ tests/integration/__init__.py
@@ -7,6 +7,7 @@ from db.testing import DatabaseTestCase
 
 class IntegrationTestCase(ServerTestCase, DatabaseTestCase):
 
+    TEST_DATA_PATH = os.path.join(os.path.dirname(os.path.realpath(__file__)), '..', '..', 'testdata')
     def setUp(self):
         ServerTestCase.setUp(self)
         DatabaseTestCase.setUp(self)
@@ -15,3 +16,10 @@ class IntegrationTestCase(ServerTestCase, DatabaseTestCase):
         ServerTestCase.tearDown(self)
         DatabaseTestCase.tearDown(self)
 
+    def path_to_data_file(self, fn):
+        """ Returns the path of the test data file relative to the test file.
+
+            Args:
+                fn: the name of the data file
+        """
+        return os.path.join(IntegrationTestCase.TEST_DATA_PATH, fn)
