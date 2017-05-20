2d31c82982dd0471c146bc3257afbb0d5df894d1
Param Singh <paramsingh258@gmail.com>
2017-05-21 03:08:56 +0530

    Make Redis connection establishment more reliable
    Do a ping and if ConnectionErrors are thrown, sleep for two
    seconds and try to establish a new connection.

--- webserver/redis_connection.py
+++ webserver/redis_connection.py
@@ -1,13 +1,26 @@
 from redis import Redis
+import redis
+import time
 from listenstore import RedisListenStore
 
 _redis = None
 
-def init_redis_connection(host, port):
+def init_redis_connection(logger, host, port):
     """Create a connection to the Redis server."""
+
     global _redis
-    _redis = RedisListenStore({
-        'REDIS_HOST': host,
-        'REDIS_PORT': port
-    })
+    while True:
+        try:
+            logger.info("Connecting to redis {}:{}".format(host, port))
+            _redis = RedisListenStore({
+                'REDIS_HOST': host,
+                'REDIS_PORT': port
+            })
+            _redis.check_connection()
+            break
+        except redis.exceptions.ConnectionError as e:
+            logger.error("Connection to redis failed: {}".format(str(e)))
+            logger.error("Sleeping 2 seconds and trying again...")
+            time.sleep(2)
+
     return _redis
