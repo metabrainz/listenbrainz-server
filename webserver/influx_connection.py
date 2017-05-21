c84a1b5681fa53bc6b4b55480b46a47b2c45912c
Param Singh <paramsingh258@gmail.com>
2017-05-21 15:22:45 +0530

    Make influx connection more reliable in webserver

--- webserver/influx_connection.py
+++ webserver/influx_connection.py
@@ -2,7 +2,16 @@ from listenstore import InfluxListenStore
 
 _influx = None
 
-def init_influx_connection(conf):
+def init_influx_connection(logger, conf):
     global _influx
-    _influx = InfluxListenStore(conf)
+    while True:
+        try:
+            _influx = InfluxListenStore(conf)
+            break
+        except Exception as e:
+            logger.error("Couldn't create InfluxListenStore instance: {}".format(str(e)))
+            logger.error("Sleeping 2 seconds and then retrying...")
+            time.sleep(2)
+
+    logger.info("Successfully created InfluxListenStore instance!")
     return _influx
