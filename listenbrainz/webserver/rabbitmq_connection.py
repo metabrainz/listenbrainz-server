79c03131eb22f4e71f743007c2c3dac4afceced5
Robert Kaye <rob@musicbrainz.org>
2017-06-05 22:14:04 +0200

    Start using the flask-pika connection pooler

--- listenbrainz/webserver/rabbitmq_connection.py
+++ listenbrainz/webserver/rabbitmq_connection.py
@@ -1,6 +1,7 @@
 import sys
 from time import sleep
-import pika
+from flask import Flask
+from flask.ext.pika import Pika as FPika
 
 _rabbitmq = None
 
@@ -8,18 +9,31 @@ def init_rabbitmq_connection(app):
     """Create a connection to the RabbitMQ server."""
     global _rabbitmq
 
-    app.logger.info("Call to init_rabbit_mq_connection")
     if "RABBITMQ_HOST" not in app.config:
         app.logger.error("RabbitMQ host:port not defined. Sleeping 2 seconds, and exiting.")
         sleep(2)
         sys.exit(-1)
 
+    FLASK_PIKA_PARAMS = {
+        'host':app.config['RABBITMQ_HOST'],
+        'port': app.config['RABBITMQ_PORT'],
+        'username': 'guest',
+        'password': 'guest',
+    }
+
+    FLASK_PIKA_POOL_PARAMS = {
+        'pool_size': 8,
+        'pool_recycle': 600
+    }
+
+    app.config['FLASK_PIKA_PARAMS'] = FLASK_PIKA_PARAMS
+    app.config['FLASK_PIKA_POOL_PARAMS'] = FLASK_PIKA_POOL_PARAMS
+
     while True:
         try:
-            _rabbitmq = pika.BlockingConnection(pika.ConnectionParameters(host=app.config['RABBITMQ_HOST'], port=app.config['RABBITMQ_PORT']))
-            app.logger.info("Connected connect to rabbitmq!")
+            _rabbitmq = FPika(app)
             return
-        except pika.exceptions.ConnectionClosed:
+        except Exception as err:
             app.logger.error("Cannot connect to rabbitmq, sleeping 2 seconds")
             sleep(2)
             continue
