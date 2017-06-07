2d60dce15c61fadc363e12e95f7334e554eccc06
Param Singh <paramsingh258@gmail.com>
2017-06-07 23:23:16 +0530

    Fix artist_mbids becoming a string during conversion from influx
    Also add a test for the from_influx method

---
+++ listenbrainz/tests/unit/test_listen.py
@@ -0,0 +1,60 @@
+import unittest
+from listenbrainz.listen import Listen
+from datetime import datetime
+import json
+
+
+class ListenTestCase(unittest.TestCase):
+
+    def test_from_influx(self):
+        """ Test for the from_influx method """
+
+        influx_row = {
+            "time": "2017-06-07T17:23:05Z",
+            "artist_mbids": "abaa7001-0d80-4e58-be5d-d2d246fd9d87",
+            "artist_msid": "aa6130f2-a12d-47f3-8ffd-d0f71340de1f",
+            "artist_name": "Majid Jordan",
+            "best_song": "definitely",
+            "genius_link": "https://genius.com/Majid-jordan-every-step-every-way-lyrics",
+            "lastfm_link": "https://www.last.fm/music/Majid+Jordan/_/Every+Step+Every+Way",
+            "other_stuff": "teststuffplsignore",
+            "recording_mbid": None,
+            "recording_msid": "db9a7483-a8f4-4a2c-99af-c8ab58850200",
+            "release_msid": "cf138a00-05d5-4b35-8fce-181efcc15785",
+            "release_name": "Majid Jordan",
+            "track_name": "Every Step Every Way",
+            "user_name": "iliekcomputers",
+            "we_dict_now.hello": "afb",
+            "we_dict_now.we_nested_now.hi": "312",
+            "tags": "sing, song"
+        }
+
+        listen = Listen.from_influx(influx_row)
+
+        # Check user name
+        self.assertEqual(listen.user_name, influx_row['user_name'])
+
+        # Check time stamp
+        dt = datetime.strptime(influx_row['time'] , '%Y-%m-%dT%H:%M:%SZ')
+        ts = int(dt.strftime("%s"))
+        self.assertEqual(listen.ts_since_epoch, ts)
+
+        # Check artist mbids
+        self.assertIsInstance(listen.data['additional_info']['artist_mbids'], list)
+        self.assertEqual(listen.data['additional_info']['artist_mbids'], influx_row['artist_mbids'].split(','))
+
+        # Check tags
+        self.assertIsInstance(listen.data['additional_info']['tags'], list)
+        self.assertEqual(listen.data['additional_info']['tags'], influx_row['tags'].split(','))
+
+        # Check track name
+        self.assertEqual(listen.data['track_name'], influx_row['track_name'])
+
+        # Check additional info
+        self.assertEqual(listen.data['additional_info']['best_song'], influx_row['best_song'])
+
+        # Check msids
+        self.assertEqual(listen.artist_msid, influx_row['artist_msid'])
+        self.assertEqual(listen.release_msid, influx_row['release_msid'])
+        self.assertEqual(listen.recording_msid, influx_row['recording_msid'])
+
