19d5d2826f46ee618f0053e83718315591962910
Param Singh <paramsingh258@gmail.com>
2017-06-09 22:33:09 +0530

    Drop schemas with cascading

--- admin/sql/drop_tables.sql
+++ admin/sql/drop_tables.sql
@@ -4,12 +4,4 @@ DROP TABLE IF EXISTS "user"               CASCADE;
 DROP TABLE IF EXISTS listen               CASCADE;
 DROP TABLE IF EXISTS listen_json          CASCADE;
 
-DROP TABLE IF EXISTS api_compat.token     CASCADE;
-DROP TABLE IF EXISTS api_compat.session   CASCADE;
-
-DROP TABLE IF EXISTS statistics.user      CASCADE;
-DROP TABLE IF EXISTS statistics.artist    CASCADE;
-DROP TABLE IF EXISTS statistics.release   CASCADE;
-DROP TABLE IF EXISTS statistics.recording CASCADE;
-
 COMMIT;
