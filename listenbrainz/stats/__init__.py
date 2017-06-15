3762c7f9f3b34cb76194314607c6560ee13c665b
Param Singh <paramsingh258@gmail.com>
2017-06-15 20:41:28 +0530

    Add run_query method and get_top_tracks function for users
    run_query takes a query and its parameters and then returns the
    results of the that query.
    
    get_top_tracks is an example query that gets the top tracks of
    the user over a particular interval of time.

--- listenbrainz/stats/__init__.py
+++ listenbrainz/stats/__init__.py
@@ -3,6 +3,10 @@ from oauth2client.client import GoogleCredentials
 import os
 import logging
 from listenbrainz.stats.exceptions import NoCredentialsVariableException, NoCredentialsFileException
+import listenbrainz.config as config
+import time
+
+JOB_COMPLETION_CHECK_DELAY = 5 # seconds
 
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.INFO)
@@ -26,3 +30,114 @@ def init_bigquery_connection():
     global bigquery
     credentials = GoogleCredentials.get_application_default()
     bigquery = discovery.build('bigquery', 'v2', credentials=credentials)
+
+
+def get_parameters_dict(parameters):
+    """ Converts a list of parameters to be passed to BigQuery into the standard format that the API requires.
+        The format can be seen here:
+        https://developers.google.com/resources/api-libraries/documentation/bigquery/v2/python/latest/bigquery_v2.jobs.html#query
+
+        Args: parameters: a list of dictionaries of the following form
+                {
+                    "name" (str): name of the parameter,
+                    "type" (str): type of the parameter,
+                    "value" (str): value of the parameter
+                }
+
+        Returns: A list of dictionaries that can be passed to the API call
+    """
+
+    bq_params = []
+    for param in parameters:
+        # construct parameter dict
+        temp = {}
+        temp["name"] = param["name"]
+        temp["parameterType"] = {
+            "type": param["type"],
+        }
+        temp["parameterValue"] = {
+            "value": param["value"],
+        }
+
+        # append parameter dict to main list
+        bq_params.append(temp)
+
+    return bq_params
+
+
+def wait_for_completion(projectId, jobId):
+    """ Make requests periodically until the passed job has been completed """
+
+    while True:
+        job = bigquery.jobs().get(projectId=projectId, jobId=jobId).execute(num_retries=5)
+        if job["status"]["state"] == "DONE":
+            return
+        else:
+            time.sleep(JOB_COMPLETION_CHECK_DELAY)
+
+
+def run_query(query, parameters):
+    """ Run provided query on Google BigQuery and return the results in the form of a dictionary
+
+        Note: This is a synchronous action
+    """
+
+    # Run the query
+    query_body = {
+        "kind": "bigquery#queryRequest",
+        "parameterMode": "NAMED",
+        "default_dataset": {
+            "projectId": config.BIGQUERY_PROJECT_ID,
+            "datasetId": config.BIGQUERY_DATASET_ID,
+        },
+        "useLegacySql": False,
+        "queryParameters": get_parameters_dict(parameters),
+        "query": query,
+        "maxResults": 10000
+    }
+
+    response = bigquery.jobs().query(
+        projectId=config.BIGQUERY_PROJECT_ID,
+        body=query_body).execute(num_retries=5)
+
+    job_reference = response['jobReference']
+
+    # Check response to see if query was completed before request timeout.
+    # If it wasn't, wait until it has been completed.
+
+    if not response['jobComplete']:
+        wait_for_completion(**job_reference)
+    else:
+        have_results = True
+
+    data = {}
+    prev_token = None
+    if have_results:
+        data['schema'] = response['schema']
+        data['rows'] = response['rows']
+        try:
+            prev_token = response['pageToken']
+        except KeyError:
+            # if there is no page token, we have all the results
+            # so just return the data
+            return data
+    else:
+        query_result = bigquery.jobs().getQueryResults(**job_reference).execute(num_retries=5)
+        data['schema'] = query_result['schema']
+        data['rows'] = query_result['rows']
+        try:
+            prev_token = query_result['pageToken']
+        except KeyError:
+            # if there is no page token, we have all the results
+            # so just return the data
+            return data
+
+    # keep making requests until we reach the last page and return the data
+    # as soon as we do
+    while True:
+        query_result = bigquery.jobs().getQueryResults(pageToken=prev_token, **job_reference).execute(num_retries=5)
+        data['rows'].extend(query_result['rows'])
+        try:
+            prev_token = query_result['pageToken']
+        except KeyError:
+            return data
---
+++ listenbrainz/stats/user.py
@@ -0,0 +1,65 @@
+"""
+This module contains functions that create jobs and retrieve stats about users
+from Google BigQuery
+"""
+import listenbrainz.config as config
+import json
+from listenbrainz import stats
+
+
+def get_top_tracks(musicbrainz_id, time_interval=None):
+    """ Get top tracks of user with given MusicBrainz ID over a particular time interval
+
+        Args:
+            musicbrainz_id (str): The MusicBrainz ID of the user
+            time_interval  (str): Interval in the BigQuery interval format which can be seen here:
+            https://cloud.google.com/bigquery/docs/reference/standard-sql/functions-and-operators#timestamp_sub
+
+        Returns:
+            sorted list of the following format
+            [
+                {
+                    "track_name" (str)
+                    "recording_msid" (uuid)
+                    "artist_name" (str)
+                    "artist_msid" (uuid)
+                    "listen_count" (int)
+                }
+            ]
+    """
+
+    filter_str = ""
+    if time_interval:
+        filter_str = "AND TIMESTAMP_SUB(CURRENT_TIMESTAMP(), INTERVAL {}".format(time_interval)
+
+    # construct the query string
+    query = """SELECT artist_name
+                    , artist_msid
+                    , recording_msid
+                    , track_name
+                    , COUNT(recording_msid) as listen_count
+                 FROM {dataset_id}.{table_id}
+                WHERE user_name = @username
+                {time_filter_clause}
+             GROUP BY recording_msid, track_name, artist_name, artist_msid
+             ORDER BY listen_count DESC
+            """.format(
+                dataset_id=config.BIGQUERY_DATASET_ID,
+                table_id=config.BIGQUERY_TABLE_ID,
+                time_filter_clause=filter_str,
+            )
+
+    # construct the parameters that must be passed to the Google BigQuery API
+
+    # start with a list of parameters to pass and then convert it to standard format
+    # required by Google BigQuery
+    parameters = [
+        {
+            "name": "username",
+            "type": "STRING",
+            "value": musicbrainz_id,
+        },
+    ]
+
+    return stats.run_query(query, parameters)
+
