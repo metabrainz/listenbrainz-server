756551a2abf907db2eded3df62e45646bef4246e
Param Singh <paramsingh258@gmail.com>
2017-06-25 16:50:30 +0530

    Move test from user to api

--- listenbrainz/webserver/views/test/test_user.py
+++ listenbrainz/webserver/views/test/test_user.py
@@ -16,43 +16,6 @@ class UserViewsTestCase(ServerTestCase, DatabaseTestCase):
         response = self.client.get(url_for('user.profile', user_name=self.user['musicbrainz_id']))
         self.assert200(response)
 
-    def test_latest_import(self):
-        """ Test for user.latest_import """
-
-        # initially the value of latest_import will be 0
-        response = self.client.get(url_for('user.latest_import'), query_string={'user_name': self.user['musicbrainz_id']})
-        self.assert200(response)
-        data = ujson.loads(response.data)
-        self.assertEqual(data['musicbrainz_id'], self.user['musicbrainz_id'])
-        self.assertEqual(data['latest_import'], 0)
-
-        # now an update
-        val = int(time.time())
-        response = self.client.post(
-            url_for('user.latest_import'),
-            data=ujson.dumps({'ts': val}),
-            headers={'Authorization': 'Token {token}'.format(token=self.user['auth_token'])}
-        )
-        self.assert200(response)
-
-        # now the value must have changed
-        response = self.client.get(url_for('user.latest_import'), query_string={'user_name': self.user['musicbrainz_id']})
-        self.assert200(response)
-        data = ujson.loads(response.data)
-        self.assertEqual(data['musicbrainz_id'], self.user['musicbrainz_id'])
-        self.assertEqual(data['latest_import'], val)
-
-    def test_latest_import_unauthorized(self):
-        """ Test for invalid tokens passed to user.latest_import view"""
-
-        val = int(time.time())
-        response = self.client.post(
-            url_for('user.latest_import'),
-            data=ujson.dumps({'ts': val}),
-            headers={'Authorization': 'Token thisisinvalid'}
-        )
-        self.assert401(response)
-
     def tearDown(self):
         ServerTestCase.tearDown(self)
         DatabaseTestCase.tearDown(self)
