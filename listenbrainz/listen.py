9f48b19a5377178f0cc855601840ce6548d6941a
Param Singh <paramsingh258@gmail.com>
2017-07-05 14:58:23 +0530

    Only add recording msid into influx tags
    Also, only use it for dedup.

--- listenbrainz/listen.py
+++ listenbrainz/listen.py
@@ -179,7 +179,6 @@ class Listen(object):
             'time' : self.ts_since_epoch,
             'tags' : {
                 'user_name' : escape(self.user_name),
-                'artist_msid' : self.artist_msid,
                 'recording_msid' : self.recording_msid,
             },
             'fields' : {
