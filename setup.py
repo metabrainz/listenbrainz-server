34c2a08a4728d8519bc4025f2d5221ad94f4449f
Robert Kaye <rob@musicbrainz.org>
2017-07-06 17:10:27 +0200

    Woo, this isn't needed!

--- setup.py
+++
@@ -1,7 +0,0 @@
-import os
-from shutil import copyfile
-
-# IMPORTANT NOTE: This file exists only for use with building docs!
-
-if not os.path.exists("listenbrainz/config.py"):
-    copyfile("listenbrainz/config.py.sample", "listenbrainz/config.py")
