6a4ff2d03a9173c231c630e67dd3ef58ecc1245f
Param Singh <paramsingh258@gmail.com>
2017-08-03 23:36:30 +0530

    Write code for calculation of user stats

---
+++ listenbrainz/stats/calculate.py
@@ -0,0 +1,21 @@
+import listenbrainz.stats.user as stats_user
+import listenbrainz.db.user as db_user
+import listenbrainz.db.stats as db_stats
+
+
+def calculate_user_stats():
+    for user in get_recently_logged_in_users():
+        recordings = stats_user.get_top_recordings(musicbrainz_id=user['musicbrainz_id'])
+        artists    = stats_user.get_top_artists(musicbrainz_id=user['musicbrainz_id'])
+        releases   = stats_user.get_top_releases(musicbrainz_id=user['musicbrainz_id'])
+
+        db_stats.insert_user_stats(
+            user_id=user['id'],
+            artists=artists,
+            recordings=recordings,
+            releases=releases,
+        )
+
+def calculate():
+    pass
+
