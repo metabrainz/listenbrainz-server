1b19a11cc453aad7cedc8ca3a62c222cffa167e4
Param Singh <paramsingh258@gmail.com>
2017-08-04 00:41:08 +0530

    Comment to think about

--- admin/sql/updates/2017-08-03-make-stats-updated-not-null.sql
+++ admin/sql/updates/2017-08-03-make-stats-updated-not-null.sql
@@ -1,5 +1,8 @@
 BEGIN;
 
+-- XXX(param): What should values that are null already be set to here now?
+-- timestamp 0 or NOW() ?
+
 ALTER TABLE statistics.user ALTER COLUMN last_updated SET NOT NULL;
 ALTER TABLE statistics.user ALTER COLUMN last_updated SET DEFAULT NOW();
 
