7362d80ad18175cfe420ad59415e9363d777c61d
Param Singh <paramsingh258@gmail.com>
2017-08-22 14:34:58 +0530

    Set NULL values of last_updated in stat tables to 0

--- admin/sql/updates/2017-08-03-make-stats-updated-not-null.sql
+++ admin/sql/updates/2017-08-03-make-stats-updated-not-null.sql
@@ -1,7 +1,9 @@
 BEGIN;
 
--- XXX(param): What should values that are null already be set to here now?
--- timestamp 0 or NOW() ?
+UPDATE statistics.user SET last_updated = to_timestamp(0) WHERE last_updated IS NULL;
+UPDATE statistics.artist SET last_updated = to_timestamp(0) WHERE last_updated IS NULL;
+UPDATE statistics.release SET last_updated = to_timestamp(0) WHERE last_updated IS NULL;
+UPDATE statistics.recording SET last_updated = to_timestamp(0) WHERE last_updated IS NULL;
 
 ALTER TABLE statistics.user ALTER COLUMN last_updated SET NOT NULL;
 ALTER TABLE statistics.user ALTER COLUMN last_updated SET DEFAULT NOW();
