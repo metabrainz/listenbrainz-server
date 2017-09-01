9351044abf3d774659fc596380fcc08b12a9f083
Param Singh <paramsingh258@gmail.com>
2017-09-01 15:22:58 +0530

    Address TODO comments and add docstrings to new functions
    Also improve tests and variable names a bit

--- listenbrainz/db/tests/test_stats.py
+++ listenbrainz/db/tests/test_stats.py
@@ -1,6 +1,4 @@
 # -*- coding: utf-8 -*-
-import time
-import uuid
 import json
 import os
 import listenbrainz.db.user as db_user
@@ -17,9 +15,6 @@ class StatsDatabaseTestCase(DatabaseTestCase):
         self.user = db_user.get_or_create('stats_user')
 
     def path_to_data_file(self, filename):
-        # XXX(param): we have this function in IntegrationTestCase also,
-        # maybe find some way to share it
-        # ListenBrainzTestCase?
        return os.path.join(StatsDatabaseTestCase.TEST_DATA_PATH, filename)
 
     def test_insert_user_stats(self):
@@ -40,10 +35,10 @@ class StatsDatabaseTestCase(DatabaseTestCase):
             artist_count=2,
         )
 
-        # TODO(param): test the last_updated values too
         result = db_stats.get_user_stats(user_id=self.user['id'])
         self.assertDictEqual(result['artists']['all_time'], artists)
         self.assertEqual(result['artists']['count'], 2)
         self.assertDictEqual(result['releases'], releases)
         self.assertDictEqual(result['recordings'], recordings)
+        self.assertGreater(int(result['last_updated'].strftime('%s')), 0)
 
