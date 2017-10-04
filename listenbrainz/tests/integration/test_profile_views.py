7780dc36eb0d2784bfaa98db1202c0b303d0d500
Alastair Porter <alastair@porter.net.nz>
2017-10-04 16:06:57 +0200

    Move helper method from user views to profile views integration test

--- listenbrainz/tests/integration/test_profile_views.py
+++ listenbrainz/tests/integration/test_profile_views.py
@@ -12,6 +12,16 @@ class ProfileViewsTestCase(IntegrationTestCase):
         super().setUp()
         self.user = db_user.get_or_create('iliekcomputers')
 
+    def send_listens(self):
+        with open(self.path_to_data_file('user_export_test.json')) as f:
+            payload = json.load(f)
+        return self.client.post(
+            url_for('api_v1.submit_listen'),
+            data=json.dumps(payload),
+            headers={'Authorization': 'Token {}'.format(self.user['auth_token'])},
+            content_type='application/json'
+        )
+
     def test_export(self):
         """
         Test for the user export of ListenBrainz data.
