4e4b407946ada69609ba6ab0e21415f0d8fcc4d3
Param Singh <param@metabrainz.org>
2017-10-15 00:46:36 +0530

    Add new table to drop_tables.sql too

--- admin/sql/drop_tables.sql
+++ admin/sql/drop_tables.sql
@@ -3,5 +3,6 @@ BEGIN;
 DROP TABLE IF EXISTS "user"               CASCADE;
 DROP TABLE IF EXISTS listen               CASCADE;
 DROP TABLE IF EXISTS listen_json          CASCADE;
+DROP TABLE IF EXISTS data_dump            CASCADE;
 
 COMMIT;
