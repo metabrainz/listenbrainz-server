76c8fc81acbaa32b7c8212502aa7d97f10adac40
Param Singh <param@metabrainz.org>
2017-10-14 22:53:33 +0530

    Add logging to the calculate_user_stats function

--- listenbrainz/stats/calculate.py
+++ listenbrainz/stats/calculate.py
@@ -2,11 +2,15 @@ import click
 import listenbrainz.db.stats as db_stats
 import listenbrainz.db.user as db_user
 import listenbrainz.stats.user as stats_user
+import logging
 import time
 
 from listenbrainz import config
 from listenbrainz import db
 from listenbrainz import stats
+
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.INFO)
 
 
 def calculate_user_stats():
@@ -14,11 +18,23 @@ def calculate_user_stats():
     """
 
     for user in db_user.get_users_with_uncalculated_stats():
-        recordings = stats_user.get_top_recordings(musicbrainz_id=user['musicbrainz_id'])
-        artists = stats_user.get_top_artists(musicbrainz_id=user['musicbrainz_id'])
-        releases = stats_user.get_top_releases(musicbrainz_id=user['musicbrainz_id'])
-        artist_count = stats_user.get_artist_count(musicbrainz_id=user['musicbrainz_id'])
 
+        logger.info('Calculating statistics for user %s...', user['musicbrainz_id'])
+
+        recordings = stats_user.get_top_recordings(musicbrainz_id=user['musicbrainz_id'])
+        logger.info('Top recordings for user %s done!', user['musicbrainz_id'])
+
+        artists = stats_user.get_top_artists(musicbrainz_id=user['musicbrainz_id'])
+        logger.info('Top artists for user %s done!', user['musicbrainz_id'])
+
+        releases = stats_user.get_top_releases(musicbrainz_id=user['musicbrainz_id'])
+        logger.info('Top releases for user %s done!', user['musicbrainz_id'])
+
+        artist_count = stats_user.get_artist_count(musicbrainz_id=user['musicbrainz_id'])
+        logger.info('Artist count for user %s done!', user['musicbrainz_id'])
+
+
+        logger.info('Inserting calculated stats for user %s into db', user['musicbrainz_id'])
         db_stats.insert_user_stats(
             user_id=user['id'],
             artists=artists,
@@ -26,6 +42,7 @@ def calculate_user_stats():
             releases=releases,
             artist_count=artist_count
         )
+        logger.info('Stats calculation for user %s done!', user['musicbrainz_id'])
 
 
 def calculate_stats():
