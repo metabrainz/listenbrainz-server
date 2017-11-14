403c5f8d612c8bd36aee69425d2d5a90842af8dd
Justin W. Flory <git@jwf.io>
2017-11-14 18:48:05 -0500

    LB-244: Use newer PostgreSQL syntax for SQL initialization

--- admin/sql/create_db.sql
+++ admin/sql/create_db.sql
@@ -1,4 +1,4 @@
 -- Create the user and the database. Must run as user postgres.
 
-CREATE USER listenbrainz NOCREATEDB NOCREATEUSER;
+CREATE USER listenbrainz NOCREATEDB NOSUPERUSER;
 CREATE DATABASE listenbrainz WITH OWNER = listenbrainz TEMPLATE template0 ENCODING = 'UNICODE';
--- admin/sql/create_test_db.sql
+++ admin/sql/create_test_db.sql
@@ -1,4 +1,4 @@
 -- Create the user and the database. Must run as user postgres.
 
-CREATE USER lb_test NOCREATEDB NOCREATEUSER;
+CREATE USER lb_test NOCREATEDB NOSUPERUSER;
 CREATE DATABASE lb_test WITH OWNER = lb_test TEMPLATE template0 ENCODING = 'UNICODE';
