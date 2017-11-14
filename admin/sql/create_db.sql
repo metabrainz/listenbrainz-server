d086fd82d8cc9caad73515451f19c0c8eedb90a6
Param Singh <param@metabrainz.org>
2017-11-15 01:33:05 +0530

    User NOSUPERUSER instead of NOCREATEUSER

--- admin/sql/create_db.sql
+++ admin/sql/create_db.sql
@@ -1,4 +1,4 @@
 -- Create the user and the database. Must run as user postgres.
 
-CREATE USER messybrainz NOCREATEDB NOCREATEUSER;
+CREATE USER messybrainz NOCREATEDB NOSUPERUSER;
 CREATE DATABASE messybrainz WITH OWNER = messybrainz TEMPLATE template0 ENCODING = 'UNICODE';
