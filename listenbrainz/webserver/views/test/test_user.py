3da2478293d5ebb175d29461180fed9388f04616
Kartikeya Sharma <09kartikeya@gmail.com>
2017-11-25 00:37:59 +0530

    Add unit test to check that URL in profile is case insensitive

--- listenbrainz/webserver/views/test/test_user.py
+++ listenbrainz/webserver/views/test/test_user.py
@@ -4,6 +4,12 @@ import listenbrainz.db.user as db_user
 from listenbrainz.db.testing import DatabaseTestCase
 from listenbrainz.webserver.testing import ServerTestCase
 
+from listenbrainz.listenstore.tests.util import create_test_data_for_influxlistenstore
+from listenbrainz.webserver.influx_connection import init_influx_connection
+from influxdb import InfluxDBClient
+from listenbrainz import config
+import logging
+
 
 class UserViewsTestCase(ServerTestCase, DatabaseTestCase):
     def setUp(self):
@@ -12,7 +18,22 @@ class UserViewsTestCase(ServerTestCase, DatabaseTestCase):
         self.user = db_user.get_or_create('iliekcomputers')
         self.weirduser = db_user.get_or_create('weird\\user name')
 
+        self.log = logging.getLogger(__name__)
+
+        influx = InfluxDBClient(host=config.INFLUX_HOST, port=config.INFLUX_PORT, database=config.INFLUX_DB_NAME)
+        influx.query('''drop database %s''' % config.INFLUX_DB_NAME)
+        influx.query('''create database %s''' % config.INFLUX_DB_NAME)
+
+        self.logstore = init_influx_connection(self.log, {
+            'REDIS_HOST': config.REDIS_HOST,
+            'REDIS_PORT': config.REDIS_PORT,
+            'INFLUX_HOST': config.INFLUX_HOST,
+            'INFLUX_PORT': config.INFLUX_PORT,
+            'INFLUX_DB_NAME': config.INFLUX_DB_NAME,
+        })
+
     def tearDown(self):
+        self.logstore = None
         ServerTestCase.tearDown(self)
         DatabaseTestCase.tearDown(self)
 
@@ -41,3 +62,17 @@ class UserViewsTestCase(ServerTestCase, DatabaseTestCase):
         )
         self.assert200(response)
         self.assertIn('var user_name = "weird%5Cuser%20name";', response.data.decode('utf-8'))
+
+    def _create_test_data(self, user_name):
+        test_data = create_test_data_for_influxlistenstore(user_name)
+        self.logstore.insert(test_data)
+
+    def test_username_case(self):
+        """Tests that the username in URL is case insenstive"""
+        self._create_test_data(self.user['musicbrainz_id'])
+
+        response1 = self.client.get(url_for('user.profile', user_name='iliekcomputers'))
+        response2 = self.client.get(url_for('user.profile', user_name='IlieKcomPUteRs'))
+        self.assert200(response1)
+        self.assert200(response2)
+        self.assertEqual(response1.data.decode('utf-8'), response2.data.decode('utf-8'))
