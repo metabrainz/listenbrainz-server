8d0b987ae4418a0714b6edb101898c971d82c82d
Alastair Porter <alastair.porter@upf.edu>
2018-03-14 18:49:16 +0100

    Fix missing query string format in error message

--- listenbrainz/webserver/views/test/test_user.py
+++ listenbrainz/webserver/views/test/test_user.py
@@ -1,6 +1,7 @@
 import listenbrainz.db.stats as db_stats
 import listenbrainz.db.user as db_user
 import ujson
+from unittest import mock
 
 from flask import url_for, current_app
 from influxdb import InfluxDBClient
@@ -69,7 +70,6 @@ class UserViewsTestCase(ServerTestCase, DatabaseTestCase):
         self.assertTemplateUsed('user/profile.html')
         self.assertContext('artist_count', '2')
 
-
     def test_scraper_username(self):
         """ Tests that the username is correctly rendered in the last.fm importer """
         response = self.client.get(
@@ -119,11 +119,9 @@ class UserViewsTestCase(ServerTestCase, DatabaseTestCase):
         self.assert200(r)
         self.assertContext('section', 'artists')
 
-
     def _create_test_data(self, user_name):
         test_data = create_test_data_for_influxlistenstore(user_name)
         self.logstore.insert(test_data)
-
 
     def test_username_case(self):
         """Tests that the username in URL is case insensitive"""
@@ -134,3 +132,45 @@ class UserViewsTestCase(ServerTestCase, DatabaseTestCase):
         self.assert200(response1)
         self.assert200(response2)
         self.assertEqual(response1.data.decode('utf-8'), response2.data.decode('utf-8'))
+
+    @mock.patch('listenbrainz.webserver.views.user.time')
+    @mock.patch('listenbrainz.webserver.influx_connection._influx.fetch_listens')
+    def test_ts_filters(self, influx, m_time):
+        """Check that max_ts and min_ts are passed to the influx """
+
+        # If no parameter is given, use current time as the to_ts
+        m_time.time.return_value = 1520946608
+        self.client.get(url_for('user.profile', user_name='iliekcomputers'))
+        influx.assert_called_with('iliekcomputers', limit=25, to_ts=1520946608)
+        influx.reset_mock()
+
+        # max_ts query param -> to_ts influx param
+        self.client.get(url_for('user.profile', user_name='iliekcomputers'), query_string={'max_ts': 1520946000})
+        influx.assert_called_with('iliekcomputers', limit=25, to_ts=1520946000)
+        influx.reset_mock()
+
+        # min_ts query param -> from_ts influx param
+        self.client.get(url_for('user.profile', user_name='iliekcomputers'), query_string={'min_ts': 1520941000})
+        influx.assert_called_with('iliekcomputers', limit=25, from_ts=1520941000)
+        influx.reset_mock()
+
+        # If max_ts and min_ts set, only max_ts is used
+        self.client.get(url_for('user.profile', user_name='iliekcomputers'),
+                        query_string={'min_ts': 1520941000, 'max_ts': 1520946000})
+        influx.assert_called_with('iliekcomputers', limit=25, to_ts=1520946000)
+
+    @mock.patch('listenbrainz.webserver.influx_connection._influx.fetch_listens')
+    def test_ts_filters_errors(self, influx):
+        """If max_ts and min_ts are not integers, show an error page"""
+        self._create_test_data('iliekcomputers')
+        response = self.client.get(url_for('user.profile', user_name='iliekcomputers'),
+                                   query_string={'max_ts': 'a'})
+        self.assert400(response)
+        self.assertIn(b'Incorrect timestamp argument max_ts: a', response.data)
+
+        response = self.client.get(url_for('user.profile', user_name='iliekcomputers'),
+                                   query_string={'min_ts': 'b'})
+        self.assert400(response)
+        self.assertIn(b'Incorrect timestamp argument min_ts: b', response.data)
+
+        influx.assert_not_called()
