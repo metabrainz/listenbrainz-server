0eca7df396451e875ea18ccb6515e72bbc566faa
Robert Kaye <rob@musicbrainz.org>
2018-03-28 10:50:17 -0700

    Edits to finish cleanup

--- lb-recommendation.py
+++ lb-recommendation.py
@@ -85,16 +85,16 @@ if __name__ == "__main__":
     print("=== Got %d playcounts from %d users on %d recordings." % (num_playcounts, num_users, num_recordings))
 
     # split playcounts into train (60%), validation (20%), and test (20%) based on the 
-    # last digit of the timestamp, add myRatings to train, and cache them
+    # last digit of the timestamp, add user_playcounts to train, and cache them
 
     # training, validation, test are all RDDs of (userId, movieId, rating)
 
     training, test, validation = playcounts.map(lambda x: x[1]).randomSplit([4, 1, 1], 45)
     numTraining = training.count()
     numValidation = validation.count()
-    numTest = test.count()
+    num_test = test.count()
 
-    print("=== Training: %d, validation: %d, test: %d" % (numTraining, numValidation, numTest))
+    print("=== Training: %d, validation: %d, test: %d" % (numTraining, numValidation, num_test))
 
     # train models and evaluate them on the validation set
 
@@ -119,22 +119,22 @@ if __name__ == "__main__":
             bestLambda = lmbda
             bestNumIter = numIter
 
-    testRmse = compute_rmse(bestModel, test, numTest)
+    test_rmse = compute_rmse(bestModel, test, num_test)
 
     # evaluate the best model on the test set
     print("The best model was trained with rank = %d and lambda = %.1f, " % (bestRank, bestLambda) \
-      + "and numIter = %d, and its RMSE on the test set is %f." % (bestNumIter, testRmse))
+      + "and numIter = %d, and its RMSE on the test set is %f." % (bestNumIter, test_rmse))
 
     # compare the best model with a naive baseline that always returns the mean rating
-    meanRating = training.union(validation).map(lambda x: x[2]).mean()
-    baselineRmse = sqrt(test.map(lambda x: (meanRating - x[2]) ** 2).reduce(add) / numTest)
-    improvement = (baselineRmse - testRmse) / baselineRmse * 100
+    mean_rating = training.union(validation).map(lambda x: x[2]).mean()
+    baseline_rmse = sqrt(test.map(lambda x: (mean_rating - x[2]) ** 2).reduce(add) / num_test)
+    improvement = (baseline_rmse - test_rmse) / baseline_rmse * 100
     print("The best model improves the baseline by %.2f" % (improvement) + "%.")
 
     # make personalized recommendations
 
-    myRatedMovieIds = set([x[1] for x in myRatings])
-    candidates = sc.parallelize([m for m in movies if m not in myRatedMovieIds])
+    user_recording_ids = set([x[1] for x in user_playcounts])
+    candidates = sc.parallelize([m for m in movies if m not in user_recording_ids])
     predictions = bestModel.predictAll(candidates.map(lambda x: (0, x))).collect()
     recommendations = sorted(predictions, key=lambda x: x[2], reverse=True)[:50]
 
