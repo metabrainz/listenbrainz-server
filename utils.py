3e967a7a40eb3fc409ae4892eb849a4eb7d289f5
Param Singh <paramsingh258@gmail.com>
2018-04-01 18:04:16 +0530

    Add function to load playcount dataframe

--- utils.py
+++ utils.py
@@ -2,9 +2,12 @@ import os
 from setup import spark, sc
 from pyspark.sql import SQLContext
 
+def load_df(directory, df):
+    sql_context = SQLContext(sc)
+    return sql_context.read.format('parquet').load(os.path.join(directory, '%s.parquet' % df))
 
 def load_listens_df(directory):
+    return load_df(directory, 'listen')
 
-    sql_context = SQLContext(sc)
-    listens_df = sql_context.read.format("parquet").load(os.path.join(directory, "listen.parquet"))
-    return listens_df
+def load_playcounts_df(directory):
+    return load_df(directory, 'playcount')
