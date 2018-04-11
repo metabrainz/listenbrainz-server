f53b3f5118a511bc7182b365ab20b27c423fa31a
Robert Kaye <rob@metabrainz.org>
2018-04-11 17:52:56 +0200

    Add proper docker support, part 1. Add status messages to scripts.

--- scripts/setup-node.sh
+++ scripts/setup-node.sh
@@ -1,9 +1,17 @@
 #!/bin/sh
 
+if [ "$#" -ne 2 ]; then
+    echo "  Usage: start-node.sh <private subnet>"
+fi
+
+PRIVATE_SUBNET=$1
+
+PRIVATE_IP=$(ip -4 route get $PRIVATE_SUBNET|awk '{print $NF;exit}')
 CONSUL_IMAGE=library/consul:0.9.3
 
 source "${BASH_SOURCE%/*}/consul.sh"
 
+# Install docker, the MetaBrainz way
 apt-get update
 apt-get -y install \
     apt-transport-https
@@ -12,13 +20,14 @@ git clone https://github.com/metabrainz/docker-helpers.git
 cd docker-helpers
 ./docker_install_xenial.sh
 
-docker pull metabrainz/spark-worker
+# start the consul agent and registrator
+start_consul_agent
+start_registrator
 
+# start the spark worker container
+docker pull metabrainz/spark-worker
 docker run \
    --name spark-worker \
    --restart=unless-stopped \
    -d \
    metabrainz/spark-worker
-
-start_consul_agent
-start_registrator
