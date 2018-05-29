218b8483856cba92f1be185829b71f9837877ad5
Kartikeya Sharma <09kartikeya@gmail.com>
2018-05-30 01:31:39 +0530

    Remove unnecessary checks
    Remove checks for valid UUID as submitted recording_json
    contain only valid UUIDs. This is ensured by ListenBrainz
    which is the only source of data currently in MessyBrainz.
    Remove check for recording MBIDs is present in recording_artist_json
    table or not. As one query while selecting MBIDs does the trick.

--- messybrainz/fetch_artist_mbids.py
+++ messybrainz/fetch_artist_mbids.py
@@ -9,16 +9,6 @@ from brainzutils.musicbrainz_db.exceptions import NoDataFoundException
 from messybrainz import db
 from messybrainz import data
 from sqlalchemy import text
-import uuid
-
-
-# lifted from AcousticBrainz
-def is_valid_uuid(u):
-    try:
-        u = uuid.UUID(u)
-        return True
-    except ValueError:
-        return False
 
 
 def insert_artist_mbids(connection, recording_mbid, artist_mbids):
@@ -37,27 +27,6 @@ def insert_artist_mbids(connection, recording_mbid, artist_mbids):
         })
 
 
-def is_recording_mbid_present(connection, recording_mbid):
-    """
-        Check if recording MBID is already present in recording_artist_join table.
-        Returns True if recording MBID is present else False is returned.
-    """
-
-    query = text("""SELECT recording_mbid
-                      FROM recording_artist_join
-                     WHERE recording_mbid = :recording_mbid
-    """)
-
-    result = connection.execute(query, {
-    "recording_mbid": recording_mbid,
-    })
-
-    if result.rowcount:
-        return True
-
-    return False
-
-
 def fetch_and_store_artist_mbids(connection, recording_mbid):
     """ Fetches artist MBIDs from the MusicBrainz database for the recording MBID.
         And inserts the artist MBIDs into the recording_artist_join table.
@@ -72,6 +41,26 @@ def fetch_and_store_artist_mbids(connection, recording_mbid):
     insert_artist_mbids(connection, recording_mbid, artist_mbids)
 
 
+def fetch_recording_mbids_not_in_recording_artist_join(connection):
+    """ Fetches recording MBIDs that are present in recording_json table
+        but are not present in recording_artist_join table and returns
+        those recording MBIDs.
+    """
+
+    query = text("""SELECT DISTINCT data ->> 'recording_mbid'
+                               FROM recording_json
+                              WHERE data ->> 'recording_mbid' NOT IN (
+                                  SELECT DISTINCT recording_mbid::text
+                                             FROM recording_artist_join
+                              )
+                                AND data ->> 'recording_mbid' IS NOT NULL
+    """)
+
+    result = connection.execute(query)
+
+    return result
+
+
 def fetch_and_store_artist_mbids_for_all_recording_mbids(reset=False):
     """ Fetches artist MBIDs from the musicbrainz database for the recording MBIDs
         in the recording_json table submitted while submitting a listen.
@@ -84,16 +73,14 @@ def fetch_and_store_artist_mbids_for_all_recording_mbids(reset=False):
             query = text("""TRUNCATE TABLE recording_artist_join""")
             connection.execute(query)
 
-        recording_mbids = data.fetch_distinct_recording_mbids(connection)
+        recording_mbids = fetch_recording_mbids_not_in_recording_artist_join(connection)
         num_recording_mbids_added = 0
         num_recording_mbids_processed = recording_mbids.rowcount
         for recording_mbid in recording_mbids:
-            if is_valid_uuid(recording_mbid[0]):
-                if not is_recording_mbid_present(connection, recording_mbid[0]):
-                    try:
-                        fetch_and_store_artist_mbids(connection, recording_mbid[0])
-                        num_recording_mbids_added += 1
-                    except NoDataFoundException:
-                        pass
+            try:
+                fetch_and_store_artist_mbids(connection, recording_mbid[0])
+                num_recording_mbids_added += 1
+            except NoDataFoundException:
+                pass
 
         return num_recording_mbids_processed, num_recording_mbids_added
