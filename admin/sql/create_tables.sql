cfb01b81d7fb8c730ed755a53383a77e33539190
Kartikeya Sharma <09kartikeya@gmail.com>
2018-06-14 00:28:56 +0530

    Add a new table
    Add artist_credit_redirect_array table to store
    cluster_id, artist_mbids_array pairs.

--- admin/sql/create_tables.sql
+++ admin/sql/create_tables.sql
@@ -22,6 +22,12 @@ CREATE TABLE artist_credit_redirect (
 );
 ALTER TABLE artist_credit_redirect ADD CONSTRAINT artist_credit_redirect_uniq UNIQUE (artist_credit_cluster_id, artist_mbid);
 
+CREATE TABLE artist_credit_redirect_array (
+  artist_credit_cluster_id UUID NOT NULL, -- FK to artist_credit_cluster.cluster_id
+  artist_mbids_array       UUID[] NOT NULL
+);
+ALTER TABLE artist_credit_redirect_array ADD CONSTRAINT artist_credit_redirect_array_artist_mbids_array_uniq UNIQUE (artist_mbids_array);
+
 CREATE TABLE recording (
   id         SERIAL,
   gid        UUID    NOT NULL,
--- admin/sql/drop_tables.sql
+++ admin/sql/drop_tables.sql
@@ -1,15 +1,16 @@
 BEGIN;
 
-DROP TABLE IF EXISTS recording              CASCADE;
-DROP TABLE IF EXISTS recording_json         CASCADE;
-DROP TABLE IF EXISTS artist_credit          CASCADE;
-DROP TABLE IF EXISTS release                CASCADE;
-DROP TABLE IF EXISTS recording_redirect     CASCADE;
-DROP TABLE IF EXISTS recording_cluster      CASCADE;
-DROP TABLE IF EXISTS artist_credit_cluster  CASCADE;
-DROP TABLE IF EXISTS artist_credit_redirect CASCADE;
-DROP TABLE IF EXISTS release_cluster        CASCADE;
-DROP TABLE IF EXISTS release_redirect       CASCADE;
-DROP TABLE IF EXISTS recording_artist_join  CASCADE;
+DROP TABLE IF EXISTS recording                    CASCADE;
+DROP TABLE IF EXISTS recording_json               CASCADE;
+DROP TABLE IF EXISTS artist_credit                CASCADE;
+DROP TABLE IF EXISTS release                      CASCADE;
+DROP TABLE IF EXISTS recording_redirect           CASCADE;
+DROP TABLE IF EXISTS recording_cluster            CASCADE;
+DROP TABLE IF EXISTS artist_credit_cluster        CASCADE;
+DROP TABLE IF EXISTS artist_credit_redirect       CASCADE;
+DROP TABLE IF EXISTS release_cluster              CASCADE;
+DROP TABLE IF EXISTS release_redirect             CASCADE;
+DROP TABLE IF EXISTS recording_artist_join        CASCADE;
+DROP TABLE IF EXISTS artist_credit_redirect_array CASCADE;
 
 COMMIT;
