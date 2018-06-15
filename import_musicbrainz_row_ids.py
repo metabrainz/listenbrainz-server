63e2550b731aa03e6b4a53fd4c39255d51823492
Param Singh <paramsingh258@gmail.com>
2018-06-15 16:17:24 +0530

    Connect to LB db only once

--- import_musicbrainz_row_ids.py
+++ import_musicbrainz_row_ids.py
@@ -9,32 +9,32 @@ def import_musicbrainz_rows(musicbrainz_db_uri):
     db.init_db_connection(config.SQLALCHEMY_DATABASE_URI)
     users = db_user.get_all_users()
     with musicbrainz_db.engine.connect() as mb_connection:
-        for user in users:
-            name = user['musicbrainz_id']
-            result = mb_connection.execute(sqlalchemy.text("""
-                    SELECT id
-                      FROM editor
-                     WHERE LOWER(name) = LOWER(:name)
-                """), {
-                    'name': name,
-                })
-            musicbrainz_row_id = None
-            if result.rowcount > 0:
-                musicbrainz_row_id = result.fetchone()['id']
-            else:
-                print('No user with specified username in the MusicBrainz db: %s' % name)
-                continue
-
-            with db.engine.connect() as connection:
-                connection.execute(sqlalchemy.text("""
-                        UPDATE "user"
-                           SET musicbrainz_row_id = :musicbrainz_row_id
-                         WHERE id = :id
+        with db.engine.connect() as connection:
+            for user in users:
+                name = user['musicbrainz_id']
+                result = mb_connection.execute(sqlalchemy.text("""
+                        SELECT id
+                          FROM editor
+                         WHERE LOWER(name) = LOWER(:name)
                     """), {
-                        'musicbrainz_row_id': musicbrainz_row_id,
-                        'id': user['id'],
+                        'name': name,
                     })
-                print('Inserted row_id %d for user %s' % (musicbrainz_row_id, name))
+                musicbrainz_row_id = None
+                if result.rowcount > 0:
+                    musicbrainz_row_id = result.fetchone()['id']
+                else:
+                    print('No user with specified username in the MusicBrainz db: %s' % name)
+                    continue
+
+                    connection.execute(sqlalchemy.text("""
+                            UPDATE "user"
+                               SET musicbrainz_row_id = :musicbrainz_row_id
+                             WHERE id = :id
+                        """), {
+                            'musicbrainz_row_id': musicbrainz_row_id,
+                            'id': user['id'],
+                        })
+                    print('Inserted row_id %d for user %s' % (musicbrainz_row_id, name))
 
 
 if __name__ == '__main__':
