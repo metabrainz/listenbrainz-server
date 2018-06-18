0942c09d7165ae855d283b3b4f724553762eb1e5
Param Singh <paramsingh258@gmail.com>
2018-06-18 17:01:23 +0530

    Count how many users can be imported in script

--- import_musicbrainz_row_ids.py
+++ import_musicbrainz_row_ids.py
@@ -4,14 +4,19 @@ from brainzutils import musicbrainz_db
 import listenbrainz.db.user as db_user
 import sqlalchemy
 
-def import_musicbrainz_rows(musicbrainz_db_uri):
+
+def import_musicbrainz_rows(musicbrainz_db_uri, dry_run=True):
     musicbrainz_db.init_db_engine(musicbrainz_db_uri)
     db.init_db_connection(config.SQLALCHEMY_DATABASE_URI)
     users = db_user.get_all_users()
+    import_count = 0
+    already_imported = 0
+    not_found = 0
     with musicbrainz_db.engine.connect() as mb_connection:
         with db.engine.connect() as connection:
             for user in users:
                 if user['musicbrainz_row_id'] is not None:
+                    already_imported += 1
                     continue
                 name = user['musicbrainz_id']
                 result = mb_connection.execute(sqlalchemy.text("""
@@ -24,20 +29,28 @@ def import_musicbrainz_rows(musicbrainz_db_uri):
                 musicbrainz_row_id = None
                 if result.rowcount > 0:
                     musicbrainz_row_id = result.fetchone()['id']
+                    import_count += 1
+                    print("hello")
                 else:
                     print('No user with specified username in the MusicBrainz db: %s' % name)
+                    not_found += 1
                     continue
 
-                connection.execute(sqlalchemy.text("""
-                        UPDATE "user"
-                           SET musicbrainz_row_id = :musicbrainz_row_id
-                         WHERE id = :id
-                    """), {
-                        'musicbrainz_row_id': musicbrainz_row_id,
-                        'id': user['id'],
-                    })
-                print('Inserted row_id %d for user %s' % (musicbrainz_row_id, name))
+                if not dry_run:
+                    connection.execute(sqlalchemy.text("""
+                            UPDATE "user"
+                               SET musicbrainz_row_id = :musicbrainz_row_id
+                             WHERE id = :id
+                        """), {
+                            'musicbrainz_row_id': musicbrainz_row_id,
+                            'id': user['id'],
+                        })
+                    print('Inserted row_id %d for user %s' % (musicbrainz_row_id, name))
+    print('Total number of ListenBrainz users: %d' % len(users))
+    print('Total number of ListenBrainz users with already imported row ids: %d' % already_imported)
+    print('Total number of ListenBrainz users whose row ids can be imported: %d' % import_count)
+    print('Total number of ListenBrainz users not found in MusicBrainz: %d' % not_found)
 
 
 if __name__ == '__main__':
-    import_musicbrainz_rows(config.MB_DATABASE_URI)
+    import_musicbrainz_rows(config.MB_DATABASE_URI, dry_run=config.MUSICBRAINZ_IMPORT_DRY_RUN)
