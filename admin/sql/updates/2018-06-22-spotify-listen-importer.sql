a99bc639971e7bce83df37943c9bdbb582072bf9
Param Singh <paramsingh258@gmail.com>
2018-06-22 22:29:37 +0530

    Add schema update script for spotify listen importer

---
+++ admin/sql/updates/2018-06-22-spotify-listen-importer.sql
@@ -0,0 +1,17 @@
+BEGIN;
+
+CREATE TABLE spotify (
+  user_id         INTEGER NOT NULL, -- FK to user.id
+  user_token      VARCHAR NOT NULL,
+  token_expires   TIMESTAMP WITH TIME ZONE,
+  refresh_token   VARCHAR NOT NULL,
+  last_updated    TIMESTAMP WITH TIME ZONE,
+  active          BOOLEAN DEFAULT TRUE,
+  update_error    VARCHAR
+);
+
+ALTER TABLE spotify ADD CONSTRAINT spotify_user_id_foreign_key FOREIGN KEY (user_id) REFERENCES "user" (id);
+
+CREATE UNIQUE INDEX user_id_ndx_spotify ON SPOTIFY (user_id);
+
+COMMIT;
