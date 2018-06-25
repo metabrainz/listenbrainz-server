f3f1a33cc8736483c0f5a78a6e27dd623e848444
Param Singh <paramsingh258@gmail.com>
2018-06-25 21:53:58 +0530

    Update schema version and add index to schema update script.

--- admin/sql/updates/2018-06-22-spotify-listen-importer.sql
+++ admin/sql/updates/2018-06-22-spotify-listen-importer.sql
@@ -12,7 +12,7 @@ CREATE TABLE spotify_auth (
 );
 
 ALTER TABLE spotify_auth ADD CONSTRAINT spotify_auth_user_id_foreign_key FOREIGN KEY (user_id) REFERENCES "user" (id);
-
 ALTER TABLE spotify_auth ADD CONSTRAINT spotify_auth_pkey PRIMARY KEY (user_id);
+CREATE INDEX latest_listened_at_spotify_auth ON spotify_auth (latest_listened_at DESC NULLS LAST);
 
 COMMIT;
