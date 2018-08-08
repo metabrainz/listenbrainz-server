64feb976894910036f372af2eafb81c674085cc9
Kartikeya Sharma <09kartikeya@gmail.com>
2018-08-09 00:44:35 +0530

    Create indexes to speedup the clustering

--- admin/sql/create_indexes.sql
+++ admin/sql/create_indexes.sql
@@ -20,4 +20,24 @@ CREATE INDEX title_ndx_release ON release (title);
 
 CREATE INDEX mbid_ndx_recording ON recording_artist_join (recording_mbid);
 
+CREATE INDEX recording_mbid_ndx_recording_redirect ON recording_redirect (recording_mbid);
+CREATE INDEX artist_mbids_array_ndx_artist_credit_redirect ON artist_credit_redirect (artist_mbids_array);
+CREATE INDEX release_mbid_ndx_recording_redirect ON release_redirect (release_mbid);
+
+CREATE INDEX recording_cluster_id_ndx_recording_redirect ON recording_redirect (recording_cluster_id);
+CREATE INDEX artist_credit_cluster_id_ndx_artist_credit_redirect ON artist_credit_redirect (artist_credit_cluster_id);
+CREATE INDEX release_cluster_id_ndx_recording_redirect ON release_redirect (release_cluster_id);
+
+CREATE INDEX recording_mbid_ndx_recording_json ON recording_json ((data ->> 'recording_mbid'));
+CREATE INDEX artist_mbid_ndx_recording_json ON recording_json ((data ->> 'artist_mbids'));
+CREATE INDEX release_mbid_ndx_recording_json ON recording_json ((data ->> 'release_mbid'));
+CREATE INDEX artist_mbid_array_ndx_recording_json ON recording_json (convert_json_array_to_sorted_uuid_array(data -> 'artist_mbids'));
+
+CREATE INDEX artist_ndx_recording ON recording (artist);
+CREATE INDEX release_ndx_recording ON recording (release);
+
+CREATE INDEX recording_mbid_ndx_recording_release_join ON recording_release_join (recording_mbid);
+CREATE INDEX release_mbid_ndx_recording_release_join ON recording_release_join (release_mbid);
+CREATE INDEX release_name_ndx_recording_release_join ON recording_release_join (release_name);
+
 COMMIT;
