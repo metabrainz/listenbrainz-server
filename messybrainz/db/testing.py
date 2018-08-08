915a1b31384cea309cf0b9aa9782493bf5c2886a
Kartikeya Sharma <09kartikeya@gmail.com>
2018-08-09 00:45:31 +0530

    Create functions first and then indexes
    We create index over function so we first have to declare
    function and then create index using it.

--- messybrainz/db/testing.py
+++ messybrainz/db/testing.py
@@ -56,8 +56,8 @@ class DatabaseTestCase(unittest.TestCase):
         db.run_sql_script(os.path.join(ADMIN_SQL_DIR, 'create_tables.sql'))
         db.run_sql_script(os.path.join(ADMIN_SQL_DIR, 'create_primary_keys.sql'))
         db.run_sql_script(os.path.join(ADMIN_SQL_DIR, 'create_foreign_keys.sql'))
-        db.run_sql_script(os.path.join(ADMIN_SQL_DIR, 'create_indexes.sql'))
         db.run_sql_script(os.path.join(ADMIN_SQL_DIR, 'create_functions.sql'))
+        db.run_sql_script(os.path.join(ADMIN_SQL_DIR, 'create_indexes.sql'))
 
 
     def drop_tables(self):
