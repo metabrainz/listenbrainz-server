72c92a3552078dc71d372fa29e85667c15e49f87
Kartikeya Sharma <09kartikeya@gmail.com>
2018-08-10 20:48:09 +0530

    Change column name to artist_mbids from artist_mbids_array

--- admin/sql/updates/2018-07-05-alter-recording-artist-join.sql
+++ admin/sql/updates/2018-07-05-alter-recording-artist-join.sql
@@ -1,6 +1,6 @@
 BEGIN;
 
 ALTER TABLE recording_artist_join DROP COLUMN artist_mbid;
-ALTER TABLE recording_artist_join ADD COLUMN artist_mbids_array UUID[] NOT NULL;
+ALTER TABLE recording_artist_join ADD COLUMN artist_mbids UUID[] NOT NULL;
 
 COMMIT;
