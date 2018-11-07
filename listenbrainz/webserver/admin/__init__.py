a5351171731e2e40eed6c16742a9995f5d37e573
Param Singh <param@metabrainz.org>
2018-11-07 11:20:15 +0530

    Fix bug in admin views due to new version of flask-login
    And add tests for the admin views

--- listenbrainz/webserver/admin/__init__.py
+++ listenbrainz/webserver/admin/__init__.py
@@ -3,18 +3,21 @@ from flask_admin.contrib.sqla import ModelView
 from flask_login import current_user
 from flask import redirect, url_for, current_app
 
+from listenbrainz.webserver import flash
+
 
 class AuthMixin:
     """All admin views that shouldn't be available to the public must inherit from this."""
 
     def is_accessible(self):
-        if current_user.is_authenticated():
+        if current_user.is_authenticated:
             if current_user.musicbrainz_id in current_app.config['ADMINS']:
                 return True
         return False
 
     def _handle_view(self, name, **kwargs):
         if not self.is_accessible():
+            flash.error('You are not authorized to view the admin page.')
             return redirect(url_for('login.index'))
 
 class AdminBaseView(AuthMixin, BaseView): pass
