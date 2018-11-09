df93e22b5e273c9116729a8b8f46956e7c3d9f75
Param Singh <param@metabrainz.org>
2018-11-09 22:17:56 +0530

    Stream files from dump one by one
    Instead of trying to extract them.

--- listenbrainz_spark/data/dump.py
+++ listenbrainz_spark/data/dump.py
@@ -1,5 +1,6 @@
 import os
 import subprocess
+import tarfile
 import tempfile
 import hdfs
 
@@ -10,42 +11,32 @@ from listenbrainz_spark import spark, sc
 
 FORCE = True
 
-def extract(archive):
-    directory = tempfile.mkdtemp()
-    command = ['tar', '-xvf', archive, '-C', directory]
-    try:
-        x = subprocess.check_output(command)
-        print('Extracted archive to %s!' % directory)
-        return directory
-    except subprocess.CalledProcessError as e:
-        print('Error while extracting: %s' % str(e))
-        print(e.output)
-        raise
-
-
-def copy_to_hdfs(directory):
+def copy_to_hdfs(archive):
+    pxz_command = ['pxz', '--decompress', '--stdout', archive, '-T4']
+    pxz = subprocess.Popen(pxz_command, stdout=subprocess.PIPE)
     hdfs_client = hdfs.Client(config.HDFS_NAMENODE_URI)
     destination_path = os.path.join('/', 'data', 'listenbrainz')
-    print('Creating remote directory...')
     if FORCE:
+        print('Removing data directory if present...')
         hdfs_client.delete(destination_path, recursive=True)
+        print('Done!')
+
+    print('Creating remote directory...')
     hdfs_client.makedirs(destination_path)
     print('Done!')
 
     print('Uploading...')
-    for root, dirs, files in os.walk(directory):
-        for directory in dirs:
-            if 'listenbrainz-listens-dump' in directory:
-                hdfs_client.upload(destination_path, os.path.join(root, directory))
-                print('Upload done!')
-                return
+    with tarfile.open(fileobj=pxz.stdout, mode='r|') as tar:
+        for member in tar:
+            if member.isfile():
+                tar.extract(member)
+                print("Uploading %s to %s..." % (os.path.join(destination_path, member.name), member.name))
+                hdfs_client.upload(os.path.join(destination_path, member.name), member.name)
+                os.remove(member.name)
+    print("Done!")
 
 
 def main(archive):
-    print('Extracting the archive...')
-    extracted_dir = extract(archive)
-    print('Done! Extracted to %s' % extracted_dir)
-
     print('Copying extracted dump to HDFS...')
-    copy_to_hdfs(extracted_dir)
+    copy_to_hdfs(archive)
     print('Done!')
