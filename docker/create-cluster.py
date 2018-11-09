95ce24995f66611cf0c5b310fb0b6d059d07c9c0
Robert Kaye <rob@musicbrainz.org>
2018-11-09 13:22:10 +0100

    Improve the create cluster script

--- docker/create-cluster.py
+++ docker/create-cluster.py
@@ -1,6 +1,9 @@
 #!/usr/bin/env python3
 
-from hetznercloud import HetznerCloudClientConfiguration, HetznerCloudClient, SERVER_TYPE_8CPU_32GB, IMAGE_UBUNTU_1604, SERVER_STATUS_RUNNING
+import click
+
+
+from hetznercloud import HetznerCloudClientConfiguration, HetznerCloudClient, SERVER_TYPE_8CPU_32GB, IMAGE_UBUNTU_1804, SERVER_STATUS_RUNNING
 from config import API_KEY
 
 START_SCRIPT = """#!/bin/bash
@@ -8,23 +11,31 @@ curl -fsSL https://github.com/metabrainz/listenbrainz-recommendation-playground/
 bash /root/setup-node.sh %s %s > /root/setup.log
 """ % ("195.201.112.36", "SWMTKN-1-5r1rg5ncj6b5vei573gs6s6xhaqdxqywn6muzio0io6t4g4dyt-5blarcxq9d18yp5px58yfhwb7")
 
-configuration = HetznerCloudClientConfiguration().with_api_key(API_KEY).with_api_version(1)
-client = HetznerCloudClient(configuration)
+@click.command()
+@click.option('--count', default=4, help='Number of workers to start.')
+def start_cluster(count):
 
-NUM_SERVERS_TO_START = 2
-servers = []
-for i in range(NUM_SERVERS_TO_START):
-    server_a, create_action = client.servers().create(name="slave%03d" % i,
-            server_type=SERVER_TYPE_8CPU_32GB,
-            image=IMAGE_UBUNTU_1604, 
-            start_after_create=True,
-            ssh_keys=["robert", "zas"],
-            user_data=START_SCRIPT)
-    servers.append((server_a, create_action))
+    print("Connect to hetzner API...")
+    configuration = HetznerCloudClientConfiguration().with_api_key(API_KEY).with_api_version(1)
+    client = HetznerCloudClient(configuration)
 
-print("Servers created, waiting to start....")
-for server, action in servers:
-    server.wait_until_status_is(SERVER_STATUS_RUNNING) 
-    print("   %s" % server.public_net_ipv4)
+    print("Create servers...")
+    servers = []
+    for i in range(count):
+        server_a, create_action = client.servers().create(name="worker%03d" % i,
+                server_type=SERVER_TYPE_8CPU_32GB,
+                image=IMAGE_UBUNTU_1804, 
+                start_after_create=True,
+                ssh_keys=["robert", "zas"],
+                user_data=START_SCRIPT)
+        servers.append((server_a, create_action))
 
-print("Done.")
+    print("Servers created, waiting to start....")
+    for server, action in servers:
+        server.wait_until_status_is(SERVER_STATUS_RUNNING) 
+        print("   %s" % server.public_net_ipv4)
+
+    print("Done.")
+
+if __name__ == '__main__':
+    start_cluster()
