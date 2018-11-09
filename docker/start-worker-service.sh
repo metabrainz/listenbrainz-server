afb053fe3552ff831035f4d75e26c7630e47b996
Robert Kaye <rob@musicbrainz.org>
2018-11-09 13:55:34 +0100

    Tag nodes with worker label, more cleanup and consistency improvements.

--- docker/start-worker-service.sh
+++ docker/start-worker-service.sh
@@ -11,6 +11,7 @@ docker service create --replicas $REPLICAS \
     --env NODE_TYPE="worker" \
     --network spark-network \
     --name yarn-workers \
+    --mount type=volume,source=hdfs-volume,destination=/home/hadoop/hdfs \
     metabrainz/hadoop-yarn:beta
 
 docker service create \
@@ -18,4 +19,5 @@ docker service create \
     --name spark-workers \
     --network spark-network \
     --env SPARK_NO_DAEMONIZE=1 \
+    --mount type=volume,source=spark-volume,destination=/home/hadoop/spark \
     metabrainz/spark-worker
