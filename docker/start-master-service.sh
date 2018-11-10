cf4011885c81bc876de798323d1f926305767112
Robert Kaye <rob@musicbrainz.org>
2018-11-10 21:36:21 +0100

    Do not publish any ports anylonger. We'll cross that bridge when we get to it.

--- docker/start-master-service.sh
+++ docker/start-master-service.sh
@@ -8,10 +8,6 @@ docker service create --replicas 1 \
     --env NODE_TYPE="master" \
     --network spark-network \
     --constraint 'node.labels.type == master' \
-    -p published=8031,target=8031,mode=host \
-    -p published=8088,target=8088,mode=host \
-    -p published=9000,target=9000,mode=host \
-    -p published=50070,target=50070,mode=host \
     --mount type=volume,source=hdfs-volume,destination=/home/hadoop/hdfs \
     metabrainz/hadoop-yarn:beta
 
@@ -20,9 +16,6 @@ docker service create --replicas 1 \
     --hostname spark-master \
     --network spark-network \
     --constraint 'node.labels.type == master' \
-    -p published=7077,target=7077,mode=host \
-    -p published=6066,target=6066,mode=host \
-    -p published=8080,target=8080,mode=host \
     --env SPARK_NO_DAEMONIZE=1 \
     --mount type=volume,source=spark-volume,destination=/home/hadoop/spark \
     metabrainz/spark-master
