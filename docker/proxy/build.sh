738f0510358dbf05afcd2a3271ba0139ad3e8605
Robert Kaye <rob@musicbrainz.org>
2018-12-18 15:30:24 +0100

    Add listenbrainz tunnel proxy

---
+++ docker/proxy/build.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+#
+# Build image from the currently checked out version of the MetaBrainz website
+# and push it to the Docker Hub, with an optional tag (by default "latest").
+#
+# Usage:
+#   $ ./push.sh [tag]
+
+TAG_PART=${1:-latest}
+docker build -t metabrainz/listenbrainz-gateway:$TAG_PART .
+#docker push metabrainz/listenbrainz-gateway:$TAG_PART
