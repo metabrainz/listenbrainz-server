7ea5eccde77845011d0d89dfa7c507af872c0ac6
Param Singh <param@metabrainz.org>
2018-12-21 15:58:22 +0530

    Refactor tests for RedisListenStore
    Make them more readable and better designed.

--- listenbrainz/listenstore/tests/test_redislistenstore.py
+++ listenbrainz/listenstore/tests/test_redislistenstore.py
@@ -1,38 +1,50 @@
 # coding=utf-8
 
 import logging
+import time
 import ujson
 
 from redis.connection import Connection
 
 import listenbrainz.db.user as db_user
 from listenbrainz.db.testing import DatabaseTestCase
+from listenbrainz.listen import Listen
 from listenbrainz.listenstore.tests.util import generate_data
 from listenbrainz.webserver.redis_connection import init_redis_connection
 
 
-class TestRedisListenStore(DatabaseTestCase):
+class RedisListenStoreTestCase(DatabaseTestCase):
 
     def setUp(self):
-        super(TestRedisListenStore, self).setUp()
-        self.log = logging.getLogger(__name__)
+        super(RedisListenStoreTestCase, self).setUp()
+        self.log = logging.getLogger()
         self._redis = init_redis_connection(self.log, self.config.REDIS_HOST, self.config.REDIS_PORT)
-        self.testuser_id = db_user.create(1, "test")
-        self._create_test_data()
+        self.testuser = db_user.get_or_create(1, "test")
 
     def tearDown(self):
         self._redis.redis.flushdb()
         Connection(self._redis.redis).disconnect()
-        super(TestRedisListenStore, self).tearDown()
-
-    def _create_test_data(self):
-        self.log.info("Inserting test data...")
-        self.listen = generate_data(self.testuser_id,'test', None , 1)[0]
-        listen = self.listen.to_json()
-        self._redis.redis.setex('playing_now' + ':' + str(listen['user_id']),
-                                ujson.dumps(listen).encode('utf-8'), self.config.PLAYING_NOW_MAX_DURATION)
-        self.log.info("Test data inserted")
+        super(RedisListenStoreTestCase, self).tearDown()
 
     def test_get_playing_now(self):
         playing_now = self._redis.get_playing_now(self.listen.user_id)
         assert playing_now is not None
+
+    def test_get_and_put_playing_now(self):
+        listen = {
+            'user_id': self.testuser['id'],
+            'user_name': self.testuser['musicbrainz_id'],
+            'listened_at': int(time.time()),
+            'track_metadata': {
+                'artist_name': 'The Strokes',
+                'track_name': 'Call It Fate, Call It Karma',
+                'additional_info': {},
+            },
+        }
+        self._redis.put_playing_now(listen['user_id'], listen, self.config.PLAYING_NOW_MAX_DURATION)
+
+        playing_now = self._redis.get_playing_now(listen['user_id'])
+        self.assertIsNotNone(playing_now)
+        self.assertIsInstance(playing_now, Listen)
+        self.assertEqual(playing_now.data['artist_name'], 'The Strokes')
+        self.assertEqual(playing_now.data['track_name'], 'Call It Fate, Call It Karma')
