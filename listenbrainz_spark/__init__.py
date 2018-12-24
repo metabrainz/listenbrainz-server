288967efbcbc096f398b202fbab37a7cac08646f
Param Singh <param@metabrainz.org>
2018-12-24 12:46:56 +0530

    Configure spark to use more memory

--- listenbrainz_spark/__init__.py
+++ listenbrainz_spark/__init__.py
@@ -5,6 +5,8 @@ spark = SparkSession \
         .builder \
         .appName("LB Spark Cluster") \
         .config("spark.hadoop.dfs.client.use.datanode.hostname", "true") \
+        .config("spark.driver.memory", "20g") \
+        .config("spark.executor.memory", "20g") \
         .getOrCreate()
 
 sc = spark.sparkContext
