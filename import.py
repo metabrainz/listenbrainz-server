d0085f71145b4edc810748f72b1ac85ac777773d
Param Singh <param@metabrainz.org>
2019-01-15 16:15:07 +0530

    Pass app name into spark session explicitly

--- import.py
+++ import.py
@@ -2,4 +2,8 @@ import sys
 from listenbrainz_spark.data import dump
 
 if __name__ == '__main__':
-    dump.main(sys.argv[1])
+    if len(sys.argv) != 3:
+        print("Usage: import.py <app_name> <dump_file>")
+        sys.exit(-1)
+
+    dump.main(app_name=sys.argv[1], archive=sys.argv[2])
