cdfffc8ee334784709490ee8f59e07db1211f75d
Param Singh <param@metabrainz.org>
2019-01-15 13:15:11 +0530

    Add script to read a written dataframe for testing

--- listenbrainz_spark/__init__.py
+++ listenbrainz_spark/__init__.py
@@ -1,12 +1,14 @@
 from pyspark import SparkContext
-from pyspark.sql import SparkSession
+from pyspark.sql import SparkSession, SQLContext
 
 spark = SparkSession \
         .builder \
         .appName("LB Spark Cluster") \
         .config("spark.hadoop.dfs.client.use.datanode.hostname", "true") \
+        .config("spark.hadoop.dfs.datanode.use.datanode.hostname", "true") \
         .config("spark.driver.memory", "20g") \
         .config("spark.executor.memory", "20g") \
         .getOrCreate()
 
 sc = spark.sparkContext
+sqlContext = SQLContext(sc)
