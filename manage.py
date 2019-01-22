0e43da998af707b1d2ce70d5686d51cf3e2b12f3
Param Singh <param@metabrainz.org>
2019-01-22 22:24:49 +0530

    Change default port to 8081

--- manage.py
+++ manage.py
@@ -57,7 +57,7 @@ def run_api_compat_server(host, port, debug=False):
 
 @cli.command(name="run_follow_server")
 @click.option("--host", "-h", default="0.0.0.0", show_default=True)
-@click.option("--port", "-p", default=8080, show_default=True)
+@click.option("--port", "-p", default=8081, show_default=True)
 @click.option("--debug", "-d", is_flag=True,
               help="Turns debugging mode on or off. If specified, overrides "
                    "'DEBUG' value in the config file.")
