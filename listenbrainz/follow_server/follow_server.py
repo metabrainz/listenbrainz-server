e9d231f373c7c46039eef4bb4efe70a772f0263f
Robert Kaye <rob@musicbrainz.org>
2019-01-22 16:43:36 +0100

    A web socket server starts and gets exposed to port 8081 on localhost.

---
+++ listenbrainz/follow_server/follow_server.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python
+from flask import Flask
+from flask_socketio import SocketIO
+import argparse
+
+from listenbrainz.webserver import load_config
+from brainzutils.flask import CustomFlask
+
+app = CustomFlask(
+    import_name=__name__,
+    use_flask_uuid=True,
+)
+load_config(app)
+
+# Logging
+app.init_loggers(
+    file_config=app.config.get('LOG_FILE'),
+    email_config=app.config.get('LOG_EMAIL'),
+    sentry_config=app.config.get('LOG_SENTRY')
+)
+
+socketio = SocketIO(app)
+
+@socketio.on('json')
+def handle_json(json):
+    print('received json: ' + str(json))
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="ListenBrainz Follow Server")
+    parser.add_argument("-d", "--debug", action="store_true",
+                        help="Turn on debugging mode to see stack traces on "
+                             "the error pages. This overrides 'DEBUG' value "
+                             "in config file.")
+    parser.add_argument("-t", "--host", default="0.0.0.0", type=str,
+                        help="Which interfaces to listen on. Default: 0.0.0.0.")
+    parser.add_argument("-p", "--port", default="8081", type=int,
+                        help="Which port to listen on. Default: 8081.")
+    args = parser.parse_args()
+    socketio.run(app, debug=True if args.debug else None,
+                    host=args.host, port=args.port)
