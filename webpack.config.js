062f19586555519e507ef2fb2bb7351a19466e41
Monkey Do <contact@monkeydo.digital>
2019-02-05 20:12:30 +0100

    Clean up webpack dist folder before build

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,12 @@
 const path = require('path');
 const ManifestPlugin = require('webpack-manifest-plugin');
+const CleanWebpackPlugin = require('clean-webpack-plugin');
 
 module.exports = function(env){
   const isProd = env === "production";
   const plugins = [
-      new ManifestPlugin()
+    new CleanWebpackPlugin(path.resolve(__dirname, 'listenbrainz/webserver/static/js/dist')),
+    new ManifestPlugin()
   ];
   return {
     mode: isProd ? "production" : "development",
