11b6400ccd2c0b3aec57017fe47fd722338ea04d
Robert Kaye <rob@musicbrainz.org>
2019-02-15 12:12:10 +0100

    Add tests for redis_listenstore

--- listenbrainz/listenstore/tests/test_redislistenstore.py
+++ listenbrainz/listenstore/tests/test_redislistenstore.py
@@ -1,5 +1,6 @@
 # coding=utf-8
 
+import datetime
 import logging
 import time
 import ujson
@@ -44,3 +45,44 @@ class RedisListenStoreTestCase(DatabaseTestCase):
         self.assertIsInstance(playing_now, Listen)
         self.assertEqual(playing_now.data['artist_name'], 'The Strokes')
         self.assertEqual(playing_now.data['track_name'], 'Call It Fate, Call It Karma')
+
+
+    def test_update_and_get_recent_listens(self):
+
+        dt0 = datetime.datetime.now()
+        dt1 = dt0.replace(second=dt0.second + 1)
+
+        recent = self._redis.get_recent_listens()
+        self.assertEqual(recent, [])
+
+        listens = []
+        listens.append({
+            'user_id': self.testuser['id'],
+            'user_name': self.testuser['musicbrainz_id'],
+            'listened_at': dt0,
+            'track_metadata': {
+                'artist_name': 'The Dimwitted Hillbillies',
+                'track_name': 'Ice cream, guns and bling!',
+                'additional_info': {},
+            },
+        })
+        listens.append({
+            'user_id': self.testuser['id'],
+            'user_name': self.testuser['musicbrainz_id'],
+            'listened_at': dt1,
+            'track_metadata': {
+                'artist_name': 'The Dimwitted Hillbillies',
+                'track_name': 'White gas and sparklers are a great combo for toddlers!',
+                'additional_info': {},
+            },
+        })
+        self._redis.update_recent_listens(listens)
+
+        recent = self._redis.get_recent_listens()
+        self.assertEqual(len(recent), 2)
+        self.assertIsInstance(recent[0], Listen)
+        self.assertEqual(recent[0].timestamp, dt1)
+        self.assertEqual(recent[1].timestamp, dt0)
+
+        recent = self._redis.get_recent_listens(1)
+        self.assertEqual(len(recent), 1)
