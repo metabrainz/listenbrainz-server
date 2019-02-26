f09c1b9f49fdcf941e3502ae686779d1804da0f2
Param Singh <param@metabrainz.org>
2019-02-26 20:03:51 +0530

    Create primary keys, indexes, fks and add schema change script

--- admin/sql/drop_tables.sql
+++ admin/sql/drop_tables.sql
@@ -3,5 +3,7 @@ BEGIN;
 DROP TABLE IF EXISTS "user"               CASCADE;
 DROP TABLE IF EXISTS data_dump            CASCADE;
 DROP TABLE IF EXISTS spotify_auth         CASCADE;
+DROP TABLE IF EXISTS follow_list          CASCADE;
+DROP TABLE IF EXISTS follow_list_member   CASCADE;
 
 COMMIT;
---
+++ admin/sql/updates/2019-02-26-add-follow-list.sql
@@ -0,0 +1,40 @@
+BEGIN;
+
+CREATE TABLE follow_list (
+  id            SERIAL, -- PK
+  name          TEXT NOT NULL,
+  creator       INTEGER NOT NULL, -- FK to "user".id
+  private       BOOLEAN NOT NULL DEFAULT FALSE
+  created       TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT NOW()
+);
+
+CREATE TABLE follow_list_member (
+  list_id      INTEGER NOT NULL,
+  user_id      INTEGER NOT NULL,
+  added        TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT NOW()
+);
+
+ALTER TABLE follow_list ADD CONSTRAINT follow_list_pkey PRIMARY KEY (id);
+ALTER TABLE follow_list_member ADD CONSTRAINT follow_list_member_pkey PRIMARY KEY (list_id, user_id);
+
+ALTER TABLE follow_list
+    ADD CONSTRAINT follow_list_user_id_foreign_key
+    FOREIGN KEY (user_id)
+    REFERENCES "user" (id)
+    ON DELETE CASCADE;
+
+ALTER TABLE follow_list_member
+    ADD CONSTRAINT follow_list_member_list_id_foreign_key
+    FOREIGN KEY (list_id)
+    REFERENCES follow_list (id)
+    ON DELETE CASCADE;
+
+ALTER TABLE follow_list_member
+    ADD CONSTRAINT follow_list_member_user_id_foreign_key
+    FOREIGN KEY (user_id)
+    REFERENCES "user" (id)
+    ON DELETE CASCADE;
+
+CREATE INDEX creator_ndx_follow_list ON follow_list (creator);
+
+COMMIT;
