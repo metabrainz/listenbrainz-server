7bca18be6e7d0d91f979d5af39e3ae070f3dbca4
Param Singh <param@metabrainz.org>
2019-02-26 20:03:51 +0530

    Create primary keys, indexes, fks and add schema change script

--- admin/sql/create_foreign_keys.sql
+++ admin/sql/create_foreign_keys.sql
@@ -24,4 +24,22 @@ ALTER TABLE spotify_auth
     REFERENCES "user" (id)
     ON DELETE CASCADE;
 
+ALTER TABLE follow_list
+    ADD CONSTRAINT follow_list_user_id_foreign_key
+    FOREIGN KEY (user_id)
+    REFERENCES "user" (id)
+    ON DELETE CASCADE;
+
+ALTER TABLE follow_list_member
+    ADD CONSTRAINT follow_list_member_list_id_foreign_key
+    FOREIGN KEY (list_id)
+    REFERENCES follow_list (id)
+    ON DELETE CASCADE;
+
+ALTER TABLE follow_list_member
+    ADD CONSTRAINT follow_list_member_user_id_foreign_key
+    FOREIGN KEY (user_id)
+    REFERENCES "user" (id)
+    ON DELETE CASCADE;
+
 COMMIT;
