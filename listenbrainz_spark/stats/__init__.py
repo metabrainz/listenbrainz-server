1c3cd969482b0e0d1de45151869ee5016af86b82
vansika <vansikapareek2001@gmail.com>
2019-03-19 13:02:37 +0530

    included try and exception blocks

--- listenbrainz_spark/stats/__init__.py
+++ listenbrainz_spark/stats/__init__.py
@@ -1,4 +1,6 @@
 import listenbrainz_spark
+from datetime import datetime
+from dateutil.relativedelta import relativedelta
 
 def run_query(query):
     """ Returns dataframe that results from running the query.
@@ -6,3 +8,8 @@ def run_query(query):
     Note: It is the responsibility of the caller to register tables etc.
     """
     return listenbrainz_spark.sql_context.sql(query)
+
+def date_for_stats_calculation():
+    t = datetime.utcnow().replace(day=1)
+    date = t + relativedelta(months=-1)
+    return date
