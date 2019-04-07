7bcac1d59d73d131755e21211ec7a92849ee6d36
Param Singh <param@metabrainz.org>
2019-04-07 17:50:30 +0200

    Count total number of listens in files

--- listenbrainz_spark/mlhd/setup/create_dataframe.py
+++ listenbrainz_spark/mlhd/setup/create_dataframe.py
@@ -10,15 +10,18 @@ from listenbrainz_spark.mlhd.schema import tsv_schema
 def load_files():
     df = None
     root_path = '/data/mlhd'
+    file_no = 1
+    count = 0
     for path, dirs, files in hdfs_connection.client.walk(root_path):
         print('Here: ', path)
         listen_files = [filename for filename in files if filename.endswith('.txt')]
         for listen_file in listen_files:
             file_path = config.HDFS_CLUSTER_URI + os.path.join(path, listen_file)
-            print('listen file: %s' % file_path)
             file_df = listenbrainz_spark.session.read.csv(file_path, sep='\t', schema=tsv_schema, timestampFormat='s')
+            count += file_df.count()
             df = df.union(file_df) if df else file_df
-    print(df.count())
+            print(file_no, '\t', count)
+            file_no += 1
     return df
 
 
