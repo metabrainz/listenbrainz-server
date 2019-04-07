a3edf4edcb9eedb55df71a61789e2e20b703c806
Param Singh <param@metabrainz.org>
2019-04-07 03:47:56 +0200

    Fix bugs in create mlhd dataframe code

--- listenbrainz_spark/mlhd/setup/create_dataframe.py
+++ listenbrainz_spark/mlhd/setup/create_dataframe.py
@@ -1,6 +1,9 @@
 import listenbrainz_spark
-from listenbrainz_spark import hdfs_connection
+import os
+
+
 from listenbrainz_spark import config
+from listenbrainz_spark import hdfs_connection
 from listenbrainz_spark.mlhd.schema import tsv_schema
 
 
@@ -11,17 +14,16 @@ def load_files():
         print('Here: ', path)
         listen_files = [filename for filename in files if filename.endswith('.txt')]
         for listen_file in listen_files:
-            print('listen file: %s' % listen_file)
-            file_path = config.HDFS_CLUSTER_URI + os.path.join(root_path, path, listen_file)
-            file_df = listenbrainz_spark.read.csv(file_path, sep='\t', schema=tsv_schema)
+            file_path = config.HDFS_CLUSTER_URI + os.path.join(path, listen_file)
+            print('listen file: %s' % file_path)
+            file_df = listenbrainz_spark.session.read.csv(file_path, sep='\t', schema=tsv_schema, timestampFormat='s')
             df = df.union(file_df) if df else file_df
     print(df.count())
-    df.show()
     return df
 
 
-def main(app_name):
-    listenbrainz_spark.init_spark_session(app_name)
+def main():
+    listenbrainz_spark.init_spark_session('mlhd_create_dataframe')
     hdfs_connection.init_hdfs(config.HDFS_HTTP_URI)
     dataframe = load_files()
     dataframe.write.format('parquet').save(config.HDFS_CLUSTER_URI + '/data/mlhd/entire_mlhd.parquet')
