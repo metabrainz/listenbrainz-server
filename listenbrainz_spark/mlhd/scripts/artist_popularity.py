cff5e31ebcd099bc9ea6ad87d698fb01b64e1c01
Param Singh <param@metabrainz.org>
2019-04-07 21:41:19 +0530

    Commit forgotten files

---
+++ listenbrainz_spark/mlhd/scripts/artist_popularity.py
@@ -0,0 +1,25 @@
+import listenbrainz_spark
+
+from listenbrainz_spark import config
+
+
+def main():
+    listenbrainz_spark.init_spark_session('artist_popularity')
+    mlhd_df_path = config.HDFS_CLUSTER_URI + os.path.join('/data', 'mlhd', 'entire_mlhd.parquet')
+    print('Loading MLHD Dataframe...')
+    mlhd_df = listenbrainz_spark.sql_context.read.format('parquet').load(mlhd_df_path)
+    print("Loaded!")
+    mlhd_df.registerTempTable('mlhd')
+    print("Running SQL...")
+    artist_popularity_df = listenbrainz_spark.sql_context.sql("""
+            SELECT artist_mbid, COUNT(artist_mbid) as cnt
+              FROM mlhd
+          GROUP BY artist_mbid
+          ORDER BY cnt DESC
+    """)
+    print("number of rows: ", artist_popularity_df.count())
+    artist_popularity_df.show()
+    print("Saving...")
+    csv_path = config.HDFS_CLUSTER_URI + os.path.join('/data', 'mlhd', 'artist_popularity.csv')
+    artist_popularity_df.write.csv(csv_path)
+    print("Saved to %s!" % csv_path)
