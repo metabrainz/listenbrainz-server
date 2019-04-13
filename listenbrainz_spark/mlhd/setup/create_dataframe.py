dcfee88fe12c02280366e334dbae182caed8e58e
Param Singh <param@metabrainz.org>
2019-04-13 13:08:22 +0200

    Load entire directories at once

--- listenbrainz_spark/mlhd/setup/create_dataframe.py
+++ listenbrainz_spark/mlhd/setup/create_dataframe.py
@@ -7,26 +7,23 @@ from listenbrainz_spark import hdfs_connection
 from listenbrainz_spark.mlhd.schema import tsv_schema
 
 
-def load_files():
+def load_files(root_path):
     df = None
-    root_path = '/data/mlhd'
-    file_no = 1
-    count = 0
-    for path, dirs, files in hdfs_connection.client.walk(root_path):
-        print('Here: ', path)
-        listen_files = [filename for filename in files if filename.endswith('.txt')]
-        for listen_file in listen_files:
-            file_path = config.HDFS_CLUSTER_URI + os.path.join(path, listen_file)
-            file_df = listenbrainz_spark.session.read.csv(file_path, sep='\t', schema=tsv_schema, timestampFormat='s')
-            count += file_df.count()
-            df = df.union(file_df) if df else file_df
-            print(file_no, '\t', count)
-            file_no += 1
+    for path in hdfs_connection.client.list(root_path):
+        files_path = config.HDFS_CLUSTER_URI + os.path.join(root_path, path, '*.txt')
+        print('New files: ', files_path)
+        files_df = listenbrainz_spark.session.read.csv(files_path, sep='\t', schema=tsv_schema, timestampFormat='s')
+        files_df.show()
+        df = df.union(files_df) if df else files_df
     return df
 
 
 def main():
     listenbrainz_spark.init_spark_session('mlhd_create_dataframe')
     hdfs_connection.init_hdfs(config.HDFS_HTTP_URI)
-    dataframe = load_files()
+    print("Creating dataframe...")
+    dataframe = load_files('/data/mlhd')
+    print("Dataframe created!")
+    print("Saving dataframe...")
     dataframe.write.format('parquet').save(config.HDFS_CLUSTER_URI + '/data/mlhd/entire_mlhd.parquet')
+    print("Saved!")
