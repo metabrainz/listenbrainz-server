1432c902ea101b0d0c592f69ef65de1b0764b6b4
vansika <vansikapareek2001@gmail.com>
2019-05-18 13:18:36 +0530

    HTML files corresponding to recommendation generation

--- listenbrainz_spark/recommendations/train_models.py
+++ listenbrainz_spark/recommendations/train_models.py
@@ -5,6 +5,7 @@ import json
 import time
 import listenbrainz_spark
 import logging
+import uuid
 
 from collections import namedtuple
 from math import sqrt
@@ -16,7 +17,7 @@ from listenbrainz_spark import config
 from listenbrainz_spark.recommendations import utils
 from time import sleep
 
-Model = namedtuple('Model', 'model error rank lmbda iteration')
+Model = namedtuple('Model', 'model error rank lmbda iteration model_id time')
 
 def parse_dataset(row):
     return Rating(row['user_id'], row['recording_id'], row['count'])
@@ -25,7 +26,6 @@ def compute_rmse(model, data, n):
     """ Compute RMSE (Root Mean Squared Error).
     """
     predictions = model.predictAll(data.map(lambda x: (x.user, x.product)))
-    predictions.collect()
     predictionsAndRatings = predictions.map(lambda x: ((x[0], x[1]), x[2])) \
       .join(data.map(lambda x: ((x[0], x[1]), x[2]))) \
       .values()
@@ -38,52 +38,67 @@ def preprocess_data(playcounts_df):
 
 def train(training_data, validation_data, num_validation, ranks, lambdas, iterations):
     best_model = None
-    model_info = {}
-    training_metadata = []
+    best_model_metadata = {}
+    model_metadata = []
     alpha = 3.0 
-    t0 = time.time()
     for rank, lmbda, iteration in itertools.product(ranks, lambdas, iterations):
+        t0 = time.time()
         model = ALS.trainImplicit(training_data, rank, iterations=iteration, lambda_=lmbda, alpha=alpha)
+        mt = "%.2f" % (time.time() - t0)
+        model_id = 'listenbrainz-recommendation-model-{}'.format(uuid.uuid4())
+        t0  =time.time()
         validation_rmse = compute_rmse(model, validation_data, num_validation)
-        training_metadata.append((rank, "%.1f" % (lmbda), iteration, "%.2f" % (validation_rmse)))
+        vt = "%.2f" % (time.time() - t0)
+        model_metadata.append((model_id, mt, rank, "%.1f" % (lmbda), iteration, "%.2f" % (validation_rmse), vt))
         if best_model is None or validation_rmse < best_model.error:
-            best_model = Model(model=model, error=validation_rmse, rank=rank, lmbda=lmbda, iteration=iteration)
-    model_info["training_metadata"] = training_metadata
-    model_info["best_model"] = {'error': "%.2f" % (best_model.error), 'rank': best_model.rank, 'lmbda': best_model.lmbda, 'iteration': best_model.iteration}
-    t = "%.2f" % (time.time() - t0)
-    return best_model, model_info, t
+            best_model = Model(model=model, error=validation_rmse, rank=rank, lmbda=lmbda, iteration=iteration, model_id=model_id, time=mt)
+    best_model_metadata = {'error': "%.2f" % (best_model.error), 'rank': best_model.rank, 'lmbda': best_model.lmbda, 
+                            'iteration': best_model.iteration, 'model_id': best_model.model_id, 'time' : best_model.time}
+    return best_model, model_metadata, best_model_metadata
 
-def main(df, lb_dump_time_window):
+def main(df):
+    time_info = {}
     t0 = time.time()
     training_data, validation_data, test_data = preprocess_data(df)
     t = "%.2f" % (time.time() - t0)
-    print("Dataframe split in: %ss" % (t))
+    time_info['preprocessing'] = t
     num_training = training_data.count()
     num_validation = validation_data.count()
     num_test = test_data.count()
     print("Training model...")
-    model, model_info, t = train(training_data, validation_data, num_validation, [8, 12], [0.1, 10.0], [10, 20])
+    t0 = time.time()
+    model, model_metadata, best_model_metadata = train(training_data, validation_data, num_validation, [8, 12], [0.1, 10.0], [10, 20])
+    t = "%.2f" % (time.time() - t0)
+    time_info['models'] = t
 
     print("Saving model...")
     date = datetime.utcnow().strftime("%Y-%m-%d")
     for attempt in range(config.MAX_RETRIES):
         try:
-            path = os.path.join('/', 'data', 'listenbrainz','listenbrainz-recommendation-mode-{}'.format(date))
+            t0 = time.time()
+            path = os.path.join('/', 'data', 'listenbrainz', '{}'.format(best_model_metadata['model_id']))
             model.model.save(listenbrainz_spark.context, config.HDFS_CLUSTER_URI + path)
+            t = "%.2f" % (time.time() - t0)
+            time_info['save_model'] = t
             break
         except Exception as err:
             sleep(config.TIME_BEFORE_RETRIES)
             if attempt == config.MAX_RETRIES - 1:
                 raise SystemExit("%s.Aborting..." % (str(err)))
-            logging.error("Unable to save model: %s.Retrying in %ss" % (type(err).__name__, config.TIME_BEFORE_RETRIES))
+            logging.error("Unable to save model: %s.Retrying in %ss" % (str(err), config.TIME_BEFORE_RETRIES))
+    
     outputfile = 'Model-Info-%s.html' % (date)
     context = {
-        'total_listens' : num_training+num_validation+num_test,
-        'model' : model_info,
-        'time' : t,
-        'lb_dump_time_window' : lb_dump_time_window,
+        'time' : time_info,
+        'num_training' : num_training,
+        'num_validation' : num_validation,
+        'num_test' : num_test,
+        'models' : model_metadata,
+        'best_model' : best_model_metadata,
+        'link' : 'Recommendations-%s.html' % (date)
     }
     utils.save_html(outputfile, context, 'model.html')
+    return best_model_metadata['model_id']
 
 
 
