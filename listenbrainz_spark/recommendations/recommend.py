fa13404d96fee83f361cc51341cc07adc09a763c
vansika <vansikapareek2001@gmail.com>
2019-05-19 23:38:50 +0530

    switched from RDD lookup to dataframe lookup

--- listenbrainz_spark/recommendations/recommend.py
+++ listenbrainz_spark/recommendations/recommend.py
@@ -24,10 +24,14 @@ def get_user_id(user_name):
     """ % user_name)
     return result.first()['user_id']
 
-def recommend_user(user_name, model, recordings_map, all_recordings):
+def recommend_user(user_name, model, all_recordings, recordings_df):
     user_info = {}
     t0 = time.time()
     user_id = get_user_id(user_name)
+    t = "%.2f" % (time.time() - t0)
+    user_info['get_user_id'] = t
+
+    t0 = time.time()
     user_playcounts = run_query("""
         SELECT user_id,
                recording_id,
@@ -40,25 +44,35 @@ def recommend_user(user_name, model, recordings_map, all_recordings):
 
     t0 = time.time()
     user_recordings = user_playcounts.rdd.map(lambda r: r['recording_id'])
-    user_recordings.count()
+    user_recordings_count = user_recordings.count()
     t = "%.2f" % (time.time() - t0)
     user_info['user-recordings-time'] = t
+    user_info['user-recordings-count'] = user_recordings_count
 
     t0 = time.time()
     candidate_recordings = all_recordings.subtract(user_recordings)
-    candidate_recordings.count()
+    candidate_recordings_count = candidate_recordings.count()
     t = "%.2f" % (time.time() - t0)
     user_info['candidate-recordings-time'] = t
+    user_info['candidate-recordings-count'] = candidate_recordings_count
 
     t0 = time.time()
     recommendations = model.predictAll(candidate_recordings.map(lambda recording: (user_id, recording))).takeOrdered(50, lambda product: -product.rating)
+    recommended_recording_ids = [(recommendations[i].product) for i in range(len(recommendations))]
     t = "%.2f" % (time.time() - t0)
     user_info['recommendations-time'] = t
 
     t0 = time.time()
-    recommended_recordings = [recordings_map.lookup(recommendations[i].product)[0] for i in range(len(recommendations))]
+    recommendations_df = recordings_df[recordings_df.recording_id.isin(recommended_recording_ids)].collect()
     t = "%.2f" % (time.time() - t0)
     user_info['lookup-time'] = t
+
+    recommended_recordings = []
+
+    for row in recommendations_df:
+        rec = (row.track_name, row.recording_msid, row.artist_name, row.artist_msid, row.release_name, row.release_msid)
+        recommended_recordings.append(rec)
+
     user_info['recordings'] = recommended_recordings
     return user_info
 
@@ -66,18 +80,13 @@ def main(users_df, playcounts_df, recordings_df, ti, bestmodel_id):
     time_info = {}
     users_df.createOrReplaceTempView('user')
     playcounts_df.createOrReplaceTempView('playcount')
-    t0 = time.time()
-    recordings_map = recordings_df.rdd.map(lambda r: (r['recording_id'], [r['track_name'], r['recording_msid'], 
-                    r['artist_name'], r['artist_msid'], r['release_name'], r["release_msid"]]))
-    recordings_map.count()
-    t = "%.2f" % (time.time() - t0)
-    time_info['recordings_map'] = t
 
     t0 = time.time()
-    all_recordings = recordings_map.keys()
-    all_recordings.count()
+    all_recordings = recordings_df.rdd.map(lambda r: r['recording_id'])
+    all_recordings_count = all_recordings.count()
     t = "%.2f" % (time.time() - t0)
     time_info['all_recordings'] = t
+
     date = datetime.utcnow().strftime("%Y-%m-%d")
     path = os.path.join('/', 'data', 'listenbrainz', '{}'.format(bestmodel_id))
     
@@ -101,7 +110,7 @@ def main(users_df, playcounts_df, recordings_df, ti, bestmodel_id):
         recommendations = {}
         for user_name in users['user_name']:
             try:
-                user_info = recommend_user(user_name, model, recordings_map, all_recordings)
+                user_info = recommend_user(user_name, model, all_recordings, recordings_df)
                 print("Recommendations for %s generated" % (user_name))
                 recommendations[user_name] = user_info
             except TypeError as err:
@@ -109,7 +118,7 @@ def main(users_df, playcounts_df, recordings_df, ti, bestmodel_id):
             except Exception as err:
                 logging.error("Recommendations for \"%s\" not generated.%s" % (user_name, str(err)))
 
-    column = ['Track Name', 'Recording MSID', 'Artist Name', 'Artist MSID', 'Release Name', 'Release MSID']
+    column = ('Track Name', 'Recording MSID', 'Artist Name', 'Artist MSID', 'Release Name', 'Release MSID')
     outputfile = 'Recommendations-%s.html' % (date)
     context = {
         'recommendations' : recommendations,
@@ -117,8 +126,7 @@ def main(users_df, playcounts_df, recordings_df, ti, bestmodel_id):
         'total_time' : int(time.time() - ti),
         'time' : time_info,
         'best_model' : bestmodel_id,
+        'all_recordings_count' : all_recordings_count,
     }
     utils.save_html(outputfile, context, 'recommend.html')
     
-            
-            
