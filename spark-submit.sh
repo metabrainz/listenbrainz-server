af48350a5d2a56641e06c2b523c237031c664c3f
Param Singh <param@metabrainz.org>
2019-06-08 20:23:09 +0200

    Fix avro plugin version

--- spark-submit.sh
+++ spark-submit.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 zip -r listenbrainz_spark.zip listenbrainz_spark/
-time ./run.sh /usr/local/spark/bin/spark-submit --master spark://spark-master.spark-network:7077 --num-executors=28 --executor-memory=1g --driver-memory=1g --py-files listenbrainz_spark.zip "$@"
+time ./run.sh /usr/local/spark/bin/spark-submit --packages org.apache.spark:spark-avro_2.11:2.4.1 --master spark://spark-master.spark-network:7077 --num-executors=28 --executor-memory=1g --driver-memory=1g --py-files listenbrainz_spark.zip "$@"
 
