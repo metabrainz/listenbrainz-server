4cbf8e654ccf63f4fe4190eabc2d97f6bfd516af
Param Singh <param@metabrainz.org>
2019-06-10 17:41:43 +0530

    Remove hardcoded paths
    The name of the output file and the directory for the MLHD
    data were hardcoded, changed them to use a single variable now.

--- listenbrainz_spark/mlhd/scripts/artist_popularity.py
+++ listenbrainz_spark/mlhd/scripts/artist_popularity.py
@@ -1,12 +1,14 @@
 import os
 import listenbrainz_spark
+from datetime import datetime
 
 from listenbrainz_spark import config
+from listenbrainz_spark.mlhd import MLHD_DATA_PATH
 
 
 def main():
     listenbrainz_spark.init_spark_session('artist_popularity')
-    mlhd_df_path = config.HDFS_CLUSTER_URI + os.path.join('/data', 'mlhd', '*.avro')
+    mlhd_df_path = config.HDFS_CLUSTER_URI + os.path.join(MLHD_DATA_PATH, '*.avro')
     print('Loading MLHD Dataframe...')
     mlhd_df = listenbrainz_spark.sql_context.read.format('avro').load(mlhd_df_path)
     print("Loaded!")
@@ -22,6 +24,7 @@ def main():
     print("number of rows: ", artist_popularity_df.count())
     artist_popularity_df.show()
     print("Saving...")
-    csv_path = config.HDFS_CLUSTER_URI + os.path.join('/data', 'mlhd', 'artist_popularity-1.csv')
+    file_name = 'mlhd-artist-popularity-%s.csv' % datetime.now.strftime('%Y%m%d-%H%M%S')
+    csv_path = config.HDFS_CLUSTER_URI + os.path.join(MLHD_DATA_PATH, 'csv', file_name)
     artist_popularity_df.write.csv(csv_path)
     print("Saved to %s!" % csv_path)
