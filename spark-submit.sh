0c056c54585a497972e302ee24555c3c53756781
Param Singh <param@metabrainz.org>
2019-06-25 18:45:23 +0530

    Create a config.sh and put values there

--- spark-submit.sh
+++ spark-submit.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
 
+source config.sh
+
 zip -r listenbrainz_spark.zip listenbrainz_spark/
 time ./run.sh /usr/local/spark/bin/spark-submit \
 	--packages org.apache.spark:spark-avro_2.11:2.4.1 \
-	--master spark://spark-master.spark-network:7077 \
-	--num-executors=7 \
-	--executor-memory=3g \
-	--driver-memory=6g \
+	--master $SPARK_URI \
+	--num-executors=$EXECUTOR_COUNT \
+	--executor-memory=$EXECUTOR_MEMORY \
+	--driver-memory=$DRIVER_MEMORY \
 	--py-files listenbrainz_spark.zip \
 	"$@"
