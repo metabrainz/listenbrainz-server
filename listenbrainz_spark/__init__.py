ccb4858c2f05ac7483b6778b1a7e61ef6cbc3587
vansika <vansikapareek2001@gmail.com>
2019-06-29 11:02:32 +0530

    use pythonic way to catch exceptions and use sql whereever possible

--- listenbrainz_spark/__init__.py
+++ listenbrainz_spark/__init__.py
@@ -1,3 +1,5 @@
+import logging
+
 from pyspark import SparkContext
 from pyspark.sql import SparkSession, SQLContext
 
@@ -8,12 +10,19 @@ sql_context = None
 
 def init_spark_session(app_name):
     global session, context, sql_context
-    session = SparkSession \
-            .builder \
-            .appName(app_name) \
-            .config("spark.hadoop.dfs.client.use.datanode.hostname", "true") \
-            .config("spark.hadoop.dfs.datanode.use.datanode.hostname", "true") \
-            .getOrCreate()
-    context = session.sparkContext
-    context.setLogLevel("ERROR")
-    sql_context = SQLContext(context)
+    try:
+        session = SparkSession \
+                .builder \
+                .appName(app_name) \
+                .config("spark.hadoop.dfs.client.use.datanode.hostname", "true") \
+                .config("spark.hadoop.dfs.datanode.use.datanode.hostname", "true") \
+                .getOrCreate()
+        context = session.sparkContext
+        context.setLogLevel("ERROR")
+        sql_context = SQLContext(context)
+    except AttributeError as err:
+        raise AttributeError(logging.error('Cannot initialize Spark session "{}": {} \n {}.'.format(app_name,
+            type(err).__name__, str(err))))
+    except Exception as err:
+        raise Exception(logging.error('An error occurred while initializing Spark session "{}": {} \n {}.'.format(
+            app_name, type(err).__name__, str(err)), exc_info=True))
