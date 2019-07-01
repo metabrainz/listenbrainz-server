dcafdd75d024e9025b94bcc5d410a7a75d6940b1
vansika <vansikapareek2001@gmail.com>
2019-07-01 12:47:41 +0530

    improve formatting and introduce sql

--- listenbrainz_spark/utils.py
+++ listenbrainz_spark/utils.py
@@ -17,8 +17,8 @@ def register_dataframe(df, table_name):
     try:
         df.createOrReplaceTempView(table_name)
     except AnalysisException as err:
-        raise AnalysisException('Cannot register dataframe "{}": {} \n{}.'.format(table_name,
-            type(err).__name__, str(err)), stackTrace=True)
+        raise AnalysisException('Cannot register dataframe "{}": {} \n{}.'.format(table_name, type(err).__name__,
+            str(err)), stackTrace=True)
     except Exception as err:
         raise Exception('An error occured while registering dataframe "{}": {} \n{}.'.format(table_name,
             type(err).__name__, str(err)))
@@ -28,8 +28,7 @@ def read_files_from_HDFS(path):
         df = listenbrainz_spark.sql_context.read.parquet(path)
         return df
     except AnalysisException as err:
-        raise AnalysisException('Cannot read "{}" from HDFS: {} \n{}. '.format(path, type(err).__name__,
-            str(err)), stackTrace=True)
+        raise AnalysisException('Cannot read "{}" from HDFS: {} \n{}.'.format(path, type(err).__name__, str(err)),
+            stackTrace=True)
     except Exception as err:
-        raise Exception('An error occured while fetching "{}": {} \n{}. '.format(path, type(err).__name__,
-            str(err)))
+        raise Exception('An error occured while fetching "{}": {} \n{}.'.format(path, type(err).__name__, str(err)))
