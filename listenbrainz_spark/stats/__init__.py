303ad0d83bc735116fc6945b0f956594637eca8c
vansika <vansikapareek2001@gmail.com>
2019-07-04 15:10:22 +0530

    catch all exceptions raised by sql in pyspark

--- listenbrainz_spark/stats/__init__.py
+++ listenbrainz_spark/stats/__init__.py
@@ -1,13 +1,38 @@
 import listenbrainz_spark
 from datetime import datetime
 from dateutil.relativedelta import relativedelta
+from pyspark.sql.utils import *
 
 def run_query(query):
     """ Returns dataframe that results from running the query.
 
-    Note: It is the responsibility of the caller to register tables etc.
+        Args:
+            query (str): SQL query to execute.
+
+    Note:
+        >> While dealing with SQL queries in pyspark, catch the outermost exceptions and not Py4JJavaError
+           since it is the innermost exception raised. For all the final(outer) exceptions refer to:
+           'https://github.com/apache/spark/blob/master/python/pyspark/sql/utils.py'.
+           In all other cases where Py4JJavaError is the only exception raised, catch it as such.
+        >> It is the responsibility of the caller to register tables etc.
     """
-    return listenbrainz_spark.sql_context.sql(query)
+    try:
+        processed_query = listenbrainz_spark.sql_context.sql(query)
+    except AnalysisException as err:
+        raise Exception('{}. Failed to analyze SQL query plan for\n{}\n{}'.format(type(err).__name__, query, str(err)))
+    except ParseException as err:
+        raise Exception('{}. Failed to parse SQL command\n{}\n{}'.format(type(err).__name__, query, str(err)))
+    except IllegalArgumentException as err:
+        raise Exception('{}. Passed an illegal or inappropriate argument to\n{}\n{}'.format(type(err).__name__, query,
+            str(err)))
+    except StreamingQueryException as err:
+        raise Exception('{}. Exception that stopped a :class:`StreamingQuery`\n{}\n{}'.format(type(err).__name__, query,
+            str(err)))
+    except QueryExecutionException as err:
+        raise Exception('{}. Failed to execute a query{}\n{}'.format(type(err).__name__, query, str(err)))
+    except UnknownException as err:
+        raise Exception('{}. An error occurred while executing{}\n{}'.format(type(err).__name__, query, str(err)))
+    return processed_query
 
 def date_for_stats_calculation():
     t = datetime.utcnow().replace(day=1)
