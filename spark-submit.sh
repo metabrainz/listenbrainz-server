fca13151864cf3bc2c12d7d11056cbf0d69fdb55
vansika <vansikapareek2001@gmail.com>
2019-07-18 12:00:47 +0530

    config values and spark submit params for standalone mode

--- spark-submit.sh
+++ spark-submit.sh
@@ -6,8 +6,10 @@ zip -r listenbrainz_spark.zip listenbrainz_spark/
 time ./run.sh /usr/local/spark/bin/spark-submit \
 	--packages org.apache.spark:spark-avro_2.11:2.4.1 \
 	--master $SPARK_URI \
-	--num-executors=$EXECUTOR_COUNT \
-	--executor-memory=$EXECUTOR_MEMORY \
-	--driver-memory=$DRIVER_MEMORY \
+	--conf "spark.scheduler.listenerbus.eventqueue.capacity"=$LISTENERBUS_CAPACITY \
+    --conf "spark.cores.max"=$MAX_CORES \
+    --conf "spark.executor.cores"=$EXECUTOR_CORES \
+    --conf "spark.executor.memory"=$EXECUTOR_MEMORY \
+    --conf "spark.driver.memory"=$DRIVER_MEMORY \
 	--py-files listenbrainz_spark.zip \
 	"$@"
