0a2b8025d580d36c698c1d63529517bb68fefd7b
vansika <vansikapareek2001@gmail.com>
2019-08-17 00:21:23 +0530

    remove files which are no longer used due to change in codebase

--- setup.py
+++
@@ -1,9 +0,0 @@
-from pyspark import SparkContext
-from pyspark.sql import SparkSession
-
-spark = SparkSession \
-        .builder \
-        .appName("LB recommender") \
-        .getOrCreate()
-
-sc = spark.sparkContext
