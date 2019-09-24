159beb7c3cedde8235482431887de3fe47861b45
Param Singh <iliekcomputers@gmail.com>
2019-09-23 18:00:07 -0700

    Add script to request user stats from the spark cluster

--- listenbrainz/stats/populate.py
+++ listenbrainz/stats/populate.py
@@ -1,84 +1,45 @@
 import click
-import listenbrainz.db.stats as db_stats
-import listenbrainz.db.user as db_user
-import listenbrainz.stats.user as stats_user
 import listenbrainz.utils as utils
-import logging
 import pika
-import sys
-import time
 import ujson
 
-from listenbrainz import config
 from listenbrainz import db
 from listenbrainz import stats
+from listenbrainz.webserver import create_app
 
-logger = logging.getLogger(__name__)
-logger.setLevel(logging.INFO)
-handler = logging.StreamHandler(sys.stdout)
-handler.setLevel(logging.DEBUG)
-formatter = logging.Formatter('%(asctime)s - %(levelname)s - %(message)s')
-handler.setFormatter(formatter)
-logger.addHandler(handler)
-
-
-def push_users_to_queue(channel, force=False):
-    """ Get users from the db whose stats haven't been calculated and push
-        them into the queue.
-
-        Args:
-            channel: the RabbitMQ channel in which we should publish the user data
-    """
-    logger.info('pushing users to stats calculation queue...')
-    if force:
-        users = db_user.get_all_users()
-    else:
-        users = db_user.get_users_with_uncalculated_stats()
-
-    for user in users:
-        data = {
-            'type': 'user',
-            'id': user['id'],
-            'musicbrainz_id': user['musicbrainz_id']
-        }
-
-        channel.basic_publish(
-            exchange=config.BIGQUERY_EXCHANGE,
-            routing_key='',
-            body=ujson.dumps(data),
-            properties=pika.BasicProperties(delivery_mode = 2,),
-        )
-    logger.info('pushed %d users!', len(users))
-
-
-def push_entities_to_queue(force=False):
-    """ Creates a RabbitMQ connection and uses it to push entities which
-        need their stats calculated into the queue.
-    """
-    rabbitmq_connection = utils.connect_to_rabbitmq(
-        username=config.RABBITMQ_USERNAME,
-        password=config.RABBITMQ_PASSWORD,
-        host=config.RABBITMQ_HOST,
-        port=config.RABBITMQ_PORT,
-        virtual_host=config.RABBITMQ_VHOST,
-    )
-    channel = rabbitmq_connection.channel()
-    channel.exchange_declare(exchange=config.BIGQUERY_EXCHANGE, exchange_type='fanout')
-    push_users_to_queue(channel, force)
-
+from flask import current_app
 
 cli = click.Group()
 
-@cli.command(name="populate_queue")
-@click.option('--force', '-f', is_flag=True, help='Force statistics calculation for ALL entities')
-def populate_queue(force):
-    """ Populate the ListenBrainz stats calculation queue with entities
-        which need their stats calculated.
-    """
-    logger.info('Connecting to database...')
-    db.init_db_connection(config.SQLALCHEMY_DATABASE_URI)
-    logger.info('Connected!')
+def send_message_to_spark_cluster(message):
+    with create_app().app_context():
+        rabbitmq_connection = utils.connect_to_rabbitmq(
+            username=current_app.config['RABBITMQ_USERNAME'],
+            password=current_app.config['RABBITMQ_PASSWORD'],
+            host=current_app.config['RABBITMQ_HOST'],
+            port=current_app.config['RABBITMQ_PORT'],
+            virtual_host=current_app.config['RABBITMQ_VHOST'],
+            error_logger=current_app.logger,
+        )
+        try:
+            channel = rabbitmq_connection.channel()
+            channel.exchange_declare(exchange=current_app.config['SPARK_JOB_REQUEST_EXCHANGE'], exchange_type='fanout')
+            channel.basic_publish(
+                exchange=current_app.config['SPARK_JOB_REQUEST_EXCHANGE'],
+                routing_key='',
+                body=ujson.dumps(message),
+                properties=pika.BasicProperties(delivery_mode=2,),
+            )
+        except Exception:
+            # this is a relatively non critical part of LB for now, so just log the error and
+            # move ahead
+            current_app.logger.error('Could not send message to spark cluster: %s', ujson.dumps(message), exc_info=True)
 
-    logger.info('Pushing entities whose stats should be calculated into the queue...')
-    push_entities_to_queue(force=force)
-    logger.info('Pushed all relevant entities, stats_calculator should calculate stats soon!')
+@cli.command(name="request_user_stats")
+def request_user_stats(force):
+    """ Send a user stats request to the spark cluster
+    """
+    send_message_to_spark_cluster({
+        'type': 'request',
+        'message': 'user_stats',
+    })
