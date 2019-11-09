de123db36f49545555712c1ed5929929540eeca8
Robert Kaye <rob@musicbrainz.org>
2019-11-09 14:27:19 +0100

    Change order of stopping services

--- docker/stop-master-service.sh
+++ docker/stop-master-service.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
-docker service rm spark-master hadoop-master
 
 # stop the request consumer
 ./stop-request-consumer-container.sh
+
+# stop the spark and hadoop service
+docker service rm spark-master hadoop-master
