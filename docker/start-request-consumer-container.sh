00e89fcd3f2799cdaf2e96d5f61e1e49f3121b1d
Robert Kaye <rob@musicbrainz.org>
2019-11-09 14:14:33 +0100

    Minor fixes, but it seems to work ok.

--- docker/start-request-consumer-container.sh
+++ docker/start-request-consumer-container.sh
@@ -2,22 +2,24 @@
 
 source config.sh
 
-CONTAINER_NAME = "spark-request-consumer"
+CONTAINER_NAME="spark-request-consumer"
 
 docker pull metabrainz/listenbrainz-spark-request-consumer:latest
 
-zip -r listenbrainz_spark.zip listenbrainz_spark/
+zip -rq listenbrainz_spark.zip listenbrainz_spark/
 docker run \
+    -d \
     -v `pwd`:/rec \
     --network spark-network \
     --name $CONTAINER_NAME \
-    --packages org.apache.spark:spark-avro_2.11:2.4.1 \
-    --master $SPARK_URI \
-    --conf "spark.scheduler.listenerbus.eventqueue.capacity"=$LISTENERBUS_CAPACITY \
-    --conf "spark.cores.max"=$MAX_CORES \
-    --conf "spark.executor.cores"=$EXECUTOR_CORES \
-    --conf "spark.executor.memory"=$EXECUTOR_MEMORY \
-    --conf "spark.driver.memory"=$DRIVER_MEMORY \
-    --py-files listenbrainz_spark.zip \
     metabrainz/listenbrainz-spark-request-consumer:latest \
-    /usr/local/spark/bin/spark-submit listenbrainz_spark/request_consumer/main.py
+    /usr/local/spark/bin/spark-submit \
+        --packages org.apache.spark:spark-avro_2.11:2.4.1 \
+        --master $SPARK_URI \
+        --conf "spark.scheduler.listenerbus.eventqueue.capacity"=$LISTENERBUS_CAPACITY \
+        --conf "spark.cores.max"=$MAX_CORES \
+        --conf "spark.executor.cores"=$EXECUTOR_CORES \
+        --conf "spark.executor.memory"=$EXECUTOR_MEMORY \
+        --conf "spark.driver.memory"=$DRIVER_MEMORY \
+        --py-files listenbrainz_spark.zip \
+        listenbrainz_spark/request_consumer/main.py
