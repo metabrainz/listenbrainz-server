e5ce522d2ce9ac4cb7a00c65d13e125a210cbd54
Robert Kaye <rob@musicbrainz.org>
2019-11-09 13:33:53 +0100

    First attempt at a request-consumer container. Expect much fail.

--- docker/stop-master-service.sh
+++ docker/stop-master-service.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
 
 docker service rm spark-master hadoop-master
+
+# stop the request consumer
+./stop-request-consumer-container.sh
---
+++ listenbrainz_spark/request_consumer/main.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+
+def main():
+    print("Dunno, looking cute.. you?")
+    sleep(1000)
+
+
+if __name__ == "__main__":
+    main()
