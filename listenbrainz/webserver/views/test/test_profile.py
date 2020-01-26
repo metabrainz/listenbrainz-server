7cac6535f27568396461327765720776470d8575
Ruud van Asseldonk <dev@veniogames.com>
2020-01-26 17:42:23 +0100

    LB-484: Stream response from /profile/export endpoint (#708)
    * Make /profile/export endpoint streaming
    
    I was running into gateway timeouts when trying to export the listens
    in my profile. My profile has about 150k listens. When looking into the
    source behind the export endpoint, I noticed that it builds two lists
    in Python with all listens: once with all raw data fetched from
    listenstore, and then once more with the dicts to export. The list of
    export dicts is built while iterating over the list of raw results, so
    the first list cannot be collected until the second one is built (it
    does share the dicts from the raw response, they are updated in place).
    Also, because these lists are built incrementally, that will cause a few
    reallocations. Furthermore, the response cannot be served until all
    listens have been loaded and converted. I suspect this is simply taking
    too long for 150k listens.
    
    Fortunately the export can be streamed. There is no need to build lists
    in memory, Flask supports generators that produce the body. Streaming
    the body has two advantages:
    
     * We can export listens in constant memory, regardless of how many
       listens there are.
    
     * We can start serving the response immediately, we don't need to load
       all listens before we can start serving.
    
    The downside is that it is a bit more complex to write. We need to
    extract the loops into functions so they can be generators, and we need
    to format the json array ourselves (but only the brackets and comma, the
    elements are still encoded by ujson).
    
    * Add another test for /profile/export endpoint
    
    There already is an integration test for this endpoint, but there are
    a few things that are not exercised by this test. It sends only three
    listens, so they would be in a single fetch batch. By mocking
    fetch_listens, we can feed more than one batch. Also, check that the
    keys that map_listen_for_export adds are all present.
    
    * Use Listen.to_api for /profile/export endpoint
    
    This changes the schema of the exported data to be consistent with other
    parts of the API.

--- listenbrainz/webserver/views/test/test_profile.py
+++ listenbrainz/webserver/views/test/test_profile.py
@@ -3,11 +3,13 @@ import listenbrainz.db.user as db_user
 import listenbrainz.db.spotify as db_spotify
 import pytz
 import time
+import ujson
 
 from datetime import datetime
 from flask import url_for
 from listenbrainz.domain import spotify
 from listenbrainz.db.testing import DatabaseTestCase
+from listenbrainz.listen import Listen
 from listenbrainz.webserver.testing import ServerTestCase
 from unittest.mock import patch
 
@@ -196,4 +198,94 @@ class ProfileViewsTestCase(ServerTestCase, DatabaseTestCase):
             'musicbrainz_id': self.user['musicbrainz_id'],
             'user_token': 'new-token',
             'permission': 'user-read-recently-played',
+        })
+
+    @patch('listenbrainz.listenstore.influx_listenstore.InfluxListenStore.fetch_listens')
+    def test_export_streaming(self, mock_fetch_listens):
+        self.temporary_login(self.user['login_id'])
+
+        # Three example listens, with only basic data for the purpose of this test.
+        # In each listen, one of {release_artist, release_msid, recording_msid}
+        # is missing.
+        listens = [
+            Listen(
+                timestamp=1539509881,
+                artist_msid='61746abb-76a5-465d-aee7-c4c42d61b7c4',
+                recording_msid='6c617681-281e-4dae-af59-8e00f93c4376',
+                data={
+                    'artist_name': 'Massive Attack',
+                    'track_name': 'The Spoils',
+                    'additional_info': {},
+                },
+            ),
+            Listen(
+                timestamp=1539441702,
+                release_msid='0c1d2dc3-3704-4e75-92f9-940801a1eebd',
+                recording_msid='7ad53fd7-5b40-4e13-b680-52716fb86d5f',
+                data={
+                    'artist_name': 'Snow Patrol',
+                    'track_name': 'Lifening',
+                    'additional_info': {},
+                },
+            ),
+            Listen(
+                timestamp=1539441531,
+                release_msid='7816411a-2cc6-4e43-b7a1-60ad093c2c31',
+                artist_msid='7e2c6fe4-3e3f-496e-961d-dce04a44f01b',
+                data={
+                    'artist_name': 'Muse',
+                    'track_name': 'Drones',
+                    'additional_info': {},
+                },
+            ),
+        ]
+
+        # We expect three calls to fetch_listens, and we return two, one, and
+        # zero listens in the batch. This tests that we fetch all batches.
+        mock_fetch_listens.side_effect = [listens[0:2], listens[2:3], []]
+
+        r = self.client.post(url_for('profile.export_data'))
+        self.assert200(r)
+
+        # r.json returns None, so we decode the response manually.
+        results = ujson.loads(r.data.decode('utf-8'))
+
+        self.assertDictEqual(results[0], {
+            'listened_at': 1539509881,
+            'recording_msid': '6c617681-281e-4dae-af59-8e00f93c4376',
+            'user_name': None,
+            'track_metadata': {
+                'artist_name': 'Massive Attack',
+                'track_name': 'The Spoils',
+                'additional_info': {
+                    'artist_msid': '61746abb-76a5-465d-aee7-c4c42d61b7c4',
+                    'release_msid': None,
+                },
+            },
+        })
+        self.assertDictEqual(results[1], {
+            'listened_at': 1539441702,
+            'recording_msid': '7ad53fd7-5b40-4e13-b680-52716fb86d5f',
+            'user_name': None,
+            'track_metadata': {
+                'artist_name': 'Snow Patrol',
+                'track_name': 'Lifening',
+                'additional_info': {
+                    'artist_msid': None,
+                    'release_msid': '0c1d2dc3-3704-4e75-92f9-940801a1eebd',
+                },
+            },
+        })
+        self.assertDictEqual(results[2], {
+            'listened_at': 1539441531,
+            'recording_msid': None,
+            'user_name': None,
+            'track_metadata': {
+                'artist_name': 'Muse',
+                'track_name': 'Drones',
+                'additional_info': {
+                    'artist_msid': '7e2c6fe4-3e3f-496e-961d-dce04a44f01b',
+                    'release_msid': '7816411a-2cc6-4e43-b7a1-60ad093c2c31',
+                },
+            },
         })
