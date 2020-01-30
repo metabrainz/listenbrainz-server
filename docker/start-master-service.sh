3a490b5f92edf8a986ebab276aec06a6e2fc9b04
Robert Kaye <rob@musicbrainz.org>
2020-01-30 13:55:58 +0100

    Add ability to send emails from spark cluster leader. (#713)
    * Attempt to setup exim on leader
    
    * Also expose 587 and fix typo

--- docker/start-master-service.sh
+++ docker/start-master-service.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 
+OUTMAIL_DOMAIN=metabrainz.org
+MAILNAME=listenbrainz.org
+NAME=exim-relay-$MAILNAME
+SPOOL_VOLUME_NAME=${2:-exim-relay-spool-$MAILNAME}
+TAG=$(echo -n "$MAILNAME"|tr '.' '-')
+IMAGE=metabrainz/docker-exim
+
 # Before this runs, make sure to run set-master-node.sh
 docker pull metabrainz/spark-master
 
@@ -20,6 +27,21 @@ docker service create --replicas 1 \
     --env SPARK_NO_DAEMONIZE=1 \
     --mount type=volume,source=spark-volume,destination=/home/hadoop/spark \
     metabrainz/spark-master
+
+docker pull $IMAGE
+docker volume create --driver local --name $SPOOL_VOLUME_NAME
+docker run \
+    --detach \
+    --name $NAME \
+    --hostname $(hostname -s).${OUTMAIL_DOMAIN} \
+    --env DOMAIN=${MAILNAME} \
+    --network spark-network \
+    --env GMAIL_GSUITE_RELAY=yes \
+    -v ${SPOOL_VOLUME_NAME}:/var/spool/exim4 \
+    --publish 25:25 \
+    --publish 587:587 \
+    --restart unless-stopped \
+    $IMAGE
 
 # start the request consumer (stats, recommendation requests from the main server farm)
 ./start-request-consumer-container.sh
