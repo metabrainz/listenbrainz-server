f3aabc78c47e05ce948d5c5139998fced0183726
vansika <vansikapareek2001@gmail.com>
2020-01-30 21:59:25 +0530

    import utils as module and not attribute

--- listenbrainz_spark/stats/user/tests/test_utils.py
+++ listenbrainz_spark/stats/user/tests/test_utils.py
@@ -1,4 +1,4 @@
-from listenbrainz_spark.stats import user
+import listenbrainz_spark.stats.user.utils as user_utils
 from listenbrainz_spark import utils
 from listenbrainz_spark.tests import SparkTestCase
 
@@ -7,16 +7,16 @@ from pyspark.sql import Row
 class UtilsTestCase(SparkTestCase):
 
     def create_df(self):
-        df = utils.create_dataframe([Row(user_name='user2', artist_name='artist1', artist_msid='1',artist_mbids='1',
+        df = utils.create_dataframe(Row(user_name='user2', artist_name='artist1', artist_msid='1',artist_mbids='1',
             track_name='test', recording_msid='1', recording_mbid='1', release_name='test',release_msid='1',
-            release_mbid='1')], schema=None)
-        df1 = utils.create_dataframe([Row(user_name='user1',artist_name='artist1', artist_msid='1',artist_mbids='1',
+            release_mbid='1'), schema=None)
+        df1 = utils.create_dataframe(Row(user_name='user1',artist_name='artist1', artist_msid='1',artist_mbids='1',
             track_name='test', recording_msid='1', recording_mbid='1', release_name='test',release_msid='1',
-             release_mbid='1')], schema=None)
+             release_mbid='1'), schema=None)
         df = df.union(df1)
-        df2 = utils.create_dataframe([Row(user_name='user1',artist_name='artist1', artist_msid='1',artist_mbids='1',
+        df2 = utils.create_dataframe(Row(user_name='user1',artist_name='artist1', artist_msid='1',artist_mbids='1',
             track_name='test', recording_msid='1', recording_mbid='1', release_name='test',release_msid='1',
-            release_mbid='1')], schema=None)
+            release_mbid='1'), schema=None)
         df = df.union(df2)
         return df
 
@@ -38,8 +38,8 @@ class UtilsTestCase(SparkTestCase):
                     }]
                      }
 
-        self.assertDictEqual(user.utils.get_artists('table'), dictionary)
-        self.assertEqual(user.utils.get_artists('table')['user1'][0]['listen_count'], 2)
+        self.assertDictEqual(user_utils.get_artists('table'), dictionary)
+        self.assertEqual(user_utils.get_artists('table')['user1'][0]['listen_count'], 2)
 
     def test_get_recordings(self):
         df = self.create_df()
@@ -71,8 +71,8 @@ class UtilsTestCase(SparkTestCase):
                     }]
                      }
 
-        self.assertDictEqual(user.utils.get_recordings('table'), dictionary)
-        self.assertEqual(user.utils.get_recordings('table')['user1'][0]['listen_count'], 2)
+        self.assertDictEqual(user_utils.get_recordings('table'), dictionary)
+        self.assertEqual(user_utils.get_recordings('table')['user1'][0]['listen_count'], 2)
 
     def test_get_releases(self):
         df = self.create_df()
@@ -98,5 +98,5 @@ class UtilsTestCase(SparkTestCase):
                     }]
                      }
 
-        self.assertDictEqual(user.utils.get_releases('table'), dictionary)
-        self.assertEqual(user.utils.get_releases('table')['user1'][0]['listen_count'], 2)
+        self.assertDictEqual(user_utils.get_releases('table'), dictionary)
+        self.assertEqual(user_utils.get_releases('table')['user1'][0]['listen_count'], 2)
