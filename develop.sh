7aa158acaee5b9d8a4ecbc104d3afa9c22270bf0
Vansika Pareek <vansika@metabrainz.org>
2020-02-20 03:08:37 +0530

    format namenode and datanode together (#712)
    * format namenode and datanode with one command
    
    * use spark_develop.sh for running spark specific commands
    
    * brief about ListenBrainz and ListenBrainz Spark setup

--- develop.sh
+++ develop.sh
@@ -5,63 +5,53 @@ if [[ ! -d "docker" ]]; then
     exit -1
 fi
 
-function invoke_docker_compose_spark {
-	docker-compose -f docker/docker-compose.spark.yml \
-				-p listenbrainzspark \
-				"$@"
-}
-
 function invoke_docker_compose {
-	docker-compose -f docker/docker-compose.yml \
-				-p listenbrainz \
-				"$@"
+    docker-compose -f docker/docker-compose.yml \
+                -p listenbrainz \
+                "$@"
 }
 
 function invoke_manage {
-	invoke_docker_compose run --rm web \
-			python3 manage.py \
-			"$@"
+    invoke_docker_compose run --rm web \
+            python3 manage.py \
+            "$@"
 }
 
 function open_psql_shell {
-	invoke_docker_compose run --rm web psql \
-			-U listenbrainz  \
-			-h db listenbrainz
+    invoke_docker_compose run --rm web psql \
+            -U listenbrainz  \
+            -h db listenbrainz
 }
 
 function npm_install {
-	invoke_docker_compose run --rm -e \
-				HOME=/tmp static_builder npm install
+    invoke_docker_compose run --rm -e \
+                HOME=/tmp static_builder npm install
 }
 # Arguments following "manage" are as it is passed to function "invoke_manage" and executed.
 # Check on each argument of manage.py is not performed here because with manage.py, develop.sh will expand too.
 # Also, if any of the arguments passed to develop.sh which invoke manage.py are incorrect, exception would be raised by manage.py
 # so we may skip extra checks in here.
 if [ "$1" == "manage" ]; then shift
-	echo "Invoking manage.py..."
-	invoke_manage "$@"
-	exit
+    echo "Invoking manage.py..."
+    invoke_manage "$@"
+    exit
 
 elif [ "$1" == "psql" ]; then
-	echo "Entering into PSQL shell to query DB..."
-	open_psql_shell
-	exit
+    echo "Entering into PSQL shell to query DB..."
+    open_psql_shell
+    exit
 
 elif [ "$1" == "npm" ]; then
-	echo "Installing node dependencies..."
-	npm_install
-	exit
-
-elif [ "$1" == "spark" ]; then shift
-	invoke_docker_compose_spark "$@"
-	exit
+    echo "Installing node dependencies..."
+    npm_install
+    exit
 
 else
-	if [ "$#" == 0 ]; then
-		echo "No argument provided. Trying to run docker-compose..."
-	else
-		echo "Trying to run the passed command with docker-compose..."
-	fi
-	invoke_docker_compose "$@"
-	exit
+    if [ "$#" == 0 ]; then
+        echo "No argument provided. Trying to run docker-compose..."
+    else
+        echo "Trying to run the passed command with docker-compose..."
+    fi
+    invoke_docker_compose "$@"
+    exit
 fi
