f4893a736ed24db2025f59fd2ed5e2bcdbad0330
Ishaan Shah <ishaan.n.shah@gmail.com>
2020-02-20 22:36:45 +0530

    Check if Importer is not null when mounted

--- listenbrainz/webserver/static/js/__tests__/lastFmImporter.test.js
+++ listenbrainz/webserver/static/js/__tests__/lastFmImporter.test.js
@@ -1,18 +1,15 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import { shallow } from 'enzyme'
 
 import LastFmImporter from '../jsx/lastFmImporter'
 
-let element;
 let props;
 
 describe('LastFmImporter is working correctly', () => {
   it('renders without crashing', () => {
     const div = document.createElement('div');
-    ReactDOM.render(<LastFmImporter {...props}/>, div);
+    ReactDOM.render(<LastFmImporter {...props} />, div);
+    expect(div.firstChild).toBeTruthy();
     ReactDOM.unmountComponentAtNode(div);
   })
-  
-
 });
