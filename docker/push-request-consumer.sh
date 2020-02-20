772a431928d6505ec64d10eaa46989983fe58a63
Param Singh <iliekcomputers@gmail.com>
2020-02-20 23:18:29 +0100

    Fix spark image push scripts

--- docker/push-request-consumer.sh
+++ docker/push-request-consumer.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 
-docker build --target metabrainz-spark-request-consumer -t metabrainz/listenbrainz-spark-request-consumer .
+cd "$(dirname "${BASH_SOURCE[0]}")/../"
+
+docker build --target metabrainz-spark-request-consumer -t metabrainz/listenbrainz-spark-request-consumer -f Dockerfile.spark .
 docker push metabrainz/listenbrainz-spark-request-consumer
