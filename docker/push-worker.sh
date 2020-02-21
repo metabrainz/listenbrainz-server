6dc57e6cc4f7dce57d7865510190e9e195f2b798
Param Singh <iliekcomputers@gmail.com>
2020-02-21 08:41:08 +0000

    Fix spark image push scripts (#741)

--- docker/push-worker.sh
+++ docker/push-worker.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 cd "$(dirname "${BASH_SOURCE[0]}")/../"
-docker build -t metabrainz/spark-worker --target metabrainz-spark-worker . && \
+docker build -t metabrainz/spark-worker --target metabrainz-spark-worker -f Dockerfile.spark . && \
     docker push metabrainz/spark-worker
