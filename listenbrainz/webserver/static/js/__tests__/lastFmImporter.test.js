c873d33be922ec06905f9d78a5cb5142b2738f9d
Ishaan Shah <ishaan.n.shah@gmail.com>
2020-02-22 21:42:22 +0530

    Update test

--- listenbrainz/webserver/static/js/__tests__/lastFmImporter.test.js
+++ listenbrainz/webserver/static/js/__tests__/lastFmImporter.test.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import ReactDOM from 'react-dom'
+import { mount } from 'enzyme'
 
 import LastFmImporter from '../jsx/lastFmImporter'
 
@@ -7,9 +7,7 @@ let props;
 
 describe('LastFmImporter is working correctly', () => {
   it('renders without crashing', () => {
-    const div = document.createElement('div');
-    ReactDOM.render(<LastFmImporter {...props} />, div);
-    expect(div.firstChild).toBeTruthy();
-    ReactDOM.unmountComponentAtNode(div);
+    const wrapper = mount(<LastFmImporter {...props} />);
+    expect(wrapper).toBeTruthy();
   })
 });
