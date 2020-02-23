86bfbb4e15dbfad5b1fa60c17a35ed3763ebe5c9
Param Singh <iliekcomputers@gmail.com>
2020-02-23 19:50:36 +0000

    Add support to the request consumer for dump imports

--- listenbrainz_spark/query_map.py
+++ listenbrainz_spark/query_map.py
@@ -1,7 +1,9 @@
 import listenbrainz_spark.stats.user.all
+import listenbrainz_spark.request_consumer.jobs.import_dump
 
 functions = {
-	'stats.user.all': listenbrainz_spark.stats.user.all.calculate,
+    'stats.user.all': listenbrainz_spark.stats.user.all.calculate,
+    'import.dump.full': listenbrainz_spark.request_consumer.jobs.import_dump.import_newest_full_dump_handler,
 }
 
 
---
+++ listenbrainz_spark/request_consumer/jobs/import_dump.py
@@ -0,0 +1,25 @@
+""" Spark job that downloads the latest listenbrainz dump and imports into HDFS
+"""
+
+import shutil
+import tempfile
+
+from datetime import datetime
+from listenbrainz_spark.ftp.download import ListenbrainzDataDownloader
+from listenbrainz_spark.hdfs.upload import ListenbrainzDataUploader
+
+
+def import_dump_to_hdfs(dump_type, force, dump_id=None):
+    temp_dir = tempfile.mkdtemp()
+    dump_type = 'incremental' if dump_type == 'incremental' else 'full'
+    src, dump_name = ListenbrainzDataDownloader().download_listens(directory=temp_dir, dump_type=dump_type)
+    ListenbrainzDataUploader().upload_listens(src, force=force)
+    shutil.rmtree(temp_dir)
+    return dump_name
+
+def import_newest_full_dump_handler():
+    dump_name = import_dump_to_hdfs('full', force=True)
+    return [{
+        'imported_dump': dump_name,
+        'time': str(datetime.utcnow()),
+    }]
