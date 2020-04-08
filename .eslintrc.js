d7b3d274d0701280e50f1265e252927cfa0b89e8
Ishaan Shah <ishaan.n.shah@gmail.com>
2020-04-08 12:23:09 +0530

    Update .eslintrc.js

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,46 +4,42 @@ module.exports = {
     es6: true,
   },
   extends: [
-    'plugin:react/recommended',
-    'airbnb',
-    'prettier',
-    'prettier/@typescript-eslint',
-    'prettier/react',
+    "plugin:react/recommended",
+    "airbnb",
+    "prettier",
+    "prettier/@typescript-eslint",
+    "prettier/react",
   ],
   globals: {
-    Atomics: 'readonly',
-    SharedArrayBuffer: 'readonly',
+    Atomics: "readonly",
+    SharedArrayBuffer: "readonly",
   },
-  parser: '@typescript-eslint/parser',
+  parser: "@typescript-eslint/parser",
   parserOptions: {
     ecmaFeatures: {
       jsx: true,
     },
     ecmaVersion: 2018,
-    sourceType: 'module',
+    sourceType: "module",
   },
-  plugins: [
-    'react',
-    '@typescript-eslint',
-    'prettier',
-  ],
+  plugins: ["react", "@typescript-eslint", "prettier"],
   rules: {
-    'prettier/prettier': 'error',
+    "prettier/prettier": "error",
   },
   overrides: [
     {
-      files: '**/*.test.js',
+      files: ["**/*.test.js", "**/*.test.ts"],
       env: {
         jest: true,
       },
-      plugins: ['jest'],
+      plugins: ["jest"],
       rules: {
-        'jest/no-disabled-tests': 'warn',
-        'jest/no-focused-tests': 'error',
-        'jest/no-identical-title': 'error',
-        'jest/prefer-to-have-length':  'warn',
-        'jest/valid-expect': 'error'
-      }
-    }
-  ]
+        "jest/no-disabled-tests": "warn",
+        "jest/no-focused-tests": "error",
+        "jest/no-identical-title": "error",
+        "jest/prefer-to-have-length": "warn",
+        "jest/valid-expect": "error",
+      },
+    },
+  ],
 };
