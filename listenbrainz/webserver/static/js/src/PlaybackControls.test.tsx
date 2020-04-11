d811e87d59cfe867439487f0d7f1c9437057e47d
Param Singh <iliekcomputers@gmail.com>
2020-04-11 19:48:10 +0100

    move play direction to types.d.ts

--- listenbrainz/webserver/static/js/src/PlaybackControls.test.tsx
+++ listenbrainz/webserver/static/js/src/PlaybackControls.test.tsx
@@ -1,7 +1,7 @@
 import * as React from "react";
 import { mount } from "enzyme";
 
-import { SpotifyPlayDirection, PlaybackControls } from "./PlaybackControls";
+import { PlaybackControls } from "./PlaybackControls";
 
 const props = {
   playPreviousTrack: () => {},
--- listenbrainz/webserver/static/js/src/PlaybackControls.tsx
+++ listenbrainz/webserver/static/js/src/PlaybackControls.tsx
@@ -14,8 +14,6 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { IconDefinition } from "@fortawesome/fontawesome-common-types"; // eslint-disable-line import/no-unresolved
 import { IconProp } from "@fortawesome/fontawesome-svg-core";
 
-export type SpotifyPlayDirection = "up" | "down" | "hidden";
-
 type PlaybackControlsProps = {
   playPreviousTrack: () => void;
   playNextTrack: (invert?: boolean) => void;
