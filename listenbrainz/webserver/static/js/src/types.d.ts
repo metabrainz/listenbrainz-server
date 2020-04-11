cc3826eacc217e6208bdf30f4548b0ae3d356cb6
Param Singh <iliekcomputers@gmail.com>
2020-04-11 22:59:25 +0100

    Convert SpotifyPlayer to typescript

--- listenbrainz/webserver/static/js/src/types.d.ts
+++ listenbrainz/webserver/static/js/src/types.d.ts
@@ -1,3 +1,4 @@
+declare module "spotify-web-playback-sdk";
 // TODO: make this type specific
 declare type Listen = any;
 
@@ -6,6 +7,23 @@ declare type ListenType = "single" | "playingNow" | "import";
 declare type SpotifyPlayDirection = "up" | "down" | "hidden";
 // TODO: make this type specific
 declare type SubmitListensPayload = any;
+
+declare type SpotifyUser = any; // TODO
+declare type SpotifyPermission = any; // TODO
+declare type SpotifyTrack = any; // TODO
+declare type SpotifyArtist = any; // TODO
+declare type SpotifyDeviceID = any; // TODO
+declare interface SpotifyImage {
+  height: number;
+}
+declare type SpotifyPlayerTrackWindow = any; // TODO
+declare type AlertType = any; // TODO
+declare type SpotifyPlayerSDKState = any;
+
+// the spotify-web-playback-sdk types are a bit messy
+// Adding an any here for now.
+// TODO: remove this any eventually
+declare type SpotifyPlayerType = any | Spotify.SpotifyPlayer;
 
 declare interface ImporterProps {
   user: {
