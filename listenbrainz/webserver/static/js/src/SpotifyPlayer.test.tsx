af8a2a1bbf62443466878819c14ae368ff66e0f0
Param Singh <iliekcomputers@gmail.com>
2020-04-12 11:31:34 +0100

    Convert SpotifyPlayer to typescript (#792)
    * Convert SpotifyPlayer to typescript
    
    * Add test and remove the temp file that is not needed now
    
    * Remove birthdate from required permissions
    
    * Add missing returns to fix alert recursion bug

---
+++ listenbrainz/webserver/static/js/src/SpotifyPlayer.test.tsx
@@ -0,0 +1,31 @@
+import * as React from "react";
+import { mount } from "enzyme";
+
+import SpotifyPlayer from "./SpotifyPlayer";
+import APIService from "./APIService";
+
+const props = {
+  spotifyUser: {
+    access_token: "heyo",
+    permission: "read",
+  },
+  direction: "up" as SpotifyPlayDirection,
+  onPermissionError: (message: string) => {},
+  onCurrentListenChange: (listen: Listen) => {},
+  newAlert: (
+    type: AlertType,
+    title: string,
+    message: string | JSX.Element
+  ) => {},
+  apiService: new APIService("base-uri"),
+  onAccountError: (message: string | JSX.Element) => {},
+  listens: [],
+};
+
+describe("SpotifyPlayer", () => {
+  it("renders", () => {
+    window.fetch = jest.fn();
+    const wrapper = mount(<SpotifyPlayer {...props} />);
+    expect(wrapper.html()).toMatchSnapshot();
+  });
+});
--- listenbrainz/webserver/static/js/src/types.d.ts
+++ listenbrainz/webserver/static/js/src/types.d.ts
@@ -1,3 +1,4 @@
+declare module "spotify-web-playback-sdk";
 // TODO: make this type specific
 declare type Listen = any;
 
@@ -6,6 +7,26 @@ declare type ListenType = "single" | "playingNow" | "import";
 declare type SpotifyPlayDirection = "up" | "down" | "hidden";
 // TODO: make this type specific
 declare type SubmitListensPayload = any;
+
+declare interface SpotifyUser {
+  access_token: string;
+  permission: string;
+}
+declare type SpotifyPermission = any; // TODO
+declare type SpotifyTrack = any; // TODO
+declare type SpotifyArtist = any; // TODO
+declare type SpotifyDeviceID = any; // TODO
+declare interface SpotifyImage {
+  height: number;
+}
+declare type SpotifyPlayerTrackWindow = any; // TODO
+declare type AlertType = any; // TODO
+declare type SpotifyPlayerSDKState = any;
+
+// the spotify-web-playback-sdk types are a bit messy
+// Adding an any here for now.
+// TODO: remove this any eventually
+declare type SpotifyPlayerType = any | Spotify.SpotifyPlayer;
 
 declare interface ImporterProps {
   user: {
