dac35d31354fc4a558a267deda0f930a57893440
Param Singh <iliekcomputers@gmail.com>
2020-04-12 14:07:39 +0100

    First cut at converting follow-users to typescript

--- listenbrainz/webserver/static/js/src/types.d.ts
+++ listenbrainz/webserver/static/js/src/types.d.ts
@@ -4,6 +4,7 @@ declare module "time-ago";
 
 // TODO: make this type specific
 declare type Listen = any;
+declare type ListenBrainzUser = any;
 
 declare type ListenType = "single" | "playingNow" | "import";
 
@@ -46,6 +47,34 @@ declare type User = {
 };
 
 declare type LastFmScrobblePage = {
+declare type FollowUsersPlayingNow = any;
+
+declare interface ImporterProps {
+  user: {
+    id?: string;
+    name: string;
+    auth_token: string;
+  };
+  profileUrl?: string;
+  apiUrl?: string;
+  lastfmApiUrl: string;
+  lastfmApiKey: string;
+}
+
+declare interface ImporterState {
+  show: boolean;
+  canClose: boolean;
+  lastfmUsername: string;
+  msg: string;
+}
+
+declare interface ModalProps {
+  disable: boolean;
+  children: React.ReactElement[];
+  onClose(event: React.MouseEvent<HTMLButtonElement>): void;
+}
+
+declare interface LastFmScrobblePage {
   recenttracks: {
     track: any;
   };
