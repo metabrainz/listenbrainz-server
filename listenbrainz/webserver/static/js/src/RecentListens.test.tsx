a5a93cab6ee532e29f1fac06af11e51ef4a66343
Ishaan Shah <ishaanshah@users.noreply.github.com>
2020-04-14 21:45:01 +0530

    Fix RecentListens snapshot test (#807)
    * Fix snapshot test
    
    * Change to shallow rendering

--- listenbrainz/webserver/static/js/src/RecentListens.test.tsx
+++ listenbrainz/webserver/static/js/src/RecentListens.test.tsx
@@ -1,5 +1,6 @@
 import * as React from "react";
-import { mount } from "enzyme";
+import { shallow } from "enzyme";
+import * as timeago from "time-ago";
 
 import * as recentListensProfilePageProps from "./__mocks__/recentListensProfilePageProps.json";
 
@@ -40,11 +41,9 @@ const props = {
 };
 
 describe("RecentListens", () => {
-  // this test fails because we show relative times for listens ("x days ago")
-  // which means that the snapshot changes with time
-  // TODO: fix this
-  it.skip("renders correctly on the profile page", () => {
-    const wrapper = mount(<RecentListens {...props} />);
+  it("renders correctly on the profile page", () => {
+    timeago.ago = jest.fn().mockImplementation(() => "1 day ago");
+    const wrapper = shallow(<RecentListens {...props} />);
     expect(wrapper.html()).toMatchSnapshot();
   });
 });
