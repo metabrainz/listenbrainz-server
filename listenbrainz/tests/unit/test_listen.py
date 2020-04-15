f8fc797502efced58e64362bbbb6c766c1f5cbaf
shivam-kapila <shivamkapila4@gmail.com>
2020-04-15 17:57:26 +0530

    Fix test_from_timescale test

--- listenbrainz/tests/unit/test_listen.py
+++ listenbrainz/tests/unit/test_listen.py
@@ -7,65 +7,76 @@ import uuid
 
 class ListenTestCase(unittest.TestCase):
 
-    def test_from_influx(self):
-        """ Test for the from_influx method """
+    def test_from_timescale(self):
+        """ Test for the from_timescale method """
 
-        influx_row = {
-            "time": "2017-06-07T17:23:05Z",
-            "artist_mbids": "abaa7001-0d80-4e58-be5d-d2d246fd9d87",
-            "artist_msid": "aa6130f2-a12d-47f3-8ffd-d0f71340de1f",
-            "artist_name": "Majid Jordan",
-            "best_song": "definitely",
-            "genius_link": "https://genius.com/Majid-jordan-every-step-every-way-lyrics",
-            "lastfm_link": "https://www.last.fm/music/Majid+Jordan/_/Every+Step+Every+Way",
-            "other_stuff": "teststuffplsignore",
-            "recording_mbid": None,
-            "recording_msid": "db9a7483-a8f4-4a2c-99af-c8ab58850200",
-            "release_msid": "cf138a00-05d5-4b35-8fce-181efcc15785",
-            "release_name": "Majid Jordan",
-            "track_name": "Every Step Every Way",
+        timescale_row = {
+            "listened_at": 1525557084,
             "user_name": "iliekcomputers",
-            "we_dict_now.hello": "afb",
-            "we_dict_now.we_nested_now.hi": "312",
-            "tags": "sing, song",
-            "inserted_timestamp": 1525557084,
+            "recording_msid": "db9a7483-a8f4-4a2c-99af-c8ab58850200",
+            "data": {
+                'track_metadata': {
+                    "track_name": "Every Step Every Way",
+                    "artist_name": "Majid Jordan",
+                    "release_name": "Majid Jordan",
+                    'additional_info': {
+                        "artist_msid": "aa6130f2-a12d-47f3-8ffd-d0f71340de1f",
+                        "artist_mbids": ["abaa7001-0d80-4e58-be5d-d2d246fd9d87"],
+                        "release_msid": "cf138a00-05d5-4b35-8fce-181efcc15785",
+                        'release_mbid': '8294645a-f996-44b6-9060-7f189b9f59f3',
+                        "recording_mbid": None,
+                        "tags": ["sing, song"],
+                        "best_song": "definitely",
+                        "genius_link": "https://genius.com/Majid-jordan-every-step-every-way-lyrics",
+                        "lastfm_link": "https://www.last.fm/music/Majid+Jordan/_/Every+Step+Every+Way",
+                        "other_stuff": "teststuffplsignore",
+                        "we_dict_now.hello": "afb",
+                        "we_dict_now.we_nested_now.hi": "312"
+                    }
+                },
+                "user_id": 1
+            }
         }
 
-        listen = Listen.from_influx(influx_row)
+        listen = Listen.from_timescale(
+            timescale_row['listened_at'], timescale_row['recording_msid'], timescale_row['user_name'], timescale_row['data'])
 
         # Check user name
-        self.assertEqual(listen.user_name, influx_row['user_name'])
+        self.assertEqual(listen.user_name, timescale_row['user_name'])
 
         # Check time stamp
-        dt = datetime.strptime(influx_row['time'] , '%Y-%m-%dT%H:%M:%SZ')
-        ts = int(dt.strftime("%s"))
+        ts = timescale_row['listened_at']
         self.assertEqual(listen.ts_since_epoch, ts)
 
         # Check artist mbids
-        self.assertIsInstance(listen.data['additional_info']['artist_mbids'], list)
-        self.assertEqual(listen.data['additional_info']['artist_mbids'], influx_row['artist_mbids'].split(','))
+        self.assertEqual(listen.data['additional_info']['artist_mbids'],
+                         timescale_row['data']['track_metadata']['additional_info']['artist_mbids'])
 
         # Check tags
-        self.assertIsInstance(listen.data['additional_info']['tags'], list)
-        self.assertEqual(listen.data['additional_info']['tags'], influx_row['tags'].split(','))
+        self.assertEqual(listen.data['additional_info']['tags'], timescale_row['data']
+                         ['track_metadata']['additional_info']['tags'])
 
         # Check track name
-        self.assertEqual(listen.data['track_name'], influx_row['track_name'])
+        self.assertEqual(listen.data['track_name'], timescale_row['data']
+                         ['track_metadata']['track_name'])
 
         # Check additional info
-        self.assertEqual(listen.data['additional_info']['best_song'], influx_row['best_song'])
+        self.assertEqual(listen.data['additional_info']['best_song'],
+                         timescale_row['data']['track_metadata']['additional_info']['best_song'])
 
         # Check msids
-        self.assertEqual(listen.artist_msid, influx_row['artist_msid'])
-        self.assertEqual(listen.release_msid, influx_row['release_msid'])
-        self.assertEqual(listen.recording_msid, influx_row['recording_msid'])
+        self.assertEqual(
+            listen.artist_msid, timescale_row['data']['track_metadata']['additional_info']['artist_msid'])
+        self.assertEqual(
+            listen.release_msid, timescale_row['data']['track_metadata']['additional_info']['release_msid'])
+        self.assertEqual(listen.recording_msid, timescale_row['recording_msid'])
 
         # make sure additional info does not contain stuff like artist names, track names
         self.assertNotIn('track_name', listen.data['additional_info'])
         self.assertNotIn('artist_name', listen.data['additional_info'])
         self.assertNotIn('release_name', listen.data['additional_info'])
 
-    def test_to_influx(self):
+    def test_to_timescale(self):
         listen = Listen(
             timestamp=int(time.time()),
             user_name='testuser',
@@ -81,7 +92,7 @@ class ListenTestCase(unittest.TestCase):
             }
         )
 
-        data = listen.to_influx(listen.user_name)
+        data = listen.to_timescale(listen)
 
         # Make sure every value that we don't explicitly support is a string
         for key in data['fields']:
