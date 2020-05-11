9f1a19f4a3df6af740e557b39064b8ea0e5ace28
vansika <vansikapareek2001@gmail.com>
2020-05-11 11:25:57 +0530

    put copy and show/hide button in same line

--- listenbrainz/webserver/static/js/info.js
+++ listenbrainz/webserver/static/js/info.js
@@ -1,13 +1,29 @@
-document.getElementById("copy-token").addEventListener("click", function() {
-    var copyText = document.getElementById("auth-token");
-    copyText.type = 'text';
-    copyText.select();
-    document.execCommand("copy");
-    copyText.type = 'hidden';
+document.getElementById("show-hide-token").addEventListener("click", function() {
+    var token = document.getElementById("auth-token");
+    var btn = document.getElementById("show-hide-token");
+
+    if (token.type === 'password'){
+        token.type = 'text';
+        btn.className = 'btn btn-info glyphicon glyphicon-eye-close';
+        btn.title = "Hide user token";
+    } else{
+        token.type = 'password';
+        btn.className = 'btn btn-info glyphicon glyphicon-eye-open';
+        btn.title = "Show user token";
+    }
 });
 
-$(document).ready(function () {
-    $('.glyphicon-dropdown').click(function () {
-        $(this).toggleClass("glyphicon-chevron-down").toggleClass("glyphicon-chevron-up");
-    });
+function copy(token) {
+    token.select();
+    document.execCommand("copy");
+}
+
+document.getElementById("copy-token").addEventListener("click", function() {
+    var token = document.getElementById("auth-token");
+    if (token.type === 'password'){
+        token.type = 'text';
+        copy(token);
+        token.type = 'password';
+    }
+    copy(token);
 });
