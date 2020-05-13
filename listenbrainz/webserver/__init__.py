b65e0b014987f0dac67483f7beded49ce9519bb9
Monkey Do <MonkeyDo@users.noreply.github.com>
2020-05-13 18:24:48 +0200

    BrainzPlayer page (#847)
    * Add /playlist view (POST)
    
    * Playlist page accepting POST request
    
    * Rename /playlist to /player
    
    * Better errors in /player endpoint
    
    * Pep8 altar sacrifice

--- listenbrainz/webserver/__init__.py
+++ listenbrainz/webserver/__init__.py
@@ -242,11 +242,13 @@ def _register_blueprints(app):
     from listenbrainz.webserver.views.follow_api import follow_api_bp
     from listenbrainz.webserver.views.stats_api import stats_api_bp
     from listenbrainz.webserver.views.status_api import status_api_bp
+    from listenbrainz.webserver.views.player import player_bp
     app.register_blueprint(index_bp)
     app.register_blueprint(login_bp, url_prefix='/login')
     app.register_blueprint(user_bp, url_prefix='/user')
     app.register_blueprint(profile_bp, url_prefix='/profile')
     app.register_blueprint(follow_bp, url_prefix='/follow')
+    app.register_blueprint(player_bp, url_prefix='/player')
     app.register_blueprint(api_bp, url_prefix=API_PREFIX)
     app.register_blueprint(follow_api_bp, url_prefix=API_PREFIX+'/follow')
     app.register_blueprint(stats_api_bp, url_prefix=API_PREFIX+'/stats')
