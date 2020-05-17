e0338ef0b259aec557132f52119dfba9fbc46b14
Param Singh <iliekcomputers@gmail.com>
2020-05-17 11:00:25 +0100

    Add skip to a test I missed and add TODOs

--- messybrainz/db/tests/test_artist.py
+++ messybrainz/db/tests/test_artist.py
@@ -152,7 +152,7 @@ class ArtistTestCase(DatabaseTestCase):
 
 
     @patch('messybrainz.db.artist.fetch_artist_mbids')
-    @unittest.skip("test fails with pg12 upgrade, this isn't used in prod anywhere")
+    @unittest.skip('This test fails with the PG12 upgrade, is not run in prod') #TODO: fix or remove me
     def test_fetch_and_store_artist_mbids_for_all_recording_mbids(self, mock_fetch_artist_mbids):
         """Test if artist MBIDs are fetched and stored correctly for all recording MBIDs
            not in recording_artist_join table.
@@ -528,7 +528,7 @@ class ArtistTestCase(DatabaseTestCase):
                 [UUID("88a8d8a9-7c9b-4f7b-8700-7f0f7a503688")], [UUID("b49a9595-3576-44bb-8ac0-e26d3f5b42ff")]
             ])
 
-    @unittest.skip("test fails with pg12 upgrade, this isn't used in prod anywhere")
+    @unittest.skip('This test fails with the PG12 upgrade, is not run in prod') #TODO: fix or remove me
     def test_create_artist_credit_clusters(self):
         """Tests if artist_credit clusters are correctly formed."""
 
@@ -704,7 +704,7 @@ class ArtistTestCase(DatabaseTestCase):
             ])
             self.assertSetEqual(set(gids), gids_from_data)
 
-    @unittest.skip("This test broke with PG12 upgrade, code is not used in prod")
+    @unittest.skip('This test fails with the PG12 upgrade, is not run in prod') #TODO: fix or remove me
     def test_fetch_artist_mbids_left_to_cluster_from_recording_artist_join(self):
         """ Tests if artist MBIDs left to add to artist_credit_redirect table are
             fetched correctly.
@@ -787,7 +787,7 @@ class ArtistTestCase(DatabaseTestCase):
             )
             self.assertDictEqual(recording_1, recordings[0])
 
-    @unittest.skip("This test broke with PG12 upgrade, code is not used in prod")
+    @unittest.skip('This test fails with the PG12 upgrade, is not run in prod') #TODO: fix or remove me
     def test_create_clusters_using_fetched_artist_mbids_without_anomalies(self):
         """ Tests if artist clusters are created correctly without considering anomalies."""
 
@@ -876,7 +876,7 @@ class ArtistTestCase(DatabaseTestCase):
             artist_mbids = artist.get_artist_mbids_using_msid(connection, gid_from_data)
             self.assertListEqual(artist_mbids, [[UUID("88a8d8a9-7c9b-4f7b-8700-7f0f7a503688")]])
 
-    @unittest.skip("This test broke with PG12 upgrade, code is not used in prod")
+    @unittest.skip('This test fails with the PG12 upgrade, is not run in prod') #TODO: fix or remove me
     def test_create_clusters_using_fetched_artist_mbids_for_anomalies(self):
         """ Tests if artist clusters are correctly formed for anomalies."""
 
@@ -938,7 +938,7 @@ class ArtistTestCase(DatabaseTestCase):
                 [UUID("88a8d8a9-7c9b-4f7b-8700-7f0f7a503688")], [UUID("b49a9595-3576-44bb-8ac0-e26d3f5b42ff")]
             ])
 
-
+    @unittest.skip('This test fails with the PG12 upgrade, is not run in prod') #TODO: fix or remove me
     def test_create_clusters_using_fetched_artist_mbids(self):
         """ Tests if clusters are created correctly using
             artist MBIDs from recording_artist_join table.
