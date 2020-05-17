b9fcb89b25ae97dd9684589871d674853fd77199
Vansika Pareek <vansika@metabrainz.org>
2020-05-17 18:00:40 +0530

    update recommendation schema to use recording mbid (#858)
    * use mbid in recommendation schema
    
    * rename recommendation.cf_recording schema col

--- admin/sql/create_tables.sql
+++ admin/sql/create_tables.sql
@@ -53,7 +53,7 @@ ALTER TABLE follow_list ADD CONSTRAINT follow_list_name_creator_key UNIQUE (name
 CREATE TABLE recommendation.cf_recording (
   id                  SERIAL, -- PK
   user_id             INTEGER NOT NULL, --FK to "user".id
-  recording_msid      UUID NOT NULL,
+  recording_mbid      UUID NOT NULL,
   type                cf_recording_type NOT NULL,
   created             TIMESTAMP WITH TIME ZONE DEFAULT NOW() NOT NULL
 );
