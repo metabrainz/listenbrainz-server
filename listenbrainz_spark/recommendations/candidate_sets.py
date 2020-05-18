da640a57bd87889a905c1cc0d2d8c3821e1c5ef9
Vansika Pareek <vansika@metabrainz.org>
2020-05-18 11:09:21 +0530

    Generate recommendations to send over lemmy (#859)
    * generate recommendations for all users
    
    * remove unused imports
    
    * update tests for recommend.py
    
    * delete recommend.html
    
    * name and identation fixes

--- listenbrainz_spark/recommendations/candidate_sets.py
+++ listenbrainz_spark/recommendations/candidate_sets.py
@@ -152,7 +152,7 @@ def get_top_artists_candidate_set(top_artists_df, recordings_df, users_df):
     df = top_artists_df.join(recordings_df, condition, 'inner')
 
     top_artists_candidate_set_df = df.join(users_df, 'user_name', 'inner')\
-                                     .select('user_id', 'recording_id', 'user_name')
+                                     .select('recording_id', 'user_id', 'user_name')
 
     return top_artists_candidate_set_df
 
@@ -178,7 +178,7 @@ def get_top_similar_artists_candidate_set(top_similar_artists_df, recordings_df,
     df = top_similar_artists_df.join(recordings_df, condition, 'inner')
 
     top_similar_artists_candidate_set_df = df.join(users_df, 'user_name', 'inner')\
-                                             .select('user_id', 'recording_id', 'user_name')
+                                             .select('recording_id', 'user_id', 'user_name')
 
     return top_similar_artists_candidate_set_df
 
