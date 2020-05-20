d3d5eef35f51cd001f9e84b0c9e5013231816889
shivam-kapila <shivamkapila4@gmail.com>
2020-05-20 23:44:58 +0530

    Add script to update table in production

--- admin/sql/updates/2020-05-19-add-lovehate-table.sql
+++ admin/sql/updates/2020-05-19-add-lovehate-table.sql
@@ -1,7 +1,7 @@
 BEGIN;
 
 -- Create new table
-CREATE TABLE recording_feedback (
+CREATE TABLE recording_lovehate (
     user_id                 INTEGER NOT NULL, -- FK to "user".id
     recording_msid          UUID NOT NULL,
     score                   SMALLINT NOT NULL,
@@ -9,9 +9,9 @@ CREATE TABLE recording_feedback (
 );
 
 -- Create primary key
-ALTER TABLE recording_feedback ADD CONSTRAINT recording_feedback_pkey PRIMARY KEY (id);
+ALTER TABLE recording_lovehate ADD CONSTRAINT recording_lovehate_pkey PRIMARY KEY (id);
 
 -- Create foreign key
-ALTER TABLE recording_feedback ADD CONSTRAINT recording_feedback_user_id_foreign_key FOREIGN KEY (user_id) REFERENCES "user" (id) ON DELETE CASCADE;
+ALTER TABLE recording_lovehate ADD CONSTRAINT recording_lovehate_user_id_foreign_key FOREIGN KEY (user_id) REFERENCES "user" (id) ON DELETE CASCADE;
 
 COMMIT;
