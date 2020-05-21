d3b86f46029b230dd86c5407ade6120c50f7dd4a
shivam-kapila <shivamkapila4@gmail.com>
2020-05-21 17:29:42 +0530

    Add validator for recording msid

--- listenbrainz/feedback.py
+++ listenbrainz/feedback.py
@@ -1,3 +1,5 @@
+import uuid
+
 from typing import List
 from pydantic import BaseModel, ValidationError, validator
 
@@ -12,3 +14,11 @@ class Feedback(BaseModel):
         if scr not in [-1, 0, 1]:
             raise ValueError('Score can have a value of 1, 0 or -1.')
         return scr
+
+    @validator('recording_msid')
+    def check_recording_msid_is_valid_uuid(cls, rec_msid):
+        try:
+            rec_msid = uuid.UUID(rec_msid)
+            return rec_msid
+        except (AttributeError, ValueError):
+            raise ValueError('Recording MSID must be a valid UUID')
