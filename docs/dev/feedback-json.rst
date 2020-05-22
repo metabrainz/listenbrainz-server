33c3ed5d6cd70cfbea9c766f8389fe01c5106033
shivam-kapila <shivamkapila4@gmail.com>
2020-05-22 23:44:39 +0530

    Add GET API endpoints

--- docs/dev/feedback-json.rst
+++ docs/dev/feedback-json.rst
@@ -3,17 +3,11 @@
 Feedback JSON Documentation
 ===========================
 
-.. note:: Do not submit copyrighted information in these fields!
-
 Submission JSON
 ---------------
 
 To submit a feedback via our API (see: :doc:`api`), ``POST`` a JSON document to
 the ``submit-feedback`` endpoint.
-
-   - The complete JSON document may not exceed :data:`~webserver.views.api.MAX_FEEDBACK_SIZE` 
-     bytes in size
-
 
 A sample feedback may look like::
 
@@ -42,35 +36,14 @@ Fetching feedback JSON
 
 The JSON documents returned from our API for user feedback look like the following::
 
-    {
-      "user_id": "-- the MusicBrainz ID of the user --",
-      "feedback": [
-        {
-        "recording_msid": "d23f4719-9212-49f0-ad08-ddbfbfc50d6f",
-        "score": 1
-        },
-        "-- more feedback data here ---"
-      ]
-    }
+[
+  {
+  "user_id": "-- the MusicBrainz ID of the user --",
+  "recording_msid": "d23f4719-9212-49f0-ad08-ddbfbfc50d6f",
+  "score": 1
+  },
+  "-- more feedback data here ---"
+]
 
-The ``user_id`` element contains the MusicBrainz ID of the user whose feedback are
-being returned. The other element is the ``feedback`` element. This is a list which contains
-the feedback JSON elements having a ``recording_msid`` and a ``score`` key.
-
-The JSON documents returned from our API for recording feedback look like the following::
-
-    {
-      "recording_msid": "d23f4719-9212-49f0-ad08-ddbfbfc50d6f",
-      "feedback": [
-        {
-        "user_id": "-- the MusicBrainz ID of the user --",
-        "score": 1
-        },
-        "-- more feedback data here ---"
-      ]
-    }
-
-The ``recording_msid`` element contains the MessyBrainz ID of the recording 
-whose feedback are being returned. The other element is the ``feedback`` element. 
-This is a list which contains the feedback JSON elements having a ``user_id`` 
-and a ``score`` key.
+This is a list which contains the feedback JSON elements having a ``user_id`` the MusicBrainz ID of the user,
+a ``recording_msid`` and a ``score`` key.
--- listenbrainz/webserver/views/feedback_api.py
+++ listenbrainz/webserver/views/feedback_api.py
@@ -10,15 +10,13 @@ from listenbrainz.webserver.errors import (APIBadRequest,
                                            APIServiceUnavailable,
                                            APIUnauthorized)
 from listenbrainz.webserver.rate_limiter import ratelimit
-from listenbrainz.webserver.views.api import _validate_auth_header
-from listenbrainz.webserver.views.api_tools import log_raise_400, DEFAULT_ITEMS_PER_GET, MAX_ITEMS_PER_GET
+from listenbrainz.webserver.views.api import _validate_auth_header, _parse_int_arg
+from listenbrainz.webserver.views.api_tools import log_raise_400, is_valid_uuid
 from listenbrainz.feedback import Feedback
 from pydantic import ValidationError
 
-#: Maximum overall feedback size in bytes, to prevent egregious spamming.
-MAX_FEEDBACK_SIZE = 10240
-
 feedback_api_bp = Blueprint('feedback_api_v1', __name__)
+
 
 @feedback_api_bp.route("submit-feedback", methods=["POST", "OPTIONS"])
 @crossdomain(headers="Authorization, Content-Type")
@@ -27,10 +25,6 @@ def submit_feedback():
     """
     Submit recording feedback (love/hate) to the server. A user token (found on  https://listenbrainz.org/profile/ )
     must be provided in the Authorization header! Each request should contain only one feedback in the payload.
-
-    Feedback should be submitted for tracks when the user has listened to them and they exist in their listen history.
-    If the user hasn't listened to the track or hasn't submitted it to ListenBrainz, it doesn't fully count as a feedback
-    and should not be submitted.
 
     For complete details on the format of the JSON to be POSTed to this endpoint, see :ref:`feedback-json-doc`.
 
@@ -42,25 +36,22 @@ def submit_feedback():
     """
     user = _validate_auth_header()
 
-    raw_data = request.get_data()
-    try:
-        data = ujson.loads(raw_data.decode("utf-8"))
-    except ValueError as e:
-        log_raise_400("Cannot parse JSON document: %s" % e, raw_data)
-
-    if len(raw_data) > MAX_FEEDBACK_SIZE:
-        log_raise_400("JSON document is too large. In aggregate, feedback may not "
-                        "be larger than %d characters." % MAX_FEEDBACK_SIZE, data)
+    data = request.json
+    
+    if 'recording_msid' not in data or 'score' not in data:
+        log_raise_400("JSON document must contain recording_msid and "
+                      "score top level keys", data)
 
     if 'recording_msid' in data and 'score' in data and len(data) > 2:
         log_raise_400("JSON document may only contain recording_msid and "
                       "score top level keys", data)
-                    
+
     try:
         feedback = Feedback(user_id=user["id"], recording_msid=data["recording_msid"], score=data["score"])
     except ValidationError as e:
-        log_raise_400("Invalid JSON document submitted: %s" % e, raw_data)
-    
+        log_raise_400("Invalid JSON document submitted: %s" % str(e).replace("\n ", ":").replace("\n", " "),
+                      data) #  str.replace() to tidy up the error message
+
     try:
         if feedback.score == 0:
             db_feedback.delete(feedback)
@@ -72,3 +63,63 @@ def submit_feedback():
         raise APIInternalServerError("Something went wrong. Please try again.")
 
     return jsonify({'status': 'ok'})
+
+
+@feedback_api_bp.route("/user/<user_name>/get-feedback", methods=["GET"])
+@crossdomain()
+@ratelimit()
+def get_feedback_for_user(user_name):
+    """
+    Get feedback given by user ``user_name``. The format for the JSON returned is defined in our :ref:`feedback-json-doc`.
+
+    If the optional argument ``score`` is not given, this endpoint will return all the feedback submitted by the user.
+    Otherwise filters the feedback to be returned by score.
+
+    :param score: If 1 then returns the loved recordings, if -1 returns hated recordings.
+    :statuscode 200: Yay, you have data!
+    :resheader Content-Type: *application/json*
+    """
+
+    score = _parse_int_arg('score')
+
+    user = db_user.get_by_mb_id(user_name)
+    if user is None:
+        raise APINotFound("Cannot find user: %s" % user_name)
+
+    if score is None:
+        feedback = db_feedback.get_feedback_by_user_id(user["id"])
+    else:
+        if score in [-1, 1]:
+            feedback = db_feedback.get_feedback_by_user_id_and_score(user["id"], score)
+        else:
+            log_raise_400("Score can have a value of 1 or -1.", request.args)
+
+    for i,fb in enumerate(feedback):
+        fb.user_id = user_name
+        feedback[i] = fb.__dict__
+
+    return jsonify(feedback)
+
+
+@feedback_api_bp.route("/recording/<recording_msid>/get-feedback", methods=["GET"])
+@crossdomain()
+@ratelimit()
+def get_feedback_for_recording(recording_msid):
+    """
+    Get feedback for recording with given ``recording_msid``. The format for the JSON returned
+    is defined in our :ref:`feedback-json-doc`.
+
+    :statuscode 200: Yay, you have data!
+    :resheader Content-Type: *application/json*
+    """
+
+    if not is_valid_uuid(recording_msid):
+            log_raise_400("%s MSID format invalid." % recording_msid)
+
+    feedback = db_feedback.get_feedback_by_recording_msid(recording_msid)
+
+    for i,fb in enumerate(feedback):
+        fb.user_id = db_user.get(fb.user_id)["musicbrainz_id"]
+        feedback[i] = fb.__dict__
+
+    return jsonify(feedback)
