7d28c77454e9208e6c8be65e5fac2d695a5d7e84
Shivam Kapila <shivamkapila4@gmail.com>
2020-05-25 21:55:31 +0530

    Add python module to interact with lovehate table (#866)
    * Add python module to interact with lovehate table
    
    * PEP8 fixes
    
    * Update table name to recording_lovehate
    
    * Improve the functions
    
    * Improve the functions
    
    * Add test data
    
    * Add tests
    
    * PEP8 fixes
    
    * Add types
    
    * Add Feedback class
    
    * Update tests
    
    * PEP-8 fixes
    
    * Add validator for recording msid
    
    * Return string instead of UUID
    
    * remove redundant code and improve documentation

---
+++ listenbrainz/db/feedback.py
@@ -0,0 +1,110 @@
+import sqlalchemy
+
+from listenbrainz import db
+from listenbrainz.feedback import Feedback
+
+
+def insert(feedback: Feedback):
+    """ Inserts a feedback record for a user's loved/hated recording into the database.
+        If the record is already present for the user, the score is updated to the new
+        value passed.
+        Args:
+            feedback: An object of class Feedback
+    """
+
+    with db.engine.connect() as connection:
+        connection.execute(sqlalchemy.text("""
+            INSERT INTO recording_feedback (user_id, recording_msid, score)
+                 VALUES (:user_id, :recording_msid, :score)
+            ON CONFLICT (user_id, recording_msid)
+          DO UPDATE SET score = :score,
+                        created = NOW()
+            """), {
+                'user_id': feedback.user_id,
+                'recording_msid': feedback.recording_msid,
+                'score': feedback.score,
+            }
+        )
+
+
+def delete(feedback: Feedback):
+    """ Deletes the feedback record for a given recording for the user from the database.
+        Args:
+            feedback: An object of class Feedback
+    """
+
+    with db.engine.connect() as connection:
+        connection.execute(sqlalchemy.text("""
+            DELETE FROM recording_feedback
+             WHERE user_id = :user_id
+               AND recording_msid = :recording_msid
+            """), {
+                'user_id': feedback.user_id,
+                'recording_msid': feedback.recording_msid,
+            }
+        )
+
+
+def get_feedback_by_user_id(user_id: int):
+    """ Get a list of recording feedbacks given by the user.
+        Args:
+            user_id: the row ID of the user in the DB
+        Returns:
+            A list of Feedback objects
+    """
+
+    with db.engine.connect() as connection:
+        result = connection.execute(sqlalchemy.text("""
+            SELECT user_id, recording_msid::text, score
+              FROM recording_feedback
+          ORDER BY created DESC
+            """), {
+                'user_id': user_id
+            }
+        )
+        return [Feedback(**dict(row)) for row in result.fetchall()]
+
+
+def get_feedback_by_user_id_and_score(user_id: int, score: int):
+    """ Get a list of recording feedbacks of particular type (loved or hated) given by the user.
+        If score is +1 then get loved recordings, if -1 then get hated recordings.
+        Args:
+            user_id: the row ID of the user
+        Returns:
+            A list of Feedback objects
+    """
+
+    with db.engine.connect() as connection:
+        result = connection.execute(sqlalchemy.text("""
+            SELECT user_id, recording_msid::text, score
+              FROM recording_feedback
+             WHERE user_id = :user_id
+               AND score = :score
+          ORDER BY created DESC
+            """), {
+                'user_id': user_id,
+                'score': score
+            }
+        )
+        return [Feedback(**dict(row)) for row in result.fetchall()]
+
+
+def get_feedback_by_recording_msid(recording_msid: str):
+    """ Get a list of feedbacks for a given recording.
+        Args:
+            recording_msid: the MessyBrainz ID of the recording
+        Returns:
+            A list of Feedback objects
+    """
+
+    with db.engine.connect() as connection:
+        result = connection.execute(sqlalchemy.text("""
+            SELECT user_id, recording_msid::text, score
+              FROM recording_feedback
+             WHERE recording_msid = :recording_msid
+          ORDER BY created DESC
+            """), {
+                'recording_msid': recording_msid
+            }
+        )
+        return [Feedback(**dict(row)) for row in result.fetchall()]
