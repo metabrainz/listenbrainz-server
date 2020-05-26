542ed78d3308152315b2e8e35acab4a41502f39f
Ishaan Shah <ishaan.n.shah@gmail.com>
2020-05-26 09:38:59 +0530

    Fix spark tests

--- listenbrainz_spark/stats/user/tests/test_all.py
+++ listenbrainz_spark/stats/user/tests/test_all.py
@@ -1,19 +1,20 @@
-from unittest.mock import patch
+from unittest.mock import patch, call
 
 from listenbrainz_spark.tests import SparkTestCase
 from listenbrainz_spark.stats.user.all import calculate
 
 
-@patch('listenbrainz_spark.stats.user.artist.get_artists_week')
-@patch('listenbrainz_spark.stats.user.artist.get_artists_month')
-@patch('listenbrainz_spark.stats.user.artist.get_artists_year')
-@patch('listenbrainz_spark.stats.user.artist.get_artists_all_time')
+@patch('listenbrainz_spark.stats.user.entity.get_entity_week')
+@patch('listenbrainz_spark.stats.user.entity.get_entity_month')
+@patch('listenbrainz_spark.stats.user.entity.get_entity_year')
+@patch('listenbrainz_spark.stats.user.entity.get_entity_all_time')
 class UserStatsAllTestCase(SparkTestCase):
-    def test_calculate(self, mock_get_artists_all_time, mock_get_artists_year,
-                       mock_get_artists_month, mock_get_artists_week):
+    def test_calculate(self, mock_get_entity_all_time, mock_get_entity_year,
+                       mock_get_entity_month, mock_get_entity_week):
         calculate()
 
-        mock_get_artists_week.assert_called_once()
-        mock_get_artists_month.assert_called_once()
-        mock_get_artists_year.assert_called_once()
-        mock_get_artists_all_time.assert_called_once()
+        calls = [call('artists'), call('releases')]
+        mock_get_entity_week.assert_has_calls(calls, any_order=True)
+        mock_get_entity_month.assert_has_calls(calls, any_order=True)
+        mock_get_entity_year.assert_has_calls(calls, any_order=True)
+        mock_get_entity_all_time.assert_has_calls(calls, any_order=True)
