0c1cf74ee2ac797bb01bb97e4df3884bc40b0e66
shivam-kapila <shivamkapila4@gmail.com>
2020-05-29 23:30:48 +0530

    Fix tests

--- listenbrainz/db/tests/test_feedback.py
+++ listenbrainz/db/tests/test_feedback.py
@@ -12,7 +12,7 @@ class FeedbackDatabaseTestCase(DatabaseTestCase):
 
     def setUp(self):
         DatabaseTestCase.setUp(self)
-        self.user = db_user.get_or_create(1, 'recording_feedback_user')
+        self.user = db_user.get_or_create(1, "recording_feedback_user")
 
         self.sample_feedback = [
             {
@@ -21,16 +21,6 @@ class FeedbackDatabaseTestCase(DatabaseTestCase):
             },
             {
                 "recording_msid": "222eb00d-9ead-42de-aec9-8f8c1509413d",
-                "score": 1
-            }
-        ]
-        self.sample_feedback_neg_score = [
-            {
-                "recording_msid": "d23f4719-9212-49f0-ad08-ddbfbfc50d6f",
-                "score": -1
-            },
-            {
-                "recording_msid": "31c2af1a-9968-47d3-b7a1-4a40d3a25f4f",
                 "score": -1
             }
         ]
@@ -38,19 +28,6 @@ class FeedbackDatabaseTestCase(DatabaseTestCase):
     def insert_test_data(self, user_id, neg_score=False):
         """ Insert test data into the database """
 
-        if neg_score:
-            # Insert the two records with negative score
-            for fb_neg in self.sample_feedback_neg_score:
-                db_feedback.insert(
-                    Feedback(
-                        user_id=user_id,
-                        recording_msid=fb_neg["recording_msid"],
-                        score=fb_neg["score"]
-                    )
-                )
-            return
-
-        # Insert the two records with positive score
         for fb in self.sample_feedback:
             db_feedback.insert(
                 Feedback(
@@ -59,93 +36,154 @@ class FeedbackDatabaseTestCase(DatabaseTestCase):
                     score=fb["score"]
                 )
             )
+        
+        return len(self.sample_feedback)
 
     def test_insert(self):
-        self.insert_test_data(self.user['id'])
-        result = db_feedback.get_feedback_by_user_id(self.user['id'])
-        self.assertEqual(len(result), 2)
+        count = self.insert_test_data(self.user["id"])
+        result = db_feedback.get_feedback_for_user(user_id=self.user["id"], limit=25, offset=0)
+        self.assertEqual(len(result), count)
 
     def test_update_score_when_feedback_already_exits(self):
-        self.insert_test_data(self.user['id'])
-        result = db_feedback.get_feedback_by_user_id(self.user['id'])
-        self.assertEqual(len(result), 2)
+        update_fb = self.sample_feedback[0]
 
-        # insert the negative score records, where 1 recording_msid already exists
-        # with postive score for the given user
-        self.insert_test_data(self.user['id'], neg_score=True)
-        result = db_feedback.get_feedback_by_user_id(self.user['id'])
-        self.assertEqual(len(result), 3)
+        count = self.insert_test_data(self.user["id"])
+        result = db_feedback.get_feedback_for_user(user_id=self.user["id"], limit=25, offset=0)
+        self.assertEqual(len(result), count)
+
+        self.assertEqual(result[1].recording_msid, update_fb["recording_msid"])
+        self.assertEqual(result[1].score, 1)
+
+        update_fb["score"] = -1  # change the score to -1
+
+        # update a record by inserting a record with updated score value
+        db_feedback.insert(
+            Feedback(
+                user_id=self.user["id"],
+                recording_msid=update_fb["recording_msid"],
+                score=update_fb["score"]
+            )
+        )
+
+        result = db_feedback.get_feedback_for_user(user_id=self.user["id"], limit=25, offset=0)
+        self.assertEqual(len(result), count)
+
+        self.assertEqual(result[0].recording_msid, update_fb["recording_msid"])
+        self.assertEqual(result[0].score, -1)
 
     def test_delete(self):
         del_fb = self.sample_feedback[0]
 
-        self.insert_test_data(self.user['id'])
-        result = db_feedback.get_feedback_by_user_id(self.user['id'])
-        self.assertEqual(len(result), 2)
+        count = self.insert_test_data(self.user["id"])
+        result = db_feedback.get_feedback_for_user(user_id=self.user["id"], limit=25, offset=0)
+        self.assertEqual(len(result), count)
         self.assertEqual(result[1].recording_msid, del_fb["recording_msid"])
 
         # delete one record for the user
         db_feedback.delete(
             Feedback(
-                user_id=self.user['id'],
+                user_id=self.user["id"],
                 recording_msid=del_fb["recording_msid"],
                 score=del_fb["score"]
             )
         )
 
-        result = db_feedback.get_feedback_by_user_id(self.user['id'])
+        result = db_feedback.get_feedback_for_user(user_id=self.user["id"], limit=25, offset=0)
         self.assertEqual(len(result), 1)
 
         self.assertNotEqual(result[0].recording_msid, del_fb["recording_msid"])
 
-    def test_get_feedback_by_user_id(self):
-        self.insert_test_data(self.user['id'])
-        result = db_feedback.get_feedback_by_user_id(self.user['id'])
-        self.assertEqual(len(result), 2)
+    def test_get_feedback_for_user(self):
+        count = self.insert_test_data(self.user["id"])
+        result = db_feedback.get_feedback_for_user(user_id=self.user["id"], limit=25, offset=0)
+        self.assertEqual(len(result), count)
 
+        self.assertEqual(result[0].user_id, self.user["id"])
+        self.assertEqual(result[0].user_name, self.user["musicbrainz_id"])
+        self.assertEqual(result[0].recording_msid, self.sample_feedback[1]["recording_msid"])
+        self.assertEqual(result[0].score, self.sample_feedback[1]["score"])
+
+        self.assertEqual(result[1].user_id, self.user["id"])
+        self.assertEqual(result[1].user_name, self.user["musicbrainz_id"])
+        self.assertEqual(result[1].recording_msid, self.sample_feedback[0]["recording_msid"])
+        self.assertEqual(result[1].score, self.sample_feedback[0]["score"])
+
+        # test the score argument
+        result = db_feedback.get_feedback_for_user(user_id=self.user["id"], limit=25, offset=0, score=1)
+        self.assertEqual(len(result), 1)
         self.assertEqual(result[0].score, 1)
-        self.assertEqual(result[1].score, 1)
 
-        # insert the negative score records
-        self.insert_test_data(self.user['id'], neg_score=True)
-        result = db_feedback.get_feedback_by_user_id(self.user['id'])
-        self.assertEqual(len(result), 3)
-
+        result = db_feedback.get_feedback_for_user(user_id=self.user["id"], limit=25, offset=0, score=-1)
+        self.assertEqual(len(result), 1)
         self.assertEqual(result[0].score, -1)
-        self.assertEqual(result[1].score, -1)
-        self.assertEqual(result[2].score, 1)
 
-    def test_get_feedback_by_user_id_and_score(self):
-        self.insert_test_data(self.user['id'])
-        result = db_feedback.get_feedback_by_user_id_and_score(self.user['id'], 1)
-        self.assertEqual(len(result), 2)
+        # test the limit argument
+        result = db_feedback.get_feedback_for_user(user_id=self.user["id"], limit=1, offset=0)
+        self.assertEqual(len(result), 1)
 
-        self.assertEqual(result[0].score, 1)
-        self.assertEqual(result[1].score, 1)
+        # test the offset argument
+        result = db_feedback.get_feedback_for_user(user_id=self.user["id"], limit=25, offset=1)
+        self.assertEqual(len(result), 1)
 
-        # insert the negative score records
-        self.insert_test_data(self.user['id'], neg_score=True)
-        result = db_feedback.get_feedback_by_user_id_and_score(self.user['id'], -1)
-        self.assertEqual(len(result), 2)
+    def test_get_feedback_count_for_user(self):
+        count = self.insert_test_data(self.user["id"])
+        result = db_feedback.get_feedback_count_for_user(user_id=self.user["id"])
+        self.assertEqual(result, count)
 
-        self.assertEqual(result[0].score, -1)
-        self.assertEqual(result[1].score, -1)
-
-    def test_get_feedback_by_recording_msid(self):
+    def test_get_feedback_for_recording(self):
         fb_msid_1 = self.sample_feedback[0]["recording_msid"]
-        fb_msid_2 = self.sample_feedback[1]["recording_msid"]
 
-        self.insert_test_data(self.user['id'])
-
-        result = db_feedback.get_feedback_by_recording_msid(fb_msid_1)
-        self.assertEqual(len(result), 1)
-        result = db_feedback.get_feedback_by_recording_msid(fb_msid_2)
+        self.insert_test_data(self.user["id"])
+        result = db_feedback.get_feedback_for_recording(recording_msid=fb_msid_1, limit=25, offset=0)
         self.assertEqual(len(result), 1)
 
-        user2 = db_user.get_or_create(2, 'recording_feedback_other_user')
-        self.insert_test_data(user2['id'])
+        self.assertEqual(result[0].user_id, self.user["id"])
+        self.assertEqual(result[0].user_name, self.user["musicbrainz_id"])
+        self.assertEqual(result[0].recording_msid, fb_msid_1)
+        self.assertEqual(result[0].score, self.sample_feedback[0]["score"])
 
-        result = db_feedback.get_feedback_by_recording_msid(fb_msid_1)
+        user2 = db_user.get_or_create(2, "recording_feedback_other_user")
+        self.insert_test_data(user2["id"])
+
+        result = db_feedback.get_feedback_for_recording(recording_msid=fb_msid_1, limit=25, offset=0)
         self.assertEqual(len(result), 2)
-        result = db_feedback.get_feedback_by_recording_msid(fb_msid_2)
+
+        self.assertEqual(result[0].user_id, user2["id"])
+        self.assertEqual(result[0].user_name, user2["musicbrainz_id"])
+        self.assertEqual(result[0].recording_msid, fb_msid_1)
+        self.assertEqual(result[0].score, self.sample_feedback[0]["score"])
+
+        self.assertEqual(result[1].user_id, self.user["id"])
+        self.assertEqual(result[1].user_name, self.user["musicbrainz_id"])
+        self.assertEqual(result[1].recording_msid, fb_msid_1)
+        self.assertEqual(result[1].score, self.sample_feedback[0]["score"])
+
+        # test the score argument
+        result = db_feedback.get_feedback_for_recording(recording_msid=fb_msid_1, limit=25, offset=0, score=1)
         self.assertEqual(len(result), 2)
+        self.assertEqual(result[0].score, 1)
+        self.assertEqual(result[1].score, 1)
+
+        result = db_feedback.get_feedback_for_recording(recording_msid=fb_msid_1, limit=25, offset=0, score=-1)
+        self.assertEqual(len(result), 0)
+
+        # test the limit argument
+        result = db_feedback.get_feedback_for_recording(recording_msid=fb_msid_1, limit=1, offset=0)
+        self.assertEqual(len(result), 1)
+
+        # test the offset argument
+        result = db_feedback.get_feedback_for_recording(recording_msid=fb_msid_1, limit=25, offset=1)
+        self.assertEqual(len(result), 1)
+
+    def test_get_feedback_count_for_recording(self):
+        fb_msid_1 = self.sample_feedback[0]["recording_msid"]
+
+        self.insert_test_data(self.user["id"])
+        result = db_feedback.get_feedback_count_for_recording(recording_msid=fb_msid_1)
+        self.assertEqual(result, 1)
+
+        user2 = db_user.get_or_create(2, "recording_feedback_other_user")
+        self.insert_test_data(user2["id"])
+
+        result = db_feedback.get_feedback_count_for_recording(recording_msid=fb_msid_1)
+        self.assertEqual(result, 2)
