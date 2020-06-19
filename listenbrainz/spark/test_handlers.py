504be8139ea9f4c8d2231e48c8f17222756f0f54
Vansika Pareek <vansika@metabrainz.org>
2020-06-19 22:58:03 +0530

    send mapping and artist relation import request to spark cluster (#914)
    * update mapping downloader
    
    * remove mapping type
    
    * remove unused import
    
    * change func name
    
    * remove redundant func
    
    * send import request to cluster
    
    * add tests for import requests
    
    * PEP-8 fixes and some cleaning
    
    * update func names

--- listenbrainz/spark/test_handlers.py
+++ listenbrainz/spark/test_handlers.py
@@ -4,10 +4,13 @@ from unittest import mock
 
 from flask import current_app
 
-from listenbrainz.spark.handlers import (
-    handle_candidate_sets, handle_dataframes, handle_dump_imported,
-    handle_model, handle_recommendations, handle_user_entity,
-    is_new_cf_recording_recommendation_batch, is_new_user_stats_batch)
+from listenbrainz.spark.handlers import (handle_candidate_sets, handle_dataframes,
+                                         handle_dump_imported, handle_model,
+                                         handle_recommendations, handle_user_entity,
+                                         notify_mapping_import, notify_artist_relation_import,
+                                         is_new_cf_recording_recommendation_batch,
+                                         is_new_user_stats_batch)
+
 from listenbrainz.webserver import create_app
 
 from listenbrainz.db.model.user_artist_stat import UserArtistStatJson, UserArtistStatRange, UserArtistRecord
@@ -189,5 +192,49 @@ class HandlersTestCase(unittest.TestCase):
                 'total_time': '3.1',
                 'from_date': str(time),
                 'to_date': str(time)
+            })
+            mock_send_mail.assert_called_once()
+
+    @mock.patch('listenbrainz.spark.handlers.send_mail')
+    def test_notify_mapping_import(self, mock_send_mail):
+        with self.app.app_context():
+            time = datetime.now()
+            mapping_name = 'msid-mbid-mapping-with-matchable-20200603-202731.tar.bz2'
+
+            # testing, should not send a mail
+            self.app.config['TESTING'] = True
+            notify_mapping_import({
+                'imported_mapping': mapping_name,
+                'time': str(time),
+            })
+            mock_send_mail.assert_not_called()
+
+            # in prod now, should send it
+            self.app.config['TESTING'] = False
+            notify_mapping_import({
+                'imported_mapping': mapping_name,
+                'time': str(time),
+            })
+            mock_send_mail.assert_called_once()
+
+    @mock.patch('listenbrainz.spark.handlers.send_mail')
+    def test_notify_artist_relation_import(self, mock_send_mail):
+        with self.app.app_context():
+            time = datetime.now()
+            artist_relation_name = 'artist-credit-artist-credit-relations-01-20191230-134806.tar.bz2'
+
+            # testing, should not send a mail
+            self.app.config['TESTING'] = True
+            notify_artist_relation_import({
+                'import_artist_relation': artist_relation_name,
+                'time': str(time),
+            })
+            mock_send_mail.assert_not_called()
+
+            # in prod now, should send it
+            self.app.config['TESTING'] = False
+            notify_artist_relation_import({
+                'import_artist_relation': artist_relation_name,
+                'time': str(time),
             })
             mock_send_mail.assert_called_once()
---
+++ listenbrainz_spark/request_consumer/jobs/tests/test_import_dump.py
@@ -0,0 +1,73 @@
+import unittest
+
+from unittest.mock import patch, MagicMock
+from listenbrainz_spark.utils import create_app
+from listenbrainz_spark.request_consumer.jobs.import_dump import (import_newest_full_dump_handler,
+                                                                  import_mapping_to_hdfs,
+                                                                  import_artist_relation_to_hdfs)
+
+
+class DumpImporterJobTestCase(unittest.TestCase):
+
+    @patch('ftplib.FTP')
+    @patch('listenbrainz_spark.ftp.download.ListenbrainzDataDownloader.download_listens')
+    @patch('listenbrainz_spark.hdfs.upload.ListenbrainzDataUploader.upload_listens')
+    @patch('listenbrainz_spark.request_consumer.jobs.import_dump.shutil.rmtree')
+    @patch('listenbrainz_spark.request_consumer.jobs.import_dump.tempfile')
+    def test_import_full_dump_handler(self, mock_temp, mock_rmtree, mock_upload, mock_download, mock_ftp_constructor):
+        mock_temp.mkdtemp.return_value = 'best_dir_ever'
+        mock_download.return_value = (MagicMock(), 'listenbrainz-listens-dump-20190101-000000-spark-full.tar.xz')
+        messages = import_newest_full_dump_handler()
+        mock_download.assert_called_once()
+        self.assertEqual(mock_download.call_args[1]['dump_type'], 'full')
+        self.assertEqual(mock_download.call_args[1]['directory'], 'best_dir_ever')
+        mock_upload.assert_called_once()
+        mock_rmtree.assert_called_once_with('best_dir_ever')
+
+        self.assertEqual(len(messages), 1)
+        self.assertEqual('listenbrainz-listens-dump-20190101-000000-spark-full.tar.xz', messages[0]['imported_dump'])
+
+    @patch('ftplib.FTP')
+    @patch('listenbrainz_spark.ftp.download.ListenbrainzDataDownloader.download_msid_mbid_mapping')
+    @patch('listenbrainz_spark.hdfs.upload.ListenbrainzDataUploader.upload_mapping')
+    @patch('listenbrainz_spark.request_consumer.jobs.import_dump.shutil.rmtree')
+    @patch('listenbrainz_spark.request_consumer.jobs.import_dump.tempfile')
+    def test_import_mapping_to_hdfs(self, mock_temp, mock_rmtree, mock_upload, mock_download, mock_ftp_constructor):
+        mock_temp.mkdtemp.return_value = 'fake_dir'
+        mock_download.return_value = ('download_dir', 'msid-mbid-mapping-with-matchable-20200603-202731.tar.bz2')
+        message = import_mapping_to_hdfs()
+
+        mock_download.assert_called_once()
+        self.assertEqual(mock_download.call_args[1]['directory'], 'fake_dir')
+
+        mock_upload.assert_called_once()
+        self.assertEqual(mock_upload.call_args[1]['archive'], 'download_dir')
+        self.assertEqual(mock_upload.call_args[1]['force'], True)
+
+        self.assertEqual(len(message), 1)
+        self.assertEqual(message[0]['imported_mapping'], 'msid-mbid-mapping-with-matchable-20200603-202731.tar.bz2')
+        self.assertTrue(message[0]['type'], 'import_mapping')
+        self.assertTrue('time' in message[0])
+
+    @patch('ftplib.FTP')
+    @patch('listenbrainz_spark.ftp.download.ListenbrainzDataDownloader.download_artist_relation')
+    @patch('listenbrainz_spark.hdfs.upload.ListenbrainzDataUploader.upload_artist_relation')
+    @patch('listenbrainz_spark.request_consumer.jobs.import_dump.shutil.rmtree')
+    @patch('listenbrainz_spark.request_consumer.jobs.import_dump.tempfile')
+    def test_import_artist_relation_to_hdfs(self, mock_temp, mock_rmtree, mock_upload, mock_download, mock_ftp_constructor):
+        mock_temp.mkdtemp.return_value = 'fake_dir'
+        mock_download.return_value = ('download_dir', 'artist-credit-artist-credit-relations-01-20191230-134806.tar.bz2')
+        message = import_artist_relation_to_hdfs()
+
+        mock_download.assert_called_once()
+        self.assertEqual(mock_download.call_args[1]['directory'], 'fake_dir')
+
+        mock_upload.assert_called_once()
+        self.assertEqual(mock_upload.call_args[1]['archive'], 'download_dir')
+        self.assertEqual(mock_upload.call_args[1]['force'], True)
+
+        self.assertEqual(len(message), 1)
+        self.assertEqual(message[0]['imported_artist_relation'],
+                         'artist-credit-artist-credit-relations-01-20191230-134806.tar.bz2')
+        self.assertTrue(message[0]['type'], 'import_artist_relation')
+        self.assertTrue('time' in message[0])
