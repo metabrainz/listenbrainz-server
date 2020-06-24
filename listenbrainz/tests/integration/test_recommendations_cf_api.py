83e8a4287bb9228e187bb134e59fc3ed9a7b07ab
vansika <vansikapareek2001@gmail.com>
2020-06-24 19:00:53 +0530

    tests for API endpoint

---
+++ listenbrainz/tests/integration/test_recommendations_cf_api.py
@@ -0,0 +1,90 @@
+import json
+from flask import url_for, current_app
+from redis import Redis
+
+import listenbrainz.db.user as db_user
+import listenbrainz.db.recommendations_cf_recording as db_recommendations_cf_recording
+from listenbrainz.tests.integration import IntegrationTestCase
+
+class CFRecommendationsViewsTestCase(IntegrationTestCase):
+    def setUp(self):
+        super(CFRecommendationsViewsTestCase, self).setUp()
+
+        self.user = db_user.get_or_create(1, 'vansika_1')
+        self.user2 = db_user.get_or_create(2, 'vansika_2')
+        self.user3 = db_user.get_or_create(3, 'vansika_3')
+        # insert recommendations
+        db_recommendations_cf_recording.insert_user_recommendation(1, ["526bd613-fddd-4bd6-9137-ab709ac74cab"], [])
+        db_recommendations_cf_recording.insert_user_recommendation(2, [], ["526bd613-fddd-4bd6-9137-ab709ac74cab"])
+
+        # get recommendations
+        self.user_recommendations = db_recommendations_cf_recording.get_user_recommendation(1)
+        self.user2_recommendations = db_recommendations_cf_recording.get_user_recommendation(2)
+
+    def tearDown(self):
+        r = Redis(host=current_app.config['REDIS_HOST'], port=current_app.config['REDIS_PORT'])
+        r.flushall()
+        super(CFRecommendationsViewsTestCase, self).tearDown()
+
+    def test_invalid_user(self):
+        response = self.client.get(url_for('recommendations_cf_recording_v1.get_recommendations', user_name='invalid_user'))
+        self.assert404(response)
+
+    def test_artist_type(self):
+        response = self.client.get(url_for('recommendations_cf_recording_v1.get_recommendations',
+                                           user_name=self.user['musicbrainz_id']))
+        self.assert400(response)
+
+        response = self.client.get(url_for('recommendations_cf_recording_v1.get_recommendations',
+                                           user_name=self.user['musicbrainz_id']), query_string={'type': 'invalid_type'})
+        self.assert400(response)
+
+    def test_inactive_user(self):
+        response = self.client.get(url_for('recommendations_cf_recording_v1.get_recommendations',
+                                           user_name=self.user3['musicbrainz_id']), query_string={'type': 'top'})
+        self.assertEqual(response.status_code, 204)
+
+
+    def test_top_artist_recommendations_not_generated(self):
+        response = self.client.get(url_for('recommendations_cf_recording_v1.get_recommendations',
+                                           user_name=self.user2['musicbrainz_id']), query_string={'type': 'top'})
+        self.assertEqual(response.status_code, 204)
+
+    def test_similar_artist_recommendations_not_generated(self):
+        response = self.client.get(url_for('recommendations_cf_recording_v1.get_recommendations',
+                                           user_name=self.user['musicbrainz_id']), query_string={'type': 'similar'})
+        self.assertEqual(response.status_code, 204)
+
+    def test_top_artist_recommendations(self):
+        response = self.client.get(url_for('recommendations_cf_recording_v1.get_recommendations',
+                                           user_name=self.user['musicbrainz_id']), query_string={'type': 'top'})
+        self.assert200(response)
+        data = json.loads(response.data)['payload']
+
+        received_user_name = data['user_name']
+        self.assertEqual(received_user_name, self.user['musicbrainz_id'])
+
+        received_ts = data['last_updated']
+        expected_ts = int(self.user_recommendations['created'].timestamp())
+        self.assertEqual(received_ts, expected_ts)
+
+        received_top_artist_recommendations = data['top_artist']['recording_mbid']
+        expected_top_artist_recommendations = self.user_recommendations['recording_mbid']['top_artist']
+        self.assertEqual(expected_top_artist_recommendations, received_top_artist_recommendations)
+
+    def test_similar_artist_recommendations(self):
+        response = self.client.get(url_for('recommendations_cf_recording_v1.get_recommendations',
+                                           user_name=self.user2['musicbrainz_id']), query_string={'type': 'similar'})
+        self.assert200(response)
+        data = json.loads(response.data)['payload']
+
+        received_user_name = data['user_name']
+        self.assertEqual(received_user_name, self.user2['musicbrainz_id'])
+
+        received_ts = data['last_updated']
+        expected_ts = int(self.user2_recommendations['created'].timestamp())
+        self.assertEqual(received_ts, expected_ts)
+
+        received_top_artist_recommendations = data['similar_artist']['recording_mbid']
+        expected_top_artist_recommendations = self.user2_recommendations['recording_mbid']['similar_artist']
+        self.assertEqual(expected_top_artist_recommendations, received_top_artist_recommendations)
--- listenbrainz/webserver/views/recommendations_cf_recording_api.py
+++ listenbrainz/webserver/views/recommendations_cf_recording_api.py
@@ -1,7 +1,9 @@
 import listenbrainz.db.user as db_user
 import listenbrainz.db.recommendations_cf_recording as db_recommendations_cf_recording
 
-from listenbrainz.webserver.errors import APIBadRequest, APINotFound
+from listenbrainz.webserver.errors import APIBadRequest, APINotFound, APINoContent
+
+from enum import Enum
 
 from flask import Blueprint, jsonify, request
 from listenbrainz.webserver.decorators import crossdomain
@@ -21,11 +23,11 @@ def get_recommendations(user_name):
         {
           "payload": {
 
-            "last_updated": "Sat, 06 Jun 2020 08:18:50 GMT",
+            "last_updated": 1588494361,
 
             "<artist_type>": {
 
-              "recording_mbids": [
+              "recording_mbid": [
                 "526bd613-fddd-4bd6-9137-ab709ac74cab",
                 "a6081bc1-2a76-4984-b21f-38bc3dcca3a5",
                 "a6ad0205-6e96-416d-a4e8-edd1773dac09",
@@ -37,99 +39,122 @@ def get_recommendations(user_name):
           }
         }
 
-        <artist_type>: 'top' or 'similar'
+        <artist_type>: 'top_artist' or 'similar_artist'
+
+        .. note::
+            - This endpoint is experimental and probably will change in the future.
+
+        :param type: Mandatory, artist type in ['top', 'similar']
+            Ex. type = top will fetch recommended recording mbids that belong to top artists
+                       listened to by the user.
+                type = similar will fetch recommended recording mbids that belong to artists
+                       similar to top artists listened to by the user.
 
         :statuscode 200: Successful query, you have data!
         :statuscode 400: Bad request, check ``response['error']`` for more details
-        :statuscode 404: User not found or recommendations not generated
+        :statuscode 404: User not found.
+        :statuscode 204: Recommendations for the user haven't been generated, empty response will be returned
     """
     user = db_user.get_by_mb_id(user_name)
     if user is None:
         raise APINotFound("Cannot find user: {}".format(user_name))
 
     artist_type = request.args.get('type')
-    if not _is_valid_artist(artist_type):
+    if not _is_valid_artist_type(artist_type):
         raise APIBadRequest("Invalid artist type: {}".format(artist_type))
 
     recommendations = db_recommendations_cf_recording.get_user_recommendation(user['id'])
 
     if recommendations is None:
-        err_msg = 'Oops! Looks like {} doesn\'t have recent listening history.' \
-                  ' Recommendations not generated :('.format(user_name)
-        raise APINotFound(err_msg)
+        err_msg = 'No recommendations due to absence of recent listening history for user {}'.format(user_name)
+        raise APINoContent(err_msg)
 
     if artist_type == 'top':
-        try:
-            payload = _get_top_artist_recommendations(recommendations, user_name)
-        except TopArtistRecommendationsNotFound:
-            err_msg = 'Oops! Looks like there was some Spark issue while generating top artist' \
-                      ' recommendations for {}. We are working on it. Please check back later!!'.format(user_name)
-            raise APINotFound(err_msg, payload={'last_updated': recommendations['created']})
+        payload = _format_top_artist_recommendations(recommendations, user_name)
 
     if artist_type == 'similar':
-        try:
-            payload = _get_similar_artist_recommendations(recommendations, user_name)
-        except SimilarArtistRecommendationsNotFound:
-            err_msg = 'Oops! Looks like there was some Spark issue while generating similar artist' \
-                      ' recommendations for {}. We are working on it. Please check back later!!'.format(user_name)
-            raise APINotFound(err_msg, payload={'last_updated': recommendations['created']})
+        payload = _format_similar_artist_recommendations(recommendations, user_name)
 
     return jsonify(payload)
 
 
-def _get_top_artist_recommendations(recommendations, user_name):
-    """ Get top artist recommended recording mbids.
+def _format_top_artist_recommendations(recommendations, user_name):
+    """ Format top artist recommendations payload.
 
-        if recommendations not found raises TopArtistRecommendationsNotFound exception.
+        Returns:
+            payload = {
+                'payload': {
+                    'top_artist': {
+                        'recording_mbid': ["526bd613-fddd-4bd6-9137-ab709ac74cab"]
+                    },
+                    'user_name': 'vansika,
+                    'last_updated': 1588494361,
+                }
+            }
+        Raises:
+            APINoContent: if top artist recommendations not found.
     """
     top_artist_recommendations = recommendations['recording_mbid']['top_artist']
+    last_updated = int(recommendations['created'].timestamp())
 
     if len(top_artist_recommendations) == 0:
-        raise TopArtistRecommendationsNotFound()
+        err_msg = 'No top artist recommendations for user {}, please try again later.'.format(user_name)
+        raise APINoContent(err_msg, payload={'last_updated': last_updated})
 
-    payload = {'payload': {
-        'top-artist': {
-            'recording_mbids': top_artist_recommendations
-        },
-        'user_name': user_name,
-        'last_updated': recommendations['created'],
-    }}
+    payload = {
+        'payload': {
+            'top_artist': {
+                'recording_mbid': top_artist_recommendations
+            },
+            'user_name': user_name,
+            'last_updated': last_updated,
+        }
+    }
 
     return payload
 
 
-def _get_similar_artist_recommendations(recommendations, user_name):
-    """ Get similar artist recommended recording mbids.
+def _format_similar_artist_recommendations(recommendations, user_name):
+    """ Format similar artist recommendations payload.
 
-        if recommendations not found raises SimilarArtistRecommendationsNotFound exception.
+        Returns:
+            payload = {
+                'payload': {
+                    'similar_artist': {
+                        'recording_mbid': ["526bd613-fddd-4bd6-9137-ab709ac74cab"]
+                    },
+                    'user_name': 'vansika,
+                    'last_updated': 1588494361,
+                }
+            }
+        Raises:
+            APINoContent: if similar artist recommendations not found.
     """
     similar_artist_recommendations = recommendations['recording_mbid']['similar_artist']
+    last_updated = int(recommendations['created'].timestamp())
 
     if len(similar_artist_recommendations) == 0:
-        raise SimilarArtistRecommendationsNotFound()
+        err_msg = 'No similar artist recommendations for user {}, please try again later.'.format(user_name)
+        raise APINoContent(err_msg, payload={'last_updated': last_updated})
 
-    payload = {'payload': {
-        'similar-artist': {
-            'recording_mbids': similar_artist_recommendations
-        },
-        'user_name': user_name,
-        'last_updated': recommendations['created'],
-    }}
+    payload = {
+        'payload': {
+            'similar_artist': {
+                'recording_mbid': similar_artist_recommendations
+            },
+            'user_name': user_name,
+            'last_updated': last_updated,
+        }
+    }
 
     return payload
 
 
-def _is_valid_artist(artist_type):
+def _is_valid_artist_type(artist_type):
     """ Check if artist type is valid.
     """
-    if artist_type not in ['top', 'similar']:
-        return False
-    return True
+    if artist_type is None:
+        raise APIBadRequest('Please provide artist type')
 
-
-class TopArtistRecommendationsNotFound(Exception):
-    pass
-
-
-class SimilarArtistRecommendationsNotFound(Exception):
-    pass
+    RecommendationArtistType = Enum('RecommendationArtistType', 'top similar')
+    return artist_type in RecommendationArtistType.__members__
