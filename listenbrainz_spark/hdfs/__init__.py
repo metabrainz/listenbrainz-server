fa4cff08953f8a33df76c22ecc8538d846f0b23c
Ishaan Shah <ishaan.n.shah@gmail.com>
2020-07-13 10:55:40 +0530

    Don't import dump if tarfile is corrupt

--- listenbrainz_spark/hdfs/__init__.py
+++ listenbrainz_spark/hdfs/__init__.py
@@ -3,12 +3,14 @@ import sys
 import time
 import shutil
 import subprocess
+from tarfile import TarError
 
 import listenbrainz_spark
 from listenbrainz_spark import utils, config, hdfs_connection
 from listenbrainz_spark.exceptions import SparkSessionNotInitializedException
 
 from flask import current_app
+
 
 class ListenbrainzHDFSUploader:
 
@@ -59,6 +61,17 @@ class ListenbrainzHDFSUploader:
         if callback is None:
             raise NotImplementedError('Callback to process JSON missing. Aboritng...')
 
+        # Extract all files in tmp_dump_dir
+        t = time.time()
+        current_app.logger.info("Extracting dump in temporary directory")
+        try:
+            tar.extractall(path=tmp_dump_dir)
+        except TarError as err:
+            current_app.logger.error("{} while extracting tarfile, aborting import".format(type(err).__name__))
+            return
+        time_taken = time.time() - t
+        current_app.logger.info("Extraction completed in {:.2f} seconds".format(time_taken))
+
         if force:
             current_app.logger.info('Removing {} from HDFS...'.format(dest_path))
             utils.delete_dir(dest_path, recursive=True)
@@ -66,22 +79,24 @@ class ListenbrainzHDFSUploader:
 
         file_count = 0
         total_time = 0.0
-        for member in tar:
-            if member.isfile() and self._is_json_file(member.name):
-                current_app.logger.info('Loading {}...'.format(member.name))
-                t = time.time()
-                tar.extract(member)
-                tmp_hdfs_path = os.path.join(tmp_dump_dir, member.name)
-                utils.upload_to_HDFS(tmp_hdfs_path, member.name)
-                callback(member.name, dest_path, tmp_hdfs_path, schema)
-                utils.delete_dir(tmp_hdfs_path, recursive=True)
-                os.remove(member.name)
-                file_count += 1
-                time_taken = time.time() - t
-                current_app.logger.info("Done! Processed {} files. Current file done in {:.2f} sec".format(
-                    file_count, time_taken))
-                total_time += time_taken
-                average_time = total_time / file_count
-                current_app.logger.info("Total time: {:.2f}, average time: {:.2f}".format(total_time, average_time))
+        for (dirpath, dirnames, filenames) in os.walk(tmp_dump_dir):
+            for filename in filenames:
+                if self._is_json_file(filename):
+                    current_app.logger.info('Loading {}...'.format(filename))
+                    t0 = time.time()
+                    file_path = os.path.join(dirpath, filename)
+                    tmp_hdfs_path = file_path
+                    utils.upload_to_HDFS(tmp_hdfs_path, file_path)
+                    callback(file_path, dest_path, tmp_hdfs_path, schema)
+                    utils.delete_dir(tmp_hdfs_path, recursive=True)
+                    os.remove(file_path)
+                    file_count += 1
+                    time_taken = time.time() - t0
+                    current_app.logger.info(
+                        "Done! Processed {} files. Current file done in {:.2f} sec".format(file_count, time_taken))
+                    total_time += time_taken
+                    average_time = total_time / file_count
+                    current_app.logger.info("Total time: {:.2f}, average time: {:.2f}".format(total_time, average_time))
+
         utils.delete_dir(tmp_dump_dir, recursive=True)
         shutil.rmtree(tmp_dump_dir)
