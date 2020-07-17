52e2895cae338d98abfe5b797184f3c996606bf0
Ishaan Shah <ishaanshah@users.noreply.github.com>
2020-07-17 21:55:38 +0530

    LB-637: Improvement to charts page (#965)
    * Add test
    
    * Change to pills
    
    * Change to functional component and reuse some code
    
    * Improve error handling
    
    * Add tests
    
    * Add date range

--- listenbrainz/webserver/static/js/src/stats/Bar.tsx
+++ listenbrainz/webserver/static/js/src/stats/Bar.tsx
@@ -1,14 +1,12 @@
 import * as React from "react";
 import { ResponsiveBar, LabelFormatter } from "@nivo/bar";
 
+import getEntityLink from "./utils";
+
 export type BarProps = {
   data: UserEntityData;
   maxValue: number;
   width?: number;
-};
-
-export type BarState = {
-  marginLeft: number;
 };
 
 type Tick = {
@@ -26,172 +24,136 @@ type Tick = {
   value: string;
 };
 
-export default class Bar extends React.Component<BarProps, BarState> {
-  // TODO: Use function from utils.tsx
-  getEntityLink = (data: UserEntityDatum, entity: string): JSX.Element => {
-    if (data.entityMBID) {
-      return (
-        <a
-          href={`http://musicbrainz.org/${data.entityType}/${data.entityMBID}`}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          {entity}
-        </a>
-      );
+export default function Bar(props: BarProps) {
+  const { data, maxValue, width } = props;
+  const marginLeft = Math.min((width || window.innerWidth) / 2, 400);
+  const tableDigitWidth = data[0]?.idx.toString().length;
+
+  const leftAlignedTick = <Tick extends any>(tick: Tick) => {
+    const datum = data[tick.tickIndex];
+    const {
+      entityType,
+      entity: entityName,
+      entityMBID,
+      artist: artistName,
+      artistMBID: artistMBIDs,
+      release: releaseName,
+      releaseMBID,
+      idx,
+    } = datum;
+
+    let artistMBID;
+    if (artistMBIDs) {
+      [artistMBID] = artistMBIDs;
     }
-    return <>{entity}</>;
-  };
 
-  getArtistLink = (data: UserEntityDatum, artist: string): JSX.Element => {
-    if (data.artistMBID && data.artistMBID.length) {
-      return (
-        <a
-          href={`http://musicbrainz.org/artist/${data.artistMBID[0]}`}
-          target="_blank"
-          rel="noopener noreferrer"
+    return (
+      <g transform={`translate(${tick.x - marginLeft}, ${tick.y})`}>
+        <foreignObject
+          height="3em"
+          width={marginLeft}
+          y={datum.entityType === "artist" ? -10 : -20}
         >
-          {artist}
-        </a>
-      );
-    }
-    return <>{artist}</>;
-  };
-
-  getReleaseLink = (data: UserEntityDatum, release: string): JSX.Element => {
-    if (data.release && data.releaseMBID) {
-      const res = (
-        <a
-          href={`http://musicbrainz.org/release/${data.releaseMBID}`}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          {release}
-        </a>
-      );
-      return res;
-    }
-    return <>{release}</>;
-  };
-
-  render() {
-    const { data, maxValue, width } = this.props;
-    const marginLeft = Math.min((width || window.innerWidth) / 2, 400);
-    const tableDigitWidth = data[0]?.idx.toString().length;
-
-    const leftAlignedTick = <Tick extends any>(tick: Tick) => {
-      const datum = data[tick.tickIndex];
-      const { entity, artist, release, idx } = datum;
-
-      return (
-        <g transform={`translate(${tick.x - marginLeft}, ${tick.y})`}>
-          <foreignObject
-            height="3em"
-            width={marginLeft}
-            y={datum.entityType === "artist" ? -10 : -20}
+          <table
+            style={{
+              width: "90%",
+              whiteSpace: "nowrap",
+              tableLayout: "fixed",
+            }}
           >
-            <table
-              style={{
-                width: "90%",
-                whiteSpace: "nowrap",
-                tableLayout: "fixed",
-              }}
-            >
-              <tbody>
-                <tr style={{ color: "black" }}>
-                  <td
-                    style={{ width: `${tableDigitWidth}em`, textAlign: "end" }}
-                  >
-                    {idx}.&nbsp;
-                  </td>
+            <tbody>
+              <tr style={{ color: "black" }}>
+                <td style={{ width: `${tableDigitWidth}em`, textAlign: "end" }}>
+                  {idx}.&nbsp;
+                </td>
+                <td
+                  style={{
+                    textOverflow: "ellipsis",
+                    overflow: "hidden",
+                  }}
+                >
+                  {getEntityLink(entityType, entityName, entityMBID)}
+                </td>
+              </tr>
+              {artistName && (
+                <tr>
+                  <td />
                   <td
                     style={{
+                      fontSize: 12,
                       textOverflow: "ellipsis",
                       overflow: "hidden",
                     }}
                   >
-                    {this.getEntityLink(datum, entity)}
+                    {getEntityLink("artist", artistName, artistMBID)}
+                    {releaseName && (
+                      <span>
+                        &nbsp;-&nbsp;
+                        {getEntityLink("release", releaseName, releaseMBID)}
+                      </span>
+                    )}
                   </td>
                 </tr>
-                {artist && (
-                  <tr>
-                    <td />
-                    <td
-                      style={{
-                        fontSize: 12,
-                        textOverflow: "ellipsis",
-                        overflow: "hidden",
-                      }}
-                    >
-                      {this.getArtistLink(datum, artist)}
-                      {release && (
-                        <span>
-                          &nbsp;-&nbsp;{this.getReleaseLink(datum, release)}
-                        </span>
-                      )}
-                    </td>
-                  </tr>
-                )}
-              </tbody>
-            </table>
-          </foreignObject>
-        </g>
-      );
-    };
+              )}
+            </tbody>
+          </table>
+        </foreignObject>
+      </g>
+    );
+  };
 
-    const labelFormatter = (((label: string) => {
-      return (
-        <tspan x={5} textAnchor="start">
-          {label}
-        </tspan>
-      );
-    }) as unknown) as LabelFormatter;
+  const labelFormatter = (((label: string) => {
+    return (
+      <tspan x={5} textAnchor="start">
+        {label}
+      </tspan>
+    );
+  }) as unknown) as LabelFormatter;
 
-    const customTooltip = (datum: any) => {
-      return (
-        <div>
-          {datum.data.entity}: <strong>{datum.value} Listens</strong>
-        </div>
-      );
-    };
+  const customTooltip = (datum: any) => {
+    return (
+      <div>
+        {datum.data.entity}: <strong>{datum.value} Listens</strong>
+      </div>
+    );
+  };
 
-    const theme = {
-      axis: {
-        ticks: {
-          text: {
-            fontSize: "14px",
-          },
-        },
-      },
-      labels: {
+  const theme = {
+    axis: {
+      ticks: {
         text: {
           fontSize: "14px",
         },
       },
-    };
+    },
+    labels: {
+      text: {
+        fontSize: "14px",
+      },
+    },
+  };
 
-    return (
-      <ResponsiveBar
-        data={data}
-        maxValue={maxValue}
-        layout="horizontal"
-        colors="#EB743B"
-        indexBy="id"
-        enableGridY={false}
-        padding={0.15}
-        labelFormat={labelFormatter}
-        labelSkipWidth={0}
-        tooltip={customTooltip}
-        margin={{
-          left: marginLeft,
-        }}
-        axisLeft={{
-          renderTick: leftAlignedTick,
-        }}
-        theme={theme}
-        keys={["count"]}
-        animate={false}
-      />
-    );
-  }
+  return (
+    <ResponsiveBar
+      data={data}
+      maxValue={maxValue}
+      layout="horizontal"
+      colors="#EB743B"
+      indexBy="id"
+      enableGridY={false}
+      padding={0.15}
+      labelFormat={labelFormatter}
+      labelSkipWidth={0}
+      tooltip={customTooltip}
+      margin={{
+        left: marginLeft,
+      }}
+      axisLeft={{
+        renderTick: leftAlignedTick,
+      }}
+      theme={theme}
+      keys={["count"]}
+      animate={false}
+    />
+  );
 }
