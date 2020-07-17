6576a9a60f35c121a513bb8ac1ec51e08fd1eb83
Param Singh <iliekcomputers@gmail.com>
2020-07-18 00:47:31 +0100

    Bring back an index I removed by mistake

--- admin/sql/create_indexes.sql
+++ admin/sql/create_indexes.sql
@@ -1,5 +1,6 @@
 BEGIN;
 
+CREATE UNIQUE INDEX auth_token_ndx_user ON "user" (auth_token);
 CREATE UNIQUE INDEX lower_musicbrainz_id_ndx_user ON "user" (lower(musicbrainz_id));
 CREATE UNIQUE INDEX login_id_ndx_user ON "user" (login_id);
 
