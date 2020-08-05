93339017d5b03c028eea5253aec38987a6755ac1
Robert Kaye <rob@musicbrainz.org>
2020-08-05 18:42:10 +0200

    Add dataset hoster based labs.api.listenbrainz.org (#1013)
    * Add in code to host two new APIs, for internal use for now.
    
    * First attempt to build on MB hardware.
    
    * Interim check-in
    
    * Add data set hoster
    
    * Got the dataset hoster running on the MB infrastructure!
    
    * Add artist msid lookup
    
    * Fix parameter name
    
    * Finish migrating the first functions
    
    * Working on the tests for the labs api
    
    * Add support for pagination in artist credit from mbid lookup and added a test.
    
    * Add tests for the country lookup
    
    * Minor bits of cleanup
    
    * Connect to williams for the mappings
    
    * Add test for lookup by artist msid
    
    * Cleanup
    
    * No longer needed.
    
    * Pep 8 fixes
    
    * STFU Pep8
    
    * Rename the top level dir of the labs api and pin data set hoster tag

---
+++ listenbrainz/labs_api/labs/api/artist_credit_from_artist_mbid.py
@@ -0,0 +1,56 @@
+from operator import itemgetter
+
+import psycopg2
+import psycopg2.extras
+from flask import current_app
+from datasethoster import Query
+
+
+class ArtistCreditIdFromArtistMBIDQuery(Query):
+
+    def names(self):
+        return ("artist-credit-from-artist-mbid", "MusicBrainz Artist Credit From Artist MBID")
+
+    def inputs(self):
+        return ['artist_mbid']
+
+    def introduction(self):
+        return """Look up all available artist credit ids from an artist mbid."""
+
+    def outputs(self):
+        return ['artist_mbid', 'artist_credit_id']
+
+    def fetch(self, params, count=-1, offset=-1):
+
+        with psycopg2.connect(current_app.config['MB_DATABASE_URI']) as conn:
+            with conn.cursor(cursor_factory=psycopg2.extras.DictCursor) as curs:
+
+                acs = tuple([p['artist_mbid'] for p in params])
+                query = """SELECT a.gid AS artist_mbid,
+                                       array_agg(ac.id) AS artist_credit_id
+                                  FROM artist_credit ac
+                                  JOIN artist_credit_name acn
+                                    ON ac.id = acn.artist_credit
+                                  JOIN artist a
+                                    ON acn.artist = a.id
+                                 WHERE a.gid in %s
+                              GROUP BY a.gid
+                              ORDER BY artist_mbid"""
+                args = [acs]
+                if count > 0:
+                    query += " LIMIT %s"
+                    args.append(count)
+                if offset >= 0:
+                    query += " OFFSET %s"
+                    args.append(offset)
+
+                curs.execute(query, tuple(args))
+                output = []
+                while True:
+                    row = curs.fetchone()
+                    if not row:
+                        break
+
+                    output.append(dict(row))
+
+                return output
