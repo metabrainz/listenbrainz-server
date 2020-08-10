4387e4a5b1176b661e21410b84a0e553f5d83d55
Ishaan Shah <ishaan.n.shah@gmail.com>
2020-08-10 16:25:57 +0530

    Make logarithmic

--- listenbrainz/webserver/static/js/src/stats/Choropleth.tsx
+++ listenbrainz/webserver/static/js/src/stats/Choropleth.tsx
@@ -1,8 +1,10 @@
-import * as React from "react";
-import * as _ from "lodash";
-import { Choropleth } from "@nivo/geo";
-import { LegendProps } from "@nivo/legends";
 import { Theme } from "@nivo/core";
+import { Choropleth } from "@nivo/geo";
+import { BoxLegendSvg, LegendProps } from "@nivo/legends";
+import { scaleThreshold } from "d3-scale";
+import { schemeOranges } from "d3-scale-chromatic";
+import * as _ from "lodash";
+import * as React from "react";
 import { useMediaQuery } from "react-responsive";
 import * as features from "./world_countries.json";
 
@@ -10,6 +12,27 @@ export type ChoroplethProps = {
   data: UserArtistMapData;
   width?: number;
 };
+
+const CustomLegend = ({
+  height,
+  legends,
+  width,
+}: {
+  height: number;
+  legends: Array<LegendProps>;
+  width: any;
+}) => (
+  <>
+    {legends.map((legend) => (
+      <BoxLegendSvg
+        key={JSON.stringify(legend.data?.map(({ id }) => id))}
+        containerHeight={height}
+        containerWidth={width}
+        {...legend}
+      />
+    ))}
+  </>
+);
 
 export default function CustomChoropleth(props: ChoroplethProps) {
   const isMobile = useMediaQuery({ maxWidth: 767 });
@@ -72,6 +95,24 @@ export default function CustomChoropleth(props: ChoroplethProps) {
   let { width } = props;
   width = width || 1200; // Set default width to 1200
 
+  // Calculate logarithmic domain
+  const domain = (() => {
+    const maxArtistCount = _.maxBy(data, (datum) => datum.value)?.value || 1;
+
+    const result = [];
+    for (let i = 0; i < 6; i += 1) {
+      result.push(
+        Math.ceil(Math.E ** ((Math.log(maxArtistCount) / 6) * (i + 1)))
+      );
+    }
+
+    return result;
+  })();
+
+  const colorScale = scaleThreshold<number, string>()
+    .domain(domain)
+    .range(schemeOranges[6]);
+
   return (
     <Choropleth
       data={data}
@@ -79,30 +120,12 @@ export default function CustomChoropleth(props: ChoroplethProps) {
       height={width * 0.5}
       features={features.features}
       margin={{ top: 0, right: 0, bottom: 0, left: 0 }}
-      colors="oranges"
+      colors={colorScale}
+      domain={domain}
       theme={isMobile ? themes.mobile : themes.desktop}
-      domain={[
-        0,
-        (() => {
-          const sortedArray = _.sortBy(data, (datum) => datum.value);
-
-          const len = sortedArray.length;
-          if (len === 0) {
-            return 0;
-          }
-          if (len % 2 === 0) {
-            return Math.max(
-              sortedArray[Math.floor(len / 2)].value +
-                sortedArray[Math.ceil(len / 2)].value,
-              9
-            );
-          }
-          return Math.max(sortedArray[Math.ceil(len / 2)].value * 2, 9);
-        })(),
-      ]}
+      valueFormat=".2s"
       unknownColor="#efefef"
       label="properties.name"
-      valueFormat=".2s"
       projectionScale={width / 5.5}
       projectionType="naturalEarth1"
       projectionTranslation={[0.5, 0.53]}
