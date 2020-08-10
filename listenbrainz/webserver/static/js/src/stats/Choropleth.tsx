2a00c0bb535903c57ff9081404c17e542783bf7c
Ishaan Shah <ishaanshah@users.noreply.github.com>
2020-08-10 22:40:35 +0530

    LB-695: Improve domain for "Artist Origin" graph (#1025)
    * Median checkpoint
    
    * Make logarithmic
    
    * Fix legend

--- listenbrainz/webserver/static/js/src/stats/Choropleth.tsx
+++ listenbrainz/webserver/static/js/src/stats/Choropleth.tsx
@@ -1,7 +1,11 @@
-import * as React from "react";
-import { Choropleth } from "@nivo/geo";
-import { LegendProps } from "@nivo/legends";
 import { Theme } from "@nivo/core";
+import { Choropleth } from "@nivo/geo";
+import { BoxLegendSvg, LegendProps } from "@nivo/legends";
+import { scaleThreshold } from "d3-scale";
+import { schemeOranges } from "d3-scale-chromatic";
+import { format } from "d3-format";
+import * as _ from "lodash";
+import * as React from "react";
 import { useMediaQuery } from "react-responsive";
 import * as features from "./world_countries.json";
 
@@ -13,7 +17,7 @@ export type ChoroplethProps = {
 export default function CustomChoropleth(props: ChoroplethProps) {
   const isMobile = useMediaQuery({ maxWidth: 767 });
 
-  const commonLegendProps: Partial<LegendProps> = {
+  const commonLegendProps = {
     anchor: "bottom-left",
     direction: "column",
     itemDirection: "left-to-right",
@@ -27,7 +31,8 @@ export default function CustomChoropleth(props: ChoroplethProps) {
         },
       },
     ],
-  };
+  } as LegendProps;
+
   const legends = {
     desktop: {
       itemWidth: 90,
@@ -36,7 +41,7 @@ export default function CustomChoropleth(props: ChoroplethProps) {
       translateX: 50,
       translateY: -50,
       ...commonLegendProps,
-    },
+    } as LegendProps,
     mobile: {
       itemWidth: 90,
       itemHeight: 10,
@@ -44,7 +49,7 @@ export default function CustomChoropleth(props: ChoroplethProps) {
       translateX: 20,
       translateY: -15,
       ...commonLegendProps,
-    },
+    } as LegendProps,
   };
 
   const themes: {
@@ -70,28 +75,65 @@ export default function CustomChoropleth(props: ChoroplethProps) {
   const { data } = props;
   let { width } = props;
   width = width || 1200; // Set default width to 1200
+  const height = width / 2;
+
+  // Calculate logarithmic domain
+  const domain = (() => {
+    const maxArtistCount = _.maxBy(data, (datum) => datum.value)?.value || 1;
+
+    const result = [];
+    for (let i = 0; i < 6; i += 1) {
+      result.push(
+        Math.ceil(Math.E ** ((Math.log(maxArtistCount) / 6) * (i + 1)))
+      );
+    }
+
+    return result;
+  })();
+
+  const colorScale = scaleThreshold<number, string>()
+    .domain(domain)
+    .range(schemeOranges[6]);
+
+  // Create a custom legend component because the default doesn't work with scaleThreshold
+  const CustomLegend = () => (
+    <BoxLegendSvg
+      containerHeight={height}
+      containerWidth={width!}
+      data={colorScale.range().map((color, index) => {
+        // eslint-disable-next-line prefer-const
+        let [start, end] = colorScale.invertExtent(color);
+
+        // Domain starts with 1
+        if (start === undefined) {
+          start = 1;
+        }
+
+        return {
+          index,
+          color,
+          id: color,
+          extent: [start, end],
+          label: `${format(".2s")(start)} - ${format(".2s")(end!)}`,
+        };
+      })}
+      {...(isMobile ? legends.mobile : legends.desktop)}
+    />
+  );
 
   return (
     <Choropleth
       data={data}
       width={width}
-      height={width * 0.5}
+      height={height}
       features={features.features}
       margin={{ top: 0, right: 0, bottom: 0, left: 0 }}
-      colors="oranges"
+      colors={colorScale}
+      domain={domain}
       theme={isMobile ? themes.mobile : themes.desktop}
-      domain={[
-        0,
-        data.reduce(
-          (prevValue, currValue) => {
-            return prevValue.value > currValue.value ? prevValue : currValue;
-          },
-          { id: "default", value: -Infinity }
-        ).value,
-      ]}
+      valueFormat=".2s"
       unknownColor="#efefef"
       label="properties.name"
-      valueFormat=".2s"
       projectionScale={width / 5.5}
       projectionType="naturalEarth1"
       projectionTranslation={[0.5, 0.53]}
@@ -100,7 +142,7 @@ export default function CustomChoropleth(props: ChoroplethProps) {
       // The typescript definition file for Choropleth is incomplete, so disable typescript
       // until it is fixed.
       // @ts-ignore
-      legends={[isMobile ? legends.mobile : legends.desktop]}
+      layers={["features", CustomLegend]}
     />
   );
 }
