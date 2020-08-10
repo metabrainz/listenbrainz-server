54941fcb89e7951885607817271835d680a80fe2
Ishaan Shah <ishaan.n.shah@gmail.com>
2020-08-10 19:01:09 +0530

    Fix 500s on artist-map endpoint

--- listenbrainz/webserver/views/stats_api.py
+++ listenbrainz/webserver/views/stats_api.py
@@ -723,21 +723,22 @@ def _get_mbids_from_msids(artist_msids: list) -> list:
     """
     request_data = [{"artist_msid": artist_msid} for artist_msid in artist_msids]
     artist_mbids = []
-    try:
-        result = requests.post("{}/artist-credit-from-artist-msid/json"
-                               .format(current_app.config['LISTENBRAINZ_LABS_API_URL']),
-                               json=request_data, params={'count': len(request_data)})
-        # Raise error if non 200 response is received
-        result.raise_for_status()
-        data = result.json()
-        for entry in data:
-            artist_mbids += entry['[artist_credit_mbid]']
-    except requests.RequestException as err:
-        current_app.logger.error("Error while getting artist_mbids, {}".format(err), exc_info=True)
-        error_msg = ("An error occurred while calculating artist_map data, "
-                     "try setting 'force_recalculate' to 'false' to get a cached copy if available."
-                     "Payload: {}. Response: {}".format(request_data, result.text))
-        raise APIInternalServerError(error_msg)
+    if len(request_data) > 0:
+        try:
+            result = requests.post("{}/artist-credit-from-artist-msid/json"
+                                   .format(current_app.config['LISTENBRAINZ_LABS_API_URL']),
+                                   json=request_data, params={'count': len(request_data)})
+            # Raise error if non 200 response is received
+            result.raise_for_status()
+            data = result.json()
+            for entry in data:
+                artist_mbids += entry['[artist_credit_mbid]']
+        except requests.RequestException as err:
+            current_app.logger.error("Error while getting artist_mbids, {}".format(err), exc_info=True)
+            error_msg = ("An error occurred while calculating artist_map data, "
+                         "try setting 'force_recalculate' to 'false' to get a cached copy if available."
+                         "Payload: {}. Response: {}".format(request_data, result.text))
+            raise APIInternalServerError(error_msg)
 
     return artist_mbids
 
@@ -747,20 +748,21 @@ def _get_country_code_from_mbids(artist_mbids: set) -> list:
     """
     request_data = [{"artist_mbid": artist_mbid} for artist_mbid in artist_mbids]
     country_codes = []
-    try:
-        result = requests.post("{}/artist-country-code-from-artist-mbid/json"
-                               .format(current_app.config['LISTENBRAINZ_LABS_API_URL']),
-                               json=request_data, params={'count': len(request_data)})
-        # Raise error if non 200 response is received
-        result.raise_for_status()
-        data = result.json()
-        for entry in data:
-            country_codes.append(entry['country_code'])
-    except requests.RequestException as err:
-        current_app.logger.error("Error while getting artist_country_codes, {}".format(err), exc_info=True)
-        error_msg = ("An error occurred while calculating artist_map data, "
-                     "try setting 'force_recalculate' to 'false' to get a cached copy if available"
-                     "Payload: {}. Response: {}".format(request_data, result.text))
-        raise APIInternalServerError(error_msg)
+    if len(request_data) > 0:
+        try:
+            result = requests.post("{}/artist-country-code-from-artist-mbid/json"
+                                   .format(current_app.config['LISTENBRAINZ_LABS_API_URL']),
+                                   json=request_data, params={'count': len(request_data)})
+            # Raise error if non 200 response is received
+            result.raise_for_status()
+            data = result.json()
+            for entry in data:
+                country_codes.append(entry['country_code'])
+        except requests.RequestException as err:
+            current_app.logger.error("Error while getting artist_country_codes, {}".format(err), exc_info=True)
+            error_msg = ("An error occurred while calculating artist_map data, "
+                         "try setting 'force_recalculate' to 'false' to get a cached copy if available"
+                         "Payload: {}. Response: {}".format(request_data, result.text))
+            raise APIInternalServerError(error_msg)
 
     return country_codes
