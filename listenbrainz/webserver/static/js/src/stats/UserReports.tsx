c0079c6ccf64f85d32f399d06073dda4db4d7cc7
Ishaan Shah <ishaan.n.shah@gmail.com>
2020-08-11 11:07:24 +0530

    Add sections to Reports page

--- listenbrainz/webserver/static/js/src/stats/UserReports.tsx
+++ listenbrainz/webserver/static/js/src/stats/UserReports.tsx
@@ -104,47 +104,55 @@ export default class UserReports extends React.Component<
             </Pill>
           </div>
         </div>
-        <ErrorBoundary>
-          <UserListeningActivity range={range} apiUrl={apiUrl} user={user} />
-        </ErrorBoundary>
-        <div className="row">
-          <div className="col-md-4">
-            <ErrorBoundary>
-              <UserTopEntity
-                range={range}
-                entity="artist"
-                apiUrl={apiUrl}
-                user={user}
-              />
-            </ErrorBoundary>
+        <section id="listening-activity">
+          <ErrorBoundary>
+            <UserListeningActivity range={range} apiUrl={apiUrl} user={user} />
+          </ErrorBoundary>
+        </section>
+        <section id="top-entity">
+          <div className="row">
+            <div className="col-md-4">
+              <ErrorBoundary>
+                <UserTopEntity
+                  range={range}
+                  entity="artist"
+                  apiUrl={apiUrl}
+                  user={user}
+                />
+              </ErrorBoundary>
+            </div>
+            <div className="col-md-4">
+              <ErrorBoundary>
+                <UserTopEntity
+                  range={range}
+                  entity="release"
+                  apiUrl={apiUrl}
+                  user={user}
+                />
+              </ErrorBoundary>
+            </div>
+            <div className="col-md-4">
+              <ErrorBoundary>
+                <UserTopEntity
+                  range={range}
+                  entity="recording"
+                  apiUrl={apiUrl}
+                  user={user}
+                />
+              </ErrorBoundary>
+            </div>
           </div>
-          <div className="col-md-4">
-            <ErrorBoundary>
-              <UserTopEntity
-                range={range}
-                entity="release"
-                apiUrl={apiUrl}
-                user={user}
-              />
-            </ErrorBoundary>
-          </div>
-          <div className="col-md-4">
-            <ErrorBoundary>
-              <UserTopEntity
-                range={range}
-                entity="recording"
-                apiUrl={apiUrl}
-                user={user}
-              />
-            </ErrorBoundary>
-          </div>
-        </div>
-        <ErrorBoundary>
-          <UserDailyActivity range={range} apiUrl={apiUrl} user={user} />
-        </ErrorBoundary>
-        <ErrorBoundary>
-          <UserArtistMap range={range} apiUrl={apiUrl} user={user} />
-        </ErrorBoundary>
+        </section>
+        <section id="daily-activity">
+          <ErrorBoundary>
+            <UserDailyActivity range={range} apiUrl={apiUrl} user={user} />
+          </ErrorBoundary>
+        </section>
+        <section id="artist-origin">
+          <ErrorBoundary>
+            <UserArtistMap range={range} apiUrl={apiUrl} user={user} />
+          </ErrorBoundary>
+        </section>
       </div>
     );
   }
