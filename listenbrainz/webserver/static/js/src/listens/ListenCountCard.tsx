44409cc8ced1f5f3fc428e6f733bbd7311cda24a
shivam-kapila <shivamkapila4@gmail.com>
2020-08-14 02:00:50 +0530

    Add tests

--- listenbrainz/webserver/static/js/src/listens/ListenCountCard.tsx
+++ listenbrainz/webserver/static/js/src/listens/ListenCountCard.tsx
@@ -1,27 +1,24 @@
 import * as React from "react";
 import Card from "../components/Card";
 
-type ListenCountCardProps = {
+export type ListenCountCardProps = {
   listenCount?: number;
 };
 
 const ListenCountCard = (props: ListenCountCardProps) => {
-
   const { listenCount } = props;
 
   return (
-    <>
-      <Card id="listen-count-card">
-        <h4>
-          You have listened to
-          <hr />
-        </h4>
-        <div>
-          <h4>{listenCount || `&nbsp;`}</h4>
-          <small className="text-muted">songs so far</small>
-        </div>
-      </Card>
-    </>
+    <Card id="listen-count-card">
+      <h4>
+        You have listened to
+        <hr />
+      </h4>
+      <div>
+        <h4>{listenCount || `&nbsp;`}</h4>
+        <small className="text-muted">songs so far</small>
+      </div>
+    </Card>
   );
 };
 
