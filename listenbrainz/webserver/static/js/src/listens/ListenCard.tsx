4e207495b016ecfce108f5aa6f3893a2f9911479
shivam-kapila <shivamkapila4@gmail.com>
2020-08-18 16:35:19 +0530

    Improve the card

--- listenbrainz/webserver/static/js/src/listens/ListenCard.tsx
+++ listenbrainz/webserver/static/js/src/listens/ListenCard.tsx
@@ -11,7 +11,7 @@ import {
 import { IconProp } from "@fortawesome/fontawesome-svg-core";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
-import { getArtistLink, getTrackLink, searchForSpotifyTrack } from "../utils";
+import { getArtistLink, getTrackLink } from "../utils";
 import Card from "../components/Card";
 import APIService from "../APIService";
 
@@ -26,8 +26,7 @@ type ListenCardProps = {
 };
 
 type ListenCardState = {
-  coverArtUrl?: string;
-  feedback: ListenFeedBack;
+  coverArtUrl?: String;
 };
 
 export default class ListenCard extends React.Component<
@@ -43,7 +42,6 @@ export default class ListenCard extends React.Component<
     this.playListen = props.playListen.bind(this, props.listen);
 
     this.state = {
-      feedback: 0,
       coverArtUrl: "",
     };
 
@@ -68,28 +66,27 @@ export default class ListenCard extends React.Component<
     );
 
     this.APIService.getCoverArt(releaseMBID, recordingMSID).then((imageUrl) => {
-      const coverArtUrl = imageUrl !== "" ? "imageUrl" : DEFAULT_COVER_ART_URL;
+      const coverArtUrl = imageUrl || DEFAULT_COVER_ART_URL;
       this.setState({ coverArtUrl });
     });
   };
 
   render() {
     const { listen, mode, className } = this.props;
-    const { feedback, coverArtUrl } = this.state;
+    const { coverArtUrl } = this.state;
 
     return (
       <Card
         onDoubleClick={this.playListen}
         className={`listen-card row ${className}`}
-        title="Double click to play"
       >
-        <MediaQuery minWidth={768}>
-          <div className="col-xs-10">
+        <div className="col-xs-9">
+          <MediaQuery minWidth={768}>
             <div className="col-xs-9">
               <div
                 className="cover-art img-responsive"
                 style={{
-                  background: `url("${coverArtUrl}")`,
+                  backgroundImage: `url("${coverArtUrl}")`,
                 }}
               />
               <div className="track-details">
@@ -125,82 +122,68 @@ export default class ListenCard extends React.Component<
                 </span>
               )}
             </div>
-          </div>
-
-          <div className="col-xs-2 text-center">
-            {mode === "follow" || mode === "recent" ? (
-              <a
-                href={`/user/${listen.user_name}`}
-                target="_blank"
-                rel="noopener noreferrer"
-              >
-                {listen.user_name}
-              </a>
-            ) : (
-              <div>
-                <FontAwesomeIcon icon={faHeart as IconProp} />
-                <FontAwesomeIcon icon={faHeartBroken as IconProp} />
-              </div>
-            )}
-          </div>
-        </MediaQuery>
-        <MediaQuery maxWidth={767}>
-          <div className="col-xs-9">
-            <div className="track-details">
-              <p title={listen.track_metadata.track_name}>
-                {getTrackLink(listen)}
-              </p>
-              <p>
-                <small
-                  className="text-muted"
-                  title={listen.track_metadata.artist_name}
-                >
-                  {listen.playing_now ? (
-                    <span className="listen-time text-center text-muted">
-                      <FontAwesomeIcon icon={faMusic as IconProp} /> Playing now
-                    </span>
-                  ) : (
-                    <span
-                      className="listen-time text-center text-muted"
-                      title={
-                        listen.listened_at_iso?.toString() ||
-                        new Date(listen.listened_at * 1000).toISOString()
-                      }
-                    >
-                      {`
+          </MediaQuery>
+          <MediaQuery maxWidth={767}>
+            <div className="col-xs-12">
+            <div
+                className="cover-art img-responsive"
+                style={{
+                  backgroundImage: `url("${coverArtUrl}")`,
+                }}
+              />
+              <div className="track-details">
+                <p title={listen.track_metadata.track_name}>
+                  {getTrackLink(listen)}
+                </p>
+                <p>
+                  <small
+                    className="text-muted"
+                    title={listen.track_metadata.artist_name}
+                  >
+                    {listen.playing_now ? (
+                      <span className="listen-time text-muted">
+                        <FontAwesomeIcon icon={faMusic as IconProp} /> Playing
+                        now
+                      </span>
+                    ) : (
+                      <span
+                        className="listen-time text-muted"
+                        title={
+                          listen.listened_at_iso?.toString() ||
+                          new Date(listen.listened_at * 1000).toISOString()
+                        }
+                      >
+                        {`
                       ${
                         listen.listened_at_iso
                           ? timeago.ago(listen.listened_at_iso, true)
                           : timeago.ago(listen.listened_at * 1000, true)
                       }
                       `}
-                      ago &#8212;
-                    </span>
-                  )}
+                        ago &#8212;
+                      </span>
+                    )}
 
-                  {` ${getArtistLink(listen)}`}
-                </small>
-              </p>
-            </div>
-          </div>
-
-          <div className="col-xs-3 text-center">
-            {mode === "follow" || mode === "recent" ? (
-              <a
-                href={`/user/${listen.user_name}`}
-                target="_blank"
-                rel="noopener noreferrer"
-              >
-                {listen.user_name}
-              </a>
-            ) : (
-              <div>
-                <FontAwesomeIcon icon={faHeart as IconProp} />
-                <FontAwesomeIcon icon={faHeartBroken as IconProp} />
+                    {` ${getArtistLink(listen)}`}
+                  </small>
+                </p>
               </div>
-            )}
-          </div>
-        </MediaQuery>
+            </div>
+          </MediaQuery>
+        </div>
+        <div className="col-xs-3 text-center">
+          {mode === "follow" || mode === "recent" ? (
+            <a
+              href={`/user/${listen.user_name}`}
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              {listen.user_name}
+            </a>
+          ) : (
+            <div />
+          )}
+        </div>
       </Card>
     );
   }
