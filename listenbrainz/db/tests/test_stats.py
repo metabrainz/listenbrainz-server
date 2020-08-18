6dd38653166e8355a0620f36032130c20a4eefd8
Ishaan Shah <ishaan.n.shah@gmail.com>
2020-08-18 18:41:38 +0530

    Add DB tests

--- listenbrainz/db/tests/test_stats.py
+++ listenbrainz/db/tests/test_stats.py
@@ -1,15 +1,16 @@
 import json
 import os
+from datetime import datetime, timezone
+
 import listenbrainz.db.stats as db_stats
 import listenbrainz.db.user as db_user
-
-from datetime import datetime, timezone
-from data.model.user_listening_activity import UserListeningActivityStatJson
-from data.model.user_daily_activity import UserDailyActivityStatJson
-from data.model.user_artist_stat import UserArtistStatJson
+from data.model.sitewide_artist_stat import SitewideArtistStatRange
 from data.model.user_artist_map import UserArtistMapStatJson
-from data.model.user_release_stat import UserReleaseStatJson
+from data.model.user_artist_stat import UserArtistStatJson
+from data.model.user_daily_activity import UserDailyActivityStatJson
+from data.model.user_listening_activity import UserListeningActivityStatJson
 from data.model.user_recording_stat import UserRecordingStatJson
+from data.model.user_release_stat import UserReleaseStatJson
 from listenbrainz.db.testing import DatabaseTestCase
 
 
@@ -166,11 +167,21 @@ class StatsDatabaseTestCase(DatabaseTestCase):
         result = db_stats.get_user_artist_map(1, 'year')
         self.assertDictEqual(result.year.dict(), artist_map_data)
 
+    def test_insert_sitewide_artists(self):
+        """ Test if sitewide artist data is inserted correctly """
+        with open(self.path_to_data_file('sitewide_top_artists_db.json')) as f:
+            artists_data = json.load(f)
+
+        db_stats.insert_sitewide_artists(stats_range='all_time', artists=SitewideArtistStatRange(**artists_data))
+
+        result = db_stats.get_sitewide_artists('all_time')
+        self.assertDictEqual(result.data.dict(), artists_data)
+
     def insert_test_data(self):
         """ Insert test data into the database """
 
         with open(self.path_to_data_file('user_top_artists_db.json')) as f:
-            artists = json.load(f)
+            user_artists = json.load(f)
         with open(self.path_to_data_file('user_top_releases_db.json')) as f:
             releases = json.load(f)
         with open(self.path_to_data_file('user_top_recordings_db.json')) as f:
@@ -181,22 +192,26 @@ class StatsDatabaseTestCase(DatabaseTestCase):
             daily_activity = json.load(f)
         with open(self.path_to_data_file('user_artist_map_db.json')) as f:
             artist_map = json.load(f)
+        with open(self.path_to_data_file('sitewide_top_artists_db.json')) as f:
+            sitewide_artists = json.load(f)
 
-        db_stats.insert_user_artists(self.user['id'], UserArtistStatJson(**{'all_time': artists}))
+        db_stats.insert_user_artists(self.user['id'], UserArtistStatJson(**{'all_time': user_artists}))
         db_stats.insert_user_releases(self.user['id'], UserReleaseStatJson(**{'all_time': releases}))
         db_stats.insert_user_recordings(self.user['id'], UserRecordingStatJson(**{'all_time': recordings}))
         db_stats.insert_user_listening_activity(
             self.user['id'], UserListeningActivityStatJson(**{'all_time': listening_activity}))
         db_stats.insert_user_daily_activity(self.user['id'], UserDailyActivityStatJson(**{'all_time': daily_activity}))
         db_stats.insert_user_artist_map(self.user['id'], UserArtistMapStatJson(**{'all_time': artist_map}))
+        db_stats.insert_sitewide_artists('all_time', SitewideArtistStatRange(**sitewide_artists))
 
         return {
-            'user_artists': artists,
+            'user_artists': user_artists,
             'user_releases': releases,
             'user_recordings': recordings,
             'user_listening_activity': listening_activity,
             'user_daily_activity': daily_activity,
-            'user_artist_map': artist_map
+            'user_artist_map': artist_map,
+            'sitewide_artists': sitewide_artists
         }
 
     def test_get_timestamp_for_last_user_stats_update(self):
@@ -234,6 +249,11 @@ class StatsDatabaseTestCase(DatabaseTestCase):
         data_inserted = self.insert_test_data()
         result = db_stats.get_user_artist_map(self.user['id'], 'all_time')
         self.assertDictEqual(result.all_time.dict(), data_inserted['user_artist_map'])
+
+    def test_get_sitewide_artists(self):
+        data_inserted = self.insert_test_data()
+        result = db_stats.get_sitewide_artists('all_time')
+        self.assertDictEqual(result.data.dict(), data_inserted['sitewide_artists'])
 
     def test_valid_stats_exist(self):
         self.assertFalse(db_stats.valid_stats_exist(self.user['id'], 7))
