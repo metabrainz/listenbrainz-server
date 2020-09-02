fcad254b325a11f4de2450a92c5c7ae0de619a10
Ishaan Shah <ishaan.n.shah@gmail.com>
2020-09-02 22:07:38 +0530

    Add tests for copy

--- listenbrainz_spark/tests/test_utils.py
+++ listenbrainz_spark/tests/test_utils.py
@@ -10,12 +10,15 @@ from pyspark.sql import Row
 
 class UtilsTestCase(SparkTestCase):
     # use path_ as prefix for all paths in this class.
-    path_ = '/test'
+    path_ = "/test"
+    temp_path_ = "/temp"
 
     def tearDown(self):
-        path_found = utils.path_exists(self.path_)
-        if path_found:
+        if utils.path_exists(self.path_):
             utils.delete_dir(self.path_, recursive=True)
+
+        if utils.path_exists(self.temp_path_):
+            utils.delete_dir(self.temp_path_, recursive=True)
 
     def test_append_dataframe(self):
         hdfs_path = self.path_ + '/test_df.parquet'
@@ -89,9 +92,37 @@ class UtilsTestCase(SparkTestCase):
         utils.create_dir(self.path_)
         test_exists = utils.path_exists(self.path_)
         self.assertTrue(test_exists)
-        utils.rename(self.path_, '/temp')
+        utils.rename(self.path_, self.temp_path_)
         test_exists = utils.path_exists(self.path_)
         self.assertFalse(test_exists)
-        temp_exists = utils.path_exists('/temp')
+        temp_exists = utils.path_exists(self.temp_path_)
         self.assertTrue(temp_exists)
-        utils.delete_dir('/temp')
+        utils.delete_dir(self.temp_path_)
+
+    def test_copy(self):
+        # Test directories
+        utils.create_dir(self.path_)
+        utils.create_dir(os.path.join(self.path_, "a"))
+        utils.create_dir(os.path.join(self.path_, "b"))
+
+        # DataFrames to create parquets
+        df_a = utils.create_dataframe([Row(column1=1, column2=2)], schema=None)
+        df_b = utils.create_dataframe([Row(column1=3, column2=4)], schema=None)
+        df_c = utils.create_dataframe([Row(column1=5, column2=6)], schema=None)
+
+        # Save DataFrames in respective directories
+        utils.save_parquet(df_a, os.path.join(self.path_, "a", "df_a.parquet"))
+        utils.save_parquet(df_b, os.path.join(self.path_, "b", "df_b.parquet"))
+        utils.save_parquet(df_c, os.path.join(self.path_, "df_c.parquet"))
+
+        utils.copy(self.path_, self.temp_path_, overwrite=True)
+
+        # Read copied DataFrame
+        cp_df_a = utils.read_files_from_HDFS(os.path.join(self.temp_path_, "a", "df_a.parquet"))
+        cp_df_b = utils.read_files_from_HDFS(os.path.join(self.temp_path_, "b", "df_b.parquet"))
+        cp_df_c = utils.read_files_from_HDFS(os.path.join(self.temp_path_, "df_c.parquet"))
+
+        # Check if both DataFrames are same
+        self.assertListEqual(df_a.rdd.map(list).collect(), cp_df_a.rdd.map(list).collect())
+        self.assertListEqual(df_b.rdd.map(list).collect(), cp_df_b.rdd.map(list).collect())
+        self.assertListEqual(df_c.rdd.map(list).collect(), cp_df_c.rdd.map(list).collect())
