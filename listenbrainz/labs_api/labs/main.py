f5ee7c1fabac8fce7d02e4799b840a063a5bc741
Robert Kaye <rob@musicbrainz.org>
2020-09-17 17:41:51 +0200

    Add recording mbid query to labs-api (#1093)
    * Add MSB stats and recording MBID lookup
    
    * Dont split array agg data
    
    * Add scripts to start a local labs api dev server
    
    * Finish fixing LB-723
    
    * Add test for recording_mbid lookup
    
    * STFU peppy.
    
    * whitespace
    
    * Fix tests

--- listenbrainz/labs_api/labs/main.py
+++ listenbrainz/labs_api/labs/main.py
@@ -4,10 +4,14 @@ from datasethoster.main import app, register_query
 from listenbrainz.labs_api.labs.api.artist_country_from_artist_mbid import ArtistCountryFromArtistMBIDQuery
 from listenbrainz.labs_api.labs.api.artist_credit_from_artist_mbid import ArtistCreditIdFromArtistMBIDQuery
 from listenbrainz.labs_api.labs.api.artist_credit_from_artist_msid import ArtistCreditIdFromArtistMSIDQuery
+from listenbrainz.labs_api.labs.api.msb_mapping_stats import MSBMappingStatsQuery
+from listenbrainz.labs_api.labs.api.recording_from_recording_mbid import RecordingFromRecordingMBIDQuery
 from listenbrainz.webserver import load_config
 
 register_query(ArtistCountryFromArtistMBIDQuery())
 register_query(ArtistCreditIdFromArtistMBIDQuery())
 register_query(ArtistCreditIdFromArtistMSIDQuery())
+register_query(MSBMappingStatsQuery())
+register_query(RecordingFromRecordingMBIDQuery())
 
 load_config(app)
