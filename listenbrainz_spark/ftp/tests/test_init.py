ed7fa5dc99e5e005996119a999eda8d0f0ecf44d
Ishaan Shah <ishaanshah@users.noreply.github.com>
2020-09-20 18:48:11 +0530

    Fix bug in SHA validation (#1098)

--- listenbrainz_spark/ftp/tests/test_init.py
+++ listenbrainz_spark/ftp/tests/test_init.py
@@ -91,6 +91,6 @@ class FTPTestCase(SparkTestCase):
     @patch('ftplib.FTP')
     def test_read_sha_file_(self, mock_ftp_cons):
         mock_ftp = mock_ftp_cons.return_value
-        with patch('listenbrainz_spark.ftp.open', mock_open(read_data='  test  filename  \n'), create=True) as mock_file:
+        with patch('listenbrainz_spark.ftp.open', mock_open(read_data='  test\n filename  \n'), create=True) as mock_file:
             result = listenbrainz_spark.ftp.ListenBrainzFTPDownloader()._read_sha_file("/sha_file.sha256")
             self.assertEqual('test', result)
