ebe9c89534730d55aa57ed0935567fd480ee41e5
shivam-kapila <shivamkapila4@gmail.com>
2020-09-21 17:07:12 +0530

    Fix error

--- listenbrainz/webserver/views/recommendations_cf_recording.py
+++ listenbrainz/webserver/views/recommendations_cf_recording.py
@@ -92,13 +92,17 @@ def _get_template(active_section, user):
             error_msg="An error occurred while processing your request. Check back later!"
         )
 
-    spotify_data = spotify.get_user_dict(current_user.id)
+    spotify_data = {}
+    current_user_data = {}
 
-    current_user_data = {
-            "id": current_user.id,
-            "name": current_user.musicbrainz_id,
-            "auth_token": current_user.auth_token,
-    }
+    if current_user:
+        spotify_data = spotify.get_user_dict(current_user.id)
+
+        current_user_data = {
+                "id": current_user.id,
+                "name": current_user.musicbrainz_id,
+                "auth_token": current_user.auth_token,
+        }
 
     props = {
         "user": {
