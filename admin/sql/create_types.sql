929bc26cbbb465b473999d216562b9734cba7fbb
vansika <vansikapareek2001@gmail.com>
2020-10-04 10:09:21 +0530

    schema for recommendation feedback

--- admin/sql/create_types.sql
+++ admin/sql/create_types.sql
@@ -3,3 +3,5 @@ CREATE TYPE cf_recording_type AS ENUM('top_artist', 'similar_artist');
 CREATE TYPE mb_missing_data_source_enum AS ENUM('cf', 'artist_map');
 
 CREATE TYPE user_relationship_enum AS ENUM('follow');
+
+CREATE TYPE feedback_type_enum AS ENUM('like_rec', 'love_rec', 'dislike_rec', 'hate_rec', 'bad_rec');
