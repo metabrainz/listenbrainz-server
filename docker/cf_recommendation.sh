11c7d13b95b794cbba5f3a5512e45dc9b28a7963
vansika <vansikapareek2001@gmail.com>
2020-10-15 07:37:05 +0530

    update crontab to get 1k recommendations

--- docker/cf_recommendation.sh
+++ docker/cf_recommendation.sh
@@ -4,7 +4,7 @@ function invoke_cf_recommendation_scripts {
     /usr/local/bin/python /code/listenbrainz/manage.py spark request_dataframes
     /usr/local/bin/python /code/listenbrainz/manage.py spark request_model
     /usr/local/bin/python /code/listenbrainz/manage.py spark request_candidate_sets
-    /usr/local/bin/python /code/listenbrainz/manage.py spark request_recommendations
+    /usr/local/bin/python /code/listenbrainz/manage.py spark request_recommendations --top=1000 --similar=1000
 }
 
 RECOMMENDATION_TYPE="$1"
