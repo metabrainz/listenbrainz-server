ecebbf088c1e76babe49ce7bb0e54ef0ab43b93e
Robert Kaye <rob@musicbrainz.org>
2020-10-21 12:30:16 +0200

    Minor fixes based on PR comments.

--- listenbrainz/labs_api/labs/api/recording_from_recording_mbid.py
+++ listenbrainz/labs_api/labs/api/recording_from_recording_mbid.py
@@ -36,8 +36,8 @@ class RecordingFromRecordingMBIDQuery(Query):
             with conn.cursor(cursor_factory=psycopg2.extras.DictCursor) as curs:
 
                 # First lookup and MBIDs that may have been redirected
-                query = '''SELECT rgr.gid AS recording_mbid_old,
-                                  r.gid AS recording_mbid_new
+                query = '''SELECT rgr.gid::TEXT AS recording_mbid_old,
+                                  r.gid::TEXT AS recording_mbid_new
                              FROM recording_gid_redirect rgr
                              JOIN recording r
                                ON r.id = rgr.new_id
@@ -54,16 +54,16 @@ class RecordingFromRecordingMBIDQuery(Query):
                         break
 
                     r = dict(row)
-                    redirect_index[str(r['recording_mbid_old'])] = str(r['recording_mbid_new'])
+                    redirect_index[r['recording_mbid_old']] = r['recording_mbid_new']
 
                 # Now start looking up actual recordings
                 for i, mbid in enumerate(mbids):
                     if mbid in redirect_index:
                         mbids[i] = redirect_index[mbid]
 
-                query = '''SELECT r.gid AS recording_mbid, r.name AS recording_name, r.length, r.comment,
+                query = '''SELECT r.gid::TEXT AS recording_mbid, r.name AS recording_name, r.length, r.comment,
                                   ac.id AS artist_credit_id, ac.name AS artist_credit_name,
-                                  array_agg(a.gid) AS artist_credit_mbids,
+                                  array_agg(a.gid)::TEXT[] AS artist_credit_mbids,
                                   first_release_date_year AS year
                              FROM recording r
                              JOIN artist_credit ac
@@ -90,43 +90,40 @@ class RecordingFromRecordingMBIDQuery(Query):
                 args = [tuple([psycopg2.extensions.adapt(p) for p in mbids])]
                 curs.execute(query, tuple(args))
 
-                # Build an index of all the fetched recordings and clean up UUID() -> str
+                # Build an index of all the fetched recordings
                 recording_index = {}
                 while True:
                     row = curs.fetchone()
                     if not row:
                         break
 
-                    r = dict(row)
-                    mbid = str(r['recording_mbid'])
-                    r['recording_mbid'] = mbid
-                    recording_index[mbid] = dict(row)
+                    recording_index[row['recording_mbid']] = dict(row)
 
-                # Finally collate all the results, ensuring that we have one entry with original_recording_mbid for each 
+                # Finally collate all the results, ensuring that we have one entry with original_recording_mbid for each
                 # input argument
                 output = []
                 for p in params:
-                    mbid = p['[recording_mbid]'] 
+                    mbid = p['[recording_mbid]']
                     try:
                         r = copy.copy(recording_index[mbid])
                     except KeyError:
                         try:
                             r = copy.copy(recording_index[redirect_index[mbid]])
                         except KeyError:
-                            out = {} 
-                            for k in self.outputs():
-                                if k == 'original_recording_mbid':
-                                    out['original_recording_mbid'] = mbid
-                                else:
-                                    out['original_recording_mbid'] = None
+                            out = dict.fromkeys(self.outputs(), None)
+                            out['original_recording_mbid'] = mbid
                             output.append(out)
                             continue
 
-                    r['[artist_credit_mbids]'] = list(set([str(r) for r in r['artist_credit_mbids']]))
+                    r['[artist_credit_mbids]'] = list(set([r for r in r['artist_credit_mbids']]))
                     del r['artist_credit_mbids']
                     r['original_recording_mbid'] = mbid
                     output.append(r)
 
+                # Ideally offset and count should be handled by the postgres query itself, but the 1:1 relationship
+                # of what the user requests and what we need to fetch is no longer true, so we can't easily use LIMIT/OFFSET.
+                # We might be able to use a RIGHT JOIN to fix this, but for now I'm happy to leave this as it. We need to
+                # revisit this when we get closer to pushing recommendation tools to production.
                 if offset > 0 and count > 0:
                     return output[offset:offset+count]
 
