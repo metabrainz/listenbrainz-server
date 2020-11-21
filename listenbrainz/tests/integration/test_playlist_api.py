87acc68c2d6571eac78e55c1fd50cc54f0ab905c
Robert Kaye <rob@musicbrainz.org>
2020-11-21 19:01:16 +0100

    Implement and add test cases for API endpoints for create/get playlist

---
+++ listenbrainz/tests/integration/test_playlist_api.py
@@ -0,0 +1,169 @@
+import ujson
+from uuid import UUID
+import listenbrainz.db.user as db_user
+import listenbrainz.db.playlist as db_playlist
+
+from redis import Redis
+from flask import url_for, current_app
+from listenbrainz.db.model.playlist import Playlist
+from listenbrainz.tests.integration import IntegrationTestCase
+
+
+class PlaylistAPITestCase(IntegrationTestCase):
+    def setUp(self):
+        super(PlaylistAPITestCase, self).setUp()
+        self.user = db_user.get_or_create(1, "testuserpleaseignore")
+        self.user2 = db_user.get_or_create(2, "anothertestuserpleaseignore")
+
+    def tearDown(self):
+        r = Redis(host=current_app.config['REDIS_HOST'], port=current_app.config['REDIS_PORT'])
+        r.flushall()
+        super(PlaylistAPITestCase, self).tearDown()
+
+    def get_test_data(self):
+        return {
+           "playlist" : {
+              "title" : "1980s flashback jams",
+              "track" : [
+                 {
+                    "identifier" : "https://musicbrainz.org/recording/e8f9b188-f819-4e43-ab0f-4bd26ce9ff56"
+                 }
+              ],
+           }
+        }
+
+    def test_playlist_create_and_get(self):
+        """ Test to ensure creating a playlist works """
+
+        playlist = get_test_data()
+
+        response = self.client.post(
+            url_for("playlist_api_v1.create_playlist"),
+            data=ujson.dumps(playlist),
+            headers={"Authorization": "Token {}".format(self.user["auth_token"])},
+            content_type="application/json"
+        )
+        self.assert200(response)
+        self.assertEqual(response.json["code"], 200)
+        self.assertNotEqual(response.json["playlist_mbid"], "")
+
+        playlist_mbid = response.json["playlist_mbid"]
+
+        # Make sure the return playlist id is valid
+        UUID(response.json["playlist_mbid"])
+
+
+        # Test to ensure fetching a playlist works
+        response = self.client.get(
+            url_for("playlist_api_v1.get_playlist", playlist_mbid=playlist_mbid),
+            headers={"Authorization": "Token {}".format(self.user["auth_token"])}
+        )
+        self.assert200(response)
+        self.assertEqual(response.json["playlist"]["playlist"]["creator"], "testuserpleaseignore")
+        self.assertEqual(response.json["playlist"]["playlist"]["identifier"], playlist_mbid)
+        self.assertEqual(response.json["playlist"]["playlist"]["track"][0]["identifier"],
+                         playlist["playlist"]["track"][0]["identifier"])
+
+    def test_playlist_get_non_existent(self):
+        """ Test to ensure fetching a non existent playlist gives 404 """
+
+        response = self.client.get(
+            url_for("playlist_api_v1.get_playlist", playlist_mbid="0ebcde36-1b14-4be5-ad3e-a088caf69134"),
+            headers={"Authorization": "Token {}".format(self.user["auth_token"])}
+        )
+        self.assert404(response)
+
+
+    def test_playlist_create_and_get_private(self):
+        """ Test to ensure creating a private playlist works """
+
+        playlist = get_test_data()
+
+        response = self.client.post(
+            url_for("playlist_api_v1.create_playlist", private="true"),
+            data=ujson.dumps(playlist),
+            headers={"Authorization": "Token {}".format(self.user["auth_token"])},
+            content_type="application/json"
+        )
+        self.assert200(response)
+        self.assertEqual(response.json["code"], 200)
+        self.assertNotEqual(response.json["playlist_mbid"], "")
+
+        playlist_mbid = response.json["playlist_mbid"]
+
+        # Test to ensure fetching a private playlist from a non-owner fails with 404
+        response = self.client.get(
+            url_for("playlist_api_v1.get_playlist", playlist_mbid=playlist_mbid),
+            headers={"Authorization": "Token {}".format(self.user2["auth_token"])}
+        )
+        self.assert404(response)
+
+    def test_playlist_create_empty(self):
+        """ Test to ensure creating an empty playlist works """
+
+        playlist = {
+           "playlist" : {
+              "title" : "yer dreams suck!"
+           }
+        }
+
+        response = self.client.post(
+            url_for("playlist_api_v1.create_playlist"),
+            data=ujson.dumps(playlist),
+            headers={"Authorization": "Token {}".format(self.user["auth_token"])},
+            content_type="application/json"
+        )
+        self.assert200(response)
+        self.assertEqual(response.json["code"], 200)
+        self.assertNotEqual(response.json["playlist_mbid"], "")
+
+        # Make sure the return playlist id is valid
+        UUID(response.json["playlist_mbid"])
+
+
+    def test_playlist_unauthorised_submission(self):
+        """ Test for checking that unauthorized submissions return 401 """
+
+        playlist = get_test_data()
+
+        # request with no authorization header
+        response = self.client.post(
+            url_for("playlist_api_v1.create_playlist"),
+            data=ujson.dumps(playlist),
+            content_type="application/json"
+        )
+        self.assert401(response)
+        self.assertEqual(response.json["code"], 401)
+
+        # request with invalid authorization header
+        response = self.client.post(
+            url_for("playlist_api_v1.create_playlist"),
+            data=ujson.dumps(playlist),
+            headers={"Authorization": "Token testtokenplsignore"},
+            content_type="application/json"
+        )
+        self.assert401(response)
+        self.assertEqual(response.json["code"], 401)
+
+    def test_playlist_json_with_missing_keys(self):
+        """ Test for checking that submitting JSON with missing keys returns 400 """
+
+        # submit a playlist without title
+        playlist = {
+           "playlist" : {
+              "track" : [
+                 {
+                    "identifier" : "https://musicbrainz.org/recording/e8f9b188-f819-4e43-ab0f-4bd26ce9ff56"
+                 }
+              ],
+           }
+        }
+
+        response = self.client.post(
+            url_for("playlist_api_v1.create_playlist"),
+            data=ujson.dumps(playlist),
+            headers={"Authorization": "Token {}".format(self.user["auth_token"])},
+            content_type="application/json"
+        )
+        self.assert400(response)
+        self.assertEqual(response.json["error"], "JSPF playlist must contain a title element with the title of the playlist.")
---
+++ listenbrainz/webserver/views/playlist_api.py
@@ -0,0 +1,163 @@
+import ujson
+from uuid import UUID
+import listenbrainz.db.user as db_user
+import listenbrainz.db.playlist as db_playlist
+
+from flask import Blueprint, current_app, jsonify, request
+from listenbrainz.webserver.decorators import crossdomain
+from listenbrainz.webserver.errors import (APIBadRequest,
+                                           APIInternalServerError, APINotFound,
+                                           APIServiceUnavailable,
+                                           APIUnauthorized)
+from listenbrainz.webserver.rate_limiter import ratelimit
+from listenbrainz.webserver.views.api import _validate_auth_header, _parse_int_arg
+from listenbrainz.webserver.views.api_tools import log_raise_400, is_valid_uuid,\
+    DEFAULT_ITEMS_PER_GET, MAX_ITEMS_PER_GET, _get_non_negative_param, parse_param_list
+from listenbrainz.db.model.playlist import Playlist, WritablePlaylist, PlaylistRecording, WritablePlaylistRecording
+from pydantic import ValidationError
+
+playlist_api_bp = Blueprint('playlist_api_v1', __name__)
+
+PLAYLIST_TRACK_URI_PREFIX = "https://musicbrainz.org/recording/"
+
+def _parse_boolean_arg(name, default=None):
+    value = request.args.get(name).lower()
+    if not value:
+        return default
+
+    if value not in ["true", "false"]:
+        raise APIBadRequest("Invalid %s argument: %s. Must be 'true' or 'false'" % (name, value))
+
+    return True if value == "true" else False
+
+
+def validate_playlist(jspf):
+    """
+        Given a JSPF dict, ensure that title is present and that if tracks are present
+        they have valid URIs or MBIDs specified. If any errors are found 400 is raised.
+    """
+
+    try:
+        title = jspf["playlist"]["title"]
+        if not title:
+            raise KeyError
+    except KeyError:
+        log_raise_400("JSPF playlist must contain a title element with the title of the playlist.")
+
+    if 'track' not in jspf:
+        return
+
+    for i, track in enumerate(jspf['track']):
+        try:
+            recording_uri = track["identifier"]
+            if not recording_uri:
+                raise KeyError
+        except KeyError:
+            log_raise_400("JSPF playlist track %d must contain an identifier element with recording MBID." % i)
+
+        if recording_uri.startswith(PLAYLIST_TRACK_URI_PREFIX):
+            recording_mbid = recording_uri[len(PLAYLIST_TRACK_URI_PREFIX):]
+        else:
+            recording_mbid = recording_uid
+
+        try:
+            uid = UUID(recording_mbid)
+        except ValueError:
+            log_raise_400("JSPF playlist track %d does not contain a valid track identifier field." % i)
+
+        track['mbid'] = recording_mbid
+            
+
+def serialize_jspf(playlist, user):
+    """
+        Given a playlist, return a properly formated dict that can be passed to jsonify.
+    """
+
+    pl = { "creator" : user["musicbrainz_id"],
+            "title" : playlist.title
+    }
+
+    tracks = []
+    for rec in playlist.recordings:
+        tracks.append({ "identifier": rec.mbid })
+
+    pl["track"] = tracks
+
+    return { "playlist": pl }
+
+
+@playlist_api_bp.route("/create", methods=["POST"])
+@crossdomain(headers="Authorization, Content-Type")
+@ratelimit()
+def create_playlist():
+    """
+    Create a playlist. The playlist must be in JSPF format with MusicBrainz extensions, which is defined
+    here: https://musicbrainz.org/doc/jspf . To create an empty playlist, you can send an empty playlist
+    with only the title field filled out. If you would like to create a playlist populated with recordings,
+    each of the track items in the playlist must have the identifier element present.
+
+    When creating a playlist, only the playlist tite and the track identifier elements will be used -- all
+    other elements in the posted JSPF wil be ignored.
+
+    :reqheader Authorization: Token <user token>
+    :statuscode 200: playlist accepted.
+    :statuscode 400: invalid JSON sent, see error message for details.
+    :statuscode 401: invalid authorization. See error message for details.
+    :resheader Content-Type: *application/json*
+    """
+
+    private = _parse_boolean_arg("private", "false")
+    user = _validate_auth_header()
+
+    data = request.json
+    validate_playlist(data)
+
+    playlist = WritablePlaylist()
+    playlist.name = data['playlist']['title']
+    playlist.creator = user["id"]
+    playlist.private = private
+
+    for track in jspf['track']:
+        pr = WritablePlaylistRecording()
+        pr.mbid = track['mbid']
+        playlist.recordings.append(pr)
+
+    try:
+        db_playlist.create(playlist)
+    except Exception as e:
+        current_app.logger.error("Error while new playlist: {}".format(e))
+        raise APIInternalServerError("Failed to create the playlist. Please try again.")
+
+    return jsonify({'status': 'ok', 'playlist_mbid': db_playlist.mbid })
+
+
+@playlist_api_bp.route("/<playlist_mbid>", methods=["GET"])
+@crossdomain()
+@ratelimit()
+def get_playlist(playlist_mbid):
+    """
+    Fetch the given playlist.
+
+    :param playlist_mbid: Optional, The playlist mbid to fetch.
+    :statuscode 200: Yay, you have data!
+    :statuscode 404: Playlist not found
+    :statuscode 401: Invalid authorization. See error message for details.
+    :resheader Content-Type: *application/json*
+    """
+    user = _validate_auth_header()
+
+    try:
+        uid = UUID(playlist_mbid)
+    except ValueError:
+        log_raise_400("Provided playlist ID is invalid.")
+
+    user = db_user.get_by_mb_id(user_name)
+    if user is None:
+        raise APINotFound("Cannot find user: %s" % user_name)
+
+    playlist = db_playlist.get_by_id(playlist_mbid)
+    if playlist is None or \
+        (playlist.creator_id != user["id"] and playlist.public == False):
+        raise APINotFound("Cannot find playlist: %s" % playlist_mbid)
+
+    return jsonify(serialize_jspf(playlist, user))
