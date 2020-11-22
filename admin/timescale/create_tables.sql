96f78b43addac1fa67864d3200c04bcd92ca64a2
Alastair Porter <alastair@metabrainz.org>
2020-11-22 13:50:25 +0100

    Load playlist tables into timescale on startup and during tests

--- admin/timescale/create_tables.sql
+++ admin/timescale/create_tables.sql
@@ -11,4 +11,28 @@ CREATE TABLE listen (
 -- 86400 seconds * 5 = 432000 seconds = 5 days
 SELECT create_hypertable('listen', 'listened_at', chunk_time_interval => 432000);
 
+-- Playlists
+
+CREATE TABLE playlist.playlist (
+    id serial,
+    mbid uuid not null default uuid_generate_v4(),
+    creator_id int not null, -- int, but not an fk because it's in the wrong database
+    name text not null,
+    description text,
+    public boolean not null,
+    created timestamp with time zone default now() not null,
+    copied_from int,
+    created_for_id int,
+    algorithm_metadata jsonb
+);
+
+CREATE TABLE playlist.playlist_recording (
+    id serial,
+    playlist_id int not null,  --FK playlist.id
+    position int not null,
+    mbid uuid not null,
+    added_by_id int not null,  -- int, but not an fk because it's in the wrong database
+    created timestamp with time zone default now() not null
+);
+
 COMMIT;
