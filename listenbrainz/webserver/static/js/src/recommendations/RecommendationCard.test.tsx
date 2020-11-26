3332884fee24a2ff6a5ae10b19dbdfd1c0fe8142
vansika <vansikapareek2001@gmail.com>
2020-11-27 03:10:45 +0530

    test interdependency of button and dropdown

--- listenbrainz/webserver/static/js/src/recommendations/RecommendationCard.test.tsx
+++ listenbrainz/webserver/static/js/src/recommendations/RecommendationCard.test.tsx
@@ -7,6 +7,16 @@ import {
   faSmileBeam,
   faGrinStars,
 } from "@fortawesome/free-solid-svg-icons";
+import RecommendationControl from "./RecommendationControl";
+
+import {
+  faThumbsUp as faThumbsUpRegular,
+  faAngry as faAngryRegular,
+  faFrown as faFrownRegular,
+  faMeh as faMehRegular,
+  faSmileBeam as faSmileBeamRegular,
+  faGrinStars as faGrinStarsRegular,
+} from "@fortawesome/free-regular-svg-icons";
 
 import RecommendationCard, {
   RecommendationCardProps,
@@ -71,6 +81,7 @@ describe("submitFeedback", () => {
       <RecommendationCard {...{ ...props, updateFeedback: jest.fn() }} />
     );
     const instance = wrapper.instance();
+    const updateFeedbackSpy = jest.spyOn(instance.props, "updateFeedback");
 
     const spy = jest.spyOn(instance.APIService, "submitRecommendationFeedback");
     spy.mockImplementation(() => Promise.resolve(200));
@@ -86,8 +97,8 @@ describe("submitFeedback", () => {
     expect(spy).toHaveBeenCalledTimes(1);
     expect(spy).toHaveBeenCalledWith("lalala", "yyyy", "hate");
 
-    expect(instance.props.updateFeedback).toHaveBeenCalledTimes(1);
-    expect(instance.props.updateFeedback).toHaveBeenCalledWith("yyyy", "hate");
+    expect(updateFeedbackSpy).toHaveBeenCalledTimes(1);
+    expect(updateFeedbackSpy).toHaveBeenCalledWith("yyyy", "hate");
 
     // Emulate updating the props after calling Recommendations component's updateFeedback
     wrapper.setProps({ currentFeedback: "hate" });
@@ -107,13 +118,14 @@ describe("submitFeedback", () => {
       />
     );
     const instance = wrapper.instance();
+    const updateFeedbackSpy = jest.spyOn(instance.props, "updateFeedback");
 
     const spy = jest.spyOn(instance.APIService, "submitRecommendationFeedback");
     spy.mockImplementation(() => Promise.resolve(200));
 
     instance.submitFeedback("dislike");
     expect(spy).toHaveBeenCalledTimes(0);
-    expect(instance.props.updateFeedback).toHaveBeenCalledTimes(0);
+    expect(updateFeedbackSpy).toHaveBeenCalledTimes(0);
     expect(wrapper.exists(".recommendation-controls")).toEqual(false);
   });
 
@@ -128,22 +140,23 @@ describe("submitFeedback", () => {
       />
     );
     const instance = wrapper.instance();
+    const updateFeedbackSpy = jest.spyOn(instance.props, "updateFeedback");
 
     const spy = jest.spyOn(instance.APIService, "submitRecommendationFeedback");
     spy.mockImplementation(() => Promise.resolve(200));
 
     instance.submitFeedback("love");
     expect(spy).toHaveBeenCalledTimes(0);
-    expect(instance.props.updateFeedback).toHaveBeenCalledTimes(0);
+    expect(updateFeedbackSpy).toHaveBeenCalledTimes(0);
     expect(wrapper.exists(".recommendation-controls")).toEqual(false);
   });
 
   it("doesn't call updateFeedback if status code is not 200", async () => {
     const wrapper = shallow<RecommendationCard>(
-      <RecommendationCard {...props} />
+      <RecommendationCard {...{ ...props, updateFeedback: jest.fn() }}/>
     );
     const instance = wrapper.instance();
-    props.updateFeedback = jest.fn();
+    const updateFeedbackSpy = jest.spyOn(instance.props, "updateFeedback");
 
     const spy = jest.spyOn(instance.APIService, "submitRecommendationFeedback");
     spy.mockImplementation(() => Promise.resolve(201));
@@ -153,20 +166,16 @@ describe("submitFeedback", () => {
     expect(spy).toHaveBeenCalledTimes(1);
     expect(spy).toHaveBeenCalledWith("lalala", "yyyy", "bad_recommendation");
 
-    expect(props.updateFeedback).toHaveBeenCalledTimes(0);
-    expect(
-      wrapper.find(".recommendation-controls").childAt(0).hasClass("love")
-    ).toEqual(true);
-    expect(wrapper.find('.recommendation-controls').childAt(0).childAt(0).prop("icon").iconName).toEqual("grin-stars");
-    expect(wrapper.find('.recommendation-controls').childAt(0).childAt(2).text()).toEqual("Love");
+    expect(updateFeedbackSpy).toHaveBeenCalledTimes(0);
+    expect(instance.props.currentFeedback).toEqual('love');
   });
 
   it("calls handleError if error is returned", async () => {
     const wrapper = shallow<RecommendationCard>(
-      <RecommendationCard {...props} />
+      <RecommendationCard {...{ ...props, updateFeedback: jest.fn() }} />
     );
     const instance = wrapper.instance();
-    props.updateFeedback = jest.fn();
+    const updateFeedbackSpy = jest.spyOn(instance.props, "updateFeedback");
     instance.handleError = jest.fn();
 
     const spy = jest.spyOn(instance.APIService, "submitRecommendationFeedback");
@@ -179,7 +188,7 @@ describe("submitFeedback", () => {
     expect(instance.handleError).toHaveBeenCalledWith(
       "Error while submitting recommendation feedback - error"
     );
-    expect(props.updateFeedback).toHaveBeenCalledTimes(0);
+    expect(updateFeedbackSpy).toHaveBeenCalledTimes(0);
     expect(
       wrapper.find(".recommendation-controls").childAt(0).hasClass("love")
     ).toEqual(true);
@@ -212,6 +221,7 @@ describe("deleteFeedback", () => {
       <RecommendationCard {...{ ...props, updateFeedback: jest.fn() }} />
     );
     const instance = wrapper.instance();
+    const updateFeedbackSpy = jest.spyOn(instance.props, "updateFeedback");
 
     const spy = jest.spyOn(instance.APIService, "deleteRecommendationFeedback");
     spy.mockImplementation(() => Promise.resolve(200));
@@ -230,8 +240,8 @@ describe("deleteFeedback", () => {
     expect(spy).toHaveBeenCalledTimes(1);
     expect(spy).toHaveBeenCalledWith("lalala", "yyyy");
 
-    expect(instance.props.updateFeedback).toHaveBeenCalledTimes(1);
-    expect(instance.props.updateFeedback).toHaveBeenCalledWith("yyyy", null);
+    expect(updateFeedbackSpy).toHaveBeenCalledTimes(1);
+    expect(updateFeedbackSpy).toHaveBeenCalledWith("yyyy", null);
     // Emulate updating the props after calling Recommendations component's updateFeedback
     wrapper.setProps({ currentFeedback: null });
 
@@ -245,44 +255,44 @@ describe("deleteFeedback", () => {
 
   it("does nothing if isCurrentUser is false", async () => {
     const wrapper = shallow<RecommendationCard>(
-      <RecommendationCard {...{ ...props, isCurrentUser: false }} />
+      <RecommendationCard {...{ ...props, isCurrentUser: false, updateFeedback: jest.fn() }} />
     );
     const instance = wrapper.instance();
-    props.updateFeedback = jest.fn();
+    const updateFeedbackSpy = jest.spyOn(instance.props, "updateFeedback");
 
     const spy = jest.spyOn(instance.APIService, "deleteRecommendationFeedback");
     spy.mockImplementation(() => Promise.resolve(200));
 
     instance.deleteFeedback();
     expect(spy).toHaveBeenCalledTimes(0);
-    expect(instance.props.updateFeedback).toHaveBeenCalledTimes(0);
+    expect(updateFeedbackSpy).toHaveBeenCalledTimes(0);
     expect(wrapper.exists(".recommendation-controls")).toEqual(false);
   });
 
   it("does nothing if CurrentUser.authtoken is not set", async () => {
     const wrapper = shallow<RecommendationCard>(
       <RecommendationCard
-        {...{ ...props, currentUser: { auth_token: undefined, name: "test" } }}
+        {...{ ...props, currentUser: { auth_token: undefined, name: "test"}, updateFeedback: jest.fn() }}
       />
     );
     const instance = wrapper.instance();
-    props.updateFeedback = jest.fn();
+    const updateFeedbackSpy = jest.spyOn(instance.props, "updateFeedback");
 
     const spy = jest.spyOn(instance.APIService, "deleteRecommendationFeedback");
     spy.mockImplementation(() => Promise.resolve(200));
 
     instance.deleteFeedback();
     expect(spy).toHaveBeenCalledTimes(0);
-    expect(instance.props.updateFeedback).toHaveBeenCalledTimes(0);
+    expect(updateFeedbackSpy).toHaveBeenCalledTimes(0);
     expect(wrapper.exists(".recommendation-controls")).toEqual(false);
   });
 
   it("doesn't call updateFeedback if status code is not 200", async () => {
     const wrapper = shallow<RecommendationCard>(
-      <RecommendationCard {...props} />
+      <RecommendationCard {...{ ...props, updateFeedback: jest.fn() }} />
     );
     const instance = wrapper.instance();
-    props.updateFeedback = jest.fn();
+    const updateFeedbackSpy = jest.spyOn(instance.props, "updateFeedback");
 
     const spy = jest.spyOn(instance.APIService, "deleteRecommendationFeedback");
     spy.mockImplementation(() => Promise.resolve(201));
@@ -292,16 +302,16 @@ describe("deleteFeedback", () => {
     expect(spy).toHaveBeenCalledTimes(1);
     expect(spy).toHaveBeenCalledWith("lalala", "yyyy");
 
-    expect(props.updateFeedback).toHaveBeenCalledTimes(0);
+    expect(updateFeedbackSpy).toHaveBeenCalledTimes(0);
   });
 
   it("calls handleError if error is returned", async () => {
     const wrapper = shallow<RecommendationCard>(
-      <RecommendationCard {...props} />
+      <RecommendationCard {...{ ...props, updateFeedback: jest.fn() }} />
     );
     const instance = wrapper.instance();
+    const updateFeedbackSpy = jest.spyOn(instance.props, "updateFeedback");
     instance.handleError = jest.fn();
-    props.updateFeedback = jest.fn();
 
     const spy = jest.spyOn(instance.APIService, "deleteRecommendationFeedback");
     spy.mockImplementation(() => {
@@ -313,6 +323,168 @@ describe("deleteFeedback", () => {
     expect(instance.handleError).toHaveBeenCalledWith(
       "Error while deleting recommendation feedback - error"
     );
-    expect(props.updateFeedback).toHaveBeenCalledTimes(0);
+    expect(updateFeedbackSpy).toHaveBeenCalledTimes(0);
+  });
+});
+
+describe("Check if button and dropdown values ae synced.", () => {
+  it("check button and dropdown values when currentFeedback == 'Hate' ", async () => {
+    const wrapper = shallow<RecommendationCard>(
+      <RecommendationCard {...{ ...props, currentFeedback: 'hate' }} />
+    );
+    const myComponents = wrapper.find(RecommendationControl);
+
+    // validate button class and properties
+    expect(
+      wrapper.find(".recommendation-controls").childAt(0).hasClass("hate")
+    ).toEqual(true);
+    expect(wrapper.find('.recommendation-controls').childAt(0).childAt(0).prop("icon").iconName).toEqual("angry");
+    expect(wrapper.find('.recommendation-controls').childAt(0).childAt(2).text()).toEqual("Hate");
+
+    // validate RecommendationComponent props for 'Angry' emoticon
+    expect(myComponents.get(0).props.iconHover).toEqual(faAngry);
+    expect(myComponents.get(0).props.icon).toEqual(faAngryRegular);
+    expect(myComponents.get(0).props.cssClass).toEqual('hate selected');
+    expect(myComponents.get(0).props.title).toEqual("I never want to hear this again!")
+
+    // validate RecommendationComponent props for 'dislike' emoticon
+    expect(myComponents.get(1).props.cssClass).toEqual('dislike ');
+
+    // validate RecommendationComponent props for 'Meh' emoticon
+    expect(myComponents.get(2).props.cssClass).toEqual('bad_recommendation ');
+
+    // validate RecommendationComponent props for 'like' emoticon
+    expect(myComponents.get(3).props.cssClass).toEqual('like ');
+
+    // validate RecommendationComponent props for 'love' emoticon
+    expect(myComponents.get(4).props.cssClass).toEqual('love ');
+  });
+
+  it("check button and dropdown values when currentFeedback == 'dislike' ", async () => {
+    const wrapper = shallow<RecommendationCard>(
+      <RecommendationCard {...{ ...props, currentFeedback: 'dislike' }} />
+    );
+    const myComponents = wrapper.find(RecommendationControl);
+
+    // validate button class and properties
+    expect(
+      wrapper.find(".recommendation-controls").childAt(0).hasClass("dislike")
+    ).toEqual(true);
+    expect(wrapper.find('.recommendation-controls').childAt(0).childAt(0).prop("icon").iconName).toEqual("frown");
+    expect(wrapper.find('.recommendation-controls').childAt(0).childAt(2).text()).toEqual("Dislike");
+
+    // validate RecommendationComponent props for 'dislike' emoticon
+    expect(myComponents.get(1).props.iconHover).toEqual(faFrown);
+    expect(myComponents.get(1).props.icon).toEqual(faFrownRegular);
+    expect(myComponents.get(1).props.cssClass).toEqual('dislike selected');
+    expect(myComponents.get(1).props.title).toEqual("I don't like this!");
+
+    // validate RecommendationComponent props for 'angry' emoticon
+    expect(myComponents.get(0).props.cssClass).toEqual('hate ');
+
+    // validate RecommendationComponent props for 'Meh' emoticon
+    expect(myComponents.get(2).props.cssClass).toEqual('bad_recommendation ');
+
+    // validate RecommendationComponent props for 'like' emoticon
+    expect(myComponents.get(3).props.cssClass).toEqual('like ');
+
+    // validate RecommendationComponent props for 'love' emoticon
+    expect(myComponents.get(4).props.cssClass).toEqual('love ');
+  });
+
+  it("check button and dropdown values when currentFeedback == 'bad_recommendation' ", async () => {
+    const wrapper = shallow<RecommendationCard>(
+      <RecommendationCard {...{ ...props, currentFeedback: 'bad_recommendation' }} />
+    );
+    const myComponents = wrapper.find(RecommendationControl);
+
+    // validate button class and properties
+    expect(
+      wrapper.find(".recommendation-controls").childAt(0).hasClass("bad_recommendation")
+    ).toEqual(true);
+    expect(wrapper.find('.recommendation-controls').childAt(0).childAt(0).prop("icon").iconName).toEqual("meh");
+    expect(wrapper.find('.recommendation-controls').childAt(0).childAt(2).text()).toEqual("Bad");
+
+    // validate RecommendationComponent props for 'Meh' emoticon
+    expect(myComponents.get(2).props.iconHover).toEqual(faMeh);
+    expect(myComponents.get(2).props.icon).toEqual(faMehRegular);
+    expect(myComponents.get(2).props.cssClass).toEqual('bad_recommendation selected');
+    expect(myComponents.get(2).props.title).toEqual("This is a bad recommendation!");
+
+    // validate RecommendationComponent props for 'angry' emoticon
+    expect(myComponents.get(0).props.cssClass).toEqual('hate ');
+
+    // validate RecommendationComponent props for 'dislike' emoticon
+    expect(myComponents.get(1).props.cssClass).toEqual('dislike ');
+
+    // validate RecommendationComponent props for 'like' emoticon
+    expect(myComponents.get(3).props.cssClass).toEqual('like ');
+
+    // validate RecommendationComponent props for 'love' emoticon
+    expect(myComponents.get(4).props.cssClass).toEqual('love ');
+  });
+
+  it("check button and dropdown values when currentFeedback == 'like' ", async () => {
+    const wrapper = shallow<RecommendationCard>(
+      <RecommendationCard {...{ ...props, currentFeedback: 'like' }} />
+    );
+    const myComponents = wrapper.find(RecommendationControl);
+
+    // validate button class and properties
+    expect(
+      wrapper.find(".recommendation-controls").childAt(0).hasClass("like")
+    ).toEqual(true);
+    expect(wrapper.find('.recommendation-controls').childAt(0).childAt(0).prop("icon").iconName).toEqual("smile-beam");
+    expect(wrapper.find('.recommendation-controls').childAt(0).childAt(2).text()).toEqual("Like");
+
+    // validate RecommendationComponent props for 'like' emoticon
+    expect(myComponents.get(3).props.iconHover).toEqual(faSmileBeam);
+    expect(myComponents.get(3).props.icon).toEqual(faSmileBeamRegular);
+    expect(myComponents.get(3).props.cssClass).toEqual('like selected');
+    expect(myComponents.get(3).props.title).toEqual("I like this!");
+
+    // validate RecommendationComponent props for 'angry' emoticon
+    expect(myComponents.get(0).props.cssClass).toEqual('hate ');
+
+    // validate RecommendationComponent props for 'dislike' emoticon
+    expect(myComponents.get(1).props.cssClass).toEqual('dislike ');
+
+    // validate RecommendationComponent props for 'bad_recommendation' emoticon
+    expect(myComponents.get(2).props.cssClass).toEqual('bad_recommendation ');
+
+    // validate RecommendationComponent props for 'love' emoticon
+    expect(myComponents.get(4).props.cssClass).toEqual('love ');
+  });
+
+  it("check button and dropdown values when currentFeedback == 'Hate' ", async () => {
+    const wrapper = shallow<RecommendationCard>(
+      <RecommendationCard {...{ ...props, currentFeedback: 'love' }} />
+    );
+    const myComponents = wrapper.find(RecommendationControl);
+
+    // validate button class and properties
+    expect(
+      wrapper.find(".recommendation-controls").childAt(0).hasClass("love")
+    ).toEqual(true);
+    expect(wrapper.find('.recommendation-controls').childAt(0).childAt(0).prop("icon").iconName).toEqual("grin-stars");
+    expect(wrapper.find('.recommendation-controls').childAt(0).childAt(2).text()).toEqual("Love");
+
+    // validate RecommendationComponent props for 'Love' emoticon
+    expect(myComponents.get(4).props.iconHover).toEqual(faGrinStars);
+    expect(myComponents.get(4).props.icon).toEqual(faGrinStarsRegular);
+    expect(myComponents.get(4).props.cssClass).toEqual('love selected');
+    expect(myComponents.get(4).props.title).toEqual("I really love this!")
+
+    // validate RecommendationComponent props for 'dislike' emoticon
+    expect(myComponents.get(1).props.cssClass).toEqual('dislike ');
+
+    // validate RecommendationComponent props for 'Meh' emoticon
+    expect(myComponents.get(2).props.cssClass).toEqual('bad_recommendation ');
+
+    // validate RecommendationComponent props for 'like' emoticon
+    expect(myComponents.get(3).props.cssClass).toEqual('like ');
+
+    // validate RecommendationComponent props for 'angry' emoticon
+    expect(myComponents.get(0).props.cssClass).toEqual('hate ');
   });
 });
