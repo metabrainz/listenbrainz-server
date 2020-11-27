ff5f2435a67927c0fa2ac3208eba4e73994a0cfe
Monkey Do <contact@monkeydo.digital>
2020-11-27 12:04:37 +0100

    Correction of SQL column name
    copied_from -> copied_from_id

--- admin/timescale/updates/2020-11-21-playlists.sql
+++ admin/timescale/updates/2020-11-21-playlists.sql
@@ -26,7 +26,7 @@ CREATE TABLE playlist.playlist_recording (
 
 CREATE UNIQUE INDEX mbid_playlist ON playlist.playlist (mbid);
 CREATE INDEX creator_id_playlist ON playlist.playlist (creator_id);
-CREATE INDEX copied_from_id_playlist ON playlist.playlist (copied_from);
+CREATE INDEX copied_from_id_playlist ON playlist.playlist (copied_from_id);
 CREATE INDEX created_for_id_playlist ON playlist.playlist (created_for_id);
 
 CREATE INDEX playlist_id_playlist_recording ON playlist.playlist_recording (playlist_id);
