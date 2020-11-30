6c83e2129dbc5ee70bef5ad346fd1ff6543c4347
Alastair Porter <alastair@metabrainz.org>
2020-11-30 17:01:16 +0100

    Make timescale test setup part of generic testing code

--- listenbrainz/db/testing.py
+++ listenbrainz/db/testing.py
@@ -4,15 +4,16 @@ import unittest
 
 from listenbrainz import config
 from listenbrainz import db
+from listenbrainz.db import timescale as ts
 
 ADMIN_SQL_DIR = os.path.join(os.path.dirname(os.path.realpath(__file__)), '..', '..', 'admin', 'sql')
 TEST_DATA_PATH = os.path.join(os.path.dirname(os.path.realpath(__file__)), '..', 'testdata')
+TIMESCALE_SQL_DIR = os.path.join(os.path.dirname(os.path.realpath(__file__)), '..', '..', 'admin', 'timescale')
 
 
 class DatabaseTestCase(unittest.TestCase):
 
     def setUp(self):
-        self.config = config
         db.init_db_connection(config.SQLALCHEMY_DATABASE_URI)
         self.reset_db()
 
@@ -44,3 +45,29 @@ class DatabaseTestCase(unittest.TestCase):
                 file_name: the name of the data file
         """
         return os.path.join(TEST_DATA_PATH, file_name)
+
+
+class TimescaleTestCase(unittest.TestCase):
+
+    def setUp(self):
+        # Reset the database in setUp, this way if there is a test failure and you want
+        # to inspect data, it won't have been cleared away
+        self.reset_timescale_db()
+
+    def reset_timescale_db(self):
+        ts.init_db_connection(config.TIMESCALE_ADMIN_URI)
+        ts.run_sql_script_without_transaction(os.path.join(TIMESCALE_SQL_DIR, 'drop_db.sql'))
+        ts.run_sql_script_without_transaction(os.path.join(TIMESCALE_SQL_DIR, 'create_db.sql'))
+        ts.engine.dispose()
+
+        ts.init_db_connection(config.TIMESCALE_ADMIN_LB_URI)
+        ts.run_sql_script_without_transaction(os.path.join(TIMESCALE_SQL_DIR, 'create_extensions.sql'))
+        ts.engine.dispose()
+
+        ts.init_db_connection(config.SQLALCHEMY_TIMESCALE_URI)
+        ts.run_sql_script_without_transaction(os.path.join(TIMESCALE_SQL_DIR, 'create_extensions.sql'))
+        ts.run_sql_script(os.path.join(TIMESCALE_SQL_DIR, 'create_tables.sql'))
+        ts.run_sql_script(os.path.join(TIMESCALE_SQL_DIR, 'create_functions.sql'))
+        ts.run_sql_script(os.path.join(TIMESCALE_SQL_DIR, 'create_views.sql'))
+        ts.run_sql_script(os.path.join(TIMESCALE_SQL_DIR, 'create_indexes.sql'))
+        ts.engine.dispose()
