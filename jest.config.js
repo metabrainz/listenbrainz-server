1d1b2414c9c55caa02d4ef60508853818e794e87
Monkey Do <contact@monkeydo.digital>
2020-12-09 16:28:27 +0100

    Add junit report format for CI tests
    For Jest tests (front-end)

--- jest.config.js
+++ jest.config.js
@@ -2,40 +2,43 @@
 // https://jestjs.io/docs/en/configuration.html
 
 module.exports = {
-    preset: 'ts-jest',
+  preset: "ts-jest",
 
-    // Automatically clear mock calls and instances between every test
-    clearMocks: true,
+  // Automatically clear mock calls and instances between every test
+  clearMocks: true,
 
-    // An array of glob patterns indicating a set of files for which coverage information should be collected
-    collectCoverageFrom: ['<rootDir>/static/js/**/*.{ts,tsx,js,jsx}'],
+  // An array of glob patterns indicating a set of files for which coverage information should be collected
+  collectCoverageFrom: ["<rootDir>/static/js/**/*.{ts,tsx,js,jsx}"],
 
-    // The directory where Jest should output its coverage files
-    coverageDirectory: 'coverage',
+  // The directory where Jest should output its coverage files
+  coverageDirectory: "coverage",
 
-    // An array of file extensions your modules use
-    moduleFileExtensions: ['ts', 'tsx', 'js', 'json', 'jsx'],
+  // An array of file extensions your modules use
+  moduleFileExtensions: ["ts", "tsx", "js", "json", "jsx"],
 
-    // The paths to modules that run some code to configure or set up the testing environment before each test
-    setupFiles: ['<rootDir>/enzyme.config.ts'],
+  // Report with junit format for Travis
+  reporters: ["default", "jest-junit"],
 
-    // The test environment that will be used for testing
-    testEnvironment: 'jsdom',
+  // The paths to modules that run some code to configure or set up the testing environment before each test
+  setupFiles: ["<rootDir>/enzyme.config.ts"],
 
-    // The glob patterns Jest uses to detect test files
-    testMatch: ['**/?(*.)+(spec|test).(ts|js)?(x)'],
+  // The test environment that will be used for testing
+  testEnvironment: "jsdom",
 
-    // An array of regexp pattern strings that are matched against all test paths, matched tests are skipped
-    testPathIgnorePatterns: ['\\\\node_modules\\\\'],
+  // The glob patterns Jest uses to detect test files
+  testMatch: ["**/?(*.)+(spec|test).(ts|js)?(x)"],
 
-    // This option sets the URL for the jsdom environment. It is reflected in properties such as location.href
-    testURL: 'http://localhost',
+  // An array of regexp pattern strings that are matched against all test paths, matched tests are skipped
+  testPathIgnorePatterns: ["\\\\node_modules\\\\"],
 
-    // An array of regexp pattern strings that are matched against all source file paths, matched files will skip transformation
-    transformIgnorePatterns: ['<rootDir>/node_modules/'],
+  // This option sets the URL for the jsdom environment. It is reflected in properties such as location.href
+  testURL: "http://localhost",
 
-    // Indicates whether each individual test should be reported during the run
-    verbose: true,
-    
-    snapshotSerializers: ["enzyme-to-json/serializer"]
-  };
+  // An array of regexp pattern strings that are matched against all source file paths, matched files will skip transformation
+  transformIgnorePatterns: ["<rootDir>/node_modules/"],
+
+  // Indicates whether each individual test should be reported during the run
+  verbose: true,
+
+  snapshotSerializers: ["enzyme-to-json/serializer"],
+};
