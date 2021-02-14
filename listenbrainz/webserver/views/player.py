3bcf5913c64b510c606cc89c922a2bc4c98b8483
vansika <vansikapareek2001@gmail.com>
2021-02-14 15:33:16 +0530

    use recommendation feedback in BP player

--- listenbrainz/webserver/views/player.py
+++ listenbrainz/webserver/views/player.py
@@ -43,17 +43,21 @@ def load():
             error_msg="'Listens' array must have one or more items."
         )
 
-    user_data = {
+    current_user_data = {
         "id": current_user.id,
         "name": current_user.musicbrainz_id,
         "auth_token": current_user.auth_token,
     }
     spotify_data = spotify.get_user_dict(current_user.id)
     props = {
-        "user": user_data,
+        "user": {
+            "id": current_user.id,
+            "name": current_user.musicbrainz_id,
+        },
+        "current_user": current_user_data,
         "spotify": spotify_data,
         "api_url": current_app.config["API_URL"],
-        "listens": listens
+        "recommendations": listens
     }
 
     return render_template(
