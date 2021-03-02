d13481315d8fe44d3a6560f1c9eea98ac048ca64
Param Singh <iliekcomputers@gmail.com>
2021-03-02 14:25:44 +0000

    Add new table for user recommendation events (#1309)
    * Add new table for user recommendation events
    
    * Rename the table
    
    * Add indexes
    
    * Drop the table too

--- admin/sql/create_tables.sql
+++ admin/sql/create_tables.sql
@@ -89,6 +89,14 @@ CREATE TABLE recommendation.recording_session (
   session_id          INTEGER NOT NULL --FK to recommendation.recommender_session.id
 );
 
+CREATE TABLE user_timeline_event (
+  id                    SERIAL, -- PK
+  user_id               INTEGER, -- FK to "user"
+  event_type            user_timeline_event_type_enum,
+  metadata              JSONB,
+  created               TIMESTAMP WITH TIME ZONE DEFAULT NOW() NOT NULL
+);
+
 CREATE TABLE spotify_auth (
   user_id                   INTEGER NOT NULL, -- PK and FK to user.id
   user_token                VARCHAR NOT NULL,
--- admin/sql/drop_tables.sql
+++ admin/sql/drop_tables.sql
@@ -8,5 +8,6 @@ DROP TABLE IF EXISTS recording_feedback             CASCADE;
 DROP TABLE IF EXISTS missing_musicbrainz_data       CASCADE;
 DROP TABLE IF EXISTS user_relationship              CASCADE;
 DROP TABLE IF EXISTS recommendation_feedback        CASCADE;
+DROP TABLE IF EXISTS user_timeline_event            CASCADE;
 
 COMMIT;
