405befd3fee3384206544a8844483952abd0a860
Monkey Do <contact@monkeydo.digital>
2021-03-17 19:03:25 +0100

    Fix charts and reports pages for old browsers
    Some of our dependencies of our graph libraries do not generate ES2015 code, therefore breaking on of versions of Firefox (< v.45) and IE.
    
    This commit adds those libraries I found (there may be others) to our babel transpilation workflow.
    Dependencies are usually expected to be ES2015 and the node_modules folder is ignored by babel by default.

--- webpack.config.js
+++ webpack.config.js
@@ -47,7 +47,8 @@ module.exports = function (env) {
       rules: [
         {
           test: /\.(js|ts)x?$/,
-          exclude: /node_modules/,
+          // some nivo/D3 dependencies need to be transpiled, we include them with the following regex
+          exclude: /node_modules\/(?!(d3-array|d3-scale|internmap)\/).*/,
           use: {
             loader: "babel-loader",
             options: {
