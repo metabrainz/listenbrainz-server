555e4612b3334a84e071c06b39732b127edc5920
Robert Kaye <rob@musicbrainz.org>
2021-03-24 17:54:01 +0100

    Added the needed exception handling

--- listenbrainz/mbid_mapping_writer/job_queue.py
+++ listenbrainz/mbid_mapping_writer/job_queue.py
@@ -1,6 +1,7 @@
 from queue import PriorityQueue, Queue, Empty
 from  concurrent.futures import ThreadPoolExecutor, wait, FIRST_COMPLETED
 import threading
+import traceback
 from time import sleep
 
 from flask import current_app
@@ -10,8 +11,8 @@ MAX_THREADS = 4
 MAX_QUEUED_JOBS = MAX_THREADS * 2
 
 
-def lookup_new_listens(listens, delivery_tag):
-#    current_app.logger.info("listen lookup!")
+def lookup_new_listens(app, listens, delivery_tag):
+    app.logger.info("listen lookup!")
     return delivery_tag
 
 
@@ -39,6 +40,7 @@ class MappingJobQueue(threading.Thread):
         return tags
 
     def add_new_listens(self, listens, delivery_tag):
+        self.app.logger.info("delivery tag %s" % str(delivery_tag))
         self.queue.put((self.priority, listens, delivery_tag))
         self.priority += 1
 
@@ -48,25 +50,38 @@ class MappingJobQueue(threading.Thread):
 
     def run(self):
         self.app.logger.info("start job queue thread")
-        with self.app.app_context():
-            with ThreadPoolExecutor(max_workers=MAX_THREADS) as executor:
-                futures = {}
-                while not self.done:
 
-                    completed, uncompleted = wait(futures, return_when=FIRST_COMPLETED)
-                    for complete in completed:
-                        self.delivery_tag_queue.put(complete.result())
-                        del futures[complete]
+        try:
+            with self.app.app_context():
+                with ThreadPoolExecutor(max_workers=MAX_THREADS) as executor:
+                    futures = {}
+                    while not self.done:
 
-                    for i in range(MAX_QUEUED_JOBS - len(uncompleted)):
-                        try:
-                            job = self.queue.get(False)
-                        except Empty:
-                            break
+                        completed, uncompleted = wait(futures, return_when=FIRST_COMPLETED)
+                        for complete in completed:
+                            self.app.logger.info("job %d complete" % (futures[complete]))
+                            self.app.logger.info(str(complete))
 
-                        if job[0] > 0:
-                            futures[executor.submit(lookup_new_listens, job[1], job[2])] = job[0]
-                        else:
-                            self.app.logger.info("Unsupported job type in MappingJobQueue (MBID Mapping Writer).")
+                            exc = complete.exception()
+                            if exc:
+                                # TODO: What happens to items that fail??
+                                self.app.logger.error("\n".join(traceback.format_tb(exc.__traceback__)))
+                            else:
+                                self.delivery_tag_queue.put(complete.result())
+                            del futures[complete]
+
+                        for i in range(MAX_QUEUED_JOBS - len(uncompleted)):
+                            try:
+                                job = self.queue.get(False)
+                            except Empty:
+                                break
+
+                            if job[0] > 0:
+                                self.app.logger.info("submit job %d" % (job[0]))
+                                futures[executor.submit(lookup_new_listens, self.app, job[1], job[2])] = job[0]
+                            else:
+                                self.app.logger.info("Unsupported job type in MappingJobQueue (MBID Mapping Writer).")
+        except Exception as err:
+            self.app.logger.info(traceback.format_exc())
 
         self.app.logger.info("job queue thread finished")
