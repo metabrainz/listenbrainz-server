b7a1209c8af1ed0d50c245acd5871134a4a6570b
Kartik Ohri <kartikohri13@gmail.com>
2021-03-25 20:54:34 +0530

    Add api endpoint for approved bots to post on user's timeline (#1342)
    * Add API for approved users to post to a user's timeline
    
    (cherry picked from commit 9b58f1b5f1ebc50dcda7f6382a3b849e32cbbf0d)
    
    * Fix TypeError: Cannot instantiate typing.Union
    
    Instantiating the union type only works if it contains a single type
    otherwise we need to instantiate the actual type.
    
    (cherry picked from commit 0ab4634036297c51ce7eb8bd84f7fa310fd3dc68)
    
    * Add notification to user_timeline_event_type_enum
    
    (cherry picked from commit aea0670fd6e5159c9613b48424813c2e1acb38a6)
    
    * Add test for create_user_notification_event
    
    (cherry picked from commit 24ffbef8d58b282bc8c6f7d94eeb4260b3a4875f)
    
    * Fix url and return in case of success
    
    (cherry picked from commit 1401c55bd34427aeee08fddf0a026c1ade0914ec)
    
    * Add tests for create notification api
    
    (cherry picked from commit 6357ac5196b9ba458255f18715094c4e6923b8f3)
    
    * Fix changes from code review
    
    * Combine message and link into single field
    
    * Fix test and throw APIForbidden instead of APIUnauthorized

--- listenbrainz/tests/integration/test_user_timeline_event_api.py
+++ listenbrainz/tests/integration/test_user_timeline_event_api.py
@@ -16,7 +16,7 @@
 # with this program; if not, write to the Free Software Foundation, Inc.,
 # 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
 
-from flask import url_for
+from flask import url_for, current_app
 from listenbrainz.db.exceptions import DatabaseException
 from listenbrainz.tests.integration import ListenAPIIntegrationTestCase
 from unittest import mock
@@ -135,3 +135,27 @@ class UserTimelineAPITestCase(ListenAPIIntegrationTestCase):
         self.assert401(r)
         data = json.loads(r.data)
         self.assertEqual("You don't have permissions to post to this user's timeline.", data['error'])
+
+    def test_post_notification_authorization_fails(self):
+        metadata = {
+            "message": "Testing",
+            "link": "http://localhost"
+        }
+        r = self.client.post(
+            url_for('user_timeline_event_api_bp.create_user_notification_event', user_name=self.user['musicbrainz_id']),
+            data=json.dumps({"metadata": metadata}),
+            headers={'Authorization': 'Token {}'.format(self.user['auth_token'])}
+        )
+        self.assert403(r)
+        data = json.loads(r.data)
+        self.assertEqual("Only approved users are allowed to post a message on a user's timeline.", data['error'])
+
+    def test_post_notification_success(self):
+        metadata = {"message": 'You have a <a href="https://listenbrainz.org/non-existent-playlist">playlist</a>'}
+        approved_user = db_user.get_or_create(11, "troi-bot")
+        r = self.client.post(
+            url_for('user_timeline_event_api_bp.create_user_notification_event', user_name=self.user['musicbrainz_id']),
+            data=json.dumps({"metadata": metadata}),
+            headers={'Authorization': 'Token {}'.format(approved_user['auth_token'])}
+        )
+        self.assert200(r)
