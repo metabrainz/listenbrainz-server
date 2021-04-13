c0b6ba401534142bd851ba11d3f28fbeebcc0170
Kartik Ohri <kartikohri13@gmail.com>
2021-04-13 15:05:10 +0530

    Filter images based on name

--- docker/jenkins/unit.sh
+++ docker/jenkins/unit.sh
@@ -25,7 +25,7 @@ function cleanup {
                    down --remove-orphans
     docker images --filter="before=${COMPOSE_PROJECT_NAME}_listenbrainz" --filter "label=org.label-schema.name=ListenBrainz" --format '{{.Repository}}:{{.Tag}}'
     # Untag LB images that were built before this test run
-    docker image rm "$(docker images --filter="before=${COMPOSE_PROJECT_NAME}_listenbrainz" --filter "label=org.label-schema.name=ListenBrainz" --format '{{.Repository}}:{{.Tag}}')"
+    docker image rm "$(docker images --filter="reference=listenbrainzunittestjenkinsbuild*_listenbrainz" --filter="before=${COMPOSE_PROJECT_NAME}_listenbrainz" --filter "label=org.label-schema.name=ListenBrainz" --format '{{.Repository}}:{{.Tag}}')"
 }
 
 function run_tests {
