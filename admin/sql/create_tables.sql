59d33981a3d59b000860cd01c6ba2a62cd788857
Kartik Ohri <kartikohri13@gmail.com>
2021-04-14 17:06:36 +0530

    Add SQL scripts for connecting new music services and migrating user data (#1384)
    * Add new external_auth table for music services
    
    * Add migration script for moving spotify users data to new tables
    
    * Rename external_auth to external_service_oauth and external_auth_service to external_service_oauth_type.
    
    * Add indices for external_service_oauth table
    
    * Rename files to make order of application clear
    
    * Fix indent

--- admin/sql/create_tables.sql
+++ admin/sql/create_tables.sql
@@ -104,6 +104,18 @@ CREATE TABLE spotify_auth (
   permission                VARCHAR NOT NULL
 );
 
+CREATE TABLE external_service_oauth (
+    id                      SERIAL,
+    user_id                 INTEGER NOT NULL,
+    service                 external_service_oauth_type NOT NULL,
+    access_token            TEXT NOT NULL,
+    refresh_token           TEXT,
+    token_expires           TIMESTAMP WITH TIME ZONE,
+    last_updated            TIMESTAMP WITH TIME ZONE DEFAULT NOW(),
+    record_listens          BOOLEAN NOT NULL,
+    service_details         JSONB
+);
+
 CREATE TABLE statistics.artist (
     id                      SERIAL, -- PK
     msid                    UUID NOT NULL,
--- admin/sql/create_types.sql
+++ admin/sql/create_types.sql
@@ -7,3 +7,5 @@ CREATE TYPE user_relationship_enum AS ENUM('follow');
 CREATE TYPE recommendation_feedback_type_enum AS ENUM('like', 'love', 'dislike', 'hate', 'bad_recommendation');
 
 CREATE TYPE user_timeline_event_type_enum AS ENUM('recording_recommendation', 'notification');
+
+CREATE TYPE external_service_oauth_type AS ENUM ('spotify', 'youtube');
