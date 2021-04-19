ad644093c71d3f42cf5bf527b42ba24e47512d44
Kartik Ohri <kartikohri13@gmail.com>
2021-04-19 14:25:10 +0530

    Catch and return exit code of tests

--- test.sh
+++ test.sh
@@ -1,9 +1,5 @@
 #!/bin/bash
 
-if [ "$CI" == "true" ]; then
-    set -e
-fi
-
 # UNIT TESTS
 # ./test.sh                build unit test containers, bring up, make database, test, bring down
 # for development:
@@ -211,8 +207,9 @@ if [ "$1" == "spark" ]; then
     docker-compose -f $SPARK_COMPOSE_FILE_LOC \
                    -p $SPARK_COMPOSE_PROJECT_NAME \
                 up test
+    RET=$?
     spark_dcdown
-    exit 0
+    exit $RET
 fi
 
 if [ "$1" == "int" ]; then
@@ -245,9 +242,10 @@ if [ "$1" == "int" ]; then
                   -wait tcp://redis:6379 -timeout 60s \
                   -wait tcp://rabbitmq:5672 -timeout 60s \
                 bash -c "pytest $TESTS_TO_RUN"
+    RET=$?
     echo "Taking containers down"
     int_dcdown
-    exit 0
+    exit $RET
 fi
 
 # Project name is sanitized by Compose, so we need to do the same thing.
@@ -272,18 +270,18 @@ if [ "$1" == "fe" ]; then
     if [ "$2" == "-t" ]; then
         echo "Running type checker"
         run_type_check
-        exit 0
+        exit $?
     fi
 
     if [ "$2" == "-f" ]; then
         echo "Running linter"
         run_lint_check
-        exit 0
+        exit $?
     fi
 
     echo "Running tests"
     run_frontend_tests
-    exit 0
+    exit $?
 fi
 
 if [ "$1" == "-s" ]; then
@@ -329,11 +327,14 @@ if [ $DB_EXISTS -eq 1 -a $DB_RUNNING -eq 1 ]; then
     docker-compose -f $COMPOSE_FILE_LOC \
                    -p $COMPOSE_PROJECT_NAME \
                 run --rm listenbrainz pytest "$@"
+    RET=$?
     unit_dcdown
+    exit $RET
 else
     # Else, we have containers, just run tests
     echo "Running tests"
     docker-compose -f $COMPOSE_FILE_LOC \
                    -p $COMPOSE_PROJECT_NAME \
                 run --rm listenbrainz pytest "$@"
+    exit $?
 fi
