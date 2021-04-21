265cf3046463a42a028fdbffca4b590240b5b32e
Kartik Ohri <kartikohri13@gmail.com>
2021-04-21 15:08:39 +0530

    Migrate spotify users to latest schema

---
+++ admin/sql/updates/2021-04-21-3-migrate-existing-spotify-users-second-version.sql
@@ -0,0 +1,44 @@
+BEGIN;
+
+WITH migration AS
+    (
+        INSERT INTO external_service_oauth
+        (
+         user_id,
+         service,
+         access_token,
+         refresh_token,
+         token_expires,
+         last_updated,
+         record_listens,
+         scopes
+        )
+        SELECT
+            user_id,
+            'spotify' as service,
+            user_token as access_token,
+            refresh_token,
+            token_expires,
+            last_updated,
+            record_listens,
+            string_to_array(permission, ' ')
+        FROM spotify_auth
+        RETURNING external_service_oauth.id as external_service_oauth_id
+    )
+INSERT INTO listens_importer
+    (
+     external_service_oauth_id,
+     user_id,
+     service,
+     latest_listened_at,
+     error_message
+    )
+SELECT
+    external_service_oauth_id,
+    user_id,
+    'spotify',
+    latest_listened_at,
+    error_message
+FROM spotify_auth, migration;
+
+COMMIT;
