2672773bd01525de17f79dbab83901556a727744
Robert Kaye <rob@musicbrainz.org>
2021-04-21 11:14:29 +0200

    Pepperson

--- listenbrainz/listenstore/timescale_utils.py
+++ listenbrainz/listenstore/timescale_utils.py
@@ -21,18 +21,20 @@ def recalculate_all_user_data():
 
     timescale.init_db_connection(config.SQLALCHEMY_TIMESCALE_URI)
     db.init_db_connection(config.SQLALCHEMY_DATABASE_URI)
-    init_cache(host=config.REDIS_HOST, port=config.REDIS_PORT, namespace=config.REDIS_NAMESPACE)
-
+    init_cache(host=config.REDIS_HOST, port=config.REDIS_PORT,
+               namespace=config.REDIS_NAMESPACE)
 
     # Find the created timestamp of the last listen
     query = "SELECT max(created) FROM listen WHERE created > :date"
     try:
         with timescale.engine.connect() as connection:
-            result = connection.execute(sqlalchemy.text(query), date=datetime.now() - timedelta(weeks=4))
+            result = connection.execute(sqlalchemy.text(
+                query), date=datetime.now() - timedelta(weeks=4))
             row = result.fetchone()
             last_created_ts = row[0]
     except psycopg2.OperationalError as e:
-        logger.error("Cannot query ts to fetch latest listen." % str(e), exc_info=True)
+        logger.error("Cannot query ts to fetch latest listen." %
+                     str(e), exc_info=True)
         raise
 
     logger.info("Last created timestamp: " + str(last_created_ts))
@@ -46,10 +48,12 @@ def recalculate_all_user_data():
             for row in result:
                 user_list.append(row[0])
     except psycopg2.OperationalError as e:
-        logger.error("Cannot query db to fetch user list." % str(e), exc_info=True)
+        logger.error("Cannot query db to fetch user list." %
+                     str(e), exc_info=True)
         raise
 
-    logger.info("Fetched %d users. Setting empty cache entries." % len(user_list))
+    logger.info("Fetched %d users. Setting empty cache entries." %
+                len(user_list))
 
     # Reset the timestamps and listen counts to 0 for all users
     for user_name in user_list:
@@ -61,10 +65,11 @@ def recalculate_all_user_data():
     logger.info("Scan the whole listen table...")
     listen_counts = defaultdict(int)
     user_timestamps = {}
-    query = "SELECT listened_at, user_name FROM listen where created <= :ts" 
+    query = "SELECT listened_at, user_name FROM listen where created <= :ts"
     try:
         with timescale.engine.connect() as connection:
-            result = connection.execute(sqlalchemy.text(query), ts=last_created_ts)
+            result = connection.execute(
+                sqlalchemy.text(query), ts=last_created_ts)
             for row in result:
                 ts = row[0]
                 user_name = row[1]
@@ -79,14 +84,16 @@ def recalculate_all_user_data():
                 listen_counts[user_name] += 1
 
     except psycopg2.OperationalError as e:
-        logger.error("Cannot query db to fetch user list." % str(e), exc_info=True)
+        logger.error("Cannot query db to fetch user list." %
+                     str(e), exc_info=True)
         raise
 
     logger.info("Setting updated cache entries.")
     # Set the timestamps and listen counts for all users
     for user_name in user_list:
         try:
-            cache._r.incrby(cache._prep_key(REDIS_USER_LISTEN_COUNT + user_name), listen_counts[user_name])
+            cache._r.incrby(cache._prep_key(
+                REDIS_USER_LISTEN_COUNT + user_name), listen_counts[user_name])
         except KeyError:
             pass
 
@@ -99,6 +106,7 @@ def recalculate_all_user_data():
                 user_timestamps[user_name][0] = min_ts
             if max_ts and max_ts > user_timestamps[user_name][1]:
                 user_timestamps[user_name][1] = max_ts
-            cache.set(REDIS_USER_TIMESTAMPS + user_name, "%d,%d" % (user_timestamps[user_name][0], user_timestamps[user_name][1]), time=0)
+            cache.set(REDIS_USER_TIMESTAMPS + user_name, "%d,%d" %
+                      (user_timestamps[user_name][0], user_timestamps[user_name][1]), time=0)
         except KeyError:
             pass
