dcf87ef0a55e193643593bb0ee53740cbcfe4a87
Robert Kaye <rob@musicbrainz.org>
2021-04-21 11:12:32 +0200

    Remove function from listenstore

--- listenbrainz/listenstore/timescale_listenstore.py
+++ listenbrainz/listenstore/timescale_listenstore.py
@@ -19,7 +19,6 @@ import sqlalchemy
 from brainzutils import cache
 
 import listenbrainz.db.user as db_user
-from listenbrainz import db
 from listenbrainz.db import timescale
 from listenbrainz import DUMP_LICENSE_FILE_PATH
 from listenbrainz.db import DUMP_DEFAULT_THREAD_COUNT
@@ -186,88 +185,6 @@ class TimescaleListenStore(ListenStore):
             self.log.error("Cannot fetch min/max timestamp: %s" %
                            str(e), exc_info=True)
             raise
-
-    def recalculate_all_user_data(self):
-
-        # Find the created timestamp of the last listen
-        query = "SELECT max(created) FROM listen WHERE created > :date"
-        try:
-            with timescale.engine.connect() as connection:
-                result = connection.execute(sqlalchemy.text(query), date=datetime.now() - timedelta(weeks=4))
-                row = result.fetchone()
-                last_created_ts = row[0]
-        except psycopg2.OperationalError as e:
-            self.log.error("Cannot query ts to fetch latest listen." % str(e), exc_info=True)
-            raise
-
-        self.log.info("Last created timestamp: " + str(last_created_ts))
-
-        # Select a list of users
-        user_list = []
-        query = 'SELECT musicbrainz_id FROM "user"'
-        try:
-            with db.engine.connect() as connection:
-                result = connection.execute(sqlalchemy.text(query))
-                for row in result:
-                    user_list.append(row[0])
-        except psycopg2.OperationalError as e:
-            self.log.error("Cannot query db to fetch user list." % str(e), exc_info=True)
-            raise
-
-        self.log.info("Fetched %d users. Setting empty cache entries." % len(user_list))
-
-        # Reset the timestamps and listen counts to 0 for all users
-        for user_name in user_list:
-            cache.set(REDIS_USER_LISTEN_COUNT + user_name, 0, time=0, encode=False)
-            cache.set(REDIS_USER_LISTEN_COUNT + user_name, 0, time=0, encode=False)
-            cache.set(REDIS_USER_TIMESTAMPS + user_name, "0,0", time=0)
-
-        # Tabulate all of the listen counts/timestamps for all users
-        self.log.info("Scan the whole listen table...")
-        listen_counts = defaultdict(int)
-        user_timestamps = {}
-        query = "SELECT listened_at, user_name FROM listen where created <= :ts" 
-        try:
-            with timescale.engine.connect() as connection:
-                result = connection.execute(sqlalchemy.text(query), ts=last_created_ts)
-                for row in result:
-                    ts = row[0]
-                    user_name = row[1]
-                    if user_name not in user_timestamps:
-                        user_timestamps[user_name] = [ts, ts]
-                    else:
-                        if ts > user_timestamps[user_name][1]:
-                            user_timestamps[user_name][1] = ts
-                        if ts < user_timestamps[user_name][0]:
-                            user_timestamps[user_name][0] = ts
-
-                    listen_counts[user_name] += 1
-
-        except psycopg2.OperationalError as e:
-            self.log.error("Cannot query db to fetch user list." % str(e), exc_info=True)
-            raise
-
-        self.log.info("Setting updated cache entries.")
-        # Set the timestamps and listen counts for all users
-        for user_name in user_list:
-            try:
-                cache._r.incrby(cache._prep_key(REDIS_USER_LISTEN_COUNT + user_name), listen_counts[user_name])
-            except KeyError:
-                pass
-
-            try:
-                tss = cache.get(REDIS_USER_TIMESTAMPS + user_name)
-                (min_ts, max_ts) = tss.split(",")
-                min_ts = int(min_ts)
-                max_ts = int(max_ts)
-                if min_ts and min_ts < user_timestamps[user_name][0]:
-                    user_timestamps[user_name][0] = min_ts
-                if max_ts and max_ts > user_timestamps[user_name][1]:
-                    user_timestamps[user_name][1] = max_ts
-                cache.set(REDIS_USER_TIMESTAMPS + user_name, "%d,%d" % (user_timestamps[user_name][0], user_timestamps[user_name][1]), time=0)
-            except KeyError:
-                pass
-
 
     def get_total_listen_count(self, cache_value=True):
         """ Returns the total number of listens stored in the ListenStore.
