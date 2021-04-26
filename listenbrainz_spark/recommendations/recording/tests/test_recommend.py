bbecce7bba8cc019afe15de403a9a2f16df6bf9e
Robert Kaye <rob@musicbrainz.org>
2021-04-26 15:42:47 +0200

    Spark sentry, revisted (#1400)
    * Add Pyspark Integration to sentry
    
    * Remove flask dependencies
    
    * Guard sentry_sdk initialization
    
    * Create a logger object, rather than using the logging module -- this makes the code
    a little more future proof with some anticpated BU improvements.
    
    * Clean up indentation
    
    * Replace some unchanged instances of logging with logger
    
    Co-authored-by: Kartik Ohri <kartikohri13@gmail.com>
    Co-authored-by: Alastair Porter <alastair@metabrainz.org>

--- listenbrainz_spark/recommendations/recording/tests/test_recommend.py
+++ listenbrainz_spark/recommendations/recording/tests/test_recommend.py
@@ -1,24 +1,20 @@
-import os
-import time
-from datetime import datetime
-import unittest
-from unittest.mock import patch, call, MagicMock, Mock
+import logging
+from unittest.mock import patch, call, MagicMock
 
 from listenbrainz_spark.tests import SparkTestCase
 from listenbrainz_spark.recommendations.recording import recommend
-from listenbrainz_spark.recommendations.recording import train_models
-from listenbrainz_spark import schema, utils, config, path, stats
+from listenbrainz_spark import schema, utils, path
 from listenbrainz_spark.exceptions import (RecommendationsNotGeneratedException,
-                                           EmptyDataframeExcpetion,
-                                           RatingOutOfRangeException)
+                                           EmptyDataframeExcpetion)
 
 from pyspark.sql import Row
-import pyspark.sql.functions as f
 from pyspark.rdd import RDD
 from pyspark.sql.functions import col
-from pyspark.mllib.recommendation import Rating
 
 # for test data/dataframes refer to listenbrainzspark/tests/__init__.py
+
+logger = logging.getLogger(__name__)
+
 
 class RecommendTestClass(SparkTestCase):
 
@@ -400,17 +396,13 @@ class RecommendTestClass(SparkTestCase):
         ))
         return df
 
-    @patch('listenbrainz_spark.recommendations.recording.recommend.current_app')
-    def test_check_for_ratings_beyond_range(self, mock_current_app):
+    def test_check_for_ratings_beyond_range(self):
         top_artist_rec_df = self.get_top_artist_rec_df()
         similar_artist_rec_df = self.get_similar_artist_rec_df()
 
-        recommend.check_for_ratings_beyond_range(top_artist_rec_df, similar_artist_rec_df)
-
-        mock_current_app.logger.info.assert_has_calls([
-            call('Some ratings are greater than 1 \nMax rating: 1.8'),
-            call('Some ratings are less than -1 \nMin rating: -2.8')
-        ])
+        min_test, max_test = recommend.check_for_ratings_beyond_range(top_artist_rec_df, similar_artist_rec_df)
+        self.assertEqual(min_test, True)
+        self.assertEqual(max_test, True)
 
     def test_create_messages(self):
         top_artist_rec_df = self.get_top_artist_rec_df()
