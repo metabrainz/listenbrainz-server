afbde73b2a15234f46e2fbb0d8102030ef4df49b
Kartik Ohri <kartikohri13@gmail.com>
2021-05-01 22:33:29 +0530

    fux test

--- listenbrainz/webserver/views/test/test_profile.py
+++ listenbrainz/webserver/views/test/test_profile.py
@@ -128,9 +128,8 @@ class ProfileViewsTestCase(IntegrationTestCase):
         r = self.client.get(url_for('profile.music_services_details'))
         self.assert200(r)
 
-        r = self.client.post(url_for('profile.music_services_disconnect', service_name='spotify'),
-                             data={'delete': 'yes'})
-        self.assertStatus(r, 302)
+        r = self.client.post(url_for('profile.music_services_disconnect', service_name='spotify'))
+        self.assert200(r)
 
         self.assertIsNone(self.service.get_user(self.user['id']))
 
