75e5f40a46c28a4834ca437ec5342830eadd3bfb
Kartik Ohri <kartikohri13@gmail.com>
2021-05-02 10:58:13 +0530

    Dedup spark Dockerfile

--- docker/spark-new-cluster/push-request-consumer.sh
+++ docker/spark-new-cluster/push-request-consumer.sh
@@ -2,5 +2,5 @@
 
 cd "$(dirname "${BASH_SOURCE[0]}")/../../"
 
-docker build -t metabrainz/listenbrainz-spark-new-cluster -f Dockerfile.spark.newcluster .
+docker build --target metabrainz-spark-prod -t metabrainz/listenbrainz-spark-new-cluster -f Dockerfile.spark .
 docker push metabrainz/listenbrainz-spark-new-cluster
