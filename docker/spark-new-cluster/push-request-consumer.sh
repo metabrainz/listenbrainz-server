f59c161e0a7b6f369fc56476f6c7f9c8fc801d83
Kartik Ohri <kartikohri13@gmail.com>
2021-05-05 21:33:57 +0530

    Add build-arg to push-request-consumer.sh

--- docker/spark-new-cluster/push-request-consumer.sh
+++ docker/spark-new-cluster/push-request-consumer.sh
@@ -2,5 +2,10 @@
 
 cd "$(dirname "${BASH_SOURCE[0]}")/../../"
 
-docker build --target metabrainz-spark-prod -t metabrainz/listenbrainz-spark-new-cluster -f Dockerfile.spark .
+docker build \
+  --target metabrainz-spark-prod \
+  -t metabrainz/listenbrainz-spark-new-cluster \
+  -f Dockerfile.spark \
+  --build-arg GIT_COMMIT_SHA=$(git describe --tags --dirty --always) \
+  .
 docker push metabrainz/listenbrainz-spark-new-cluster
