b5a90bcd2e195c8f45b216940d974befb64f34e0
Monkey Do <contact@monkeydo.digital>
2021-05-05 17:32:20 +0200

    Fix typescript compilation
    Added allowDeclareFields: true option, otherwise we get compilation errors and failure.
    Moved all babel compilation options to babel.config.js to avoid duplicate configs

--- babel.config.js
+++ babel.config.js
@@ -1,4 +1,23 @@
 module.exports = {
-  presets: ['@babel/preset-env', '@babel/preset-react'],
-  plugins: ['@babel/plugin-proposal-class-properties', '@babel/plugin-transform-runtime']
+  presets: [
+    [
+      "@babel/preset-env",
+      {
+        useBuiltIns: "usage",
+        corejs: { version: "3.9", proposals: true },
+        targets: {
+          node: "10",
+          browsers: ["> 0.2% and not dead", "firefox >= 44"],
+        },
+      },
+    ],
+    [
+      "@babel/preset-typescript",
+      {
+        allowDeclareFields: true,
+      },
+    ],
+    "@babel/preset-react",
+  ],
+  plugins: ["@babel/plugin-transform-runtime"],
 };
