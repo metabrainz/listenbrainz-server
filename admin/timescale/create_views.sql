f4796d9cce975a9d4ca594a3337c2785e83e0820
Robert Kaye <rob@musicbrainz.org>
2021-05-05 10:33:42 +0200

    Start migrating to TS 2.2

--- admin/timescale/create_views.sql
+++ admin/timescale/create_views.sql
@@ -1,8 +1,4 @@
-BEGIN;
-
-CREATE VIEW listen_count_30day
-       WITH (timescaledb.continuous, timescaledb.refresh_lag=0, timescaledb.refresh_interval=900, timescaledb.max_interval_per_job = '5184000')
+CREATE MATERIALIZED VIEW listen_count_30day
+       WITH (timescaledb.continuous)
          AS SELECT time_bucket(bigint '2592000', listened_at) AS listened_at_bucket, user_name, count(listen)
             FROM listen group by time_bucket(bigint '2592000', listened_at), user_name;
-
-COMMIT;
