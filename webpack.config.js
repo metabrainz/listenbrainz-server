4e132cc7319619bd73674410b2a6a3687ada8515
Monkey Do <MonkeyDo@users.noreply.github.com>
2021-05-06 13:48:25 +0200

    Fix typescript compilation (#1429)
    * Fix typescript compilation
    
    Added allowDeclareFields: true option, otherwise we get compilation errors and failure.
    Moved all babel compilation options to babel.config.js to avoid duplicate configs
    
    * Copy babel config for docker image build
    
    * Re-add babel plugin-proposal-class-properties
    
    this plugin should already be part of @babel/preset-env, but ¯\_(ツ)_/¯
    
    * Rearrange presets and add typescript transform plugin
    
    * Lock d3-array to 2.3.* due to babel incompatibility
    
    Co-authored-by: Kartik Ohri <kartikohri13@gmail.com>

--- webpack.config.js
+++ webpack.config.js
@@ -49,30 +49,9 @@ module.exports = function (env) {
           test: /\.(js|ts)x?$/,
           // some nivo/D3 dependencies need to be transpiled, we include them with the following regex
           exclude: /node_modules\/(?!(d3-array|d3-scale|internmap)\/).*/,
-          use: {
-            loader: "babel-loader",
-            options: {
-              presets: [
-                [
-                  "@babel/preset-env",
-                  {
-                    useBuiltIns: "usage",
-                    corejs: { version: "3.9", proposals: true },
-                    targets: {
-                      node: "10",
-                      browsers: ["> 0.2% and not dead", "firefox >= 44"],
-                    },
-                  },
-                ],
-                "@babel/preset-typescript",
-                "@babel/preset-react",
-              ],
-              plugins: [
-                "@babel/plugin-proposal-class-properties",
-                "@babel/plugin-transform-runtime",
-              ],
-            },
-          },
+          // Don't specify the babel configuration here
+          // Configuration can be found in ./babel.config.js
+          use: "babel-loader",
         },
       ],
     },
