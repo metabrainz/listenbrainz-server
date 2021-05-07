ac66b8d74c06443b0c74cf36822328925698cb24
Kartik Ohri <kartikohri13@gmail.com>
2021-05-07 16:22:59 +0530

    Run spark-request-consumer without docker

--- docker/stop-request-consumer-container.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-set -e
-
-cd "$(dirname "${BASH_SOURCE[0]}")/../"
-
-docker stop spark-request-consumer
-docker rm spark-request-consumer
-rm -r pyspark_venv pyspark_venv.tar.gz listenbrainz_spark_request_consumer.zip
-
