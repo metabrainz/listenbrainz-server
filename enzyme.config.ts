9b3e53c52ce03621c72b49aa7f56575ad772d7cc
Monkey Do <contact@monkeydo.digital>
2021-05-07 15:37:59 +0200

    Catch unhandled promise rejections
    From Node 15 onwards uncaught Promise rejections aren't logged to console and terminate the process.

--- enzyme.config.ts
+++ enzyme.config.ts
@@ -3,3 +3,12 @@ import * as Enzyme from 'enzyme';
 import * as Adapter from 'enzyme-adapter-react-16';
 
 Enzyme.configure({ adapter: new Adapter() });
+
+// In Node > v15 unhandled promise rejections will terminate the process
+if (!process.env.LISTENING_TO_UNHANDLED_REJECTION) {
+  process.on("unhandledRejection", (reason, promise) => {
+    console.log("Unhandled promise rejection:", reason, promise);
+  });
+  // Avoid memory leak by adding too many listeners
+  process.env.LISTENING_TO_UNHANDLED_REJECTION = "true";
+}
