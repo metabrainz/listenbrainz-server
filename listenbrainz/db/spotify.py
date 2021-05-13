d8427439d63c9451a374791ca5b49d45fa4bf9e6
Kartik Ohri <kartikohri13@gmail.com>
2021-05-13 20:23:57 +0530

    Fix bugs after Youtube Player release (#1456)
    * Fix error while trying to delete not connected user
    
    * Fetch latest_listened_at as well during refreshing user token
    
    * Pass youtube_user props to feed page
    
    * Revert to manually creating email in spotify reader error emails
    
    * Update spotify restart import script
    
    * Rearrange exception catch order to catch specific exception first
    
    * Only change refresh token if new one is available
    
    * Document get_user method
    
    * Do not pass function but its name to make_api_request
    
    This is needed because if we pass the function directly it will not get updated after refreshing the access token.
    
    * Use LEFT JOIN in spotify.get_user
    
    We are using this method for both streaming only users (frontend) and listen import (spotify reader), hence need the left join/
    
    * Check if a token has expired at the time that we need the check
    
    Previously we used token_expired, which came from the database at the
    time that the query was made. If this user stayed in a queue for
    some time, the token could have expired between the time that we
    requested it and the time that we processed it.
    
    * Catch AttributeError and TypeError explicitly
    
    * Fix error logging
    
    * Increase offset in tests
    
    Some tests in test_profile.py use current time as expires as we have added a 5 min leeway in refreshing tokens we should increase the offset there to make the token look expired.
    Alternatively, we may use freezegun in future for these tests.
    
    * Fix comments in test_user_oauth_token_has_expired
    
    * Fix "is expired" condition
    
    Co-authored-by: Alastair Porter <alastair@metabrainz.org>

--- listenbrainz/db/spotify.py
+++ listenbrainz/db/spotify.py
@@ -1,4 +1,4 @@
-from typing import List
+from typing import List, Optional
 
 from listenbrainz import db
 import sqlalchemy
@@ -16,7 +16,6 @@ def get_active_users_to_process() -> List[dict]:
                  , refresh_token
                  , listens_importer.last_updated
                  , token_expires
-                 , token_expires < now() as token_expired
                  , scopes
                  , latest_listened_at
                  , error_message
@@ -32,7 +31,7 @@ def get_active_users_to_process() -> List[dict]:
         return [dict(row) for row in result.fetchall()]
 
 
-def get_user_import_details(user_id: int):
+def get_user_import_details(user_id: int) -> Optional[dict]:
     """ Return user's spotify linking details to display on connect services page
 
     Args:
@@ -53,6 +52,38 @@ def get_user_import_details(user_id: int):
             """), {
                 'user_id': user_id,
             })
+        if result.rowcount > 0:
+            return dict(result.fetchone())
+    return None
+
+
+def get_user(user_id: int) -> Optional[dict]:
+    """ This get_user method is different from the one in external_service_oauth.py because
+     here we join against the listens_importer table to fetch the latest_listened_at column.
+     We need latest_listened_at column for using in the spotify_reader."""
+    with db.engine.connect() as connection:
+        result = connection.execute(sqlalchemy.text("""
+            SELECT external_service_oauth.user_id
+                 , "user".musicbrainz_id
+                 , "user".musicbrainz_row_id
+                 , external_service_oauth.service
+                 , access_token
+                 , refresh_token
+                 , external_service_oauth.last_updated
+                 , token_expires
+                 , scopes
+                 , latest_listened_at
+                 , error_message
+              FROM external_service_oauth
+              JOIN "user"
+                ON "user".id = external_service_oauth.user_id
+         LEFT JOIN listens_importer
+                ON listens_importer.external_service_oauth_id = external_service_oauth.id
+             WHERE external_service_oauth.service = 'spotify'
+               AND "user".id = :user_id
+        """), {
+            'user_id': user_id
+        })
         if result.rowcount > 0:
             return dict(result.fetchone())
     return None
---
+++ listenbrainz/domain/tests/test_external_service.py
@@ -0,0 +1,25 @@
+import datetime
+
+from freezegun import freeze_time
+
+from listenbrainz.domain.spotify import SpotifyService
+from listenbrainz.webserver.testing import ServerTestCase
+
+
+class ExternalServiceTestCase(ServerTestCase):
+
+    @freeze_time("2021-05-12 03:21:34", tz_offset=0)
+    def test_user_oauth_token_has_expired(self):
+        service = SpotifyService()
+
+        # has expired
+        user = {'token_expires': datetime.datetime(2021, 5, 12, 3, 0, 40, tzinfo=datetime.timezone.utc)}
+        assert service.user_oauth_token_has_expired(user) is True
+
+        # expires within the 5 minute threshold
+        user = {'token_expires': datetime.datetime(2021, 5, 12, 3, 24, 40, tzinfo=datetime.timezone.utc)}
+        assert service.user_oauth_token_has_expired(user) is True
+
+        # hasn't expired
+        user = {'token_expires': datetime.datetime(2021, 5, 12, 4, 1, 40, tzinfo=datetime.timezone.utc)}
+        assert service.user_oauth_token_has_expired(user) is False
