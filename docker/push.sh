17da7ab5f0726611f4f14e0ded57a71854bc7248
Kartik Ohri <kartikohri13@gmail.com>
2021-05-17 17:15:20 +0530

    Do not use cache-from for now (#1464)
    * Do not use cache-from for now
    
    * Do not build latest image either

--- docker/push.sh
+++ docker/push.sh
@@ -19,7 +19,6 @@ TAG=${1:-beta}
 function build_and_push_image {
     echo "building for tag $1"
     docker build \
-        --cache-from metabrainz/listenbrainz:latest \
         --tag metabrainz/listenbrainz:"$1" \
         --target listenbrainz-prod \
         --build-arg GIT_COMMIT_SHA="$GIT_COMMIT_SHA" . && \
@@ -27,9 +26,3 @@ function build_and_push_image {
 }
 
 build_and_push_image "$TAG"
-
-# Only build and push the latest image if the TAG matches the date
-# pattern (v-YYYY-MM-DD.N) we use for production releases.
-if [[ $TAG =~ ^v-[0-9]{4}-[0-9]{2}-[0-9]{2}\.[0-9]+$  ]]; then
-    build_and_push_image "latest"
-fi
