63c53d831a348ab583c1f99c6bb5ff7328ebad74
Robert Kaye <rob@musicbrainz.org>
2021-05-17 18:57:53 +0200

    Our init script wants SQL commands on a single line...

--- admin/timescale/create_views.sql
+++ admin/timescale/create_views.sql
@@ -2,7 +2,4 @@
 CREATE MATERIALIZED VIEW listen_count_30day WITH (timescaledb.continuous) AS SELECT time_bucket(bigint '2592000', listened_at) AS listened_at_bucket, user_name, count(listen) FROM listen GROUP BY listened_at_bucket, user_name;
 
 -- Add a policy to keep the listen_count_30day up to date for the last year, but the last bucket
-SELECT add_continuous_aggregate_policy('listen_count_30day',
-    start_offset => 31536000,
-    end_offset => 432000,
-    schedule_interval => INTERVAL '1 hour');
+SELECT add_continuous_aggregate_policy('listen_count_30day', start_offset => 31536000, end_offset => 432000, schedule_interval => INTERVAL '1 hour');
