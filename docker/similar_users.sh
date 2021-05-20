5e019248bd726361569f2ec3e7bad6f356096b2c
Kartik Ohri <kartikohri13@gmail.com>
2021-05-20 14:01:32 +0530

    Move similar users request from script to click command

--- docker/similar_users.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-/usr/local/bin/python /code/listenbrainz/manage.py spark request_dataframes --job-type="similar_users" --days=365 --listens-threshold=50
-/usr/local/bin/python /code/listenbrainz/manage.py spark request_similar_users
