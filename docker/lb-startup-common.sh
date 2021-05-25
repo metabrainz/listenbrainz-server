1543ae17acdcd4ba774227eba775740aa8556f0b
Alastair Porter <alastair@metabrainz.org>
2021-05-25 14:23:19 +0200

    Improve startup health check functions

--- docker/lb-startup-common.sh
+++ docker/lb-startup-common.sh
@@ -6,13 +6,22 @@ function log() {
 }
 
 function generate_message() {
-  message="$service terminated. "
-  if [ $# -gt 0 ] && [ $1 -eq 0 ]; then
+  # Generate a message indicating that a service has terminated.
+  # If the exit code is known, an additional message is added indicating what happened.
+  # arguments:
+  #   $1 service name
+  #   $2 exit code of the service
+  # return:
+  #   sets variable $message with the value of the generated message
+  message="$1 terminated. "
+  if [ $# -gt 1 ] && [ $2 -eq 0 ]; then
     message+="Exit status 0 (consul triggered reload or service was stopped)"
-  elif [ $# -gt 0 ] && [ $1 -eq 22 ]; then
+  elif [ $# -gt 1 ] && [ $2 -eq 15 ]; then
+    message+="Exit status 15 (consul-template failed to start up)"
+  elif [ $# -gt 1 ] && [ $2 -eq 22 ]; then
     message+="Exit status 22 (uwsgi failed to start app, check container logs)"
-  elif [ $# -gt 0 ]; then
-    message+="Exit status $1"
+  elif [ $# -gt 1 ]; then
+    message+="Exit status $2"
   else
     message+="unexpectedly called with no arguments"
   fi
@@ -20,15 +29,21 @@ function generate_message() {
 
 function send_sentry_message() {
   # Send a message to sentry
-  # Requires these arguments:
-  #   project: the name of the project that is running (e.g. 'listenbrainz')
-  #   $message: the message to report
+  # required globals:
   #   $SENTRY_SERVICE_ERROR_DSN: the DSN to report errors to
+  #   $PROJECT: the name of the project that is running (e.g. 'listenbrainz')
+  # arguments:
+  #   $1: the message to report
+  if [ -z "${PROJECT}" ]; then
+    log "PROJECT environment variable isn't set, using default"
+    PROJECT=unknown
+  fi
+
   if [ -z "${SENTRY_SERVICE_ERROR_DSN}" ]; then
     log "SENTRY_SERVICE_ERROR_DSN environment variable isn't set, not logging"
   elif ! command -v sentry-cli ; then
     log "Cannot find sentry-cli, not logging"
   else
-    SENTRY_DSN="${SENTRY_SERVICE_ERROR_DSN}" sentry-cli send-event -f $(uuidgen) -E "$project" -m "$message"
+    SENTRY_DSN="${SENTRY_SERVICE_ERROR_DSN}" sentry-cli send-event -f $(uuidgen) -E "$PROJECT" -m "$@"
   fi
 }
