d7dab6b31a330e7bfe401f0811334669dd5b550b
Alastair Porter <alastair@metabrainz.org>
2021-05-27 19:09:06 +0200

    Remove out of date faqs
    We link to docker documentation about how to install it, users should
    follow the instructions here to run without root/sudo
    
    Spark namenode startup was fixed in 01cf42d9ebc0

--- develop.sh
+++ develop.sh
@@ -56,7 +56,7 @@ elif [[ "$1" == "shell" ]]; then
     echo "Running flask shell..."
     invoke_docker_compose run --rm web flask shell
 elif [[ "$1" == "redis" ]]; then
-    echo "Running flask shell..."
+    echo "Running redis shell..."
     invoke_docker_compose exec redis redis-cli
 elif [[ "$1" == "psql" ]]; then
     echo "Connecting to postgresql..."
