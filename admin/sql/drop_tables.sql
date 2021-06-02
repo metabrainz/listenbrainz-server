737ab187e3dd6a60c22a1044cca0ec45e4084297
Monkey Do <contact@monkeydo.digital>
2021-06-02 18:02:46 +0200

    Drop reported_users table in drop_tables SQL script

--- admin/sql/drop_tables.sql
+++ admin/sql/drop_tables.sql
@@ -10,5 +10,6 @@ DROP TABLE IF EXISTS missing_musicbrainz_data       CASCADE;
 DROP TABLE IF EXISTS user_relationship              CASCADE;
 DROP TABLE IF EXISTS recommendation_feedback        CASCADE;
 DROP TABLE IF EXISTS user_timeline_event            CASCADE;
+DROP TABLE IF EXISTS reported_users            CASCADE;
 
 COMMIT;
