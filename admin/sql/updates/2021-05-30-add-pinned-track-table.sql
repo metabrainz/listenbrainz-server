98e1824e97ca9cdcfb8b1336a8db82d5b87864d7
jason <jasondaok@gmail.com>
2021-06-01 22:26:18 -0700

    Track table changes
    This commit
    - changes 'pinned_track' to 'pinned_recording'
    - removes currently_pinned column
    - adds pinned_until column

--- admin/sql/updates/2021-05-30-add-pinned-track-table.sql
+++ admin/sql/updates/2021-05-30-add-pinned-track-table.sql
@@ -1,22 +1,22 @@
 BEGIN;
 
-CREATE TABLE pinned_track(
+CREATE TABLE pinned_recording(
     id                      SERIAL, -- PK
     user_id                 INTEGER NOT NULL, -- FK to "user".id
     recording_mbid          UUID NOT NULL,
-    blurb_content           VARCHAR,
-    currently_pinned        BOOLEAN NOT NULL DEFAULT TRUE,
+    blurb_content           TEXT,
+    pinned_until            TIMESTAMP WITH TIME ZONE NOT NULL,
     created                 TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT NOW()
 );
 
-ALTER TABLE pinned_track ADD CONSTRAINT pinned_track_pkey PRIMARY KEY (id);
+ALTER TABLE pinned_recording ADD CONSTRAINT pinned_recording_pkey PRIMARY KEY (id);
 
-ALTER TABLE pinned_track
-    ADD CONSTRAINT pinned_track_user_id_foreign_key
+ALTER TABLE pinned_recording
+    ADD CONSTRAINT pinned_recording_user_id_foreign_key
     FOREIGN KEY (user_id)
     REFERENCES "user" (id)
     ON DELETE CASCADE;
 
-CREATE UNIQUE INDEX user_id_rec_mbid_ndx_pinned_track ON pinned_track (user_id, recording_mbid);
+CREATE INDEX user_id_ndx_pinned_recording ON pinned_recording (user_id);
 
 COMMIT;
