67b7236a0244922388941da5dfe62b31326e76f7
Robert Kaye <rob@musicbrainz.org>
2021-06-17 18:27:25 +0200

    Add PROD_ENV check

--- admin/cron_lock.py
+++ admin/cron_lock.py
@@ -12,7 +12,12 @@ CRON_LOCK_FILE = os.path.join(CRON_LOGS_DIR, "cron.lock")
 
 def sanity_check():
     """ Check to make sure the logs dir exists and that the CONTAINER_NAME env
-        var is set correctly for the cron container."""
+        var is set correctly for the cron container. If the PROD_ENV var is not
+        set, assume that we are not running in production and exist with success. """
+
+    if "PROD_ENV" not in os.environ:
+        print("Not running in a production env, not locking cron.")
+        sys.exit(0)
 
     if not os.path.exists(CRON_LOGS_DIR):
         print("cron logs dir does not exist. Is this code running in the container?")
