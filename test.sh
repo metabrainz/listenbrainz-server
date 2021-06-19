672884fec0b256833e7400cc36ec7b5ecbb62018
Kartik Ohri <kartikohri13@gmail.com>
2021-06-19 18:04:17 +0530

    Fix spark test run command - 2

--- test.sh
+++ test.sh
@@ -210,8 +210,9 @@ if [ "$1" == "spark" ]; then
     echo "Running tests"
     docker-compose -f $SPARK_COMPOSE_FILE_LOC \
                    -p $SPARK_COMPOSE_PROJECT_NAME \
-                   -e PYTHONDONTWRITEBYTECODE=1 \
-                run --rm request_consumer \
+                run \
+                -e PYTHONDONTWRITEBYTECODE=1 \
+                --rm request_consumer \
                 dockerize -wait tcp://namenode:9000 -timeout 60s \
                 python -m pytest -c pytest.spark.ini "$@"
     RET=$?
