ee5cb73a6a81710dcf8c47dc1c16a763e1a41e79
Kartik Ohri <kartikohri13@gmail.com>
2021-06-22 13:45:10 +0530

    LB-865: Move MSB lookups to TS Writer
    We have decided to move messybrainz lookup to the TS writer. Since,
    the now playing listens are only transient and not stored in the
    database, we have believe it is safe to stop doing msb lookups for
    now playing listens.

--- listenbrainz/webserver/views/api_tools.py
+++ listenbrainz/webserver/views/api_tools.py
@@ -49,7 +49,7 @@ def insert_payload(payload, user, listen_type=LISTEN_TYPE_IMPORT):
         Returns: augmented_listens
     """
     try:
-        augmented_listens = _get_augmented_listens(payload, user, listen_type)
+        augmented_listens = _get_augmented_listens(payload, user)
         _send_listens_to_queue(listen_type, augmented_listens)
     except (APIInternalServerError, APIServiceUnavailable):
         raise
@@ -213,94 +213,12 @@ def is_valid_uuid(u):
         return False
 
 
-def _get_augmented_listens(payload, user, listen_type):
-    """ Converts the payload to augmented list after lookup
-        in the MessyBrainz database
-    """
-
-    augmented_listens = []
-    msb_listens = []
-    for l in payload:
-        listen = l.copy()   # Create a local object to prevent the mutation of the passed object
+def _get_augmented_listens(payload, user):
+    """ Converts the payload to augmented list after adding user_id and user_name attributes """
+    for listen in payload:
         listen['user_id'] = user['id']
         listen['user_name'] = user['musicbrainz_id']
-
-        msb_listens.append(listen)
-
-    for chunk in chunked(msb_listens, MAX_ITEMS_PER_MESSYBRAINZ_LOOKUP):
-        augmented_listens.extend(_messybrainz_lookup(chunk))
-
-    return augmented_listens
-
-
-def _messybrainz_lookup(listens):
-
-    msb_listens = []
-    for listen in listens:
-        messy_dict = {
-            'artist': listen['track_metadata']['artist_name'],
-            'title': listen['track_metadata']['track_name'],
-        }
-        if 'release_name' in listen['track_metadata']:
-            messy_dict['release'] = listen['track_metadata']['release_name']
-
-        if 'additional_info' in listen['track_metadata']:
-            ai = listen['track_metadata']['additional_info']
-            if 'artist_mbids' in ai and isinstance(ai['artist_mbids'], list):
-                messy_dict['artist_mbids'] = ai['artist_mbids']
-            if 'release_mbid' in ai:
-                messy_dict['release_mbid'] = ai['release_mbid']
-            if 'recording_mbid' in ai:
-                messy_dict['recording_mbid'] = ai['recording_mbid']
-            if 'track_number' in ai:
-                messy_dict['track_number'] = ai['track_number']
-            if 'spotify_id' in ai:
-                messy_dict['spotify_id'] = ai['spotify_id']
-        msb_listens.append(messy_dict)
-
-    try:
-        msb_responses = messybrainz.submit_listens(msb_listens)
-    except messybrainz.exceptions.BadDataException as e:
-        log_raise_400(str(e))
-    except messybrainz.exceptions.NoDataFoundException:
-        return []
-    except messybrainz.exceptions.ErrorAddingException as e:
-        raise APIServiceUnavailable(str(e))
-
-    augmented_listens = []
-    for listen, messybrainz_resp in zip(listens, msb_responses['payload']):
-        messybrainz_resp = messybrainz_resp['ids']
-
-        if 'additional_info' not in listen['track_metadata']:
-            listen['track_metadata']['additional_info'] = {}
-
-        try:
-            listen['recording_msid'] = messybrainz_resp['recording_msid']
-            listen['track_metadata']['additional_info']['artist_msid'] = messybrainz_resp['artist_msid']
-        except KeyError:
-            current_app.logger.error("MessyBrainz did not return a proper set of ids")
-            raise APIInternalServerError
-
-        try:
-            listen['track_metadata']['additional_info']['release_msid'] = messybrainz_resp['release_msid']
-        except KeyError:
-            pass
-
-        artist_mbids = messybrainz_resp.get('artist_mbids', [])
-        release_mbid = messybrainz_resp.get('release_mbid', None)
-        recording_mbid = messybrainz_resp.get('recording_mbid', None)
-
-        if 'artist_mbids'   not in listen['track_metadata']['additional_info'] and \
-           'release_mbid'   not in listen['track_metadata']['additional_info'] and \
-           'recording_mbid' not in listen['track_metadata']['additional_info']:
-
-            if len(artist_mbids) > 0 and release_mbid and recording_mbid:
-                listen['track_metadata']['additional_info']['artist_mbids'] = artist_mbids
-                listen['track_metadata']['additional_info']['release_mbid'] = release_mbid
-                listen['track_metadata']['additional_info']['recording_mbid'] = recording_mbid
-
-        augmented_listens.append(listen)
-    return augmented_listens
+    return payload
 
 
 def log_raise_400(msg, data=""):
