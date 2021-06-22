489a7e527b805b368d3ce6271a65cb39c9f7b7ea
Jason <jasondaok@gmail.com>
2021-06-22 00:53:49 -0700

    Improve pinnedRecording database queries + models (#1510)
    * Add 2nd model, improve validators, db queries, add tests
    
    * pep8 fixes
    
    * pep8 again
    
    * Add writeablePinnedRecording model to derive from PinnedRecording)

--- listenbrainz/db/pinned_recording.py
+++ listenbrainz/db/pinned_recording.py
@@ -2,19 +2,20 @@ import sqlalchemy
 import json
 
 from listenbrainz import db
-from listenbrainz.db.model.pinned_recording import PinnedRecording
+from listenbrainz.db.model.pinned_recording import PinnedRecording, WritablePinnedRecording
 from typing import List
 
-PINNED_REC_GET_COLUMNS = ['user_id', 'id AS row_id', 'recording_mbid::text', 'blurb_content', 'pinned_until',
-                          'created']
+
+PINNED_REC_GET_COLUMNS = ['user_id', 'pin.id AS row_id', 'recording_mbid::text', 'blurb_content', 'pinned_until',
+                          'pin.created as created']
 
 
-def pin(pinnedRecording: PinnedRecording):
+def pin(pinnedRecording: WritablePinnedRecording):
     """ Inserts a pinned recording record into the database for the user.
         If the user already has an active pinned recording, it will be unpinned before the new one is pinned.
 
         Args:
-            pinnedRecording: An object of class PinnedRecording
+            pinnedRecording: An object of class WritablePinnedRecording
     """
     args = {
         'user_id': pinnedRecording.user_id,
@@ -40,10 +41,14 @@ def unpin(user_id: int):
 
         Args:
             user_id: the row ID of the user in the DB
+
+        Returns:
+            True if an active pinned recording was unpinned,
+            False if no pinned recording belonging to the given user_id was currently pinned.
     """
 
     with db.engine.connect() as connection:
-        connection.execute(sqlalchemy.text("""
+        result = connection.execute(sqlalchemy.text("""
             UPDATE pinned_recording
                SET pinned_until = NOW()
              WHERE (user_id = :user_id AND pinned_until >= NOW())
@@ -51,6 +56,7 @@ def unpin(user_id: int):
             'user_id': user_id,
             }
         )
+        return result.rowcount == 1
 
 
 def delete(row_id: int, user_id: int):
@@ -58,10 +64,14 @@ def delete(row_id: int, user_id: int):
 
         Args:
             row_id: The row id of the pinned_recording to delete from the DB's 'pinned_recording' table
+
+        Returns:
+            True if a pinned recording for the given row_id and user_id was deleted.
+            False if the pinned recording for the given row_id and user_id did not exist.
     """
 
     with db.engine.connect() as connection:
-        connection.execute(sqlalchemy.text("""
+        result = connection.execute(sqlalchemy.text("""
             DELETE FROM pinned_recording
              WHERE id = :row_id
                AND user_id = :user_id
@@ -70,6 +80,7 @@ def delete(row_id: int, user_id: int):
             'user_id': user_id
             }
         )
+        return result.rowcount == 1
 
 
 def get_current_pin_for_user(user_id: int) -> PinnedRecording:
@@ -85,7 +96,7 @@ def get_current_pin_for_user(user_id: int) -> PinnedRecording:
     with db.engine.connect() as connection:
         result = connection.execute(sqlalchemy.text("""
             SELECT {columns}
-              FROM pinned_recording
+              FROM pinned_recording as pin
              WHERE (user_id = :user_id
                AND pinned_until >= NOW())
             """.format(columns=','.join(PINNED_REC_GET_COLUMNS))), {'user_id': user_id})
@@ -94,11 +105,11 @@ def get_current_pin_for_user(user_id: int) -> PinnedRecording:
 
 
 def get_pin_history_for_user(user_id: int, count: int, offset: int) -> List[PinnedRecording]:
-    """ Get a list of pinned recording records for the user in descending order of their created date
+    """ Get a list of pinned recordings for the user in descending order of their created date
 
         Args:
             user_id: the row ID of the user in the DB
-            count: number of rows to be returned
+            count: number of pinned recordings to be returned
             offset: number of pinned recordings to skip from the beginning
 
         Returns:
@@ -108,7 +119,7 @@ def get_pin_history_for_user(user_id: int, count: int, offset: int) -> List[Pinn
     with db.engine.connect() as connection:
         result = connection.execute(sqlalchemy.text("""
             SELECT {columns}
-              FROM pinned_recording
+              FROM pinned_recording as pin
              WHERE user_id = :user_id
              ORDER BY created DESC
              LIMIT :count
@@ -120,6 +131,41 @@ def get_pin_history_for_user(user_id: int, count: int, offset: int) -> List[Pinn
         })
         return [PinnedRecording(**dict(row)) for row in result.fetchall()]
 
+
+def get_pins_for_user_following(user_id: int, count: int, offset: int) -> List[PinnedRecording]:
+    """ Get a list of currently active pinned recordings for users
+        in the user's following list in descending order of their created date.
+
+        Args:
+            user_id: the row ID of the main user in the DB
+            count: number of pinned recordings to be returned
+            offset: number of pinned recordings to skip from the beginning
+
+        Returns:
+            A list of PinnedRecording objects sorted by newest to oldest creation date.
+    """
+    COLUMNS_WITH_USERNAME = PINNED_REC_GET_COLUMNS + ['"user".musicbrainz_id as user_name']
+
+    with db.engine.connect() as connection:
+        result = connection.execute(sqlalchemy.text("""
+            SELECT {columns}
+              FROM user_relationship
+              JOIN "user"
+                ON user_1 = "user".id
+              JOIN pinned_recording as pin
+                ON user_1 = pin.user_id
+             WHERE user_0 = :user_id
+               AND relationship_type = 'follow'
+               AND pinned_until >= NOW()
+             ORDER BY created DESC
+             LIMIT :count
+            OFFSET :offset
+            """.format(columns=','.join(COLUMNS_WITH_USERNAME))), {
+            'user_id': user_id,
+            'count': count,
+            'offset': offset
+        })
+        return [PinnedRecording(**dict(row)) for row in result.fetchall()]
 
 def get_pin_count_for_user(user_id: int) -> int:
     """ Get the total number pinned_recordings for the user.
