0219e0010e42c631f34aa92cc57067d1a0f56d77
Kartik Ohri <kartikohri13@gmail.com>
2021-06-29 00:50:33 +0530

    Use psycopg2 execute_values to insert list of tuples

--- listenbrainz/webserver/login/copy_files_from_mb_to_lb.py
+++ listenbrainz/webserver/login/copy_files_from_mb_to_lb.py
@@ -1,5 +1,5 @@
-import sqlalchemy
-from sqlalchemy.exc import SQLAlchemyError
+import psycopg2
+from psycopg2.extras import execute_values
 from brainzutils.musicbrainz_db.editor import fetch_multiple_editors
 from flask import current_app
 
@@ -8,23 +8,27 @@ from listenbrainz import db as lb_db
 
 def copy_emails():
     current_app.logger.info("Beginning to update emails for users...")
-    with lb_db.engine.connect() as connection:
-        try:
-            result = connection.execute(sqlalchemy.text('''SELECT musicbrainz_row_id FROM "user"'''))
+    connection = lb_db.engine.raw_connection()
+    try:
+        with connection.cursor() as cursor:
+            cursor.execute('''SELECT musicbrainz_row_id FROM "user"''')
             current_app.logger.info("Fetched editor ids from ListenBrainz.")
-            editor_ids = [row['musicbrainz_row_id'] for row in result.fetchall()]
+            editor_ids = [row[0] for row in cursor.fetchall()]
             editors = fetch_multiple_editors(editor_ids)
             current_app.logger.info("Fetched editor emails from MusicBrainz.")
             emails = [(editor_id, editor['email']) for editor_id, editor in editors.items()]
-            connection.execute(sqlalchemy.text("""
+            query = """
                 UPDATE "user"
                 SET email = editor_email_temp.email
-                FROM (VALUES :emails) 
+                FROM (VALUES %s)
                 AS editor_email_temp(id, email)
                 WHERE editor_email_temp.id = "user".musicbrainz_row_id
-            """), {"emails": tuple(emails)})
+            """
+            execute_values(cursor, query, emails, template=None)
             current_app.logger.info("Updated emails of ListenBrainz users.")
-            connection.commit()
-        except SQLAlchemyError as error:
-            current_app.logger.error(error, exc_info=True)
-            connection.rollback()
+        connection.commit()
+    except psycopg2.errors.OperationalError:
+        current_app.logger.error("Error while updating emails of ListenBrainz users", exc_info=True)
+        connection.rollback()
+    finally:
+        connection.close()
