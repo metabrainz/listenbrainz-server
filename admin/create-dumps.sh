a7b67f766b1f57bf4ee380afd623fa3e1314808a
Robert Kaye <rob@musicbrainz.org>
2021-07-12 10:44:51 +0200

    Address the latest PR feedback

--- admin/create-dumps.sh
+++ admin/create-dumps.sh
@@ -65,7 +65,7 @@ function on_exit {
     fi
 
     # Remove the cron lock
-    admin/cron_lock.py unlock-cron create-dumps
+    python3 admin/cron_lock.py unlock-cron create-dumps
 }
 
 START_TIME=$(date +%s)
@@ -91,12 +91,10 @@ else
 fi
 
 # Lock cron, so it cannot be accidentally terminated.
-admin/cron_lock.py lock-cron create-dumps "Creating $DUMP_TYPE dump."
+python3 admin/cron_lock.py lock-cron create-dumps "Creating $DUMP_TYPE dump."
 
 # Trap should not be called before we lock cron to avoid wiping out an existing lock file
 trap on_exit EXIT
-
-sleep 9999
 
 DUMP_TEMP_DIR="$DUMP_BASE_DIR/$SUB_DIR.$$"
 echo "DUMP_BASE_DIR is $DUMP_BASE_DIR"
