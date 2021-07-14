1a0c34f2114aa32d82228f0e8545ae67a4486841
Kartik Ohri <kartikohri13@gmail.com>
2021-07-14 16:19:19 +0530

    Fix timestamp leftover from debugging

--- admin/sql/updates/2021-07-14-migrate-import-ts-from-users-to-listens-importer.sql
+++ admin/sql/updates/2021-07-14-migrate-import-ts-from-users-to-listens-importer.sql
@@ -1,6 +1,6 @@
 BEGIN;
 INSERT INTO listens_importer(user_id, service, latest_listened_at)
-SELECT id, 'lastfm', now()
+SELECT id, 'lastfm', latest_import
 FROM "user"
 WHERE latest_import > 'epoch';
 COMMIT;
