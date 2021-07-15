3641d75c12780ca64d2c99299109e9977b9ff5cb
Kartik Ohri <kartikohri13@gmail.com>
2021-07-15 15:39:08 +0530

    Add function to upload new parquet full dumps

--- listenbrainz_spark/hdfs/upload.py
+++ listenbrainz_spark/hdfs/upload.py
@@ -1,11 +1,12 @@
 import os
+import pathlib
 import time
 import tarfile
 import tempfile
 import logging
 
 from listenbrainz_spark import schema, path, utils
-from listenbrainz_spark.hdfs import ListenbrainzHDFSUploader
+from listenbrainz_spark.hdfs import ListenbrainzHDFSUploader, TEMP_DIR_PATH
 
 
 logger = logging.getLogger(__name__)
@@ -93,3 +94,37 @@ class ListenbrainzDataUploader(ListenbrainzHDFSUploader):
             with tempfile.TemporaryDirectory() as tmp_dump_dir:
                 self.upload_archive(tmp_dump_dir, tar, path.SIMILAR_ARTIST_DATAFRAME_PATH, schema.artist_relation_schema,
                                     self.process_json, overwrite=True)
+
+    def upload_new_listens_full_dump(self, archive: str):
+        """ Upload new format parquet listens dumps to HDFS.
+
+            Args:
+                  archive: path to parquet listens dump to be uploaded
+        """
+        with tempfile.TemporaryDirectory() as local_temp_dir:
+            hdfs_temp_dir = os.path.join(TEMP_DIR_PATH, archive)
+
+            logger.info("Cleaning HDFS temporary directory...")
+            if utils.path_exists(hdfs_temp_dir):
+                utils.delete_dir(hdfs_temp_dir, recursive=True)
+
+            logger.info("Uploading listens to temporary directory in HDFS...")
+            self.extract_and_upload_archive(archive, local_temp_dir, hdfs_temp_dir)
+
+        dest_path = path.LISTENBRAINZ_NEW_DATA_DIRECTORY
+        # Delete existing dumps if any
+        if utils.path_exists(dest_path):
+            logger.info(f'Removing {dest_path} from HDFS...')
+            utils.delete_dir(dest_path, recursive=True)
+            logger.info('Done!')
+
+        logger.info("Moving the processed files to {}".format(dest_path))
+        t0 = time.monotonic()
+
+        # Check if parent directory exists, if not create a directory
+        dest_path_parent = pathlib.Path(dest_path).parent
+        if not utils.path_exists(dest_path_parent):
+            utils.create_dir(dest_path_parent)
+
+        utils.rename(hdfs_temp_dir, dest_path)
+        utils.logger.info(f"Done! Time taken: {time.monotonic() - t0:.2f}")
