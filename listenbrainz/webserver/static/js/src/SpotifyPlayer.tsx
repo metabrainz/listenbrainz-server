88ccebeb59a26c078950e08f0c1d88ba275b9be9
Monkey Do <contact@monkeydo.digital>
2021-07-16 18:06:05 +0200

    Update snapshots

--- listenbrainz/webserver/static/js/src/SpotifyPlayer.tsx
+++ listenbrainz/webserver/static/js/src/SpotifyPlayer.tsx
@@ -100,7 +100,6 @@ export default class SpotifyPlayer
       loadScriptAsync(document, "https://sdk.scdn.co/spotify-player.js");
     } else {
       this.handleAccountError();
-      props.onInvalidateDataSource(this);
     }
   }
 
