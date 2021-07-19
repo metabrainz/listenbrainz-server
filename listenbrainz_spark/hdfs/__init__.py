4f6e134627cad199c3596aecf6729fec80f635be
Kartik Ohri <kartikohri13@gmail.com>
2021-07-19 22:47:51 +0530

    We do not need to delete the extracted files manually as we use temp dir anyways

--- listenbrainz_spark/hdfs/__init__.py
+++ listenbrainz_spark/hdfs/__init__.py
@@ -159,7 +159,6 @@ class ListenbrainzHDFSUploader:
                     hdfs_path = os.path.join(hdfs_dir, member.name)
                     local_path = os.path.join(local_dir, member.name)
                     utils.upload_to_HDFS(hdfs_path, local_path)
-                    os.remove(local_path)
 
                     time_taken = time.monotonic() - t0
                     total_files += 1
