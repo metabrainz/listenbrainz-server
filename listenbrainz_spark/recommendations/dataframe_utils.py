45c072732576f535e0dc4854a62d3e25b98972ee
Kartik Ohri <kartikohri13@gmail.com>
2021-07-21 22:28:13 +0530

    Try moving create_dataframes to use new parquet dumps

--- listenbrainz_spark/recommendations/dataframe_utils.py
+++ listenbrainz_spark/recommendations/dataframe_utils.py
@@ -1,6 +1,7 @@
 import uuid
 import logging
 
+from datetime import datetime
 import listenbrainz_spark.utils.mapping as mapping_utils
 from listenbrainz_spark.stats import (replace_days,
                                       offset_days)
@@ -43,41 +44,11 @@ def get_dates_to_train_data(train_model_window):
             from_date (datetime): Date from which start fetching listens.
             to_date (datetime): Date upto which fetch listens.
     """
-    to_date = get_latest_listen_ts()
+    to_date = datetime.now()
     from_date = offset_days(to_date, train_model_window)
     # shift to the first of the month
     from_date = replace_days(from_date, 1)
     return to_date, from_date
-
-
-def get_mapped_artist_and_recording_mbids(partial_listens_df, msid_mbid_mapping_df):
-    """ Map recording msid->mbid and artist msid->mbids so that every listen has an mbid.
-
-        Args:
-            partial_listens_df (dataframe): listens without artist mbid and recording mbid.
-            msid_mbid_mapping_df (dataframe): msid->mbid mapping. For columns refer to
-                                              msid_mbid_mapping_schema in listenbrainz_spark/schema.py
-        Returns:
-            mapped_listens_df (dataframe): listens mapped with msid_mbid_mapping.
-    """
-    condition = [
-        partial_listens_df.track_name_matchable == msid_mbid_mapping_df.msb_recording_name_matchable,
-        partial_listens_df.artist_name_matchable == msid_mbid_mapping_df.msb_artist_credit_name_matchable
-    ]
-
-    df = partial_listens_df.join(msid_mbid_mapping_df, condition, 'inner')
-    # msb_release_name_matchable is skipped till the bug in mapping is resolved.
-    # bug : release_name in listens and mb_release_name in mapping is different.
-    mapped_listens_df = df.select('listened_at',
-                                  'mb_artist_credit_id',
-                                  'mb_artist_credit_mbids',
-                                  'mb_recording_mbid',
-                                  'mb_release_mbid',
-                                  'msb_artist_credit_name_matchable',
-                                  'msb_recording_name_matchable',
-                                  'user_name')
-
-    return mapped_listens_df
 
 
 def get_listens_for_training_model_window(to_date, from_date, dest_path):
