47f7a45546ac4a15dbfd131d80325e3c158ef323
jason <jasondaok@gmail.com>
2021-07-21 03:05:32 -0700

    Add PinEventMetadata, update EventTypeT + EventMetadata types

--- listenbrainz/webserver/static/js/src/types.d.ts
+++ listenbrainz/webserver/static/js/src/types.d.ts
@@ -468,12 +468,17 @@ declare type UserTrackRecommendationMetadata = {
   artist_msid: string;
 };
 
+declare type PinEventMetadata = Listen & {
+  blurb_content?: string;
+};
+
 /** ***********************************
  ********  USER FEED TIMELINE  ********
  ************************************* */
 
 type EventTypeT =
   | "recording_recommendation"
+  | "recording_pin"
   | "listen"
   | "like"
   | "follow"
@@ -495,6 +500,7 @@ type NotificationEventMetadata = {
 type EventMetadata =
   | Listen
   | UserRelationshipEventMetadata
+  | PinEventMetadata
   | NotificationEventMetadata;
 
 type TimelineEvent = {
