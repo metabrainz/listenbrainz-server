aa9b0e478d2c403d159c18d886393f50e420a607
Robert Kaye <rob@musicbrainz.org>
2021-08-05 11:08:12 +0200

    Experimenting with solr for release index for improving release info in the mapping

---
+++ listenbrainz/mbid_mapping/mapping/release_lookup_index.py
@@ -0,0 +1,61 @@
+import sys
+import re
+import time
+import datetime
+
+from unidecode import unidecode
+import psycopg2
+import pysolr
+
+import config
+from mapping.utils import log
+
+
+BATCH_SIZE = 5000
+SOLR_HOST = "listenbrainz-solr"
+SOLR_PORT = 8983
+
+
+def build_release_lookup_index():
+
+    solr = pysolr.Solr('http://%s:%d/solr/' % (SOLR_HOST, SOLR_PORT), always_commit=False)
+
+    with psycopg2.connect(config.MBID_MAPPING_DATABASE_URI) as conn:
+        with conn.cursor(cursor_factory=psycopg2.extras.DictCursor) as curs:
+
+            query = ("""SELECT ac.id AS artist_credit_id,
+                               ac.name AS artist_credit_name,
+                               rel.gid AS release_mbid,
+                               rel.name AS release_name,
+                               array_agg(rec.name) AS recording_names
+                          FROM artist_credit ac
+                          JOIN release rel
+                            ON rel.artist_credit = ac.id
+                          JOIN medium m
+                            ON m.release = rel.id
+                          JOIN track t
+                            ON t.medium = m.id
+                          JOIN recording rec
+                            ON t.recording = rec.id
+                      GROUP BY ac.id, ac.name, rel.gid, rel.name""")
+
+            curs.execute(query)
+
+            docs = []
+            for row in curs:
+                docs.append({
+                    "id": row["release_mbid"],
+                    "title": row["release_name"],
+                    "artist_credit_id": row["artist_credit_id"],
+                    "artist_credit_name": row["artist_credit_name"],
+                    "recording_names": row["recording_names"]})
+
+                if len(docs) == BATCH_SIZE:
+                    solr.add(docs)
+                    docs = []
+
+            if len(docs):
+                solr.add(docs)
+
+
+
