7dd6a42dc0b5f2659f5049ac9c28362c8b6a0164
Jason <jasondaok@gmail.com>
2021-08-09 11:56:42 -0700

    Add pin events to user timeline (backend) (#1559)
    * Add class APIPinEvent, and update UserTimelineEventType
    
    * Update fetch_track_metadata_for_pin() to also return artist_name
    
    * Add get_pins_for_feed() to pinned_recording.py and add test
    
    This function a list of PinnedRecordings for a list of users in descending order of their created date. this will be used to converted to timeline events to render pin events on user feeds
    
    * Retrieve pin events in user_timeline_event_api.py
    
    * pep8
    
    * Merge branch 'master' into pin-timeline-backend
    
    * Update user_timeline_event_api to work with refactored fetch_track_metadata function
    
    * Revert "Merge branch 'master' into pin-timeline-backend"
    
    This reverts commit 2b2350fdfd4f71abb4c50122f9e0ea562bb2c5ba.
    
    * Retest

--- data/model/user_timeline_event.py
+++ data/model/user_timeline_event.py
@@ -30,6 +30,7 @@ class UserTimelineEventType(Enum):
     FOLLOW = 'follow'
     LISTEN = 'listen'
     NOTIFICATION = 'notification'
+    RECORDING_PIN = 'recording_pin'
 
 
 class RecordingRecommendationMetadata(pydantic.BaseModel):
@@ -68,7 +69,11 @@ class APIFollowEvent(pydantic.BaseModel):
     created: int
 
 
-APIEventMetadata = Union[APIListen, APIFollowEvent, APINotificationEvent]
+class APIPinEvent(APIListen):
+    blurb_content: Optional[str]
+
+
+APIEventMetadata = Union[APIListen, APIFollowEvent, APINotificationEvent, APIPinEvent]
 
 
 class APITimelineEvent(pydantic.BaseModel):
