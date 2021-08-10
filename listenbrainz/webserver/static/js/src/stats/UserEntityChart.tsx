b4cc27a67959b0c0052534d89d7cf2c7ca797aea
Alastair Porter <alastair@porter.net.nz>
2021-08-10 12:38:18 +0200

    Don't pass the click event to functions where it isn't needed

--- listenbrainz/webserver/static/js/src/stats/UserEntityChart.tsx
+++ listenbrainz/webserver/static/js/src/stats/UserEntityChart.tsx
@@ -78,7 +78,7 @@ export default class UserEntityChart extends React.Component<
     window.history.replaceState(
       null,
       "",
-      `?page=${page}&range=${range}&entity=${entity}`
+      this.buildURLParams(page, range, entity)
     );
     this.syncStateWithURL();
     this.handleResize();
@@ -89,27 +89,13 @@ export default class UserEntityChart extends React.Component<
     window.removeEventListener("resize", this.handleResize);
   }
 
-  changePage = (
-    newPage: number,
-    event?: React.MouseEvent<HTMLElement>
-  ): void => {
-    if (event) {
-      event.preventDefault();
-    }
-
+  changePage = (newPage: number): void => {
     const { entity, range } = this.state;
     this.setURLParams(newPage, range, entity);
     this.syncStateWithURL();
   };
 
-  changeRange = (
-    newRange: UserStatsAPIRange,
-    event?: React.MouseEvent<HTMLElement>
-  ): void => {
-    if (event) {
-      event.preventDefault();
-    }
-
+  changeRange = (newRange: UserStatsAPIRange): void => {
     const { entity } = this.state;
     this.setURLParams(1, newRange, entity);
     this.syncStateWithURL();
@@ -378,23 +364,38 @@ export default class UserEntityChart extends React.Component<
     );
   };
 
+  /*
+  Build a url querystring (including the ?) for a page, range, and entity
+   */
   buildURLParams = (
     page: number,
     range: UserStatsAPIRange,
     entity: Entity
   ): string => {
-    const params = new URLSearchParams({
-      page: page.toString(),
-      range: range as string,
-      entity: entity as string,
-    }).toString();
-    return `?${params}`;
+    return `?page=${page}&range=${range}&entity=${entity}`;
   };
 
   handleResize = () => {
     this.setState({
       graphContainerWidth: this.graphContainer.current?.offsetWidth,
     });
+  };
+
+  /*
+  Handle a click on a link that will perform an action
+   - if control is held down, don't prevent the event from firing
+     this will allow ctrl/cmd-click to open links in a new tab
+   - otherwise, prevent the event from happening and call a callback
+     that performs some action (e.g. load a new page)
+   */
+  handleClickEvent = (
+    e: React.MouseEvent<HTMLAnchorElement>,
+    callback: () => any
+  ) => {
+    if (!e.ctrlKey) {
+      e.preventDefault();
+      callback();
+    }
   };
 
   render() {
@@ -466,7 +467,11 @@ export default class UserEntityChart extends React.Component<
                       <a
                         href={this.buildURLParams(1, "week", entity)}
                         role="button"
-                        onClick={(event) => this.changeRange("week", event)}
+                        onClick={(e) => {
+                          this.handleClickEvent(e, () => {
+                            this.changeRange("week");
+                          });
+                        }}
                       >
                         Week
                       </a>
@@ -475,7 +480,11 @@ export default class UserEntityChart extends React.Component<
                       <a
                         href={this.buildURLParams(1, "month", entity)}
                         role="button"
-                        onClick={(event) => this.changeRange("month", event)}
+                        onClick={(e) => {
+                          this.handleClickEvent(e, () => {
+                            this.changeRange("month");
+                          });
+                        }}
                       >
                         Month
                       </a>
@@ -484,7 +493,11 @@ export default class UserEntityChart extends React.Component<
                       <a
                         href={this.buildURLParams(1, "year", entity)}
                         role="button"
-                        onClick={(event) => this.changeRange("year", event)}
+                        onClick={(e) => {
+                          this.handleClickEvent(e, () => {
+                            this.changeRange("year");
+                          });
+                        }}
                       >
                         Year
                       </a>
@@ -493,7 +506,11 @@ export default class UserEntityChart extends React.Component<
                       <a
                         href={this.buildURLParams(1, "all_time", entity)}
                         role="button"
-                        onClick={(event) => this.changeRange("all_time", event)}
+                        onClick={(e) => {
+                          this.handleClickEvent(e, () => {
+                            this.changeRange("all_time");
+                          });
+                        }}
                       >
                         All Time
                       </a>
@@ -567,9 +584,13 @@ export default class UserEntityChart extends React.Component<
                       }`}
                     >
                       <a
-                        href={this.buildURLParams(prevPage, range, entity)}
+                        href=""
                         role="button"
-                        onClick={(event) => this.changePage(prevPage, event)}
+                        onClick={(e) => {
+                          this.handleClickEvent(e, () => {
+                            this.changePage(prevPage);
+                          });
+                        }}
                       >
                         &larr; Previous
                       </a>
@@ -582,7 +603,11 @@ export default class UserEntityChart extends React.Component<
                       <a
                         href={this.buildURLParams(nextPage, range, entity)}
                         role="button"
-                        onClick={(event) => this.changePage(nextPage, event)}
+                        onClick={(e) => {
+                          this.handleClickEvent(e, () => {
+                            this.changePage(nextPage);
+                          });
+                        }}
                       >
                         Next &rarr;
                       </a>
