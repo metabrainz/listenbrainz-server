9406dd72051cb18f6563850a9c6fbb782455c5b5
Kartik Ohri <kartikohri13@gmail.com>
2021-08-19 15:47:05 +0530

    Update entity stats tests

---
+++ listenbrainz_spark/stats/user/tests/__init__.py
@@ -0,0 +1,15 @@
+from listenbrainz_spark.tests import SparkNewTestCase
+
+
+class StatsTestCase(SparkNewTestCase):
+
+    @classmethod
+    def setUpClass(cls) -> None:
+        super(StatsTestCase, cls).setUpClass()
+        cls.upload_test_listens()
+        cls.get_all_test_listens().createOrReplaceTempView('test_listens')
+
+    @classmethod
+    def tearDownClass(cls) -> None:
+        super(StatsTestCase, cls).tearDownClass()
+        cls.delete_uploaded_listens()
