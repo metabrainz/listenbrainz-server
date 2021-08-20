ad32f9c2d2862dfcb0837084bf1189020aca8a89
Kartik Ohri <kartikohri13@gmail.com>
2021-08-21 01:01:23 +0530

    Fix remaining recommendation tests

--- listenbrainz_spark/recommendations/recording/tests/__init__.py
+++ listenbrainz_spark/recommendations/recording/tests/__init__.py
@@ -1,5 +1,8 @@
 import os
 
+from pyspark import Row
+
+import listenbrainz_spark
 from listenbrainz_spark import utils
 from listenbrainz_spark.path import LISTENBRAINZ_NEW_DATA_DIRECTORY, RECOMMENDATION_RECORDING_MAPPED_LISTENS
 from listenbrainz_spark.tests import SparkNewTestCase, TEST_DATA_PATH
@@ -16,9 +19,14 @@ class RecommendationsTestCase(SparkNewTestCase):
     @classmethod
     def tearDownClass(cls) -> None:
         super(RecommendationsTestCase, cls).tearDownClass()
-        cls.delete_uploaded_listens()
-        if utils.path_exists(RECOMMENDATION_RECORDING_MAPPED_LISTENS):
-            utils.delete_dir(RECOMMENDATION_RECORDING_MAPPED_LISTENS, recursive=True)
+        cls.delete_dir()
+
+    @classmethod
+    def get_candidate_set(cls):
+        return listenbrainz_spark.session.createDataFrame([
+            Row(user_id=1, recording_id=1, user_name='vansika'),
+            Row(user_id=2, recording_id=2, user_name='rob')
+        ])
 
     @classmethod
     def get_dataframe_metadata(cls, df_id):
