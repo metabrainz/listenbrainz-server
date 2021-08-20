e9cb11e5f2f9a66ab7aad8814c1c38754fc2b4fd
Kartik Ohri <kartikohri13@gmail.com>
2021-08-20 23:22:46 +0530

    Fix candidate sets tests

--- listenbrainz_spark/recommendations/recording/tests/__init__.py
+++ listenbrainz_spark/recommendations/recording/tests/__init__.py
@@ -1,4 +1,8 @@
-from listenbrainz_spark.tests import SparkNewTestCase
+import os
+
+from listenbrainz_spark import utils
+from listenbrainz_spark.path import LISTENBRAINZ_NEW_DATA_DIRECTORY, RECOMMENDATION_RECORDING_MAPPED_LISTENS
+from listenbrainz_spark.tests import SparkNewTestCase, TEST_DATA_PATH
 
 
 class RecommendationsTestCase(SparkNewTestCase):
@@ -6,10 +10,15 @@ class RecommendationsTestCase(SparkNewTestCase):
     @classmethod
     def setUpClass(cls) -> None:
         super(RecommendationsTestCase, cls).setUpClass()
+        utils.upload_to_HDFS(LISTENBRAINZ_NEW_DATA_DIRECTORY, os.path.join(TEST_DATA_PATH, 'rec_listens.parquet'))
+        utils.upload_to_HDFS(RECOMMENDATION_RECORDING_MAPPED_LISTENS, os.path.join(TEST_DATA_PATH, 'mapped_listens.parquet'))
 
     @classmethod
     def tearDownClass(cls) -> None:
         super(RecommendationsTestCase, cls).tearDownClass()
+        cls.delete_uploaded_listens()
+        if utils.path_exists(RECOMMENDATION_RECORDING_MAPPED_LISTENS):
+            utils.delete_dir(RECOMMENDATION_RECORDING_MAPPED_LISTENS, recursive=True)
 
     @classmethod
     def get_dataframe_metadata(cls, df_id):
