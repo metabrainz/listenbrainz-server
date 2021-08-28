e7b419733707cddc5e49dc9cacc730540e35d20e
Kartik Ohri <kartikohri13@gmail.com>
2021-08-28 13:22:03 +0530

    LB-772: Add playlists to admin interface

---
+++ listenbrainz/model/playlist_recording.py
@@ -0,0 +1,27 @@
+import uuid
+from datetime import datetime
+
+from sqlalchemy.dialects.postgresql import UUID
+
+from listenbrainz.model import db
+from listenbrainz.webserver.admin import AdminModelView
+
+
+class PlaylistRecording(db.Model):
+    __bind_key__ = 'timescale'
+    __tablename__ = "playlist_recording"
+    __table_args__ = {"schema": "playlist"}
+
+    id = db.Column(db.Integer, autoincrement=True, primary_key=True)
+    playlist_id = db.Column(db.Integer, db.ForeignKey('playlist.playlist.id', ondelete='CASCADE'), nullable=False)
+    position = db.Column(db.Integer, nullable=False)
+    mbid = db.Column(UUID(as_uuid=True), nullable=False, default=uuid.uuid4)
+    added_by_id = db.Column(db.Integer, nullable=False)
+    created = db.Column(db.DateTime(timezone=True), default=datetime.utcnow, nullable=False)
+
+    def __str__(self):
+        return str(self.mbid)
+
+
+class PlaylistRecordingAdminView(AdminModelView):
+    column_list = ['playlist', 'position', 'mbid', 'added_by_id', 'created']
