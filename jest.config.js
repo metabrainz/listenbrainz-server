8ce958e775e4ac845a812740f425024ab7a12fce
Monkey Do <contact@monkeydo.digital>
2021-09-13 21:35:57 +0200

    Add browser location object mocking library
    Solves warnings about navigation not being implemented in JSDOM:
    https://github.com/jsdom/jsdom/issues/2112

--- jest.config.js
+++ jest.config.js
@@ -19,6 +19,8 @@ module.exports = {
     // The paths to modules that run some code to configure or set up the testing environment before each test
     setupFiles: ['<rootDir>/enzyme.config.ts', "jest-canvas-mock"],
 
+    setupFilesAfterEnv:["jest-location-mock"],
+    
     // The test environment that will be used for testing
     testEnvironment: 'jsdom',
 
