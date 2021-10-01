fd044bde879541723282965ae648610adcbd56a2
Kartik Ohri <kartikohri13@gmail.com>
2021-10-01 20:46:09 +0530

    Add sitewide release stats - 2

--- listenbrainz_spark/stats/sitewide/entity.py
+++ listenbrainz_spark/stats/sitewide/entity.py
@@ -5,8 +5,11 @@ from typing import List, Optional
 
 from data.model.sitewide_artist_stat import SitewideArtistRecord
 from data.model.sitewide_entity import SitewideEntityStatMessage
+from data.model.user_artist_stat import UserArtistRecord
+from data.model.user_release_stat import UserReleaseRecord
 from listenbrainz_spark.stats import get_dates_for_stats_range
 from listenbrainz_spark.stats.sitewide.artist import get_artists
+from listenbrainz_spark.stats.sitewide.release import get_releases
 from listenbrainz_spark.utils import get_listens_from_new_dump
 from pydantic import ValidationError
 
@@ -16,10 +19,12 @@ logger = logging.getLogger(__name__)
 
 entity_handler_map = {
     "artists": get_artists,
+    "releases": get_releases
 }
 
 entity_model_map = {
-    "artists": SitewideArtistRecord
+    "artists": UserArtistRecord,
+    "releases": UserReleaseRecord
 }
 
 
