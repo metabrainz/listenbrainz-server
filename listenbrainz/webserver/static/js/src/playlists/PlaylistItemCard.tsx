847a3eb01cb3d10405db819f5fa3babd5cbe419e
Monkey Do <contact@monkeydo.digital>
2021-10-07 18:01:46 +0200

    Fix some typos, remove unused import

--- listenbrainz/webserver/static/js/src/playlists/PlaylistItemCard.tsx
+++ listenbrainz/webserver/static/js/src/playlists/PlaylistItemCard.tsx
@@ -67,7 +67,7 @@ export default class PlaylistItemCard extends React.Component<
   /** React to events sent by BrainzPlayer */
   receiveBrainzPlayerMessage = (event: MessageEvent) => {
     if (event.origin !== window.location.origin) {
-      // Reveived postMessage from different origin, ignoring it
+      // Received postMessage from different origin, ignoring it
       return;
     }
     const { type, payload } = event.data;
