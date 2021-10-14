b4156937030bf1f450ed7d391c4adaeccd2abf37
Kartik Ohri <kartikohri13@gmail.com>
2021-10-14 20:11:19 +0530

    Change pxz to xz

--- listenbrainz/db/dump.py
+++ listenbrainz/db/dump.py
@@ -269,7 +269,7 @@ def _create_dump(location, dump_type, tables, dump_time, threads=DUMP_DEFAULT_TH
 
     with open(archive_path, 'w') as archive:
 
-        pxz_command = ['pxz', '--compress',
+        pxz_command = ['xz', '--compress',
                        '-T{threads}'.format(threads=threads)]
         pxz = subprocess.Popen(
             pxz_command, stdin=subprocess.PIPE, stdout=archive)
@@ -605,7 +605,7 @@ def _import_dump(archive_path, dump_type, tables, threads=DUMP_DEFAULT_THREAD_CO
                             db.DUMP_DEFAULT_THREAD_COUNT
     """
 
-    pxz_command = ['pxz', '--decompress', '--stdout',
+    pxz_command = ['xz', '--decompress', '--stdout',
                    archive_path, '-T{threads}'.format(threads=threads)]
     pxz = subprocess.Popen(pxz_command, stdout=subprocess.PIPE)
 
