51ca456d3835143d2ee0fde4ff142e2ef6d0c601
Robert Kaye <rob@musicbrainz.org>
2021-10-26 13:08:34 +0200

    Sort incremental dumps by created and add an index on the created column of the listens table

--- admin/timescale/create_indexes.sql
+++ admin/timescale/create_indexes.sql
@@ -1,6 +1,7 @@
 BEGIN;
 
 CREATE INDEX listened_at_user_name_ndx_listen ON listen (listened_at DESC, user_name);
+CREATE INDEX created_user_name_ndx_listen ON listen (created);
 CREATE UNIQUE INDEX listened_at_track_name_user_name_ndx_listen ON listen (listened_at DESC, track_name, user_name);
 
 -- View indexes are created in listenbrainz/db/timescale.py
