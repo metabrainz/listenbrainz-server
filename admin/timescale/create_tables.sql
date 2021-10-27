abe191e55b8ba3716d53dd6c90fc32332e056a19
Kartik Ohri <kartikohri13@gmail.com>
2021-10-28 00:52:01 +0530

    Fix bug in spark incremental dumps - use full outer joins
    The various mapping columns should only be null if the match_type is
    no_match, otherwise the columns should be non null. we have had bugs
    where we completely forgot to insert values for a column (#1684) and it
    went unchecked because it is not possible to mark the column as NOT NULL
    . however, we can use this constraint to enforce the NOT NULL check
    conditionally. this should help in preventing regressions and future bugs.

--- admin/timescale/create_tables.sql
+++ admin/timescale/create_tables.sql
@@ -65,4 +65,33 @@ create table listen_mbid_mapping (
 ALTER TABLE listen_mbid_mapping
     ADD CONSTRAINT listen_mbid_mapping_artist_mbids_check
     CHECK ( array_ndims(artist_mbids) = 1 );
+
+-- the various mapping columns should only be null if the match_type is no_match, otherwise the columns should be
+-- non null. we have had bugs where we completely forgot to insert values for a column and it went unchecked because
+-- it is not possible to mark the column as NOT NULL. however, we can use this constraint to enforce the NOT NULL
+-- check conditionally. this should help in preventing regressions and future bugs.
+ALTER TABLE listen_mbid_mapping
+    ADD CONSTRAINT listen_mbid_mapping_fields_null_check
+    CHECK (
+        (
+              match_type = 'no_match'
+          AND artist_credit_id IS NULL
+          AND recording_mbid IS NULL
+          AND release_mbid IS NULL
+          AND release_name IS NULL
+          AND artist_mbids IS NULL
+          AND artist_credit_name IS NULL
+          AND recording_name IS NULL
+        ) OR (
+              match_type <> 'no_match'
+          AND artist_credit_id IS NOT NULL
+          AND recording_mbid IS NOT NULL
+          AND release_mbid IS NOT NULL
+          AND release_name IS NOT NULL
+          AND artist_mbids IS NOT NULL
+          AND artist_credit_name IS NOT NULL
+          AND recording_name IS NOT NULL
+        )
+    );
+
 COMMIT;
