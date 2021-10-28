615a957679d15fd3039ae919279e532e09b453b9
Kartik Ohri <kartikohri13@gmail.com>
2021-10-29 01:17:19 +0530

    Add doc

--- listenbrainz/webserver/models.py
+++ listenbrainz/webserver/models.py
@@ -2,5 +2,6 @@ import pydantic
 
 
 class SubmitListenUserMetadata(pydantic.BaseModel):
+    """ Represents the fields of a user required in submission pipeline"""
     musicbrainz_id: str
     user_id: int
