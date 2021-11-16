93638a5cbf81e4b7c14a1d7da7d51bfa09ca2c2f
Kartik Ohri <kartikohri13@gmail.com>
2021-11-17 01:55:28 +0530

    Debugging - 2

--- listenbrainz/db/stats.py
+++ listenbrainz/db/stats.py
@@ -271,7 +271,8 @@ def get_year_in_music(user_id):
 
 
 def insert_most_prominent_color(data):
-    with db.engine.connect() as connection, connection.cursor() as cursor:
+    connection = db.engine.raw_connection()
+    with connection.cursor() as cursor:
         cursor.execute(sqlalchemy.text("""
             WITH user_colors AS (
                 SELECT "user".id AS user_id
@@ -288,3 +289,4 @@ def insert_most_prominent_color(data):
           DO UPDATE SET data = statistics.year_in_music.data || EXCLUDED.data
         """), colors=data)
         current_app.logger.error("Most Prominent Color Affected Row Count: %s", cursor.rowcount)
+    connection.commit()
