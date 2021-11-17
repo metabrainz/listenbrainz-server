479948ffae403b31c1a65acf32d74344cf5d1084
Kartik Ohri <kartikohri13@gmail.com>
2021-11-17 20:38:24 +0530

    Drop global similarity

--- listenbrainz_spark/year_in_music/similar_users.py
+++ listenbrainz_spark/year_in_music/similar_users.py
@@ -7,9 +7,18 @@ from listenbrainz_spark.user_similarity import user_similarity
 def get_similar_users():
     train_model_window = (date.today() - date(2021, 1, 1)).days
     create_dataframes.main(train_model_window, "similar_users", 50)
-    messages = user_similarity.main(25)
-    for message in messages:
-        yield {
-            "type": "similar_users_2021",
-            "data": message["data"]
+    similar_users_df = user_similarity.get_similar_users_df(25)
+
+    itr = similar_users_df.toLocalIterator()
+    message = {
+        row.user_name: {
+            user.other_user_name: user.similarity
+            for user in row.similar_users
         }
+        for row in itr
+    }
+
+    yield {
+        "type": "similar_users_2021",
+        "data": message
+    }
