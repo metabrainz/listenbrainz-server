c8c09f8cb8a2aafae493c77ff01e3aa07ccaf642
rik61072@gmail.com <rik61072@gmail.com>
2021-11-23 01:10:26 +0530

    DOM and inline changes, plus snapshot update

--- listenbrainz/webserver/static/js/src/MissingMBData.tsx
+++ listenbrainz/webserver/static/js/src/MissingMBData.tsx
@@ -182,94 +182,8 @@ export default class MissingMBDataPage extends React.Component<
                 >
                   <Loader isLoading={loading} />
                 </div>
-                <div>
-                  {missingData.map((data) => {
-                    const listen = {
-                      listened_at:
-                        new Date(`${data.listened_at}Z`).getTime() / 1000,
-                      user_name: user.name,
-                      track_metadata: {
-                        artist_name: data.artist_name,
-                        track_name: data.recording_name,
-                        release_name: data?.release_name,
-                      },
-                    };
-                    const additionalActions = (
-                      <>
-                        <ListenControl
-                          icon={faPlus}
-                          title=""
-                          // eslint-disable-next-line react/jsx-no-bind
-                          action={this.submitMissingData.bind(this, listen)}
-                        />
-                      </>
-                    );
-                    return (
-                      <div className="event-content" style={{ width: "100%" }}>
-                        <ListenCard
-                          key={`${data.recording_name}-${data.artist_name}-${data.listened_at}`}
-                          showTimestamp
-                          showUsername={false}
-                          newAlert={newAlert}
-                          listen={listen}
-                          additionalActions={additionalActions}
-                        />
-                      </div>
-                    );
-                  })}
-                </div>
-                <ul className="pager" style={{ display: "flex" }}>
-                  <li
-                    className={`previous ${
-                      currPage && currPage <= 1 ? "hidden" : ""
-                    }`}
-                  >
-                    <a
-                      role="button"
-                      onClick={this.handleClickPrevious}
-                      onKeyDown={(e) => {
-                        if (e.key === "Enter") this.handleClickPrevious();
-                      }}
-                      tabIndex={0}
-                    >
-                      &larr; Previous
-                    </a>
-                  </li>
-                  <li
-                    className={`next ${
-                      currPage && currPage >= totalPages ? "hidden" : ""
-                    }`}
-                    style={{ marginLeft: "auto" }}
-                  >
-                    <a
-                      role="button"
-                      onClick={this.handleClickNext}
-                      onKeyDown={(e) => {
-                        if (e.key === "Enter") this.handleClickNext();
-                      }}
-                      tabIndex={0}
-                    >
-                      Next &rarr;
-                    </a>
-                  </li>
-                </ul>
-              </div>
-            </div>
-            <div
-              className="col-xs-12"
-              /* eslint-disable no-dupe-keys */
-              style={{
-                position: "-webkit-sticky",
-                // @ts-ignore
-                position: "sticky",
-                top: 20,
-              }}
-              /* eslint-enable no-dupe-keys */
-            >
-              <BrainzPlayer
-                direction={direction}
-                listens={missingData?.map((data) => {
-                  return {
+                {missingData.map((data) => {
+                  const listen = {
                     listened_at:
                       new Date(`${data.listened_at}Z`).getTime() / 1000,
                     user_name: user.name,
@@ -279,13 +193,95 @@ export default class MissingMBDataPage extends React.Component<
                       release_name: data?.release_name,
                     },
                   };
+                  const additionalActions = (
+                    <>
+                      <ListenControl
+                        icon={faPlus}
+                        title=""
+                        // eslint-disable-next-line react/jsx-no-bind
+                        action={this.submitMissingData.bind(this, listen)}
+                      />
+                    </>
+                  );
+                  return (
+                    <ListenCard
+                      key={`${data.recording_name}-${data.artist_name}-${data.listened_at}`}
+                      showTimestamp
+                      showUsername={false}
+                      newAlert={newAlert}
+                      listen={listen}
+                      additionalActions={additionalActions}
+                    />
+                  );
                 })}
-                newAlert={newAlert}
-                listenBrainzAPIBaseURI={APIService.APIBaseURI}
-                refreshSpotifyToken={APIService.refreshSpotifyToken}
-                refreshYoutubeToken={APIService.refreshYoutubeToken}
-              />
+              </div>
+              <ul className="pager" style={{ display: "flex" }}>
+                <li
+                  className={`previous ${
+                    currPage && currPage <= 1 ? "hidden" : ""
+                  }`}
+                >
+                  <a
+                    role="button"
+                    onClick={this.handleClickPrevious}
+                    onKeyDown={(e) => {
+                      if (e.key === "Enter") this.handleClickPrevious();
+                    }}
+                    tabIndex={0}
+                  >
+                    &larr; Previous
+                  </a>
+                </li>
+                <li
+                  className={`next ${
+                    currPage && currPage >= totalPages ? "hidden" : ""
+                  }`}
+                  style={{ marginLeft: "auto" }}
+                >
+                  <a
+                    role="button"
+                    onClick={this.handleClickNext}
+                    onKeyDown={(e) => {
+                      if (e.key === "Enter") this.handleClickNext();
+                    }}
+                    tabIndex={0}
+                  >
+                    Next &rarr;
+                  </a>
+                </li>
+              </ul>
             </div>
+          </div>
+          <div
+            className="col-xs-12"
+            /* eslint-disable no-dupe-keys */
+            style={{
+              position: "-webkit-sticky",
+              // @ts-ignore
+              position: "sticky",
+              top: 20,
+            }}
+            /* eslint-enable no-dupe-keys */
+          >
+            <BrainzPlayer
+              direction={direction}
+              listens={missingData?.map((data) => {
+                return {
+                  listened_at:
+                    new Date(`${data.listened_at}Z`).getTime() / 1000,
+                  user_name: user.name,
+                  track_metadata: {
+                    artist_name: data.artist_name,
+                    track_name: data.recording_name,
+                    release_name: data?.release_name,
+                  },
+                };
+              })}
+              newAlert={newAlert}
+              listenBrainzAPIBaseURI={APIService.APIBaseURI}
+              refreshSpotifyToken={APIService.refreshSpotifyToken}
+              refreshYoutubeToken={APIService.refreshYoutubeToken}
+            />
           </div>
         </div>
       </div>
