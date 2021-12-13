726214741cda1586b1cedb0a2d80b39a0305a303
Kartik Ohri <kartikohri13@gmail.com>
2021-12-13 22:34:53 +0530

    fix user name key

--- listenbrainz/spark/handlers.py
+++ listenbrainz/spark/handlers.py
@@ -375,13 +375,13 @@ def handle_most_listened_year(message):
 
 
 def handle_top_stats(message):
-    musicbrainz_id = message["user_name"]
+    musicbrainz_id = message["musicbrainz_id"]
     user = db_user.get_by_mb_id(musicbrainz_id)
     year_in_music.handle_top_stats(user["id"], message["data"])
 
 
 def handle_listens_per_day(message):
-    musicbrainz_id = message["user_name"]
+    musicbrainz_id = message["musicbrainz_id"]
     user = db_user.get_by_mb_id(musicbrainz_id)
     year_in_music.handle_listens_per_day(user["id"], message["data"])
 
