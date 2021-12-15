f24ea4a3ca1b3aba01eb24312a00040b2a085258
akshaaatt <tiwariakshat03@gmail.com>
2021-12-15 13:05:23 +0530

    Revert to props

--- listenbrainz/webserver/static/js/src/year-in-music/YearInMusic.tsx
+++ listenbrainz/webserver/static/js/src/year-in-music/YearInMusic.tsx
@@ -659,7 +659,7 @@ document.addEventListener("DOMContentLoaded", () => {
       <GlobalAppContext.Provider value={globalProps}>
         <YearInMusicWithAlertNotifications
           user={user}
-          yearInMusicData={fakeData}
+          yearInMusicData={yearInMusicData}
         />
       </GlobalAppContext.Provider>
     </ErrorBoundary>,
