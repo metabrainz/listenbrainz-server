e81eff599acfbd44906866231656c6fb1e66f3cc
Monkey Do <contact@monkeydo.digital>
2021-12-15 18:13:37 +0100

    YIM: Fixes to YIM page

--- listenbrainz/webserver/static/js/src/year-in-music/YearInMusic.tsx
+++ listenbrainz/webserver/static/js/src/year-in-music/YearInMusic.tsx
@@ -129,37 +129,37 @@ export default class YearInMusic extends React.Component<
 
   getCoverArt = async () => {
     const { ca } = this.state;
-    for (const caKey in ca) {
-      console.log(caKey);
-      try {
-        const CAAResponse = await fetch(
-          `https://coverartarchive.org/release/${caKey}`
-        );
-        if (CAAResponse.ok) {
-          const body: CoverArtArchiveResponse = await CAAResponse.json();
-          if (!body.images?.[0]?.thumbnails) {
-            return undefined;
-          }
-          const { thumbnails } = body.images[0];
-          this.setState({
-            ca: {
-              key:
-                thumbnails[250] ??
-                thumbnails.small ??
-                // If neither of the above exists, return the first one we find
-                // @ts-ignore
-                thumbnails[Object.keys(thumbnails)?.[0]],
-            },
-          });
-        }
-      } catch (error) {
-        // eslint-disable-next-line no-console
-        console.warn(
-          `Couldn't fetch Cover Art Archive entry for ${caKey}`,
-          error
-        );
-      }
-    }
+    // for (const caKey in ca) {
+    //   console.log(caKey);
+    //   try {
+    //     const CAAResponse = await fetch(
+    //       `https://coverartarchive.org/release/${caKey}`
+    //     );
+    //     if (CAAResponse.ok) {
+    //       const body: CoverArtArchiveResponse = await CAAResponse.json();
+    //       if (!body.images?.[0]?.thumbnails) {
+    //         return undefined;
+    //       }
+    //       const { thumbnails } = body.images[0];
+    //       this.setState({
+    //         ca: {
+    //           key:
+    //             thumbnails[250] ??
+    //             thumbnails.small ??
+    //             // If neither of the above exists, return the first one we find
+    //             // @ts-ignore
+    //             thumbnails[Object.keys(thumbnails)?.[0]],
+    //         },
+    //       });
+    //     }
+    //   } catch (error) {
+    //     // eslint-disable-next-line no-console
+    //     console.warn(
+    //       `Couldn't fetch Cover Art Archive entry for ${caKey}`,
+    //       error
+    //     );
+    //   }
+    // }
     return false;
   };
 
@@ -354,33 +354,10 @@ export default class YearInMusic extends React.Component<
                   },
                 }}
               >
-                {yearInMusicData.top_releases.slice(0, 50).map((release) => (
+                {yearInMusicData.top_releases.slice(0, 10).map((release) => (
                   <img
                     src="/static/img/cover-art-placeholder.jpg"
                     alt={release.release_name}
-                    onClick={() => {
-                      window.postMessage(
-                        {
-                          brainzplayer_event: "play-listen",
-                          payload: {
-                            listened_at: 0,
-                            track_metadata: {
-                              artist_name: release.artist_name,
-                              release_name: release.release_name,
-                              additional_info: {
-                                release_mbid: release.release_mbid,
-                                artist_mbids: release.artist_mbids,
-                              },
-                            },
-                          },
-                        },
-                        window.location.origin
-                      );
-                    }}
-                    style={{
-                      display: "block",
-                      width: "100%",
-                    }}
                   />
                 ))}
               </Coverflow>
@@ -437,6 +414,32 @@ export default class YearInMusic extends React.Component<
                   "artist",
                   artist.artist_name,
                   artist.artist_mbids[0]
+                );
+                const thumbnail = (
+                  <span className="badge badge-info">
+                    {artist.listen_count} listens
+                  </span>
+                );
+                return (
+                  <ListenCard
+                    compact
+                    key={`top-artists-${artist.artist_name}-${artist.artist_mbids}`}
+                    listen={{
+                      listened_at: 0,
+                      track_metadata: {
+                        track_name: "",
+                        artist_name: artist.artist_name,
+                        additional_info: {
+                          artist_mbids: artist.artist_mbids,
+                        },
+                      },
+                    }}
+                    thumbnail={thumbnail}
+                    listenDetails={details}
+                    showTimestamp={false}
+                    showUsername={false}
+                    newAlert={newAlert}
+                  />
                 );
               })}
             </div>
