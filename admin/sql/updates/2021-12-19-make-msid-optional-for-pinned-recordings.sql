d6884bf8619bbd188a1a01cec45c1425d68b1fdf
Kartik Ohri <kartikohri13@gmail.com>
2021-12-19 18:06:09 +0530

    add check that either mbid or msid is not null

--- admin/sql/updates/2021-12-19-make-msid-optional-for-pinned-recordings.sql
+++ admin/sql/updates/2021-12-19-make-msid-optional-for-pinned-recordings.sql
@@ -2,4 +2,8 @@ BEGIN;
 
 ALTER TABLE pinned_recording ALTER COLUMN recording_msid DROP NOT NULL;
 
+ALTER TABLE pinned_recording
+    ADD CONSTRAINT recording_msid_or_recording_mbid_check
+    CHECK ( recording_msid IS NOT NULL OR recording_mbid IS NOT NULL );
+
 COMMIT;
