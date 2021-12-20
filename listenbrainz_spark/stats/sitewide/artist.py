bffa7648d7989976c6107e118c4fbeaa20bf168d
Kartik Ohri <kartikohri13@gmail.com>
2021-12-20 16:45:52 +0530

    Cap listen counts for sitewide stats (#1763)
    * Cap listen_count to 500 sitewide recordings, artists and release stats

--- listenbrainz_spark/stats/sitewide/artist.py
+++ listenbrainz_spark/stats/sitewide/artist.py
@@ -1,13 +1,14 @@
 from listenbrainz_spark.stats import run_query, SITEWIDE_STATS_ENTITY_LIMIT
 
 
-def get_artists(table: str, limit: int = SITEWIDE_STATS_ENTITY_LIMIT):
+def get_artists(table: str, user_listen_count_limit, top_artists_limit: int = SITEWIDE_STATS_ENTITY_LIMIT):
     """ Get artist information (artist_name, artist_msid etc) for every time range specified
         the "time_range" table ordered by listen count
 
         Args:
-            table: Name of the temporary table.
-            limit: number of top artists to retain
+            table: name of the temporary table
+            user_listen_count_limit: per user per entity listen count above which it should be capped
+            top_artists_limit: number of top artists to retain
         Returns:
             iterator (iter): An iterator over result
     """
@@ -16,21 +17,30 @@ def get_artists(table: str, limit: int = SITEWIDE_STATS_ENTITY_LIMIT):
     # order of collected results is not guaranteed so sort again
     # with sort_array.
     result = run_query(f"""
-        WITH intermediate_table as (
-            SELECT first(artist_name) AS any_artist_name
+        WITH user_counts as (
+            SELECT user_name
+                 , first(artist_name) AS artist_name
                  , artist_credit_mbids
-                 , count(*) as listen_count
+                 , LEAST(count(*), {user_listen_count_limit}) as listen_count
               FROM {table}
+          GROUP BY user_name
+                 , lower(artist_name)
+                 , artist_credit_mbids
+        ), intermediate_table AS (
+            SELECT first(artist_name) AS artist_name
+                 , artist_credit_mbids
+                 , SUM(listen_count) as total_listen_count
+              FROM user_counts
           GROUP BY lower(artist_name)
                  , artist_credit_mbids
-          ORDER BY listen_count DESC
-             LIMIT {limit}
+          ORDER BY total_listen_count DESC
+             LIMIT {top_artists_limit}
         )
         SELECT sort_array(
                     collect_list(
                         struct(
-                            listen_count
-                          , any_artist_name AS artist_name
+                            total_listen_count AS listen_count
+                          , artist_name
                           , coalesce(artist_credit_mbids, array()) AS artist_mbids
                         )
                     )
