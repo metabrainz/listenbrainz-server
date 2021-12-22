cee50fb68c31ee178f472798de64499f6b146698
Kartik Ohri <kartikohri13@gmail.com>
2021-12-22 15:03:04 +0530

    Add count field

--- data/model/sitewide_entity.py
+++ data/model/sitewide_entity.py
@@ -1,4 +1,4 @@
-from pydantic import constr
+from pydantic import constr, NonNegativeInt
 
 from data.model.common_stat_spark import StatMessage
 from data.model.user_entity import EntityRecord
@@ -7,3 +7,4 @@ from data.model.user_entity import EntityRecord
 class SitewideEntityStatMessage(StatMessage[EntityRecord]):
     """ Format of messages sent to the ListenBrainz Server """
     entity: constr(min_length=1) # The entity for which stats are calculated, i.e artist, release or recording
+    count: NonNegativeInt
--- listenbrainz_spark/stats/sitewide/tests/test_sitewide_artist.py
+++ listenbrainz_spark/stats/sitewide/tests/test_sitewide_artist.py
@@ -6,7 +6,6 @@ from listenbrainz_spark.stats.user.tests import StatsTestCase
 class SitewideArtistTestCase(StatsTestCase):
 
     def test_get_artist(self):
-        self.maxDiff = None
         with open(self.path_to_data_file('sitewide_top_artists_output.json')) as f:
             expected = json.load(f)
         received = list(entity.get_entity_stats('artists', 'all_time'))
