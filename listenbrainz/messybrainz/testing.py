098a69ca53334e9baa65cd28ca17aa41b80956c7
Kartik Ohri <kartikohri13@gmail.com>
2021-12-23 22:16:21 +0530

    try some more stuff - 3

--- listenbrainz/messybrainz/testing.py
+++ listenbrainz/messybrainz/testing.py
@@ -23,9 +23,6 @@ from listenbrainz import config
 
 ADMIN_SQL_DIR = os.path.join(os.path.dirname(os.path.realpath(__file__)), '..', '..', 'admin', 'messybrainz', 'sql')
 
-logging.basicConfig()
-log = logging.getLogger(__name__)
-
 
 class MessyBrainzTestCase(unittest.TestCase):
 
@@ -33,11 +30,9 @@ class MessyBrainzTestCase(unittest.TestCase):
         init_db_connection(config.MESSYBRAINZ_SQLALCHEMY_DATABASE_URI)
         self.drop_tables()
         self.init_db()
-        log.error("MessyBrainzTestCase.setUp called")
 
     def tearDown(self):
         self.drop_tables()
-        log.error("MessyBrainzTestCase.tearDown called")
 
     def init_db(self):
         run_sql_script(os.path.join(ADMIN_SQL_DIR, 'create_tables.sql'))
