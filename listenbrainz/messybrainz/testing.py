3684432231bab5590dfd43cc0f76e052015f88f2
Kartik Ohri <kartikohri13@gmail.com>
2021-12-24 15:49:10 +0530

    fix whitespace

--- listenbrainz/messybrainz/testing.py
+++ listenbrainz/messybrainz/testing.py
@@ -15,6 +15,7 @@
 # You should have received a copy of the GNU General Public License along
 # with this program; if not, write to the Free Software Foundation, Inc.,
 # 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
+
 import os
 import unittest
 from listenbrainz.messybrainz import init_db_connection, run_sql_script
