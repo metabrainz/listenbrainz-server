a2a24f3c89ff9aad56625905da5f55676b013e14
Kartik Ohri <kartikohri13@gmail.com>
2022-01-16 00:25:15 +0530

    make min_listened_at and max_listened_at nullable

--- admin/timescale/updates/2022-01-13-add-listen-helper-table.sql
+++ admin/timescale/updates/2022-01-13-add-listen-helper-table.sql
@@ -2,8 +2,8 @@ BEGIN;
 CREATE TABLE listen_helper (
     user_id             INTEGER                     NOT NULL,
     count               BIGINT                      NOT NULL, -- count of listens the user has earlier than `created`
-    min_listened_at     BIGINT                      NOT NULL, -- minimum listened_at timestamp seen for the user in listens till `created`
-    max_listened_at     BIGINT                      NOT NULL, -- maximum listened_at timestamp seen for the user in listens till `created`
+    min_listened_at     BIGINT, -- minimum listened_at timestamp seen for the user in listens till `created`
+    max_listened_at     BIGINT, -- maximum listened_at timestamp seen for the user in listens till `created`
     created             TIMESTAMP WITH TIME ZONE    NOT NULL  -- the created timestamp when data for this user was updated last
 );
 CREATE UNIQUE INDEX user_id_ndx_listen_count ON listen_helper (user_id);
