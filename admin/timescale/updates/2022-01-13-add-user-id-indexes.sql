7ff0c1263f0ac13184d619ae1a6719b43066fded
Kartik Ohri <kartikohri13@gmail.com>
2022-01-15 10:52:30 +0530

    Update sql script to drop default

--- admin/timescale/updates/2022-01-13-add-user-id-indexes.sql
+++ admin/timescale/updates/2022-01-13-add-user-id-indexes.sql
@@ -5,3 +5,9 @@
 -- This operation doesn't support being run inside a transaction.
 CREATE INDEX listened_at_user_id_ndx_listen ON listen (listened_at DESC, user_id) WITH (timescaledb.transaction_per_chunk);
 CREATE UNIQUE INDEX listened_at_track_name_user_id_ndx_listen ON listen (listened_at DESC, track_name, user_id) WITH (timescaledb.transaction_per_chunk);
+
+-- the user_id was initially set to DEFAULT to 0 while a background process updated user ids
+-- according to the user name now the process is complete so a default is no longer needed.
+BEGIN;
+ALTER TABLE listen ALTER COLUMN user_id DROP DEFAULT;
+COMMIT;
