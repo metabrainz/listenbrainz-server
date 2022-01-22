8fda28e4c45f805d69af1baed759c85cef614253
Kartik Ohri <kartikohri13@gmail.com>
2022-01-22 23:05:53 +0530

    fix delete query

--- listenbrainz/webserver/views/test/test_profile.py
+++ listenbrainz/webserver/views/test/test_profile.py
@@ -248,6 +248,7 @@ class ProfileViewsTestCase(IntegrationTestCase):
         self.assertDictEqual(results[0], {
             'inserted_at': 0,
             'listened_at': 1539509881,
+            'recording_mbid': None,
             'recording_msid': '6c617681-281e-4dae-af59-8e00f93c4376',
             'user_name': None,
             'track_metadata': {
@@ -262,6 +263,7 @@ class ProfileViewsTestCase(IntegrationTestCase):
         self.assertDictEqual(results[1], {
             'inserted_at': 0,
             'listened_at': 1539441702,
+            'recording_mbid': None,
             'recording_msid': '7ad53fd7-5b40-4e13-b680-52716fb86d5f',
             'user_name': None,
             'track_metadata': {
@@ -276,6 +278,7 @@ class ProfileViewsTestCase(IntegrationTestCase):
         self.assertDictEqual(results[2], {
             'inserted_at': 0,
             'listened_at': 1539441531,
+            'recording_mbid': None,
             'recording_msid': None,
             'user_name': None,
             'track_metadata': {
@@ -322,6 +325,7 @@ class ProfileViewsTestCase(IntegrationTestCase):
         results = ujson.loads(r.data.decode('utf-8'))
 
         self.assertDictEqual(results[0], {
+            'recording_mbid': None,
             'recording_msid': '6c617681-281e-4dae-af59-8e00f93c4376',
             'score': 1,
             'user_id': None,
@@ -329,6 +333,7 @@ class ProfileViewsTestCase(IntegrationTestCase):
             'track_metadata': None,
         })
         self.assertDictEqual(results[1], {
+            'recording_mbid': None,
             'recording_msid': '7ad53fd7-5b40-4e13-b680-52716fb86d5f',
             'score': 1,
             'user_id': None,
@@ -336,6 +341,7 @@ class ProfileViewsTestCase(IntegrationTestCase):
             'track_metadata': None,
         })
         self.assertDictEqual(results[2], {
+            'recording_mbid': None,
             'recording_msid': '7816411a-2cc6-4e43-b7a1-60ad093c2c31',
             'score': -1,
             'user_id': None,
