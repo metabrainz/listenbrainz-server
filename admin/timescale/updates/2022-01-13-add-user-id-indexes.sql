eb2ade9349cdf4d3322a4299ecaecdf6cea2891c
Kartik Ohri <kartikohri13@gmail.com>
2022-01-28 17:24:28 +0530

    Apply PR feedback

--- admin/timescale/updates/2022-01-13-add-user-id-indexes.sql
+++ admin/timescale/updates/2022-01-13-add-user-id-indexes.sql
@@ -4,7 +4,9 @@
 -- This lets inserts and selects work on the table while it's running
 -- This operation doesn't support being run inside a transaction.
 CREATE INDEX listened_at_user_id_ndx_listen ON listen (listened_at DESC, user_id) WITH (timescaledb.transaction_per_chunk);
-CREATE UNIQUE INDEX listened_at_track_name_user_id_ndx_listen ON listen (listened_at DESC, track_name, user_id) WITH (timescaledb.transaction_per_chunk);
+
+-- unique index cannot be created using transaction_per_chunk
+CREATE UNIQUE INDEX listened_at_track_name_user_id_ndx_listen ON listen (listened_at DESC, track_name, user_id);
 
 -- the user_id was initially set to DEFAULT to 0 while a background process updated user ids
 -- according to the user name now the process is complete so a default is no longer needed.
