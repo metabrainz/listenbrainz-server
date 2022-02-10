f0cd150dc90e01349f64a42048f7038b13bd8c3f
Robert Kaye <rob@musicbrainz.org>
2022-02-10 13:40:07 +0100

    Basic playlist creation is in place

--- listenbrainz/webserver/views/player.py
+++ listenbrainz/webserver/views/player.py
@@ -1,6 +1,13 @@
-import ujson
+from datetime import datetime
+
 from flask import Blueprint, render_template, current_app, request
 from flask_login import current_user, login_required
+from werkzeug.exceptions import BadRequest
+import ujson
+
+from listenbrainz.webserver.views.playlist_api import serialize_jspf
+from listenbrainz.db.model.playlist import WritablePlaylistRecording, WritablePlaylist
+from listenbrainz.webserver.views.api_tools import is_valid_uuid
 
 player_bp = Blueprint("player", __name__)
 
@@ -56,12 +63,39 @@ def load():
         user=current_user
     )
 
-@player_bp.route("/instant", methods=["GET"])
+@player_bp.route("/", methods=["GET"])
 def load_instant():
     """
+        This endpoint takes in a list of recording_mbids and then loads their metadata and creates
+        a JSPF file from this data. 
     """
+
+    recordings = request.args.get("recording_mbids", default=None)
+    if recordings is None:
+        raise BadRequest("recording_mbids argument must be present and contain a comma separated list of recording_mbids")
+
+    recording_mbids = []
+    for mbid in recordings.split(","):
+        if not is_valid_uuid(mbid):
+            raise BadRequest(f"Recording mbid {mbid} is not valid.")
+
+        recording_mbids.append(mbid)
+
+    desc = request.args.get("desc", default="")
+    if not desc:
+        desc = "Instant playlist"
+
+    name = request.args.get("name", default="")
+    if not name:
+        name = "Instant playlist"
+
+    now = datetime.now()
+    playlist = WritablePlaylist(description=desc, name=name, creator="listenbrainz", creator_id=1, created=now)
+    for i, mbid in enumerate(recording_mbids):
+        rec = WritablePlaylistRecording(position=i, mbid=mbid, added_by_id=1, created=now)
+        playlist.recordings.append(rec)
 
     return render_template(
         "index/player.html",
-        props=ujson.dumps(jspf)
+        props=ujson.dumps(serialize_jspf(playlist))
     )
