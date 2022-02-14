3b5d42970b145e726ded559319e794760f0c9b0b
akshaaatt <tiwariakshat03@gmail.com>
2022-02-14 20:32:18 +0530

    Try making the button run away on mouse enter

--- listenbrainz/webserver/static/js/info.js
+++ listenbrainz/webserver/static/js/info.js
@@ -1,31 +1,45 @@
-document.getElementById("show-hide-token").addEventListener("click", function() {
-    var token = document.getElementById("auth-token");
-    var btn = document.getElementById("show-hide-token");
+document
+  .getElementById("show-hide-token")
+  .addEventListener("click", function () {
+    const token = document.getElementById("auth-token");
+    const btn = document.getElementById("show-hide-token");
 
-    if (token.type === 'password') {
-        token.type = 'text';
-        btn.className = 'btn btn-info glyphicon glyphicon-eye-close';
-        btn.title = "Hide user token";
+    if (token.type === "password") {
+      token.type = "text";
+      btn.className = "btn btn-info glyphicon glyphicon-eye-close";
+      btn.title = "Hide user token";
     } else {
-        token.type = 'password';
-        btn.className = 'btn btn-info glyphicon glyphicon-eye-open';
-        btn.title = "Show user token";
+      token.type = "password";
+      btn.className = "btn btn-info glyphicon glyphicon-eye-open";
+      btn.title = "Show user token";
     }
-});
+  });
 
 function copy(token) {
-    token.select();
-    document.execCommand("copy");
+  token.select();
+  document.execCommand("copy");
 }
 
-document.getElementById("copy-token").addEventListener("click", function() {
-    var token = document.getElementById("auth-token");
-    if (token.type === 'password') {
-        token.type = 'text';
-        copy(token);
-        token.type = 'password';
-    }
+document.getElementById("copy-token").addEventListener("click", function () {
+  const token = document.getElementById("auth-token");
+  if (token.type === "password") {
+    token.type = "text";
     copy(token);
-    var copyButton = document.getElementById("copy-token");
-    copyButton.textContent = "Copied!"
+    token.type = "password";
+  }
+  copy(token);
+  const copyButton = document.getElementById("copy-token");
+  copyButton.textContent = "Copied!";
 });
+
+document
+  .getElementById("delete-account")
+  .addEventListener("mouseenter", function () {
+    const randX = Math.floor(Math.random() * (window.innerWidth - 100));
+    const randY = Math.floor(Math.random() * (window.innerHeight - 100));
+    console.log([randX, randY]);
+    document
+      .getElementById("delete-account")
+      .stop()
+      .animate({ left: `${randX}px`, top: `${randY}px` });
+  });
