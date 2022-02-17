926b73118a993cb318d10074def55bbb645b93b5
Robert Kaye <rob@musicbrainz.org>
2022-02-17 11:13:24 +0100

    Create instant playlist and play release endpoints (#1864)
    * Adding an instant player endpoint for instant playlists
    
    * Create initial ReleasePage component
    
    * Basic playlist creation is in place
    
    * Create template for release page
    
    * Load playlist metadata
    
    * Fix props and set correct template
    
    * Take out playlist = to props for consistency
    
    * Put the playlist back, cuz I stoopid
    
    * Correction of release page template path
    
    * Make ReleasePage component more flexible
    
    We don't currently have a lot of the info we want to show, so let's hide it or make it conditional.
    
    * Add new /playlist player endpoint
    
    * Correction of release-page template path
    
    * Implement recording fetching
    
    * Rename ReleasePage to PlayerPage
    
    And make it more flexible to accept ephemeral playlists as well as releases (for now that's all, but extendable in the future)
    
    * Accept empty playlist but display placeholder message
    
    * Finish implementing the release endpoint.
    
    * Add a link to the source album
    
    * Add simple view tests
    
    * Undo docker-compose change. sigh.
    
    * peppelicous
    
    * Make endpoint URL end in /
    
    * Fix path to MissingMBData in webpack.config.js and fix tests
    
    * Fix PlayerPage.tsx
    
    * Fix the track mbid vs recording mbid issue
    
    * Clean up imports
    
    * Update BU version
    
    Co-authored-by: Monkey Do <contact@monkeydo.digital>
    Co-authored-by: akshaaatt <tiwariakshat03@gmail.com>

---
+++ listenbrainz/webserver/views/test/test_player.py
@@ -0,0 +1,19 @@
+from flask import url_for
+
+from listenbrainz.db.testing import DatabaseTestCase
+from listenbrainz.webserver.testing import ServerTestCase
+
+
+class PlayerViewsTestCase(ServerTestCase, DatabaseTestCase):
+
+    def setUp(self):
+        ServerTestCase.setUp(self)
+        DatabaseTestCase.setUp(self)
+
+    def test_player_get_instant(self):
+        resp = self.client.get(url_for('player.load_instant', recording_mbids="97e69767-5d34-4c97-b36a-f3b2b1ef9dae"))
+        self.assert200(resp)
+
+    def test_player_release(self):
+        resp = self.client.get(url_for('player.load_release', release_mbid="9db51cd6-38f6-3b42-8ad5-559963d68f35"))
+        self.assert200(resp)
