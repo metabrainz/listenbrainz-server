2d6eb3b6b86074c2d133606d06dd0d03528d4d67
Kartik Ohri <kartikohri13@gmail.com>
2022-02-18 16:39:46 +0530

    Fix update script

--- admin/timescale/updates/2022-02-02-add-listen-table-delete-column.sql
+++ admin/timescale/updates/2022-02-02-add-listen-table-delete-column.sql
@@ -1,3 +1,8 @@
 BEGIN;
-ALTER TABLE listen ADD COLUMN delete BOOLEAN;
+CREATE TABLE listen_delete_metadata (
+    user_id             INTEGER                     NOT NULL,
+    listened_at         BIGINT                      NOT NULL,
+    recording_msid      UUID                        NOT NULL,
+    created             TIMESTAMP WITH TIME ZONE    NOT NULL DEFAULT NOW()
+);
 COMMIT;
