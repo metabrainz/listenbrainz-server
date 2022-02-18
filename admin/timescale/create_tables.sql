ecccdef5d6ba392afd7e7978479a7809f6d19aa5
Kartik Ohri <kartikohri13@gmail.com>
2022-02-18 21:55:44 +0530

    Update integration tests

--- admin/timescale/create_tables.sql
+++ admin/timescale/create_tables.sql
@@ -13,7 +13,7 @@ CREATE TABLE listen_delete_metadata (
     id                  SERIAL                      NOT NULL,
     user_id             INTEGER                     NOT NULL,
     listened_at         BIGINT                      NOT NULL,
-    recording_msid      UUID                        NOT NULL,
+    recording_msid      UUID                        NOT NULL
 );
 
 CREATE TABLE listen_user_metadata (
