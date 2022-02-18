c95a765957a0a5a721a540da9a8f820f6391ccf9
Kartik Ohri <kartikohri13@gmail.com>
2022-02-18 16:56:59 +0530

    Use id column instead of created

--- admin/timescale/create_tables.sql
+++ admin/timescale/create_tables.sql
@@ -10,10 +10,10 @@ CREATE TABLE listen (
 );
 
 CREATE TABLE listen_delete_metadata (
+    id                  SERIAL                      NOT NULL,
     user_id             INTEGER                     NOT NULL,
     listened_at         BIGINT                      NOT NULL,
     recording_msid      UUID                        NOT NULL,
-    created             TIMESTAMP WITH TIME ZONE    NOT NULL DEFAULT NOW()
 );
 
 CREATE TABLE listen_user_metadata (
