3e614b98b75fbc1beed4c9d6b655504fe4957653
Monkey Do <contact@monkeydo.digital>
2022-02-21 16:03:36 +0100

    Remove unused "direction" Brainzplayer prop

--- listenbrainz/webserver/static/js/src/user/UserFeedback.tsx
+++ listenbrainz/webserver/static/js/src/user/UserFeedback.tsx
@@ -545,7 +545,6 @@ export default class UserFeedback extends React.Component<
           style={{ position: "-webkit-sticky", position: "sticky", top: 20 }}
         >
           <BrainzPlayer
-            direction={direction}
             listens={listensFromFeedback}
             newAlert={newAlert}
             listenBrainzAPIBaseURI={APIService.APIBaseURI}
