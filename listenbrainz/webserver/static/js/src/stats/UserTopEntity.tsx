723469b60b77776f52020c569c8bbd0521e8ee06
Kartik Ohri <kartikohri13@gmail.com>
2022-03-03 23:09:13 +0530

    Update stats acc. to feedback - 2

--- listenbrainz/webserver/static/js/src/stats/UserTopEntity.tsx
+++ listenbrainz/webserver/static/js/src/stats/UserTopEntity.tsx
@@ -71,13 +71,13 @@ export default class UserTopEntity extends React.Component<
   getData = async (): Promise<UserEntityResponse> => {
     const { entity, range, user } = this.props;
     try {
-      let data;
-      if (user) {
-        data = this.APIService.getUserEntity(user.name, entity, range, 0, 10);
-      } else {
-        data = this.APIService.getSiteWideEntity(entity, range, 0, 10);
-      }
-      return await data;
+      return await this.APIService.getUserEntity(
+        user ? user.name : undefined,
+        entity,
+        range,
+        0,
+        10
+      );
     } catch (error) {
       if (error.response && error.response.status === 204) {
         this.setState({
