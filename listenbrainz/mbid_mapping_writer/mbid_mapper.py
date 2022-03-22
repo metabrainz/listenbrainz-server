74ec79a362e00942119bf72360aa8c8c77b08325
Robert Kaye <rob@musicbrainz.org>
2022-03-22 20:03:56 +0100

    Catch the empty query message and return no hits

--- listenbrainz/mbid_mapping_writer/mbid_mapper.py
+++ listenbrainz/mbid_mapping_writer/mbid_mapper.py
@@ -183,7 +183,7 @@ class MBIDMapper():
 
             query = " ".join(cleaned_query)
 
-        if not query:
+        if query is None or query == "":
             return None
 
         search_parameters = {
@@ -195,12 +195,13 @@ class MBIDMapper():
 
         while True:
             try:
-                hits = self.client.collections[COLLECTION_NAME].documents.search(
-                    search_parameters)
+                hits = self.client.collections[COLLECTION_NAME].documents.search(search_parameters)
                 break
             except requests.exceptions.ReadTimeout:
                 print("Got socket timeout, sleeping 5 seconds, trying again.")
                 sleep(5)
+            except typesense.exceptions.RequestMalformed:
+                return None
 
         if len(hits["hits"]) == 0:
             return None
