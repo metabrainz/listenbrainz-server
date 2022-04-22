8bce519a4673dd7251b63c4a13ee596b62d62c92
Robert Kaye <rob@musicbrainz.org>
2022-04-22 19:50:17 +0200

    Add support for running troi after recommendations are run. Rough start, but gets things going. Tested? No. I have no idea how to rest this.

---
+++ listenbrainz/spark/troi_bot.py
@@ -0,0 +1,22 @@
+""" This function contains code to run the various troi-bot functions after
+    recommendations have been generated.
+"""
+from troi.core import generate_playlist
+import config
+
+USER_TO_PROCESS = ["mr_monkey", "rob", "akshaaatt", "Damselfish", "lucifer", "alastairp", "CatCat", "atj"]
+
+def run_post_recommendation_troi_bot(user):
+    """
+        Top level function called after spark CF recommendations have been completed.
+    """
+
+    if user in USERS_TO_PROCESS:
+        make_playlist_from_recommendations(user)
+        # Add others here
+
+def make_playlist_from_recommendations(user):
+
+    token = config.WHITELISTED_AUTH_TOKENS[0]
+    for type in ["top", "similar"]:
+        generate_playlist("recs-to-playlist", args=[user, type], upload=True, token=token, created_for=user)
