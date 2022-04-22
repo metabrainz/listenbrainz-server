008fac99c9a02b1926ff534d785b64ed7b2888c7
Chinmay Kunkikar <chinmay.kunkikar@gmail.com>
2022-04-22 16:58:48 +0530

    LB-1086: Add tests and snapshots for rendering user's name
    Two tests are:
    1. ListenCountCard renders user's name instead of 'You' when
       visiting another user's page.
    2. ListenCountCard renders 'You' when on current user's page.
    
    Essentially, ListenCountCard should render user's name by default
    with the exception of rendering "You" when a user is logged in
    and is visiting their own Listens page.

--- listenbrainz/webserver/static/js/tests/listens/ListenCountCard.test.tsx
+++ listenbrainz/webserver/static/js/tests/listens/ListenCountCard.test.tsx
@@ -1,15 +1,50 @@
 import * as React from "react";
 import { mount } from "enzyme";
 
+import GlobalAppContext, {
+  GlobalAppContextT,
+} from "../../src/utils/GlobalAppContext";
 import ListenCountCard from "../../src/listens/ListenCountCard";
+import APIService from "../../src/utils/APIService";
+
+const user = {
+  id: 1,
+  name: "track_listener",
+};
+
+const loggedInUser = {
+  id: 2,
+  name: "iliekcomputers",
+};
+
+const globalContext: GlobalAppContextT = {
+  APIService: new APIService("foo"),
+  currentUser: loggedInUser,
+};
 
 describe("ListenCountCard", () => {
   it("renders correctly when listen count is not zero", () => {
-    const wrapper = mount(<ListenCountCard listenCount={100} />);
+    const wrapper = mount(<ListenCountCard user={user} listenCount={100} />);
     expect(wrapper).toMatchSnapshot();
   });
   it("renders correctly when listen count is zero or undefined", () => {
-    const wrapper = mount(<ListenCountCard />);
+    const wrapper = mount(<ListenCountCard user={user} />);
+    expect(wrapper).toMatchSnapshot();
+  });
+  it("renders user's name instead of 'You' when visiting another user's page", () => {
+    const wrapper = mount(
+      <GlobalAppContext.Provider value={globalContext}>
+        <ListenCountCard user={user} listenCount={100} />
+      </GlobalAppContext.Provider>
+    );
+    expect(wrapper).toMatchSnapshot();
+  });
+  it("renders 'You' when on current user's page", () => {
+    const wrapper = mount(
+      <GlobalAppContext.Provider value={globalContext}>
+        <ListenCountCard user={loggedInUser} listenCount={100} />
+      </GlobalAppContext.Provider>
+    );
     expect(wrapper).toMatchSnapshot();
   });
 });
