002edc7d6f643cfd5e57e7582a9268136d2491a4
Monkey Do <contact@monkeydo.digital>
2022-04-26 12:28:28 +0200

    Update playing_now_metadata_viewer route
    add required decorators and load playing_now  data

--- listenbrainz/webserver/views/metadata_viewer.py
+++ listenbrainz/webserver/views/metadata_viewer.py
@@ -1,14 +1,33 @@
-from flask import Blueprint, render_template, request
+from flask import Blueprint, render_template, current_app
 from flask_login import current_user, login_required
-from werkzeug.exceptions import BadRequest, ServiceUnavailable, NotFound
+from listenbrainz import webserver
+from listenbrainz.webserver.decorators import web_listenstore_needed
 import ujson
 
 metadata_viewer_bp = Blueprint("metadata_viewer", __name__)
 
+
 @metadata_viewer_bp.route("/", methods=["GET"])
+@web_listenstore_needed
+@login_required
 def playing_now_metadata_viewer():
-	
+    """ Show a page with details of the currently playing listen """
+
+    # Which database to use to show playing_now stream.
+    playing_now_conn = webserver.redis_connection._redis
+    
+    # Get the now_playing listen for this user
+    playing_now = playing_now_conn.get_playing_now(current_user.id)
+        
+    # TODO: Load initial recording metadata for playing_now listen
+    # and add to props as 'metadata'
+    
+    props = {
+        "playing_now": playing_now,
+        "web_sockets_server_url": current_app.config['WEBSOCKETS_SERVER_URL'],
+    }
+
     return render_template(
         "player/metadata-viewer.html",
-        # props=ujson.dumps({"playing_now": })
+        props=ujson.dumps(props)
     )
