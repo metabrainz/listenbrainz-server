96349590fdc49de7c80f39717aedca9a33152387
Robert Kaye <rob@musicbrainz.org>
2022-04-27 17:55:44 +0200

    Finish debugging the notification

--- listenbrainz/spark/troi_bot.py
+++ listenbrainz/spark/troi_bot.py
@@ -1,19 +1,19 @@
-""" This function contains code to run the various troi-bot functions after
+""" This module contains code to run the various troi-bot functions after
     recommendations have been generated.
 """
+from listenbrainz.db.user import get_by_mb_id
 from listenbrainz.db.user_relationship import get_followers_of_user
 from listenbrainz.db.user_timeline_event import create_user_timeline_event
 from data.model.user_timeline_event import (
     UserTimelineEvent,
     UserTimelineEventType,
-    UserTimelineEventMetadata,
+    NotificationMetadata
 )
 from flask import current_app
 from troi.core import generate_playlist
 
 TROI_BOT_USER_ID = 12939
-#USERS_TO_PROCESS = ["mr_monkey", "rob", "akshaaatt", "Damselfish", "lucifer", "alastairp", "CatCat", "atj"]
-USERS_TO_PROCESS = ["rob"]
+USERS_TO_PROCESS = ["mr_monkey", "rob", "akshaaatt", "Damselfish", "lucifer", "alastairp", "CatCat", "atj"]
 
 
 def run_post_recommendation_troi_bot():
@@ -22,8 +22,8 @@ def run_post_recommendation_troi_bot():
     """
 
     # Save playlists for just a handful of people
-#    for user in USERS_TO_PROCESS:
-#        make_playlist_from_recommendations(user)
+    for user in USERS_TO_PROCESS:
+        make_playlist_from_recommendations(user)
 
     # Now generate daily jams (and other in the future) for users who follow troi bot
     users = get_followers_of_user(TROI_BOT_USER_ID)
@@ -47,16 +47,19 @@ def run_daily_jams(user):
     """
     token = current_app.config["WHITELISTED_AUTH_TOKENS"][0]
     try:
-        url = generate_playlist("daily-jams", args=[user["musicbrainz_id"], "top"], upload=True, token=token, created_for=user)
+        url = generate_playlist("daily-jams", args=[user, "top"], upload=True, token=token, created_for=user)
     except RuntimeError as err:
-        current_app.logger.error("Cannot create daily-jams for user %s. (%s)" % (user["musicbrainz_id"], str(err)))
+        current_app.logger.error("Cannot create daily-jams for user %s. (%s)" % (user, str(err)))
         return
     enter_timeline_notification(user, """Your daily-jams playlist has been updated. <a href="%s">Give it a listen!</a>.""" % url)
 
 
-def enter_timeline_notification(user, message):
+def enter_timeline_notification(username, message):
     """
        Helper function for createing a timeline notification for troi-bot
     """
 
-    create_user_timeline_event(user["id"], 'notification', NotificationMetadata(creator="troi-bot", message=message))
+    user = get_by_mb_id(username)
+    create_user_timeline_event(user["id"],
+       UserTimelineEventType.NOTIFICATION,
+       NotificationMetadata(creator="troi-bot", message=message))
