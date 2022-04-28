2204b6951e4ec71747d593806600f8add82f8f5b
Kartik Ohri <kartikohri13@gmail.com>
2022-04-28 10:30:23 +0530

    try fixing test - 3

--- listenbrainz_spark/missing_mb_data/tests/test_missing_mb_data.py
+++ listenbrainz_spark/missing_mb_data/tests/test_missing_mb_data.py
@@ -10,7 +10,7 @@ class MissingMBDataTestCase(SparkNewTestCase):
 
     def test_get_data_missing_from_musicbrainz(self):
         utils.upload_to_HDFS(LISTENBRAINZ_NEW_DATA_DIRECTORY, self.path_to_data_file('rec_listens.parquet'))
-        print(utils.hdfs_walk("/"))
+        print(list(utils.hdfs_walk("/")))
         # use a very long day range so that listens are used
         messages = missing_mb_data.main(10000)
         with open(self.path_to_data_file('missing_musicbrainz_data.json')) as f:
