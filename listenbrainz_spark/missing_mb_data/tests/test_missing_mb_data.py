031f248c36c5ee6479c9049ece16fbf913ca878b
Kartik Ohri <kartikohri13@gmail.com>
2022-04-28 09:55:49 +0530

    try fixing test

--- listenbrainz_spark/missing_mb_data/tests/test_missing_mb_data.py
+++ listenbrainz_spark/missing_mb_data/tests/test_missing_mb_data.py
@@ -1,14 +1,16 @@
 import json
 import os
 
+from listenbrainz_spark import utils
 from listenbrainz_spark.missing_mb_data import missing_mb_data
-from listenbrainz_spark.recommendations.recording.tests import RecommendationsTestCase
-from listenbrainz_spark.tests import TEST_DATA_PATH
+from listenbrainz_spark.path import LISTENBRAINZ_NEW_DATA_DIRECTORY
+from listenbrainz_spark.tests import TEST_DATA_PATH, SparkNewTestCase
 
 
-class MissingMBDataTestCase(RecommendationsTestCase):
+class MissingMBDataTestCase(SparkNewTestCase):
 
     def test_get_data_missing_from_musicbrainz(self):
+        utils.upload_to_HDFS(LISTENBRAINZ_NEW_DATA_DIRECTORY, os.path.join(TEST_DATA_PATH, 'rec_listens.parquet'))
         # use a very long day range so that listens are used
         messages = missing_mb_data.main(10000)
         with open(os.path.join(TEST_DATA_PATH, 'missing_musicbrainz_data.json')) as f:
