043a39da40390558919d62d2f35e8648cf3fc62a
Kartik Ohri <kartikohri13@gmail.com>
2022-04-29 21:35:36 +0530

    Do not scale ratings for recommendations for now (#1972)
    * Do not scale ratings for recommendations for now
    
    Before working out how to scale ratings, let's analyse how the unscaled
    ratings look like. Therefore, deleting the code to scale ratings output
    by recommender.

--- listenbrainz_spark/recommendations/recording/recommend.py
+++ listenbrainz_spark/recommendations/recording/recommend.py
@@ -5,7 +5,7 @@ The best_model saved in HDFS is loaded with the help of model_id which is fetche
 `spark_user_id` and `recording_id` are fetched from top_artist_candidate_set_df and are given as input to the
 recommender. An RDD of `user`, `product` and `rating` is returned from the recommender which is later converted to
 a dataframe by filtering top X (an int supplied as an argument to the script) recommendations for all users sorted on rating
-and fields renamed as `spark_user_id`, `recording_id` and `rating`. The ratings are scaled so that they lie between 0 and 1.
+and fields renamed as `spark_user_id`, `recording_id` and `rating`.
 This dataframe is joined with recordings_df on recording_id to get the recording mbids which are then sent over the queue.
 
 The same process is done for similar artist candidate set.
@@ -149,41 +149,6 @@ def generate_recommendations(candidate_set, params: RecommendationParams, limit)
     return recommendation_df
 
 
-def get_scale_rating_udf(rating):
-    """ Get user defined function (udf) to scale ratings so that they fall in the
-        range: 0.0 -> 1.0.
-
-        Args:
-            rating (float): score given to recordings by CF.
-
-        Returns:
-            rating udf.
-    """
-    scaled_rating = (rating / 2.0) + 0.5
-
-    return round(min(max(scaled_rating, -1.0), 1.0), 3)
-
-
-def scale_rating(df: pyspark.sql.DataFrame):
-    """ Scale the ratings column of dataframe so that they fall in the
-        range: 0.0 -> 1.0.
-
-        Args:
-            df: Dataframe to scale.
-
-        Returns:
-            df: Dataframe with scaled rating.
-    """
-    scaling_udf = func.udf(get_scale_rating_udf, DoubleType())
-
-    df = df.withColumn("scaled_rating", scaling_udf(df.rating)) \
-           .select(col('recording_id'),
-                   col('spark_user_id'),
-                   col('scaled_rating').alias('rating'))
-
-    return df
-
-
 def get_candidate_set_rdd_for_user(candidate_set_df, users):
     """ Get candidate set RDD for a given user.
 
@@ -228,33 +193,6 @@ def get_user_name_and_user_id(params: RecommendationParams, users):
         raise EmptyDataframeExcpetion('No active users found!')
 
     return users_df
-
-
-def check_for_ratings_beyond_range(top_artist_rec_df, similar_artist_rec_df):
-    """ Check if rating in top_artist_rec_df and similar_artist_rec_df does not belong to [-1, 1].
-
-        Args:
-            top_artist_rec_df (dataframe): Top artist recommendations for all users.
-            similar_artist_rec_df (dataframe): Similar artist recommendations for all users.
-
-        Returns:
-            a tuple of booleans (max out of range, min out of range)
-    """
-    max_rating = top_artist_rec_df.select(func.max('rating').alias('rating')).take(1)[0].rating
-
-    max_rating = max(similar_artist_rec_df.select(func.max('rating').alias('rating')).take(1)[0].rating, max_rating)
-
-    min_rating = top_artist_rec_df.select(func.min('rating').alias('rating')).take(1)[0].rating
-
-    min_rating = min(similar_artist_rec_df.select(func.min('rating').alias('rating')).take(1)[0].rating, min_rating)
-
-    if max_rating > 1.0:
-        logger.info('Some ratings are greater than 1 \nMax rating: {}'.format(max_rating))
-
-    if min_rating < -1.0:
-        logger.info('Some ratings are less than -1 \nMin rating: {}'.format(min_rating))
-
-    return max_rating > 1.0, min_rating < -1.0
 
 
 def create_messages(params, top_artist_rec_mbid_df, similar_artist_rec_mbid_df, active_user_count,
@@ -445,15 +383,8 @@ def main(recommendation_top_artist_limit=None, recommendation_similar_artist_lim
     logger.info('Took {:.2f}sec to get top artist and similar artist user count'.format(time.monotonic() - ts))
 
     ts = time.monotonic()
-    check_for_ratings_beyond_range(top_artist_rec_df, similar_artist_rec_df)
-
-    top_artist_rec_scaled_df = scale_rating(top_artist_rec_df)
-    similar_artist_rec_scaled_df = scale_rating(similar_artist_rec_df)
-    logger.info('Took {:.2f}sec to scale the ratings'.format(time.monotonic() - ts))
-
-    ts = time.monotonic()
-    top_artist_rec_mbid_df = get_recording_mbids(params, top_artist_rec_scaled_df, users_df)
-    similar_artist_rec_mbid_df = get_recording_mbids(params, similar_artist_rec_scaled_df, users_df)
+    top_artist_rec_mbid_df = get_recording_mbids(params, top_artist_rec_df, users_df)
+    similar_artist_rec_mbid_df = get_recording_mbids(params, similar_artist_rec_df, users_df)
     logger.info('Took {:.2f}sec to get mbids corresponding to recording ids'.format(time.monotonic() - ts))
 
     # persisted data must be cleared from memory after usage to avoid OOM
