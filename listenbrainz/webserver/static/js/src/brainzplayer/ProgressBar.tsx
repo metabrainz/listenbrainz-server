725c69b678bee586a69fbd0c43b9d11101c5b642
Chinmay Kunkikar <chinmay.kunkikar@gmail.com>
2022-05-20 00:12:31 +0530

    LB-1084: Show a tooltip on progressbar and other changes  - Use `react-tooltip` component to show a tooltip of time    while hovering over the progressbar.  - Replace cursor `crosshair` with `pointer`.  - Add `box-shadow` to player to give it an elevated look    to make it stand out from the rest of the page.  - Increase progressbar default height from 3px to 4px and    hover height from 6px to 8px to make it easier to click.    (Based on discussion from https://tickets.metabrainz.org/projects/LB/issues/LB-1084)
    Other changes:
     - Declare magic numbers and magic strings as constants
       at the start of the file to reduce confusion.
     - Modify `progressClickHandler()` -> `mouseHandler()` to
       handle multiple mouse events (onClick and onMouseMove).

--- listenbrainz/webserver/static/js/src/brainzplayer/ProgressBar.tsx
+++ listenbrainz/webserver/static/js/src/brainzplayer/ProgressBar.tsx
@@ -1,5 +1,7 @@
 import { isNaN } from "lodash";
 import * as React from "react";
+import ReactTooltip from "react-tooltip";
+import { msToTime } from "../playlists/utils";
 
 type ProgressBarProps = {
   progressMs: number;
@@ -8,17 +10,26 @@ type ProgressBarProps = {
 };
 
 // How many milliseconds to navigate to with keyboard left/right arrows
-const keyboardStepMS: number = 5000;
-const keyboardBigStepMS: number = 10000;
+const KEYBOARD_BIG_STEP_MS: number = 5000;
+const KEYBOARD_STEP_MS: number = 10000;
+
+const EVENT_KEY_ARROWLEFT: string = "ArrowLeft";
+const EVENT_KEY_ARROWRIGHT: string = "ArrowRight";
+const EVENT_TYPE_CLICK: string = "click";
+const EVENT_TYPE_MOUSEMOVE: string = "mousemove";
+
+const TOOLTIP_INITIAL_CONTENT: string = "0:00";
+const TOOLTIP_TOP_OFFSET: number = 102;
 
 const ProgressBar = (props: ProgressBarProps) => {
   const { durationMs, progressMs, seekToPositionMs } = props;
+  const [tipContent, setTipContent] = React.useState(TOOLTIP_INITIAL_CONTENT);
   const progressPercentage = Math.round(
     Number((progressMs * 100) / durationMs)
   );
   const hideProgressBar = isNaN(progressPercentage) || progressPercentage <= 0;
 
-  const progressClickHandler = (
+  const mouseEventHandler = (
     event: React.MouseEvent<HTMLInputElement>
   ): void => {
     const progressBarBoundingRect = event.currentTarget.getBoundingClientRect();
@@ -28,34 +39,38 @@ const ProgressBar = (props: ProgressBarProps) => {
     const relativeClickXPos = absoluteClickXPos - musicPlayerXOffset;
     const percentPos = relativeClickXPos / progressBarWidth;
     const positionMs = Math.round(durationMs * percentPos);
-    seekToPositionMs(positionMs);
+    const positionTime = msToTime(positionMs);
+
+    if (event.type === EVENT_TYPE_MOUSEMOVE) {
+      setTipContent(positionTime);
+    } else if (event.type === EVENT_TYPE_CLICK) {
+      seekToPositionMs(positionMs);
+    }
   };
 
   const onKeyPressHandler = (
     event: React.KeyboardEvent<HTMLInputElement>
   ): void => {
-    if (
-      document.activeElement?.localName === "input" ||
-      document.activeElement?.localName === "textarea"
-    ) {
+    const activeElement = document.activeElement?.localName;
+    if (activeElement === "input" || activeElement === "textarea") {
       // If user has a text input/textarea in focus, ignore key navigation
       return;
     }
-    if (event.key === "ArrowLeft") {
+    if (event.key === EVENT_KEY_ARROWLEFT) {
       let oneStepEarlier;
       if (event.shiftKey) {
-        oneStepEarlier = progressMs - keyboardStepMS;
+        oneStepEarlier = progressMs - KEYBOARD_BIG_STEP_MS;
       } else {
-        oneStepEarlier = progressMs - keyboardBigStepMS;
+        oneStepEarlier = progressMs - KEYBOARD_STEP_MS;
       }
       seekToPositionMs(oneStepEarlier > 0 ? oneStepEarlier : 0);
     }
-    if (event.key === "ArrowRight") {
+    if (event.key === EVENT_KEY_ARROWRIGHT) {
       let oneStepLater;
       if (event.shiftKey) {
-        oneStepLater = progressMs + keyboardBigStepMS;
+        oneStepLater = progressMs + KEYBOARD_STEP_MS;
       } else {
-        oneStepLater = progressMs + keyboardStepMS;
+        oneStepLater = progressMs + KEYBOARD_BIG_STEP_MS;
       }
       if (oneStepLater <= durationMs - 500) {
         seekToPositionMs(oneStepLater);
@@ -66,19 +81,31 @@ const ProgressBar = (props: ProgressBarProps) => {
   return (
     <div
       className={`progress${hideProgressBar ? " hidden" : ""}`}
-      onClick={progressClickHandler}
+      onClick={mouseEventHandler}
+      onMouseMove={mouseEventHandler}
       onKeyDown={onKeyPressHandler}
-      aria-label="Audio Progress Control"
+      aria-label="Audio progress control"
       role="progressbar"
       aria-valuemin={0}
       aria-valuemax={100}
       aria-valuenow={progressPercentage}
       tabIndex={0}
+      data-tip={tipContent}
     >
       <div
         className="progress-bar"
         style={{
           width: `${progressPercentage}%`,
+        }}
+      />
+      <ReactTooltip
+        className="progress-tooltip"
+        getContent={() => tipContent}
+        globalEventOff="click"
+        overridePosition={({ left, top }) => {
+          // eslint-disable-next-line no-param-reassign
+          top = document.documentElement.clientHeight - TOOLTIP_TOP_OFFSET;
+          return { left, top };
         }}
       />
     </div>
