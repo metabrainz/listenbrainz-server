226246f5318739ec23c1ec2331a1a970bdedce31
Kartik Ohri <kartikohri13@gmail.com>
2022-05-28 18:00:24 +0530

    Make options required

--- listenbrainz/spark/request_manage.py
+++ listenbrainz/spark/request_manage.py
@@ -347,10 +347,10 @@ def request_similar_users(max_num_users):
 
 
 @cli.command(name='request_similar_recordings')
-@click.option("--steps", type=int, help="The number of lookahead steps to use.")
-@click.option("--days", type=int, help="The number of days of listens to use.")
-@click.option("--session", type=int, help="The maximum duration in seconds between two listens to consider for similarity.")
-@click.option("--threshold", type=int, help="The minimum similarity to retain pair of recordings in similarity index.")
+@click.option("--steps", type=int, help="The number of lookahead steps to use.", required=True)
+@click.option("--days", type=int, help="The number of days of listens to use.", required=True)
+@click.option("--session", type=int, help="The maximum duration in seconds between two listens to consider for similarity.", required=True)
+@click.option("--threshold", type=int, help="The minimum similarity to retain pair of recordings in similarity index.", required=True)
 def request_similar_recordings(steps, days, session, threshold):
     """ Send the cluster a request to generate similar recordings index. """
     send_request_to_spark_cluster('similarity.recording', steps=steps, days=days, session=session, threshold=threshold)
