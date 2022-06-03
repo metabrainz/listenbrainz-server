ee6640b4d2043b2cabf63be03ceaf41d6b471e1b
Kartik Ohri <kartikohri13@gmail.com>
2022-06-03 22:48:44 +0530

    fix tests

--- listenbrainz/db/tests/test_msid_mbid_mapping.py
+++ listenbrainz/db/tests/test_msid_mbid_mapping.py
@@ -14,10 +14,10 @@ class MappingTestCase(TimescaleTestCase):
         with self.assertRaisesRegex(ValueError, 'at least one of recording_msid or recording_mbid should be specified'):
             model = MsidMbidModel(recording_mbid=None, recording_msid=None)
 
-        with self.assertRaisesRegex(ValueError, "'recording_msid' must be a valid UUID."):
+        with self.assertRaisesRegex(ValueError, "(?s)recording_msid.*must be a valid UUID"):
             model = MsidMbidModel(recording_msid='', recording_mbid=str(uuid.uuid4()))
 
-        with self.assertRaises(ValueError, "'recording_mbid' must be a valid UUID."):
+        with self.assertRaises(ValueError, "(?s)recording_mbid.*must be a valid UUID"):
             model = MsidMbidModel(recording_msid=str(uuid.uuid4()), recording_mbid='')
 
         # test that 2 valid uuids doesn't raise error
