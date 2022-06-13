b182c845ef0e4a80e4536b2bce13c02777c3aa2c
Chinmay Kunkikar <chinmay.kunkikar@gmail.com>
2022-06-13 16:09:31 +0530

    UserSocialNetwork: Show helpful messages for empty states.
     - Move UserSocialNetwork modals to individual cards.
     - Conditionally show helpful messages on all modals if their state is empty.
     - Adjust and add appropriate styles.

--- listenbrainz/webserver/static/js/src/follow/SimilarUsersModal.tsx
+++ listenbrainz/webserver/static/js/src/follow/SimilarUsersModal.tsx
@@ -25,22 +25,33 @@ const SimilarUsersModal = (props: SimilarUsersModalProps) => {
 
   return (
     <>
-      <h3 className="text-center">
-        Users similar to {user.name === currentUser?.name ? "you" : user.name}
+      <h3 className="text-center" style={{ marginTop: "10px" }}>
+        Similar Users
       </h3>
-      <div className="similar-users-list">
-        {similarUsersList.map((listEntry: SimilarUser) => {
-          return (
-            <UserListModalEntry
-              mode="similar-users"
-              key={listEntry.name}
-              user={listEntry}
-              loggedInUserFollowsUser={loggedInUserFollowsUser(listEntry)}
-              updateFollowingList={updateFollowingList}
-            />
-          );
-        })}
-      </div>
+      {similarUsersList.length === 0 ? (
+        <>
+          <hr style={{ margin: "0px 2em", borderTop: "1px solid #eee" }} />
+          <div className="similar-users-empty text-center text-muted">
+            Users with similar music tastes to{" "}
+            {user.name === currentUser?.name ? "you" : user.name} will appear
+            here.
+          </div>
+        </>
+      ) : (
+        <div className="similar-users-list">
+          {similarUsersList.map((listEntry: SimilarUser) => {
+            return (
+              <UserListModalEntry
+                mode="similar-users"
+                key={listEntry.name}
+                user={listEntry}
+                loggedInUserFollowsUser={loggedInUserFollowsUser(listEntry)}
+                updateFollowingList={updateFollowingList}
+              />
+            );
+          })}
+        </div>
+      )}
     </>
   );
 };
