14fe289d94fbc434acb480d3b3725ae1b431b47e
Kartik Ohri <kartikohri13@gmail.com>
2022-06-24 11:33:47 +0530

    Rename release_radar to recent_releases

--- listenbrainz/webserver/__init__.py
+++ listenbrainz/webserver/__init__.py
@@ -317,8 +317,8 @@ def _register_blueprints(app):
     from listenbrainz.webserver.views.stats_api import stats_api_bp
     app.register_blueprint(stats_api_bp, url_prefix=API_PREFIX+'/stats')
 
-    from listenbrainz.webserver.views.release_radar import release_radar_bp
-    app.register_blueprint(release_radar_bp, url_prefix=API_PREFIX)
+    from listenbrainz.webserver.views.recent_releases import recent_releases_bp
+    app.register_blueprint(recent_releases_bp, url_prefix=API_PREFIX)
 
     from listenbrainz.webserver.views.status_api import status_api_bp
     app.register_blueprint(status_api_bp, url_prefix=API_PREFIX+'/status')
