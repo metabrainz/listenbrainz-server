e8a15ef2d6e37153b241e8094cb836257a12dafd
Chinmay Kunkikar <chinmay.kunkikar@gmail.com>
2022-07-06 00:09:20 +0530

    RecentReleases: Add and populate release cards  - Use fake data from https://github.com/metabrainz/listenbrainz-server/blob/701617a6eef70755b5fd52aed75854395ac871a0/listenbrainz_spark/release_radar/release.json  - Filter out duplicate releases using `lodash.uniqBy()` and handle an edge case.

--- listenbrainz/webserver/static/js/src/recent-releases/RecentReleases.tsx
+++ listenbrainz/webserver/static/js/src/recent-releases/RecentReleases.tsx
@@ -2,15 +2,57 @@ import * as React from "react";
 import * as ReactDOM from "react-dom";
 import * as Sentry from "@sentry/react";
 import { Integrations } from "@sentry/tracing";
+import { uniqBy } from "lodash";
 import { withAlertNotifications } from "../notifications/AlertNotificationsHOC";
 import APIServiceClass from "../utils/APIService";
 import GlobalAppContext, { GlobalAppContextT } from "../utils/GlobalAppContext";
 
 import { getPageProps } from "../utils/utils";
 import ErrorBoundary from "../utils/ErrorBoundary";
+import ReleaseCard from "./ReleaseCard";
+import * as fakeData from "./fakeData.json";
 
 export default function RecentReleases() {
-  return <h3>Recent Releases</h3>;
+  return (
+    <div className="releases-page">
+      <h3 id="releases-title">Recent and upcoming releases</h3>
+      <div className="release-cards-container">
+        <div className="release-cards-grid">
+          {// Deduplicate releases based on same release name by same artist name.
+          uniqBy(fakeData, (datum) => {
+            return (
+              /*
+               * toLowerCase() solves an edge case.
+               * Example:
+               * "release_name": "Waterslide, Diving Board, Ladder to the Sky"
+               * "release_name": "Waterslide, Diving Board, Ladder To The Sky"
+               * These releases will be considered unique.
+               */
+              datum.release_name.toLowerCase() +
+              datum.artist_credit_name.toLowerCase()
+            );
+          })
+            .slice(620, 650)
+            .map((release) => {
+              return (
+                <ReleaseCard
+                  key={release.release_mbid}
+                  releaseDate={release.date}
+                  releaseMBID={release.release_mbid}
+                  releaseName={release.release_name}
+                  releaseType={
+                    release.release_group_primary_type ||
+                    release.release_group_secondary_type
+                  }
+                  artistCreditName={release.artist_credit_name}
+                  artistMBIDs={release.artist_mbids}
+                />
+              );
+            })}
+        </div>
+      </div>
+    </div>
+  );
 }
 
 document.addEventListener("DOMContentLoaded", () => {
