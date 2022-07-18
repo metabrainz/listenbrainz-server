2209a261e61ad34c5ebb8915a93f5354b5498cdf
Kartik Ohri <kartikohri13@gmail.com>
2022-07-18 20:21:51 +0530

    update tests - 11

--- listenbrainz/db/tests/test_stats.py
+++ listenbrainz/db/tests/test_stats.py
@@ -42,30 +42,26 @@ class StatsDatabaseTestCase(DatabaseTestCase):
         db_stats.insert_stats_in_couchdb(database2, from_ts2, to_ts2, data[:1])
         return original, from_ts1, to_ts1, from_ts2, to_ts2
 
-    def _test_one_stat(self, entity, range_, data_file):
+    def _test_one_stat(self, entity, range_, data_file, model):
         original, from_ts1, to_ts1, from_ts2, to_ts2 = self.insert_stats(entity, range_, data_file)
 
-        received = db_stats.get_entity_stats(1, entity, range_)
-        received["user_id"] = int(received["_id"])
-        del received["_id"]
-        del received["_rev"]
+        received = db_stats.get_entity_stats(1, entity, range_, model).dict()
 
         expected = original[0] | {
             "from_ts": from_ts2,
             "to_ts": to_ts2,
             "last_updated": received["last_updated"],
+            "stats_range": range_
         }
         self.assertEqual(received, expected)
 
-        received = db_stats.get_entity_stats(2, entity, range_)
-        received["user_id"] = int(received["_id"])
-        del received["_id"]
-        del received["_rev"]
+        received = db_stats.get_entity_stats(2, entity, range_, model).dict()
 
         expected = original[1] | {
             "from_ts": from_ts1,
             "to_ts": to_ts1,
             "last_updated": received["last_updated"],
+            "stats_range": range_
         }
         self.assertEqual(received, expected)
 
@@ -82,85 +78,16 @@ class StatsDatabaseTestCase(DatabaseTestCase):
                 with self.subTest(f"{range_} daily_activity user stats", range_=range_):
                     self._test_one_stat("daily_activity", range_, f'user_daily_activity_db_data_for_api_test_{range_}.json')
 
-    # def test_insert_user_listening_activity(self):
-    #     """ Test if listening activity stats are inserted correctly """
-    #     with open(self.path_to_data_file('user_listening_activity_db.json')) as f:
-    #         listening_activity_data = json.load(f)
-    #     db_stats.insert_user_jsonb_data(
-    #         user_id=self.user['id'], stats_type='listening_activity',
-    #         stats=StatRange[ListeningActivityRecord](**listening_activity_data)
-    #     )
-    #
-    # def test_insert_user_daily_activity(self):
-    #     """ Test if daily activity stats are inserted correctly """
-    #     with open(self.path_to_data_file('user_daily_activity_db.json')) as f:
-    #         daily_activity_data = json.load(f)
-    #     db_stats.insert_user_jsonb_data(
-    #         user_id=self.user['id'], stats_type='daily_activity',
-    #         stats=StatRange[DailyActivityRecord](**daily_activity_data)
-    #     )
-    #
-    #     result = db_stats.get_user_daily_activity(user_id=self.user['id'], stats_range='all_time')
-    #     self.assertDictEqual(result.dict(exclude={'user_id', 'last_updated', 'count'}), daily_activity_data)
-    #
-    # def test_insert_user_artist_map(self):
-    #     """ Test if daily activity stats are inserted correctly """
-    #     with open(self.path_to_data_file('user_artist_map_db.json')) as f:
-    #         artist_map_data = json.load(f)
-    #     db_stats.insert_user_jsonb_data(
-    #         user_id=self.user['id'], stats_type='artist_map',
-    #         stats=StatRange[UserArtistMapRecord](**artist_map_data)
-    #     )
-    #
-    #     result = db_stats.get_user_artist_map(user_id=self.user['id'], stats_range='all_time')
-    #     self.assertDictEqual(result.dict(exclude={'user_id', 'last_updated', 'count'}), artist_map_data)
-    #
-    #
-    # def test_get_timestamp_for_last_user_stats_update(self):
-    #     ts = datetime.now(timezone.utc)
-    #     self.insert_test_data()
-    #     received_ts = db_stats.get_timestamp_for_last_user_stats_update()
-    #     self.assertGreaterEqual(received_ts, ts)
-    #
-    # def test_get_user_artists(self):
-    #     data_inserted = self.insert_test_data()
-    #     result = db_stats.get_user_stats(self.user['id'], 'all_time', 'artists')
-    #     self.assertDictEqual(result.dict(exclude={'user_id', 'last_updated'}), data_inserted['user_artists'])
-    #
-    # def test_get_user_releases(self):
-    #     data_inserted = self.insert_test_data()
-    #     result = db_stats.get_user_stats(self.user['id'], 'all_time', 'releases')
-    #     self.assertDictEqual(result.dict(exclude={'user_id', 'last_updated'}), data_inserted['user_releases'])
-    #
-    # def test_get_user_recordings(self):
-    #     data_inserted = self.insert_test_data()
-    #     result = db_stats.get_user_stats(self.user['id'], 'all_time', 'recordings')
-    #     self.assertDictEqual(result.dict(exclude={'user_id', 'last_updated'}), data_inserted['user_recordings'])
-    #
-    # def test_get_user_listening_activity(self):
-    #     data_inserted = self.insert_test_data()
-    #     result = db_stats.get_user_listening_activity(self.user['id'], 'all_time')
-    #     self.assertDictEqual(result.dict(exclude={'user_id', 'last_updated', 'count'}), data_inserted['user_listening_activity'])
-    #
-    # def test_get_user_daily_activity(self):
-    #     data_inserted = self.insert_test_data()
-    #     result = db_stats.get_user_daily_activity(self.user['id'], 'all_time')
-    #     self.assertDictEqual(result.dict(exclude={'user_id', 'last_updated', 'count'}), data_inserted['user_daily_activity'])
-    #
-    # def test_get_user_artist_map(self):
-    #     data_inserted = self.insert_test_data()
-    #     result = db_stats.get_user_artist_map(self.user['id'], 'all_time')
-    #     self.assertDictEqual(result.dict(exclude={'user_id', 'last_updated', 'count'}), data_inserted['user_artist_map'])
-    #
-    # def test_get_sitewide_artists(self):
+                with self.subTest(f"{range_} listening_activity user stats", range_=range_):
+                    self._test_one_stat("listening_activity", range_, f'user_listening_activity_db_data_for_api_test_{range_}.json')
+
+                with self.subTest(f"{range_} artist_map user stats", range_=range_):
+                    self._test_one_stat("listening_activity", range_, f'user_artist_map_db_data_for_api_test_{range_}.json')
+
+   # def test_get_sitewide_artists(self):
     #     data_inserted = self.insert_test_data()
     #     result = db_stats.get_sitewide_stats('all_time', 'artists')
     #     self.assertDictEqual(result.dict(exclude={'user_id', 'last_updated'}), data_inserted['sitewide_artists'])
-    #
-    # def test_valid_stats_exist(self):
-    #     self.assertFalse(db_stats.valid_stats_exist(self.user['id'], 7))
-    #     self.insert_test_data()
-    #     self.assertTrue(db_stats.valid_stats_exist(self.user['id'], 7))
     #
     # def test_delete_user_stats(self):
     #     self.assertFalse(db_stats.valid_stats_exist(self.user['id'], 7))
