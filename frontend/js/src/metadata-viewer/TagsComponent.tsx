2c92bf4ae197bdbb8fe810784f4c98813b794cb0
Monkey Do <contact@monkeydo.digital>
2022-07-18 18:15:54 +0200

    Listening now: Improve "add tag" component
    Actually enable users to write a new tag, and stub implementation of submission mechanism

--- frontend/js/src/metadata-viewer/TagsComponent.tsx
+++ frontend/js/src/metadata-viewer/TagsComponent.tsx
@@ -1,24 +1,74 @@
 import * as React from "react";
 import TagComponent from "./TagComponent";
 
-function AddTagComponent() {
+function AddTagComponent(props: {
+  entityType: "artist" | "release-group" | "recording";
+  entityMBID: string;
+  callback?: Function;
+}) {
   const [expanded, setExpanded] = React.useState(false);
+  const inputRef = React.useRef<HTMLInputElement>(null);
+  const { entityMBID, entityType, callback } = props;
+
+  const submitNewTag = React.useCallback(
+    async (event: React.FormEvent) => {
+      event.preventDefault();
+      const newTag = inputRef?.current?.value;
+      const requestURL = `https://musicbrainz.org/${entityType}/${entityMBID}/tags/upvote?tags=${newTag}&client=brainzplayer`;
+      try {
+        const request = await fetch(requestURL, {
+          method: "GET",
+          mode: "cors",
+          credentials: "include",
+          headers: {
+            Accept: "application/json",
+          },
+        });
+        if (request.ok && callback) {
+          callback(newTag);
+        }
+      } catch (err) {
+        console.error(err);
+      }
+    },
+    [entityType, entityMBID]
+  );
   return (
-    <div className="add-tag">
-      <button
-        className="btn btn-xs btn-outline"
-        type="button"
-        onClick={() => setExpanded(!expanded)}
-      >
-        + Add tag
-      </button>
-      {expanded && (
-        <form action="">
-          <input type="text" />
-          <button className="btn btn-xs btn-outline" type="submit">
-            OK
-          </button>
+    <div className={`add-tag ${expanded ? "expanded" : ""}`}>
+      {expanded ? (
+        <form className="input-group" onSubmit={submitNewTag}>
+          <input
+            ref={inputRef}
+            type="text"
+            minLength={1}
+            // eslint-disable-next-line jsx-a11y/no-autofocus
+            autoFocus
+            placeholder="Enter a new tag…"
+            className="form-control"
+          />
+          <div className="input-group-btn btn-group-sm">
+            <button className="btn btn-success" type="submit">
+              OK
+            </button>
+            <button
+              title="cancel"
+              className="btn btn-danger"
+              type="button"
+              onClick={() => setExpanded(false)}
+            >
+              x
+            </button>
+          </div>
         </form>
+      ) : (
+        <button
+          className="btn btn-xs btn-outline"
+          type="button"
+          onClick={entityMBID ? () => setExpanded(!expanded) : undefined}
+          disabled={!entityMBID}
+        >
+          + Add tag
+        </button>
       )}
     </div>
   );
@@ -48,7 +98,7 @@ export default function TagsComponent(props: {
           <span>Be the first to add a tag</span>
         )}
       </div>
-      <AddTagComponent />
+      <AddTagComponent entityType={entityType} entityMBID={entityMBID} />
     </div>
   );
 }
