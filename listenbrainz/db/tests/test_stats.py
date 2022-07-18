5434c6720c3ea6f2e19059542452548371d67da5
Kartik Ohri <kartikohri13@gmail.com>
2022-07-18 20:36:59 +0530

    update tests - 12

--- listenbrainz/db/tests/test_stats.py
+++ listenbrainz/db/tests/test_stats.py
@@ -73,16 +73,36 @@ class StatsDatabaseTestCase(DatabaseTestCase):
             for range_ in ranges:
                 for entity in entities:
                     with self.subTest(f"{range_} {entity} user stats", entity=entity, range_=range_):
-                        self._test_one_stat(entity, range_, f'user_top_{entity}_db_data_for_api_test_{range_}.json')
+                        self._test_one_stat(
+                            entity,
+                            range_,
+                            f'user_top_{entity}_db_data_for_api_test_{range_}.json',
+                            EntityRecord
+                        )
 
                 with self.subTest(f"{range_} daily_activity user stats", range_=range_):
-                    self._test_one_stat("daily_activity", range_, f'user_daily_activity_db_data_for_api_test_{range_}.json')
+                    self._test_one_stat(
+                        "daily_activity",
+                        range_,
+                        f'user_daily_activity_db_data_for_api_test_{range_}.json',
+                        DailyActivityRecord
+                    )
 
                 with self.subTest(f"{range_} listening_activity user stats", range_=range_):
-                    self._test_one_stat("listening_activity", range_, f'user_listening_activity_db_data_for_api_test_{range_}.json')
+                    self._test_one_stat(
+                        "listening_activity",
+                        range_,
+                        f'user_listening_activity_db_data_for_api_test_{range_}.json',
+                        ListeningActivityRecord
+                    )
 
                 with self.subTest(f"{range_} artist_map user stats", range_=range_):
-                    self._test_one_stat("listening_activity", range_, f'user_artist_map_db_data_for_api_test_{range_}.json')
+                    self._test_one_stat(
+                        "artist_map",
+                        range_,
+                        f'user_artist_map_db_data_for_api_test_{range_}.json',
+                        UserArtistMapRecord
+                    )
 
    # def test_get_sitewide_artists(self):
     #     data_inserted = self.insert_test_data()
