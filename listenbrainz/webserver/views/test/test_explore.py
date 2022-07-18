bbea958e513e3b5bd06bd123309dd197d5a85a95
Robert Kaye <rob@musicbrainz.org>
2022-07-18 12:52:31 +0200

    Add fresh releases backend (#2065)
    * Add upcoming releases
    
    * Add DB module
    
    * Finish first cut implementation of upcoming releases
    
    * Create explore blueprint.
    
    * Add new explore and explore_api blueprints
    
    * Add explore.py
    
    * Move around tests to matche the recent changes.
    
    * Add parameter support to API call for upcoming releases
    
    * pepperoni
    
    * Peppy
    
    * Add explore api test
    
    * Undo default network
    
    * Remove extra import
    
    * upcoming releases -> fresh releases
    
    * Check in missing tests
    
    * Remove redundant block
    
    * Address PR feedbackt

---
+++ listenbrainz/webserver/views/test/test_explore.py
@@ -0,0 +1,45 @@
+import datetime
+from unittest import mock
+from unittest.mock import patch
+
+from flask import url_for
+
+from listenbrainz.db.testing import DatabaseTestCase
+from listenbrainz.webserver.testing import ServerTestCase
+
+
+class ExploreViewsTestCase(ServerTestCase, DatabaseTestCase):
+
+    def setUp(self):
+        ServerTestCase.setUp(self)
+        DatabaseTestCase.setUp(self)
+
+    def test_hue_sound(self):
+        resp = self.client.get(url_for('explore.huesound', color="FF00FF"))
+        self.assert200(resp)
+
+    def test_hue_sound_redirect(self):
+        resp = self.client.get(url_for('index.huesound', color="FF00FF"))
+        self.assertStatus(resp, 302)
+
+    def test_similar_users(self):
+        resp = self.client.get(url_for('explore.similar_users'))
+        self.assert200(resp)
+
+    def test_similar_users_redirect(self):
+        resp = self.client.get(url_for('index.similar_users'))
+        self.assertStatus(resp, 302)
+
+    def test_fresh_releases(self):
+        resp = self.client.get(url_for('explore.fresh_releases'))
+        self.assert200(resp)
+
+    @patch('listenbrainz.db.fresh_releases.get_sitewide_fresh_releases')
+    def test_fresh_releases_api(self, mock_fresh):
+        resp = self.client.get(url_for('explore_api_v1.get_fresh_releases'))
+        self.assert200(resp)
+        mock_fresh.assert_called_with(datetime.date.today(), 14)
+
+        resp = self.client.get(url_for('explore_api_v1.get_fresh_releases', release_date="2022-01-01", days=5))
+        self.assert200(resp)
+        mock_fresh.assert_called_with(datetime.date(year=2022, month=1, day=1), 5)
