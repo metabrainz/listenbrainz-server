966c7363fa8c6e8d2987298fe232b393eb3d5b72
Chinmay Kunkikar <chinmay.kunkikar@gmail.com>
2022-07-29 23:43:47 +0530

    FreshReleases: useState for release data

--- listenbrainz/webserver/static/js/src/fresh-releases/FreshReleases.tsx
+++ listenbrainz/webserver/static/js/src/fresh-releases/FreshReleases.tsx
@@ -10,18 +10,50 @@ import GlobalAppContext, { GlobalAppContextT } from "../utils/GlobalAppContext";
 import { getPageProps } from "../utils/utils";
 import ErrorBoundary from "../utils/ErrorBoundary";
 import ReleaseCard from "./ReleaseCard";
-import * as fakeData from "./fakeData.json";
+import fakeData from "./fakeData.json";
 import ReleaseFilters from "./ReleaseFilters";
+
+type ReleaseDataType = {
+  date: string;
+  artist_mbids: Array<string>;
+  release_mbid: string;
+  release_name: string;
+  artist_credit_name: string;
+  release_group_primary_type: string | null;
+  release_group_secondary_type: string | null;
+};
 
 export default function FreshReleases() {
   const RELEASE_TYPE_OTHER = "Other";
 
+  const [releaseData, setReleaseData] = React.useState<Array<ReleaseDataType>>(
+    fakeData
+  );
   const [typesList, setTypesList] = React.useState<Array<string | null>>([]);
+
+  React.useEffect(() => {
+    setReleaseData(
+      // Deduplicate releases based on same release name by same artist name.
+      uniqBy(fakeData, (datum) => {
+        return (
+          /*
+           * toLowerCase() solves an edge case.
+           * Example:
+           * "release_name": "Waterslide, Diving Board, Ladder to the Sky"
+           * "release_name": "Waterslide, Diving Board, Ladder To The Sky"
+           * These releases will be considered unique.
+           */
+          datum.release_name.toLowerCase() +
+          datum.artist_credit_name.toLowerCase()
+        );
+      })
+    );
+  }, []);
 
   React.useEffect(() => {
     setTypesList(
       uniqWith(
-        fakeData.map((release) => {
+        releaseData.map((release) => {
           if (
             release.release_group_primary_type !== null ||
             release.release_group_secondary_type !== null
@@ -49,37 +81,22 @@ export default function FreshReleases() {
           <ReleaseFilters filters={typesList} />
         </div>
         <div className="release-cards-grid col-xs-12 col-md-10">
-          {// Deduplicate releases based on same release name by same artist name.
-          uniqBy(fakeData, (datum) => {
+          {releaseData.slice(600, 620).map((release) => {
             return (
-              /*
-               * toLowerCase() solves an edge case.
-               * Example:
-               * "release_name": "Waterslide, Diving Board, Ladder to the Sky"
-               * "release_name": "Waterslide, Diving Board, Ladder To The Sky"
-               * These releases will be considered unique.
-               */
-              datum.release_name.toLowerCase() +
-              datum.artist_credit_name.toLowerCase()
+              <ReleaseCard
+                key={release.release_mbid}
+                releaseDate={release.date}
+                releaseMBID={release.release_mbid}
+                releaseName={release.release_name}
+                releaseType={
+                  release.release_group_primary_type ||
+                  release.release_group_secondary_type
+                }
+                artistCreditName={release.artist_credit_name}
+                artistMBIDs={release.artist_mbids}
+              />
             );
-          })
-            .slice(990, 1005)
-            .map((release) => {
-              return (
-                <ReleaseCard
-                  key={release.release_mbid}
-                  releaseDate={release.date}
-                  releaseMBID={release.release_mbid}
-                  releaseName={release.release_name}
-                  releaseType={
-                    release.release_group_primary_type ||
-                    release.release_group_secondary_type
-                  }
-                  artistCreditName={release.artist_credit_name}
-                  artistMBIDs={release.artist_mbids}
-                />
-              );
-            })}
+          })}
         </div>
         <div className="releases-timeline col-xs-12 col-md-1">Timeline</div>
       </div>
@@ -88,12 +105,7 @@ export default function FreshReleases() {
 }
 
 document.addEventListener("DOMContentLoaded", () => {
-  const {
-    domContainer,
-    // reactProps,
-    globalReactProps,
-    // optionalAlerts,
-  } = getPageProps();
+  const { domContainer, globalReactProps } = getPageProps();
   const {
     api_url,
     sentry_dsn,
@@ -102,7 +114,6 @@ document.addEventListener("DOMContentLoaded", () => {
     youtube,
     sentry_traces_sample_rate,
   } = globalReactProps;
-  // const {} = reactProps;
 
   if (sentry_dsn) {
     Sentry.init({
