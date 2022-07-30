10bfe0e4815d993ded2482718336699012d36da8
Kartik Ohri <kartikohri13@gmail.com>
2022-07-31 00:32:49 +0530

    Update spark implementation of fresh releases
    Use the sitewide fresh releases endpoint to fetch releases instead
    of using hardcoded file. Rename spark part from reent_releases to
    fresh_releases.

--- listenbrainz_spark/fresh_releases/fresh_releases.py
+++ listenbrainz_spark/fresh_releases/fresh_releases.py
@@ -1,50 +1,49 @@
-import json
 from datetime import timedelta, datetime
-from pathlib import Path
 
+import requests
 from more_itertools import chunked
 from pyspark import Row
 
 import listenbrainz_spark
 from listenbrainz_spark.constants import LAST_FM_FOUNDING_YEAR
-from listenbrainz_spark.schema import release_radar_schema
+from listenbrainz_spark.schema import fresh_releases_schema
 from listenbrainz_spark.stats import run_query
 from listenbrainz_spark.utils import get_latest_listen_ts, get_listens_from_new_dump
 
 USERS_PER_MESSAGE = 5
 
 
+FRESH_RELEASES_ENDPOINT = "https://api.listenbrainz.org/1/explore/fresh-releases/"
+
+
 def load_all_releases():
-    with Path(__file__).parent.joinpath("release.json").open() as f:
-        data = json.load(f)
+    response = requests.get(FRESH_RELEASES_ENDPOINT)
+    data = response.json()
 
-        releases = []
-        for release in data:
-            releases.append(Row(
-                date=release["date"],
-                artist_credit_name=release["artist_credit_name"],
-                artist_mbids=release["artist_mbids"],
-                release_name=release["release_name"],
-                release_mbid=release["release_mbid"],
-                release_group_primary_type=release["release_group_primary_type"],
-                release_group_secondary_type=release["release_group_secondary_type"]
-            ))
+    releases = []
+    for release in data:
+        releases.append(Row(
+            date=release["date"],
+            artist_credit_name=release["artist_credit_name"],
+            artist_mbids=release["artist_mbids"],
+            release_name=release["release_name"],
+            release_mbid=release["release_mbid"],
+            release_group_primary_type=release["release_group_primary_type"],
+            release_group_secondary_type=release["release_group_secondary_type"]
+        ))
 
-        return listenbrainz_spark.session.createDataFrame(
-            releases,
-            schema=release_radar_schema
-        )
+    return listenbrainz_spark.session.createDataFrame(releases, schema=fresh_releases_schema)
 
 
 def get_query():
     return """
         WITH artists AS (
             SELECT DISTINCT explode(artist_mbids) AS artist_mbid
-              FROM release_radar_releases
+              FROM fresh_releases
         ), exploded_listens AS (
             SELECT user_id
                  , explode(artist_credit_mbids) AS artist_mbid
-              FROM release_radar_listens
+              FROM fresh_releases_listens
         ), artist_discovery AS (
             SELECT user_id
                  , artist_mbid
@@ -64,7 +63,7 @@ def get_query():
                  , rr.release_group_secondary_type
                  , SUM(partial_confidence) AS confidence
               FROM artist_discovery ad
-              JOIN release_radar_releases rr
+              JOIN fresh_releases rr
                 ON array_contains(rr.artist_mbids, ad.artist_mbid)
           GROUP BY ad.user_id
                  , rr.release_name
@@ -108,9 +107,14 @@ def main(days: int, database: str):
     else:
         from_date = datetime(LAST_FM_FOUNDING_YEAR, 1, 1)
     get_listens_from_new_dump(from_date, to_date) \
-        .createOrReplaceTempView("release_radar_listens")
+        .createOrReplaceTempView("fresh_releases_listens")
 
-    load_all_releases().createOrReplaceTempView("release_radar_releases")
+    load_all_releases().createOrReplaceTempView("fresh_releases")
+
+    yield {
+        "type": "couchdb_data_start",
+        "database": database
+    }
 
     itr = run_query(get_query()).toLocalIterator()
     for rows in chunked(itr, USERS_PER_MESSAGE):
@@ -120,6 +124,11 @@ def main(days: int, database: str):
             entry["database"] = database
             entries.append(entry)
         yield {
-            "type": "recent_releases",
+            "type": "fresh_releases",
             "data": entries
         }
+
+    yield {
+        "type": "couchdb_data_end",
+        "database": database
+    }
