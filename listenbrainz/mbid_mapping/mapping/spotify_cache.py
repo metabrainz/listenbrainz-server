d4c4178b7b863f0564a6b63bde1c687b40d5777d
Robert Kaye <rob@musicbrainz.org>
2022-08-03 17:05:01 +0200

    Reworking things to have more control over what gets looked up next. Was working until the spotify api stopped responding.

--- listenbrainz/mbid_mapping/mapping/spotify_cache.py
+++ listenbrainz/mbid_mapping/mapping/spotify_cache.py
@@ -1,14 +1,18 @@
 #!/usr/bin/env python3
 from datetime import datetime, timedelta
+from time import sleep
 import json
 from queue import Queue
+import uuid
 
-import couchdb
 import spotipy
 import dateutil.parser
 from spotipy.oauth2 import SpotifyClientCredentials
 
+from listenbrainz.db import couchdb
 import config
+from icecream import ic
+
 
 class UniqueQueue(object):
     def __init__(self):
@@ -17,7 +21,7 @@ class UniqueQueue(object):
 
     def put(self, d):
         if not d in self.set:
-            self.queue.put(d) 
+            self.queue.put(d)
             self.set.add(d)
             return True
         return False
@@ -30,6 +34,9 @@ class UniqueQueue(object):
     def size(self):
         return self.queue.qsize()
 
+    def empty(self):
+        return self.queue.empty()
+
 
 class SpotifyMetadataCache():
 
@@ -38,23 +45,24 @@ class SpotifyMetadataCache():
 
     def __init__(self):
         self.id_queue = UniqueQueue()
-        self.seen_ids = {}
         self.sp = spotipy.Spotify(auth_manager=SpotifyClientCredentials(client_id=config.SPOTIFY_APP_CLIENT_ID,
                                                                         client_secret=config.SPOTIFY_APP_CLIENT_SECRET))
-        self.couch = couchdb.Server('http://listenbrainz:listenbrainz@localhost:5984/')
-        try:
-            self.db = self.couch[self.COUCHDB_NAME]
-        except couchdb.http.ResourceNotFound:
-            self.create_db()
-            self.db = self.couch[self.COUCHDB_NAME]
-
-    def create_db(self):
-        self.couch.create(self.COUCHDB_NAME)
+        self.couch = couchdb.init(config.COUCHDB_USER, config.COUCHDB_PASSWORD, config.COUCHDB_HOST, config.COUCHDB_PORT)
+        dbs = couchdb.list_databases("")
+        if self.COUCHDB_NAME not in dbs:
+            couchdb.create_database(self.COUCHDB_NAME)
+        couchdb.create_index(self.COUCHDB_NAME, {
+            "index": {
+                "fields": ["status", "queued"]
+            },
+            "name": "status-index",
+            "type": "json"
+        })
 
     def queue_id(self, spotify_id):
         self.id_queue.put(spotify_id)
 
-    def fetch_artist(self, artist_id):
+    def fetch_artist(self, artist_id, add_discovered_artists=False):
         artist = self.sp.artist(artist_id)
 
         results = self.sp.artist_albums(artist_id, album_type='album,single,compilation')
@@ -70,10 +78,11 @@ class SpotifyMetadataCache():
                 results = self.sp.next(results)
                 tracks.extend(results["items"])
 
-            for track in tracks:
-                for track_artist in track["artists"]:
-                    if track_artist["id"] != artist_id and track_artist["id"] not in self.seen_ids:
-                        self.id_queue.put("artist:%s" % track_artist["id"])
+            if add_discovered_artists:
+                for track in tracks:
+                    for track_artist in track["artists"]:
+                        if track_artist["id"] != artist_id and track_artist["id"]:
+                            self.id_queue.put("artist:%s" % track_artist["id"])
 
             album["tracks"] = tracks
 
@@ -82,55 +91,77 @@ class SpotifyMetadataCache():
         return artist
 
     def fetch_artist_ids_from_track_id(self, track_id):
-        return [ a["id"] for a in self.sp.track(track_id)["artists"] ]
+        return [a["id"] for a in self.sp.track(track_id)["artists"]]
 
-    def insert_artist(self, artist, exists=False):
+    def insert_artist(self, artist):
         artist["_id"] = artist["id"]
         artist["fetched"] = datetime.utcnow().isoformat()
         artist["expires"] = (datetime.utcnow() + timedelta(days=self.CACHE_TIME)).isoformat()
-        if exists:
-            self.db.update([artist])
-        else:
-            self.db.save(artist)
-        print("Inserted artist '%s' %s" % (artist["name"], artist["id"]))
+        artist["status"] = "fetched"
+        couchdb.insert_data(self.COUCHDB_NAME, [artist])
+
+    def add_pending_spotify_ids(self, spotify_ids):
+        for spotify_id in spotify_ids:
+            if not spotify_id.startswith("artist:") and not spotify_id.startswith("track:"):
+                print("Invalid ID submitted -- all IDs must start with artist: or track:")
+                return
+
+        data = {"status": "pending",
+                          "_id": str(uuid.uuid4()),
+                          "spotify_ids": spotify_ids,
+                          "queued": datetime.utcnow().isoformat()}
+        couchdb.insert_data(self.COUCHDB_NAME, [data])
+
+    def load_ids_to_process(self):
+        mango = {"selector": {"status": "pending"},
+                 "fields": ["queued", "spotify_ids"],
+                 "sort": [{"queued": "asc"}]}
+        ret = False
+        for row in couchdb.find_data(self.COUCHDB_NAME, mango):
+            for spotify_id in row["spotify_ids"]:
+                print("Add spotify id %s to queue" % spotify_id)
+                self.id_queue.put(spotify_id)
+                ret = True
+
+        return ret
 
     def process_artist(self, artist_id):
 
-        # Check our internal cache if we've seen it recently
-        if artist_id in self.seen_ids:
-            return
-
-        # Mark this ID as recently seen
-        self.seen_ids[artist_id] = datetime.now()
+        print(f"Process {artist_id}")
 
         # Fetch an existing doc and if found, see if it has expired
-        existing_doc = self.db.get(artist_id)
+        existing_doc = couchdb.get(self.COUCHDB_NAME, artist_id)
         if existing_doc is not None:
             expires = dateutil.parser.isoparse(existing_doc["expires"])
             if dateutil.parser.isoparse(existing_doc["expires"]) > datetime.utcnow():
-                print("Artist '%s' in db, not stale. Skipping." % artist_id)
+                print("Find existing doc!")
                 return
-            else:
-                print("Artist '%s' in db, stale. Fetch new." % artist_id)
+            rev = int(existing_doc["_rev"]) + 1
+        else:
+            rev = 1
 
         artist_data = self.fetch_artist(artist_id)
+        artist_data["_rev"] = str(rev)
 
-        self.insert_artist(artist_data, existing_doc is not None)
+        self.insert_artist(artist_data)
 
     def start(self):
         """ Main loop of the application """
 
         while True:
-            spotify_id = self.id_queue.get()
+            # If we have no more artists in the queue, fetch from the DB. If there are none, sleep, try again.
+            if self.id_queue.empty():
+                if not self.load_ids_to_process():
+                    sleep(60)
+                    continue
 
-            # Check to see if we've recently queried this id, if so move to the next id
-            if spotify_id in self.seen_ids:
-                continue
+            spotify_id = self.id_queue.get()
+            print(f"got {spotify_id}")
 
             if spotify_id.startswith("track:"):
                 artist_ids = self.fetch_artist_ids_from_track_id(spotify_id[6:])
             elif spotify_id.startswith("artist:"):
-                artist_ids = [ spotify_id[7:] ]
+                artist_ids = [spotify_id[7:]]
             else:
                 raise ValueError("Unknown ID type", spotify_id)
 
@@ -142,6 +173,9 @@ class SpotifyMetadataCache():
 
 def run_spotify_metadata_cache():
     smc = SpotifyMetadataCache()
-    smc.queue_id("artist:6liAMWkVf5LH7YR9yfFy1Y")
-    smc.queue_id("track:5wdub8zu2WLds6uRN0jUsV")
     smc.start()
+
+
+def queue_spotify_ids(spotify_ids):
+    smc = SpotifyMetadataCache()
+    smc.add_pending_spotify_ids(spotify_ids)
