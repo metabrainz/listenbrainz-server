a5399bea25f856b09c3eda8a95da9c17ec3c7295
Kartik Ohri <kartikohri13@gmail.com>
2022-08-08 16:12:26 +0530

    update listen api integration tests - 21

--- listenbrainz/webserver/login/__init__.py
+++ listenbrainz/webserver/login/__init__.py
@@ -47,7 +47,10 @@ class User(UserMixin):
 @login_manager.user_loader
 def load_user(user_login_id):
     try:
+        print(user_login_id)
         user = db_user.get_by_login_id(db_conn, user_login_id)
+        print(user)
+        print(db_conn.execute('SELECT * FROM "user"').fetchall())
     except Exception as e:
         current_app.logger.error("Error while getting user by login ID: %s", str(e), exc_info=True)
         return None
