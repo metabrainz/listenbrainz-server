5b076460285f816e5a35b2ae2015c09a9e9c586d
Kartik Ohri <kartikohri13@gmail.com>
2022-08-11 18:56:12 +0530

    Remove misc unused files

--- listenbrainz_spark/sentry_daemon.py
+++
@@ -1,9 +0,0 @@
-import sentry_sdk
-from sentry_sdk.integrations.spark import SparkWorkerIntegration
-import pyspark.daemon as original_daemon
-
-import listenbrainz_spark.config as conf
-
-if __name__ == '__main__':
-    sentry_sdk.init(**conf.LOG_SENTRY, integrations=[SparkWorkerIntegration()])
-    original_daemon.manager()
