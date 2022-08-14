c3c813135103568f65af4588775c63f0b8f9bb43
Kartik Ohri <kartikohri13@gmail.com>
2022-08-15 00:30:47 +0530

    remove pika import

--- listenbrainz_spark/utils/__init__.py
+++ listenbrainz_spark/utils/__init__.py
@@ -1,11 +1,9 @@
 import errno
 import logging
 import os
-from time import sleep
 from datetime import datetime
 from typing import List
 
-import pika
 from py4j.protocol import Py4JJavaError
 from pyspark.sql import DataFrame, functions
 from pyspark.sql.utils import AnalysisException
@@ -55,25 +53,6 @@ def append(df, dest_path):
         df.write.mode('append').parquet(config.HDFS_CLUSTER_URI + dest_path)
     except Py4JJavaError as err:
         raise DataFrameNotAppendedException(err.java_exception, df.schema)
-
-
-def init_rabbitmq(username, password, host, port, vhost, log=logger.error,
-                  heartbeat=None, connection_name="listenbrainz-spark-request-consumer"):
-    while True:
-        try:
-            credentials = pika.PlainCredentials(username, password)
-            connection_parameters = pika.ConnectionParameters(
-                host=host,
-                port=port,
-                virtual_host=vhost,
-                credentials=credentials,
-                heartbeat=heartbeat,
-                client_properties={"connection_name": connection_name}
-            )
-            return pika.BlockingConnection(connection_parameters)
-        except Exception as e:
-            log('Error while connecting to RabbitMQ', exc_info=True)
-            sleep(1)
 
 
 def create_dataframe(row, schema):
