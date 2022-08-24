c0384f85d705735581b001cbec7ebfec821029cd
Kartik Ohri <kartikohri13@gmail.com>
2022-08-24 18:31:11 +0530

    Try resetting TS db by deleting instead of recreating

--- listenbrainz/db/testing.py
+++ listenbrainz/db/testing.py
@@ -25,22 +25,8 @@ class DatabaseTestCase(unittest.TestCase):
     def reset_db(self):
         db.run_sql_script(os.path.join(ADMIN_SQL_DIR, 'reset_tables.sql'))
 
-    def init_db(self):
-        db.run_sql_script(os.path.join(ADMIN_SQL_DIR, 'create_schema.sql'))
-        db.run_sql_script(os.path.join(ADMIN_SQL_DIR, 'create_tables.sql'))
-        db.run_sql_script(os.path.join(ADMIN_SQL_DIR, 'create_primary_keys.sql'))
-        db.run_sql_script(os.path.join(ADMIN_SQL_DIR, 'create_foreign_keys.sql'))
-        db.run_sql_script(os.path.join(ADMIN_SQL_DIR, 'create_indexes.sql'))
-
-    def drop_tables(self):
-        self.drop_schema()
-        db.run_sql_script(os.path.join(ADMIN_SQL_DIR, 'drop_tables.sql'))
-
-    def drop_schema(self):
-        db.run_sql_script(os.path.join(ADMIN_SQL_DIR, 'drop_schema.sql'))
-
     @classmethod
-    def path_to_data_file(cls, file_name):
+    def path_to_data_file(self, file_name):
         """ Returns the path of the test data file relative to listenbrainz/db/testing.py.
 
             Args:
@@ -67,24 +53,8 @@ class TimescaleTestCase(unittest.TestCase):
     def setUp(self):
         # Reset the database in setUp, this way if there is a test failure and you want
         # to inspect data, it won't have been cleared away
+        ts.init_db_connection(config.SQLALCHEMY_TIMESCALE_URI)
         self.reset_timescale_db()
 
     def reset_timescale_db(self):
-        ts.init_db_connection(config.TIMESCALE_ADMIN_URI)
-        ts.run_sql_script_without_transaction(os.path.join(TIMESCALE_SQL_DIR, 'drop_db.sql'))
-        ts.run_sql_script_without_transaction(os.path.join(TIMESCALE_SQL_DIR, 'create_db.sql'))
-        ts.engine.dispose()
-
-        ts.init_db_connection(config.TIMESCALE_ADMIN_LB_URI)
-        ts.run_sql_script_without_transaction(os.path.join(TIMESCALE_SQL_DIR, 'create_extensions.sql'))
-        ts.engine.dispose()
-
-        ts.init_db_connection(config.SQLALCHEMY_TIMESCALE_URI)
-        ts.run_sql_script(os.path.join(TIMESCALE_SQL_DIR, 'create_schemas.sql'))
-        ts.run_sql_script(os.path.join(TIMESCALE_SQL_DIR, 'create_types.sql'))
-        ts.run_sql_script(os.path.join(TIMESCALE_SQL_DIR, 'create_tables.sql'))
-        ts.run_sql_script(os.path.join(TIMESCALE_SQL_DIR, 'create_functions.sql'))
-        ts.run_sql_script(os.path.join(TIMESCALE_SQL_DIR, 'create_indexes.sql'))
-        ts.run_sql_script(os.path.join(TIMESCALE_SQL_DIR, 'create_primary_keys.sql'))
-        ts.run_sql_script(os.path.join(TIMESCALE_SQL_DIR, 'create_foreign_keys.sql'))
-        ts.engine.dispose()
+        ts.run_sql_script(os.path.join(TIMESCALE_SQL_DIR, 'reset_tables.sql'))
