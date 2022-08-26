ab4f38296ed5991812bfb7fccd2a8b7ff45ae7c9
treeshateorcs <basedtho@gmail.com>
2022-08-26 15:09:45 -0400

    Error message for an incorrectly entered username (#2117)
    * Error message for an incorrectly entered username
    
    * Reimplement the logic entirely
    
    * Change the error message
    
    * wrong_username is in a correct place now
    
    * draft test
    
    * minor stylistic correction
    
    * Fix "should throw user not found" test
    
    * Use ?.
    
    Co-authored-by: treeshateorcs <trees@hateorcs.com>
    Co-authored-by: Kartik Ohri <kartikohri13@gmail.com>

--- listenbrainz/webserver/static/js/tests/lastfm/LastFMImporter.test.tsx
+++ listenbrainz/webserver/static/js/tests/lastfm/LastFMImporter.test.tsx
@@ -1,7 +1,9 @@
 import * as React from "react";
 import { mount, shallow } from "enzyme";
 import fetchMock from "jest-fetch-mock";
-import LastFmImporter, { LASTFM_RETRIES } from "../../src/lastfm/LastFMImporter";
+import LastFmImporter, {
+  LASTFM_RETRIES,
+} from "../../src/lastfm/LastFMImporter";
 // Mock data to test functions
 import * as page from "../__mocks__/page.json";
 import * as getInfo from "../__mocks__/getInfo.json";
@@ -127,6 +129,19 @@ describe("getTotalNumberOfScrobbles", () => {
     await expect(instance.getTotalNumberOfScrobbles()).rejects.toThrowError();
     expect(instance.state.msg?.props.children).toMatch(
       "An error occurred, please try again. :("
+    );
+  });
+  it("should throw user not found", async () => {
+    window.fetch = jest.fn().mockImplementation(() =>
+      Promise.resolve({
+        status: 404,
+        json: () => Promise.resolve({ message: "User not found", error: 6 }),
+      })
+    );
+    instance.setState({ lastfmUsername: "nonexistentusernamedonttryathome" });
+
+    await expect(instance.getTotalNumberOfScrobbles()).rejects.toThrowError(
+      "User not found"
     );
   });
 });
