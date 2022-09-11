72a6e672a1dfad67a68b9ed2ae2d2eb24daa023a
Kartik Ohri <kartikohri13@gmail.com>
2022-09-11 17:50:19 +0530

    Add msb migration script

--- admin/timescale/create_indexes.sql
+++ admin/timescale/create_indexes.sql
@@ -37,6 +37,6 @@ CREATE INDEX last_updated_ndx_mbid_mapping ON mbid_mapping (last_updated);
 -- messybrainz
 CREATE UNIQUE INDEX messybrainz_gid_ndx ON messybrainz.submissions (gid);
 -- can't have a unique index here due to historical duplicates
-CREATE INDEX messybrainz_data_ndx ON messybrainz.submissions (lower(recording), lower(artist_credit), lower(release));
+CREATE INDEX messybrainz_data_ndx ON messybrainz.submissions (lower(recording), lower(artist_credit), lower(release), lower(track_number), duration);
 
 COMMIT;
