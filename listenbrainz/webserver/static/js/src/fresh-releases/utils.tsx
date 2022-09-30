a99398c45181a062c546a95098e99e61de79f24b
Chinmay Kunkikar <chinmay.kunkikar@gmail.com>
2022-09-30 20:46:23 +0530

    utils: introduce getAlbumArtFromReleaseMBID() Get rid of the redundant utility function

--- listenbrainz/webserver/static/js/src/fresh-releases/utils.tsx
+++ listenbrainz/webserver/static/js/src/fresh-releases/utils.tsx
@@ -1,51 +1,3 @@
-const originalFetch = window.fetch;
-const fetchWithRetry = require("fetch-retry")(originalFetch);
-
-const getCoverartFromReleaseMBID = async (
-  releaseMBID: string
-): Promise<string | undefined> => {
-  try {
-    const CAAResponse = await fetchWithRetry(
-      `https://coverartarchive.org/release/${releaseMBID}`,
-      {
-        retries: 3,
-        retryOn: [429],
-        retryDelay(attempt: number) {
-          const maxRetryTime = 800;
-          const minRetryTime = 400;
-          return Math.floor(
-            Math.random() * (maxRetryTime - minRetryTime) +
-              attempt * minRetryTime
-          );
-        },
-      }
-    );
-    if (CAAResponse.ok) {
-      const body: CoverArtArchiveResponse = await CAAResponse.json();
-      if (!body.images?.length) {
-        return undefined;
-      }
-      const frontImage = body.images.find((image) => image.front);
-
-      if (frontImage?.id) {
-        const { id } = frontImage;
-        return `https://archive.org/download/mbid-${releaseMBID}/mbid-${releaseMBID}-${id}_thumb250.jpg`;
-      }
-
-      // No front image? Fallback to whatever the first image is
-      const { thumbnails, image } = body.images[0];
-      return thumbnails[250] ?? thumbnails.small ?? image;
-    }
-  } catch (error) {
-    // eslint-disable-next-line no-console
-    console.warn(
-      `Couldn't fetch Cover Art Archive entry for ${releaseMBID}`,
-      error
-    );
-  }
-  return undefined;
-};
-
 const formattedReleaseDate = (releaseDate: string) => {
   return new Intl.DateTimeFormat("default", {
     month: "short",
@@ -57,4 +9,5 @@ const formattedReleaseDate = (releaseDate: string) => {
     .join("");
 };
 
-export { getCoverartFromReleaseMBID, formattedReleaseDate };
+// eslint-disable-next-line import/prefer-default-export
+export { formattedReleaseDate };
