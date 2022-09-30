e2c677db40f1b87d639b4eef26c15f86d9b76bf1
Kartik Ohri <kartikohri13@gmail.com>
2022-09-30 22:03:28 +0530

    fix marking artist mbids as dirty - 4

--- listenbrainz/mbid_mapping/mapping/mb_metadata_cache.py
+++ listenbrainz/mbid_mapping/mapping/mb_metadata_cache.py
@@ -599,7 +599,7 @@ class MusicBrainzMetadataCache(BulkInsertTable):
                   UPDATE {self.table_name}
                      SET dirty = 't'
                     FROM dirty_mbids
-                   WHERE {self.table_name}.artist_mbids @> dirty_mbids.artist_mbid::text
+                   WHERE {self.table_name}.artist_mbids::text[] @> dirty_mbids.artist_mbid::text
                 """
                 execute_values(curs, query, [(mbid,) for mbid in artist_mbids], page_size=len(artist_mbids))
 
