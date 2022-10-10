6e42e53f5bbe41d9df525938f114b1c511bce713
Kartik Ohri <kartikohri13@gmail.com>
2022-10-11 00:47:14 +0530

    Add detunings

--- listenbrainz/labs_api/labs/api/spotify/utils.py
+++ listenbrainz/labs_api/labs/api/spotify/utils.py
@@ -8,6 +8,13 @@ from psycopg2.extras import execute_values
 from psycopg2.sql import SQL, Identifier
 
 
+def detune(artist_name: str):
+    phrases = [" ft ", " feat ", " ft. ", " feat. "]
+    for phrase in phrases:
+        artist_name = artist_name.replace(phrase, " ")
+    return artist_name
+
+
 def perform_combined_lookup(column: str, lookups: list[tuple]):
     query = SQL("""
           WITH lookups (idx, value) AS (VALUES %s)
@@ -34,7 +41,7 @@ def lookup_using_metadata(params):
         combined_lookup_all = unidecode(re.sub(r'[^\w]+', '', combined_text_all).lower())
         lookups.append((idx, combined_lookup_all))
 
-    remaining_lookups = []
+    remaining_items = {}
     index = perform_combined_lookup("combined_lookup_all", lookups)
     for idx, item in metadata.items():
         spotify_id = index.get(idx)
@@ -42,16 +49,43 @@ def lookup_using_metadata(params):
             metadata[idx]["spotify_track_id"] = spotify_id
         else:
             metadata[idx]["spotify_track_id"] = None
+            remaining_items[idx] = item
 
+    if remaining_items:
+        remaining_lookups = []
+        for idx, item in remaining_items.items():
+            combined_text_all_detuned = detune(item["artist_name"]) + item["release_name"] + item["track_name"]
+            combined_lookup_all_detuned = unidecode(re.sub(r'[^\w]+', '', combined_text_all_detuned).lower())
+            remaining_lookups.append((idx, combined_lookup_all_detuned))
+
+        index = perform_combined_lookup("combined_lookup_all", remaining_lookups)
+        for idx, spotify_id in index.items():
+            metadata[idx]["spotify_track_id"] = spotify_id
+            remaining_items.pop(idx, None)
+
+    if remaining_items:
+        remaining_lookups = []
+        for idx, item in remaining_items.items():
             combined_text_without_album = item["artist_name"] + item["track_name"]
             combined_lookup_without_album = unidecode(re.sub(r'[^\w]+', '', combined_text_without_album).lower())
             remaining_lookups.append((idx, combined_lookup_without_album))
 
-    if remaining_lookups:
         index = perform_combined_lookup("combined_lookup_without_album", remaining_lookups)
-        print(index)
         for idx, spotify_id in index.items():
             metadata[idx]["spotify_track_id"] = spotify_id
+            remaining_items.pop(idx, None)
+
+    if remaining_items:
+        remaining_lookups = []
+        for idx, item in remaining_items.items():
+            combined_text_without_album_detuned = detune(item["artist_name"]) + item["track_name"]
+            combined_lookup_without_album_detuned = unidecode(re.sub(r'[^\w]+', '', combined_text_without_album_detuned).lower())
+            remaining_lookups.append((idx, combined_lookup_without_album_detuned))
+
+        index = perform_combined_lookup("combined_lookup_without_album", remaining_lookups)
+        for idx, spotify_id in index.items():
+            metadata[idx]["spotify_track_id"] = spotify_id
+            remaining_items.pop(idx, None)
 
     return list(metadata.values())
 
