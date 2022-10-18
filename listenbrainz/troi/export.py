64e40a87d7bd6805133f7183c13c2bd56d14e750
Kartik Ohri <kartikohri13@gmail.com>
2022-10-18 20:08:22 +0530

    Update frontend tests and troi

--- listenbrainz/troi/export.py
+++ listenbrainz/troi/export.py
@@ -1,16 +1,22 @@
 import spotipy
 from troi.core import generate_playlist
+from troi.patches.playlist_from_listenbrainz import TransferPlaylistPatch
 
 
 def export_to_spotify(lb_token, spotify_token, playlist_mbid, is_public):
     sp = spotipy.Spotify(auth=spotify_token)
     # TODO: store spotify user ids in external_service_oauth table
     spotify_user_id = sp.current_user()["id"]
-    spotify_args = {
-        "user_id": spotify_user_id,
-        "token": spotify_token,
-        "is_public": is_public,
-        "is_collaborative": False
+    args = {
+        "mbid": playlist_mbid,
+        "read_only_token": lb_token,
+        "spotify": {
+            "user_id": spotify_user_id,
+            "token": spotify_token,
+            "is_public": is_public,
+            "is_collaborative": False
+        },
+        "upload": True
     }
-    playlist = generate_playlist("resave-playlist", args=[playlist_mbid, lb_token], spotify=spotify_args, upload=True)
+    playlist = generate_playlist(TransferPlaylistPatch(), args)
     return playlist.playlists[0].external_urls[0]
