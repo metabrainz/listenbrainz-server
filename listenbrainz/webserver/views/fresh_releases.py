8c77f92117c542c4ab976e94253204ecbfc67406
Kartik Ohri <kartikohri13@gmail.com>
2022-10-21 19:26:32 +0530

    Add @crossdomain to fresh releases api

--- listenbrainz/webserver/views/fresh_releases.py
+++ listenbrainz/webserver/views/fresh_releases.py
@@ -3,12 +3,14 @@ from flask import Blueprint, jsonify
 
 import listenbrainz.db.user as db_user
 from listenbrainz.db.fresh_releases import get_fresh_releases
+from listenbrainz.webserver.decorators import crossdomain
 from listenbrainz.webserver.errors import APINoContent, APINotFound
 
 fresh_releases_bp = Blueprint('fresh_releases_v1', __name__)
 
 
 @fresh_releases_bp.route("/user/<user_name>/fresh_releases")
+@crossdomain
 def get_releases(user_name):
     """ Get fresh releases data for the given user. """
     user = db_user.get_by_mb_id(user_name)
