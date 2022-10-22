ccfc6dc19fe8ebdd0bb3703333e41ef458de4439
Kartik Ohri <kartikohri13@gmail.com>
2022-10-23 00:13:32 +0530

    Initial LB setup for similar recordings

---
+++ admin/timescale/updates/2022-10-23-add-recording-similarity.sql
@@ -0,0 +1,15 @@
+BEGIN;
+
+CREATE SCHEMA similarity;
+
+CREATE TABLE similarity.recording (
+    mbid0 UUID NOT NULL,
+    mbid1 UUID NOT NULL,
+    metadata JSONB NOT NULL
+
+    CONSTRAINT alphabetical_uuids CHECK (mbid0 <= mbid1)
+);
+
+CREATE UNIQUE INDEX similar_recordings_uniq_idx ON similarity.recording (mbid0, mbid1);
+
+COMMIT;
