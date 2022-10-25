f4c2b3697f33dfd3df93c40d2de83fb8f7772661
Kartik Ohri <kartikohri13@gmail.com>
2022-10-25 19:34:22 +0530

    Constant-ify preference name

--- listenbrainz/webserver/views/user_settings_api.py
+++ listenbrainz/webserver/views/user_settings_api.py
@@ -1,18 +1,14 @@
 import ujson
-
 from brainzutils.ratelimit import ratelimit
 from flask import Blueprint, jsonify, request
 
-from listenbrainz.webserver import flash
-import listenbrainz.db.user as db_user
 import listenbrainz.db.user_setting as db_usersetting
-from listenbrainz.db.exceptions import DatabaseException
+from listenbrainz.troi.troi_bot import SPOTIFY_EXPORT_PREFERENCE
 from listenbrainz.webserver.decorators import crossdomain
 from listenbrainz.webserver.errors import APIInternalServerError, APIBadRequest
 from listenbrainz.webserver.views.api_tools import (
     validate_auth_header,
 )
-
 
 user_settings_api_bp = Blueprint('user_settings_api_v1', __name__)
 
@@ -68,12 +64,12 @@ def update_troi_prefs():
     except (ValueError, KeyError) as e:
         raise APIBadRequest(f"Invalid JSON: {str(e)}")
 
-    if "export_to_spotify" not in data:
-        raise APIBadRequest("JSON document must contain export_to_spotify key", data)
+    if SPOTIFY_EXPORT_PREFERENCE not in data:
+        raise APIBadRequest(f"JSON document must contain {SPOTIFY_EXPORT_PREFERENCE} key", data)
 
-    export_to_spotify = data["export_to_spotify"]
+    export_to_spotify = data[SPOTIFY_EXPORT_PREFERENCE]
     if type(export_to_spotify) != bool:
-        raise APIBadRequest("export_to_spotify key in the JSON document must be a boolean", data)
+        raise APIBadRequest(f"{SPOTIFY_EXPORT_PREFERENCE} key in the JSON document must be a boolean", data)
 
     db_usersetting.update_troi_prefs(user["id"], export_to_spotify)
     return jsonify({"status": "ok"})
