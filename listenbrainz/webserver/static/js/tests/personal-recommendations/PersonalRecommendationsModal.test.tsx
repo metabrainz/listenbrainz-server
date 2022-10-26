26bc03d7bf9224f178a34bdf17b58df1a7422f36
rik61072@gmail.com <rik61072@gmail.com>
2022-10-27 00:31:37 +0530

    changes to the test

--- listenbrainz/webserver/static/js/tests/personal-recommendations/PersonalRecommendationsModal.test.tsx
+++ listenbrainz/webserver/static/js/tests/personal-recommendations/PersonalRecommendationsModal.test.tsx
@@ -14,7 +14,6 @@ const recordingToPersonallyRecommend: Listen = {
       release_mbid: "release_mbid",
       recording_msid: "recording_msid",
       recording_mbid: "recording_mbid",
-      artist_msid: "artist_msid",
     },
   },
 };
