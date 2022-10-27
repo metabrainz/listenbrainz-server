461743d319efb82be4370ca327c0bb162c123234
Kartik Ohri <kartikohri13@gmail.com>
2022-10-27 17:00:46 +0530

    Add limit for max similar recordings

--- listenbrainz_spark/recording_similarity/recording.py
+++ listenbrainz_spark/recording_similarity/recording.py
@@ -11,11 +11,11 @@ from listenbrainz_spark.utils import get_listens_from_new_dump
 RECORDINGS_PER_MESSAGE = 1000
 
 
-def build_sessioned_index(listen_table, mbc_table, session, threshold):
+def build_sessioned_index(listen_table, mbc_table, session, threshold, limit):
     # TODO: Handle case of unmatched recordings breaking sessions!
     #  Detect and remove skips!
     return f"""
-        WITH listens AS (
+            WITH listens AS (
                  SELECT user_id
                       , BIGINT(listened_at)
                       , CAST(COALESCE(recording_data.length / 1000, 180) AS BIGINT) AS duration
@@ -45,17 +45,39 @@ def build_sessioned_index(listen_table, mbc_table, session, threshold):
                   JOIN sessions s2
                  USING (user_id, session_id)
                  WHERE s1.recording_mbid != s2.recording_mbid
-            ) SELECT lexical_mbid0 AS mbid0
-                   , lexical_mbid1 AS mbid1
-                   , COUNT(*) AS score
-                FROM grouped_mbids
-            GROUP BY lexical_mbid0
-                   , lexical_mbid1
-              HAVING score > {threshold}
+            ), thresholded_mbids AS (
+                SELECT lexical_mbid0 AS mbid0
+                     , lexical_mbid1 AS mbid1
+                     , COUNT(*) AS score
+                  FROM grouped_mbids
+              GROUP BY lexical_mbid0
+                     , lexical_mbid1
+                HAVING score > {threshold}
+            ), ranked_mbids AS (
+                SELECT mbid0
+                     , mbid1
+                     , score
+                     , rank() OVER w AS rank
+                  FROM thresholded_mbids
+                WINDOW w AS (PARTITION BY mbid0 ORDER BY score DESC)     
+            )   SELECT mbid0
+                     , mbid1
+                     , score
+                  FROM ranked_mbids
+                 WHERE rank <= {limit}   
     """
 
 
-def main(days, session, threshold):
+def main(days, session, threshold, limit):
+    """ Generate similar recordings based on user listening sessions.
+
+    Args:
+        days: the number of days of listens to consider for calculating listening sessions
+        session: the max time difference between two listens in a listening session
+        threshold: the minimum similarity score for two recordings to be considered similar
+        limit: the maximum number of similar recordings to request for a given recording
+            (this limit is instructive only, upto 2x number of recordings may be returned)
+    """
     to_date = datetime.combine(date.today(), time.min)
     from_date = to_date + timedelta(days=-days)
 
@@ -67,10 +89,10 @@ def main(days, session, threshold):
     metadata_df = listenbrainz_spark.sql_context.read.json(config.HDFS_CLUSTER_URI + "/mb_metadata_cache.jsonl")
     metadata_df.createOrReplaceTempView(metadata_table)
 
-    query = build_sessioned_index(table, metadata_table, session, threshold)
+    query = build_sessioned_index(table, metadata_table, session, threshold, limit)
     data = run_query(query).toLocalIterator()
 
-    algorithm = f"session_based_days_{days}_session_{session}_threshold_{threshold}"
+    algorithm = f"session_based_days_{days}_session_{session}_threshold_{threshold}_limit_{limit}"
 
     for entries in chunked(data, RECORDINGS_PER_MESSAGE):
         items = [row.asDict() for row in entries]
