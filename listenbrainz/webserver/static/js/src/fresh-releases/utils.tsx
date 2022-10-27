4adcb9fe269bac02c8df520786f3d1a520e74fc1
Chinmay Kunkikar <chinmay.kunkikar@gmail.com>
2022-10-27 20:07:50 +0530

    FreshReleases: Improve responsiveness for mobile and tablets

--- listenbrainz/webserver/static/js/src/fresh-releases/utils.tsx
+++ listenbrainz/webserver/static/js/src/fresh-releases/utils.tsx
@@ -1,4 +1,6 @@
-export default function formattedReleaseDate(releaseDate: string) {
+import { useEffect, useState } from "react";
+
+export function formattedReleaseDate(releaseDate: string) {
   return new Intl.DateTimeFormat("en-US", {
     month: "short",
     day: "numeric",
@@ -7,4 +9,31 @@ export default function formattedReleaseDate(releaseDate: string) {
     .reverse()
     .map((date_parts) => date_parts.value)
     .join("");
+}
+
+// Originally from https://usehooks-ts.com/react-hook/use-media-query
+export function useMediaQuery(query: string) {
+  const getMatches = (query: string): boolean => {
+    if (typeof window !== "undefined") {
+      return window.matchMedia(query).matches;
+    }
+    return false;
+  };
+
+  const [matches, setMatches] = useState<boolean>(getMatches(query));
+
+  function handleChange() {
+    setMatches(getMatches(query));
+  }
+
+  useEffect(() => {
+    const matchMedia = window.matchMedia(query);
+    handleChange();
+    matchMedia.addEventListener("change", handleChange);
+    return () => {
+      matchMedia.removeEventListener("change", handleChange);
+    };
+  }, [query]);
+
+  return matches;
 }
