48bae7c4a4cea1fc30b2bd0dd070ca3234408171
Chinmay Kunkikar <chinmay.kunkikar@gmail.com>
2022-10-27 20:34:46 +0530

    ReleaseTimeline: Remove unneeded code
    We got rid of the dates labels in the responsiveness commit.
    There was still so much related unneeded code lying around in the file.

--- listenbrainz/webserver/static/js/src/fresh-releases/ReleaseTimeline.tsx
+++ listenbrainz/webserver/static/js/src/fresh-releases/ReleaseTimeline.tsx
@@ -10,26 +10,10 @@ type ReleaseTimelineProps = {
 export default function ReleaseTimeline(props: ReleaseTimelineProps) {
   const { releases } = props;
 
-  const minDate = releases[0].release_date;
-  const maxDate = releases[releases.length - 1].release_date;
-
-  const [minSliderDate, setMinSliderDate] = React.useState<number>(0);
-  const [maxSliderDate, setMaxSliderDate] = React.useState<number>(100);
   const [currentValue, setCurrentValue] = React.useState<number>();
   const [marks, setMarks] = React.useState<{ [key: number]: string }>({});
 
-  const screenMd = useMediaQuery("(max-width: 992px)");
-
-  function getDatesInRange(startDate: any, endDate: any) {
-    const date = new Date(startDate.getTime());
-    const dates = [];
-    while (date <= endDate) {
-      dates.push(new Date(date).toLocaleDateString("en-US"));
-      date.setDate(date.getDate() + 1);
-    }
-    setMinSliderDate(new Date(dates[0]).getTime());
-    setMaxSliderDate(new Date(dates[dates.length - 1]).getTime());
-  }
+  const screenMd = useMediaQuery("(max-width: 992px)"); // @screen-md
 
   // let's keep the type to any until we figure out a proper one
   const changeHandler = React.useCallback((percent: any) => {
@@ -68,28 +52,20 @@ export default function ReleaseTimeline(props: ReleaseTimelineProps) {
   }
 
   React.useEffect(() => {
-    getDatesInRange(new Date(minDate), new Date(maxDate));
     setMarks(createMarks(releases));
   }, [releases]);
 
-  function renderSlider() {
-    if (minSliderDate && maxSliderDate) {
-      return (
-        <div className="slider-container">
-          <Slider
-            className={screenMd ? "slider-horizontal" : "slider-vertical"}
-            vertical={!screenMd}
-            reverse={!screenMd}
-            included={false}
-            marks={marks}
-            value={currentValue}
-            onChange={changeHandler}
-          />
-        </div>
-      );
-    }
-    return <div className="text-muted">Couldn&apos;t load timeline</div>;
-  }
-
-  return renderSlider();
+  return (
+    <div className="slider-container">
+      <Slider
+        className={screenMd ? "slider-horizontal" : "slider-vertical"}
+        vertical={!screenMd}
+        reverse={!screenMd}
+        included={false}
+        marks={marks}
+        value={currentValue}
+        onChange={changeHandler}
+      />
+    </div>
+  );
 }
