5b88ff9f5081e730b28d7797b7a4740a8151c0ec
Robert Kaye <rob@musicbrainz.org>
2022-11-08 12:45:16 +0100

    Force the art page to be served to art.lb.org

--- listenbrainz/webserver/views/art.py
+++ listenbrainz/webserver/views/art.py
@@ -2,7 +2,7 @@ from flask import Flask, render_template, Blueprint
 
 art_bp = Blueprint('art', __name__)
 
-@art_bp.route("/")
+@art_bp.route("/", subdomain="art")
 def index():
     """ This page shows of a bit of what can be done with the cover art, as a sort of showcase. """
     return render_template("art/index.html")
