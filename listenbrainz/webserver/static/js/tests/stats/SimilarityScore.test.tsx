99bcc34bf4400fc49b87ae4d4e027c9413d8d5f4
Monkey Do <contact@monkeydo.digital>
2022-11-15 17:28:39 +0100

    React 18: Adapt SimilarityScore tests

--- listenbrainz/webserver/static/js/tests/stats/SimilarityScore.test.tsx
+++ listenbrainz/webserver/static/js/tests/stats/SimilarityScore.test.tsx
@@ -1,7 +1,10 @@
 import * as React from "react";
-import { mount } from "enzyme";
+import { mount, ReactWrapper } from "enzyme";
 
-import SimilarityScore, { SimilarityScoreProps } from "../../src/stats/SimilarityScore";
+import SimilarityScore, {
+  SimilarityScoreProps,
+} from "../../src/stats/SimilarityScore";
+import { waitForComponentToPaint } from "../test-utils";
 
 const props: SimilarityScoreProps = {
   similarityScore: 0.239745792,
@@ -10,28 +13,42 @@ const props: SimilarityScoreProps = {
 };
 
 describe("SimilarityScore", () => {
+  let wrapper: ReactWrapper<any, any, any> | undefined;
+  beforeEach(() => {
+    wrapper = undefined;
+  });
+  afterEach(() => {
+    if (wrapper) {
+      /* Unmount the wrapper at the end of each test, otherwise react-dom throws errors
+        related to async lifecycle methods run against a missing dom 'document'.
+        See https://github.com/facebook/react/issues/15691
+      */
+      wrapper.unmount();
+    }
+  });
   it("renders correctly for type = 'regular'", () => {
-    const wrapper = mount<SimilarityScoreProps>(<SimilarityScore {...props} />);
+    wrapper = mount<SimilarityScoreProps>(<SimilarityScore {...props} />);
     expect(wrapper).toMatchSnapshot();
   });
 
   it("renders correctly for type = 'compact'", () => {
-    const wrapper = mount<SimilarityScoreProps>(
+    wrapper = mount<SimilarityScoreProps>(
       <SimilarityScore {...{ ...props, type: "compact" }} />
     );
 
     expect(wrapper).toMatchSnapshot();
   });
 
-  it("updates the class name based on similiarty score", () => {
+  it("updates the class name based on similiarty score", async () => {
     /* sets class red for score 0.2 */
-    let wrapper = mount<SimilarityScoreProps>(<SimilarityScore {...props} />);
+    wrapper = mount<SimilarityScoreProps>(<SimilarityScore {...props} />);
     expect(wrapper.find(".progress").childAt(0).hasClass("red")).toEqual(true);
 
     /* sets class orange for score 0.5 */
     wrapper = mount<SimilarityScoreProps>(
       <SimilarityScore {...{ ...props, similarityScore: 0.57457 }} />
     );
+    await waitForComponentToPaint(wrapper);
     expect(wrapper.find(".progress").childAt(0).hasClass("orange")).toEqual(
       true
     );
@@ -40,6 +57,7 @@ describe("SimilarityScore", () => {
     wrapper = mount<SimilarityScoreProps>(
       <SimilarityScore {...{ ...props, similarityScore: 0.945792 }} />
     );
+    await waitForComponentToPaint(wrapper);
     expect(wrapper.find(".progress").childAt(0).hasClass("purple")).toEqual(
       true
     );
