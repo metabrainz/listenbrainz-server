dda914a32e94d7b23633d935e8450c2f3b42a4dd
Monkey Do <contact@monkeydo.digital>
2022-11-15 17:29:57 +0100

    React 18: Adapt UserDailyActivity tests
    Waiting for the shoe to drop…

--- listenbrainz/webserver/static/js/tests/stats/UserDailyActivity.test.tsx
+++ listenbrainz/webserver/static/js/tests/stats/UserDailyActivity.test.tsx
@@ -1,5 +1,5 @@
 import * as React from "react";
-import { mount, shallow } from "enzyme";
+import { mount } from "enzyme";
 
 import UserDailyActivity, {
   UserDailyActivityProps,
@@ -21,8 +21,21 @@ const props: UserDailyActivityProps = {
 Date.prototype.getTimezoneOffset = () => -330;
 
 describe("UserDailyActivity", () => {
+  let wrapper: ReactWrapper<any, any, any> | undefined;
+  beforeEach(() => {
+    wrapper = undefined;
+  });
+  afterEach(() => {
+    if (wrapper) {
+      /* Unmount the wrapper at the end of each test, otherwise react-dom throws errors
+        related to async lifecycle methods run against a missing dom 'document'.
+        See https://github.com/facebook/react/issues/15691
+      */
+      wrapper.unmount();
+    }
+  });
   it("renders correctly", () => {
-    const wrapper = shallow<UserDailyActivity>(
+    wrapper = mount<UserDailyActivity>(
       <UserDailyActivity {...{ ...props, range: "all_time" }} />
     );
 
@@ -37,205 +50,186 @@ describe("UserDailyActivity", () => {
   });
 
   it("renders corectly when range is invalid", () => {
-    const wrapper = mount<UserDailyActivity>(<UserDailyActivity {...props} />);
+    wrapper = mount<UserDailyActivity>(<UserDailyActivity {...props} />);
 
     wrapper.setProps({ range: "invalid_range" as UserStatsAPIRange });
     wrapper.update();
 
     expect(wrapper).toMatchSnapshot();
   });
-});
+  describe("componentDidMount", () => {
+    it('adds event listener for "resize" event', () => {
+      wrapper = mount<UserDailyActivity>(<UserDailyActivity {...props} />);
+      const instance = wrapper.instance();
 
-describe("componentDidMount", () => {
-  it('adds event listener for "resize" event', () => {
-    const wrapper = shallow<UserDailyActivity>(
-      <UserDailyActivity {...props} />
-    );
-    const instance = wrapper.instance();
+      const spy = jest.spyOn(window, "addEventListener");
+      spy.mockImplementationOnce(() => {});
+      instance.handleResize = jest.fn();
+      instance.componentDidMount();
 
-    const spy = jest.spyOn(window, "addEventListener");
-    spy.mockImplementationOnce(() => {});
-    instance.handleResize = jest.fn();
-    instance.componentDidMount();
+      expect(spy).toHaveBeenCalledWith("resize", instance.handleResize);
+    });
 
-    expect(spy).toHaveBeenCalledWith("resize", instance.handleResize);
-  });
+    it('calls "handleResize" once', () => {
+      wrapper = mount<UserDailyActivity>(<UserDailyActivity {...props} />);
+      const instance = wrapper.instance();
 
-  it('calls "handleResize" once', () => {
-    const wrapper = shallow<UserDailyActivity>(
-      <UserDailyActivity {...props} />
-    );
-    const instance = wrapper.instance();
+      instance.handleResize = jest.fn();
+      instance.componentDidMount();
 
-    instance.handleResize = jest.fn();
-    instance.componentDidMount();
-
-    expect(instance.handleResize).toHaveBeenCalledTimes(1);
-  });
-});
-
-describe("componentDidUpdate", () => {
-  it("it sets correct state if range is incorrect", () => {
-    const wrapper = shallow<UserDailyActivity>(
-      <UserDailyActivity {...props} />
-    );
-
-    wrapper.setProps({ range: "invalid_range" as UserStatsAPIRange });
-    wrapper.update();
-
-    expect(wrapper.state()).toMatchObject({
-      loading: false,
-      hasError: true,
-      errorMessage: "Invalid range: invalid_range",
+      expect(instance.handleResize).toHaveBeenCalledTimes(1);
     });
   });
 
-  it("calls loadData once if range is valid", () => {
-    const wrapper = shallow<UserDailyActivity>(
-      <UserDailyActivity {...props} />
-    );
-    const instance = wrapper.instance();
+  describe("componentDidUpdate", () => {
+    it("it sets correct state if range is incorrect", () => {
+      wrapper = mount<UserDailyActivity>(<UserDailyActivity {...props} />);
 
-    instance.loadData = jest.fn();
-    wrapper.setProps({ range: "month" });
-    wrapper.update();
+      wrapper.setProps({ range: "invalid_range" as UserStatsAPIRange });
+      wrapper.update();
 
-    expect(instance.loadData).toHaveBeenCalledTimes(1);
-  });
-});
+      expect(wrapper.state()).toMatchObject({
+        loading: false,
+        hasError: true,
+        errorMessage: "Invalid range: invalid_range",
+      });
+    });
 
-describe("componentWillUnmount", () => {
-  it('removes event listener for "resize" event', () => {
-    const wrapper = shallow<UserDailyActivity>(
-      <UserDailyActivity {...props} />
-    );
-    const instance = wrapper.instance();
+    it("calls loadData once if range is valid", () => {
+      wrapper = mount<UserDailyActivity>(<UserDailyActivity {...props} />);
+      const instance = wrapper.instance();
 
-    const spy = jest.spyOn(window, "removeEventListener");
-    spy.mockImplementationOnce(() => {});
-    instance.handleResize = jest.fn();
-    instance.componentWillUnmount();
+      instance.loadData = jest.fn();
+      wrapper.setProps({ range: "month" });
+      wrapper.update();
 
-    expect(spy).toHaveBeenCalledWith("resize", instance.handleResize);
-  });
-});
-
-describe("getData", () => {
-  it("calls getUserDailyActivity with correct params", async () => {
-    const wrapper = shallow<UserDailyActivity>(
-      <UserDailyActivity {...props} />
-    );
-    const instance = wrapper.instance();
-
-    const spy = jest.spyOn(instance.APIService, "getUserDailyActivity");
-    spy.mockImplementation(() =>
-      Promise.resolve(userDailyActivityResponse as UserDailyActivityResponse)
-    );
-    const result = await instance.getData();
-
-    expect(spy).toHaveBeenCalledWith("foobar", "week");
-    expect(result).toEqual(userDailyActivityResponse);
-  });
-
-  it("sets state correctly if data is not calculated", async () => {
-    const wrapper = shallow<UserDailyActivity>(
-      <UserDailyActivity {...props} />
-    );
-    const instance = wrapper.instance();
-
-    const spy = jest.spyOn(instance.APIService, "getUserDailyActivity");
-    const noContentError = new APIError("NO CONTENT");
-    noContentError.response = {
-      status: 204,
-    } as Response;
-    spy.mockImplementation(() => Promise.reject(noContentError));
-    await instance.getData();
-
-    expect(wrapper.state()).toMatchObject({
-      loading: false,
-      hasError: true,
-      errorMessage: "Statistics for the user have not been calculated",
+      expect(instance.loadData).toHaveBeenCalledTimes(1);
     });
   });
 
-  it("throws error", async () => {
-    const wrapper = shallow<UserDailyActivity>(
-      <UserDailyActivity {...props} />
-    );
-    const instance = wrapper.instance();
+  describe("componentWillUnmount", () => {
+    it('removes event listener for "resize" event', () => {
+      wrapper = mount<UserDailyActivity>(<UserDailyActivity {...props} />);
+      const instance = wrapper.instance();
 
-    const spy = jest.spyOn(instance.APIService, "getUserDailyActivity");
-    const notFoundError = new APIError("NOT FOUND");
-    notFoundError.response = {
-      status: 404,
-    } as Response;
-    spy.mockImplementation(() => Promise.reject(notFoundError));
+      const spy = jest.spyOn(window, "removeEventListener");
+      spy.mockImplementationOnce(() => {});
+      instance.handleResize = jest.fn();
+      instance.componentWillUnmount();
 
-    await expect(instance.getData()).rejects.toThrow("NOT FOUND");
-  });
-});
-
-describe("processData", () => {
-  it("processes data correctly for all_time", () => {
-    const wrapper = shallow<UserDailyActivity>(
-      <UserDailyActivity {...{ ...props, range: "all_time" }} />
-    );
-    const instance = wrapper.instance();
-
-    const result = instance.processData(
-      userDailyActivityResponse as UserDailyActivityResponse
-    );
-
-    // Sort results for stable comparison with test fixture
-    result.forEach((day) => {
-      day.data.sort((a, b) => (a.x as number) - (b.x as number));
+      expect(spy).toHaveBeenCalledWith("resize", instance.handleResize);
     });
-    expect(result).toEqual(userDailyActivityProcessedData);
-  });
-  it("returns an empty array if no payload", () => {
-    const wrapper = shallow<UserDailyActivity>(
-      <UserDailyActivity {...{ ...props, range: "all_time" }} />
-    );
-    const instance = wrapper.instance();
-
-    // When stats haven't been calculated, processData is called with an empty object
-    const result = instance.processData({} as UserDailyActivityResponse);
-
-    expect(result).toEqual([]);
-  });
-});
-
-describe("loadData", () => {
-  it("calls getData once", async () => {
-    const wrapper = shallow<UserDailyActivity>(
-      <UserDailyActivity {...props} />
-    );
-    const instance = wrapper.instance();
-
-    instance.getData = jest.fn();
-    instance.processData = jest.fn();
-    await instance.loadData();
-
-    expect(instance.getData).toHaveBeenCalledTimes(1);
   });
 
-  it("set state correctly", async () => {
-    const wrapper = shallow<UserDailyActivity>(
-      <UserDailyActivity {...props} />
-    );
-    const instance = wrapper.instance();
+  describe("getData", () => {
+    it("calls getUserDailyActivity with correct params", async () => {
+      wrapper = mount<UserDailyActivity>(<UserDailyActivity {...props} />);
+      const instance = wrapper.instance();
 
-    instance.getData = jest
-      .fn()
-      .mockImplementationOnce(() => Promise.resolve(userDailyActivityResponse));
-    await instance.loadData();
+      const spy = jest.spyOn(instance.APIService, "getUserDailyActivity");
+      spy.mockImplementation(() =>
+        Promise.resolve(userDailyActivityResponse as UserDailyActivityResponse)
+      );
+      const result = await instance.getData();
 
-    expect(wrapper.state().loading).toEqual(false);
-    const { data } = wrapper.state();
-    // Sort results for stable comparison with test fixture
-    data.forEach((day) => {
-      day.data.sort((a, b) => (a.x as number) - (b.x as number));
+      expect(spy).toHaveBeenCalledWith("foobar", "week");
+      expect(result).toEqual(userDailyActivityResponse);
     });
-    expect(data).toEqual(userDailyActivityProcessedData);
+
+    it("sets state correctly if data is not calculated", async () => {
+      wrapper = mount<UserDailyActivity>(<UserDailyActivity {...props} />);
+      const instance = wrapper.instance();
+
+      const spy = jest.spyOn(instance.APIService, "getUserDailyActivity");
+      const noContentError = new APIError("NO CONTENT");
+      noContentError.response = {
+        status: 204,
+      } as Response;
+      spy.mockImplementation(() => Promise.reject(noContentError));
+      await instance.getData();
+
+      expect(wrapper.state()).toMatchObject({
+        loading: false,
+        hasError: true,
+        errorMessage: "Statistics for the user have not been calculated",
+      });
+    });
+
+    it("throws error", async () => {
+      wrapper = mount<UserDailyActivity>(<UserDailyActivity {...props} />);
+      const instance = wrapper.instance();
+
+      const spy = jest.spyOn(instance.APIService, "getUserDailyActivity");
+      const notFoundError = new APIError("NOT FOUND");
+      notFoundError.response = {
+        status: 404,
+      } as Response;
+      spy.mockImplementation(() => Promise.reject(notFoundError));
+
+      await expect(instance.getData()).rejects.toThrow("NOT FOUND");
+    });
+  });
+
+  describe("processData", () => {
+    it("processes data correctly for all_time", () => {
+      wrapper = mount<UserDailyActivity>(
+        <UserDailyActivity {...{ ...props, range: "all_time" }} />
+      );
+      const instance = wrapper.instance();
+
+      const result = instance.processData(
+        userDailyActivityResponse as UserDailyActivityResponse
+      );
+
+      // Sort results for stable comparison with test fixture
+      result.forEach((day) => {
+        day.data.sort((a, b) => (a.x as number) - (b.x as number));
+      });
+      expect(result).toEqual(userDailyActivityProcessedData);
+    });
+    it("returns an empty array if no payload", () => {
+      wrapper = mount<UserDailyActivity>(
+        <UserDailyActivity {...{ ...props, range: "all_time" }} />
+      );
+      const instance = wrapper.instance();
+
+      // When stats haven't been calculated, processData is called with an empty object
+      const result = instance.processData({} as UserDailyActivityResponse);
+
+      expect(result).toEqual([]);
+    });
+  });
+
+  describe("loadData", () => {
+    it("calls getData once", async () => {
+      wrapper = mount<UserDailyActivity>(<UserDailyActivity {...props} />);
+      const instance = wrapper.instance();
+
+      instance.getData = jest.fn();
+      instance.processData = jest.fn();
+      await instance.loadData();
+
+      expect(instance.getData).toHaveBeenCalledTimes(1);
+    });
+
+    it("set state correctly", async () => {
+      wrapper = mount<UserDailyActivity>(<UserDailyActivity {...props} />);
+      const instance = wrapper.instance();
+
+      instance.getData = jest
+        .fn()
+        .mockImplementationOnce(() =>
+          Promise.resolve(userDailyActivityResponse)
+        );
+      await instance.loadData();
+
+      expect(wrapper.state().loading).toEqual(false);
+      const { data } = wrapper.state();
+      // Sort results for stable comparison with test fixture
+      data.forEach((day) => {
+        day.data.sort((a, b) => (a.x as number) - (b.x as number));
+      });
+      expect(data).toEqual(userDailyActivityProcessedData);
+    });
   });
 });
