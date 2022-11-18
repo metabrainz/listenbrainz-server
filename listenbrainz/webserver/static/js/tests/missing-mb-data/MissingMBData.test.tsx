c234e06082d4d685f2e6d47e003e44cecce746f9
Monkey Do <contact@monkeydo.digital>
2022-11-18 14:25:22 +0100

    React18: Adapt MissingMBData page tests

--- listenbrainz/webserver/static/js/tests/missing-mb-data/MissingMBData.test.tsx
+++ listenbrainz/webserver/static/js/tests/missing-mb-data/MissingMBData.test.tsx
@@ -1,16 +1,16 @@
 import * as React from "react";
 import { mount } from "enzyme";
 
+import { act } from "react-dom/test-utils";
 import * as missingDataProps from "../__mocks__/missingMBDataProps.json";
-import {
-  youtube,
-  spotify,
-  user,
-} from "../__mocks__/missingMBDataProps.json";
+import { youtube, spotify, user } from "../__mocks__/missingMBDataProps.json";
 
 import MissingMBDataPage from "../../src/missing-mb-data/MissingMBData";
-import GlobalAppContext, { GlobalAppContextT } from "../../src/utils/GlobalAppContext";
+import GlobalAppContext, {
+  GlobalAppContextT,
+} from "../../src/utils/GlobalAppContext";
 import APIService from "../../src/utils/APIService";
+import { waitForComponentToPaint } from "../test-utils";
 
 // Font Awesome generates a random hash ID for each icon everytime.
 // // Mocking Math.random() fixes this
@@ -42,118 +42,128 @@ describe("MissingMBDataPage", () => {
       </GlobalAppContext.Provider>
     );
     expect(wrapper.html()).toMatchSnapshot();
-  });
-});
-
-describe("handleClickPrevious", () => {
-  beforeAll(() => {
-    window.HTMLElement.prototype.scrollIntoView = jest.fn();
-  });
-  it("doesn't do anything if on the first page", async () => {
-    const wrapper = mount<MissingMBDataPage>(
-      <GlobalAppContext.Provider
-        value={{ ...mountOptions.context, currentUser: props.user }}
-      >
-        <MissingMBDataPage {...props} />
-      </GlobalAppContext.Provider>
-    );
-    const instance = wrapper.instance();
-    instance.afterDisplay = jest.fn();
-
-    instance.handleClickPrevious();
-
-    expect(wrapper.state("loading")).toBeFalsy();
-    expect(wrapper.state("currPage")).toEqual(1);
-    expect(wrapper.state("totalPages")).toEqual(3);
-    expect(wrapper.state("missingData")).toEqual(
-      props.missingData.slice(0, 25)
-    );
-    expect(instance.afterDisplay).toHaveBeenCalledTimes(0);
+    wrapper.unmount();
   });
 
-  it("goes to previous (first) page when on second page", () => {
-    const wrapper = mount<MissingMBDataPage>(
-      <GlobalAppContext.Provider
-        value={{ ...mountOptions.context, currentUser: props.user }}
-      >
-        <MissingMBDataPage {...props} />
-      </GlobalAppContext.Provider>
-    );
-    const instance = wrapper.instance();
-    const afterDisplaySpy = jest.spyOn(instance, "afterDisplay");
+  describe("handleClickPrevious", () => {
+    beforeAll(() => {
+      window.HTMLElement.prototype.scrollIntoView = jest.fn();
+    });
+    it("doesn't do anything if on the first page", async () => {
+      const wrapper = mount<MissingMBDataPage>(
+        <GlobalAppContext.Provider
+          value={{ ...mountOptions.context, currentUser: props.user }}
+        >
+          <MissingMBDataPage {...props} />
+        </GlobalAppContext.Provider>
+      );
+      const instance = wrapper.instance();
+      instance.afterDisplay = jest.fn();
+      await act(() => {
+        instance.handleClickPrevious();
+      });
 
-    wrapper.setState({
-      currPage: 2,
-      missingData: props.missingData.slice(25, 50),
+      expect(wrapper.state("loading")).toBeFalsy();
+      expect(wrapper.state("currPage")).toEqual(1);
+      expect(wrapper.state("totalPages")).toEqual(3);
+      expect(wrapper.state("missingData")).toEqual(
+        props.missingData.slice(0, 25)
+      );
+      expect(instance.afterDisplay).toHaveBeenCalledTimes(0);
+      wrapper.unmount();
     });
 
-    instance.handleClickPrevious();
+    it("goes to previous (first) page when on second page", async () => {
+      const wrapper = mount<MissingMBDataPage>(
+        <GlobalAppContext.Provider
+          value={{ ...mountOptions.context, currentUser: props.user }}
+        >
+          <MissingMBDataPage {...props} />
+        </GlobalAppContext.Provider>
+      );
+      const instance = wrapper.instance();
+      const afterDisplaySpy = jest.spyOn(instance, "afterDisplay");
+      await act(() => {
+        wrapper.setState({
+          currPage: 2,
+          missingData: props.missingData.slice(25, 50),
+        });
+      });
 
-    expect(wrapper.state("loading")).toBeFalsy();
-    expect(wrapper.state("currPage")).toEqual(1);
-    expect(wrapper.state("totalPages")).toEqual(3);
-    expect(wrapper.state("missingData")).toEqual(
-      props.missingData.slice(0, 25)
-    );
-    expect(afterDisplaySpy).toHaveBeenCalledTimes(1);
+      await act(() => {
+        instance.handleClickPrevious();
+      });
+
+      expect(wrapper.state("loading")).toBeFalsy();
+      expect(wrapper.state("currPage")).toEqual(1);
+      expect(wrapper.state("totalPages")).toEqual(3);
+      expect(wrapper.state("missingData")).toEqual(
+        props.missingData.slice(0, 25)
+      );
+      expect(afterDisplaySpy).toHaveBeenCalledTimes(1);
+      wrapper.unmount();
+    });
   });
-});
 
-describe("handleClickNext", () => {
-  beforeAll(() => {
-    window.HTMLElement.prototype.scrollIntoView = jest.fn();
-  });
-  it("doesn't do anything if on the last page", async () => {
-    const wrapper = mount<MissingMBDataPage>(
-      <GlobalAppContext.Provider
-        value={{ ...mountOptions.context, currentUser: props.user }}
-      >
-        <MissingMBDataPage {...props} />
-      </GlobalAppContext.Provider>
-    );
-    const instance = wrapper.instance();
-    instance.afterDisplay = jest.fn();
+  describe("handleClickNext", () => {
+    beforeAll(() => {
+      window.HTMLElement.prototype.scrollIntoView = jest.fn();
+    });
+    it("doesn't do anything if on the last page", async () => {
+      const wrapper = mount<MissingMBDataPage>(
+        <GlobalAppContext.Provider
+          value={{ ...mountOptions.context, currentUser: props.user }}
+        >
+          <MissingMBDataPage {...props} />
+        </GlobalAppContext.Provider>
+      );
+      const instance = wrapper.instance();
+      instance.afterDisplay = jest.fn();
 
-    wrapper.setState({
-      currPage: 3,
-      missingData: props.missingData.slice(50, 73),
+      await act(() => {
+        wrapper.setState({
+          currPage: 3,
+          missingData: props.missingData.slice(50, 73),
+        });
+      });
+
+      await act(() => {
+        instance.handleClickNext();
+      });
+
+      expect(wrapper.state("loading")).toBeFalsy();
+      expect(wrapper.state("currPage")).toEqual(3);
+      expect(wrapper.state("totalPages")).toEqual(3);
+      expect(wrapper.state("missingData")).toEqual(
+        props.missingData.slice(50, 73)
+      );
+      expect(instance.afterDisplay).toHaveBeenCalledTimes(0);
+      wrapper.unmount();
     });
 
-    instance.handleClickNext();
+    it("goes to next page when on first page", async () => {
+      const wrapper = mount<MissingMBDataPage>(
+        <GlobalAppContext.Provider
+          value={{ ...mountOptions.context, currentUser: props.user }}
+        >
+          <MissingMBDataPage {...props} />
+        </GlobalAppContext.Provider>
+      );
+      const instance = wrapper.instance();
+      const afterDisplaySpy = jest.spyOn(instance, "afterDisplay");
 
-    expect(wrapper.state("loading")).toBeFalsy();
-    expect(wrapper.state("currPage")).toEqual(3);
-    expect(wrapper.state("totalPages")).toEqual(3);
-    expect(wrapper.state("missingData")).toEqual(
-      props.missingData.slice(50, 73)
-    );
-    expect(instance.afterDisplay).toHaveBeenCalledTimes(0);
-  });
+      await act(() => {
+        instance.handleClickNext();
+      });
 
-  it("goes to next page when on first page", () => {
-    const wrapper = mount<MissingMBDataPage>(
-      <GlobalAppContext.Provider
-        value={{ ...mountOptions.context, currentUser: props.user }}
-      >
-        <MissingMBDataPage {...props} />
-      </GlobalAppContext.Provider>
-    );
-    const instance = wrapper.instance();
-    const afterDisplaySpy = jest.spyOn(instance, "afterDisplay");
-
-    // wrapper.setState({
-    // currPage: 2,
-    // missingData: props.missingData.slice(25, 50),
-    // });
-
-    instance.handleClickNext();
-
-    expect(wrapper.state("loading")).toBeFalsy();
-    expect(wrapper.state("currPage")).toEqual(2);
-    expect(wrapper.state("totalPages")).toEqual(3);
-    expect(wrapper.state("missingData")).toEqual(
-      props.missingData.slice(25, 50)
-    );
-    expect(afterDisplaySpy).toHaveBeenCalledTimes(1);
+      expect(wrapper.state("loading")).toBeFalsy();
+      expect(wrapper.state("currPage")).toEqual(2);
+      expect(wrapper.state("totalPages")).toEqual(3);
+      expect(wrapper.state("missingData")).toEqual(
+        props.missingData.slice(25, 50)
+      );
+      expect(afterDisplaySpy).toHaveBeenCalledTimes(1);
+      wrapper.unmount();
+    });
   });
 });
