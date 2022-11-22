2b2ff17f9d8269c95265f80a363ff92c7d3b2948
Kartik Ohri <kartikohri13@gmail.com>
2022-11-22 18:51:29 +0530

    Refactor resolve_redirect_mbids and resolve_canonical_mbids
    We do this resolving at multiple places so makes it sense to refactor it
    to one helper function.

--- listenbrainz/labs_api/labs/api/recording_from_recording_mbid.py
+++ listenbrainz/labs_api/labs/api/recording_from_recording_mbid.py
@@ -21,7 +21,7 @@ class RecordingFromRecordingMBIDQuery(Query):
 
     def outputs(self):
         return ['recording_mbid', 'recording_name', 'length', 'comment', 'artist_credit_id',
-                'artist_credit_name', '[artist_credit_mbids]', 'original_recording_mbid']
+                'artist_credit_name', '[artist_credit_mbids]', 'canonical_recording_mbid', 'original_recording_mbid']
 
     def fetch(self, params, offset=-1, count=-1):
         if not current_app.config["MB_DATABASE_URI"]:
--- listenbrainz/labs_api/labs/api/spotify/spotify_mbid_lookup.py
+++ listenbrainz/labs_api/labs/api/spotify/spotify_mbid_lookup.py
@@ -7,6 +7,7 @@ from flask import current_app
 from werkzeug.exceptions import BadRequest
 
 from listenbrainz.labs_api.labs.api.spotify.utils import lookup_using_metadata
+from listenbrainz.labs_api.labs.api.utils import resolve_redirect_mbids, resolve_canonical_mbids
 
 
 class SpotifyIdFromMBIDQuery(Query):
@@ -25,61 +26,7 @@ class SpotifyIdFromMBIDQuery(Query):
     def outputs(self):
         return ['recording_mbid', 'artist_name', 'release_name', 'track_name', 'spotify_track_ids']
 
-    def resolve_redirect_mbids(self, mbids):
-        """ Given a list of mbids, resolve redirects if any and return the list of new mbids and a dict of
-        redirected mbids.
-        """
-        query = """
-              WITH mbids (gid) AS (VALUES %s)
-            SELECT rgr.gid::TEXT AS old
-                 , r.gid::TEXT AS new
-              FROM recording_gid_redirect rgr
-              JOIN mbids m
-                ON rgr.gid = m.gid::UUID
-              JOIN recording r
-                ON r.id = rgr.new_id
-        """
-        with psycopg2.connect(current_app.config["MB_DATABASE_URI"]) as conn, conn.cursor() as curs:
-            execute_values(curs, query, [(mbid,) for mbid in mbids], page_size=len(mbids))
-            index = {row[0]: row[1] for row in curs.fetchall()}
-
-        redirected_mbids = []
-        for mbid in mbids:
-            # redirect the mbid to find the new recording if one exists otherwise use the original mbid itself
-            redirected_mbids.append(index.get(mbid, mbid))
-
-        return redirected_mbids, index
-
-    def resolve_canonical_mbids(self, mbids):
-        """ Check the list of mbids for canonical redirects and return list of canonical mbids.
-
-        Args:
-            mbids: list of mbids to check for canonical mbids
-
-        Returns:
-            tuple of (list of canonical mbids, dict of redirected mbids as key and the canonical mbid
-            replacing it as value)
-        """
-        query = """
-              WITH mbids (gid) AS (VALUES %s)
-            SELECT recording_mbid::TEXT AS old
-                 , canonical_recording_mbid::TEXT AS new
-              FROM mapping.canonical_recording_redirect
-              JOIN mbids
-                ON recording_mbid = gid::UUID
-        """
-        with psycopg2.connect(current_app.config["SQLALCHEMY_TIMESCALE_URI"]) as conn, conn.cursor() as curs:
-            execute_values(curs, query, [(mbid,) for mbid in mbids], page_size=len(mbids))
-            index = {row[0]: row[1] for row in curs.fetchall()}
-
-        canonical_mbids = []
-        for mbid in mbids:
-            # get the canonical mbid redirect if one exists otherwise the original mbid is the canonical one itself
-            canonical_mbids.append(index.get(mbid, mbid))
-
-        return canonical_mbids, index
-
-    def fetch_metadata_from_mbids(self, mbids):
+    def fetch_metadata_from_mbids(self, curs, mbids):
         """ Retrieve metadata from canonical tables for given mbids. Note that all mbids should be canonical mbids
         otherwise metadata may not be found. """
         query = """
@@ -92,18 +39,17 @@ class SpotifyIdFromMBIDQuery(Query):
         RIGHT JOIN mbids
                 ON recording_mbid = gid::UUID
         """
-        with psycopg2.connect(current_app.config["SQLALCHEMY_TIMESCALE_URI"]) as conn, conn.cursor() as curs:
-            execute_values(curs, query, [(mbid,) for mbid in mbids], page_size=len(mbids))
+        execute_values(curs, query, [(mbid,) for mbid in mbids], page_size=len(mbids))
 
-            metadata = {}
-            for row in curs.fetchall():
-                metadata[row[0]] = {
-                    "track_name": row[1],
-                    "artist_name": row[2],
-                    "release_name": row[3]
-                }
+        metadata = {}
+        for row in curs.fetchall():
+            metadata[row[0]] = {
+                "track_name": row[1],
+                "artist_name": row[2],
+                "release_name": row[3]
+            }
 
-            return metadata
+        return metadata
 
     def fetch(self, params, offset=-1, count=-1):
         mbids = []
@@ -114,10 +60,12 @@ class SpotifyIdFromMBIDQuery(Query):
                 raise BadRequest(f"Invalid recording mbid: {param['[recording_mbid]']}")
             mbids.append(param["[recording_mbid]"])
 
-        redirected_mbids, redirect_index = self.resolve_redirect_mbids(mbids)
-        canonical_mbids, canonical_index = self.resolve_canonical_mbids(redirected_mbids)
+        with psycopg2.connect(current_app.config["MB_DATABASE_URI"]) as conn, conn.cursor() as curs:
+            redirected_mbids, redirect_index, _ = resolve_redirect_mbids(curs, mbids)
 
-        metadata = self.fetch_metadata_from_mbids(canonical_mbids)
+        with psycopg2.connect(current_app.config["SQLALCHEMY_TIMESCALE_URI"]) as conn, conn.cursor() as curs:
+            canonical_mbids, canonical_index, _ = resolve_canonical_mbids(curs, redirected_mbids)
+            metadata = self.fetch_metadata_from_mbids(curs, canonical_mbids)
 
         ordered_metadata = []
         for mbid in mbids:
