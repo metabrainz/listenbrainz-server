64549b8dfb35b8364943228479d85144b862f412
Kartik Ohri <kartikohri13@gmail.com>
2022-11-28 20:44:35 +0530

    address feedback from review

--- listenbrainz_spark/similarity/artist.py
+++ listenbrainz_spark/similarity/artist.py
@@ -9,17 +9,18 @@ from listenbrainz_spark.utils import get_listens_from_new_dump
 
 
 RECORDINGS_PER_MESSAGE = 10000
+# the duration value in seconds to use for track whose duration data in not available in MB
+DEFAULT_TRACK_LENGTH = 180
 
 
 def build_sessioned_index(listen_table, metadata_table, session, max_contribution, threshold, limit, _filter, skip_threshold):
     # TODO: Handle case of unmatched recordings breaking sessions!
-    #  Detect and remove skips!
     filter_artist_credit = "AND s1.artist_credit_id != s2.artist_credit_id" if _filter else ""
     return f"""
             WITH listens AS (
                  SELECT user_id
                       , BIGINT(listened_at)
-                      , CAST(COALESCE(recording_data.length / 1000, 180) AS BIGINT) AS duration
+                      , CAST(COALESCE(recording_data.length / 1000, {DEFAULT_TRACK_LENGTH}) AS BIGINT) AS duration
                       , artist_credit_id
                       , explode(artist_mbids) AS artist_mbid
                    FROM {listen_table} l
