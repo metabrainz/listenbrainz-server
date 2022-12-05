bacb279dbfc09e207daa90e2427d488a3bcfeedb
Kartik Ohri <kartikohri13@gmail.com>
2022-12-05 22:35:44 +0530

    Fix YIM stats broken due to other refactorings

--- listenbrainz_spark/year_in_music/top_stats.py
+++ listenbrainz_spark/year_in_music/top_stats.py
@@ -16,4 +16,9 @@ def calculate_top_entity_stats(year):
             from_date, to_date, table, entity, "this_year", "year_in_music_top_stats"
         )
         for message in stats:
+            # yim stats are stored in postgres instead of couchdb so drop those messages for yim
+            if message["type"] == "couchdb_data_start" or message["type"] == "couchdb_data_end":
+                continue
+
+            message["year"] = year
             yield message
