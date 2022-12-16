a2a47a93bb17f3e2399024181b8036cd603bee1a
Kartik Ohri <kartikohri13@gmail.com>
2022-12-16 22:39:12 +0530

    fix format

--- listenbrainz_spark/year_in_music/tracks_of_the_year.py
+++ listenbrainz_spark/year_in_music/tracks_of_the_year.py
@@ -30,4 +30,4 @@ def calculate_tracks_of_the_year(year):
              , artist_credit_mbids
     """
 
-    run_query(query).write.format("csv").save(config.HDFS_CLUSTER_URI + "/tracks_of_the_year", mode="overwrite")
+    run_query(query).write.format("json").save(config.HDFS_CLUSTER_URI + "/tracks_of_the_year", mode="overwrite")
