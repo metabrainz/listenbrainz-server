bc9ef99ff8a152cb14b0c89798f8fcdef778628f
Kartik Ohri <kartikohri13@gmail.com>
2022-12-16 20:29:02 +0530

    some simplifications - 2

--- listenbrainz_spark/year_in_music/new_releases_of_top_artists.py
+++ listenbrainz_spark/year_in_music/new_releases_of_top_artists.py
@@ -1,3 +1,5 @@
+from datetime import datetime, date, time
+
 import listenbrainz_spark
 from data.model.new_releases_stat import NewReleasesStat
 from listenbrainz_spark import config
@@ -5,12 +7,12 @@ from listenbrainz_spark.path import RELEASE_GROUPS_YEAR_DATAFRAME
 from listenbrainz_spark.postgres.release_group import create_year_release_groups
 
 from listenbrainz_spark.stats import run_query
+from listenbrainz_spark.utils import get_all_listens_from_new_dump
 from listenbrainz_spark.year_in_music.utils import setup_listens_for_year
 
 
 def get_new_releases_of_top_artists(year):
-    setup_listens_for_year(year)
-
+    get_all_listens_from_new_dump().createOrReplaceTempView("listens")
     create_year_release_groups(year)
     listenbrainz_spark\
         .sql_context\
@@ -18,10 +20,9 @@ def get_new_releases_of_top_artists(year):
         .parquet(config.HDFS_CLUSTER_URI + RELEASE_GROUPS_YEAR_DATAFRAME)\
         .createOrReplaceTempView("release_groups_of_year")
 
-    run_query(_get_top_50_artists()).createOrReplaceTempView("top_50_artists")
-    new_releases = run_query(_get_new_releases_of_top_artists()).toLocalIterator()
+    new_releases = run_query(_get_new_releases_of_top_artists(year))
 
-    for entry in new_releases:
+    for entry in new_releases.toLocalIterator():
         data = entry.asDict(recursive=True)
         yield NewReleasesStat(
             type="year_in_music_new_releases_of_top_artists",
@@ -31,45 +32,40 @@ def get_new_releases_of_top_artists(year):
         ).dict(exclude_none=True)
 
 
-def _get_top_50_artists():
-    return """
-        WITH intermediate_table as (
+def _get_new_releases_of_top_artists(year):
+    start = datetime.combine(date(year, 1, 1), time.min)
+    end = datetime.combine(date(year, 12, 31), time.max)
+    return f"""
+        WITH artist_counts as (
             SELECT user_id
                  , artist_credit_mbids
                  , count(*) as listen_count
-              FROM listens_of_year
-             WHERE artist_credit_mbids IS NOT NULL
+              FROM listens
+             WHERE listened_at >= to_timestamp({start})
+               AND listened_at <= to_timestamp({end})
+               AND artist_credit_mbids IS NOT NULL
           GROUP BY user_id
                  , artist_credit_mbids
-        ), intermediate_table_2 AS (
+        ), top_artists AS (
             SELECT user_id
                  , artist_credit_mbids
-                 , listen_count
                  , row_number() OVER(PARTITION BY user_id ORDER BY listen_count DESC) AS row_number
-              FROM intermediate_table
+              FROM artist_counts
         )
             SELECT user_id
-                 , artist_credit_mbids
-                 , listen_count
-              FROM intermediate_table_2
+                 , collect_list(
+                        struct(
+                           rgoy.title
+                         , rgoy.artist_credit_name  
+                         , rgoy.release_group_mbid
+                         , rgoy.artist_credit_mbids
+                         , rgoy.caa_id
+                         , rgoy.caa_release_mbid
+                        )
+                    ) AS new_releases
+              FROM release_groups_of_year rgoy
+              JOIN top_artists t50a
+                ON cardinality(array_intersect(rgoy.artist_credit_mbids, t50a.artist_credit_mbids)) > 0
              WHERE row_number <= 50
-    """
-
-def _get_new_releases_of_top_artists():
-    return """
-        SELECT user_id
-             , collect_list(
-                    struct(
-                       rgoy.title
-                     , rgoy.artist_credit_name  
-                     , rgoy.release_group_mbid
-                     , rgoy.artist_credit_mbids
-                     , rgoy.caa_id
-                     , rgoy.caa_release_mbid
-                    )
-               ) AS new_releases
-          FROM release_groups_of_year rgoy
-          JOIN top_50_artists t50a
-            ON cardinality(array_intersect(rgoy.artist_credit_mbids, t50a.artist_credit_mbids)) > 0
-      GROUP BY user_id
+          GROUP BY user_id
     """
