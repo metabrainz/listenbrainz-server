9659e13b5fd7fece4129fad6da2871b5224fef24
Kartik Ohri <kartikohri13@gmail.com>
2022-12-17 13:55:34 +0530

    Update spark tests' testdata

--- listenbrainz_spark/stats/user/tests/__init__.py
+++ listenbrainz_spark/stats/user/tests/__init__.py
@@ -1,3 +1,5 @@
+from listenbrainz_spark.hdfs import upload_to_HDFS, delete_dir
+from listenbrainz_spark.path import RELEASE_METADATA_CACHE_DATAFRAME
 from listenbrainz_spark.tests import SparkNewTestCase
 
 
@@ -7,8 +9,13 @@ class StatsTestCase(SparkNewTestCase):
     def setUpClass(cls) -> None:
         super(StatsTestCase, cls).setUpClass()
         cls.upload_test_listens()
+        upload_to_HDFS(
+            RELEASE_METADATA_CACHE_DATAFRAME,
+            cls.path_to_data_file("release_data_cache.parquet")
+        )
 
     @classmethod
     def tearDownClass(cls) -> None:
         super(StatsTestCase, cls).tearDownClass()
         cls.delete_uploaded_listens()
+        delete_dir(RELEASE_METADATA_CACHE_DATAFRAME)
