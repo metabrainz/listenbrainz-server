6780876e0edf66588b45cbc7cc5783b129c6c041
Kartik Ohri <kartikohri13@gmail.com>
2022-12-19 16:12:59 +0530

    Add job to dump data for tracks of the year (#2292)
    * Add job to dump data for tracks of the year stat
    
    The tracks of the year playlist needs all tracks a user listened to in a given year. The query is too intensive to run on LB db directly. Run the query on spark cluster and save output to HDFS. Copy the files manually over to wolf.
    
    * fix query
    
    * fix query - 2
    
    * fix format
    
    * write 1 json file

---
+++ listenbrainz_spark/year_in_music/tracks_of_the_year.py
@@ -0,0 +1,37 @@
+from datetime import datetime, date, time
+
+from listenbrainz_spark import config
+from listenbrainz_spark.stats import run_query
+from listenbrainz_spark.utils import get_all_listens_from_new_dump
+
+
+def calculate_tracks_of_the_year(year):
+    """ Calculate all tracks a user has listened to in the given year. """
+    get_all_listens_from_new_dump().createOrReplaceTempView("listens")
+
+    start = datetime.combine(date(year, 1, 1), time.min)
+    end = datetime.combine(date(year, 12, 31), time.max)
+
+    query = f"""
+        SELECT user_id
+             , recording_name
+             , recording_mbid
+             , artist_name
+             , artist_credit_mbids
+             , count(*) AS listen_count
+          FROM listens
+         WHERE listened_at >= to_timestamp('{start}')
+           AND listened_at <= to_timestamp('{end}')
+           AND recording_mbid IS NOT NULL
+      GROUP BY user_id
+             , recording_name
+             , recording_mbid
+             , artist_name
+             , artist_credit_mbids
+    """
+
+    run_query(query)\
+        .repartition(1)\
+        .write\
+        .format("json")\
+        .save(config.HDFS_CLUSTER_URI + "/tracks_of_the_year", mode="overwrite")
