481fe53334d77678837b3fda3a65d94fccb1f1fa
Monkey Do <contact@monkeydo.digital>
2023-01-24 13:09:22 +0100

    Webserver endpoint for new taste page
    combine queries from previous pins and feedback endpoints into one
    Not sure how well all this is going to work for request arguments such as count and offset, they might need to be separated into separate arguments for feedback and pins.
    Offset and count for pins was hardcoded, so we're not losing any functionality currently as far as i can tell.

--- listenbrainz/webserver/views/user.py
+++ listenbrainz/webserver/views/user.py
@@ -62,8 +62,8 @@ redirect_bp.add_url_rule("/collaborations/", "redirect_collaborations",
 redirect_bp.add_url_rule("/recommendations/",
                          "redirect_recommendations",
                          redirect_user_page("user.recommendation_playlists"))
-redirect_bp.add_url_rule("/pins/", "redirect_pins",
-                         redirect_user_page("user.pins"))
+redirect_bp.add_url_rule("/taste/", "redirect_taste",
+                         redirect_user_page("user.taste"))
 redirect_bp.add_url_rule("/year-in-music/", "redirect_year_in_music",
                          redirect_user_page("user.year_in_music"))
 
@@ -360,36 +360,6 @@ def collaborations(user_name: str):
     )
 
 
-@user_bp.route("/<user_name>/pins/")
-def pins(user_name: str):
-    """ Show user pin history """
-
-    user = _get_user(user_name)
-    user_data = {
-        "name": user.musicbrainz_id,
-        "id": user.id,
-    }
-
-    pins = get_pin_history_for_user(user_id=user.id, count=25, offset=0)
-    pins = [pin.to_api() for pin in fetch_track_metadata_for_items(pins)]
-    total_count = get_pin_count_for_user(user_id=user.id)
-
-    props = {
-        "user": user_data,
-        "pins": pins,
-        "profile_url": url_for('user.profile', user_name=user_name),
-        "total_count": total_count,
-        "logged_in_user_follows_user": logged_in_user_follows_user(user),
-    }
-
-    return render_template(
-        "user/pins.html",
-        active_section="pins",
-        props=ujson.dumps(props),
-        user=user
-    )
-
-
 @user_bp.route("/<user_name>/report-user/", methods=['POST'])
 @api_login_required
 def report_abuse(user_name):
@@ -455,10 +425,11 @@ def logged_in_user_follows_user(user):
     return None
 
 
-@user_bp.route("/<user_name>/feedback/")
+@user_bp.route("/<user_name>/taste/")
 @web_listenstore_needed
-def feedback(user_name: str):
-    """ Show user feedback, with filter on score (love/hate).
+def taste(user_name: str):
+    """ Show user feedback(love/hate) and pins.
+    Feedback has filter on score (1 or -1).
 
     Args:
         musicbrainz_id (str): the MusicBrainz ID of the user
@@ -496,18 +467,25 @@ def feedback(user_name: str):
 
     feedback_count = get_feedback_count_for_user(user.id, score)
     feedback = get_feedback_for_user(user.id, count, offset, score, True)
+    
+    pins = get_pin_history_for_user(user_id=user.id, count=25, offset=0)
+    pins = [pin.to_api() for pin in fetch_track_metadata_for_items(pins)]
+    pin_count = get_pin_count_for_user(user_id=user.id)
 
     props = {
         "feedback": [f.to_api() for f in feedback],
         "feedback_count": feedback_count,
         "user": user_data,
-        "active_section": "feedback",
+        "active_section": "taste",
         "logged_in_user_follows_user": logged_in_user_follows_user(user),
+        "pins": pins,
+        "pin_count": pin_count,
+        "profile_url": url_for('user.profile', user_name=user_name),
     }
 
     return render_template(
-        "user/feedback.html",
-        active_section="feedback",
+        "user/taste.html",
+        active_section="taste",
         props=ujson.dumps(props),
         user=user
     )
