315dee738881ce13796a5f5fbb9ff21a634296cf
Monkey Do <contact@monkeydo.digital>
2023-02-14 11:42:52 +0100

    Remove Playlists page and count query params
    in the front-end page. With two navigations on the same page it doesn't make sense anymore to have those query parameters.

--- frontend/js/src/playlists/PlaylistsList.tsx
+++ frontend/js/src/playlists/PlaylistsList.tsx
@@ -47,58 +47,6 @@ export default class PlaylistsList extends React.Component<
     };
   }
 
-  componentDidMount(): void {
-    const { APIService } = this.context;
-    this.APIService = APIService;
-
-    // Listen to browser previous/next events and load page accordingly
-    window.addEventListener("popstate", this.handleURLChange);
-    // Call it once to allow navigating straight to a certain page
-    // The server route provides for this, but just in case…
-    // There's a check in handleURLChange to prevent wasting an API call.
-    this.handleURLChange();
-  }
-
-  componentWillUnmount() {
-    window.removeEventListener("popstate", this.handleURLChange);
-  }
-
-  handleURLChange = async (): Promise<void> => {
-    const url = new URL(window.location.href);
-    const { paginationOffset, playlistsPerPage } = this.state;
-    let offset = paginationOffset;
-    let count = playlistsPerPage;
-    if (url.searchParams.get("offset")) {
-      offset = Number(url.searchParams.get("offset"));
-    }
-    if (url.searchParams.get("count")) {
-      count = Number(url.searchParams.get("count"));
-    }
-    if (offset === paginationOffset && count === playlistsPerPage) {
-      // Nothing changed
-      return;
-    }
-
-    this.setState({ loading: true });
-    const { user, activeSection, newAlert } = this.props;
-    const { currentUser } = this.context;
-    try {
-      const newPlaylists = await this.APIService.getUserPlaylists(
-        user.name,
-        currentUser?.auth_token,
-        offset,
-        count,
-        activeSection === "recommendations",
-        activeSection === "collaborations"
-      );
-
-      this.handleAPIResponse(newPlaylists, false);
-    } catch (error) {
-      newAlert("danger", "Error loading playlists", error?.message ?? error);
-      this.setState({ loading: false });
-    }
-  };
-
   isOwner = (playlist: JSPFPlaylist): boolean => {
     const { currentUser } = this.context;
     return Boolean(currentUser) && currentUser?.name === playlist.creator;
@@ -139,22 +87,14 @@ export default class PlaylistsList extends React.Component<
     if (newOffset >= playlistCount) {
       return;
     }
-    this.setState({ loading: true });
-    try {
-      const newPlaylists = await this.APIService.getUserPlaylists(
-        user.name,
-        currentUser?.auth_token,
-        newOffset,
-        playlistsPerPage,
-        activeSection === "recommendations",
-        activeSection === "collaborations"
-      );
-
-      this.handleAPIResponse(newPlaylists);
-    } catch (error) {
-      newAlert("danger", "Error loading playlists", error?.message ?? error);
-      this.setState({ loading: false });
-    }
+    await this.fetchPlaylists(
+      user,
+      currentUser,
+      newOffset,
+      playlistsPerPage,
+      activeSection,
+      newAlert
+    );
   };
 
   handleClickPrevious = async () => {
@@ -166,6 +106,45 @@ export default class PlaylistsList extends React.Component<
       return;
     }
     const newOffset = Math.max(0, paginationOffset - playlistsPerPage);
+    await this.fetchPlaylists(
+      user,
+      currentUser,
+      newOffset,
+      playlistsPerPage,
+      activeSection,
+      newAlert
+    );
+  };
+
+  handleAPIResponse = (newPlaylists: {
+    playlists: JSPFObject[];
+    playlist_count: number;
+    count: string;
+    offset: string;
+  }) => {
+    const parsedOffset = parseInt(newPlaylists.offset, 10);
+    const parsedCount = parseInt(newPlaylists.count, 10);
+    this.setState({
+      playlists: newPlaylists.playlists.map((pl: JSPFObject) => pl.playlist),
+      playlistCount: newPlaylists.playlist_count,
+      paginationOffset: parsedOffset,
+      playlistsPerPage: parsedCount,
+      loading: false,
+    });
+  };
+
+  fetchPlaylists = async (
+    user: ListenBrainzUser,
+    currentUser: ListenBrainzUser,
+    newOffset: number,
+    playlistsPerPage: number,
+    activeSection: string,
+    newAlert: (
+      type: AlertType,
+      title: string,
+      message: string | JSX.Element
+    ) => void
+  ) => {
     this.setState({ loading: true });
     try {
       const newPlaylists = await this.APIService.getUserPlaylists(
@@ -181,33 +160,6 @@ export default class PlaylistsList extends React.Component<
     } catch (error) {
       newAlert("danger", "Error loading playlists", error?.message ?? error);
       this.setState({ loading: false });
-    }
-  };
-
-  handleAPIResponse = (
-    newPlaylists: {
-      playlists: JSPFObject[];
-      playlist_count: number;
-      count: string;
-      offset: string;
-    },
-    pushHistory: boolean = true
-  ) => {
-    const parsedOffset = parseInt(newPlaylists.offset, 10);
-    const parsedCount = parseInt(newPlaylists.count, 10);
-    this.setState({
-      playlists: newPlaylists.playlists.map((pl: JSPFObject) => pl.playlist),
-      playlistCount: newPlaylists.playlist_count,
-      paginationOffset: parsedOffset,
-      playlistsPerPage: parsedCount,
-      loading: false,
-    });
-    if (pushHistory) {
-      window.history.pushState(
-        null,
-        "",
-        `?offset=${parsedOffset}&count=${parsedCount}`
-      );
     }
   };
 
