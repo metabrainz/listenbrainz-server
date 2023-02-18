4cec1750d9ae87e6178d24b4d210892902344f84
Kartik Ohri <kartikohri13@gmail.com>
2023-02-18 12:58:48 +0530

    add typesense_index for release data too

--- mbid_mapping/mapping/typesense_index.py
+++ mbid_mapping/mapping/typesense_index.py
@@ -1,4 +1,3 @@
-import sys
 import re
 import time
 import datetime
@@ -13,14 +12,13 @@ from mapping.utils import log
 
 
 BATCH_SIZE = 5000
-COLLECTION_NAME_PREFIX = 'canonical_musicbrainz_data_'
 
 
 def prepare_string(text):
     return unidecode(re.sub(" +", " ", re.sub(r'[^\w ]+', '', text)).lower())
 
 
-def build_index():
+def build_index(collection_name_prefix, table, combiner):
 
     client = typesense.Client({
         'nodes': [{
@@ -32,16 +30,16 @@ def build_index():
         'connection_timeout_seconds': 1000000
     })
 
-    collection_name = COLLECTION_NAME_PREFIX + datetime.datetime.now().strftime('%Y%m%d_%H%M%S')
+    collection_name = collection_name_prefix + datetime.datetime.now().strftime('%Y%m%d_%H%M%S')
     try:
         log("typesense index: build index '%s'" % collection_name)
-        build(client, collection_name)
+        build(client, collection_name, table, combiner)
     except typesense.exceptions.TypesenseClientError as err:
         log("typesense index: Cannot build index: ", str(err))
         return -1
 
     try:
-        latest = COLLECTION_NAME_PREFIX + "latest"
+        latest = collection_name_prefix + "latest"
         log("typesense index: alias index '%s' to %s" % (collection_name, latest))
         aliased_collection = {"collection_name": collection_name}
         client.aliases.upsert(latest, aliased_collection)
@@ -54,19 +52,19 @@ def build_index():
             if collection["name"] == collection_name:
                 continue
 
-            if collection["name"].startswith(COLLECTION_NAME_PREFIX):
+            if collection["name"].startswith(collection_name_prefix):
                 log("typesense index: delete collection '%s'" % collection["name"])
                 client.collections[collection["name"]].delete()
             else:
                 log("typesense index: ignore collection '%s'" % collection["name"])
 
-    except typesense.exceptions.ObjectNotFound:
+    except typesense.exceptions.ObjectNotFound as err:
         log("typesense index: Failed to delete collection '%s'.", str(err))
 
     return 0
 
 
-def build(client, collection_name):
+def build(client, collection_name, table, combiner):
 
     schema = {
         'name': collection_name,
@@ -89,19 +87,21 @@ def build(client, collection_name):
     with psycopg2.connect(config.SQLALCHEMY_TIMESCALE_URI) as conn:
         with conn.cursor(cursor_factory=psycopg2.extras.DictCursor) as curs:
 
-            curs.execute("SELECT max(score) FROM mapping.canonical_musicbrainz_data")
+            curs.execute(f"SELECT max(score) FROM {table}")
             max_score = curs.fetchone()[0]
 
-            query = ("""SELECT recording_name,
-                               recording_mbid,
-                               release_name,
-                               release_mbid,
-                               artist_credit_id,
-                               artist_credit_name,
-                               artist_mbids,
-                               score,
-                               year
-                          FROM mapping.canonical_musicbrainz_data""")
+            query = f"""
+                SELECT recording_name
+                     , recording_mbid
+                     , release_name
+                     , release_mbid
+                     , artist_credit_id
+                     , artist_credit_name
+                     , artist_mbids
+                     , score
+                     , year
+                  FROM {table}
+            """
 
             curs.execute(query)
             documents = []
@@ -109,7 +109,7 @@ def build(client, collection_name):
                 document = dict(row)
                 document['artist_mbids'] = "{" + row["artist_mbids"][1:-1] + "}"
                 document['score'] = max_score - document['score']
-                document['combined'] = prepare_string(document['recording_name'] + " " + document['artist_credit_name'])
+                document['combined'] = prepare_string(combiner(document))
                 documents.append(document)
 
                 if len(documents) == BATCH_SIZE:
@@ -124,3 +124,15 @@ def build(client, collection_name):
 
     log("typesense index: indexing complete. waiting for background tasks to finish.")
     time.sleep(5)
+
+
+def build_all():
+    def combine_artist_recording(document):
+        return document['recording_name'] + " " + document['artist_credit_name']
+
+    def combine_artist_recording_release(document):
+        return document['recording_name'] + " " + document['artist_credit_name'] + " " + document['release_name']
+
+    build_index("canonical_musicbrainz_data_", "mapping.canonical_musicbrainz_data", combine_artist_recording)
+    build_index("canonical_musicbrainz_data_release_", "mapping.canonical_musicbrainz_data_release", combine_artist_recording_release)
+
