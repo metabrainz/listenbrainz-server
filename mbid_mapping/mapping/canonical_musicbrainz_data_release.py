82def6cd57766c52bb4c288a1bb76644f861f150
Kartik Ohri <kartikohri13@gmail.com>
2023-02-18 11:47:13 +0530

    fix indices

--- mbid_mapping/mapping/canonical_musicbrainz_data_release.py
+++ mbid_mapping/mapping/canonical_musicbrainz_data_release.py
@@ -12,6 +12,14 @@ class CanonicalMusicBrainzDataRelease(CanonicalMusicBrainzDataBase):
     def __init__(self, mb_conn, lb_conn=None, batch_size=None):
         super().__init__("mapping.canonical_musicbrainz_data_release", mb_conn, lb_conn, batch_size)
 
+    def get_index_names(self):
+        table = self.table_name.split(".")[-1]
+        return [
+            (f"{table}_idx_combined_lookup",              "combined_lookup", False),
+            (f"{table}_idx_artist_credit_recording_name_release_name", "artist_credit_name, recording_name, release_name", False),
+            (f"{table}_idx_recording_mbid_release_mbid", "recording_mbid, release_mbid", True)
+        ]
+
     def get_post_process_queries(self):
         return []
 
