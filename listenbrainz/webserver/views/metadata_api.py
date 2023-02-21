d005cf3ce606d3b660dd498f7fab6da598233ae7
Kartik Ohri <kartikohri13@gmail.com>
2023-02-21 19:51:10 +0530

    test changes for api support

--- listenbrainz/webserver/views/metadata_api.py
+++ listenbrainz/webserver/views/metadata_api.py
@@ -5,6 +5,9 @@ from listenbrainz.db.mbid_manual_mapping import create_mbid_manual_mapping, get_
 from listenbrainz.db.metadata import get_metadata_for_recording
 from listenbrainz.db.model.mbid_manual_mapping import MbidManualMapping
 from listenbrainz.labs_api.labs.api.artist_credit_recording_lookup import ArtistCreditRecordingLookupQuery
+from listenbrainz.labs_api.labs.api.artist_credit_recording_release_lookup import \
+    ArtistCreditRecordingReleaseLookupQuery
+from listenbrainz.mbid_mapping_writer.mbid_mapper import MBIDMapper
 from listenbrainz.mbid_mapping_writer.mbid_mapper_metadata_api import MBIDMapperMetadataAPI
 from listenbrainz.webserver.decorators import crossdomain
 from listenbrainz.webserver.errors import APIBadRequest, APIInternalServerError
@@ -127,6 +130,7 @@ def get_mbid_mapping():
     """
     artist_name = request.args.get("artist_name")
     recording_name = request.args.get("recording_name")
+    release_name = request.args.get("release_name")
     if not artist_name:
         raise APIBadRequest("artist_name is invalid or not present in arguments")
     if not recording_name:
@@ -138,9 +142,16 @@ def get_mbid_mapping():
     params = [
         {
             "[artist_credit_name]": artist_name,
-            "[recording_name]": recording_name
+            "[recording_name]": recording_name,
+            "[release_name]": release_name
         }
     ]
+
+    if release_name:
+        q = ArtistCreditRecordingReleaseLookupQuery(debug=False)
+        exact_results = q.fetch(params)
+        if exact_results:
+            return process_results(exact_results[0], metadata, incs)
 
     try:
         q = ArtistCreditRecordingLookupQuery(debug=False)
@@ -148,8 +159,8 @@ def get_mbid_mapping():
         if exact_results:
             return process_results(exact_results[0], metadata, incs)
 
-        q = MBIDMapperMetadataAPI(timeout=10, remove_stop_words=True, debug=False)
-        fuzzy_result = q.search(artist_name, recording_name)
+        q = MBIDMapper(timeout=10, remove_stop_words=True, debug=False)
+        fuzzy_result = q.search(artist_name, recording_name, release_name)
         if fuzzy_result:
             return process_results(fuzzy_result, metadata, incs)
 
