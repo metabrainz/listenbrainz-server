6eb0cc0d0843c1b201a1f29198a9a952eb133bf9
Kartik Ohri <kartikohri13@gmail.com>
2023-03-12 19:50:02 +0530

    update SpotifyStream type

--- frontend/js/src/utils/types.d.ts
+++ frontend/js/src/utils/types.d.ts
@@ -72,12 +72,27 @@ declare type Listen = BaseListenFormat & {
 };
 
 declare type SpotifyStream = {
-  ts: string;
+  ts: string; // Date string
+  username: string;
+  platform: string;
   ms_played: number;
+  conn_country: string;
+  ip_addr_decrypted: string;
+  user_agent_decrypted: string | null;
   master_metadata_track_name: string;
   master_metadata_album_artist_name: string;
   master_metadata_album_album_name: string;
   spotify_track_uri: string;
+  episode_name: string | null;
+  episode_show_name: string | null;
+  spotify_episode_uri: string | null;
+  reason_start: string;
+  reason_end: string;
+  shuffle: boolean;
+  skipped: boolean;
+  offline: boolean;
+  offline_timestamp: number;
+  incognito_mode: boolean;
 };
 
 declare type Recommendation = Listen & {
