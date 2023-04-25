d32374c52233017d47dce6ccb6513518094a861e
Monkey Do <contact@monkeydo.digital>
2023-04-25 14:49:37 +0200

    Fix YIM22 content width

--- frontend/js/src/explore/year-in-music/2022/YearInMusic.tsx
+++ frontend/js/src/explore/year-in-music/2022/YearInMusic.tsx
@@ -325,7 +325,7 @@ export default class YearInMusic extends React.Component<
 
     if (!yearInMusicData || isEmpty(yearInMusicData)) {
       return (
-        <div role="main" id="year-in-music" className="yim-2022 container">
+        <div id="year-in-music" className="yim-2022 container">
           <div id="main-header" className="flex-center">
             <img
               className="img-responsive header-image"
@@ -473,7 +473,7 @@ export default class YearInMusic extends React.Component<
     );
     const linkToThisPage = `https://listenbrainz.org/user/${user.name}/year-in-music/2022`;
     return (
-      <div role="main" id="year-in-music" className="yim-2022">
+      <div id="year-in-music" className="yim-2022">
         <div id="main-header" className="flex-center">
           <img
             className="img-responsive header-image"
