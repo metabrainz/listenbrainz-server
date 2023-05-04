84c760f38a1142c75896100a34cc96bdcb09bfe7
Kartik Ohri <kartikohri13@gmail.com>
2023-05-04 14:41:47 +0530

    fix inserts - 2

--- listenbrainz/db/similarity.py
+++ listenbrainz/db/similarity.py
@@ -112,13 +112,7 @@ def start_prod_table(name, algorithm):
 def insert_prod_table(name, data, algorithm):
     """ Insert similar recordings for the production dataset. """
     table = Identifier("similarity", f"{name}_prod_tmp")
-    query = SQL("""
-        INSERT INTO {table} AS sr (mbid0, mbid1, score)
-             VALUES %s
-        ON CONFLICT (mbid0, mbid1)
-          DO UPDATE
-                SET score = EXCLUDED.score
-    """).format(table=table)
+    query = SQL("INSERT INTO {table} (mbid0, mbid1, score) VALUES %s").format(table=table)
     values = [(x["mbid0"], x["mbid1"], x["score"]) for x in data]
 
     conn = timescale.engine.raw_connection()
