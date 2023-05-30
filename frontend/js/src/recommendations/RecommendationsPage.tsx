04a743bf349409451ff97b51ef704cd4ab2ed51b
Monkey Do <contact@monkeydo.digital>
2023-05-30 17:44:15 +0200

    Recs: Reimplement recommendations page

--- frontend/js/src/recommendations/RecommendationsPage.tsx
+++ frontend/js/src/recommendations/RecommendationsPage.tsx
@@ -7,6 +7,9 @@ import { createRoot } from "react-dom/client";
 import * as Sentry from "@sentry/react";
 import { Integrations } from "@sentry/tracing";
 import NiceModal from "@ebay/nice-modal-react";
+import { toast, ToastContainer } from "react-toastify";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faSave } from "@fortawesome/free-solid-svg-icons";
 import {
   withAlertNotifications,
   WithAlertNotificationsInjectedProps,
@@ -15,22 +18,19 @@ import GlobalAppContext from "../utils/GlobalAppContext";
 import Loader from "../components/Loader";
 import ErrorBoundary from "../utils/ErrorBoundary";
 import { getPageProps } from "../utils/utils";
-import PlaylistsList from "../playlists/PlaylistsList";
-import { PlaylistType } from "../playlists/utils";
+import { getPlaylistId, JSPFTrackToListen } from "../playlists/utils";
+import ListenCard from "../listens/ListenCard";
+import RecommendationPlaylistSettings from "./RecommendationPlaylistSettings";
 
 export type RecommendationsPageProps = {
   playlists?: JSPFObject[];
   user: ListenBrainzUser;
-  paginationOffset: string;
-  playlistCount: number;
 } & WithAlertNotificationsInjectedProps;
 
 export type RecommendationsPageState = {
   playlists: JSPFPlaylist[];
-  playlistSelectedForOperation?: JSPFPlaylist;
+  selectedPlaylist?: JSPFPlaylist;
   loading: boolean;
-  paginationOffset: number;
-  playlistCount: number;
 };
 
 export default class RecommendationsPage extends React.Component<
@@ -47,42 +47,108 @@ export default class RecommendationsPage extends React.Component<
     this.state = {
       playlists: concatenatedPlaylists ?? [],
       loading: false,
-      paginationOffset: parseInt(props.paginationOffset, 10) || 0,
-      playlistCount: props.playlistCount,
     };
   }
 
-  alertMustBeLoggedIn = () => {
-    const { newAlert } = this.props;
-    newAlert("danger", "Error", "You must be logged in for this operation");
-  };
-
-  updatePlaylists = (playlists: JSPFPlaylist[]): void => {
-    this.setState({ playlists });
+  selectPlaylist = async (playlist: JSPFPlaylist) => {
+    // The playlist prop only contains generic info, not the actual tracks
+    // We need to fetch the playlist to get it in full.
+    // Perhaps this should be revisited and we can send the full palylists directly.
+    const { APIService, currentUser } = this.context;
+    const playlistId = getPlaylistId(playlist);
+    try {
+      const response = await APIService.getPlaylist(
+        playlistId,
+        currentUser?.auth_token
+      );
+      const JSPFObject: JSPFObject = await response.json();
+      this.setState({ selectedPlaylist: JSPFObject.playlist });
+    } catch (error) {
+      toast.error(error.message);
+    }
   };
 
   render() {
     const { user, newAlert } = this.props;
-    const { playlists, paginationOffset, playlistCount, loading } = this.state;
-
+    const { currentUser } = this.context;
+    const { playlists, selectedPlaylist, loading } = this.state;
+    const isLoggedIn = Boolean(currentUser?.auth_token);
+    const [weeklyJams, ...otherPlaylists] = playlists;
     return (
-      <div>
-        <h3>Recommendation playlists created for {user.name}</h3>
-        <p>
-          These playlists are ephemeral and will only be available for a month.
-          Be sure to save the ones you like to your own playlists !
-        </p>
+      <div id="recommendations">
+        <h3>Created for {user.name}</h3>
+
         <Loader isLoading={loading} />
-        <PlaylistsList
-          onPaginatePlaylists={this.updatePlaylists}
-          playlists={playlists}
-          activeSection={PlaylistType.recommendations}
-          user={user}
-          paginationOffset={paginationOffset}
-          playlistCount={playlistCount}
-          selectPlaylistForEdit={() => {}}
-          newAlert={newAlert}
-        />
+        <div className="playlists-masonry">
+          {weeklyJams && (
+            <div
+              className={`weekly-jams ${
+                selectedPlaylist?.identifier === weeklyJams.identifier
+                  ? "selected"
+                  : ""
+              }`}
+              onClick={this.selectPlaylist.bind(this, weeklyJams)}
+              onKeyDown={this.selectPlaylist.bind(this, weeklyJams)}
+              role="button"
+              tabIndex={0}
+            >
+              <div className="title">Weekly Jams</div>
+              {isLoggedIn && (
+                <div className="btn btn-info btn-rounded">
+                  <FontAwesomeIcon icon={faSave} title="Save to my playlists" />{" "}
+                  Save
+                </div>
+              )}
+            </div>
+          )}
+          {otherPlaylists.map((playlist) => {
+            return (
+              <div
+                className={`${
+                  selectedPlaylist?.identifier === playlist.identifier
+                    ? "selected"
+                    : ""
+                }`}
+                onClick={this.selectPlaylist.bind(this, playlist)}
+                onKeyDown={this.selectPlaylist.bind(this, playlist)}
+                role="button"
+                tabIndex={0}
+              >
+                <div className="title">{playlist.title}</div>
+                {isLoggedIn && (
+                  <div className="btn btn-info btn-rounded btn-sm">
+                    <FontAwesomeIcon
+                      icon={faSave}
+                      title="Save to my playlists"
+                    />
+                  </div>
+                )}
+              </div>
+            );
+          })}
+        </div>
+
+        {selectedPlaylist && (
+          <section id="selected-playlist">
+            <div className="playlist-items">
+              {selectedPlaylist.track.map((playlistTrack) => {
+                const listen = JSPFTrackToListen(playlistTrack);
+
+                return (
+                  <ListenCard
+                    key={playlistTrack.identifier}
+                    className="playlist-item-card"
+                    listen={listen}
+                    showTimestamp={false}
+                    showUsername={false}
+                    newAlert={newAlert}
+                  />
+                );
+              })}
+            </div>
+            <RecommendationPlaylistSettings playlist={selectedPlaylist} />
+          </section>
+        )}
       </div>
     );
   }
@@ -105,13 +171,7 @@ document.addEventListener("DOMContentLoaded", () => {
       tracesSampleRate: sentry_traces_sample_rate,
     });
   }
-  const {
-    playlists,
-    user,
-    playlist_count: playlistCount,
-    pagination_offset: paginationOffset,
-    playlists_per_page: playlistsPerPage,
-  } = reactProps;
+  const { playlists, user } = reactProps;
 
   const RecommendationsPageWithAlertNotifications = withAlertNotifications(
     RecommendationsPage
@@ -120,13 +180,16 @@ document.addEventListener("DOMContentLoaded", () => {
   const renderRoot = createRoot(domContainer!);
   renderRoot.render(
     <ErrorBoundary>
+      <ToastContainer
+        position="bottom-right"
+        autoClose={8000}
+        hideProgressBar
+      />
       <GlobalAppContext.Provider value={globalAppContext}>
         <NiceModal.Provider>
           <RecommendationsPageWithAlertNotifications
             initialAlerts={optionalAlerts}
-            playlistCount={playlistCount}
             playlists={playlists}
-            paginationOffset={paginationOffset}
             user={user}
           />
         </NiceModal.Provider>
