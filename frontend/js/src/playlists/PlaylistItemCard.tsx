80e8596498d44111c30e53470d67c87e93395a23
Monkey Do <contact@monkeydo.digital>
2023-06-09 11:49:50 +0200

    Recs: optional deletion and props for PlaylistItemCard
    Make this component a little more flexible by allowing to pass certain props, and removing the functionality if the prop is not passed.
    In this case, the ability to delete tracks from a playlist, and to control if we show username and timestamp.

--- frontend/js/src/playlists/PlaylistItemCard.tsx
+++ frontend/js/src/playlists/PlaylistItemCard.tsx
@@ -1,5 +1,5 @@
 import * as React from "react";
-import { get as _get } from "lodash";
+import { get as _get, isFunction, isUndefined } from "lodash";
 import { faGripLines, faMinusCircle } from "@fortawesome/free-solid-svg-icons";
 import { IconProp } from "@fortawesome/fontawesome-svg-core";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
@@ -12,7 +12,7 @@ export type PlaylistItemCardProps = {
   track: JSPFTrack;
   currentFeedback: ListenFeedBack;
   canEdit: Boolean;
-  removeTrackFromPlaylist: (track: JSPFTrack) => void;
+  removeTrackFromPlaylist?: (track: JSPFTrack) => void;
   updateFeedbackCallback: (
     recordingMsid: string,
     score: ListenFeedBack | RecommendationFeedBack,
@@ -23,6 +23,8 @@ export type PlaylistItemCardProps = {
     title: string,
     message: string | JSX.Element
   ) => void;
+  showTimestamp?: boolean;
+  showUsername?: boolean;
 };
 
 export default class PlaylistItemCard extends React.Component<
@@ -30,7 +32,9 @@ export default class PlaylistItemCard extends React.Component<
 > {
   removeTrack = () => {
     const { track, removeTrackFromPlaylist } = this.props;
-    removeTrackFromPlaylist(track);
+    if (removeTrackFromPlaylist) {
+      removeTrackFromPlaylist(track);
+    }
   };
 
   render() {
@@ -40,6 +44,9 @@ export default class PlaylistItemCard extends React.Component<
       currentFeedback,
       newAlert,
       updateFeedbackCallback,
+      showUsername,
+      showTimestamp,
+      removeTrackFromPlaylist,
     } = this.props;
     // const customFields = getTrackExtension(track);
     // const trackDuration = track.duration
@@ -55,7 +62,7 @@ export default class PlaylistItemCard extends React.Component<
       </div>
     ) : undefined;
     let additionalMenuItems;
-    if (canEdit) {
+    if (canEdit && isFunction(removeTrackFromPlaylist)) {
       additionalMenuItems = [
         <ListenControl
           title="Remove from playlist"
@@ -71,9 +78,8 @@ export default class PlaylistItemCard extends React.Component<
         className="playlist-item-card"
         listen={listen}
         currentFeedback={currentFeedback}
-        showTimestamp={Boolean(listen.listened_at_iso)}
-        showUsername={Boolean(listen.user_name)}
-        // showTrackLength
+        showTimestamp={showTimestamp ?? Boolean(listen.listened_at_iso)}
+        showUsername={showUsername ?? Boolean(listen.user_name)}
         newAlert={newAlert}
         beforeThumbnailContent={dragHandle}
         data-recording-mbid={track.id}
