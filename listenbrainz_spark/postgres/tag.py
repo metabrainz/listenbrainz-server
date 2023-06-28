b858164d6f55e1438040a822cc072b6a1ab01d89
Kartik Ohri <kartikohri13@gmail.com>
2023-06-28 20:53:03 +0530

    change to query sum type to bigint

--- listenbrainz_spark/postgres/tag.py
+++ listenbrainz_spark/postgres/tag.py
@@ -82,7 +82,7 @@ def create_tag_cache():
              , t.name
     ) SELECT recording_mbid
            , tag
-           , SUM(tag_count) AS tag_count
+           , BIGINT(SUM(tag_count)) AS tag_count
         FROM intermediate
     GROUP BY recording_mbid
            , tag
