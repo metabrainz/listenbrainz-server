5c80c535e611cdfeaeebb49e5731b90fa0d1e706
Kartik Ohri <kartikohri13@gmail.com>
2023-07-03 20:51:22 +0530

    fix passing jam type to troi

--- listenbrainz/troi/troi_bot.py
+++ listenbrainz/troi/troi_bot.py
@@ -139,7 +139,8 @@ def run_daily_jams(user, existing_url, service):
         "upload": True,
         "token": token,
         "created_for": username,
-        "jam_date": user["jam_date"]
+        "jam_date": user["jam_date"],
+        "type": "daily-jams"
     }
 
     if user["export_to_spotify"]:
@@ -149,7 +150,7 @@ def run_daily_jams(user, existing_url, service):
                 spotify["existing_urls"] = [existing_url]
             args["spotify"] = spotify
 
-    playlist = generate_playlist(PeriodicJamsPatch(type="daily-jams"), args)
+    playlist = generate_playlist(PeriodicJamsPatch(), args)
 
     if playlist is not None and len(playlist.playlists) > 0:
         url = current_app.config["SERVER_ROOT_URL"] + "/playlist/" + playlist.playlists[0].mbid
