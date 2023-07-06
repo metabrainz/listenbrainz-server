f22da456f81912129e1b3baeb2b58c6a627a71ea
Kartik Ohri <kartikohri13@gmail.com>
2023-07-06 08:57:27 +0530

    Consider user's timezone while batch generating playlists (#2515)
    * Consider user's timezone while batch generating playlists
    
    Just like daily jams, create weekly playlists based on timezones.
    
    * Fix connection = None
    
    * fix users fetch

--- listenbrainz_spark/troi/periodic_jams.py
+++ listenbrainz_spark/troi/periodic_jams.py
@@ -2,6 +2,7 @@ from datetime import datetime, timedelta
 
 from more_itertools import chunked
 
+import listenbrainz_spark
 from listenbrainz_spark.path import RECORDING_FEEDBACK_DATAFRAME, RAW_RECOMMENDATIONS
 from listenbrainz_spark.stats import run_query
 
@@ -10,8 +11,8 @@ USERS_PER_MESSAGE = 100
 MAX_TRACKS_PER_PLAYLIST = 50
 
 
-def main(slug):
-    """ Bulk generate troi playlists for the given slug """
+def main(slug, users):
+    """ Bulk generate troi playlists for the given slug and the given list of users """
     if slug == "weekly-jams":
         max_listened_at = datetime.now() - timedelta(days=DAYS_OF_RECENT_LISTENS_TO_EXCLUDE)
         max_listened_at = max_listened_at.replace(hour=0, minute=0, second=0, microsecond=0)
@@ -21,26 +22,36 @@ def main(slug):
     else:
         return []
 
+    table = "users_periodic_jams"
+    users_df = listenbrainz_spark.session.createDataFrame(users)
+    users_df.createOrReplaceTempView(table)
+
     query = f"""
         WITH recommendations AS (
             SELECT user_id
+                 , jam_date
                  , recording_mbid
                  , rank() OVER (PARTITION BY user_id ORDER BY score DESC) AS ranking
-              FROM parquet.`{RAW_RECOMMENDATIONS}`
+              FROM {table}
+              JOIN parquet.`{RAW_RECOMMENDATIONS}`
+             USING (user_id)
          LEFT JOIN parquet.`{RECORDING_FEEDBACK_DATAFRAME}`
              USING (user_id, recording_mbid)
              WHERE {time_filter}
                AND (feedback IS NULL OR feedback != -1)
         ), randomized AS (
             SELECT user_id
+                 , jam_date
                  , recording_mbid
                  , rank() over (PARTITION BY user_id ORDER BY RANDOM()) AS position
               FROM recommendations
              WHERE ranking <= {MAX_TRACKS_PER_PLAYLIST}
         )   SELECT user_id
+                 , jam_date
                  , array_sort(collect_list(struct(position, recording_mbid))) AS recordings
               FROM randomized
           GROUP BY user_id
+                 , jam_date
     """
     data = run_query(query).toLocalIterator()
 
@@ -50,6 +61,7 @@ def main(slug):
         for playlist in raw_playlists:
             playlists.append({
                 "user_id": playlist["user_id"],
+                "jam_date": playlist["jam_date"],
                 "recordings": [r["recording_mbid"] for r in playlist["recordings"]]
             })
 
