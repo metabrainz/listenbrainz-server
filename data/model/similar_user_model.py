9c46c8513ce38d9ba49a9abc63832433264403e5
Jasjeet Singh <98077881+07jasjeet@users.noreply.github.com>
2023-07-12 16:30:02 +0530

    Leftover change

--- data/model/similar_user_model.py
+++ data/model/similar_user_model.py
@@ -1,6 +1,10 @@
 from pydantic import BaseModel, NonNegativeInt
-
+from typing import List
 
 class SimilarUsers(BaseModel):
     user_id: NonNegativeInt
-    similar_users: dict
+    similar_users: List[dict]
+
+    def toMap(self) -> dict:
+        ''' Returns a map of musicbrainz_id and similarity'''
+        return {user['musicbrainz_id']: user['similarity'] for user in self.similar_users}
