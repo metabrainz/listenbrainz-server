db3051f8dc89f0cd9ac06529b02757a24d3643ac
Robert Kaye <rob@musicbrainz.org>
2023-08-08 15:42:18 +0200

    Break into separate components

---
+++ frontend/js/src/explore/lb-radio/Playlist.tsx
@@ -0,0 +1,152 @@
+/* eslint-disable jsx-a11y/anchor-is-valid */
+
+import { createRoot } from "react-dom/client";
+import * as React from "react";
+import NiceModal from "@ebay/nice-modal-react";
+import { useState } from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { IconProp } from "@fortawesome/fontawesome-svg-core";
+import { faCog, faFileExport } from "@fortawesome/free-solid-svg-icons";
+import { faSpotify } from "@fortawesome/free-brands-svg-icons";
+import { toast } from "react-toastify";
+import { saveAs } from "file-saver";
+import { get, set } from "lodash";
+import ErrorBoundary from "../../utils/ErrorBoundary";
+import GlobalAppContext from "../../utils/GlobalAppContext";
+import { getPageProps } from "../../utils/utils";
+import PlaylistItemCard from "../../playlists/PlaylistItemCard";
+import Loader from "../../components/Loader";
+import withAlertNotifications from "../../notifications/AlertNotificationsHOC";
+import {
+  JSPFTrackToListen,
+  MUSICBRAINZ_JSPF_TRACK_EXTENSION,
+  getRecordingMBIDFromJSPFTrack,
+} from "../../playlists/utils";
+import BrainzPlayer from "../../brainzplayer/BrainzPlayer";
+import { ToastMsg } from "../../notifications/Notifications";
+
+type UserFeedbackProps = {
+  feedback: string[];
+};
+
+type PlaylistProps = {
+  playlist?: JSPFPlaylist;
+  title: string;
+  onSavePlaylist: () => void;
+  enableOptions: boolean;
+  onSaveToSpotify: () => void;
+  onExportJSPF: () => void;
+};
+
+export function UserFeedback(props: UserFeedbackProps) {
+  const { feedback } = props;
+
+  if (feedback.length === 0) {
+    return <div className="feedback" />;
+  }
+
+  return (
+    <div id="feedback" className="alert alert-info">
+      <div className="feedback-header">Query feedback:</div>
+      <ul>
+        {feedback.map((item: string) => {
+          return <li key={`${item}`}>{`${item}`}</li>;
+        })}
+      </ul>
+    </div>
+  );
+}
+
+export function Playlist(props: PlaylistProps) {
+  const {
+    playlist,
+    title,
+    onSavePlaylist,
+    enableOptions,
+    onSaveToSpotify,
+    onExportJSPF,
+  } = props;
+  const { spotifyAuth } = React.useContext(GlobalAppContext);
+  const showSpotifyExportButton = spotifyAuth?.permission?.includes(
+    "playlist-modify-public"
+  );
+  if (!playlist?.track?.length) {
+    if (playlist !== undefined) {
+      return <div id="title">No playlist was generated.</div>;
+    }
+    return null;
+  }
+  return (
+    <div>
+      <div id="playlist-title">
+        {enableOptions && (
+          <span className="dropdown pull-right">
+            <button
+              className="btn btn-info dropdown-toggle"
+              type="button"
+              id="options-dropdown"
+              data-toggle="dropdown"
+              aria-haspopup="true"
+              aria-expanded="true"
+            >
+              <FontAwesomeIcon icon={faCog as IconProp} title="Options" />
+              &nbsp;Options
+            </button>
+            <ul
+              className="dropdown-menu dropdown-menu-right"
+              aria-labelledby="options-dropdown"
+            >
+              <li>
+                <a onClick={onSavePlaylist} role="button" href="#">
+                  Save
+                </a>
+              </li>
+              {showSpotifyExportButton && (
+                <>
+                  <li role="separator" className="divider" />
+                  <li>
+                    <a
+                      onClick={onSaveToSpotify}
+                      id="exportPlaylistToSpotify"
+                      role="button"
+                      href="#"
+                    >
+                      <FontAwesomeIcon icon={faSpotify as IconProp} /> Export to
+                      Spotify
+                    </a>
+                  </li>
+                </>
+              )}
+              <li role="separator" className="divider" />
+              <li>
+                <a
+                  onClick={onExportJSPF}
+                  id="exportPlaylistToJSPF"
+                  role="button"
+                  href="#"
+                >
+                  <FontAwesomeIcon icon={faFileExport as IconProp} /> Export as
+                  JSPF
+                </a>
+              </li>
+            </ul>
+          </span>
+        )}
+        <div id="title">{title}</div>
+      </div>
+      <div>
+        {playlist.track.map((track: JSPFTrack, index) => {
+          return (
+            <PlaylistItemCard
+              key={`${track.id}-${index.toString()}`}
+              canEdit={false}
+              track={track}
+              currentFeedback={0}
+              updateFeedbackCallback={() => {}}
+            />
+          );
+        })}
+      </div>
+    </div>
+  );
+}
---
+++ frontend/js/src/explore/lb-radio/Prompt.tsx
@@ -0,0 +1,150 @@
+/* eslint-disable jsx-a11y/anchor-is-valid */
+
+import { createRoot } from "react-dom/client";
+import * as React from "react";
+import NiceModal from "@ebay/nice-modal-react";
+import { useState } from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { IconProp } from "@fortawesome/fontawesome-svg-core";
+import { faCog, faFileExport } from "@fortawesome/free-solid-svg-icons";
+import { faSpotify } from "@fortawesome/free-brands-svg-icons";
+import { toast } from "react-toastify";
+import { saveAs } from "file-saver";
+import { get, set } from "lodash";
+import ErrorBoundary from "../../utils/ErrorBoundary";
+import GlobalAppContext from "../../utils/GlobalAppContext";
+import { getPageProps } from "../../utils/utils";
+import PlaylistItemCard from "../../playlists/PlaylistItemCard";
+import Loader from "../../components/Loader";
+import withAlertNotifications from "../../notifications/AlertNotificationsHOC";
+import {
+  JSPFTrackToListen,
+  MUSICBRAINZ_JSPF_TRACK_EXTENSION,
+  getRecordingMBIDFromJSPFTrack,
+} from "../../playlists/utils";
+import BrainzPlayer from "../../brainzplayer/BrainzPlayer";
+import { ToastMsg } from "../../notifications/Notifications";
+
+type PromptProps = {
+  onGenerate: (prompt: string, mode: string) => void;
+  errorMessage: string;
+  initMode: string;
+  initPrompt: string;
+};
+
+function Prompt(props: PromptProps) {
+  const { onGenerate, errorMessage, initMode, initPrompt } = props;
+  const [prompt, setPrompt] = useState<string>(initPrompt);
+  const [mode, setMode] = useState<string>(initMode);
+  const [hideExamples, setHideExamples] = React.useState(false);
+
+  const generateCallbackFunction = React.useCallback(
+    (event: React.FormEvent<HTMLFormElement>) => {
+      event.preventDefault();
+      setHideExamples(true);
+      onGenerate(prompt, mode);
+    },
+    [prompt, onGenerate, mode]
+  );
+
+  const onInputChangeCallback = React.useCallback(
+    (event: React.ChangeEvent<HTMLInputElement>) => {
+      const text = event.target.value;
+      setPrompt(text);
+    },
+    []
+  );
+
+  const onModeSelectChange = React.useCallback(
+    (event: React.ChangeEvent<HTMLSelectElement>) => {
+      const text = event.target.value;
+      setMode(text);
+    },
+    []
+  );
+
+  React.useEffect(() => {
+    if (prompt.length > 0) {
+      setHideExamples(true);
+      onGenerate(initPrompt, initMode);
+    }
+  }, [initMode, initPrompt, onGenerate, prompt.length]);
+
+  return (
+    <div className="prompt">
+      <div>
+        <h3>
+          ListenBrainz Radio playlist generator
+          <small>
+            <a
+              id="doc-link"
+              href="https://troi.readthedocs.io/en/add-user-stats-entity/lb_radio.html"
+            >
+              How do I write a query?
+            </a>
+          </small>
+        </h3>
+      </div>
+      <form onSubmit={generateCallbackFunction}>
+        <div className="input-group input-group-flex" id="prompt-input">
+          <input
+            type="text"
+            className="form-control form-control-lg"
+            name="prompt"
+            value={prompt}
+            placeholder="Enter prompt..."
+            onChange={onInputChangeCallback}
+          />
+          <select
+            className="form-control"
+            id="mode-dropdown"
+            name="mode"
+            value={mode}
+            onChange={onModeSelectChange}
+          >
+            <option value="easy">easy</option>
+            <option value="medium">medium</option>
+            <option value="hard">hard</option>
+          </select>
+          <span className="input-group-btn">
+            <button
+              type="submit"
+              className="btn btn-primary"
+              disabled={prompt?.length <= 3}
+            >
+              Generate
+            </button>
+          </span>
+        </div>
+      </form>
+      {hideExamples === false && (
+        <div>
+          <div id="examples">
+            Examples:
+            <a href="/explore/lb-radio?prompt=artist:(radiohead)&mode=easy">
+              artist:(radiohead)
+            </a>
+            <a href="/explore/lb-radio?prompt=tag:(trip hop)&mode=easy">
+              tag:(trip hop)
+            </a>
+            <a href="/explore/lb-radio?prompt=%23metal&mode=easy">#metal</a>
+            <a href="/explore/lb-radio?prompt=stats:rob&mode=easy">stats:rob</a>
+          </div>
+          <div id="made-with-postgres">
+            <img
+              src="/static/img/explore/made-with-postgres.png"
+              alt="Made with Postgres, not AI!"
+            />
+          </div>
+        </div>
+      )}
+      {errorMessage.length > 0 && (
+        <div id="error-message" className="alert alert-danger">
+          {errorMessage}
+        </div>
+      )}
+    </div>
+  );
+}
+
+export default Prompt;
