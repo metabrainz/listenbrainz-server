a18f6da6336fa9c1e4ebb1b7ae36f16f3e5c65c5
Monkey Do <contact@monkeydo.digital>
2023-08-10 16:38:05 +0200

    Adapt LB Radio ListenCards to new feedback management

--- frontend/js/src/explore/lb-radio/Playlist.tsx
+++ frontend/js/src/explore/lb-radio/Playlist.tsx
@@ -126,8 +126,6 @@ export function Playlist(props: PlaylistProps) {
               key={`${track.id}-${index.toString()}`}
               canEdit={false}
               track={track}
-              currentFeedback={0}
-              updateFeedbackCallback={() => {}}
             />
           );
         })}
