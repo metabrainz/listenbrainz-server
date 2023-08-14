675a70edf0e1ec3517ed9a86ae34a7ec2cdc1956
vardansaini <vardan1@ualberta.ca>
2023-08-14 09:56:20 -0600

    "Apple metadata cache "

--- admin/timescale/create_tables.sql
+++ admin/timescale/create_tables.sql
@@ -187,6 +187,55 @@ CREATE TABLE spotify_cache.rel_track_artist (
     position        INTEGER NOT NULL
 );
 
+CREATE TABLE apple_cache.raw_cache_data (
+    id              INTEGER GENERATED ALWAYS AS IDENTITY NOT NULL,
+    album_id        TEXT NOT NULL,
+    data            JSONB,
+    last_refresh    TIMESTAMP WITH TIME ZONE NOT NULL,
+    expires_at      TIMESTAMP WITH TIME ZONE NOT NULL
+);
+
+CREATE TABLE apple_cache.album (
+    id                      INTEGER GENERATED ALWAYS AS IDENTITY NOT NULL,
+    apple_id              TEXT   NOT NULL,
+    name                    TEXT   NOT NULL,
+    type                    TEXT   NOT NULL,
+    release_date            TEXT   NOT NULL,
+    last_refresh            TIMESTAMP WITH TIME ZONE NOT NULL,
+    expires_at              TIMESTAMP WITH TIME ZONE NOT NULL,
+    data                    JSONB  NOT NULL
+);
+
+CREATE TABLE apple_cache.artist (
+    id                      INTEGER GENERATED ALWAYS AS IDENTITY NOT NULL,
+    apple_id              TEXT NOT NULL,
+    name                    TEXT NOT NULL,
+    data                    JSONB NOT NULL
+);
+
+CREATE TABLE apple_cache.track (
+    id                      INTEGER GENERATED ALWAYS AS IDENTITY NOT NULL,
+    apple_id              TEXT NOT NULL,
+    name                    TEXT NOT NULL,
+    track_number            INTEGER NOT NULL,
+    album_id                TEXT NOT NULL,
+    data                    JSONB NOT NULL
+);
+
+CREATE TABLE apple_cache.rel_album_artist (
+    id              INTEGER GENERATED ALWAYS AS IDENTITY NOT NULL,
+    album_id        TEXT NOT NULL,
+    artist_id       TEXT NOT NULL,
+    position        INTEGER NOT NULL
+);
+
+CREATE TABLE apple_cache.rel_track_artist (
+    id              INTEGER GENERATED ALWAYS AS IDENTITY NOT NULL,
+    track_id        TEXT NOT NULL,
+    artist_id       TEXT NOT NULL,
+    position        INTEGER NOT NULL
+);
+
 CREATE TABLE background_worker_state (
     key     TEXT NOT NULL,
     value   TEXT
