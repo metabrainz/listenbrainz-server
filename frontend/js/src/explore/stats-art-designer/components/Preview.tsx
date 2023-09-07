aebeb52e25089111717dceee0912887657999354
Monkey Do <contact@monkeydo.digital>
2023-09-07 13:18:47 +0200

    Stats art: Catch errors loading SVGs
    Temporary second API request to catch errors, until https://github.com/shubhamjain/svg-loader/pull/42 is merged and allows catching errors directly

--- frontend/js/src/explore/stats-art-designer/components/Preview.tsx
+++ frontend/js/src/explore/stats-art-designer/components/Preview.tsx
@@ -15,11 +15,36 @@ const Preview = React.forwardRef(function PreviewComponent(
   props: PreviewProps,
   ref: React.ForwardedRef<SVGSVGElement>
 ) {
+  const [error, setError] = React.useState<string>();
   const { url, styles, size = 750 } = props;
   const hasCustomStyles = Boolean(
     Object.values(styles)?.filter(Boolean).length
   );
   const { textColor, bgColor1, bgColor2 } = styles;
+  /* The library used to dynamically load the SVG does not currently allow handling errors
+  so we have to separately try to hit the URL to catch and display any API errors (no data for user for range, etc.)
+  See https://github.com/shubhamjain/svg-loader/pull/42 */
+  React.useEffect(() => {
+    const fetchUrl = async () => {
+      if (!url) {
+        return;
+      }
+      try {
+        const response = await fetch(url);
+        // We only care if there was an error
+        if (!response.ok) {
+          const json = await response.json();
+          setError(json.error || "Something went wrong");
+        } else {
+          setError(undefined);
+        }
+      } catch (err) {
+        setError(err.toString());
+      }
+    };
+    fetchUrl();
+  }, [url]);
+
   if (!url) {
     return (
       <div style={{ width: "500px", aspectRatio: 1 }}>
@@ -27,9 +52,20 @@ const Preview = React.forwardRef(function PreviewComponent(
       </div>
     );
   }
+  if (error) {
+    return (
+      <div className="alert alert-danger">
+        There was an error trying to load statistics for this user and time
+        range:
+        <pre>{error}</pre>
+        Please check the username or try another time range.
+      </div>
+    );
+  }
+
   /*
-    We use a library to dynamically load the SVG into the page so that we can customize it dynamically with CSS.
-    If using an <object> element, the SVG is in a separate DOM and we can't style it.
+    We use a library to dynamically load the SVG into the page so that we can customize it dynamically with CSS,
+    using nested SVGs. If using an <object> element, the SVG is in a separate DOM and we can't style it.
   */
 
   return (
@@ -63,7 +99,7 @@ const Preview = React.forwardRef(function PreviewComponent(
             : ""}
         </style>
       )}
-      <svg data-src={url} />
+      <svg data-src={url} data-js="enabled" />
     </svg>
   );
 });
