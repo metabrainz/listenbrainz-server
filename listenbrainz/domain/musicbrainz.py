4eb60bb0ad516c996bd10469e756fb6b4e5fbd4a
Kartik Ohri <kartikohri13@gmail.com>
2023-10-03 16:49:17 +0530

    Make musicbrainz instance url configurable
    This makes it simpler to test the new oauth system by simply changing
    a url in the config.py.

--- listenbrainz/domain/musicbrainz.py
+++ listenbrainz/domain/musicbrainz.py
@@ -6,8 +6,6 @@ from listenbrainz.domain.brainz_service import BaseBrainzService
 
 MUSICBRAINZ_SCOPES = ["tag", "rating", "profile"]
 
-MUSICBRAINZ_USER_INFO_URL = "https://musicbrainz.org/oauth2/userinfo"
-
 
 class MusicBrainzService(BaseBrainzService):
 
@@ -17,12 +15,15 @@ class MusicBrainzService(BaseBrainzService):
             client_id=current_app.config["MUSICBRAINZ_CLIENT_ID"],
             client_secret=current_app.config["MUSICBRAINZ_CLIENT_SECRET"],
             redirect_uri=url_for('login.musicbrainz_post', _external=True),
-            authorize_url="https://musicbrainz.org/oauth2/authorize",
-            token_url="https://musicbrainz.org/oauth2/token",
+            authorize_url=f"{current_app.config['MUSICBRAINZ_BASE_URL']}/oauth2/authorize",
+            token_url=f"{current_app.config['MUSICBRAINZ_BASE_URL']}/oauth2/token",
             scopes=MUSICBRAINZ_SCOPES
         )
 
     def get_user_info(self, token: str):
-        response = requests.post(MUSICBRAINZ_USER_INFO_URL, headers={"Authorization": f"Bearer {token}"})
+        response = requests.post(
+            f"{current_app.config['MUSICBRAINZ_BASE_URL']}/userinfo",
+            headers={"Authorization": f"Bearer {token}"}
+        )
         response.raise_for_status()
         return response.json()
