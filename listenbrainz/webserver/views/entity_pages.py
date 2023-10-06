dc1715202e46a938f3f39fe11106654e0f5fbdc8
Robert Kaye <rob@musicbrainz.org>
2023-10-06 15:05:16 +0200

    Top recordings now sent via props

--- listenbrainz/webserver/views/entity_pages.py
+++ listenbrainz/webserver/views/entity_pages.py
@@ -4,6 +4,7 @@ from listenbrainz import webserver
 from listenbrainz.webserver.decorators import web_listenstore_needed
 from listenbrainz.db.metadata import get_metadata_for_artist
 from listenbrainz.webserver.views.api_tools import is_valid_uuid
+import requests
 from werkzeug.exceptions import BadRequest, NotFound
 import orjson
 
@@ -18,6 +19,7 @@ def artist_entity(artist_mbid):
     if not is_valid_uuid(artist_mbid):
         raise BadRequest("Provided artist ID is invalid: %s" % artist_mbid)
 
+    # Fetch the artist cached data
     artist_data = get_metadata_for_artist([artist_mbid])
     if len(artist_data) == 0:
         raise NotFound(f"artist {artist_mbid} not found in the metadata cache")
@@ -26,8 +28,16 @@ def artist_entity(artist_mbid):
     item.update(**artist_data[0].artist_data)
     item["tag"] = artist_data[0].tag_data
 
+    # Fetch top recordings for artist
+    r = requests.post("https://datasets.listenbrainz.org/popular-recordings/json?count=15", json=[{"[artist_mbid]": artist_mbid}])
+    if r.status_code != 200:
+        popular_recordings = []
+    else:
+        popular_recordings = list(r.json())
+
     props = {
-        "artist_data": item
+        "artist_data": item,
+        "popular_recordings": popular_recordings
     }
 
     return render_template(
