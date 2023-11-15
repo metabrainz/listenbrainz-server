0035e382d9afa749d611e31a68f8d198314ffbd1
Kartik Ohri <kartikohri13@gmail.com>
2023-11-16 01:04:56 +0530

    Resolve duplicate MSIDs when fetching MSID data (#2629)
    * Resolve duplicate MSIDs when fetching MSID data
    
    This is temporary step while MSIDs in all tables are fixed up.
    
    * Create messybrainz.submissions_redirect in testing
    
    * Remove redirected_msid from output for now

--- admin/timescale/create_foreign_keys.sql
+++ admin/timescale/create_foreign_keys.sql
@@ -1,5 +1,11 @@
 BEGIN;
 
+ALTER TABLE messybrainz.submissions_redirect
+    ADD CONSTRAINT messybrainz_submissions_msid_foreign_key
+    FOREIGN KEY (original_msid)
+    REFERENCES messybrainz.submissions (gid)
+    ON DELETE NO ACTION;
+
 ALTER TABLE playlist.playlist_recording
     ADD CONSTRAINT playlist_id_foreign_key
     FOREIGN KEY (playlist_id)
