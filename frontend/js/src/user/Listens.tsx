8115f701f77e574f87c206e8556ad70874162cd0
Monkey Do <contact@monkeydo.digital>
2023-11-23 15:26:11 +0100

    Rewrite Listens page tests to RTL
    Installed jest-websocket-mock to facilitate testing of websocket feature.
    A few accessibility changes to go with RTL's testing strategy.

--- frontend/js/src/user/Listens.tsx
+++ frontend/js/src/user/Listens.tsx
@@ -749,7 +749,7 @@ export default class Listens extends React.Component<
             {webSocketListens.length > 0 && (
               <div className="webSocket-box">
                 <h4>New listens since you arrived</h4>
-                <div id="webSocketListens">
+                <div id="webSocketListens" data-testid="webSocketListens">
                   {webSocketListens.map((listen) => this.getListenCard(listen))}
                 </div>
                 <div className="read-more">
@@ -831,6 +831,7 @@ export default class Listens extends React.Component<
                 </div>
                 <div
                   id="listens"
+                  data-testid="listens"
                   ref={this.listensTable}
                   style={{ opacity: loading ? "0.4" : "1" }}
                 >
@@ -847,11 +848,13 @@ export default class Listens extends React.Component<
                   >
                     <a
                       role="button"
+                      aria-label="Navigate to most recent listens"
                       onClick={this.handleClickNewest}
                       onKeyDown={(e) => {
                         if (e.key === "Enter") this.handleClickNewest();
                       }}
                       tabIndex={0}
+                      aria-disabled={isNewestButtonDisabled}
                       href={window.location.pathname}
                     >
                       &#x21E4;
@@ -864,11 +867,13 @@ export default class Listens extends React.Component<
                   >
                     <a
                       role="button"
+                      aria-label="Navigate to more recent listens"
                       onClick={this.handleClickNewer}
                       onKeyDown={(e) => {
                         if (e.key === "Enter") this.handleClickNewer();
                       }}
                       tabIndex={0}
+                      aria-disabled={isNewerButtonDisabled}
                       href={
                         isNewerButtonDisabled
                           ? undefined
@@ -903,11 +908,13 @@ export default class Listens extends React.Component<
                     style={{ marginLeft: "auto" }}
                   >
                     <a
+                      aria-label="Navigate to older listens"
                       role="button"
                       onClick={this.handleClickOlder}
                       onKeyDown={(e) => {
                         if (e.key === "Enter") this.handleClickOlder();
                       }}
+                      aria-disabled={isOlderButtonDisabled}
                       tabIndex={0}
                       href={
                         isOlderButtonDisabled
@@ -924,12 +931,14 @@ export default class Listens extends React.Component<
                     }`}
                   >
                     <a
+                      aria-label="Navigate to oldest listens"
                       role="button"
                       onClick={this.handleClickOldest}
                       onKeyDown={(e) => {
                         if (e.key === "Enter") this.handleClickOldest();
                       }}
                       tabIndex={0}
+                      aria-disabled={isOldestButtonDisabled}
                       href={
                         isOldestButtonDisabled
                           ? undefined
