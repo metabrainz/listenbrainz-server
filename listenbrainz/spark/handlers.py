f2b2694e4c5fd13fa798c0c4be1d55064d3b34bf
Kartik Ohri <kartikohri13@gmail.com>
2023-12-06 22:14:19 +0530

    Migrate YIM playlists to spark

--- listenbrainz/spark/handlers.py
+++ listenbrainz/spark/handlers.py
@@ -20,8 +20,8 @@ from listenbrainz.db.fresh_releases import insert_fresh_releases
 from listenbrainz.db import similarity
 from listenbrainz.db.similar_users import import_user_similarities
 from listenbrainz.troi.daily_jams import run_post_recommendation_troi_bot
-from listenbrainz.troi.weekly_playlists import batch_process_playlists, batch_process_playlists_end
-from listenbrainz.troi.year_in_music import yim_patch_runner
+from listenbrainz.troi.weekly_playlists import process_weekly_playlists, process_weekly_playlists_end
+from listenbrainz.troi.year_in_music import process_yim_playlists, process_yim_playlists_end
 
 TIME_TO_CONSIDER_STATS_AS_OLD = 20  # minutes
 TIME_TO_CONSIDER_RECOMMENDATIONS_AS_OLD = 7  # days
@@ -332,7 +332,7 @@ def handle_similar_users(message):
 
 
 def handle_yim_similar_users(message):
-    year_in_music.insert_similar_recordings(message["year"], message["data"])
+    year_in_music.insert_similar_users(message["year"], message["data"])
 
 
 def handle_yim_new_releases_of_top_artists(message):
@@ -380,19 +380,12 @@ def handle_yim_top_genres(message):
     year_in_music.handle_multi_large_insert("top_genres", message["year"], message["data"])
 
 
-def handle_yim_tracks_of_the_year_start(message):
-    year_in_music.create_tracks_of_the_year(message["year"])
+def handle_yim_playlists(message):
+    process_yim_playlists(message["slug"], message["year"], message["data"])
 
 
-def handle_yim_tracks_of_the_year_data(message):
-    year_in_music.insert_tracks_of_the_year(message["year"], message["data"])
-
-
-def handle_yim_tracks_of_the_year_end(message):
-    # all of the tracks data has been inserted, now we can generate the playlists
-    # year_in_music.finalise_tracks_of_the_year(message["year"])
-    # yim_patch_runner(message["year"])
-    pass
+def handle_yim_playlists_end(message):
+    process_yim_playlists_end(message["slug"], message["year"])
 
 
 def handle_similar_recordings(message):
@@ -404,8 +397,8 @@ def handle_similar_artists(message):
 
 
 def handle_troi_playlists(message):
-    batch_process_playlists(message["slug"], message["data"])
+    process_weekly_playlists(message["slug"], message["data"])
 
 
 def handle_troi_playlists_end(message):
-    batch_process_playlists_end(message["slug"])
+    process_weekly_playlists_end(message["slug"])
--- listenbrainz_spark/year_in_music/utils.py
+++ listenbrainz_spark/year_in_music/utils.py
@@ -1,7 +1,6 @@
 from datetime import datetime, date, time
 
-import listenbrainz_spark
-from listenbrainz_spark import path
+from listenbrainz_spark.stats import run_query
 from listenbrainz_spark.utils import get_listens_from_dump
 
 
@@ -10,3 +9,20 @@ def setup_listens_for_year(year):
     end = datetime.combine(date(year, 12, 31), time.max)
     listens = get_listens_from_dump(start, end)
     listens.createOrReplaceTempView("listens_of_year")
+
+
+def create_tracks_of_the_year(year):
+    end = datetime.combine(date(year, 12, 31), time.max)
+    listens = get_listens_from_dump(end=end)
+    listens.createOrReplaceTempView("listens_for_tracks_of_year")
+    query = f"""
+            SELECT user_id
+                 , recording_mbid
+                 , count(*) AS score
+              FROM listens_for_tracks_of_year
+             WHERE recording_mbid IS NOT NULL
+          GROUP BY user_id
+                 , recording_mbid
+            HAVING date_part('YEAR', min(listened_at)) = {year}
+    """
+    run_query(query).createOrReplaceTempView("tracks_of_year")
