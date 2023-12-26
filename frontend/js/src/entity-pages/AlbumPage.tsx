7889eb18348367ffb736bdfd83b82ef10d2b2335
Kartik Ohri <kartikohri13@gmail.com>
2023-12-27 01:24:21 +0530

    Add total listeners count to top listeners stat

--- frontend/js/src/entity-pages/AlbumPage.tsx
+++ frontend/js/src/entity-pages/AlbumPage.tsx
@@ -62,6 +62,7 @@ export default function AlbumPage(props: AlbumPageProps): JSX.Element {
   const {
     total_listen_count: listenCount,
     listeners: topListeners,
+    total_user_count: userCount,
   } = listening_stats;
 
   const [metadata, setMetadata] = React.useState(initialReleaseGroupMetadata);
@@ -376,7 +377,7 @@ export default function AlbumPage(props: AlbumPageProps): JSX.Element {
             <div className="separator" />
             <div className="text-center">
               <div className="number">
-                {bigNumberFormatter.format(topListeners.length)}
+                {bigNumberFormatter.format(userCount)}
               </div>
               <div className="text-muted small">
                 <FontAwesomeIcon icon={faUserAstronaut} /> listeners
