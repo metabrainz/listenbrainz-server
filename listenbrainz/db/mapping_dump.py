a1479a3573dd2628155b82c013ebe6979d09f1b8
Michael Wiencek <mwtuea@gmail.com>
2023-12-26 12:05:56 -0600

    Fix broken readthedocs links
    I noticed the "data dumps" link on the homepage was broken in the same way as
    https://github.com/metabrainz/listenbrainz-server/pull/2594.
    
    Then I searched for other instances of "en/latest/dev" and found even more
    broken links. I guess something changed with your readthedocs setup at some
    point (?).

--- listenbrainz/db/mapping_dump.py
+++ listenbrainz/db/mapping_dump.py
@@ -1,7 +1,7 @@
 """ This module contains data dump creation and import functions.
 
 Read more about the data dumps in our documentation here:
-https://listenbrainz.readthedocs.io/en/production/dev/listenbrainz-dumps.html
+https://listenbrainz.readthedocs.io/en/latest/users/listenbrainz-dumps.html
 """
 
 # listenbrainz-server - Server for the ListenBrainz project
