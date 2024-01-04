9a28d68f8176e111c42680b3f08316de40ffd062
anshg1214 <anshg1214@gmail.com>
2024-01-04 18:34:25 +0000

    feat: Setup Delete Pages

---
+++ frontend/js/src/settings/index.tsx
@@ -0,0 +1,46 @@
+import * as React from "react";
+
+import * as Sentry from "@sentry/react";
+import { Integrations } from "@sentry/tracing";
+import { createRoot } from "react-dom/client";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { ToastContainer } from "react-toastify";
+import ErrorBoundary from "../utils/ErrorBoundary";
+import GlobalAppContext from "../utils/GlobalAppContext";
+import { getPageProps } from "../utils/utils";
+import getSettingsRoutes from "./routes";
+
+document.addEventListener("DOMContentLoaded", () => {
+  const {
+    domContainer,
+    reactProps,
+    globalAppContext,
+    sentryProps,
+  } = getPageProps();
+  const { sentry_dsn, sentry_traces_sample_rate } = sentryProps;
+
+  if (sentry_dsn) {
+    Sentry.init({
+      dsn: sentry_dsn,
+      integrations: [new Integrations.BrowserTracing()],
+      tracesSampleRate: sentry_traces_sample_rate,
+    });
+  }
+
+  const routes = getSettingsRoutes();
+  const router = createBrowserRouter(routes);
+
+  const renderRoot = createRoot(domContainer!);
+  renderRoot.render(
+    <ErrorBoundary>
+      <ToastContainer
+        position="bottom-right"
+        autoClose={8000}
+        hideProgressBar
+      />
+      <GlobalAppContext.Provider value={globalAppContext}>
+        <RouterProvider router={router} />
+      </GlobalAppContext.Provider>
+    </ErrorBoundary>
+  );
+});
---
+++ frontend/js/src/settings/pages/DeleteListens.tsx
@@ -0,0 +1,106 @@
+import * as React from "react";
+
+import { redirect } from "react-router-dom";
+import { toast } from "react-toastify";
+import { ToastMsg } from "../../notifications/Notifications";
+import GlobalAppContext from "../../utils/GlobalAppContext";
+import { downloadFile } from "./ExportData";
+
+export default function DeleteListens() {
+  const { currentUser } = React.useContext(GlobalAppContext);
+  const { name } = currentUser;
+  const downloadListens = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+
+    try {
+      await downloadFile(window.location.href);
+      toast.success(
+        <ToastMsg
+          title="Success"
+          message="Your listens have been downloaded."
+        />
+      );
+    } catch (error) {
+      toast.error(
+        <ToastMsg
+          title="Error"
+          message={`Failed to download listens: ${error}`}
+        />
+      );
+    }
+  };
+
+  // eslint-disable-next-line consistent-return
+  const deleteListens = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+
+    try {
+      const response = await fetch(window.location.href, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+      });
+
+      toast.success(
+        <ToastMsg
+          title="Success"
+          message="Your listens have been deleted. You will be logged out in 5 seconds."
+        />
+      );
+
+      setTimeout(() => {
+        window.location.href = `/user/${name}/`;
+      }, 3000);
+    } catch (error) {
+      toast.error(
+        <ToastMsg
+          title="Error"
+          message={`Error while deleting listens for user ${name}`}
+        />
+      );
+
+      return redirect("/profile/");
+    }
+  };
+
+  return (
+    <>
+      <h3 className="page-title">Delete your listens</h3>
+      <p>
+        Hi {name}, are you sure you want to delete listens imported into your
+        ListenBrainz account?
+      </p>
+
+      <p>Once deleted, all your listens data will be removed PERMANENTLY.</p>
+
+      <p>
+        Note: you can export your ListenBrainz data before deleting your
+        listens.
+      </p>
+
+      <form onSubmit={downloadListens}>
+        <button
+          className="btn btn-warning btn-lg"
+          type="submit"
+          style={{ width: "250px" }}
+        >
+          Export my listens.
+        </button>
+      </form>
+      <br />
+      <p>Yes, I am sure I want to erase my entire listening history</p>
+
+      <form onSubmit={deleteListens}>
+        <button
+          id="btn-delete-listens"
+          className="btn btn-danger btn-lg"
+          type="submit"
+          style={{ width: "250px" }}
+        >
+          Delete my listens.
+        </button>
+      </form>
+    </>
+  );
+}
