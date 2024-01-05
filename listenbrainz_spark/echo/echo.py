ec8db3f7bfd50ae58b6565768f5eb45acbae81b3
Kartik Ohri <kartikohri13@gmail.com>
2024-01-05 10:40:54 +0530

    fix echo handler

--- listenbrainz_spark/echo/echo.py
+++ listenbrainz_spark/echo/echo.py
@@ -1,6 +1,8 @@
 def handler(message):
     """ Handler that echos its message it receives back to the queue as response as-is """
-    return {
-        "type": "echo",
-        "message": message
-    }
+    return [
+        {
+            "type": "echo",
+            "data": message["data"]
+        }
+    ]
