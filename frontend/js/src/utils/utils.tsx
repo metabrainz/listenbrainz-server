0545b60226d2a5a4778b74d3fcd2347b876a29a1
Monkey Do <MonkeyDo@users.noreply.github.com>
2024-01-31 12:48:20 +0100

    Remove outdated comment

--- frontend/js/src/utils/utils.tsx
+++ frontend/js/src/utils/utils.tsx
@@ -982,7 +982,7 @@ export function getReviewEventContent(
             readonly
             onClick={() => {}}
             className="rating-stars"
-            ratingValue={eventMetadata.rating} // CB stores ratings in 0 - 5 scale but the component requires 0 - 100 UPDATE: component requires 0-5 scale too
+            ratingValue={eventMetadata.rating}
             transition
             size={20}
             iconsCount={5}
