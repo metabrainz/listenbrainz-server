dcc88e235413663ec55b1d002c0d7cda62926fd6
anshg1214 <anshg1214@gmail.com>
2024-02-08 10:19:11 +0000

    feat: Add More pages to SPA

---
+++ frontend/js/src/import-data/ImportData.tsx
@@ -0,0 +1,27 @@
+import * as React from "react";
+
+export default function ImportData() {
+  return (
+    <>
+      <h2 className="page-title">Importing your data to Listenbrainz</h2>
+      <h3>Importing data from Last.fm</h3>
+
+      <p>
+        We encourage Last.fm users to save their listen histories to
+        ListenBrainz.
+      </p>
+      <p>
+        To help us test this service, please import your listen history from
+        Last.fm. To proceed, you will need a MusicBrainz account.
+      </p>
+      <div className="well" style={{ maxWidth: "600px", margin: "0 auto" }}>
+        <a
+          href="/settings/import/"
+          className="btn btn-primary btn-lg btn-block"
+        >
+          Import my listen history
+        </a>
+      </div>
+    </>
+  );
+}
