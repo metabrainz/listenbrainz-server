3ab8f26585b4b93e344835c3de9c281d4568037c
huhridge <sharmaharij21@gmail.com>
2024-04-16 18:41:42 +0530

    add: info tooltip
    Added the information tooltip that also opens the stats when clicked!

--- frontend/js/src/user/components/follow/CompatibilityCard.tsx
+++ frontend/js/src/user/components/follow/CompatibilityCard.tsx
@@ -1,9 +1,13 @@
 import * as React from "react";
 import { includes as _includes } from "lodash";
 
+import { faCircleInfo } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { Link } from "react-router-dom";
+import ReactTooltip from "react-tooltip";
 import Card from "../../../components/Card";
 import SimilarityScore from "./SimilarityScore";
+import GlobalAppContext from "../../../utils/GlobalAppContext";
 
 export type CompatibilityCardProps = {
   user: ListenBrainzUser;
@@ -17,6 +21,7 @@ export type CompatibilityCardProps = {
 
 function CompatibilityCard(props: CompatibilityCardProps) {
   const { user, similarityScore, similarArtists } = props;
+  const { currentUser } = React.useContext(GlobalAppContext);
 
   let content;
 
@@ -52,6 +57,15 @@ function CompatibilityCard(props: CompatibilityCardProps) {
 
   return (
     <Card id="compatibility-card" data-testid="compatibility-card">
+      <div className="info-icon" data-tip data-for="info-tooltip">
+        <Link
+          to={`/user/${currentUser.name}/stats/?range=all_time`}
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <FontAwesomeIcon icon={faCircleInfo} />
+        </Link>
+      </div>
       <span>Your compatibility with {user.name} :</span>
       <SimilarityScore
         similarityScore={similarityScore}
@@ -60,6 +74,10 @@ function CompatibilityCard(props: CompatibilityCardProps) {
       />
       <hr />
       {content}
+      <ReactTooltip id="info-tooltip" place="top">
+        These artists are calculated on the basis of the top 100 <br />
+        artists of all time for both of the users. Click for more stats!
+      </ReactTooltip>
     </Card>
   );
 }
