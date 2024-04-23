793432e8cd62b4211d28fd38a1085882b8b78683
Monkey Do <salicon.petiller@gmail.com>
2024-04-23 13:12:43 +0200

    Fix test environment issues
    JSDom doesn't play well with msw (mock service worker) library, but they came out with a fixed jsdom environment we can use.
    
    See https://github.com/mswjs/jest-fixed-jsdom  and https://mswjs.io/docs/migrations/1.x-to-2.x#requestresponsetextencoder-is-not-defined-jest

--- jest.config.js
+++ jest.config.js
@@ -17,7 +17,7 @@ module.exports = {
   moduleFileExtensions: ["ts", "tsx", "js", "json", "jsx"],
 
   // The paths to modules that run some code to configure or set up the testing environment before each test
-  setupFiles: ["<rootDir>/enzyme.config.ts", "jest-canvas-mock","<rootDir>/frontend/js/tests/node-globals-setup.ts"],
+  setupFiles: ["<rootDir>/enzyme.config.ts", "jest-canvas-mock"],
 
   setupFilesAfterEnv: [
     "jest-location-mock",
@@ -25,7 +25,7 @@ module.exports = {
   ],
 
   // The test environment that will be used for testing
-  testEnvironment: "jest-environment-jsdom",
+  testEnvironment: "jest-fixed-jsdom",
 
   // The glob patterns Jest uses to detect test files
   testMatch: ["**/?(*.)+(spec|test).(ts|js)?(x)"],
@@ -36,7 +36,7 @@ module.exports = {
   // This option sets the URL for the jsdom environment. It is reflected in properties such as location.href
   testEnvironmentOptions: {
     url: "http://localhost",
-    customExportConditions: [''],
+    customExportConditions: [""],
   },
   moduleNameMapper: {
     'react-markdown': '<rootDir>/node_modules/react-markdown/react-markdown.min.js',
