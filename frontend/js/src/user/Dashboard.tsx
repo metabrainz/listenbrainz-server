03670f25b4cedae58cad0ffe1ebb29e93b3dea72
anshg1214 <anshg1214@gmail.com>
2024-04-24 11:47:26 +0000

    refactor: formatting

--- frontend/js/src/user/Dashboard.tsx
+++ frontend/js/src/user/Dashboard.tsx
@@ -237,7 +237,7 @@ export default function Listen() {
     });
 
     const connectHandler = () => {
-      if (user){
+      if (user) {
         socket.emit("json", { user: user.name });
       }
     };
