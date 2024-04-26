a41bbddba4a717486b02ff936b7c7f530fbbc8e0
anshg1214 <anshg1214@gmail.com>
2024-04-26 07:33:53 +0000

    feat: Refactor Protected Routes Layout
    This prevents the navbar and the footer from re-rendering on switching between protected and public routes.

--- frontend/js/src/utils/ProtectedRoutes.tsx
+++ frontend/js/src/utils/ProtectedRoutes.tsx
@@ -2,7 +2,6 @@ import React from "react";
 
 import { Navigate, useLocation } from "react-router-dom";
 import GlobalAppContext from "./GlobalAppContext";
-import Layout from "../layout";
 
 function ProtectedRoutes() {
   const { currentUser } = React.useContext(GlobalAppContext);
@@ -11,7 +10,7 @@ function ProtectedRoutes() {
   const urlEncodedPathname = encodeURIComponent(pathname);
 
   return currentUser?.name ? (
-    <Layout />
+    <></>
   ) : (
     <Navigate to={`/login/?next=${urlEncodedPathname}`} />
   );
