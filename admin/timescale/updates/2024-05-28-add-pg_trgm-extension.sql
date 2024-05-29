82491b004c421dc60e84dc0e3a63b0502bcdc186
anshg1214 <anshg1214@gmail.com>
2024-05-29 13:40:27 +0000

    fix: Update Database name

--- admin/timescale/updates/2024-05-28-add-pg_trgm-extension.sql
+++ admin/timescale/updates/2024-05-28-add-pg_trgm-extension.sql
@@ -1,2 +1,2 @@
 CREATE EXTENSION IF NOT EXISTS "pg_trgm";
-ALTER DATABASE listenbrainz SET pg_trgm.word_similarity_threshold = 0.1;
+ALTER DATABASE listenbrainz_ts SET pg_trgm.word_similarity_threshold = 0.1;
