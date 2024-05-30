fb89d47facae0fc2fcf0fdcf2ae5a754bb8c59b2
anshg1214 <anshg1214@gmail.com>
2024-05-30 17:56:44 +0000

    format: PEP8

--- listenbrainz/db/playlist.py
+++ listenbrainz/db/playlist.py
@@ -465,7 +465,7 @@ def search_playlists_for_user(db_conn, ts_conn, user_id: int, query: str, count:
     """)
 
     result = ts_conn.execute(query, params)
-    playlists =  _get_playlist_for_search(db_conn, result)
+    playlists = _get_playlist_for_search(db_conn, result)
 
     # Fetch the total count of playlists
     total_count = 0
@@ -550,7 +550,7 @@ def search_playlist(db_conn, ts_conn, query: str, count: int = 0, offset: int = 
     """)
 
     result = ts_conn.execute(query, params)
-    playlists =  _get_playlist_for_search(db_conn, result)
+    playlists = _get_playlist_for_search(db_conn, result)
 
     # Fetch the total count of playlists
     total_count = 0
