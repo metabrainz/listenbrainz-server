f60b954ffce3153e42c5b1380caf3c987269ab30
Eric Deng <dev@ericd.me>
2024-06-01 10:12:38 +0800

    feed endpoint for user listens

--- listenbrainz/webserver/views/atom.py
+++ listenbrainz/webserver/views/atom.py
@@ -1,29 +1,39 @@
 from feedgen.feed import FeedGenerator
-from flask import Blueprint, Response, jsonify
-from listenbrainz.webserver.decorators import crossdomain
+from flask import Blueprint, Response
+from listenbrainz.webserver.decorators import crossdomain, api_listenstore_needed
+from brainzutils.ratelimit import ratelimit
+import listenbrainz.db.user as db_user
+from listenbrainz.webserver import db_conn, timescale_connection
 
 atom_bp = Blueprint("atom", __name__)
 
 
-@atom_bp.route("/feed/test", methods=["GET"])
+@atom_bp.route("/user/<user_name>/listens", methods=["GET"])
 @crossdomain
-def feed_test():
+@ratelimit()
+@api_listenstore_needed
+def get_listens(user_name):
+    user = db_user.get_by_mb_id(db_conn, user_name)
+    if user is None:
+        return Response(status=404)
+    
+    listens, _, _ = timescale_connection._ts.fetch_listens(user)
+
     fg = FeedGenerator()
-    fg.id("https://listenbrainz.org/feed/")
-    fg.title("My Feed for ercd")
+    fg.id(f"https://listenbrainz.org/user/{user_name}")
+    fg.title(f"Listens for {user_name}")
     fg.author({"name": "ListenBrainz"})
-    fg.link(href="https://listenbrainz.org/feed/", rel="alternate")
+    fg.link(href=f"https://listenbrainz.org/user/{user_name}", rel="alternate")
+    fg.link(href=f"https://listenbrainz.org/feed/user/{user_name}/listens", rel="self")
     fg.logo("https://listenbrainz.org/static/img/listenbrainz_logo_icon.svg")
-    fg.subtitle("This is a cool feed!")
-    fg.link(href="https://listenbrainz.org/atom/feed", rel="self")
     fg.language("en")
     
-    fe = fg.add_entry()
-    fe.id('https://example.com')
-    fe.title('ercd listened to Get Lucky - Daft Punk')
-    fe.link(href="https://example.com")
-    fe.content("ercd Listened to Get Lucky by Daft Punk on 2021-07-01 12:00:00")
-
+    for listen in listens:
+        fe = fg.add_entry()
+        fe.id(f"https://listenbrainz.org/user/{user_name}")
+        fe.title(f"{listen.user_name} listened to {listen.data['track_name']} - {listen.data['artist_name']} on {listen.timestamp}")
+        fe.link(href=f"https://listenbrainz.org/feed/user/{user_name}/listens", rel="self")
+    
     atomfeed = fg.atom_str(pretty=True)
 
     return Response(atomfeed, mimetype="application/atom+xml")
