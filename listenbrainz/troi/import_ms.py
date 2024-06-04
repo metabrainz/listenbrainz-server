fe73ee619683fbc500c984e019cf9eb878f017e5
Rimma Kubanova <rimma.kubanova@gmail.com>
2024-06-04 16:09:19 +0500

    fixed indentation

--- listenbrainz/troi/import_ms.py
+++ listenbrainz/troi/import_ms.py
@@ -1,6 +1,5 @@
 from troi.patches.playlist_from_ms import ImportPlaylistPatch
 
-
 def import_from_spotify(token, user, playlist_id):
     args = {
         "ms_token": token,
@@ -15,5 +14,5 @@ def import_from_spotify(token, user, playlist_id):
     playlist = patch.generate_playlist()
     result = playlist.get_jspf()
     result.update({'identifier': playlist.playlists[0].mbid})
-        
+
     return result
