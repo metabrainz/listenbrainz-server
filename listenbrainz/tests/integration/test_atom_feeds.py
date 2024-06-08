8b811ebe4226c64fa9a581dc3d821a52254cffe6
Eric Deng <dev@ericd.me>
2024-06-08 17:52:44 +0800

    add test for atom xml content

--- listenbrainz/tests/integration/test_atom_feeds.py
+++ listenbrainz/tests/integration/test_atom_feeds.py
@@ -1,13 +1,32 @@
 import json
+import time
 from listenbrainz.tests.integration import ListenAPIIntegrationTestCase
+from lxml import etree
 
 
 class AtomFeedsTestCase(ListenAPIIntegrationTestCase):
     def test_user_listens_feed(self):
-        with open(self.path_to_data_file('single_more_than_one_listen.json'), 'r') as f:
+        """
+        Check server sends valid listens feed.
+        """
+        with open(self.path_to_data_file("valid_single.json"), "r") as f:
             payload = json.load(f)
-        response = self.send_data(payload)
-        self.assert400(response)
-        self.assertEqual(response.json['code'], 400)
-        response = self.client.get('/atom/user/testuserpleaseignore/listens')
-        self.assertEqual(response.status_code, 200)
+
+        ts = int(time.time())
+        payload["payload"][0]["listened_at"] = ts
+        response = self.send_data(payload, recalculate=True)
+        self.assert200(response)
+        self.assertEqual(response.json["status"], "ok")
+
+        url = self.custom_url_for(
+            "atom.get_listens", user_name=self.user["musicbrainz_id"]
+        )
+        response = self.client.get(url, query_string={"interval": 1})
+        self.assert200(response)
+
+        xml_tree = etree.fromstring(response.data)
+        nsAtom = "{http://www.w3.org/2005/Atom}"
+        element = xml_tree.find(f"{nsAtom}id")
+        if element is None:
+            self.fail("No id element found in feed")
+        self.assertEqual(element.text, f"https://listenbrainz.org/user/{self.user['musicbrainz_id']}")
