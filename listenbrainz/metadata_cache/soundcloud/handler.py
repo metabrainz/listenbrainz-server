593e311bc7348070a5bb0567745c5f3801add1ca
Kartik Ohri <kartikohri13@gmail.com>
2024-06-27 00:21:16 +0530

    update docstrings, pagination limit, queue name

--- listenbrainz/metadata_cache/soundcloud/handler.py
+++ listenbrainz/metadata_cache/soundcloud/handler.py
@@ -29,7 +29,7 @@ class SoundcloudCrawlerHandler(BaseHandler):
     def __init__(self, app):
         super().__init__(
             name="listenbrainz-soundcloud-metadata-cache",
-            external_service_queue=app.config["EXTERNAL_SERVICES_SPOTIFY_CACHE_QUEUE"]
+            external_service_queue=app.config["EXTERNAL_SERVICES_SOUNDCLOUD_CACHE_QUEUE"]
         )
         self.app = app
 
@@ -49,6 +49,7 @@ class SoundcloudCrawlerHandler(BaseHandler):
         self.session.mount("https://", self.adapter)
 
     def make_request(self, endpoint, **kwargs):
+        """ Make an API request to a soundcloud partner api endpoint"""
         response = self.session.get(
             PARTNER_API_URL + endpoint,
             params={**kwargs, "client_id": self.app.config["SOUNDCLOUD_CLIENT_ID"]}
@@ -57,8 +58,11 @@ class SoundcloudCrawlerHandler(BaseHandler):
         return response.json()
 
     def make_paginated_request(self, endpoint, **kwargs):
+        """ Make an API request to a soundcloud partner api endpoint that supports pagination and retrieve all
+         items in the collection.
+        """
         tracks = []
-        data = self.make_request(endpoint, **kwargs)
+        data = self.make_request(endpoint, limit=100, **kwargs)
         tracks.extend(data["collection"])
 
         while True:
@@ -98,13 +102,13 @@ class SoundcloudCrawlerHandler(BaseHandler):
         )
 
     def fetch_track_from_track_urn(self, track_urn: str) -> SoundcloudTrack:
-        """ retrieve album data from spotify to store in the spotify metadata cache """
+        """ retrieve track data from soundcloud to store in the soundcloud metadata cache """
         data = self.make_request("/tracks/" + track_urn)
         track = self.transform_track(data)
         return track
 
     def fetch_tracks_from_user_urn(self, user_urn: str) -> list[SoundcloudTrack]:
-        """ lookup tracks of the given artist """
+        """ lookup tracks uploaded by the given user """
         if user_urn not in self.discovered_artists:
             self.discovered_artists.add(user_urn)
             self.metrics["discovered_artists_count"] += 1
@@ -121,6 +125,7 @@ class SoundcloudCrawlerHandler(BaseHandler):
         return tracks
 
     def discover_users_from_user_urn(self, user_urn: str) -> set[str]:
+        """ search for new users to crawl in the given user's liked tracks and followings """
         new_user_urns = set()
         user_likes = self.make_paginated_request("/users/" + user_urn + "/likes/tracks")
         for track in user_likes:
@@ -132,7 +137,7 @@ class SoundcloudCrawlerHandler(BaseHandler):
 
 
     def get_seed_ids(self) -> list[str]:
-        """ Retrieve spotify track ids from new releases for all markets"""
+        """ Retrieve soundcloud track ids from new releases for all markets"""
         data = self.make_request("/trending")
         genres = data["categories"]["music"]
 
@@ -146,6 +151,7 @@ class SoundcloudCrawlerHandler(BaseHandler):
 
     @staticmethod
     def insert(curs, tracks: list[SoundcloudTrack]):
+        """ insert a list of soundcloud tracks in the database """
         query = SQL("""
             INSERT INTO soundcloud_cache.artist (artist_id, name, data)
                  VALUES %s
@@ -176,7 +182,7 @@ class SoundcloudCrawlerHandler(BaseHandler):
         execute_values(curs, query, values)
 
     def update_cache(self, tracks: list[SoundcloudTrack]):
-        """ Insert multiple tracks from the same *user* in database and update their status in redis cache. """
+        """ Insert multiple tracks from in database and update their status in redis cache. """
         track_expires_at = datetime.now() + timedelta(days=TRACK_CACHE_TIME)
         track_expires_at_ts = int(track_expires_at.timestamp())
 
@@ -218,7 +224,6 @@ class SoundcloudCrawlerHandler(BaseHandler):
         """ Processes the list of items (example: track ids) received from the crawler. """
         all_items = set()
         for item_id in item_ids:
-            self.app.logger.info("Processing %s", item_id)
             if item_id.startswith(SOUNDCLOUD_TRACK_PREFIX):
                 items = self.process_track(item_id)
             elif item_id.startswith(SOUNDCLOUD_USER_PREFIX):
