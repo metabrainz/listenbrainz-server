3b420f56e741deabac02c594b2cbc8d9e991d2bc
Monkey Do <salicon.petiller@gmail.com>
2024-07-10 13:05:24 +0200

    Move meta tags to HTML template
    Otherwise, website crawlers that use the meta tags will not see anything as they do not wait for our JS bundles to load and Helmet to take over the meta tags

--- frontend/js/src/index.tsx
+++ frontend/js/src/index.tsx
@@ -32,44 +32,7 @@ document.addEventListener("DOMContentLoaded", async () => {
   renderRoot.render(
     <ErrorBoundary>
       <GlobalAppContext.Provider value={globalAppContext}>
-        <Helmet defaultTitle="ListenBrainz" titleTemplate="%s - ListenBrainz">
-          <meta
-            name="description"
-            content="Track, explore, visualise and share the music you listen to.
-          Follow your favourites and discover great new music."
-          />
-          {/* OpenGraph meta tags */}
-          <meta property="og:type" content="website" />
-          <meta property="og:title" content="ListenBrainz" />
-          <meta
-            property="og:description"
-            content="Track, explore, visualise and share the music you listen to.
-          Follow your favourites and discover great new music."
-          />
-          <meta property="og:url" content={window.location.href} />
-          {/* OpenGraph image meta tags */}
-          <meta
-            property="og:image"
-            content={`${window.location.origin}/static/img/share-header.png`}
-          />
-          <meta property="og:image:width" content="1280" />
-          <meta property="og:image:height" content="640" />
-          {/* Twitter meta tags */}
-          <meta name="twitter:title" content="ListenBrainz" />
-          <meta property="twitter:domain" content={window.location.hostname} />
-          <meta property="twitter:url" content={window.location.href} />
-          <meta
-            property="twitter:description"
-            content="Track, explore, visualise and share the music you listen to.
-          Follow your favourites and discover great new music."
-          />
-          {/* Twitter image meta tags */}
-          <meta name="twitter:card" content="summary_large_image" />
-          <meta
-            name="twitter:image"
-            content={`${window.location.origin}/static/img/share-header.png`}
-          />
-        </Helmet>
+        <Helmet defaultTitle="ListenBrainz" titleTemplate="%s - ListenBrainz" />
         <ReactQueryDevtool client={queryClient}>
           <RouterProvider router={router} />
         </ReactQueryDevtool>
