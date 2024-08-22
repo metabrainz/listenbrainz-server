6a6cd17f73b04d03c79fb822cc653c2ecf5b2153
Rimma Kubanova <rimma.kubanova@gmail.com>
2024-08-22 21:53:07 +0500

    soundcloud export & import

---
+++ frontend/js/src/utils/soundcloudTypes.d.ts
@@ -0,0 +1,39 @@
+declare type SoundCloudUser = {
+    access_token?: string;
+  };
+
+declare type SoundCloudTrackObject = {
+    id: string;
+    permalink_url: string;
+    artwork_url?: string;
+    title: string;
+    uri: string;
+    duration: number;
+    user: {
+      id: string;
+      username: string;
+      avatar_url?: string;
+    };
+};
+
+declare type SoundCloudPlaylistObject = {
+    id: string;
+    title: string;
+    description?: string;
+    tracks: SoundCloudTrackObject[];
+    created_at: string;
+    duration: number;
+    artwork_url?: string;
+    genre?: string;
+    user: {
+      id: number;
+      username: string;
+      avatar_url?: string; 
+    };
+};
+
+declare type SoundcloudAPIError = {
+    code: integer;
+    message: string;
+    link: string;
+};
--- listenbrainz/troi/import_ms.py
+++ listenbrainz/troi/import_ms.py
@@ -18,7 +18,6 @@ def import_from_spotify(token, user, playlist_id):
     result.update({"identifier": playlist.playlists[0].mbid})
     return result
 
-
 def import_from_apple_music(token, apple_user_token, user, playlist_id):
     args = {
         "ms_token": token,
@@ -36,4 +35,22 @@ def import_from_apple_music(token, apple_user_token, user, playlist_id):
     result = playlist.get_jspf()
     result.update({'identifier': playlist.playlists[0].mbid})
 
+    return result
+
+def import_from_soundcloud(token, user, playlist_id):
+    args = {
+        "ms_token": token,
+        "token": user,
+        "playlist_id": playlist_id,
+        "music_service": 'soundcloud',
+        "apple_user_token": "",
+        "upload": True,
+        "created_for": None,
+        "echo": False,
+        "min_recordings": 1
+    }
+    patch = ImportPlaylistPatch(args)
+    playlist = patch.generate_playlist()
+    result = playlist.get_jspf()
+    result.update({"identifier": playlist.playlists[0].mbid})
     return result
