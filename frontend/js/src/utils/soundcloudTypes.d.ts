483b70bea22fefd764c744dab71731cc44b62065
anshg1214 <anshg1214@gmail.com>
2024-08-30 11:09:34 +0000

    format: Lint files

--- frontend/js/src/utils/soundcloudTypes.d.ts
+++ frontend/js/src/utils/soundcloudTypes.d.ts
@@ -1,39 +1,39 @@
 declare type SoundCloudUser = {
-    access_token?: string;
-  };
+  access_token?: string;
+};
 
 declare type SoundCloudTrackObject = {
+  id: string;
+  permalink_url: string;
+  artwork_url?: string;
+  title: string;
+  uri: string;
+  duration: number;
+  user: {
     id: string;
-    permalink_url: string;
-    artwork_url?: string;
-    title: string;
-    uri: string;
-    duration: number;
-    user: {
-      id: string;
-      username: string;
-      avatar_url?: string;
-    };
+    username: string;
+    avatar_url?: string;
+  };
 };
 
 declare type SoundCloudPlaylistObject = {
-    id: string;
-    title: string;
-    description?: string;
-    tracks: SoundCloudTrackObject[];
-    created_at: string;
-    duration: number;
-    artwork_url?: string;
-    genre?: string;
-    user: {
-      id: number;
-      username: string;
-      avatar_url?: string; 
-    };
+  id: string;
+  title: string;
+  description?: string;
+  tracks: SoundCloudTrackObject[];
+  created_at: string;
+  duration: number;
+  artwork_url?: string;
+  genre?: string;
+  user: {
+    id: number;
+    username: string;
+    avatar_url?: string;
+  };
 };
 
 declare type SoundcloudAPIError = {
-    code: integer;
-    message: string;
-    link: string;
+  code: integer;
+  message: string;
+  link: string;
 };
