e0b4f2ff013b5ee12200308198a94370bd128f2b
Kartik Ohri <kartikohri13@gmail.com>
2024-10-15 19:31:51 -0600

    improve test, minor fixes

--- listenbrainz/tests/integration/test_export.py
+++ listenbrainz/tests/integration/test_export.py
@@ -6,6 +6,11 @@ from io import BytesIO
 from brainzutils import cache
 
 import listenbrainz.db.user as db_user
+import listenbrainz.db.pinned_recording as db_pinned_rec
+import listenbrainz.db.feedback as db_feedback
+from listenbrainz.db.model.feedback import Feedback
+
+from listenbrainz.db.model.pinned_recording import WritablePinnedRecording
 from listenbrainz.listenstore.timescale_utils import recalculate_all_user_data
 from listenbrainz.tests.integration import ListenAPIIntegrationTestCase
 
@@ -40,11 +45,62 @@ class ExportTestCase(ListenAPIIntegrationTestCase):
         self.temporary_login(self.user['login_id'])
 
         self.send_listens()
-        url = self.custom_url_for('api_v1.get_listens',
-                                  user_name=self.user['musicbrainz_id'])
+        url = self.custom_url_for('api_v1.get_listens', user_name=self.user['musicbrainz_id'])
         response = self.wait_for_query_to_have_items(url, 1, query_string={'count': '1'})
         data = json.loads(response.data)['payload']
         self.assert200(response)
+
+        pinned_recordings = [
+            {
+                "recording_msid": "7f3d82ee-3817-4367-9eec-f33a312247a1",
+                "recording_mbid": "83b57de1-7f69-43cb-a0df-5f77a882e954",
+                "blurb_content": "Amazing first recording",
+            },
+            {
+                "recording_msid": "7f3d82ee-3817-4367-9eec-f33a312247a1",
+                "recording_mbid": "7e4142f4-b01e-4492-ae13-553493bad634",
+                "blurb_content": "Wonderful second recording",
+            },
+        ]
+        for pin_rec in pinned_recordings:
+            db_pinned_rec.pin(
+                self.db_conn,
+                WritablePinnedRecording(
+                    user_id=self.user["id"],
+                    recording_msid=pin_rec["recording_msid"],
+                    recording_mbid=pin_rec["recording_mbid"],
+                    blurb_content=pin_rec["blurb_content"],
+                )
+            )
+
+        feedback = [
+            {
+                "recording_msid": "d23f4719-9212-49f0-ad08-ddbfbfc50d6f",
+                "score": 1
+            },
+            {
+                "recording_msid": "222eb00d-9ead-42de-aec9-8f8c1509413d",
+                "score": -1
+            },
+            {
+                "recording_mbid": "076255b4-1575-11ec-ac84-135bf6a670e3",
+                "score": 1
+            },
+            {
+                "recording_mbid": "1fd178b4-1575-11ec-b98a-d72392cd8c97",
+                "score": -1
+            }
+        ]
+        for fb in feedback:
+            db_feedback.insert(
+                self.db_conn,
+                Feedback(
+                    user_id=self.user["id"],
+                    recording_msid=fb.get("recording_msid"),
+                    recording_mbid=fb.get("recording_mbid"),
+                    score=fb["score"]
+                )
+            )
 
         response = self.client.post(self.custom_url_for('export.create_export_task'))
         self.assert200(response)
@@ -104,3 +160,23 @@ class ExportTestCase(ListenAPIIntegrationTestCase):
                 self.assertEqual(expected["track_metadata"]["artist_name"], received["track_metadata"]["artist_name"])
                 self.assertEqual(expected["track_metadata"]["release_name"], received["track_metadata"]["release_name"])
                 self.assertEqual(expected["listened_at"], received["listened_at"])
+
+            with export_zip.open("pinned_recording.jsonl", "r") as f:
+                received_pins = []
+                for line in f:
+                    received_pins.append(json.loads(line.strip()))
+
+            for expected, received in zip(pinned_recordings, received_pins):
+                self.assertEqual(expected["recording_msid"], received["recording_msid"])
+                self.assertEqual(expected["recording_mbid"], received["recording_mbid"])
+                self.assertEqual(expected["blurb_content"], received["blurb_content"])
+
+            with export_zip.open("feedback.jsonl", "r") as f:
+                received_feedback = []
+                for line in f:
+                    received_feedback.append(json.loads(line.strip()))
+
+            for expected, received in zip(feedback, received_feedback):
+                self.assertEqual(expected.get("recording_msid"), received.get("recording_msid"))
+                self.assertEqual(expected.get("recording_mbid"), received.get("recording_mbid"))
+                self.assertEqual(expected["score"], received["score"])
