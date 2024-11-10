29caf9e9394f9820a4144fc42b5b594d149b0973
anshg1214 <anshg1214@gmail.com>
2024-11-10 17:11:21 +0000

    feat: Add Username Component

---
+++ frontend/js/src/common/Username.tsx
@@ -0,0 +1,50 @@
+import * as React from "react";
+import { Link } from "react-router-dom";
+import useUserFlairs from "../utils/FlairLoader";
+
+function Username({
+  username,
+  elementType,
+  hideFlair = false,
+  showLink = false,
+  ...otherProps
+}: {
+  username: string;
+  elementType?: keyof JSX.IntrinsicElements;
+  hideFlair?: boolean;
+  showLink?: boolean;
+  [key: string]: any;
+}) {
+  const Element = elementType;
+  const flairType = useUserFlairs(username);
+
+  if (showLink) {
+    return (
+      <Link
+        to={`/user/${username}/`}
+        {...otherProps}
+        className={`${otherProps?.className || ""} ${
+          !hideFlair ? flairType || "" : ""
+        }`}
+      >
+        {username}
+      </Link>
+    );
+  }
+  if (Element) {
+    return (
+      <Element
+        {...otherProps}
+        className={`${otherProps?.className || ""} ${
+          !hideFlair ? flairType || "" : ""
+        }`}
+      >
+        {username}
+      </Element>
+    );
+  }
+  // eslint-disable-next-line react/jsx-no-useless-fragment
+  return <>{username}</>;
+}
+
+export default Username;
