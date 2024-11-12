9270a8f98d109291a3066c3df8802196b936042b
Robert Kaye <rob@musicbrainz.org>
2024-11-12 12:25:30 +0100

    Add stats monitoring endpoint

--- listenbrainz/webserver/views/status_api.py
+++ listenbrainz/webserver/views/status_api.py
@@ -1,8 +1,15 @@
-from flask import Blueprint, request, jsonify
+from flask import Blueprint, request, jsonify, current_app
+import requests
+from time import sleep
+
 from listenbrainz.webserver.errors import APIBadRequest, APINotFound
 from brainzutils.ratelimit import ratelimit
+from brainzutils import cache
 
 import listenbrainz.db.dump as db_dump
+
+STATUS_PREFIX = 'listenbrainz.status'  # prefix used in key to cache status
+CACHE_TIME = 60 * 60  # time in seconds we cache the stats
 
 status_api_bp = Blueprint("status_api_v1", __name__)
 
@@ -64,3 +71,38 @@ def _convert_timestamp_to_string_dump_format(timestamp):
         String of the format "20190625-170100"
     """
     return timestamp.strftime("%Y%m%d-%H%M%S")
+
+
+@status_api_bp.route("/get-stats-info", methods=["GET"])
+@ratelimit()
+def get_stats_info():
+    """ Check to see when statistics were last generated for a "random" user. Returns JSON:
+
+    .. code-block:: json
+
+        {
+            "last_updated": 19243535
+        }
+
+    :statuscode 200: You have data.
+    :resheader Content-Type: *application/json*
+    """
+
+    last_updated = cache.get(STATUS_PREFIX + ".stats")
+    if last_updated is None:
+        current_app.logger.warn("no cached data!")
+        url = current_app.config["API_URL"] + "/1/stats/user/rob/artists"
+        while True:
+            r = requests.get(url)
+            if r.status_code == 419:
+                sleep(1)
+                continue
+
+            if r.status_code == 200:
+                break
+
+        last_updated = r.json()["payload"]["last_updated"]
+        cache.set(STATUS_PREFIX + ".stats", last_updated, CACHE_TIME)
+
+    return jsonify({ "last_updated": last_updated })
+
