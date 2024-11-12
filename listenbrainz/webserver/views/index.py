c0f45d504aa5d2fcab40d7549a7816b7bea1bb4b
Robert Kaye <rob@musicbrainz.org>
2024-11-12 17:42:24 +0100

    Service status is now returned for the current-status page

--- listenbrainz/webserver/views/index.py
+++ listenbrainz/webserver/views/index.py
@@ -1,5 +1,4 @@
 import locale
-import os
 import requests
 import time
 
@@ -21,6 +20,7 @@ from listenbrainz.webserver.decorators import web_listenstore_needed
 from listenbrainz.webserver import flash, db_conn, meb_conn, ts_conn
 from listenbrainz.webserver.timescale_connection import _ts
 from listenbrainz.webserver.redis_connection import _redis
+from listenbrainz.webserver.views.status_api import get_service_status 
 import listenbrainz.db.stats as db_stats
 import listenbrainz.db.user_relationship as db_user_relationship
 from listenbrainz.db.donation import get_recent_donors
@@ -90,8 +90,7 @@ def blog_data():
 @web_listenstore_needed
 def current_status():
 
-    load = "%.2f %.2f %.2f" % os.getloadavg()
-
+    service_status = get_service_status() 
     listen_count = _ts.get_total_listen_count()
     try:
         user_count = format(int(_get_user_count()), ',d')
@@ -113,7 +112,7 @@ def current_status():
         })
 
     data = {
-        "load": load,
+        "service-status": service_status,
         "listenCount": format(int(listen_count), ",d") if listen_count else "0",
         "userCount": user_count,
         "listenCountsPerDay": listen_counts_per_day,
