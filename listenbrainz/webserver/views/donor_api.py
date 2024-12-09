7e9691e158a32b41a7bac2dd699e2c348449165c
Kartik Ohri <kartikohri13@gmail.com>
2024-12-09 14:35:02 +0530

    Fix donor apis for local development
    If SQLALCHEMY_METABRAINZ_URI is not configured, return [] for donors.

--- listenbrainz/webserver/views/donor_api.py
+++ listenbrainz/webserver/views/donor_api.py
@@ -1,5 +1,5 @@
 from brainzutils.ratelimit import ratelimit
-from flask import Blueprint, jsonify
+from flask import Blueprint, jsonify, current_app
 
 from listenbrainz.db.donation import get_recent_donors, get_biggest_donors
 from listenbrainz.webserver import db_conn, meb_conn
@@ -22,6 +22,9 @@ def recent_donors():
     count = _parse_int_arg("count", DEFAULT_DONOR_COUNT)
     offset = _parse_int_arg("offset", 0)
 
+    if not current_app.config["SQLALCHEMY_METABRAINZ_URI"]:
+        return []
+
     donors, _ = get_recent_donors(meb_conn, db_conn, count, offset)
     return jsonify(donors)
 
@@ -35,6 +38,9 @@ def biggest_donors():
     """
     count = _parse_int_arg("count", DEFAULT_DONOR_COUNT)
     offset = _parse_int_arg("offset", 0)
+
+    if not current_app.config["SQLALCHEMY_METABRAINZ_URI"]:
+        return []
 
     donors, _ = get_biggest_donors(meb_conn, db_conn, count, offset)
     return jsonify(donors)
