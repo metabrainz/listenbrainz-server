3b9842843696098d37cdce3bd7f963e6d1869a0d
anshg1214 <anshg1214@gmail.com>
2024-12-18 18:21:57 +0000

    format: PEP8 - Lint SQL Query

--- listenbrainz/db/genre.py
+++ listenbrainz/db/genre.py
@@ -3,18 +3,18 @@ from psycopg2.extras import DictCursor
 
 def load_genre_with_subgenres(mb_curs: DictCursor):
     query = """
-        SELECT 
+        SELECT
             g1.name as genre,
             g1.gid::text as genre_gid,
             g2.name as subgenre,
             g2.gid::text as subgenre_gid
         FROM genre g1
         LEFT JOIN (
-            SELECT entity0, entity1 
+            SELECT entity0, entity1
             FROM l_genre_genre lgg
             WHERE lgg.link IN (
-                SELECT id 
-                FROM link 
+                SELECT id
+                FROM link
                 WHERE link_type = 1095
             )
         ) lgg ON g1.id = lgg.entity0
