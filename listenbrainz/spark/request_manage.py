3f273f319f5ac2e43d8c8c20b9cb5aa4a0f041fb
Kartik Ohri <kartikohri13@gmail.com>
2025-01-07 01:09:06 +0530

    fix stats type check

--- listenbrainz/spark/request_manage.py
+++ listenbrainz/spark/request_manage.py
@@ -103,7 +103,7 @@ def request_user_stats(type_, range_, entity, database):
     if type_ in ["entity", "listener"] and entity:
         params["entity"] = entity
 
-    if not database and type_ != entity:
+    if not database and type_ != "entity":
         today = date.today().strftime("%Y%m%d")
         if type_ == "listeners":
             prefix = f"{entity}_listeners"
