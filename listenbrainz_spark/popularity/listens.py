2c82f166e3b2a5549e14371b25f55b2bd661d98f
Kartik Ohri <kartikohri13@gmail.com>
2025-01-19 23:54:28 +0530

    Incrementalize Popularity Generation

---
+++ listenbrainz_spark/popularity/listens.py
@@ -0,0 +1,148 @@
+from typing import List
+
+from listenbrainz_spark.path import LISTENBRAINZ_POPULARITY_DIRECTORY, RELEASE_METADATA_CACHE_DATAFRAME
+from listenbrainz_spark.popularity.common import get_popularity_per_artist_query, \
+    get_release_group_popularity_per_artist_query, get_popularity_query, get_release_group_popularity_query
+from listenbrainz_spark.stats.incremental.provider import Provider
+from listenbrainz_spark.stats.incremental.range_selector import ListenRangeSelector
+
+
+class PopularityProvider(Provider):
+
+    def __init__(self, selector: ListenRangeSelector, entity: str):
+        super().__init__(selector)
+        self._entity = entity
+
+    @property
+    def entity(self):
+        return self._entity
+
+    def get_table_prefix(self) -> str:
+        return f"popularity_{self.entity}_{self.stats_range}"
+
+    def get_base_path(self) -> str:
+        return LISTENBRAINZ_POPULARITY_DIRECTORY
+
+    def get_filter_aggregate_query(self, existing_aggregate: str, incremental_aggregate: str) -> str:
+        entity_id = self.get_entity_id()
+        return f"""
+            WITH incremental_users AS (
+                SELECT DISTINCT {entity_id} FROM {incremental_aggregate}
+            )
+            SELECT *
+              FROM {existing_aggregate} ea
+             WHERE EXISTS(SELECT 1 FROM incremental_users iu WHERE iu.{entity_id} = ea.{entity_id})
+        """
+
+    def get_cache_tables(self) -> List[str]:
+        if self.entity == "release_group":
+            return [RELEASE_METADATA_CACHE_DATAFRAME]
+        return []
+
+    def get_entity_id(self):
+        return self.entity + "_mbid"
+
+    def get_aggregate_query(self, table: str, cache_tables: List[str]) -> str:
+        if self.entity == "artist":
+            return get_popularity_per_artist_query("artist", table)
+        elif self.entity == "release_group":
+            return get_release_group_popularity_per_artist_query(table, cache_tables[0])
+        else:
+            return get_popularity_query(self.entity, table)
+
+    def get_stats_query(self, final_aggregate: str) -> str:
+        return f"SELECT * FROM {final_aggregate}"
+
+    def get_combine_aggregates_query(self, existing_aggregate: str, incremental_aggregate: str) -> str:
+        entity_mbid = self.get_entity_id()
+        return f"""
+          WITH intermediate_table AS (
+            SELECT {entity_mbid}
+                 , total_listen_count
+                 , total_user_count
+              FROM {existing_aggregate}
+             UNION ALL
+            SELECT {entity_mbid}
+                 , total_listen_count
+                 , total_user_count
+              FROM {incremental_aggregate}
+          )
+            SELECT {entity_mbid}
+                 , SUM(total_listen_count) AS total_listen_count
+                 , SUM(total_user_count) AS total_user_count
+              FROM intermediate_table
+          GROUP BY {entity_mbid}
+        """
+
+
+class TopPerArtistPopularityProvider(Provider):
+
+    def __init__(self, selector: ListenRangeSelector, entity: str):
+        super().__init__(selector)
+        self._entity = entity
+
+    @property
+    def entity(self):
+        return self._entity
+
+    def get_table_prefix(self) -> str:
+        return f"popularity_top_per_artist_{self.entity}_{self.stats_range}"
+
+    def get_base_path(self) -> str:
+        return LISTENBRAINZ_POPULARITY_DIRECTORY
+
+    def get_filter_aggregate_query(self, existing_aggregate: str, incremental_aggregate: str) -> str:
+        entity_id = self.get_entity_id()
+        return f"""
+            WITH incremental_artists AS (
+                SELECT DISTINCT artist_mbid, {entity_id} FROM {incremental_aggregate}
+            )
+            SELECT *
+              FROM {existing_aggregate} ea
+             WHERE EXISTS(SELECT 1
+                            FROM incremental_users iu
+                           WHERE iu.{entity_id} = ea.{entity_id}
+                             AND iu.artist_mbid = ea.artist_mbid
+             )
+        """
+
+    def get_cache_tables(self) -> List[str]:
+        if self.entity == "release_group":
+            return [RELEASE_METADATA_CACHE_DATAFRAME]
+        return []
+
+    def get_entity_id(self):
+        return self.entity + "_mbid"
+
+    def get_aggregate_query(self, table: str, cache_tables: List[str]) -> str:
+        if self.entity == "release_group":
+            return get_release_group_popularity_query(table, cache_tables[0])
+        return get_popularity_per_artist_query(self.entity, table)
+
+    def get_stats_query(self, final_aggregate: str) -> str:
+        return f"SELECT * FROM {final_aggregate}"
+
+    def get_combine_aggregates_query(self, existing_aggregate: str, incremental_aggregate: str) -> str:
+        entity_mbid = self.get_entity_id()
+        return f"""
+          WITH intermediate_table AS (
+            SELECT artist_mbid
+                 , {entity_mbid}
+                 , total_listen_count
+                 , total_user_count
+              FROM {existing_aggregate}
+             UNION ALL
+            SELECT artist_mbid
+                 , {entity_mbid}
+                 , total_listen_count
+                 , total_user_count
+              FROM {incremental_aggregate}
+          )
+            SELECT artist_mbid
+                 , {entity_mbid}
+                 , SUM(total_listen_count) AS total_listen_count
+                 , SUM(total_user_count) AS total_user_count
+              FROM intermediate_table
+          GROUP BY artist_mbid
+                 , {entity_mbid}
+        """
--- listenbrainz_spark/popularity/main.py
+++ listenbrainz_spark/popularity/main.py
@@ -1,142 +1,33 @@
+import logging
 from datetime import datetime
 
-from more_itertools import chunked
-
-from listenbrainz_spark import config
 from listenbrainz_spark.constants import LAST_FM_FOUNDING_YEAR
-from listenbrainz_spark.path import MLHD_PLUS_DATA_DIRECTORY, RELEASE_METADATA_CACHE_DATAFRAME
-from listenbrainz_spark.stats import run_query
-from listenbrainz_spark.utils import get_listens_from_dump, read_files_from_HDFS, get_latest_listen_ts
+from listenbrainz_spark.popularity.common import PopularityMessageCreator
+from listenbrainz_spark.popularity.listens import PopularityProvider, TopPerArtistPopularityProvider
+from listenbrainz_spark.popularity.mlhd import MlhdAggregator
+from listenbrainz_spark.stats.incremental.aggregator import Aggregator
+from listenbrainz_spark.stats.incremental.range_selector import FromToRangeListenRangeSelector
+from listenbrainz_spark.utils import get_latest_listen_ts
 
-STATS_PER_MESSAGE = 10000
-
-
-def generate_popularity_stats(name, query, mlhd):
-    """ Execute the given query and generate statistics. """
-    df = run_query(query)
-    df\
-        .write\
-        .format("parquet")\
-        .save(config.HDFS_CLUSTER_URI + "/" + name, mode="overwrite")
-
-    itr = df.toLocalIterator()
-
-    for rows in chunked(itr, STATS_PER_MESSAGE):
-        entries = []
-        for row in rows:
-            entry = row.asDict(recursive=True)
-            entries.append(entry)
-
-        yield {
-            "type": name,
-            "data": entries,
-            "mlhd": mlhd
-        }
+logger = logging.getLogger(__name__)
 
 
-def get_release_group_popularity_query(table, rel_cache_table):
-    """ Get the query to generate release group popularity stats using both MLHD+ and listens data """
-    return f"""
-        SELECT rel.release_group_mbid
-             , count(*) AS total_listen_count
-             , count(DISTINCT user_id) AS total_user_count
-          FROM {table} t
-          JOIN {rel_cache_table} rel
-            ON t.release_mbid = rel.release_mbid
-           AND rel.release_group_mbid != ""
-      GROUP BY rel.release_group_mbid
-    """
-
-
-def get_release_group_popularity_per_artist_query(table, rel_cache_table):
-    """ Get the query to generate release group popularity per artists stats using both MLHD+ and listens data """
-    return f"""
-        WITH intermediate AS (
-            SELECT explode(artist_credit_mbids) AS artist_mbid
-                 , release_mbid
-                 , user_id
-              FROM {table}
-        )   SELECT artist_mbid
-                 , rel.release_group_mbid
-                 , count(*) AS total_listen_count
-                 , count(DISTINCT user_id) AS total_user_count
-              FROM intermediate i
-              JOIN {rel_cache_table} rel
-                ON i.release_mbid = rel.release_mbid
-             WHERE artist_mbid IS NOT NULL
-               AND rel.release_group_mbid != ""
-          GROUP BY artist_mbid
-                 , rel.release_group_mbid
-    """
-
-
-def get_popularity_query(entity, table):
-    """ Get the query to generate popularity stats for the given entity using both MLHD+ and listens data """
-    entity_mbid = f"{entity}_mbid"
-    return f"""
-        SELECT {entity_mbid}
-             , count(*) AS total_listen_count
-             , count(DISTINCT user_id) AS total_user_count
-          FROM {table}
-         WHERE {entity_mbid} IS NOT NULL
-           AND {entity_mbid} != ""
-      GROUP BY {entity_mbid}
-    """
-
-
-def get_popularity_per_artist_query(entity, table):
-    """ Get the query to generate top popular entities per artists stats from MLHD+ and listens data """
-    if entity == "artist":
-        select_clause = "artist_mbid"
-        explode_clause = "explode(artist_credit_mbids) AS artist_mbid"
-        where_clause = "artist_mbid IS NOT NULL"
-    else:
-        entity_mbid = f"{entity}_mbid"
-        select_clause = f"artist_mbid, {entity_mbid}"
-        explode_clause = f"explode(artist_credit_mbids) AS artist_mbid, {entity_mbid}"
-        where_clause = f"artist_mbid IS NOT NULL AND {entity_mbid} IS NOT NULL AND {entity_mbid} != ''"
-    return f"""
-        WITH intermediate AS (
-            SELECT {explode_clause}
-                 , user_id
-              FROM {table}
-        )   SELECT {select_clause}
-                 , count(*) AS total_listen_count
-                 , count(DISTINCT user_id) AS total_user_count
-              FROM intermediate
-             WHERE {where_clause}
-          GROUP BY {select_clause}
-    """
-
-
-def main(mlhd):
+def main(type, entity, is_mlhd):
     """ Generate popularity data for MLHD data. """
-    if mlhd:
-        table = f"parquet.`{MLHD_PLUS_DATA_DIRECTORY}`"
+    start = datetime(LAST_FM_FOUNDING_YEAR, 1, 1)
+    end = get_latest_listen_ts()
+
+    selector = FromToRangeListenRangeSelector(start, end)
+    selector.stats_range = type
+
+    if type == "popularity":
+        provider = PopularityProvider(selector, entity)
     else:
-        table = "listens_popularity"
-        start = datetime(LAST_FM_FOUNDING_YEAR, 1, 1)
-        end = get_latest_listen_ts()
-        get_listens_from_dump(start, end).createOrReplaceTempView(table)
+        provider = TopPerArtistPopularityProvider(selector, entity)
+    message_creator = PopularityMessageCreator(entity=entity, message_type=type, is_mlhd=is_mlhd)
 
-    rel_cache_table = "release_data_cache"
-    read_files_from_HDFS(RELEASE_METADATA_CACHE_DATAFRAME).createOrReplaceTempView(rel_cache_table)
-
-    queries = {
-        "popularity_top_recording": get_popularity_per_artist_query("recording", table),
-        "popularity_recording": get_popularity_query("recording", table),
-        "popularity_release": get_popularity_query("release", table),
-        "popularity_release_group": get_release_group_popularity_query(table, rel_cache_table),
-        "popularity_top_release_group": get_release_group_popularity_per_artist_query(table, rel_cache_table),
-        "popularity_artist": get_popularity_per_artist_query("artist", table),
-        "popularity_top_release": get_popularity_per_artist_query("release", table)
-    }
-
-    for name, query in queries.items():
-        if mlhd:
-            name = "mlhd_" + name
-
-        yield {"type": f"{name}_start"}
-        for message in generate_popularity_stats(name, query, mlhd):
-            yield message
-        yield {"type": f"{name}_end"}
+    if is_mlhd:
+        aggregator = MlhdAggregator(provider, message_creator)
+    else:
+        aggregator = Aggregator(provider, message_creator)
+    return aggregator.main()
