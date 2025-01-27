b9a0f2943b26b14c73ad2c8cb749cdafc6e2d7da
Suvid Singhal <anooptest100@gmail.com>
2025-01-27 18:27:32 +0530

    Add value to the correct enum

--- admin/sql/updates/2025-01-27-add-thanks-event-type.sql
+++ admin/sql/updates/2025-01-27-add-thanks-event-type.sql
@@ -1,1 +1,1 @@
-ALTER TYPE hide_user_timeline_event_type_enum ADD VALUE 'thanks' AFTER 'personal_recording_recommendation';
+ALTER TYPE user_timeline_event_type_enum ADD VALUE 'thanks' AFTER 'personal_recording_recommendation';
