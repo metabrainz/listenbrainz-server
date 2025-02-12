1f4e9e9f0797c66fe4a6a537c49dc54082447d5e
Kartik Ohri <kartikohri13@gmail.com>
2025-02-12 20:42:02 +0530

    generate artist map stats

--- listenbrainz_spark/stats/user/entity.py
+++ listenbrainz_spark/stats/user/entity.py
@@ -3,6 +3,7 @@ from typing import Iterator, Optional, Dict, Type
 
 from pandas import DataFrame
 
+from listenbrainz_spark.stats import run_query
 from listenbrainz_spark.stats.incremental.incremental_stats_engine import IncrementalStatsEngine
 from listenbrainz_spark.stats.incremental.range_selector import StatsRangeListenRangeSelector
 from listenbrainz_spark.stats.incremental.user.artist import ArtistUserEntity
@@ -40,7 +41,8 @@ def get_entity_stats(entity: str, stats_range: str, database: str = None) -> Ite
         artist_map_database = database.replace("artists", "artist_map") if database else None
         artist_map_entity = ArtistMapUserEntity(selector, NUMBER_OF_TOP_ENTITIES)
         artist_map_message_creator = ArtistMapStatsMessageCreator(entity, "user_entity", selector, artist_map_database)
-        artist_map_results = artist_map_entity.get_stats_query(engine._final_table, engine._cache_tables)
+        artist_map_query = artist_map_entity.get_stats_query(engine._final_table, engine._cache_tables)
+        artist_map_results = run_query(artist_map_query)
         artist_map_stats = engine.create_messages(artist_map_results, engine._only_inc, artist_map_message_creator)
 
         for message in artist_map_stats:
