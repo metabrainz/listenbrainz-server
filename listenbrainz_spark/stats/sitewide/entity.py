749a787812c213eb9505ddb442ff475b80925f59
Kartik Ohri <kartikohri13@gmail.com>
2025-02-13 01:08:21 +0530

    move sitewide artist map stat to spark

--- listenbrainz_spark/stats/sitewide/entity.py
+++ listenbrainz_spark/stats/sitewide/entity.py
@@ -1,10 +1,12 @@
 import logging
 from typing import Dict, Iterator, Type
 
-from listenbrainz_spark.stats import SITEWIDE_STATS_ENTITY_LIMIT
+from listenbrainz_spark.stats import SITEWIDE_STATS_ENTITY_LIMIT, run_query
 from listenbrainz_spark.stats.incremental.incremental_stats_engine import IncrementalStatsEngine
 from listenbrainz_spark.stats.incremental.range_selector import StatsRangeListenRangeSelector
 from listenbrainz_spark.stats.incremental.sitewide.artist import AritstSitewideEntity
+from listenbrainz_spark.stats.incremental.sitewide.artist_map import ArtistMapSitewideEntity, \
+    ArtistMapSitewideStatsMessageCreator
 from listenbrainz_spark.stats.incremental.sitewide.entity import SitewideEntityStatsQueryProvider, \
     SitewideEntityStatsMessageCreator
 from listenbrainz_spark.stats.incremental.sitewide.recording import RecordingSitewideEntity
@@ -28,6 +30,19 @@ def get_entity_stats(entity: str, stats_range: str) -> Iterator[Dict]:
     entity_cls = incremental_sitewide_map[entity]
     entity_obj: SitewideEntityStatsQueryProvider = entity_cls(selector, SITEWIDE_STATS_ENTITY_LIMIT)
     message_creator = SitewideEntityStatsMessageCreator(entity, selector)
-
     engine = IncrementalStatsEngine(entity_obj, message_creator)
-    return engine.run()
+
+    if entity == "artists":
+        artist_stats = engine.run()
+        for message in artist_stats:
+            yield message
+
+        artist_map_entity = ArtistMapSitewideEntity(selector, SITEWIDE_STATS_ENTITY_LIMIT)
+        artist_map_message_creator = ArtistMapSitewideStatsMessageCreator(selector)
+        artist_map_query = artist_map_entity.get_stats_query(engine._final_table, engine._cache_tables)
+        artist_map_results = run_query(artist_map_query)
+        artist_map_stats = engine.create_messages(artist_map_results, engine._only_inc, artist_map_message_creator)
+        for message in artist_map_stats:
+            yield message
+    else:
+        return engine.run()
