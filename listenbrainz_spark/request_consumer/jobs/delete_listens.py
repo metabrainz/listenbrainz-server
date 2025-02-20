8f0f7542c8f67875c398198f81825b0a9a2260b3
Kartik Ohri <kartikohri13@gmail.com>
2025-02-20 19:55:03 +0530

    fix import of deleted listens

--- listenbrainz_spark/request_consumer/jobs/delete_listens.py
+++ listenbrainz_spark/request_consumer/jobs/delete_listens.py
@@ -1,11 +1,41 @@
 import uuid
 
-from listenbrainz_spark import config
+from listenbrainz_spark import config, hdfs_connection
 from listenbrainz_spark.hdfs.utils import move
 from listenbrainz_spark.path import DELETED_LISTENS_SAVE_PATH, DELETED_USER_LISTEN_HISTORY_SAVE_PATH
 from listenbrainz_spark.postgres.utils import load_from_db
 from listenbrainz_spark.stats import run_query
 from listenbrainz_spark.utils import read_files_from_HDFS
+
+
+def combine_if_exists(new_df, save_path, combine_query_template):
+    """ If a dataframe already exists at the save path, load it and combine it with the new dataframe using the provided
+    query. Otherwise, save the new dataframe at the save path directly.
+    """
+    if hdfs_connection.client.status(save_path, strict=False):
+        existing_table = f"existing_df_{uuid.uuid4()}"
+        new_table = f"new_df_{uuid.uuid4()}"
+        existing_df = read_files_from_HDFS(save_path)
+        existing_df.createOrReplaceTempView(existing_table)
+        new_df.createOrReplaceTempView(new_table)
+
+        query = combine_query_template.format(existing_table=existing_table, new_table=new_table)
+        combined_df = run_query(query)
+
+        temp_df_path = f"/tmp/{str(uuid.uuid4())}"
+        combined_df \
+            .repartition(1) \
+            .write \
+            .mode("overwrite") \
+            .parquet(temp_df_path)
+
+        move(temp_df_path, save_path)
+    else:
+        new_df \
+            .repartition(1) \
+            .write \
+            .mode("overwrite") \
+            .parquet(save_path)
 
 
 def import_deleted_listens():
@@ -19,56 +49,30 @@ def import_deleted_listens():
          WHERE status = 'complete'::listen_delete_metadata_status_enum
     """
     new_listens_to_delete_df = load_from_db(config.TS_JDBC_URI, config.TS_USER, config.TS_PASSWORD, query)
-    existing_listens_to_delete_df = read_files_from_HDFS(DELETED_LISTENS_SAVE_PATH)
-
-    new_listens_to_delete_df.createOrReplaceTempView("new_listens_to_delete")
-    existing_listens_to_delete_df.createOrReplaceTempView("existing_listens_to_delete")
-
     columns = "id, user_id, listened_at, recording_msid, created"
-    all_listens_to_delete_df = run_query(f"""\
+    query = f"""\
         WITH intermediate AS (
-            SELECT {columns} FROM new_listens_to_delete
+            SELECT {columns} FROM {{new_table}}
              UNION ALL
-            SELECT {columns} FROM existing_listens_to_delete
+            SELECT {columns} FROM {{existing_table}}
         )
             SELECT {columns} FROM intermediate GROUP BY {columns}
-    """)
-
-    temp_df_path = f"/tmp/{str(uuid.uuid4())}"
-    all_listens_to_delete_df \
-        .repartition(1) \
-        .write \
-        .mode("overwrite") \
-        .parquet(temp_df_path)
-
-    move(temp_df_path, DELETED_LISTENS_SAVE_PATH)
+    """
+    combine_if_exists(new_listens_to_delete_df, DELETED_LISTENS_SAVE_PATH, query)
 
 
 def import_deleted_user_listen_history():
     query = """SELECT id, user_id, max_created FROM deleted_user_listen_history"""
     new_deleted_history_df = load_from_db(config.TS_JDBC_URI, config.TS_USER, config.TS_PASSWORD, query)
-    existing_deleted_history_df = read_files_from_HDFS(DELETED_USER_LISTEN_HISTORY_SAVE_PATH)
-
-    new_deleted_history_df.createOrReplaceTempView("new_listen_history_to_delete")
-    existing_deleted_history_df.createOrReplaceTempView("existing_listen_history_to_delete")
-
-    all_deleted_history_df = run_query("""\
+    query = """\
         WITH intermediate AS (
-            SELECT user_id, max_created FROM new_listen_history_to_delete
+            SELECT user_id, max_created FROM {new_table}
          UNION ALL
-            SELECT user_id, max_created FROM existing_listen_history_to_delete
+            SELECT user_id, max_created FROM {existing_table}
         )
            SELECT user_id, max(max_created) AS max_created FROM intermediate GROUP BY user_id
-    """)
-
-    temp_df_path = f"/tmp/{str(uuid.uuid4())}"
-    all_deleted_history_df \
-        .repartition(1) \
-        .write \
-        .mode("overwrite") \
-        .parquet(temp_df_path)
-
-    move(temp_df_path, DELETED_USER_LISTEN_HISTORY_SAVE_PATH)
+    """
+    combine_if_exists(new_deleted_history_df, DELETED_USER_LISTEN_HISTORY_SAVE_PATH, query)
 
 
 def main():
