3a36fde443816e59661b3e2f24d39128bcd00d3c
Kartik Ohri <kartikohri13@gmail.com>
2025-02-27 12:12:24 +0530

    Remove unused utils

--- listenbrainz_spark/utils.py
+++ listenbrainz_spark/utils.py
@@ -1,6 +1,4 @@
-import errno
 import logging
-import os
 
 from py4j.protocol import Py4JJavaError
 from pyspark.sql.utils import AnalysisException
@@ -11,25 +9,9 @@ from listenbrainz_spark.exceptions import (DataFrameNotAppendedException,
                                            DataFrameNotCreatedException,
                                            FileNotFetchedException,
                                            FileNotSavedException,
-                                           PathNotFoundException,
-                                           ViewNotRegisteredException)
+                                           PathNotFoundException)
 
 logger = logging.getLogger(__name__)
-
-# A typical listen is of the form:
-# {
-#   "artist_mbids": [],
-#   "artist_name": "Cake",
-#   "listened_at": "2005-02-28T20:39:08Z",
-#   "recording_msid": "c559b2f8-41ff-4b55-ab3c-0b57d9b85d11",
-#   "recording_mbid": "1750f8ca-410e-4bdc-bf90-b0146cb5ee35",
-#   "release_mbid": "",
-#   "release_name": null,
-#   "tags": [],
-#   "track_name": "Tougher Than It Is"
-#   "user_id": 5,
-# }
-# All the keys in the dict are column/field names in a Spark dataframe.
 
 
 def append(df, dest_path):
@@ -64,28 +46,6 @@ def create_dataframe(row, schema):
         raise DataFrameNotCreatedException(err.java_exception, row)
 
 
-def create_path(path):
-    try:
-        os.makedirs(path)
-    except OSError as e:
-        if e.errno != errno.EEXIST:
-            raise
-
-
-def register_dataframe(df, table_name):
-    """ Creates a view to be used for Spark SQL, etc. Replaces the view if a view with the
-        same name exists.
-
-        Args:
-            df (dataframe): Dataframe to register.
-            table_name (str): Name of the view.
-    """
-    try:
-        df.createOrReplaceTempView(table_name)
-    except Py4JJavaError as err:
-        raise ViewNotRegisteredException(err.java_exception, table_name)
-
-
 def read_files_from_HDFS(path):
     """ Loads the dataframe stored at the given path in HDFS.
 
@@ -103,7 +63,7 @@ def read_files_from_HDFS(path):
         raise FileNotFetchedException(err.java_exception, path)
 
 
-def save_parquet(df, path, mode='overwrite'):
+def save_parquet(df, path, mode="overwrite"):
     """ Save dataframe as parquet to given path in HDFS.
 
         Args:
@@ -112,20 +72,6 @@ def save_parquet(df, path, mode='overwrite'):
             mode (str): The mode with which to write the paquet.
     """
     try:
-        df.write.format('parquet').save(config.HDFS_CLUSTER_URI + path, mode=mode)
+        df.write.format("parquet").save(config.HDFS_CLUSTER_URI + path, mode=mode)
     except Py4JJavaError as err:
         raise FileNotSavedException(err.java_exception, path)
-
-
-def read_json(hdfs_path, schema):
-    """ Upload JSON file to HDFS as parquet.
-
-        Args:
-            hdfs_path (str): HDFS path to upload JSON.
-            schema: Blueprint of parquet.
-
-        Returns:
-            df (parquet): Dataframe.
-    """
-    df = listenbrainz_spark.session.read.json(config.HDFS_CLUSTER_URI + hdfs_path, schema=schema)
-    return df
