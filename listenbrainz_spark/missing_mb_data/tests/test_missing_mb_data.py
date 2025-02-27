2aa942f25465de469296af769465ab53ef6dc49e
Kartik Ohri <kartikohri13@gmail.com>
2025-02-28 02:26:25 +0530

    Fix remaining spark/stats tests

--- listenbrainz_spark/missing_mb_data/tests/test_missing_mb_data.py
+++ listenbrainz_spark/missing_mb_data/tests/test_missing_mb_data.py
@@ -7,11 +7,16 @@ from listenbrainz_spark.tests import SparkNewTestCase
 
 class MissingMBDataTestCase(SparkNewTestCase):
 
-    def test_get_data_missing_from_musicbrainz(self):
+    def setUp(self):
+        super().setUp()
         import_incremental_dump_to_hdfs(self.dump_loader, 5)
-        # use a very long day range so that listens are used
+
+    def tearDown(self):
+        self.delete_uploaded_listens()
+        super().tearDown()
+
+    def test_get_data_missing_from_musicbrainz(self):
         messages = missing_mb_data.main(10000)
-        with open(self.path_to_data_file('missing_musicbrainz_data.json')) as f:
+        with open(self.path_to_data_file("missing_musicbrainz_data.json")) as f:
             expected_missing_mb_data = json.load(f)
-        print(json.dumps(expected_missing_mb_data, indent=2))
         self.assertCountEqual(expected_missing_mb_data, messages)
