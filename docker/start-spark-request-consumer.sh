88fe2a14289ce4ce0dcb9cfe06f1a108c8b555d2
Kartik Ohri <kartikohri13@gmail.com>
2025-03-08 17:35:35 +0530

    use python 3.13

--- docker/start-spark-request-consumer.sh
+++ docker/start-spark-request-consumer.sh
@@ -1,16 +1,17 @@
 #!/bin/bash
 set -e
 
-source ~/.profile
-
 cd "$(dirname "${BASH_SOURCE[0]}")/../"
 
 rm -rf pyspark_venv pyspark_venv.tar.gz listenbrainz_spark_request_consumer.zip models.zip
 
-python3 -m venv pyspark_venv
+python3.13 -m venv pyspark_venv
 source pyspark_venv/bin/activate
 pip install setuptools wheel venv-pack -r requirements_spark.txt
 venv-pack -o pyspark_venv.tar.gz
+
+export PYSPARK_DRIVER_PYTHON=python3.13
+export PYSPARK_PYTHON=./environment/bin/python3.13
 
 GIT_COMMIT_SHA="$(git describe --tags --dirty --always)"
 echo "$GIT_COMMIT_SHA" > .git-version
