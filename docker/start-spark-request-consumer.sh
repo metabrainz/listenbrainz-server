414dd45b7206ff585e8b10a09ed9938570d8b90a
Kartik Ohri <kartikohri13@gmail.com>
2025-03-08 23:08:12 +0530

    try fixing pyspark_driver_python

--- docker/start-spark-request-consumer.sh
+++ docker/start-spark-request-consumer.sh
@@ -10,7 +10,7 @@ source pyspark_venv/bin/activate
 pip install setuptools wheel venv-pack -r requirements_spark.txt
 venv-pack -o pyspark_venv.tar.gz
 
-export PYSPARK_DRIVER_PYTHON=python3.13
+export PYSPARK_DRIVER_PYTHON=./environment/bin/python3.13
 export PYSPARK_PYTHON=./environment/bin/python3.13
 
 GIT_COMMIT_SHA="$(git describe --tags --dirty --always)"
