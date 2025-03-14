e6589c9b2e037bd083a1e995d97d25957ac229be
Kartik Ohri <kartikohri13@gmail.com>
2025-03-14 22:16:21 +0530

    try importing 4 chunks at once

--- listenbrainz_spark/mlhd/download.py
+++ listenbrainz_spark/mlhd/download.py
@@ -4,7 +4,8 @@ import shutil
 import tarfile
 import tempfile
 import time
-from datetime import datetime
+from concurrent.futures import ThreadPoolExecutor
+from datetime import datetime, timezone
 from glob import glob
 from pathlib import Path
 
@@ -146,20 +147,25 @@ def download_chunk(filename, dest) -> str:
     return download_path
 
 
+def process_chunk(filename):
+    with tempfile.TemporaryDirectory() as local_temp_dir:
+        downloaded_chunk = download_chunk(filename, local_temp_dir)
+        extract_chunk(filename, downloaded_chunk, local_temp_dir)
+        transform_chunk(local_temp_dir)
+
+
 def import_mlhd_dump_to_hdfs():
     """ Import the MLHD+ dump. """
     MLHD_PLUS_FILES = [f"mlhdplus-complete-{chunk}.tar" for chunk in MLHD_PLUS_CHUNKS]
 
     for filename in MLHD_PLUS_FILES:
-        with tempfile.TemporaryDirectory() as local_temp_dir:
-            downloaded_chunk = download_chunk(filename, local_temp_dir)
-            extract_chunk(filename, downloaded_chunk, local_temp_dir)
-            transform_chunk(local_temp_dir)
+        with ThreadPoolExecutor(max_workers=4) as executor:
+            executor.submit(process_chunk, filename)
 
     post_process_mlhd_plus()
     delete_dir(path.MLHD_PLUS_RAW_DATA_DIRECTORY, recursive=True)
 
     return [{
         'type': 'import_mlhd_dump',
-        'time': str(datetime.utcnow()),
+        'time': str(datetime.now(tz=timezone.utc).isoformat()),
     }]
