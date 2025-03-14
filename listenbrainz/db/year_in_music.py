8c436d06763045a1bdb3cb07832fb13b0bcd0892
junaid <shaikjunaid124@gmail.com>
2025-03-14 12:25:27 +0000

    escaping doublequote and backslash

--- listenbrainz/db/year_in_music.py
+++ listenbrainz/db/year_in_music.py
@@ -179,11 +179,9 @@ def send_mail(subject, to_name, to_email, content, html, logo, logo_cid):
 
 
 def sanitize_username(username):
-    weird_chars = {',',':',';','@','<','>','"','(',')','[',']','\\'}
-    for i in username:
-        if i in weird_chars:
-            return f'"{username}"'
-    return username
+    username.replace('\\','\\\\')
+    username.replace('"','\\"')
+    return f'"{username}"'
 
 
 def notify_yim_users(db_conn, ts_conn, year):
