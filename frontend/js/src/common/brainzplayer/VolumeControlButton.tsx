6dfb113a51c13b99adc44630ec4824b9a352282c
Mshahnawaz1 <shahnawaz919956@gmail.com>
2025-05-12 03:23:37 +0530

    FIX:LB-1793 Persistence volume

--- frontend/js/src/common/brainzplayer/VolumeControlButton.tsx
+++ frontend/js/src/common/brainzplayer/VolumeControlButton.tsx
@@ -1,20 +1,36 @@
 import * as React from "react";
-import { useBrainzPlayerDispatch } from "./BrainzPlayerContext";
+import {
+  useBrainzPlayerDispatch,
+  useBrainzPlayerContext,
+} from "./BrainzPlayerContext";
+
+const STORAGE_KEY = "brainzplayer-volume";
 
 function VolumeControlButton() {
   const dispatch = useBrainzPlayerDispatch();
+  const { isActivated } = useBrainzPlayerContext();
+  const [volume, setVolume] = React.useState(() => {
+    const savedVolume = localStorage.getItem(STORAGE_KEY);
+    return savedVolume !== null ? Number(savedVolume) : 100;
+  });
+
+  React.useEffect(() => {
+    if (isActivated) {
+      dispatch({ type: "VOLUME_CHANGE", data: volume });
+    }
+  }, [isActivated, volume, dispatch]);
+
   const handleVolumeChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    dispatch({
-      type: "VOLUME_CHANGE",
-      data: e.currentTarget?.value ?? 100,
-    });
+    const newVolume = Number(e.target.value);
+    setVolume(newVolume);
+    localStorage.setItem(STORAGE_KEY, newVolume.toString());
   };
   return (
     <input
       onChange={handleVolumeChange}
       className="volume-input"
       type="range"
-      defaultValue="100"
+      value={volume}
       max="100"
       min="0"
       step="5"
