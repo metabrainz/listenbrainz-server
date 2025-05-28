3333a598966a9657e4be7b47ebeebc66191a9ab1
Monkey Do <salicon.petiller@gmail.com>
2025-05-28 13:26:29 +0200

    Rework ImportStatus component UI

--- frontend/js/src/settings/music-services/details/components/ImportStatus.tsx
+++ frontend/js/src/settings/music-services/details/components/ImportStatus.tsx
@@ -1,20 +1,23 @@
 import * as React from "react";
 import { toast } from "react-toastify";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faRefresh, faTimes } from "@fortawesome/free-solid-svg-icons";
+import {
+  faArrowRightLong,
+  faChevronCircleRight,
+  faRefresh,
+} from "@fortawesome/free-solid-svg-icons";
 import { ToastMsg } from "../../../../notifications/Notifications";
 import Loader from "../../../../components/Loader";
 import GlobalAppContext from "../../../../utils/GlobalAppContext";
 
 type ImportStatusProps = {
-  onClose: () => void;
   serviceName: ImportService;
 };
-
-export default function ImportStatus({
-  onClose,
-  serviceName,
-}: ImportStatusProps) {
+export enum ImportStatusT {
+  inProgress = "Importing",
+  complete = "Synced",
+}
+export default function ImportStatus({ serviceName }: ImportStatusProps) {
   const [loading, setLoading] = React.useState(false);
   const [
     importData,
@@ -51,58 +54,66 @@ export default function ImportStatus({
     fetchStatus();
   }, [fetchStatus]);
 
+  let bsColorClass = "secondary";
+  if (importData?.status?.state === ImportStatusT.complete) {
+    bsColorClass = "success";
+  } else if (importData?.status?.state === ImportStatusT.inProgress) {
+    bsColorClass = "info";
+  }
+
+  const statusString = importData?.status?.state ?? "N/A";
+
   return (
-    <div className="panel panel-info">
-      <div className="panel-heading">
-        <div className="pull-right">
-          <button
-            type="button"
-            className="btn btn-default btn-xs"
-            onClick={fetchStatus}
-            disabled={loading}
-            title="Refresh Status"
-            style={{ marginLeft: "5px" }}
-          >
-            <FontAwesomeIcon icon={faRefresh} spin={loading} />
-          </button>
-          <button
-            type="button"
-            className="btn btn-danger btn-xs"
-            onClick={onClose}
-            title="Close Status"
-          >
-            <FontAwesomeIcon icon={faTimes} />
-          </button>
+    <div className={`alert alert-${bsColorClass}`} role="alert">
+      <details>
+        <summary>
+          <h4 className="alert-heading" style={{ marginBottom: 0 }}>
+            <button
+              type="button"
+              className="btn btn-sm btn-transparent pull-right"
+              onClick={fetchStatus}
+              disabled={loading}
+              title="Refresh status"
+            >
+              <FontAwesomeIcon icon={faRefresh} spin={loading} />
+            </button>
+            <FontAwesomeIcon
+              icon={faChevronCircleRight}
+              size="sm"
+              className="summary-indicator"
+            />{" "}
+            Import status{importData?.status?.state ? `: ${statusString}` : ""}
+          </h4>
+        </summary>
+        <div className="alert-body">
+          {loading && (
+            <div style={{ textAlign: "center", margin: "1em 0" }}>
+              <Loader isLoading={loading} />
+            </div>
+          )}
+
+          {!loading && !importData && (
+            <p className="text-muted">No active import data found.</p>
+          )}
+
+          {!loading && importData && (
+            <dl className="row mt-10">
+              <dt className="col-xs-6">Status:</dt>
+              <dd className="col-xs-6">{importData?.status?.state ?? "N/A"}</dd>
+              <dt className="col-xs-6">Listens Imported:</dt>
+              <dd className="col-xs-6">{importData?.status?.count ?? 0}</dd>
+              <dt className="col-xs-6">Service:</dt>
+              <dd className="col-xs-6">{serviceName}</dd>
+              <dt className="col-xs-6">Timestamp of last imported listen:</dt>
+              <dd className="col-xs-6">
+                {new Date(
+                  (importData?.latest_import ?? 0) * 1000
+                ).toLocaleString()}
+              </dd>
+            </dl>
+          )}
         </div>
-        <h3 className="panel-title">Import Status</h3>
-      </div>
-
-      <div className="panel-body">
-        {loading && (
-          <div style={{ textAlign: "center", margin: "1em 0" }}>
-            <Loader isLoading={loading} />
-          </div>
-        )}
-
-        {!loading && !importData && (
-          <p className="text-muted">No active import data found.</p>
-        )}
-
-        {!loading && importData && (
-          <dl className="row">
-            <dt className="col-xs-4">Service:</dt>
-            <dd className="col-xs-8">{serviceName}</dd>
-            <dt className="col-xs-4">Status:</dt>
-            <dd className="col-xs-8">{importData.status?.state ?? "N/A"}</dd>
-            <dt className="col-xs-4">Listens Imported:</dt>
-            <dd className="col-xs-8">{importData.status?.count ?? 0}</dd>
-            <dt className="col-xs-4">Timestamp of last imported listen:</dt>
-            <dd className="col-xs-8">
-              {new Date(importData.latest_import * 1000 ?? 0).toLocaleString()}
-            </dd>
-          </dl>
-        )}
-      </div>
+      </details>
     </div>
   );
 }
