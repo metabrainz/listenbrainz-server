68b0cd26ea634b687c1ff409f3466b1a223acd1b
Suvid Singhal <suvidsinghal@gmail.com>
2025-06-10 07:46:18 +0000

    Used the existing listens_importer model

--- listenbrainz/webserver/views/import.py
+++ listenbrainz/webserver/views/import.py
@@ -17,25 +17,47 @@ import_bp = Blueprint("import", __name__)
 @import_bp.post("/")
 @api_login_required
 @web_listenstore_needed
-def create_import_task(file_path):
+def create_import_task(file_path, service):
     """ Add a request to import the listening history data in background. """
     try:
-        query = """
-            INSERT INTO user_data_import (user_id, file_path, type, status, progress)
-                 VALUES (:user_id, :file_path, :type, 'waiting', :progress)
-            ON CONFLICT (user_id, type)
-                  WHERE status = 'waiting' OR status = 'in_progress'
-             DO NOTHING
-              RETURNING id, type, created, progress, status
-        """
-        result = db_conn.execute(text(query), {
-            "user_id": current_user.id,
-            "type": "import_all_user_data",
-            "file_path": file_path,
-            "progress": "Your data import will start soon."
-        })
-        import_task = result.first()
 
+        fetch_oauth_query = text("""
+            SELECT id FROM external_service_oauth
+            WHERE user_id = :user_id AND service = :service
+        """)
+        result = db_conn.execute(fetch_oauth_query, {
+            "user_id": current_user.id,
+            "service": service
+        }).first()
+
+        if result:
+            external_oauth_id = result.id
+        else:
+            insert_oauth_query = text("""
+                INSERT INTO external_service_oauth (user_id, service, last_updated)
+                VALUES (:user_id, :service, NOW())
+                RETURNING id
+            """)
+            result = db_conn.execute(insert_oauth_query, {
+                "user_id": current_user.id,
+                "service": service
+            }).first()
+            external_oauth_id = result.id
+
+        insert_importer_query = text("""
+            INSERT INTO listens_importer (
+                external_service_oauth_id, user_id, service, last_updated, status
+            )
+            VALUES (:external_id, :user_id, :service, NOW(), :progress)
+        """)
+        import_task = db_conn.execute(insert_importer_query, {
+            "external_id": external_oauth_id,
+            "user_id": current_user.id,
+            "service": service,
+            "progress": "Your data import will start soon."
+        }).first()
+
+        
         if import_task is not None:
             query = "INSERT INTO background_tasks (user_id, task, metadata) VALUES (:user_id, :task, :metadata) ON CONFLICT DO NOTHING RETURNING id"
             result = db_conn.execute(text(query), {
@@ -48,9 +70,8 @@ def create_import_task(file_path):
                 db_conn.commit()
                 return jsonify({
                     "import_id": import_task.id,
-                    "type": import_task.type,
-                    "created": import_task.created.isoformat(),
-                    "progress": import_task.progress,
+                    "service": import_task.service,
+                    "last_updated": import_task.last_updated.isoformat(),
                     "status": import_task.status,
                 })
 
@@ -68,17 +89,15 @@ def create_import_task(file_path):
 def list_export_tasks():
     """ Retrieve the all import tasks for the current user """
     result = db_conn.execute(
-        text("SELECT * FROM user_data_import WHERE user_id = :user_id ORDER BY created DESC"),
+        text("SELECT * FROM listens_importer WHERE user_id = :user_id ORDER BY last_updated DESC"),
         {"user_id": current_user.id}
     )
     rows = result.mappings().all()
     return jsonify([{
         "import_id": row.id,
-        "type": row.type,
-        "created": row.created.isoformat(),
-        "progress": row.progress,
+        "service": row.service,
+        "last_updated": row.last_updated.isoformat(),
         "status": row.status,
-        "file_path": row.file_path,
     } for row in rows])
 
 
@@ -88,7 +107,7 @@ def list_export_tasks():
 def get_import_task(import_id):
     """ Retrieve the requested import's data if it belongs to the specified user """
     result = db_conn.execute(
-        text("SELECT * FROM user_data_import WHERE user_id = :user_id AND id = :import_id"),
+        text("SELECT * FROM listens_importer WHERE user_id = :user_id AND id = :import_id"),
         {"user_id": current_user.id, "import_id": import_id}
     )
     row = result.first()
@@ -96,9 +115,7 @@ def get_import_task(import_id):
         raise APINotFound("Import not found")
     return jsonify({
         "import_id": row.id,
-        "type": row.type,
-        "created": row.created.isoformat(),
-        "progress": row.progress,
+        "service": row.service,
+        "last_updated": row.last_updated.isoformat(),
         "status": row.status,
-        "file_path": row.file_path,
     })
