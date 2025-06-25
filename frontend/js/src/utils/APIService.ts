ce979cba375636414e9f22003c804ccf7d6bb5b6
holycow23 <granthbagadia2004@gmail.com>
2025-06-25 18:59:29 +0000

    sitewide removed

--- frontend/js/src/utils/APIService.ts
+++ frontend/js/src/utils/APIService.ts
@@ -607,12 +607,7 @@ export default class APIService {
     range: UserStatsAPIRange = "all_time"
   ): Promise<UserGenreDayActivityResponse> => {
     let url;
-    if (userName) {
-      url = `${this.APIBaseURI}/stats/user/${userName}/genre-activity`;
-    } else {
-      url = `${this.APIBaseURI}/stats/sitewide/genre-activity`;
-    }
-    url += `?range=${range}`;
+	url = `${this.APIBaseURI}/stats/user/${userName}/genre-activity?range=${range}`;
     const response = await fetch(url);
     await this.checkStatus(response);
     if (response.status === 204) {
