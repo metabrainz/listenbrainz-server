a69eb946477ca9e1bb566f367513cedb30921b4f
Mohammad Amanullah <ullahaman072003@gmail.com>
2025-06-25 23:10:47 +0530

    updated db schema (fw_servers,tokens)

--- admin/sql/updates/2025-06-01-add-funkwhale-servers-table.sql
+++ admin/sql/updates/2025-06-01-add-funkwhale-servers-table.sql
@@ -1,17 +1,16 @@
 BEGIN;
 
+DROP TABLE IF EXISTS funkwhale_servers CASCADE;
+
 CREATE TABLE funkwhale_servers (
-    user_id                 INTEGER NOT NULL,
-    host_url                TEXT NOT NULL,
-    client_id               TEXT,
-    client_secret           TEXT,
-    access_token            TEXT,
-    refresh_token           TEXT,
-    token_expiry            TIMESTAMP WITH TIME ZONE,
-    PRIMARY KEY (user_id, host_url)
+    id          SERIAL PRIMARY KEY,
+    host_url    TEXT NOT NULL UNIQUE,
+    client_id   TEXT,
+    client_secret TEXT,
+    scopes      TEXT,
+    created     TIMESTAMP WITH TIME ZONE DEFAULT NOW()
 );
 
-CREATE INDEX user_id_ndx_funkwhale_servers ON funkwhale_servers (user_id);
 CREATE INDEX host_url_ndx_funkwhale_servers ON funkwhale_servers (host_url);
 
 COMMIT;
