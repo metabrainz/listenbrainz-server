f6f6bc9ad2f16671e52b33ae709a2aff4c1c3d08
RayyanSeliya <rayyanseliya786@gmail.com>
2025-07-23 01:36:34 +0530

    add cors support for IA search api  using @crossdomain decorator

--- listenbrainz/webserver/views/internet_archive_api.py
+++ listenbrainz/webserver/views/internet_archive_api.py
@@ -1,10 +1,12 @@
 from flask import Blueprint, request, jsonify
 from listenbrainz.webserver import ts_conn
+from listenbrainz.webserver.decorators import crossdomain
 from listenbrainz.metadata_cache.internetarchive.store import search_ia_tracks
 
 internet_archive_api_bp = Blueprint('internet_archive_api', __name__)
 
 @internet_archive_api_bp.get('/search')
+@crossdomain
 def search_ia():
     artist = request.args.get('artist')
     track = request.args.get('track')
