6a03e95f27d484f3a14d93b1af4c1a27fe46eb5a
holycow23 <granthbagadia2004@gmail.com>
2025-08-20 03:45:21 +0000

    fix: Artist Evolution dataset naming fix

--- listenbrainz/spark/spark_dataset.py
+++ listenbrainz/spark/spark_dataset.py
@@ -166,7 +166,7 @@ class _UserStatsDataset(_StatsDataset):
 UserEntityStatsDataset = _UserStatsDataset("user_entity")
 DailyActivityStatsDataset = _UserStatsDataset("user_daily_activity")
 ListeningActivityStatsDataset = _UserStatsDataset("user_listening_activity")
-ArtistEvolutionStatsDataset = _UserStatsDataset("user_artist_evolution")
+ArtistEvolutionStatsDataset = _UserStatsDataset("user_artist_evolution_activity")
 GenreActivityStatsDataset = _UserStatsDataset("user_genre_activity")
 
 class _EntityListenerStatsDataset(_StatsDataset):
