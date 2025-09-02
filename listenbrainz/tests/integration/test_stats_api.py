32f8420b2e37c055846e92c0ecfdda445db72200
Granth Bagadia <66285223+granth23@users.noreply.github.com>
2025-09-02 21:01:05 +0530

    fix: duplicate functions removed

--- listenbrainz/tests/integration/test_stats_api.py
+++ listenbrainz/tests/integration/test_stats_api.py
@@ -272,32 +272,21 @@ class StatsAPITestCase(IntegrationTestCase):
         self.assert200(response)
         received = json.loads(response.data)['payload']
         sent = request[0]
-
-        self.assertEqual(sent['from_ts'], received['from_ts'])
-        self.assertEqual(sent['to_ts'], received['to_ts'])
-
-          
-    def assertGenreActivityEqual(self, request, response):
-        self.assert200(response)
-        received = json.loads(response.data)['payload']
-        sent = request[0]
-
+    
         self.assertEqual(sent['from_ts'], received['from_ts'])
         self.assertEqual(sent['to_ts'], received['to_ts'])
         self.assertEqual(sent['data'], received['genre_activity'])
         self.assertEqual(self.user['musicbrainz_id'], received['user_id'])
-
-
+    
     def assertEraActivityEqual(self, request, response):
         self.assert200(response)
         received = json.loads(response.data)['payload']
         sent = request[0]
-
+    
         self.assertEqual(sent['from_ts'], received['from_ts'])
         self.assertEqual(sent['to_ts'], received['to_ts'])
         self.assertEqual(sent['data'], received['era_activity'])
         self.assertEqual(self.user['musicbrainz_id'], received['user_id'])
-
 
     def assertArtistMapEqual(self, request, response):
         self.assert200(response)
