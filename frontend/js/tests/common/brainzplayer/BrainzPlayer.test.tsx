7b244ceae03b138fc4fdae33cf57a9cded07cf89
Monkey Do <salicon.petiller@gmail.com>
2025-10-14 14:39:25 +0200

    Fix BrainzPlayer tests

--- frontend/js/tests/common/brainzplayer/BrainzPlayer.test.tsx
+++ frontend/js/tests/common/brainzplayer/BrainzPlayer.test.tsx
@@ -1,8 +1,9 @@
 import * as React from "react";
 
 import fetchMock from "jest-fetch-mock";
-import { screen } from "@testing-library/react";
+import { screen, waitFor } from "@testing-library/react";
 import userEvent from "@testing-library/user-event";
+import { Provider as JotaiProvider, createStore } from "jotai";
 import BrainzPlayer, {
   DataSourceType,
 } from "../../../src/common/brainzplayer/BrainzPlayer";
@@ -14,25 +15,23 @@ import { renderWithProviders } from "../../test-utils/rtl-test-utils";
 import { listenOrJSPFTrackToQueueItem } from "../../../src/common/brainzplayer/utils";
 import IntersectionObserver from "../../__mocks__/intersection-observer";
 import { ReactQueryWrapper } from "../../test-react-query";
-import { queueAtom, ambientQueueAtom, BrainzPlayerContextT } from "../../../src/common/brainzplayer/BrainzPlayerAtoms";
-import { Provider as JotaiProvider } from "jotai";
-import { createStore } from "jotai";
-import { currentListenAtom, currentTrackNameAtom, currentTrackArtistAtom, playerPausedAtom } from "../../../src/common/brainzplayer/BrainzPlayerAtoms";
-
-// Font Awesome generates a random hash ID for each icon everytime.
-// Mocking Math.random() fixes this
-// https://github.com/FortAwesome/react-fontawesome/issues/194#issuecomment-627235075
-jest.spyOn(global.Math, "random").mockImplementation(() => 0);
+import {
+  queueAtom,
+  ambientQueueAtom,
+  BrainzPlayerContextT,
+  currentListenAtom,
+  currentTrackNameAtom,
+  currentTrackArtistAtom,
+  playerPausedAtom,
+  currentDataSourceNameAtom,
+  isActivatedAtom,
+} from "../../../src/common/brainzplayer/BrainzPlayerAtoms";
 
 const spotifyAccountWithPermissions = {
   access_token: "haveyouseenthefnords",
   permission: ["streaming", "user-read-email", "user-read-private"] as Array<
     SpotifyPermission
   >,
-};
-
-const soundcloudPermissions = {
-  access_token: "ihavenotseenthefnords",
 };
 
 const GlobalContextMock: { context: GlobalAppContextT } = {
@@ -47,7 +46,7 @@ const GlobalContextMock: { context: GlobalAppContextT } = {
       ] as Array<SpotifyPermission>,
     },
     soundcloudAuth: {
-      access_token: "heyo-soundcloud",
+      access_token: "ihavenotseenthefnords",
     },
     youtubeAuth: {
       api_key: "fake-api-key",
@@ -59,9 +58,6 @@ const GlobalContextMock: { context: GlobalAppContextT } = {
     ),
   },
 };
-
-const useBrainzPlayerDispatch = jest.fn();
-const useBrainzPlayerContext = jest.fn();
 
 // Give yourself a two minute break and go listen to this gem
 // https://musicbrainz.org/recording/7fcaf5b3-e682-4ce6-be61-d3bce775a43f
@@ -80,9 +76,13 @@ const listen2 = listenOrJSPFTrackToQueueItem({
     track_name: "Never Gonna Give You Up",
   },
 });
+const store = createStore();
 
-function BrainzPlayerWithWrapper({additionalContextValues}: {additionalContextValues?: Partial<BrainzPlayerContextT>}) {
-  const store = createStore();
+function BrainzPlayerWithWrapper({
+  additionalContextValues,
+}: {
+  additionalContextValues?: Partial<BrainzPlayerContextT>;
+}) {
   if (additionalContextValues?.currentListen) {
     store.set(currentListenAtom, additionalContextValues.currentListen);
   }
@@ -90,7 +90,10 @@ function BrainzPlayerWithWrapper({additionalContextValues}: {additionalContextVa
     store.set(currentTrackNameAtom, additionalContextValues.currentTrackName);
   }
   if (additionalContextValues?.currentTrackArtist) {
-    store.set(currentTrackArtistAtom, additionalContextValues.currentTrackArtist);
+    store.set(
+      currentTrackArtistAtom,
+      additionalContextValues.currentTrackArtist
+    );
   }
   if (additionalContextValues?.playerPaused) {
     store.set(playerPausedAtom, additionalContextValues.playerPaused);
@@ -101,6 +104,9 @@ function BrainzPlayerWithWrapper({additionalContextValues}: {additionalContextVa
   if (additionalContextValues?.ambientQueue) {
     store.set(ambientQueueAtom, additionalContextValues.ambientQueue);
   }
+  if (additionalContextValues?.isActivated) {
+    store.set(isActivatedAtom, additionalContextValues.isActivated);
+  }
 
   return (
     <JotaiProvider store={store}>
@@ -109,8 +115,6 @@ function BrainzPlayerWithWrapper({additionalContextValues}: {additionalContextVa
   );
 }
 
-const mockDispatch = jest.fn();
-
 jest.mock("react-router", () => ({
   ...jest.requireActual("react-router"),
   useLocation: () => ({
@@ -118,30 +122,18 @@ jest.mock("react-router", () => ({
   }),
 }));
 
+const stopOtherBrainzPlayersMock = jest.fn();
+jest.mock("../../../src/common/brainzplayer/hooks/useCrossTabSync", () => ({
+  __esModule: true,
+  default: () => ({ stopOtherBrainzPlayers: stopOtherBrainzPlayersMock }),
+}));
+
 describe("BrainzPlayer", () => {
   beforeEach(() => {
-    (useBrainzPlayerContext as jest.MockedFunction<
-      typeof useBrainzPlayerContext
-    >).mockReturnValue({});
-
-    (useBrainzPlayerDispatch as jest.MockedFunction<
-      typeof useBrainzPlayerDispatch
-    >).mockReturnValue(mockDispatch);
-
-    Object.defineProperty(window, "localStorage", {
-      value: {
-        getItem: jest.fn(() => null),
-        setItem: jest.fn(() => null),
-      },
-      writable: true,
-    });
+    jest.clearAllMocks();
   });
   beforeAll(() => {
-    window.location.href = "http://nevergonnagiveyouup.com";
-
     global.IntersectionObserver = IntersectionObserver;
-    window.HTMLElement.prototype.scrollIntoView = jest.fn();
-
     fetchMock.enableMocks();
   });
 
@@ -236,7 +228,6 @@ describe("BrainzPlayer", () => {
       />,
       {
         ...GlobalContextMock.context,
-        spotifyAuth: spotifyAccountWithPermissions,
       },
       {
         wrapper: ReactQueryWrapper,
@@ -245,19 +236,29 @@ describe("BrainzPlayer", () => {
 
     const playButton = screen.getByTestId("bp-play-button");
     await user.click(playButton);
+    await waitFor(() => {
+      expect(store.get(isActivatedAtom)).toBe(true);
+    });
 
-    // Now the queue should have the second listen item
     let queueList = screen.getByTestId("queue");
     expect(queueList).toBeInTheDocument();
-    expect(queueList.innerHTML).toContain("Rick Astley");
+    expect(queueList.innerHTML).toContain("Bird's Lament");
+    expect(queueList.innerHTML).toContain("Never Gonna Give You Up");
+
+    // Now the queue should have the second listen item
+    await waitFor(() => {
+      expect(queueList.innerHTML).not.toContain("Bird's Lament");
+    });
+    expect(queueList.innerHTML).toContain("Never Gonna Give You Up");
 
     // Now click on the next button
     const nextButton = screen.getByTestId("bp-next-button");
     await user.click(nextButton);
-
-    // Now check if the queue is empty
-    queueList = screen.getByTestId("queue");
-    expect(queueList.innerHTML).toContain("Nothing in this queue yet");
+    await waitFor(() => {
+      // Now check if the queue is empty
+      queueList = screen.getByTestId("queue");
+      expect(queueList.innerHTML).toContain("Nothing in this queue yet");
+    });
   });
 
   test("previous track from queue is being played correctly", async () => {
@@ -287,37 +288,56 @@ describe("BrainzPlayer", () => {
     const previousButton = screen.getByTestId("bp-previous-button");
     await user.click(previousButton);
 
-    // Now check if the queue should be empty as the previous track wraped around to the end
-    queueList = screen.getByTestId("queue");
-    expect(queueList).toBeInTheDocument();
-    expect(queueList.innerHTML).toContain("Nothing in this queue yet");
+    await waitFor(() => {
+      // Now check if the queue should be empty as the previous track wraped around to the end
+      queueList = screen.getByTestId("queue");
+      expect(queueList.innerHTML).toContain("Never Gonna Give You Up");
+    });
   });
 
-  test("localstorage brainzplayer stop time should be updated", async () => {
+  test("tells other brainzplayer to stop playback", async () => {
+    store.set(currentDataSourceNameAtom, "youtube");
     renderWithProviders(
       <BrainzPlayerWithWrapper
         additionalContextValues={{
           currentListen: listen,
+          currentListenIndex: 0,
           queue: [listen, listen2],
+          isActivated: true,
         }}
       />,
       {
         ...GlobalContextMock.context,
-        spotifyAuth: spotifyAccountWithPermissions,
+        userPreferences: {
+          brainzplayer: {
+            youtubeEnabled: true,
+            spotifyEnabled: false,
+            soundcloudEnabled: false,
+            internetArchiveEnabled: false,
+            funkwhaleEnabled: false,
+            navidromeEnabled: false,
+            appleMusicEnabled: false,
+          },
+        },
       },
       {
         wrapper: ReactQueryWrapper,
       }
     );
 
+    const nextButton = screen.getByTestId("bp-next-button");
     const playButton = screen.getByTestId("bp-play-button");
-    await user.click(playButton);
 
-    expect(window.localStorage.setItem).toHaveBeenCalledTimes(1);
-
+    // Player is already activated (by setting jotai store), so just click on the next button
+    await user.click(nextButton);
+    // await waitFor(() => {
+    expect(stopOtherBrainzPlayersMock).toHaveBeenCalledTimes(1);
+    // });
     // Now click on the pause button
     await user.click(playButton);
 
-    expect(window.localStorage.setItem).toHaveBeenCalledTimes(2);
+    await waitFor(() => {
+      expect(stopOtherBrainzPlayersMock).toHaveBeenCalledTimes(2);
+    });
   });
 });
