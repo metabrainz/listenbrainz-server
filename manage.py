25de80908d3daa1e16f9c8b46ce4995e22ec7480
Karan Gayaki <karangayaki48@gmail.com>
2026-01-09 19:56:00 +0530

    Improve manage.py CLI usability with command documentation
    This PR improves the usability of the ListenBrainz management CLI
    by adding a short description of available commands in manage.py.
    
    This helps new contributors and users understand which commands
    are supported and reduces confusion when using the CLI.
    
    No functional behavior is changed.

--- manage.py
+++ manage.py
@@ -2,7 +2,14 @@ import listenbrainz.spark.request_manage as spark_request_manage
 from listenbrainz.dumps import manager as dump_manager
 from listenbrainz.manage import cli
 
-# Add other commands here
+'''
+ListenBrainz management CLI.
+Available commands:
+
+  spark : Manage Spark-related requests
+  dump  : Manage database dumps
+'''
+
 cli.add_command(spark_request_manage.cli, name="spark")
 cli.add_command(dump_manager.cli, name="dump")
 
