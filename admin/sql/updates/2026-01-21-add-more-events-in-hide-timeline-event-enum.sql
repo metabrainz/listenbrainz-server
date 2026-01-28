8e312bc34b670fed176199fbdeea4d0b03bda892
Monkey Do <MonkeyDo@users.noreply.github.com>
2026-01-28 11:56:06 +0100

    Add transaction to SQL file

--- admin/sql/updates/2026-01-21-add-more-events-in-hide-timeline-event-enum.sql
+++ admin/sql/updates/2026-01-21-add-more-events-in-hide-timeline-event-enum.sql
@@ -1,4 +1,7 @@
+BEGIN;
+
 ALTER TYPE hide_user_timeline_event_type_enum ADD VALUE 'notification';
 ALTER TYPE hide_user_timeline_event_type_enum ADD VALUE 'thanks';
 ALTER TYPE hide_user_timeline_event_type_enum ADD VALUE 'critiquebrainz_review';
 
+COMMIT;
