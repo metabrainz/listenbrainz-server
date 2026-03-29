ed7fbd8eb8a1aeceee9e59d9b2946d4b39195c92
Gopal Tengale <gopalrtengale0106@gmail.com>
2026-03-29 08:02:55 +0530

    LB-1651: Fix captionBgColor format to #rrggbb for color picker, restore fill-opacity in preview

--- frontend/js/src/explore/art-creator/components/Preview.tsx
+++ frontend/js/src/explore/art-creator/components/Preview.tsx
@@ -102,7 +102,7 @@ const Preview = React.forwardRef(function PreviewComponent(
           }
           ${
             captionBgColor
-              ? `.caption rect { fill: ${captionBgColor} !important; }`
+              ? `.caption rect { fill: ${captionBgColor} !important; fill-opacity: 0.48; }`
               : ""
           }
         `}
