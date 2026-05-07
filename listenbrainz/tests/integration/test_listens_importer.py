8f0933299642b23ff73bd222ccf4939d3f42779d
Monkey Do <salicon.petiller@gmail.com>
2026-05-07 20:04:59 +0200

    Update test for skipped spotify imported listens
    Account for the new detailed message, and fix the loose comparison of attempted imports with the actual expected value

--- listenbrainz/tests/integration/test_listens_importer.py
+++ listenbrainz/tests/integration/test_listens_importer.py
@@ -476,9 +476,11 @@ class ImportTestCase(ListenAPIIntegrationTestCase):
         metadata = response.json["metadata"]
         self.assertIn("attempted_count", metadata)
         self.assertIn("success_count", metadata)
-        self.assertEqual(metadata["success_count"], 2)
+        self.assertIn("detailed_message", metadata)
         # More tracks were attempted but filtered during processing
-        self.assertGreaterEqual(metadata["attempted_count"], 2)
+        self.assertEqual(metadata["success_count"], 2)
+        self.assertEqual(metadata["attempted_count"], 15)
+        self.assertEqual(metadata["detailed_message"], "1 skipped (incognito mode), 12 skipped (short duration)")
 
 
     def test_import_listenbrainz(self):
