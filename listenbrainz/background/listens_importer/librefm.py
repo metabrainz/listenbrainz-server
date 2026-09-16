211ec53b804625b9a97397a33303808b14a22056
Monkey Do <salicon.petiller@gmail.com>
2026-09-16 13:01:52 +0200

    Fix Libre.fm importer name
    capiitalize the same as Last.fm

--- listenbrainz/background/listens_importer/librefm.py
+++ listenbrainz/background/listens_importer/librefm.py
@@ -13,7 +13,7 @@ class LibrefmListensImporter(BaseListensImporter):
     """Libre.fm-specific listens importer."""
     def __init__(self, db_conn, ts_conn):
         super().__init__(db_conn, ts_conn)
-        self.importer_name = "Libre.Fm Archive Importer"
+        self.importer_name = "Libre.fm Archive Importer"
 
     def process_import_file(self, import_task: dict[str, Any]) -> Iterator[list[dict[str, Any]]]:
         """Processes the libre.fm csv archive file and returns a generator of batches of items."""
