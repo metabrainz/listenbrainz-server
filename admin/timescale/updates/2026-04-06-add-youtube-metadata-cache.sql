9270a360173e4a68af89e5f4448e6acecd36d829
Monkey Do <MonkeyDo@users.noreply.github.com>
2026-09-25 15:45:57 +0200

    Remove duplicate youtube_cache index
    Index is defined explicitly right below the table definition, line 14

--- admin/timescale/updates/2026-04-06-add-youtube-metadata-cache.sql
+++ admin/timescale/updates/2026-04-06-add-youtube-metadata-cache.sql
@@ -4,7 +4,7 @@ CREATE SCHEMA youtube_cache;
 
 CREATE TABLE youtube_cache.video (
     id            INTEGER GENERATED ALWAYS AS IDENTITY NOT NULL,
-    video_id      TEXT UNIQUE NOT NULL,
+    video_id      TEXT NOT NULL,
     title         TEXT NOT NULL,
     channel_name  TEXT NOT NULL,
     duration_ms   INTEGER,
